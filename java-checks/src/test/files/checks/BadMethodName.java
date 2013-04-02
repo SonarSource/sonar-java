7a7e4b5331a74d2e9736c13142b2f9990cf11250
Evgeny Mandrikov <mandrikov@gmail.com>
2013-04-02 15:13:53 +0200

    SONARJAVA-118 Rule S00100: Method names should comply with a naming convention

---
+++ java-checks/src/test/files/checks/BadMethodName.java
@@ -0,0 +1,7 @@
+class BadMethodName {
+  void Bad() {
+  }
+
+  void good() {
+  }
+}
--- sonar-findbugs-plugin/src/test/java/org/sonar/plugins/findbugs/SonarWayWithFindbugsProfileTest.java
+++ sonar-findbugs-plugin/src/test/java/org/sonar/plugins/findbugs/SonarWayWithFindbugsProfileTest.java
@@ -34,7 +34,7 @@ public class SonarWayWithFindbugsProfileTest {
     ValidationMessages validation = ValidationMessages.create();
     RulesProfile profile = sonarWayWithFindbugs.createProfile(validation);
     assertThat(profile.getActiveRulesByRepository(FindbugsConstants.REPOSITORY_KEY))
-        .hasSize(404);
+        .hasSize(403);
     assertThat(validation.hasErrors()).isFalse();
   }
 
