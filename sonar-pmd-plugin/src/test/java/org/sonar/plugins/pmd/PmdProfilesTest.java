1f7710db0b82c0ec07ad8dbda7e5e12e3f419dcc
Evgeny Mandrikov <mandrikov@gmail.com>
2013-04-04 14:03:35 +0200

    SONARJAVA-152 Deprecate PMD rule AvoidDollarSigns in favor of S00114 - S00117

--- sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdProfilesTest.java
+++ sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdProfilesTest.java
@@ -45,7 +45,7 @@ public class PmdProfilesTest {
     RulesProfile profile = sonarWay.createProfile(validation);
 
     assertThat(profile.getActiveRulesByRepository(PmdConstants.REPOSITORY_KEY).size())
-        .isEqualTo(71);
+        .isEqualTo(70);
     assertThat(validation.hasErrors()).isFalse();
   }
 
@@ -56,7 +56,7 @@ public class PmdProfilesTest {
     RulesProfile profile = sonarWayWithFindbugs.createProfile(validation);
 
     assertThat(profile.getActiveRulesByRepository(PmdConstants.REPOSITORY_KEY).size())
-        .isEqualTo(71);
+        .isEqualTo(70);
     assertThat(validation.hasErrors()).isFalse();
   }
 
