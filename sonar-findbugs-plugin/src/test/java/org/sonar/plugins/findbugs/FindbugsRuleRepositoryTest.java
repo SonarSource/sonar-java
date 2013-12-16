b6a7769dc2e080574a5c0cca57bc2bc78e6670d8
Evgeny Mandrikov <mandrikov@gmail.com>
2013-12-16 17:38:09 +0100

    SONARJAVA-334 Upgrade to Findbugs 2.0.3

--- sonar-findbugs-plugin/src/test/java/org/sonar/plugins/findbugs/FindbugsRuleRepositoryTest.java
+++ sonar-findbugs-plugin/src/test/java/org/sonar/plugins/findbugs/FindbugsRuleRepositoryTest.java
@@ -43,7 +43,7 @@ public class FindbugsRuleRepositoryTest {
   public void testLoadRepositoryFromXml() {
     List<Rule> rules = repository.createRules();
 
-    assertThat(rules.size()).isEqualTo(418);
+    assertThat(rules.size()).isEqualTo(422);
     for (Rule rule : rules) {
       assertThat(rule.getKey()).isNotNull();
       assertThat(rule.getConfigKey()).isNotNull();
