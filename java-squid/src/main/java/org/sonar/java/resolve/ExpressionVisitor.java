a06e1aa263074c6144dd75953f96ecd0fda87083
Nicolas PERU <nicolas.peru@sonarsource.com>
2014-10-15 13:50:05 +0200

    Semantic: When site is package search for type or package

--- java-squid/src/main/java/org/sonar/java/resolve/ExpressionVisitor.java
+++ java-squid/src/main/java/org/sonar/java/resolve/ExpressionVisitor.java
@@ -300,8 +300,7 @@ public class ExpressionVisitor extends BaseTreeVisitor {
         } else if (site.kind == Symbol.TYP) {
           site = resolve.findIdentInType(env, (Symbol.TypeSymbol) site, name, Symbol.VAR | Symbol.TYP);
         } else if (site.kind == Symbol.PCK) {
-          // FIXME(Godin): contract of method says that kind should be mask of "TYP" and "PCK", but not "VAR" as here:
-          site = resolve.findIdentInPackage(env, site, name, Symbol.VAR | Symbol.PCK);
+          site = resolve.findIdentInPackage(env, site, name, Symbol.TYP | Symbol.PCK);
         } else if (site.kind == Symbol.MTH) {
           // FIXME(Godin): Unhandled case! For example see org.sonar.duplications.java.JavaStatementBuilder , which uses fluent interface
           registerType(tree, symbols.unknownType);
