a8f3c3e478306d810b828f32bac81e50cd6a2917
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-01-21 16:13:22 +0100

    SONARJAVA-864 RSPEC-2326 Unused Type parameters

---
+++ java-checks/src/test/files/checks/UnusedTypeParameterCheck.java
@@ -0,0 +1,10 @@
+class A<T, S> {
+  T field;
+  <W,X> void fun(X x) {}
+}
+interface B<U, V> {
+  U foo();
+}
+class C {
+  void fun(){}
+}
--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaSonarWayProfileTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaSonarWayProfileTest.java
@@ -44,7 +44,7 @@ public class JavaSonarWayProfileTest {
     RulesProfile profile = definition.createProfile(validation);
 
     assertThat(profile.getLanguage()).isEqualTo(Java.KEY);
-    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY)).hasSize(168);
+    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY)).hasSize(169);
     assertThat(profile.getName()).isEqualTo("Sonar way");
     assertThat(validation.hasErrors()).isFalse();
   }
