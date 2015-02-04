36071782e811092a23e188f6a1b8c746493e8ca6
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-02-04 08:49:48 +0100

    SONARJAVA-868_RSPEC-2175 Inappropriate collection calls check

---
+++ java-checks/src/test/files/checks/CollectionInappropriateCallsCheck.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class A {
+  private void myMethod() {
+    List<String> myList = new ArrayList<String>();
+    Set<A> myASet = new HashSet<A>();
+    ArrayList<B> myBList = new ArrayList<B>();
+    List<Set<Integer>> mySetList = new ArrayList<Set<Integer>>();
+
+    Integer myInteger = Integer.valueOf(1);
+    String myString = "";
+    String[] myArrayString = new String[] {"myString"};
+    Integer[] myArrayInteger = new Integer[] {Integer.valueOf(1)};
+
+    if (myList.contains(myInteger)) { // Noncompliant. Always false.
+      myList.remove(myInteger); // Noncompliant. list.add(iger) doesn't compile, so this will always return false
+    }
+
+    if (myList.contains(myString)) { // Compliant
+      myList.remove(myString); // Compliant
+    }
+
+    if (myBList.contains(myInteger)) { // Noncompliant
+      myBList.remove(myInteger); // Noncompliant
+    }
+
+    if (mySetList.contains(myString)) { // Noncompliant
+      mySetList.remove(myString); // Noncompliant
+    }
+
+    if (mySetList.contains(returnOne())) { // Noncompliant
+      mySetList.remove(B.returnOne()); // Noncompliant
+    }
+
+    if (myBList.contains(new B())) { // Compliant
+      myBList.remove(new A()); // Noncompliant
+    }
+
+    if (myList.contains(myArrayInteger)) { // Noncompliant - False negative
+      myList.remove(myArrayInteger[0]); // Noncompliant
+      myList.remove(myArrayString[0]); // Compliant
+    }
+
+    if (myASet.contains(new C())) { // Compliant
+      myASet.remove(new B()); // Compliant
+    }
+  }
+
+  private static Integer returnOne() {
+    return Integer.valueOf(1);
+  }
+}
+
+class B extends A {
+  public String value;
+
+  public static Integer returnOne() {
+    return Integer.valueOf(1);
+  }
+}
+
+class C extends B {
+
+  private void myOtherMethod() {
+    Set mySet = new HashSet<B>();
+    A myA = new A();
+
+    if (mySet.contains(myA)) { // Compliant
+      mySet.remove(new B()); // Compliant
+    }
+  }
+}
+
+class MyCollection<E> extends ArrayList<E> {
+  
+  @Override
+  public boolean add(E e) {
+    if (contains(e)) { // Compliant
+      return false;
+    }
+    return super.add(e);
+  }
+
+}
--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaSonarWayProfileTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaSonarWayProfileTest.java
@@ -43,7 +43,7 @@ public class JavaSonarWayProfileTest {
     RulesProfile profile = definition.createProfile(validation);
 
     assertThat(profile.getLanguage()).isEqualTo(Java.KEY);
-    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY)).hasSize(174);
+    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY)).hasSize(175);
     assertThat(profile.getName()).isEqualTo("Sonar way");
     assertThat(validation.hasErrors()).isFalse();
   }
