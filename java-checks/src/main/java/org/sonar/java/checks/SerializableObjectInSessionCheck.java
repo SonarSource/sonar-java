ca0dacee5dd2212cc5e21c211104c4db7c99ddbc
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-02-12 17:43:44 +0100

    SONARJAVA-899 RSPEC-2441 Non Serializable object in HTTPSession

---
+++ java-checks/src/main/java/org/sonar/java/checks/SerializableObjectInSessionCheck.java
@@ -0,0 +1,63 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.java.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.java.checks.methods.MethodInvocationMatcher;
+import org.sonar.java.model.AbstractTypedTree;
+import org.sonar.java.resolve.Type;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+
+import java.util.List;
+
+@Rule(
+  key = "S2441",
+  name = "Non-serializable objects should not be stored in \"HttpSessions\"",
+  tags = {"bug"},
+  priority = Priority.CRITICAL)
+@ActivatedByDefault
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.DATA_RELIABILITY)
+@SqaleConstantRemediation("20min")
+public class SerializableObjectInSessionCheck extends AbstractMethodDetection {
+
+
+  @Override
+  protected List<MethodInvocationMatcher> getMethodInvocationMatchers() {
+    return ImmutableList.of(MethodInvocationMatcher.create().typeDefinition("javax.servlet.http.HttpSession")
+        .name("setAttribute").addParameter("java.lang.String").addParameter("java.lang.Object"));
+  }
+
+  @Override
+  protected void onMethodFound(MethodInvocationTree mit) {
+    ExpressionTree argument = mit.arguments().get(1);
+    Type type = ((AbstractTypedTree) argument).getSymbolType();
+    if(!type.isSubtypeOf("java.io.Serializable")) {
+      addIssue(argument, "Make \""+type+"\" serializable or don't store it in the session.");
+    }
+  }
+}
---
+++ java-checks/src/test/java/org/sonar/java/checks/SerializableObjectInSessionCheckTest.java
@@ -0,0 +1,45 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.java.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.java.JavaAstScanner;
+import org.sonar.java.model.VisitorsBridge;
+import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.squidbridge.checks.CheckMessagesVerifierRule;
+
+import java.io.File;
+
+public class SerializableObjectInSessionCheckTest {
+
+  @Rule
+  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
+
+  @Test
+  public void detected() {
+    SourceFile file = JavaAstScanner.scanSingleFile(new File("src/test/files/checks/SerializableObjectInSessionCheck.java"),
+        new VisitorsBridge(new SerializableObjectInSessionCheck(), ImmutableList.of(new File("target/test-classes"))));
+    checkMessagesVerifier.verify(file.getCheckMessages())
+        .next().atLine(6).withMessage("Make \"Address\" serializable or don't store it in the session.")
+        .next().atLine(7).withMessage("Make \"Person\" serializable or don't store it in the session.");
+  }
+}
