14cf3be921d3047f07cddd14cfd36c2d9e8d59b6
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-02-13 09:43:22 +0100

    SONARJAVA-904 RSPEC-2444 Lazy static field initialization

---
+++ java-checks/src/main/java/org/sonar/java/checks/StaticFieldInitializationCheck.java
@@ -0,0 +1,70 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.java.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.MethodInvocationMatcher;
+import org.sonar.java.resolve.Symbol;
+import org.sonar.plugins.java.api.tree.AssignmentExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+
+import java.util.List;
+
+@Rule(
+  key = "S2444",
+  name = "Lazy initialization of \"static\" fields should be \"synchronized\"",
+  tags = {"bug", "multi-threading"},
+  priority = Priority.CRITICAL)
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.SYNCHRONIZATION_RELIABILITY)
+@SqaleConstantRemediation("30min")
+public class StaticFieldInitializationCheck extends AbstractInSynchronizeChecker {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(Tree.Kind.ASSIGNMENT, Tree.Kind.METHOD, Tree.Kind.METHOD_INVOCATION, Tree.Kind.SYNCHRONIZED_STATEMENT);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    if (hasSemantic() && tree.is(Tree.Kind.ASSIGNMENT)) {
+      AssignmentExpressionTree aet = (AssignmentExpressionTree) tree;
+      if (aet.variable().is(Tree.Kind.IDENTIFIER) && !isInSyncBlock()) {
+        IdentifierTree variable = (IdentifierTree) aet.variable();
+        Symbol symbol = getSemanticModel().getReference(variable);
+        if (symbol.isStatic() && !symbol.isVolatile()) {
+          addIssue(variable, "Synchronize this lazy initialization of '" + variable.name() + "'");
+        }
+      }
+    }
+    super.visitNode(tree);
+  }
+
+  @Override
+  protected List<MethodInvocationMatcher> getMethodInvocationMatchers() {
+    return ImmutableList.of();
+  }
+
+}
---
+++ java-checks/src/test/files/checks/StaticFieldInitializationCheck.java
@@ -0,0 +1,51 @@
+class A {
+  protected static Object instance = null;
+
+  public static Object getInstance() {
+    if (instance != null) {
+      return instance;
+    }
+
+    instance = new Object();  // Noncompliant
+    return instance;
+  }
+
+  protected static volatile Object instance2 = null;
+
+  public static Object getInstance2() {
+    if (instance2 != null) {
+      return instance2;
+    }
+
+    instance2 = new Object();
+    return instance2;
+  }
+
+  protected static Object instance3 = null;
+
+  public static synchronized Object getInstance3() {
+    if (instance3 != null) {
+      return instance3;
+    }
+
+    instance3 = new Object();
+    return instance3;
+  }
+  public static void foo() {
+    synchronized(instance) {
+      instance3 = new Object();
+    }
+  }
+
+  protected volatile static Object instance4 = null;
+
+  public static Object getInstance() {
+    if (instance4 != null) {
+      return instance4;
+    }
+    foo();
+    instance4 = new Object();  // compliant instance 4 is volatile
+    return instance4;
+  }
+
+}
