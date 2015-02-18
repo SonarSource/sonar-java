707352e1f81c154ff4f943ddd6b3afc32bfb646c
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-02-18 17:51:35 +0100

    SONARJAVA-913 RSPEC-2438 Thread instead of Runnable parameter check

---
+++ java-checks/src/test/files/checks/ThreadAsRunnableArgumentCheck.java
@@ -0,0 +1,61 @@
+class A {
+
+  A(int i) {
+  }
+
+  public void foo() {
+    Thread t = new Thread() {
+    };
+    new Thread(t).start(); // Noncompliant
+
+    new Thread(bar()).start(); // Noncompliant
+
+    MyThread myThread = new MyThread();
+    new Thread(myThread).start(); // Noncompliant
+
+    Runnable r = new Runnable() {
+      @Override
+      public void run() {
+      }
+    };
+    new Thread(r).start(); // Compliant
+
+    new A(0);
+
+    MyClass m = new MyClass(myThread); // Noncompliant
+    m.foo(myThread); // Noncompliant
+    m = new MyClass(0, new MyThread()); // Noncompliant
+    m = new MyClass(0, myThread, r, new MyThread()); // Noncompliant because of arg1 and arg3
+    m = new MyClass(0, new Thread[] {myThread, new MyThread()}); // Noncompliant
+    m = new MyClass(0); // Compliant
+    m = new MyClass(0, new Runnable[] {}); // Compliant
+
+    m.bar(myThread); // Compliant
+    m.qix(); // Compliant
+  }
+
+  public Thread bar() {
+    return new Thread() {
+    };
+  }
+}
+
+class MyThread extends Thread {
+}
+
+class MyClass {
+  MyClass(Runnable r) {
+  }
+
+  MyClass(int i, String s) {
+  }
+
+  MyClass(int i, Runnable... runners) {
+  }
+
+  void foo(Runnable r) {
+  }
+
+  void qix(Runnable... runners) {
+  }
+}
---
+++ java-checks/src/test/java/org/sonar/java/checks/ThreadAsRunnableArgumentCheckTest.java
@@ -0,0 +1,53 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.java.checks;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.java.JavaAstScanner;
+import org.sonar.java.model.VisitorsBridge;
+import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.squidbridge.checks.CheckMessagesVerifierRule;
+
+import java.io.File;
+
+public class ThreadAsRunnableArgumentCheckTest {
+
+  @Rule
+  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
+
+  @Test
+  public void detected() {
+    SourceFile file = JavaAstScanner.scanSingleFile(
+      new File("src/test/files/checks/ThreadAsRunnableArgumentCheck.java"),
+      new VisitorsBridge(new ThreadAsRunnableArgumentCheck()));
+    checkMessagesVerifier.verify(file.getCheckMessages())
+      .next().atLine(9).withMessage("\"t\" is a \"Thread\".")
+      .next().atLine(11).withMessage("\"Argument 1\" is a \"Thread\".")
+      .next().atLine(14).withMessage("\"myThread\" is a \"Thread\".")
+      .next().atLine(25).withMessage("\"myThread\" is a \"Thread\".")
+      .next().atLine(26).withMessage("\"myThread\" is a \"Thread\".")
+      .next().atLine(27).withMessage("\"Argument 2\" is a \"Thread\".")
+      .next().atLine(28).withMessage("\"Argument 4\" is a \"Thread\".")
+      .next().atLine(28).withMessage("\"myThread\" is a \"Thread\".")
+      .next().atLine(29).withMessage("\"Argument 2\" is a \"Thread[]\".")
+      .noMore();
+  }
+}
