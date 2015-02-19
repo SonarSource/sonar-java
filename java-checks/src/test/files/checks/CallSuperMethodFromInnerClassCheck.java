ae66f99c380375076d88504c7d8f3e323ffc36ee
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-02-19 11:32:27 +0100

    SONARJAVA-914 RSPEC-2388 call to super method from inner class

---
+++ java-checks/src/test/files/checks/CallSuperMethodFromInnerClassCheck.java
@@ -0,0 +1,18 @@
+public class Parent {
+  public void foo() {  }
+}
+
+public class Outer {
+
+  public void foo() {  }
+
+  public class Inner extends Parent {
+
+    public void doTheThing() {
+      foo();  // Noncompliant; was Outer.this.foo() intended instead?
+      super.foo(); //Compliant: unambiguous
+      Outer.this.foo(); //Compliant: unambiguous
+      // ...
+    }
+  }
+}
