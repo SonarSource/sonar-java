f220fa27b9b37c846941b8ee61d13079651f60f8
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-03-26 18:54:39 +0100

    SONARJAVA-925 Fix FP on S1452 : do not report issue on private methods

--- java-checks/src/test/files/checks/WildcardReturnParameterTypeCheck.java
+++ java-checks/src/test/files/checks/WildcardReturnParameterTypeCheck.java
@@ -6,23 +6,26 @@ import java.util.List;
 class Animal {
 
   public List<? extends Animal> getAnimals() {        // NOK
-    Collection<?> c = new ArrayList<String>();        // OK
+    Collection<?> c = new ArrayList<String>();        // Compliant
   }
 
-  public List<Animal> getAnimals(){                   // OK
+  public List<Animal> getAnimals(){                   // Compliant
   }
 
   class InnerCat {
-    public List<? extends Cat> getCats() {            // NOK
+    public List<? extends Cat> getCats() {            // NonCompliant
     }
   }
 
   class InnerDog extends Animal {
-    public List<? extends Animal> getAnimals() { // OK method is overriden
+    public List<? extends Animal> getAnimals() { // Compliant method is overriden
       return super.getAnimals();
    }
 
-   public List<Class<?>> foo() {} // OK Class is ignored
-   public List<? extends Class<String>> bar() {} // NOK wildcard is not in Class
+   public List<Class<?>> foo() {} // Compliant Class is ignored
+   public List<? extends Class<String>> bar() {} // NonCompliant wildcard is not in Class
+   private List<? extends Cat> getCats() { //Compliant private method are ignored
+
+    }
   }
 }
