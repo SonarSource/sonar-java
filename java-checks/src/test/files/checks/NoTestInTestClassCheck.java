7d4b4199fbf13db8aaf781250db7ea6779fd6837
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-04-22 19:19:44 +0200

    SONARJAVA-1011 TestCases should contains test

---
+++ java-checks/src/test/files/checks/NoTestInTestClassCheck.java
@@ -0,0 +1,27 @@
+class A extends junit.framework.TestCase {
+  void testFoo() {
+  }
+}
+class B extends junit.framework.TestCase { // Noncompliant {{Add some tests to this class.}}
+  void foo() {
+  }
+}
+
+class ATest { // Noncompliant {{Add some tests to this class.}}
+  void foo() {
+    new AnonymousClass() {
+      void testfoo(){
+      }
+    };
+  }
+}
+class BTest {
+  @org.junit.Test
+  void foo()  {
+    class MyInnerTest { }
+  }
+}
+enum MyTest {}
+class AnonymousClass extends junit.framework.TestCase{
+  void testfoo(){}
+}
