fd97b2082fc42076319db11be6fd5e17db2c211a
Samuel Mercier <samuel.mercier@sonarsource.com>
2015-04-23 10:07:57 +0200

    SONARJAVA-1020 RSPEC-2677 Fix FP: Remove Iterator.next() from the scope of the rule

--- java-checks/src/test/files/checks/UnusedReturnedData.java
+++ java-checks/src/test/files/checks/UnusedReturnedData.java
@@ -4,15 +4,12 @@ public class Class {
 
   public boolean method() {
     java.util.Iterator<Boolean> itr;
-    java.io.Reader rd;
+    final java.io.Reader rd;
     java.io.BufferedReader brd;
 
-    itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
     rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     brd.readLine(); // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
 
-    String str = itr.next(); // Compliant
-    str = itr.next(); // Compliant
     String line = brd.readLine(); // Compliant
     line = brd.readLine(); // Compliant
     int value = rd.read(); // Compliant
@@ -24,24 +21,18 @@ public class Class {
     // call nested in assignment
     InnerClass cls = new InnerClass() {
       public void innerMethod() {
-        itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
+        rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
       }
     };
     cls = new InnerClass() {
       public void innerMethod() {
-        itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
+        rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
       }
     };
 
     // statements
     do {
-      itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    } while (itr.next()); // Compliant
-    do {
-    } while (itr.next() == null); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    do {
-    } while ((itr.next() == null) && true); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    do {
+      rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     } while (brd.readLine() == null); // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
     do {
     } while ((brd.readLine() == null) && true); // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
@@ -56,19 +47,11 @@ public class Class {
     do {
     } while (rd.read() != 0); // Compliant
 
-    for (itr.next();;) { // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
     for (brd.readLine();;) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
     }
     for (rd.read();;) { // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
 
-    for (; itr.next();) { // Compliant
-    }
-    for (; itr.next() == null;) { // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
-    for (; itr.next() != null;) { // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
     for (; brd.readLine() == null;) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
     }
     for (; brd.readLine() != null;) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
@@ -76,58 +59,44 @@ public class Class {
     for (; rd.read() != 0;) { // Compliant
     }
 
-    for (;; itr.next()) { // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
     for (;; brd.readLine()) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
     }
     for (;; rd.read()) { // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
 
     for (;;) {
-      itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
+      rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
 
-    if (itr.next()) { // Compliant
-      itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    } else {
-      itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
-    if (itr.next() == null) { // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
-    if (itr.next() != null) { // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
     if ((brd.readLine()) == null) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
+      rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
+    } else {
+      rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
     if (brd.readLine() != (null)) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
     }
     if (rd.read() == 0) { // Compliant
     }
 
-    switch (itr.next()) { // Compliant
-      case TRUE:
-        itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
-    }
     switch (brd.readLine()) { // Compliant
       case "string":
-        itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
+        rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
     switch (brd.readLine() != null) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
       case TRUE:
-        itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
+        rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
     switch (rd.read()) { // Compliant
       case 0:
-        itr.next(); // Noncompliant {{Use or store the value returned from "next" instead of throwing it away.}}
+        rd.read(); // Noncompliant {{Use or store the value returned from "read" instead of throwing it away.}}
     }
 
-    while (itr.next()) { // Compliant
-    }
     while (brd.readLine() != null) { // Noncompliant {{Use or store the value returned from "readLine" instead of throwing it away.}}
     }
     while (rd.read() == 0) { // Compliant
     }
 
-    while ((str = itr.next()) != null) { // Compliant
+    while ((str = brd.readLine()) != null) { // Compliant
     }
 
     return true;
