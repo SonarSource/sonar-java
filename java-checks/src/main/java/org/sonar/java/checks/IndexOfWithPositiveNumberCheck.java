1fba3e724d8c7595ea20b75190b8f78bc8053090
Samuel Mercier <samuel.mercier@sonarsource.com>
2015-04-29 13:51:00 +0200

    SONARJAVA-1034 RSPEC-2692 "indexOf" checks should not be for positive numbers

---
+++ java-checks/src/main/java/org/sonar/java/checks/IndexOfWithPositiveNumberCheck.java
@@ -0,0 +1,98 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.java.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.MethodInvocationMatcher;
+import org.sonar.java.checks.methods.MethodInvocationMatcherCollection;
+import org.sonar.java.checks.methods.TypeCriteria;
+import org.sonar.java.model.LiteralUtils;
+import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+
+import javax.annotation.Nullable;
+
+import java.util.List;
+
+@Rule(
+  key = "S2692",
+  name = "\"indexOf\" checks should not be positive numbers",
+  tags = {"pitfall"},
+  priority = Priority.CRITICAL)
+@ActivatedByDefault
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.LOGIC_RELIABILITY)
+@SqaleConstantRemediation("2min")
+public class IndexOfWithPositiveNumberCheck extends SubscriptionBaseVisitor {
+
+  private static final String INDEXOF = "indexOf";
+
+  private static final MethodInvocationMatcherCollection CHECKED_METHODS = MethodInvocationMatcherCollection.create(
+    MethodInvocationMatcher.create()
+      .typeDefinition(String.class.getName())
+      .name(INDEXOF)
+      .addParameter("int"),
+    MethodInvocationMatcher.create()
+      .typeDefinition(String.class.getName())
+      .name(INDEXOF)
+      .addParameter(String.class.getName()),
+    MethodInvocationMatcher.create()
+      .typeDefinition(TypeCriteria.subtypeOf("java.util.List"))
+      .name(INDEXOF)
+      .addParameter("java.lang.Object")
+    );
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(Tree.Kind.GREATER_THAN, Tree.Kind.GREATER_THAN_OR_EQUAL_TO, Tree.Kind.LESS_THAN, Tree.Kind.LESS_THAN_OR_EQUAL_TO);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    BinaryExpressionTree binaryTree = (BinaryExpressionTree) tree;
+    if (tree.is(Tree.Kind.GREATER_THAN, Tree.Kind.GREATER_THAN_OR_EQUAL_TO)) {
+      checkForIssue(tree, binaryTree.leftOperand(), LiteralUtils.longLiteralValue(binaryTree.rightOperand()));
+    } else {
+      checkForIssue(tree, binaryTree.rightOperand(), LiteralUtils.longLiteralValue(binaryTree.leftOperand()));
+    }
+  }
+
+  private void checkForIssue(Tree tree, ExpressionTree operand, @Nullable Long constant) {
+    if (constant != null && constant >= getConstant(tree) && isIndexOfOnArrayOrString(operand)) {
+      addIssue(tree, "0 is a valid index, but is ignored by this check.");
+    }
+  }
+
+  private boolean isIndexOfOnArrayOrString(Tree tree) {
+    return tree.is(Tree.Kind.METHOD_INVOCATION) && CHECKED_METHODS.anyMatch((MethodInvocationTree) tree);
+  }
+
+  private long getConstant(Tree tree) {
+    return tree.is(Tree.Kind.GREATER_THAN, Tree.Kind.LESS_THAN) ? 0 : 1;
+  }
+
+}
