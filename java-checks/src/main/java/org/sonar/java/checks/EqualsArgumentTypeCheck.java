8df175198cbf518e45fdc382ec0cb52621dc61fd
Samuel Mercier <samuel.mercier@sonarsource.com>
2015-05-06 15:45:53 +0200

    SONARJAVA-1050 RSPEC-2097 Raise issue only if there is a cast of the parameter

--- java-checks/src/main/java/org/sonar/java/checks/EqualsArgumentTypeCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/EqualsArgumentTypeCheck.java
@@ -35,6 +35,7 @@ import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.ParenthesizedTree;
 import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.TypeCastTree;
 import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
@@ -72,13 +73,52 @@ public class EqualsArgumentTypeCheck extends SubscriptionBaseVisitor {
     if (methodTree.block() != null && "equals".equals(methodTree.symbol().name()) && methodTree.parameters().size() == 1) {
       Symbol parameterSymbol = methodTree.parameters().get(0).symbol();
       if (parameterSymbol.type().is("java.lang.Object")) {
-        ExpressionVisitor visitor = new ExpressionVisitor(parameterSymbol);
-        methodTree.accept(visitor);
-        if (!visitor.typeChecked) {
-          addIssue(tree, "Add a type test to this method.");
+        CastVisitor castVisitor = new CastVisitor(parameterSymbol);
+        methodTree.accept(castVisitor);
+        if (castVisitor.hasCast) {
+          ExpressionVisitor expressionVisitor = new ExpressionVisitor(parameterSymbol);
+          methodTree.accept(expressionVisitor);
+          if (!expressionVisitor.typeChecked) {
+            addIssue(tree, "Add a type test to this method.");
+          }
         }
       }
     }
+  }
+
+  private static ExpressionTree removeParenthesis(ExpressionTree tree) {
+    ExpressionTree result = tree;
+    while (true) {
+      if (result.is(Tree.Kind.PARENTHESIZED_EXPRESSION)) {
+        result = ((ParenthesizedTree) result).expression();
+      } else {
+        return result;
+      }
+    }
+  }
+
+  private static boolean isArgument(ExpressionTree tree, Symbol parameterSymbol) {
+    ExpressionTree expressionTree = removeParenthesis(tree);
+    return expressionTree.is(Tree.Kind.IDENTIFIER) && ((IdentifierTree) expressionTree).symbol().equals(parameterSymbol);
+  }
+
+  private static class CastVisitor extends BaseTreeVisitor {
+    private final Symbol parameterSymbol;
+    boolean hasCast;
+
+    public CastVisitor(Symbol parameterSymbol) {
+      this.parameterSymbol = parameterSymbol;
+    }
+
+    @Override
+    public void visitTypeCast(TypeCastTree tree) {
+      if (isArgument(tree.expression(), parameterSymbol)) {
+        hasCast = true;
+      } else {
+        super.visitTypeCast(tree);
+      }
+    }
+
   }
 
   private static class ExpressionVisitor extends BaseTreeVisitor {
@@ -91,7 +131,7 @@ public class EqualsArgumentTypeCheck extends SubscriptionBaseVisitor {
 
     @Override
     public void visitInstanceOf(InstanceOfTree tree) {
-      if (isArgument(tree.expression())) {
+      if (isArgument(tree.expression(), parameterSymbol)) {
         typeChecked = true;
       }
     }
@@ -112,45 +152,31 @@ public class EqualsArgumentTypeCheck extends SubscriptionBaseVisitor {
     @Override
     public void visitMethodInvocation(MethodInvocationTree tree) {
       if (EQUALS_MATCHER.matches(tree)) {
-        typeChecked = true;
+        if (isInvocationOnArgument(tree)) {
+          typeChecked = true;
+        } else if (isArgument(tree.arguments().get(0), parameterSymbol)) {
+          typeChecked = true;
+        }
       }
     }
 
-    private boolean isArgument(ExpressionTree tree) {
-      ExpressionTree expressionTree = removeParenthesis(tree);
-      return expressionTree.is(Tree.Kind.IDENTIFIER) && ((IdentifierTree) expressionTree).symbol().equals(parameterSymbol);
-    }
-
     private boolean isExplicitComparison(ExpressionTree operand1, ExpressionTree operand2) {
-      return (isArgument(operand1) && isThis(operand2)) || (isArgument(operand2) && isThis(operand1));
+      return (isArgument(operand1, parameterSymbol) && isThis(operand2)) || (isArgument(operand2, parameterSymbol) && isThis(operand1));
     }
 
     private boolean isGetClassOnArgument(ExpressionTree tree) {
       ExpressionTree expressionTree = removeParenthesis(tree);
-      if (expressionTree.is(Tree.Kind.METHOD_INVOCATION) && GETCLASS_MATCHER.matches((MethodInvocationTree) expressionTree)) {
-        ExpressionTree methodSelect = ((MethodInvocationTree) expressionTree).methodSelect();
-        if (methodSelect.is(Tree.Kind.MEMBER_SELECT)) {
-          ExpressionTree expression = ((MemberSelectExpressionTree) methodSelect).expression();
-          return isArgument(expression);
-        }
-      }
-      return false;
+      return expressionTree.is(Tree.Kind.METHOD_INVOCATION) && GETCLASS_MATCHER.matches((MethodInvocationTree) expressionTree)
+        && isInvocationOnArgument((MethodInvocationTree) expressionTree);
+    }
+
+    private boolean isInvocationOnArgument(MethodInvocationTree tree) {
+      return tree.methodSelect().is(Tree.Kind.MEMBER_SELECT) && isArgument(((MemberSelectExpressionTree) tree.methodSelect()).expression(), parameterSymbol);
     }
 
     private boolean isThis(ExpressionTree tree) {
       ExpressionTree expressionTree = removeParenthesis(tree);
       return expressionTree.is(Tree.Kind.IDENTIFIER) && "this".equals(((IdentifierTree) expressionTree).identifierToken().text());
-    }
-
-    private ExpressionTree removeParenthesis(ExpressionTree tree) {
-      ExpressionTree result = tree;
-      while (true) {
-        if (result.is(Tree.Kind.PARENTHESIZED_EXPRESSION)) {
-          result = ((ParenthesizedTree) result).expression();
-        } else {
-          return result;
-        }
-      }
     }
   }
 
--- java-checks/src/test/files/checks/EqualsArgumentType.java
+++ java-checks/src/test/files/checks/EqualsArgumentType.java
@@ -1,231 +1,212 @@
 public class TestClassInstanceof1 {
-
   @Override
   public boolean equals(Object that) { // Compliant
     if (that instanceof Object) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassInstanceof2 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
     if (this instanceof Object) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassInstanceof3 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
     if (0 == 0 || ((that.getClass()) instanceof Object)) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassGetClassEqual {
-
   @Override
   public boolean equals(Object that) { // Compliant
     if ((that.getClass()) == (Object.class)) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassGetClassNotEqual {
-
   @Override
   public boolean equals(Object that) { // Compliant
     if (that.getClass() != Object.class) {
-      return false;
+      return ((Object) that) == null;
     }
     return true;
   }
-
 }
 
 public class TestClassGetClassEqual2 {
-
   @Override
   public boolean equals(Object that) { // Compliant
     if ((Object.class) == (that.getClass())) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassGetClassEqual3 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
     if (0 == 0) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassGetClassEqual4 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
     if (getClass() == Object.class) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassGetClassEqual5 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
     if ((that.hashCode()) == 0) {
+      return ((Object) that) == null;
     }
     return false;
   }
-
 }
 
 public class TestClassAssign {
-
   @Override
   public boolean equals(Object that) { // Compliant
     boolean result;
     result = that.getClass() == Object.class && true;
-    return result;
+    return ((Object) that) == null;
   }
-
 }
 
 public class TestClassExplicitComparison1 {
-
   @Override
   public boolean equals(Object that) { // Compliant
-    return (this) == that;
+    if ((this) == that) {
+      return ((Object) that) == null;
+    }
   }
-
 }
 
 public class TestClassExplicitComparison2 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
-    return (that) == that;
+    if ((that) == that) {
+      return ((Object) that) == null;
+    }
   }
-
 }
 
 public class TestClassExplicitComparison3 {
-
   @Override
   public boolean equals(Object that) { // Compliant
-    return that == this;
+    if (that == this) {
+      return ((Object) that) == null;
+    }
   }
-
-}
-
-public class TestClassExplicitComparison4 {
-
-  @Override
-  public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
-    return that == that;
-  }
-
 }
 
 public class TestClassExplicitComparison5 {
-
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
-    return that == "";
+    if (that == "") {
+      return ((Object) that) == null;
+    }
   }
-
 }
 
-public class TestClassEqualsCall {
-
+public class TestClassEqualsCall1 {
   @Override
   public boolean equals(Object that) { // Compliant
-    return this.equals(that);
+    if (this.equals(that)) {
+      return ((Object) that) == null;
+    }
   }
+}
 
+public class TestClassEqualsCall2 {
+  @Override
+  public boolean equals(Object that) { // Compliant
+    if (that.equals(this)) {
+      return ((Object) that) == null;
+    }
+  }
+}
+
+public class TestClassEqualsCall3 {
+  @Override
+  public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
+    if (this.equals(this)) {
+      return ((Object) that) == null;
+    }
+  }
+}
+
+public class TestClassEqualsCall4 {
+  @Override
+  public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
+    if (equals(this)) {
+      return ((Object) that) == null;
+    }
+  }
 }
 
 public class TestMethodCall {
-
   public boolean method() {
     return true;
   }
 
   @Override
   public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
-    return method();
+    if (method()) {
+      return ((Object) that) == null;
+    }
   }
-
 }
 
-public class TestClassInitializer {
-
-  @Override
-  public boolean equals(Object that) { // Compliant
-    boolean result = that.getClass() == Object.class && true;
-    return result;
-  }
-
+public interface testInterface {
+  public boolean equals(Object that); // Compliant
 }
 
-public class TestClassNestedIf {
+public class TestClass {
+  public boolean method(Object that) { // Compliant, different name
+    return ((Object) that) == null;
+  }
+
+  public Object equals() { // Compliant, different signature
+    return ((Object) that);
+  }
+
+  public boolean equals(String that) { // Compliant, different signature
+    return ((Object) that) == null;
+  }
 
   @Override
   public boolean equals(Object that) { // Compliant
     if (true) {
-      return that.getClass() == Object.class;
+      return (Object) this;
     }
-  }
-
-}
-
-public class TestClassReturn {
-
-  @Override
-  public boolean equals(Object that) { // Compliant
-    return that.getClass() == Object.class && true;
-  }
-
-}
-
-public interface testInterface {
-
-  public boolean equals(Object that); // Compliant
-
-}
-
-public class TestClass {
-
-  public boolean method(Object that) { // Compliant, different name
-  }
-
-  public boolean equals() { // Compliant, different signature
-  }
-
-  public boolean equals(String that) { // Compliant, different signature
-  }
-
-  @Override
-  public boolean equals(Object that) { // Noncompliant {{Add a type test to this method.}}
+    if (true) {
+      return (Object) equals();
+    }
     return false;
   }
-
 }
