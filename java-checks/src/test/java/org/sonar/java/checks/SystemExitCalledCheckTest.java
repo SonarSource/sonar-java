96abeed9adca73c876929fd9836d317da2417fd1
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-05-07 18:00:06 +0200

    SONARJAVA-916 Fix FP on S1147

--- java-checks/src/test/java/org/sonar/java/checks/SystemExitCalledCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/SystemExitCalledCheckTest.java
@@ -19,29 +19,14 @@
  */
 package org.sonar.java.checks;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.sonar.java.JavaAstScanner;
-import org.sonar.java.model.VisitorsBridge;
-import org.sonar.squidbridge.api.SourceFile;
-import org.sonar.squidbridge.checks.CheckMessagesVerifierRule;
-
-import java.io.File;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
 public class SystemExitCalledCheckTest {
 
-  @Rule
-  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
-
   @Test
   public void detected() {
-    SourceFile file = JavaAstScanner.scanSingleFile(new File("src/test/files/checks/SystemExitCalledCheck.java"), new VisitorsBridge(new SystemExitCalledCheck()));
-    checkMessagesVerifier.verify(file.getCheckMessages())
-        .next().atLine(3).withMessage("Remove this call to \"exit\" or ensure it is really required.")
-        .next().atLine(4)
-        .next().atLine(8)
-        .next().atLine(12).withMessage("Remove this call to \"halt\" or ensure it is really required.")
-    .noMore();
+    JavaCheckVerifier.verify("src/test/files/checks/SystemExitCalledCheck.java", new SystemExitCalledCheck());
   }
 
 }
