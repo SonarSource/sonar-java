598c71b9bf13612bbd46f0ccea21523071458e40
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-05-19 17:56:05 +0200

    SONARJAVA-978 Migrate to JavaCheckVerifier test

--- java-checks/src/test/files/checks/UselessParenthesesCheck.java
+++ java-checks/src/test/files/checks/UselessParenthesesCheck.java
@@ -1,23 +1,25 @@
 class Foo {
   void foo() {
     return 3;             // Compliant
-    return (x);           // Non-Compliant
-    return (x + 1);       // Non-Compliant
-    int x = (y / 2 + 1);  // Non-Compliant
+    return (x);           // Noncompliant {{Remove those useless parentheses.}}
+    return (x + 1);       // Noncompliant
+    int x = (y / 2 + 1);  // Noncompliant
     int y = (4+X) * y;    // Compliant
 
     if (0) {              // Compliant
     }
-
-    System.out.println(false ? (true ? 1 : 2) : 2); // Was previously noncompliant
-    System.out.println(false ? 0 : (true ? 1 : 2)); // Was previously compliant
-    invoke(((Cast)plop));
-    System.out.println((false) ? 0 : 1); // Compliant : Do not check for useless parenthesis on condition
+    // Was previously noncompliant
+    System.out.println(false ? (true ? 1 : 2) : 2); // Noncompliant
+    // Was previously compliant
+    System.out.println(false ? 0 : (true ? 1 : 2)); // compliant
+    invoke(((Cast)plop)); // Noncompliant
+    //Do not check for useless parenthesis on condition
+    System.out.println((false) ? 0 : 1); // compliant
     int[] tab;
-    tab[(1+2)];
+    tab[(1+2)]; // Noncompliant
     tab[(1+2)/2];
-    A a = new A((1/3));
-    return (((x & 0x0000FFFF)) | y);
+    A a = new A((1/3)); // Noncompliant
+    return (((x & 0x0000FFFF)) | y); // Noncompliant 2
     getContentSpec(((int[])contentSpec.value)[0], contentSpec);
   }
     public static final short value = (short)(0);
