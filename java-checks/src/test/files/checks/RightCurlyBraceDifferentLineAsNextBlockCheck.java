4e7956d0f43cf1719cc444c80fac11bab054cb4b
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-05-19 11:25:11 +0200

    SONARJAVA-1076 Remove usage of ASTNode in RightCurlyBraceDifferentLineAsNextBlockCheck

--- java-checks/src/test/files/checks/RightCurlyBraceDifferentLineAsNextBlockCheck.java
+++ java-checks/src/test/files/checks/RightCurlyBraceDifferentLineAsNextBlockCheck.java
@@ -2,20 +2,33 @@ class Foo {
   public void myMethod() {
     if(something) {
       executeTask();
-    } else if (somethingElse) {          // Non-Compliant
+    } else if (somethingElse) {          // Noncompliant {{Move this "else" keyword to a new dedicated line.}}
       doSomethingElse();
     }
+    else {                               // Compliant
+       generateError();
+    }
+    
+    if (somethingElse)
+      doSomethingElse();
     else {                               // Compliant
        generateError();
     }
 
     try {
       generateOrder();
-    } catch (Exception e) {              // Non-Compliant
+    } catch (Exception e) {              // Noncompliant {{Move this "catch" keyword to a new dedicated line.}}
       log(e);
     }
     finally {                            // Compliant
       closeConnection();
+    }
+    
+    try {
+      generateOrder();
+    } 
+    catch (Exception e) {                // Compliant
+      log(e);
     }
 
     if (0) {
