5965e6f4fae98f545991935fdcee37aecaf7a8af
René Wolfert <ne-tje@gmx.net>
2015-05-31 16:51:46 +0200

    SONARJAVA-984 Update rule with 2 new patterns

--- java-checks/src/test/files/checks/PrintfCheck.java
+++ java-checks/src/test/files/checks/PrintfCheck.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 class A {
   void foo(){
     Object myObject;
+    double value;
     String.format("The value of my integer is %d", "Hello World");  // Noncompliant; an 'int' is expected rather than a String
     String.format("First {0} and then {1}", "foo", "bar");  //Noncompliant. Looks like there is a confusion with the use of {{java.text.MessageFormat}}, parameters "foo" and "bar" will be simply ignored here
     String.format("Duke's Birthday year is %tX", 12l);  //Noncompliant; X is not a supported time conversion character
@@ -15,6 +16,8 @@ class A {
     String.format("First Line\n");   //Noncompliant; %n should be used in place of \n to produce the platform-specific line separator
     String.format("%< is equals to %d", 2);   //Noncompliant; the argument index '<' refers to the previous format specifier but there isn't one
     String.format("Is myObject null ? %b", myObject);   //Noncompliant; when a non-boolean argument is formatted with %b, it prints true for any nonnull value, and false for null. Even if intended, this is misleading. It's better to directly inject the boolean value (myObject == null in this case)
+    String.format("value is " + value); // non-compliant : string concatenation in argument.
+    String.format("string without arguments"); // non-compliant
 
     PrintWriter pr;
     PrintStream ps;
@@ -40,5 +43,28 @@ class A {
     String.format("Duke's Birthday year is %tH", gc);
     String.format("Duke's Birthday year is %t", loc);  //Noncompliant; t argument is empty AND loc is not compatible
 
+    pr.format("string without arguments"); // non-compliant
+    pr.format(loc, "string without arguments"); // non-compliant
+    pr.printf("string without arguments"); // non-compliant
+    pr.printf(loc, "string without arguments"); // non-compliant
+    ps.format("string without arguments"); // non-compliant
+    ps.format(loc, "string without arguments"); // non-compliant
+    ps.printf("string without arguments"); // non-compliant
+    ps.printf(loc, "string without arguments"); // non-compliant
+    formatter.format("string without arguments"); // non-compliant
+    formatter.format(loc, "string without arguments"); // non-compliant
+    
+    pr.format("value is " + value); // non-compliant
+    pr.format(loc, "value is " + value); // non-compliant
+    pr.printf("value is " + value); // non-compliant
+    pr.printf(loc, "value is " + value); // non-compliant
+    ps.format("value is " + value); // non-compliant
+    ps.format(loc, "value is " + value); // non-compliant
+    ps.printf("value is " + value); // non-compliant
+    ps.printf(loc, "value is " + value); // non-compliant
+    formatter.format("value is " + value); // non-compliant
+    formatter.format(loc, "value is " + value); // non-compliant
+
+    String.format("value is %d", value); // compliant
   }
 }
