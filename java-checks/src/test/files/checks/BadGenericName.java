dc880219d6e2e7341fbe0fa71c65f3495b936ea2
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2015-06-02 11:49:37 +0200

    SONARJAVA-1102 S00119: use ^[A-Z][0-9]?$ as default regex for type parameters.
    using type parameter like V1, V2 is pretty common to indicate
    types parameters that have a closely related role in an algorithm.
    eg.
    static <K,V1,V2> Map<K,V2> transformValues(Map<K,V1> fromMap,
                                      Function<? super V1,V2> function)
    from guava com.google.common.collect.Maps.

--- java-checks/src/test/files/checks/BadGenericName.java
+++ java-checks/src/test/files/checks/BadGenericName.java
@@ -6,4 +6,6 @@ class MyClass<TYPE> {
 public class MyClass2<T> {
   <T> void addAll(Collection<T> c) {
   }
+  <T0 extends T, T1 extends T> void addAll(Collection<T1> c1, Collection<T0> c2) {
+  }
 }
