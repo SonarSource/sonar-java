e4dc1feb9dd935042cc490e2ba52a0a2f16869d1
David Gageot <david@gageot.net>
2015-06-16 10:52:59 +0200

    Install TravisCI toolbelt

--- travis.sh
+++ travis.sh
@@ -2,9 +2,15 @@
 
 set -euo pipefail
 
+function installTravisTools {
+  curl -sSL https://raw.githubusercontent.com/dgageot/travis-utils/Unstable/install.sh | bash
+}
+
 if [ "$TESTS" == "CI" ]; then
   mvn verify -B -e -V
 else
   mvn install -DskipTests=true
+
+  installTravisTools
   travis_run_its "${TESTS:3}"
 fi
