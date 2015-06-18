43c68dc9177f414f97e5746279fe913581803c0c
tomverin <thomas.verin@sonarsource.com>
2015-06-18 13:57:30 +0200

    Added PR analysis from travis-ci

--- travis.sh
+++ travis.sh
@@ -14,3 +14,16 @@ else
   installTravisTools
   travis_run_its "${TESTS}"
 fi
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] 
+then
+	echo "Start pullrequest analysis"
+ mvn sonar:sonar \
+ -Dsonar.analysis.mode=preview \
+ -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+ -Dsonar.github.repository=$SONAR_GITHUB_REPOSITORY \
+ -Dsonar.forceUpdate=true \
+ -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+ -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+ -Dsonar.host.url=$SONAR_HOST_URL 
+fi 
