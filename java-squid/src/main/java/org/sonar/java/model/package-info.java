43a185e462ac472edc02bee86c9fd19cbc45facd
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-07-07 10:18:27 +0200

    Update parent pom and licenses: change of project group id.

--- java-squid/src/main/java/org/sonar/java/model/package-info.java
+++ java-squid/src/main/java/org/sonar/java/model/package-info.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Java
  * Copyright (C) 2012 SonarSource
- * dev@sonar.codehaus.org
+ * sonarqube@googlegroups.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,6 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-
 @javax.annotation.ParametersAreNonnullByDefault
 @MethodsAreNonnullByDefault
 package org.sonar.java.model;
