dbe7d526dfbd453b94c0bc1489184addb7210c05
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-08-06 18:47:33 +0200

    SONARJAVA-1172 Migrates to JavaCheckVerifier

--- java-checks/src/test/files/checks/PreparedStatementAndResultSetCheck.java
+++ java-checks/src/test/files/checks/PreparedStatementAndResultSetCheck.java
@@ -9,49 +9,49 @@ class A {
   void foo(Connection connection) throws SQLException {
     PreparedStatement ps = connection.prepareStatement("SELECT fname, lname FROM employees where hireDate > ? and salary < ?");
 
-    ps.setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
-    ps.setDouble(3, 0.0); // Noncompliant - Statement has only 2 parameters
+    ps.setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
+    ps.setDouble(3, 0.0); // Noncompliant {{This "PreparedStatement" only has 2 parameters.}}
     ps.setString(getIntValue(), ""); // Compliant - first argument can not be evaluated
     ps.setInt(1, 0); // Compliant
 
     ResultSet rs = ps.executeQuery();
-    rs.getString(0); // Noncompliant - First column index is 1
-    rs.getDate(0, new GregorianCalendar()); // Noncompliant - First column index is 1
+    rs.getString(0); // Noncompliant {{ResultSet indices start at 1.}}
+    rs.getDate(0, new GregorianCalendar()); // Noncompliant {{ResultSet indices start at 1.}}
     rs.getString(1); // Compliant
   }
   
   void bar(Connection connection) throws SQLException {
     PreparedStatement ps = connection.prepareStatement("SELECT fname, lname FROM employees where hireDate > 1986");
 
-    ps.setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
-    ps.setDouble(3, 0.0); // Noncompliant - Statement has no parameter
+    ps.setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
+    ps.setDouble(3, 0.0); // Noncompliant {{This "PreparedStatement" has no parameters.}}
   }
   
   void dam(Connection connection, String query) throws SQLException {
     PreparedStatement ps = connection.prepareStatement(query);
 
-    ps.setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
+    ps.setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
     ps.setDouble(3, 0.0); // Compliant - Query of the preparedStatement is unknown
   }
   
   void cro(PreparedStatement ps) throws SQLException {
-    ps.setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
+    ps.setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
     ps.setDouble(3, 0.0); // Compliant - Query of the preparedStatement is unknown
   }
   
   void elk() throws SQLException {
-    getPreparedStatement().setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
+    getPreparedStatement().setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
     getPreparedStatement().setDouble(3, 0.0); // Compliant - Query of the preparedStatement is unknown
   }
   
   void gra() throws SQLException {
     PreparedStatement ps = getPreparedStatement();
     
-    ps.setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
+    ps.setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
     ps.setDouble(3, 0.0); // Compliant - Query of the preparedStatement is unknown
     
     PreparedStatement ps2 = ps;
-    ps2.setDate(0, new Date(0)); // Noncompliant - First parameter index is 1
+    ps2.setDate(0, new Date(0)); // Noncompliant {{PreparedStatement indices start at 1.}}
     ps2.setDouble(3, 0.0); // Compliant - Query of the preparedStatement is unknown
   }
 
--- java-checks/src/test/java/org/sonar/java/checks/PreparedStatementAndResultSetCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/PreparedStatementAndResultSetCheckTest.java
@@ -19,38 +19,14 @@
  */
 package org.sonar.java.checks;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.sonar.java.ast.JavaAstScanner;
-import org.sonar.java.model.VisitorsBridge;
-import org.sonar.squidbridge.api.SourceFile;
-import org.sonar.squidbridge.checks.CheckMessagesVerifierRule;
-
-import java.io.File;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
 public class PreparedStatementAndResultSetCheckTest {
 
-  @Rule
-  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
-
   @Test
   public void detected() {
-    SourceFile file = JavaAstScanner.scanSingleFile(
-      new File("src/test/files/checks/PreparedStatementAndResultSetCheck.java"),
-      new VisitorsBridge(new PreparedStatementAndResultSetCheck()));
-    checkMessagesVerifier.verify(file.getCheckMessages())
-      .next().atLine(12).withMessage("PreparedStatement indices start at 1.")
-      .next().atLine(13).withMessage("This \"PreparedStatement\" only has 2 parameters.")
-      .next().atLine(18).withMessage("ResultSet indices start at 1.")
-      .next().atLine(19).withMessage("ResultSet indices start at 1.")
-      .next().atLine(26).withMessage("PreparedStatement indices start at 1.")
-      .next().atLine(27).withMessage("This \"PreparedStatement\" has no parameters.")
-      .next().atLine(33).withMessage("PreparedStatement indices start at 1.")
-      .next().atLine(38).withMessage("PreparedStatement indices start at 1.")
-      .next().atLine(43).withMessage("PreparedStatement indices start at 1.")
-      .next().atLine(50).withMessage("PreparedStatement indices start at 1.")
-      .next().atLine(54).withMessage("PreparedStatement indices start at 1.")
-      .noMore();
+    JavaCheckVerifier.verify("src/test/files/checks/PreparedStatementAndResultSetCheck.java", new PreparedStatementAndResultSetCheck());
   }
 
 }
