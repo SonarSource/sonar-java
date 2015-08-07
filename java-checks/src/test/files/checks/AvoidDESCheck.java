db4dd90e276e8fe51cba5b4e7c75fd26992ec788
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-08-07 16:53:25 +0200

    SONARJAVA-1210 Handle special case of properties

--- java-checks/src/test/files/checks/AvoidDESCheck.java
+++ java-checks/src/test/files/checks/AvoidDESCheck.java
@@ -7,4 +7,34 @@ class A {
     c = Cipher.getInstance("DES/ECB/PKCS5Padding");// Noncompliant {{Use the recommended AES (Advanced Encryption Standard) instead.}}
     c = Cipher.getInstance("AES/GCM/NoPadding");//Compliant
   }
+  
+  void usingJavaUtilProperties(java.util.Properties props, String otherAlgo) {
+    String algo = props.getProperty("myAlgo", "DES/ECB/PKCS5Padding");
+    c = Cipher.getInstance(algo); // Noncompliant {{Use the recommended AES (Advanced Encryption Standard) instead.}}
+    c = Cipher.getInstance(props.getProperty("myAlgo", "DES/ECB/PKCS5Padding")); // Noncompliant {{Use the recommended AES (Advanced Encryption Standard) instead.}}
+    c = Cipher.getInstance(getAlgo()); // Compliant
+    c = Cipher.getInstance("/"); // Compliant
+    
+    String algo2 = props.getProperty("myAlgo");
+    c = Cipher.getInstance(algo2); // Compliant
+    
+    String algo3 = props.getProperty("myAlgo", "DES/ECB/PKCS5Padding");
+    algo3 = "myOtherAlgo";
+    c = Cipher.getInstance(algo3); // Compliant
+    
+    String algo4 = getAlgo();
+    c = Cipher.getInstance(algo4); // Compliant
+    
+    c = Cipher.getInstance(otherAlgo); // Compliant
+    
+    String algo5 = "myAlgo";
+    c = Cipher.getInstance(algo5); // Compliant
+    
+    String algo6 = props.getProperty("myAlgo", getAlgo());
+    c = Cipher.getInstance(algo6); // Compliant
+  }
+  
+  private String getAlgo() {
+    return null;
+  }
 }
