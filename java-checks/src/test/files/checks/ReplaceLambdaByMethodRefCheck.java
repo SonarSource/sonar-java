43f3dd6e46e0692ed8825292abc86a04308f942c
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-08-14 10:16:17 +0200

    SONARJAVA-1190 Migrates to JavaCheckVerifier

--- java-checks/src/test/files/checks/ReplaceLambdaByMethodRefCheck.java
+++ java-checks/src/test/files/checks/ReplaceLambdaByMethodRefCheck.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
+
 class A {
   void fun() {
 
     IntStream.range(1, 5)
         .map((x) -> x * x)
-        .map(x -> square(x)) //Non-Compliant
-        .map(x -> { //Non-Compliant
+        .map(x -> square(x)) // Noncompliant {{Replace this lambda with a method reference.}}
+        .map(x -> { // Noncompliant
           return square(x);
         })
         .map(this::square) //Compliant
         .forEach(System.out::println);
     IntStream.range(1, 5)
-        .forEach(x -> System.out.println(x));
+        .forEach(x -> System.out.println(x)); // Noncompliant
     IntStream.range(1, 5)
-        .forEach(x -> {
+        .forEach(x -> { // Noncompliant
           System.out.println(x);
         });
     IntStream.range(1, 5).forEach(x -> {return;});
--- java-checks/src/test/java/org/sonar/java/checks/ReplaceLambdaByMethodRefCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/ReplaceLambdaByMethodRefCheckTest.java
@@ -19,29 +19,14 @@
  */
 package org.sonar.java.checks;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.sonar.java.ast.JavaAstScanner;
-import org.sonar.java.model.VisitorsBridge;
-import org.sonar.squidbridge.api.SourceFile;
-import org.sonar.squidbridge.checks.CheckMessagesVerifierRule;
-
-import java.io.File;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
 public class ReplaceLambdaByMethodRefCheckTest {
 
-  @Rule
-  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
-
   @Test
   public void detected() {
-    SourceFile file = JavaAstScanner.scanSingleFile(new File("src/test/files/checks/ReplaceLambdaByMethodRefCheck.java"), new VisitorsBridge(new ReplaceLambdaByMethodRefCheck()));
-    checkMessagesVerifier.verify(file.getCheckMessages())
-      .next().atLine(6).withMessage("Replace this lambda with a method reference.")
-      .next().atLine(7).withMessage("Replace this lambda with a method reference.")
-      .next().atLine(13).withMessage("Replace this lambda with a method reference.")
-      .next().atLine(15).withMessage("Replace this lambda with a method reference.")
-    ;
+    JavaCheckVerifier.verify("src/test/files/checks/ReplaceLambdaByMethodRefCheck.java", new ReplaceLambdaByMethodRefCheck());
   }
 
 }
