54f423a6d58f616d2a3caaab2dff4653d7bc898c
Massimo Paladin <massimo.paladin@gmail.com>
2015-09-16 17:31:02 +0200

    SONARJAVA-958 Migrate Unused private method to semantic apis

--- java-checks/src/test/java/org/sonar/java/checks/UnusedPrivateMethodCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/UnusedPrivateMethodCheckTest.java
@@ -20,56 +20,13 @@
 package org.sonar.java.checks;
 
 import org.junit.Test;
-import org.sonar.java.JavaConfiguration;
-import org.sonar.java.JavaSquid;
-import org.sonar.plugins.java.api.JavaResourceLocator;
-import org.sonar.squidbridge.api.CodeVisitor;
-import org.sonar.squidbridge.api.SourceCode;
-import org.sonar.squidbridge.api.SourceFile;
-import org.sonar.squidbridge.checks.CheckMessagesVerifier;
-import org.sonar.squidbridge.indexer.QueryByType;
-
-import java.io.File;
-import java.nio.charset.Charset;
-import java.util.Collection;
-import java.util.Collections;
-
-import static org.mockito.Mockito.mock;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
 public class UnusedPrivateMethodCheckTest {
 
-  private final UnusedPrivateMethodCheck check = new UnusedPrivateMethodCheck();
-
-  public static SourceFile scan(CodeVisitor visitor) {
-    File baseDir = new File("src/test/resources/");
-    File bytecodeFile = new File("target/test-classes/");
-
-    JavaSquid javaSquid = new JavaSquid(new JavaConfiguration(Charset.forName("UTF-8")), mock(JavaResourceLocator.class), visitor);
-    javaSquid.scan(Collections.singleton(new File(baseDir, "Lambdas.java")), Collections.<File>emptyList(), Collections.singleton(bytecodeFile));
-
-    Collection<SourceCode> sources = javaSquid.getIndex().search(new QueryByType(SourceFile.class));
-    if (sources.size() != 1) {
-      throw new IllegalStateException("Only one SourceFile was expected whereas " + sources.size() + " has been returned.");
-    }
-    return (SourceFile) sources.iterator().next();
-  }
-
   @Test
   public void test() {
-    SourceFile file = BytecodeFixture.scan("UnusedPrivateMethod", check);
-    CheckMessagesVerifier.verify(file.getCheckMessages())
-        .next().withMessage("Private constructor 'org.sonar.java.checks.targets.UnusedPrivateMethod$A(UnusedPrivateMethod)' is never used.")
-        .next().atLine(54).withMessage("Private method 'unusedPrivateMethod' is never used.")
-        .next().atLine(57).withMessage("Private method 'unusedPrivateMethod' is never used.")
-        .next().atLine(67).withMessage("Private constructor 'org.sonar.java.checks.targets.UnusedPrivateMethod$Attribute(String,String[],int)' is never used.")
-        .noMore();
-  }
-
-  @Test
-  public void lambdas_should_not_raise_issue() throws Exception {
-    SourceFile file = scan(check);
-    CheckMessagesVerifier.verify(file.getCheckMessages())
-        .noMore();
+    JavaCheckVerifier.verify("src/test/files/checks/UnusedPrivateMethod.java", new UnusedPrivateMethodCheck());
   }
 
 }
