82f45db679248f0f91f33e5432387b07c9740026
Massimo Paladin <massimo.paladin@gmail.com>
2015-09-17 10:41:09 +0200

    Move ThrowsSeveralCheckedExceptionCheck test to JavaCheckVerifier

---
+++ java-checks/src/test/files/checks/ThrowsSeveralCheckedException.java
@@ -0,0 +1,131 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ThrowsSeveralCheckedException extends Base {
+
+  public void foo1() {
+  }
+
+  public void foo2() throws Throwable {
+  }
+
+  public void foo3() throws Error {
+  }
+
+  public void foo4() throws MyException {
+  }
+
+  public void foo5() throws RuntimeException {
+  }
+
+  public void foo6() throws IllegalArgumentException {
+  }
+
+  public void foo7() throws MyRuntimeException {
+  }
+
+  public void foo8() throws IllegalArgumentException, MyException, NullPointerException {
+  }
+
+  public void foo9() throws IOException, MyException { // Noncompliant {{Refactor this method to throw at most one checked exception instead of: java.io.IOException, ThrowsSeveralCheckedException.MyException}}
+  }
+
+  public void foo10() throws IOException, IOException, SQLException { // Noncompliant {{Refactor this method to throw at most one checked exception instead of: java.io.IOException, java.io.IOException, java.sql.SQLException}}
+  }
+
+  void foo11() throws IOException, IOException, SQLException {
+  }
+
+  public class MyException extends Exception {
+  }
+
+  public class MyRuntimeException extends RuntimeException {
+  }
+
+  @Override
+  public void overridenMethod() throws IOException, SQLException { // Compliant - overriden methods
+  }
+
+}
+
+class Base {
+
+  public void overridenMethod() throws IOException, SQLException { // Noncompliant
+  }
+
+}
+
+class Implements implements I {
+
+  @Override
+  public void foo() {
+  }
+
+  @Override
+  public void bar() throws IOException, SQLException { // Compliant - overriden
+  }
+
+  public void baz() {
+  }
+
+  public void qux() throws IOException, SQLException { // Noncompliant
+  }
+
+}
+
+interface I {
+  public void foo();
+
+  public void bar() throws IOException, SQLException; // Noncompliant
+
+}
+
+class BaseClass<T> {
+  public T meth() throws IOException, SQLException{return null;} // Noncompliant
+}
+class BaseChildClass<T> extends BaseClass<T>{}
+class Synth extends BaseChildClass<Integer> {
+  @Override
+  public Integer meth() throws IOException { return 1;}
+}
+
+class J {
+  public void method(int a, String b) throws IOException, SQLException{} // Noncompliant
+  public J method2(int a, String b) throws IOException, SQLException{return null;} // Noncompliant
+  public String method3(int a, String b) throws IOException, SQLException{return null;} // Noncompliant
+}
+class K extends J {
+  public void method(String a, String b) throws IOException, SQLException{} // Noncompliant
+  public void method(int a, String b) throws IOException, SQLException{}
+  public K method2(int a, String b) throws IOException, SQLException{return null;}
+  public String method3(int a) throws IOException, SQLException{return null;} // Noncompliant
+}
+interface L {
+  void method(String a, String b) throws IOException, SQLException; // Noncompliant
+}
+interface M extends L {}
+class O implements M {
+  public void method(String a, String b) throws IOException, SQLException {}
+}
+class P {
+  public void method(Q a, String b) throws IOException, SQLException {} // Noncompliant
+  private void privateMethod(Q a, String b) throws IOException, SQLException {}
+  class Q{}
+}
+class R extends P {
+  public void method(Q a, String b) throws IOException, SQLException {}
+  public static void foo(Q a, String b) throws IOException, SQLException { // Noncompliant
+    P p = new P() { // Ignore anonymous classes: false negative SONARJAVA-645
+      public void method(Q a, String b) throws IOException, SQLException {}
+    };
+  }
+}
+class S<T> {
+  public T method(T a) throws IOException, SQLException { return null;} // Noncompliant
+}
+class U extends S<String> {
+  public String method(String a) throws IOException, SQLException { return null; } // false positive
+}
+class V extends P {
+  public void privateMethod(Q a, String b) throws IOException, SQLException {} // Noncompliant
+}
--- java-checks/src/test/java/org/sonar/java/checks/ThrowsSeveralCheckedExceptionCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/ThrowsSeveralCheckedExceptionCheckTest.java
@@ -19,56 +19,14 @@
  */
 package org.sonar.java.checks;
 
-import com.google.common.collect.Lists;
-import org.junit.Rule;
 import org.junit.Test;
-import org.sonar.java.ast.JavaAstScanner;
-import org.sonar.java.model.VisitorsBridge;
-import org.sonar.squidbridge.api.SourceFile;
-import org.sonar.squidbridge.checks.CheckMessagesVerifierRule;
-
-import java.io.File;
-
-import static org.fest.assertions.Assertions.assertThat;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
 public class ThrowsSeveralCheckedExceptionCheckTest {
 
-  @Rule
-  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
-
-  private final ThrowsSeveralCheckedExceptionCheck check = new ThrowsSeveralCheckedExceptionCheck();
-
   @Test
   public void test() {
-    File bytecodeDir = new File("target/test-classes");
-    assertThat(bytecodeDir).exists();
-    SourceFile file = JavaAstScanner.scanSingleFile(new File("src/test/java/org/sonar/java/checks/targets/ThrowsSeveralCheckedExceptionCheck.java"), new VisitorsBridge(check, Lists.newArrayList(bytecodeDir)));
-    checkMessagesVerifier
-      .verify(file.getCheckMessages())
-
-      .next().atLine(53)
-      .withMessage(
-          "Refactor this method to throw at most one checked exception instead of: java.io.IOException, ThrowsSeveralCheckedExceptionCheck.MyException")
-
-      .next().atLine(56)
-      .withMessage("Refactor this method to throw at most one checked exception instead of: java.io.IOException, java.io.IOException, java.sql.SQLException")
-      .next().atLine(76)
-      .next().atLine(94)
-      .next().atLine(102)
-      .next().atLine(107)
-      .next().atLine(116)
-      .next().atLine(117)
-      .next().atLine(118)
-      .next().atLine(121)
-      .next().atLine(124)
-      .next().atLine(127)
-      .next().atLine(134)
-      .next().atLine(140)
-      .next().atLine(147)
-      .next().atLine(153)
-      .next().atLine(157)
-      .next().atLine(162)
-    ;
+    JavaCheckVerifier.verify("src/test/files/checks/ThrowsSeveralCheckedException.java", new ThrowsSeveralCheckedExceptionCheck());
   }
 
 }
