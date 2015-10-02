a93c294ae89891c055ca9da34a496348d4f846a4
Massimo Paladin <massimo.paladin@gmail.com>
2015-10-02 15:32:59 +0200

    Fix quality flaw: Add unit tests for SonarComponents

--- java-squid/src/test/java/org/sonar/java/SonarComponentsTest.java
+++ java-squid/src/test/java/org/sonar/java/SonarComponentsTest.java
@@ -21,17 +21,21 @@ package org.sonar.java;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.internal.DefaultFileSystem;
 import org.sonar.api.batch.rule.CheckFactory;
 import org.sonar.api.batch.rule.Checks;
 import org.sonar.api.component.ResourcePerspectives;
+import org.sonar.api.issue.Issuable;
+import org.sonar.api.measures.FileLinesContext;
 import org.sonar.api.measures.FileLinesContextFactory;
+import org.sonar.api.source.Highlightable;
+import org.sonar.api.source.Symbolizable;
 import org.sonar.plugins.java.api.CheckRegistrar;
 import org.sonar.plugins.java.api.JavaCheck;
 import org.sonar.squidbridge.api.CodeVisitor;
@@ -41,8 +45,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyCollectionOf;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -72,9 +78,8 @@ public class SonarComponentsTest {
     when(this.checks.addAnnotatedChecks(anyCollectionOf(Class.class))).thenReturn(this.checks);
   }
 
-  @After
   public void postTestExecutionChecks() {
-    // each time a SonarComponent is instanciated the following methods must be called twice
+    // each time a SonarComponent is instantiated the following methods must be called twice
     // once for custom checks, once for custom java checks
     verify(this.checkFactory, times(2)).create(REPOSITORY_NAME);
     verify(this.checks, times(2)).addAnnotatedChecks(anyCollectionOf(Class.class));
@@ -87,32 +92,30 @@ public class SonarComponentsTest {
     JavaTestClasspath javaTestClasspath = mock(JavaTestClasspath.class);
     ImmutableList<File> javaTestClasspathList = ImmutableList.of();
     when(javaTestClasspath.getElements()).thenReturn(javaTestClasspathList);
+    File file = new File("");
+    Issuable issuable = mock(Issuable.class);
+    when(resourcePerspectives.as(eq(Issuable.class), any(InputFile.class))).thenReturn(issuable);
+    Highlightable highlightable = mock(Highlightable.class);
+    when(resourcePerspectives.as(eq(Highlightable.class), any(InputFile.class))).thenReturn(highlightable);
+    Symbolizable symbolizable = mock(Symbolizable.class);
+    when(resourcePerspectives.as(eq(Symbolizable.class), any(InputFile.class))).thenReturn(symbolizable);
+    FileLinesContext fileLinesContext = mock(FileLinesContext.class);
+    when(fileLinesContextFactory.createFor(any(InputFile.class))).thenReturn(fileLinesContext);
 
-    JavaCheck expectedCheck = new CustomCheck();
-    CheckRegistrar expectedRegistrar = new CheckRegistrar() {
-      @Override
-      public void register(RegistrarContext registrarContext) {
-        registrarContext.registerClassesForRepository(
-          REPOSITORY_NAME,
-          Lists.<Class<? extends JavaCheck>>newArrayList(CustomTestCheck.class),
-          null);
-      }
-    };
-
-    when(this.checks.all()).thenReturn(Lists.newArrayList(expectedCheck)).thenReturn(new ArrayList<JavaCheck>());
-    SonarComponents sonarComponents = new SonarComponents(fileLinesContextFactory, resourcePerspectives, fs, null, javaTestClasspath, checkFactory, new CheckRegistrar[] {
-      expectedRegistrar
-    });
+    SonarComponents sonarComponents = new SonarComponents(fileLinesContextFactory, resourcePerspectives, fs, null, javaTestClasspath, checkFactory);
 
     CodeVisitor[] visitors = sonarComponents.checkClasses();
-    assertThat(visitors).hasSize(1);
-    assertThat(visitors[0]).isEqualTo(expectedCheck);
+    assertThat(visitors).hasSize(0);
     Collection<JavaCheck> testChecks = sonarComponents.testCheckClasses();
     assertThat(testChecks).hasSize(0);
     assertThat(sonarComponents.getFileSystem()).isEqualTo(fs);
     assertThat(sonarComponents.getResourcePerspectives()).isEqualTo(resourcePerspectives);
     assertThat(sonarComponents.getJavaClasspath()).isEmpty();
     assertThat(sonarComponents.getJavaTestClasspath()).isEqualTo(javaTestClasspathList);
+    assertThat(sonarComponents.issuableFor(file)).isEqualTo(issuable);
+    assertThat(sonarComponents.highlightableFor(file)).isEqualTo(highlightable);
+    assertThat(sonarComponents.symbolizableFor(file)).isEqualTo(symbolizable);
+    assertThat(sonarComponents.fileLinesContextFor(file)).isEqualTo(fileLinesContext);
   }
 
   @Test
@@ -138,6 +141,8 @@ public class SonarComponentsTest {
     assertThat(visitors[0]).isEqualTo(expectedCheck);
     Collection<JavaCheck> testChecks = sonarComponents.testCheckClasses();
     assertThat(testChecks).hasSize(0);
+
+    postTestExecutionChecks();
   }
 
   @Test
@@ -163,6 +168,8 @@ public class SonarComponentsTest {
     Collection<JavaCheck> testChecks = sonarComponents.testCheckClasses();
     assertThat(testChecks).hasSize(1);
     assertThat(testChecks.iterator().next()).isEqualTo(expectedCheck);
+
+    postTestExecutionChecks();
   }
 
   @Test
@@ -191,6 +198,8 @@ public class SonarComponentsTest {
     assertThat(testChecks).hasSize(1);
     assertThat(testChecks.iterator().next()).isEqualTo(expectedTestCheck);
     assertThat(sonarComponents.checks()).hasSize(2);
+
+    postTestExecutionChecks();
   }
 
   private static class CustomCheck implements JavaCheck {
