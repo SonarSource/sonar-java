b4e4f7b1152e99d4f9c4b6f879d9b7b406a5f583
DidierBesset <didier.besset@sonarsource.com>
2015-10-06 18:10:34 +0200

    SONARJAVA-1320 - Initialization of forEach loop
    Variables of forEach loop must be initialized to a new symbolic value,
    unrelated to any previous ones.
    
    squash  SONARJAVA-1320 - Initialization of forEach loop

--- java-squid/src/test/files/se/NullDereferenceCheck.java
+++ java-squid/src/test/files/se/NullDereferenceCheck.java
@@ -1,5 +1,7 @@
 @SomeAnnotation(name = value) package javax.annotation;
 
+import java.lang.Object;
+import java.util.ArrayList;
 import java.util.List;
 
 @interface CheckForNull {
@@ -358,6 +360,32 @@ class NullPointerTest {
     head.hashCode();
     value.hashCode();
   }
+  
+  public void testForEachLoopGood() {
+	  List<String> foos = collectFoos();
+	  for (String foo : foos) {
+		foo.toString();
+	}
+  }
+  
+  public void testPrimitiveForEachLoopGood() {
+	  boolean[] foos = collectFoos();
+	  for (boolean foo : foos) {
+		if (foo) {
+			println("true");
+		}
+	}
+  }
+
+  public static int testPrimitiveForEachLoopGuava(boolean... values) {
+	    int count = 0;
+	    for (boolean value : values) {
+	      if (value) {
+	        count++;
+	      }
+	    }
+	    return count;
+	  }
 
   public void testForEachLoopGood() {
     List<String> foos = collectFoos();
