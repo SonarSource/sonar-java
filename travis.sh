2cfd84b30c6df752d7c6b34b41794e76c0cbb56a
Massimo Paladin <massimo.paladin@gmail.com>
2015-12-04 10:55:44 +0100

    Run pr-analysis and dory analysis from travis

--- travis.sh
+++ travis.sh
@@ -8,10 +8,59 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
+function strongEcho {
+  echo ""
+  echo "================ $1 ================="
+}
+
 case "$TEST" in
 
 CI)
-  mvn verify -B -e -V
+  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    strongEcho "Build and analyze commit in master"
+    # this commit is master must be built and analyzed (with upload of report)
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -B -e -V
+
+    # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+    export PATH=$JAVA_HOME/bin:$PATH
+
+    mvn sonar:sonar -B -e -V \
+       -Dsonar.host.url=$SONAR_HOST_URL \
+       -Dsonar.login=$SONAR_TOKEN
+
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
+    # For security reasons environment variables are not available on the pull requests
+    # coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
+    # That's why the analysis does not need to be executed if the variable SONAR_GITHUB_OAUTH is not defined.
+
+    strongEcho "Build and analyze pull request"
+    # this pull request must be built and analyzed (without upload of report)
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -B -e -V
+
+    # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+    export PATH=$JAVA_HOME/bin:$PATH
+
+    # integration of jacoco report is quite memory-consuming
+    export MAVEN_OPTS="-Xmx1G -Xms128m"
+    mvn sonar:sonar -B -e -V \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
+
+  else
+    strongEcho 'Build, no analysis'
+    # Build branch, without any analysis
+
+    # No need for Maven goal "install" as the generated JAR file does not need to be installed
+    # in Maven local repository
+    mvn verify -B -e -V
+  fi
   ;;
 
 plugin|ruling)
