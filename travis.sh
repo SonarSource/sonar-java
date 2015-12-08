fbc17379490297afe507e58b1a7de3f6029285e6
Massimo Paladin <massimo.paladin@gmail.com>
2015-12-08 11:54:26 +0100

    Sonar analysis: run pr-analysis without coverage-per-test and increase dory analysis memory

--- travis.sh
+++ travis.sh
@@ -19,13 +19,15 @@ CI)
   if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     strongEcho "Build and analyze commit in master"
     # this commit is master must be built and analyzed (with upload of report)
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -B -e -V
+    mvn verify -B -e -V
 
     # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
-    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-    export PATH=$JAVA_HOME/bin:$PATH
+    source $HOME/.jdk_switcher_rc
+    jdk_switcher use oraclejdk8
 
-    mvn sonar:sonar -B -e -V \
+    # integration of jacoco report is quite memory-consuming
+    export MAVEN_OPTS="-Xmx1G -Xms128m"
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test sonar:sonar -B -e -V \
        -Dsonar.host.url=$SONAR_HOST_URL \
        -Dsonar.login=$SONAR_TOKEN
 
@@ -33,19 +35,16 @@ CI)
     # For security reasons environment variables are not available on the pull requests
     # coming from outside repositories
     # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-    # That's why the analysis does not need to be executed if the variable SONAR_GITHUB_OAUTH is not defined.
-
+    # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.
     strongEcho "Build and analyze pull request"
-    # this pull request must be built and analyzed (without upload of report)
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -B -e -V
+    mvn verify -B -e -V
 
     # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
-    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-    export PATH=$JAVA_HOME/bin:$PATH
+    source $HOME/.jdk_switcher_rc
+    jdk_switcher use oraclejdk8
 
-    # integration of jacoco report is quite memory-consuming
     export MAVEN_OPTS="-Xmx1G -Xms128m"
-    mvn sonar:sonar -B -e -V \
+    mvn verify sonar:sonar -B -e -V \
         -Dsonar.analysis.mode=issues \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
