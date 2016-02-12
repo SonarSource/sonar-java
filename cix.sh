5aa76539c8f16528a980333fb1b266483787ec44
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-02-12 16:05:06 +0100

    QA: add performancing to qa and factorize commands

--- cix.sh
+++ cix.sh
@@ -3,24 +3,19 @@ set -euo pipefail
 echo "Running $TEST with SQ=$SQ_VERSION"
 
 case "$TEST" in
-PLUGIN)
-  cd its/plugin
-  mvn package -Pit-plugin -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false -B -e -V
-;;
-RULING)
+ruling)
   #fetch submodule containing sources of ruling projects
   git submodule update --init --recursive
   #ruling requires java 8
   export JAVA_HOME=/opt/sonarsource/jvm/java-1.8.0-sun-x64
-  cd its/ruling
-  mvn package -Pit-ruling -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false -B -e -V
 ;;
-PERFORMANCE)
-  echo "ALMOST DONE !!"
+plugin|performancing)
 ;;
-
 *)
   echo "Unexpected TEST mode: $TEST"
   exit 1
 ;;
+
 esac
+cd its/$TEST
+mvn package -Pit-$TEST -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false -B -e -V
