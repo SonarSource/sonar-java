113158f5e90e4b9a88dae2c5cc8e168a6c2ae7c1
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-02-12 17:02:34 +0100

    QA: Add fetching its sources for performancing

--- cix.sh
+++ cix.sh
@@ -3,19 +3,22 @@ set -euo pipefail
 echo "Running $TEST with SQ=$SQ_VERSION"
 
 case "$TEST" in
-ruling)
-  #fetch submodule containing sources of ruling projects
-  git submodule update --init --recursive
-  #ruling requires java 8
-  export JAVA_HOME=/opt/sonarsource/jvm/java-1.8.0-sun-x64
-;;
-plugin|performancing)
-;;
-*)
-  echo "Unexpected TEST mode: $TEST"
-  exit 1
-;;
-
+  ruling)
+    #fetch submodule containing sources of ruling projects
+    git submodule update --init --recursive
+    #ruling requires java 8
+    export JAVA_HOME=/opt/sonarsource/jvm/java-1.8.0-sun-x64
+  ;;
+  performancing)
+    #fetch submodule containing sources of performancing projects
+    git submodule update --init --recursive
+  ;;
+  plugin)
+  ;;
+  *)
+    echo "Unexpected TEST mode: $TEST"
+    exit 1
+  ;;
 esac
 cd its/$TEST
 mvn package -Pit-$TEST -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false -B -e -V
