b6239f89e20f9f34fc5455e52e517ca34fbfec74
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-03-31 14:01:04 +0200

    SONARJAVA-1621 resolve methods of bounds of a type variable

--- java-frontend/src/test/files/resolve/GenericMethods.java
+++ java-frontend/src/test/files/resolve/GenericMethods.java
@@ -26,3 +26,31 @@ interface I2<K, V> {
   V put(K key, V value);
   V get(Object key);
 }
+
+class B {
+  void print(String s) { }
+
+  <T> void myMethod(T param) {
+    print(param.toString());
+  }
+
+  <T extends C> void myMethod1(T param) {
+    print(param.bar());
+  }
+
+  <T extends I3> void myMethod2(T param) {
+    print(param.foo());
+  }
+
+  <T extends C & I3> void myMethod3(T param) {
+    print(param.bar() + param.foo() + param.toString());
+  }
+}
+
+class C {
+  String bar() { return ""; }
+}
+
+interface I3 {
+  String foo();
+}
