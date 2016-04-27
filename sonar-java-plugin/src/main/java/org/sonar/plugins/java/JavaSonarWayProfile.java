89ea926781198b28c4cc003cb1f571f7a84434ce
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-04-27 10:24:04 +0200

    Handle legacy keys in SonarWay profile

--- sonar-java-plugin/src/main/java/org/sonar/plugins/java/JavaSonarWayProfile.java
+++ sonar-java-plugin/src/main/java/org/sonar/plugins/java/JavaSonarWayProfile.java
@@ -25,12 +25,15 @@ import com.google.gson.Gson;
 import org.sonar.api.profiles.ProfileDefinition;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.rules.Rule;
+import org.sonar.api.utils.AnnotationUtils;
 import org.sonar.api.utils.ValidationMessages;
 import org.sonar.java.checks.CheckList;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Replacement for org.sonar.plugins.squid.SonarWayProfile
@@ -44,8 +47,9 @@ public class JavaSonarWayProfile extends ProfileDefinition {
     RulesProfile profile = RulesProfile.create("Sonar way", Java.KEY);
     URL resource = JavaRulesDefinition.class.getResource("/org/sonar/l10n/java/rules/squid/Sonar_way_profile.json");
     Profile jsonProfile = gson.fromJson(readResource(resource), Profile.class);
+    Map<String, String> keys = legacyKeys();
     for (String key : jsonProfile.ruleKeys) {
-      profile.activateRule(Rule.create(CheckList.REPOSITORY_KEY, key), null);
+      profile.activateRule(Rule.create(CheckList.REPOSITORY_KEY, keys.get(key)), null);
     }
     return profile;
   }
@@ -56,6 +60,21 @@ public class JavaSonarWayProfile extends ProfileDefinition {
     } catch (IOException e) {
       throw new IllegalStateException("Failed to read: " + resource, e);
     }
+  }
+
+  private static Map<String, String> legacyKeys() {
+    Map<String, String> result = new HashMap<>();
+    for (Class checkClass : CheckList.getChecks()) {
+      org.sonar.check.Rule ruleAnnotation = AnnotationUtils.getAnnotation(checkClass, org.sonar.check.Rule.class);
+      String key = ruleAnnotation.key();
+      org.sonar.java.RspecKey rspecKeyAnnotation = AnnotationUtils.getAnnotation(checkClass, org.sonar.java.RspecKey.class);
+      if(rspecKeyAnnotation == null) {
+        result.put(key, key);
+      } else {
+        result.put(rspecKeyAnnotation.value(), key);
+      }
+    }
+    return result;
   }
 
   private static class Profile {
