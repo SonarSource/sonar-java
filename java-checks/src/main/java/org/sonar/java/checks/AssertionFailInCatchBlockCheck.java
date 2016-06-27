7dc7efdaabf83977c8aa25ea12d7df3c78d2f461
Alix LOURME <alix.lourme@gmail.com>
2016-06-27 12:40:38 +0200

    SONARJAVA-1750 Fail method usage in catch block (new rule RSPEC-3658) Squash for Remediation cost update and benzonico review fix x2

---
+++ java-checks/src/main/java/org/sonar/java/checks/AssertionFailInCatchBlockCheck.java
@@ -0,0 +1,54 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.java.matcher.NameCriteria;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.Tree.Kind;
+
+@Rule(key = "S3658")
+public class AssertionFailInCatchBlockCheck extends AbstractMethodDetection {
+
+  @Override
+  protected List<MethodMatcher> getMethodInvocationMatchers() {
+    return Lists.newArrayList(
+      MethodMatcher.create().typeDefinition("org.junit.Assert").name("fail").withNoParameterConstraint(),
+      MethodMatcher.create().typeDefinition("junit.framework.Assert").name(NameCriteria.startsWith("fail")).withNoParameterConstraint(),
+      MethodMatcher.create().typeDefinition("org.fest.assertions.Fail").name(NameCriteria.startsWith("fail")).withNoParameterConstraint());
+  }
+
+  @Override
+  protected void onMethodInvocationFound(MethodInvocationTree mit) {
+    if (isInCatch(mit)) {
+      reportIssue(mit, "Remove this failure assertion and simply add the exception type to the method signature.");
+    }
+  }
+
+  private static boolean isInCatch(Tree tree) {
+    Tree parent = tree.parent();
+    return parent != null && !parent.is(Kind.METHOD) && (parent.is(Kind.CATCH) || isInCatch(parent));
+  }
+}
---
+++ java-checks/src/test/files/checks/AssertionFailInCatchBlockCheck.java
@@ -0,0 +1,80 @@
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.IOException;
+
+public class MyTest {
+
+  static {
+    try {
+      File.createTempFile(null, null);
+    } catch (IOException e) {
+      Assert.fail(e.getMessage()); // Noncompliant [[sc=7;ec=34]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+    Assert.fail(); // Compliant, not in catch
+  }
+
+  @Test
+  public void testMethodKoWithMessage() {
+
+    try {
+      // Some code
+    } catch (Exception e) {
+      if (true) {
+        Assert.fail(e.getMessage()); // Noncompliant [[sc=9;ec=36]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+      }
+    }
+
+    try {
+      // Some code
+    } catch (Exception e) {
+      org.junit.Assert.fail(e.getMessage()); // Noncompliant [[sc=7;ec=44]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+
+    try {
+      // Some code
+    } catch (Exception e) {
+      junit.framework.Assert.fail(e.getMessage()); // Noncompliant [[sc=7;ec=50]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+
+    try {
+      // Some code
+    } catch (Exception e) {
+      org.fest.assertions.Fail.fail(); // Noncompliant [[sc=7;ec=38]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+
+  }
+
+  @Test
+  public void testMethodKoWithNoMessage() {
+    try {
+      // Some code
+    } catch (Exception e) {
+      Assert.fail(); // Noncompliant [[sc=7;ec=20]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+
+    try {
+      // Some code
+    } catch (Exception e) {
+      org.junit.Assert.fail(); // Noncompliant [[sc=7;ec=30]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+
+    try {
+      // Some code
+    } catch (Exception e) {
+      junit.framework.Assert.fail(); // Noncompliant [[sc=7;ec=36]] {{Remove this failure assertion and simply add the exception type to the method signature.}}
+    }
+
+  }
+
+  @Test
+  public void testMethodOk() throws Exception {
+    // Some code
+    Assert.fail("Compliant : out of try-catch");
+    try {
+      Assert.fail("Compliant : in try");
+    } catch (Exception e) {
+
+    }
+  }
+
+}
