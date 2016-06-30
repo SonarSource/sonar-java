ec87307930b9334a6259f6bf7c2c83c102560dcf
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-06-30 11:56:06 +0200

    SONARJAVA-1735 Improve regexp

--- java-checks/src/test/files/checks/naming/BadTestClassNameCheck.java
+++ java-checks/src/test/files/checks/naming/BadTestClassNameCheck.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-class A { // Noncompliant {{Rename class "A" to match the regular expression: '^((Test|IT)[a-zA-Z0-9]*|[A-Z][a-zA-Z0-9]*(Test|IT|TestCase|ITCase))$'}}
+class A { // Noncompliant {{Rename class "A" to match the regular expression: '^((Test|IT)[a-zA-Z0-9]+|[A-Z][a-zA-Z0-9]*(Test|IT|TestCase|ITCase))$'}}
   @Test
   void foo() {}
 }
