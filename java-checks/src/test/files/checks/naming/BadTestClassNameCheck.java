67b019dd60385a2cf2a71ef6e3620804e177892b
benzonico <nicolas.peru@gmail.com>
2016-06-30 14:22:38 +0200

    SONARJAVA-1735 Fix default regExp to match surefire/failsafe defaults (#913)
    * SONARJAVA-1735 Fix default regExp to match surefire/failsafe defaults
    
    * SONARJAVA-1735 Improve regexp

--- java-checks/src/test/files/checks/naming/BadTestClassNameCheck.java
+++ java-checks/src/test/files/checks/naming/BadTestClassNameCheck.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-class A { // Noncompliant {{Rename class "A" to match the regular expression: '^[A-Z][a-zA-Z0-9]*Test$'}}
+class A { // Noncompliant {{Rename class "A" to match the regular expression: '^((Test|IT)[a-zA-Z0-9]+|[A-Z][a-zA-Z0-9]*(Test|IT|TestCase|ITCase))$'}}
   @Test
   void foo() {}
 }
@@ -14,4 +14,29 @@ class B { // Compliant
   void foo() {}
 
   A a = new A() {};
+}
+class TestA {
+  @Test
+  void foo() {
+  }
+}
+class ATestCase {
+  @Test
+  void foo() {
+  }
+}
+class ITanIntegration {
+  @Test
+  void foo() {
+  }
+}
+class AnIT {
+  @Test
+  void foo() {
+  }
+}
+class AnITCase {
+  @Test
+  void foo() {
+  }
 }
