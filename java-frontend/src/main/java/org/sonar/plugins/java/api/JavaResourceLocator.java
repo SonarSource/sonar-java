d9c48e5bc1cc014858f7ebfe2a3364550d6d930e
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-07-21 14:27:49 +0200

    Fix quality flaw: Document JavaResourceLocator

--- java-frontend/src/main/java/org/sonar/plugins/java/api/JavaResourceLocator.java
+++ java-frontend/src/main/java/org/sonar/plugins/java/api/JavaResourceLocator.java
@@ -29,23 +29,58 @@ import javax.annotation.CheckForNull;
 import java.io.File;
 import java.util.Collection;
 
+/**
+ * Interface to get the mapping between java classes and files (as multiple classes can be declared in one file).
+ */
 @Beta
 @BatchSide
 public interface JavaResourceLocator extends JavaFileScanner {
 
   /**
+   * Retrieve a SonarQube resource by the class name.
+   * @param className fully qualified name of the analyzed class
    * @return null if not found
    */
   @CheckForNull
   InputFile findResourceByClassName(String className);
 
+  /**
+   * get source file key by class name.
+   * @deprecated since 4.1 : will be dropped with no replacement.
+   * @param className fully qualified name of the analyzed class.
+   * @return key of the source file for the given class.
+   */
+  @Deprecated
   String findSourceFileKeyByClassName(String className);
 
+  /**
+   * fully qualified names of the classes to be analyzed by sonar java analyzer.
+   * @deprecated since 4.1 : used by bytecode visitor scanner and should be dropped when it is removed.
+   * @return collection of fully qualified names.
+   */
+  @Deprecated
   Collection<String> classKeys();
 
+  /**
+   * .class files to be analyzed.
+   * Used by the findbugs plugin.
+   * @return a list of .class files corresponding to the source files to be analyzed.
+   */
   Collection<File> classFilesToAnalyze();
 
+  /**
+   * Classpath configured for the project.
+   * This classpath method is used by the findbugs plugin to configure the analysis.
+   * @return the list of jar and class files constituting the classpath of the analyzed project.
+   */
   Collection<File> classpath();
 
+  /**
+   * Get the first line of a method definition.
+   * @deprecated since 4.1 : used by bytecode visitor scanner and should be dropped when it is removed.
+   * @param fullyQualifiedMethodName the full name of the method.
+   * @return the line number where this method is defined.
+   */
+  @Deprecated
   Integer getMethodStartLine(String fullyQualifiedMethodName);
 }
