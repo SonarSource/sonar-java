b6afda26dd579b517f3632ffb0bef2c16feff847
Janos Gyerik <janos@kronos>
2016-08-05 13:37:55 +0200

    SONARJAVA-1800 Add examples of all supported formats of @see

--- java-checks/src/test/files/checks/TooLongLine_S00103_Check/LineLengthSeeWithinBlockCommentNoncompliant.java
+++ java-checks/src/test/files/checks/TooLongLine_S00103_Check/LineLengthSeeWithinBlockCommentNoncompliant.java
@@ -1,8 +1,12 @@
-// Noncompliant@+4 {{Split this 208 characters long line (which is greater than 100 authorized).}}
+// Noncompliant@+6 {{Split this 206 characters long line (which is greater than 100 authorized).}}
+// Noncompliant@+6 {{Split this 138 characters long line (which is greater than 100 authorized).}}
+// Noncompliant@+6 {{Split this 134 characters long line (which is greater than 100 authorized).}}
 /**
  * Example of usage:
  * <pre>
- * Noncompliant: long see, but also long text on the same line, longer than the marker line at the top. @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1">HTTP/1.1 documentation</a>
+ * Long see, but also long text on the same line, longer than the marker line above.................. @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1">HTTP/1.1 documentation</a>
+ * Long see, but also long text on the same line, longer than the marker line above.................. @see "The Java Programming Language"
+ * Long see, but also long text on the same line, longer than the marker line above.................. @see package.class#member  label
  * </pre>
  */
 class LineLength {}
