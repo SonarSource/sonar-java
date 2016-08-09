240ce977641bbbbba0e5af0bf79b83e2ef5f1e09
Janos Gyerik <janos@kronos>
2016-08-09 15:15:57 +0200

    SONARJAVA-1800 Merge test files, use just one per configuration

--- java-checks/src/test/files/checks/TooLongLine_S00103_Check/LineLengthSeeWithinBlockComment.java
+++
@@ -1,9 +0,0 @@
-/**
- * Example of usage:
- * <pre>
- * @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1">HTTP/1.1 documentation</a>
- * @see "The Java Programming Language"
- * @see package.class#member  label
- * </pre>
- */
-class LineLength {}
--- java-checks/src/test/files/checks/TooLongLine_S00103_Check/LineLengthSeeWithinInlineCommentNoncompliant.java
+++
@@ -1,7 +0,0 @@
-// Noncompliant@+1 {{Split this 206 characters long line (which is greater than 100 authorized).}}
-// Long see, but also long text on the same line, longer than the marker line above.................. @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1">HTTP/1.1 documentation</a>
-// Noncompliant@+1 {{Split this 138 characters long line (which is greater than 100 authorized).}}
-// Long see, but also long text on the same line, longer than the marker line above.................. @see "The Java Programming Language"
-// Noncompliant@+1 {{Split this 134 characters long line (which is greater than 100 authorized).}}
-// Long see, but also long text on the same line, longer than the marker line above.................. @see package.class#member  label
-class LineLength {}
