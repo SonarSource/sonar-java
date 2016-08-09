501eeadf73f6e84bbe3eedd5e233ee4d748ad50e
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-08-09 10:05:20 +0200

    SONARJAVA-1809 Fix NPE on EnumConstant with body (#952)
    * SONARJAVA-1809 Fix NPE on EnumConstant with body
    
    * SONARJAVA-1809 Add missing test cases

--- java-checks/src/test/files/checks/Dit.java
+++ java-checks/src/test/files/checks/Dit.java
@@ -1,15 +1,23 @@
 public class Dit extends One { // Noncompliant {{This class has 3 parents which is greater than 2 authorized.}}
 
   void foo() {
-    Object o = new Dit() { // Noncompliant {{This class has 4 parents which is greater than 2 authorized.}}
-
-    };
+    Object o = new Dit() {}; // Noncompliant {{This class has 4 parents which is greater than 2 authorized.}}
   }
 }
 
-class One extends Two {
-}
+class One extends Two {}
+class Two {}
 
-class Two {
+enum Any {
+  enumConst { // Compliant - enum constant are not considered (level=3: enumConst -> Any -> Enum<Any> -> Object)
 
+    public static class Dit2 extends One {} // Noncompliant
+
+    @Override
+    public String getString() {
+      return "string";
+    }
+  };
+
+  public abstract String getString();
 }
