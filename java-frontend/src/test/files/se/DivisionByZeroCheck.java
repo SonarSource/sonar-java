258d4898206fddb6e09ce21de60a1ad0d0b4e669
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-08-10 09:35:24 +0200

    SONARJAVA-1808 simplify test case triggering ISE without modification

--- java-frontend/src/test/files/se/DivisionByZeroCheck.java
+++ java-frontend/src/test/files/se/DivisionByZeroCheck.java
@@ -373,25 +373,17 @@ class A {
   }
 
   private void decodeBigInteger(int value) throws Exception {
-    boolean done = false;
-    while (!done) {
-      long lowBits = 0;
-      while (value > 0 && !done) {
-        int b = value & 0xFF;
-        if (b == 0) {
-          done = true;
-          if (lowBits == 0) {
-            // do something
-          }
-        } else if (b != 0xFF) {
-          if (value == 1) {
-            int digit = (b >>> 4) & 0x0F;
-            lowBits = lowBits * 10 + digit;
-          }
+    long lowBits = 0;
+    while (value > 0) {
+      int b = value & 1;
+      if (b == 0) {
+        if (lowBits == 0) {
+          // do something
         }
-        value++;
+      } else {
+        lowBits += b;
       }
-        lowBits = -lowBits;
     }
+    lowBits = -lowBits;
   }
 }
