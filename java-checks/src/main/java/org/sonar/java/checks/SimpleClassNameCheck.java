314a56f4a3b41d0ba1b3469bf1eb3300494b4c8d
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-08-10 09:12:18 +0200

    cleanup

--- java-checks/src/main/java/org/sonar/java/checks/SimpleClassNameCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/SimpleClassNameCheck.java
@@ -20,6 +20,8 @@
 package org.sonar.java.checks;
 
 import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
 import org.sonar.check.Rule;
 import org.sonar.java.resolve.SemanticModel;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
@@ -30,9 +32,6 @@ import org.sonar.plugins.java.api.tree.ImportTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.Tree.Kind;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Rule(key = "S1942")
 public class SimpleClassNameCheck extends IssuableSubscriptionVisitor {
