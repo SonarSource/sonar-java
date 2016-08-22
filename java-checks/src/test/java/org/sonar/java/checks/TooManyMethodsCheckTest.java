5b38a1cc1a0825c92519aa3d7b69089409ffdddb
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-08-22 14:22:53 +0200

    Fix quality flaw

--- java-checks/src/test/java/org/sonar/java/checks/TooManyMethodsCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/TooManyMethodsCheckTest.java
@@ -22,7 +22,7 @@ package org.sonar.java.checks;
 import org.junit.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
-public class  TooManyMethodsCheckTest {
+public class TooManyMethodsCheckTest {
 
   @Test
   public void test() {
