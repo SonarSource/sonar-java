caf27ec1c18364906a32196a1f694237612e2114
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-09-01 17:59:41 +0200

    SONARJAVA-1893 Xproc: implement method behavior

---
+++ java-frontend/src/test/resources/se/MethodBehavior.java
@@ -0,0 +1,28 @@
+abstract class MethodBehavior {
+  public boolean topMethod(boolean a) {
+    boolean result = false;
+    if(a) {
+      result = bar(a);
+    } else {
+      result = foo(a);
+    }
+    qix();
+    int i = "".length();
+    return result;
+  }
+
+  private boolean foo(boolean a) {
+    return a;
+  }
+
+  private boolean bar(boolean a) {
+    return !a;
+  }
+
+  private abstract void qix();
+
+  private void independent(){
+  }
+
+
+}
