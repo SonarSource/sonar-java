d6df8849a3a59717c957d886c416bfec8574e135
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-09-05 19:21:34 +0200

    XProc: Introduce method yields

--- java-frontend/src/test/java/org/sonar/java/se/SymbolicExecutionVisitorTest.java
+++ java-frontend/src/test/java/org/sonar/java/se/SymbolicExecutionVisitorTest.java
@@ -22,17 +22,22 @@ package org.sonar.java.se;
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
 import com.sonar.sslr.api.typed.ActionParser;
+
 import org.junit.Test;
 import org.sonar.java.ast.parser.JavaParser;
 import org.sonar.java.resolve.SemanticModel;
 import org.sonar.java.se.checks.NullDereferenceCheck;
+import org.sonar.java.se.constraint.BooleanConstraint;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.semantic.Symbol.MethodSymbol;
 import org.sonar.plugins.java.api.tree.CompilationUnitTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -49,6 +54,39 @@ public class SymbolicExecutionVisitorTest {
     // check order of method exploration : last is the topMethod as it requires the other to get its behavior.
     // Then, as we explore fully a path before switching to another one (see the LIFO in EGW) : qix is handled before foo.
     assertThat(sev.behaviorCache.keySet().stream().map(Symbol.MethodSymbol::name).collect(Collectors.toList())).containsSequence("topMethod", "bar", "qix", "foo", "independent");
+  }
+
+  @Test
+  public void method_behavior_yields() {
+    SymbolicExecutionVisitor sev = createSymbolicExecutionVisitor("src/test/resources/se/MethodYields.java");
+    Optional<MethodSymbol> topMethod = sev.behaviorCache.keySet().stream().filter(s -> "method".equals(s.name())).findFirst();
+
+    assertThat(topMethod.isPresent()).isTrue();
+    MethodBehavior mb = sev.behaviorCache.get(topMethod.get());
+    List<MethodYield> yields = mb.yields();
+    assertThat(yields).hasSize(3);
+
+    List<MethodYield> trueResults = yields.stream().filter(my -> BooleanConstraint.TRUE.equals(my.result.constraint)).collect(Collectors.toList());
+    assertThat(trueResults).hasSize(1);
+
+    // 'a' has constraint "null"
+    assertThat(trueResults.get(0).parameters.get(0).constraint.isNull()).isTrue();
+    // no constraint on 'b'
+    assertThat(trueResults.get(0).parameters.get(1).constraint).isNull();
+
+    List<MethodYield> falseResults = yields.stream().filter(my -> BooleanConstraint.FALSE.equals(my.result.constraint)).collect(Collectors.toList());
+    assertThat(falseResults).hasSize(2);
+    // for both results, 'a' has the constraint "not null"
+    assertThat(falseResults.stream().filter(my -> !my.parameters.get(0).constraint.isNull()).count()).isEqualTo(2);
+    // when 'b' has constraint "false", result is 'b'
+    assertThat(falseResults.stream()
+      .filter(my -> {
+        MethodYield.ConstrainedSymbolicValue b = my.parameters.get(1);
+        return BooleanConstraint.FALSE.equals(b.constraint) && b.symbolicValue.equals(my.result.symbolicValue);
+      }).count()).isEqualTo(1);
+
+    // FIXME SV associated to 'b' is cleaned in the true branch, so the constraint is lost when it should be 'true'
+    assertThat(falseResults.stream().filter(my -> my.parameters.get(1).constraint == null).count()).isEqualTo(1);
   }
 
   @Test
