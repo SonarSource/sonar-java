fc1f2da108b52c7221c2a5c317fb9ea9ae25ad07
Alban Auzeill <alban.auzeill@sonarsource.com>
2016-09-05 17:56:43 +0200

    SONARJAVA-1768 S1874 : CallToDeprecatedMethod: overriding a concrete deprecated method should raise an issue

--- java-checks/src/test/files/checks/CallToDeprecatedMethod.java
+++ java-checks/src/test/files/checks/CallToDeprecatedMethod.java
@@ -1,8 +1,8 @@
 import java.lang.Deprecated;
 
-public class CallToDeprecatedMethodCheck {
+public class CallToDeprecatedMethod {
 
-  public CallToDeprecatedMethodCheck() {
+  public CallToDeprecatedMethod() {
     String string = new String("my string");
     string.getBytes(1, 1, new byte[3], 7); // Noncompliant [[sc=12;ec=20]] {{Remove this use of "getBytes"; it is deprecated.}}
     new DeprecatedConstructor(); // Noncompliant [[sc=9;ec=30]] {{Remove this use of "DeprecatedConstructor"; it is deprecated.}}
@@ -19,12 +19,43 @@ public class CallToDeprecatedMethodCheck {
     static int a;
   }
 
+  public static class ExtendsDeprecatedClass extends MyDeprecatedClass { // Noncompliant [[sc=54;ec=71]] {{Remove this use of "MyDeprecatedClass"; it is deprecated.}}
+  }
+
+  public static abstract class ClassWithDeprecatedMethods {
+    @Deprecated
+    public void deprecatedMethod1() {
+    }
+    @Deprecated
+    public void deprecatedMethod2() {
+    }
+    @Deprecated
+    public abstract void deprecatedMethod3();
+  }
+
+  public interface InterfaceWithDeprecatedMethods {
+    @Deprecated
+    public void deprecatedMethod4();
+  }
+
+  public static class ClassOverridingDeprecatedMethods extends ClassWithDeprecatedMethods implements InterfaceWithDeprecatedMethods {
+    public void deprecatedMethod1() { // Noncompliant [[sc=17;ec=34]] {{Don't override a deprecated method or explicitly mark it as "@Deprecated".}}
+    }
+    @Deprecated
+    public void deprecatedMethod2() { // Compliant, explicitely marked as "@Deprecated"
+    }
+    public void deprecatedMethod3() { // Compliant, override abstract method
+    }
+    public void deprecatedMethod4() { // Compliant, override interface
+    }
+  }
+
   private static class DeprecatedConstructor {
     @Deprecated
     public DeprecatedConstructor() {
-      string.getBytes(1, 1, new byte[3], 7);
+      "".getBytes(1, 1, new byte[3], 7);
     }
-    Object a  = "".getBytes(1, 1, new byte[3], 7); // Noncompliant
+    String a  = new String(new byte[3], 7); // Noncompliant
   }
 
   @Deprecated
@@ -36,8 +67,8 @@ public class CallToDeprecatedMethodCheck {
     @Deprecated
     void foo() {
       Object a = new DeprecatedConstructor();
-      string.getBytes(1, 1, new byte[3], 7);
-     }
+      "".getBytes(1, 1, new byte[3], 7);
+    }
   }
 
 }
