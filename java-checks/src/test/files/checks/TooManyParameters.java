87fc37a2f6987a4195fcff0f5f90fffec26b51e8
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-10-31 09:31:31 +0100

    SONARJAVA-1852 S00107 ignores methods annotated with JsonCreator (#1054)

--- java-checks/src/test/files/checks/TooManyParameters.java
+++ java-checks/src/test/files/checks/TooManyParameters.java
@@ -1,4 +1,5 @@
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 class TooManyParameters {
   TooManyParameters(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8) { // Noncompliant {{Constructor has 8 parameters, which is greater than 7 authorized.}}
@@ -25,4 +26,9 @@ class MethodsUsingSpringRequestMapping {
 
   @RequestMapping
   void bar(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8) {} // Compliant
+}
+
+class MethodsUsingJsonCreator {
+  @JsonCreator
+  void foo(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8) {} // Compliant
 }
