18e3cdbeecc7df321080faf18fd78957c30ec9b4
Magnus Reftel <magnus.reftel@skatteetaten.no>
2016-11-12 10:05:19 +0100

    Refactor HardcodedIpCheck.java

--- java-checks/src/main/java/org/sonar/java/checks/HardcodedIpCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/HardcodedIpCheck.java
@@ -20,15 +20,17 @@
 package org.sonar.java.checks;
 
 import com.google.common.base.Splitter;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.JavaFileScanner;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
 import org.sonar.plugins.java.api.tree.LiteralTree;
 import org.sonar.plugins.java.api.tree.Tree;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Rule(key = "S1313")
 public class HardcodedIpCheck extends BaseTreeVisitor implements JavaFileScanner {
@@ -48,17 +50,21 @@ public class HardcodedIpCheck extends BaseTreeVisitor implements JavaFileScanner
     if (tree.is(Tree.Kind.STRING_LITERAL)) {
       IP.reset(tree.value());
       if (IP.matches()) {
+        ArrayList<Integer> octets = new ArrayList<>(4);
         String ip = IP.group(1);
-        if (areAllBelow256(Splitter.on('.').split(ip))) {
+        for (String s : Splitter.on('.').split(ip)) {
+          octets.add(Integer.valueOf(s));
+        }
+        if (areAllBelow256(octets)) {
           context.reportIssue(this, tree, "Make this IP \"" + ip + "\" address configurable.");
         }
       }
     }
   }
 
-  private static boolean areAllBelow256(Iterable<String> numbersAsStrings) {
-    for (String numberAsString : numbersAsStrings) {
-      if (Integer.valueOf(numberAsString) > 255) {
+  private static boolean areAllBelow256(Iterable<Integer> octets) {
+    for (int octet : octets) {
+      if (octet > 255) {
         return false;
       }
     }
