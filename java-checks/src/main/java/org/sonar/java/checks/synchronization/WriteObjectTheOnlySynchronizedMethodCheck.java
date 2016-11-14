e176be8dae07d04fb0ce7993619a8f38241a994c
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2016-11-14 19:13:41 +0100

    SONARJAVA-1905 Rule S3042: "writeObject" should not be the only "synchronized" code in a class

---
+++ java-checks/src/main/java/org/sonar/java/checks/synchronization/WriteObjectTheOnlySynchronizedMethodCheck.java
@@ -0,0 +1,92 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.synchronization;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.serialization.SerializableContract;
+import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.java.model.ModifiersUtils;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.SynchronizedStatementTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import java.util.List;
+
+import static org.sonar.java.model.ModifiersUtils.hasModifier;
+import static org.sonar.plugins.java.api.tree.Modifier.SYNCHRONIZED;
+import static org.sonar.plugins.java.api.tree.Tree.Kind.METHOD;
+
+@Rule(key = "S3042")
+public class WriteObjectTheOnlySynchronizedMethodCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(METHOD);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    if (!hasSemantic()) {
+      return;
+    }
+    MethodTree methodTree = (MethodTree) tree;
+    Symbol.TypeSymbol enclosingClass = methodTree.symbol().enclosingClass();
+    String className = enclosingClass.type().fullyQualifiedName();
+    MethodMatcher writeObjectMatcher = SerializableContract.writeObjectMatcher(className);
+    if (writeObjectMatcher.matches(methodTree) && hasModifier(methodTree.modifiers(), SYNCHRONIZED)) {
+      SynchronizationVisitor visitor = new SynchronizationVisitor(methodTree);
+      enclosingClass.declaration().accept(visitor);
+      if (!visitor.moreThanSingleLock) {
+        reportIssue(ModifiersUtils.getModifier(methodTree.modifiers(), SYNCHRONIZED), "Remove this \"synchronized\" keyword.");
+      }
+    }
+  }
+
+  private static class SynchronizationVisitor extends BaseTreeVisitor {
+
+    private final MethodTree writeObjectMethodTree;
+    private boolean moreThanSingleLock;
+
+    public SynchronizationVisitor(MethodTree writeObjectMethodTree) {
+      this.writeObjectMethodTree = writeObjectMethodTree;
+    }
+
+    @Override
+    public void visitSynchronizedStatement(SynchronizedStatementTree tree) {
+      moreThanSingleLock = true;
+    }
+
+    @Override
+    public void visitMethod(MethodTree tree) {
+      if (tree == writeObjectMethodTree || moreThanSingleLock) {
+        return;
+      }
+      if (hasModifier(tree.modifiers(), SYNCHRONIZED)) {
+        moreThanSingleLock = true;
+        return;
+      }
+      super.visitMethod(tree);
+    }
+  }
+}
---
+++ java-checks/src/test/files/checks/WriteObjectTheOnlySynchronizedMethodCheck.java
@@ -0,0 +1,84 @@
+import java.io.*;
+
+public class RubberBall {
+
+  public void bounce(float angle, float velocity) {
+  }
+
+  private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Noncompliant {{Remove this "synchronized" keyword.}}
+    // ...
+  }
+}
+
+public class RubberBall2 {
+
+  public void bounce(float angle, float velocity) {
+  }
+
+  private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Compliant, maybe FN
+    // ...
+  }
+
+  class Nested {
+
+    public void bounce(float angle, float velocity) {
+    }
+
+    private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Noncompliant {{Remove this "synchronized" keyword.}}
+      // ...
+    }
+  }
+
+  class NestedCompliant {
+
+    public Nested(Color color, int diameter) {
+      synchronized (this) {
+        System.out.println("This is OK");
+      }
+    }
+
+    private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Compliant
+      // ...
+    }
+  }
+}
+
+public class ClassWithSynchronizedStmtIsOK {
+
+  public void bounce(float angle, float velocity) {
+    synchronized (this) {
+      // ...
+    }
+  }
+
+  private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Compliant
+    // ...
+  }
+}
+
+public class TwoSynchrozniedMethodIsOK {
+
+  public RubberBall(Color color, int diameter) {  }
+
+  public synchronized void bounce(float angle, float velocity) {
+
+  }
+
+  private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Compliant
+    // ...
+  }
+}
+
+public class Nested3 {
+
+  private synchronized void writeObject(ObjectOutputStream stream) throws IOException { // Compliant
+  }
+
+  class Nested {
+    Nested() {
+      synchronized (this) {
+
+      }
+    }
+  }
+}
