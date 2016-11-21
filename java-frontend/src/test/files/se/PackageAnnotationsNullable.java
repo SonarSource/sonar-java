152e6facf15134943a4e31bbf16efce11516ee33
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-11-21 14:45:44 +0100

    SONARJAVA-1932 only consider nullable non-primitive parameters when package annotated with ParametersAreNullableByDefault

--- java-frontend/src/test/files/se/PackageAnnotationsNullable.java
+++ java-frontend/src/test/files/se/PackageAnnotationsNullable.java
@@ -4,4 +4,14 @@ class PackageAnnotations {
   void foo(Object input) {
     input.toString(); // Noncompliant {{NullPointerException might be thrown as 'input' is nullable here}}
   }
+
+  private Integer bar1(int i) { return i; }
+  private void qix1(int i) {
+    bar1(i).intValue(); // Compliant - primitive types cannot be null, despite the fact that the package is annotated with "ParametersAreNullableByDefault"
+  }
+
+  private Integer bar2(Integer i) { return i; }
+  private void qix2(Integer i) {
+    bar2(i).intValue(); // Noncompliant {{NullPointerException might be thrown as 'bar2' is nullable here}}
+  }
 }
