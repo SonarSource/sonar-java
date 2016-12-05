c0af377cec131f97a72519ea25217eced579716d
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-12-05 11:46:58 +0100

    SONARJAVA-1952 Use LOC for S138

--- java-checks/src/test/files/checks/MethodTooBigCheckCustom5.java
+++ java-checks/src/test/files/checks/MethodTooBigCheckCustom5.java
@@ -1,14 +1,14 @@
 class A {
   public A() { // Noncompliant {{This method has 6 lines, which is greater than the 5 lines authorized. Split it into smaller methods.}}
-    // 2
-    // 3
-    // 4
-    // 5
-  } // 6
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+  }
 
   public void f() { // 1
-    // 2
-    // 3
-    // 4
-  } // 5
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+  }
 }
