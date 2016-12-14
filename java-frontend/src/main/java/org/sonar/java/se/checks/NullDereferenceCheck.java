f6f9bab38f218e2445d3f1921d04348ff0814873
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2016-12-14 16:21:43 +0100

    SONARJAVA-1998 NPE issue location is also item of the flow

--- java-frontend/src/main/java/org/sonar/java/se/checks/NullDereferenceCheck.java
+++ java-frontend/src/main/java/org/sonar/java/se/checks/NullDereferenceCheck.java
@@ -19,6 +19,7 @@
  */
 package org.sonar.java.se.checks;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import org.sonar.check.Rule;
 import org.sonar.java.se.CheckerContext;
@@ -34,9 +35,7 @@ import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Rule(key = "S2259")
 public class NullDereferenceCheck extends SECheck {
@@ -87,14 +86,15 @@ public class NullDereferenceCheck extends SECheck {
     ProgramState programState = context.getState();
     Constraint constraint = programState.getConstraint(currentVal);
     if (constraint != null && constraint.isNull()) {
-      String message = "NullPointerException might be thrown as '" + SyntaxTreeNameFinder.getName(syntaxNode) + "' is nullable here";
-      Set<List<JavaFileScannerContext.Location>> flows = new HashSet<>();
+      String symbolName = SyntaxTreeNameFinder.getName(syntaxNode);
+      String message = "NullPointerException might be thrown as '" + symbolName + "' is nullable here";
       SymbolicValue val = null;
       if (!SymbolicValue.NULL_LITERAL.equals(currentVal)) {
         val = currentVal;
       }
-      flows.add(FlowComputation.flow(context.getNode(), val));
-      context.reportIssue(syntaxNode, this, message, flows);
+      List<JavaFileScannerContext.Location> flow = FlowComputation.flow(context.getNode(), val);
+      addDereferenceMessage(flow, syntaxNode);
+      reportIssue(syntaxNode, message, ImmutableSet.of(flow));
       return null;
     }
     constraint = programState.getConstraint(currentVal);
@@ -103,6 +103,17 @@ public class NullDereferenceCheck extends SECheck {
       return programState.addConstraint(currentVal, ObjectConstraint.NOT_NULL);
     }
     return programState;
+  }
+
+  private static void addDereferenceMessage(List<JavaFileScannerContext.Location> flow, Tree syntaxNode) {
+    String symbolName = SyntaxTreeNameFinder.getName(syntaxNode);
+    String msg;
+    if (syntaxNode.is(Tree.Kind.MEMBER_SELECT) && ((MemberSelectExpressionTree) syntaxNode).expression().is(Tree.Kind.METHOD_INVOCATION)) {
+      msg = String.format("Result of %s() is dereferenced", symbolName);
+    } else {
+      msg = String.format("%s is dereferenced", symbolName);
+    }
+    flow.add(new JavaFileScannerContext.Location(msg, syntaxNode));
   }
 
   @Override
