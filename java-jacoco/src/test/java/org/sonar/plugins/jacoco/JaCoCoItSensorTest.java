cb1baeadb6f9beaab8a04a639a33b90944f73f19
benzonico <nicolas.peru@gmail.com>
2017-01-11 09:05:38 +0100

    SONARJAVA-2041 Update parent pom and license headers (#1176)

--- java-jacoco/src/test/java/org/sonar/plugins/jacoco/JaCoCoItSensorTest.java
+++ java-jacoco/src/test/java/org/sonar/plugins/jacoco/JaCoCoItSensorTest.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Java
- * Copyright (C) 2010-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
+ * Copyright (C) 2010-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
