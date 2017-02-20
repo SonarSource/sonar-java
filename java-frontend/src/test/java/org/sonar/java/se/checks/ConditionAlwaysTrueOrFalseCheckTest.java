1c293dd46f94e85f78063b46e9d2518ed2f629a1
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-02-20 16:18:34 +0100

    SONARJAVA-2116 Program State equality need to rely on full stack

--- java-frontend/src/test/java/org/sonar/java/se/checks/ConditionAlwaysTrueOrFalseCheckTest.java
+++ java-frontend/src/test/java/org/sonar/java/se/checks/ConditionAlwaysTrueOrFalseCheckTest.java
@@ -31,8 +31,7 @@ public class ConditionAlwaysTrueOrFalseCheckTest {
 
   @Test
   public void whole_stack_required_for_ps_equality() throws Exception {
-    // issue raised is a false positive : SONARJAVA-2116
-    JavaCheckVerifier.verify("src/test/files/se/PsEqualityRequiresFullStack.java", new ConditionAlwaysTrueOrFalseCheck());
+    JavaCheckVerifier.verifyNoIssue("src/test/files/se/PsEqualityRequiresFullStack.java", new ConditionAlwaysTrueOrFalseCheck());
   }
 
   @Test
