998d35653df3741d79a4f26ac8ffec276d4c2898
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-02-23 16:27:55 +0100

    SONARJAVA-2100 review feedback

--- java-frontend/src/test/files/se/ThisSuperNotNull.java
+++ java-frontend/src/test/files/se/ThisSuperNotNull.java
@@ -1,12 +1,12 @@
-class A {
+class Super {}
+
+class A extends Super {
 
   void f() {
     if (this == null) { // Noncompliant {{Change this condition so that it does not always evaluate to "false"}}
 
     }
-    if (super == null) { // Noncompliant {{Change this condition so that it does not always evaluate to "false"}}
-
-    }
+    super.toString(); // Compliant, as super is never null
   }
 
   void jdk9_zippath_fp(A o) {
