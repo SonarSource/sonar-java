b437b8c0d2eabb93cce21719fb3b78f680dc31f4
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-03-01 15:44:49 +0100

    SONARJAVA-2150 Only raise one issue by file on the first usage of sun class.

--- java-checks/src/test/files/checks/SunPackagesUsedCheck.java
+++ java-checks/src/test/files/checks/SunPackagesUsedCheck.java
@@ -1,19 +1,19 @@
-import com.sun.imageio.plugins; // Noncompliant {{Replace this usage of Sun classes by ones from the Java API.}}
-import com.sun.jersey.api.client.ClientHandlerException; // Noncompliant
+import com.sun.imageio.plugins; // Noncompliant [[sc=8;ec=31;secondary=2,7,8,10,11,13,17]]  {{Use classes from the Java API instead of Sun classes.}}
+import com.sun.jersey.api.client.ClientHandlerException; // secondary
 import java.util.ArrayList;
 
 class A {
   private void f() {
-    com.sun.imageio.plugins.bmp d = new com.sun.imageio.plugins.bmp(); // Noncompliant
-    new com.sun.imageio.plugins.bmp(); // Noncompliant
+    com.sun.imageio.plugins.bmp d = // secondary
+      new com.sun.imageio.plugins.bmp(); // secondary
     java.util.List a;
-    sun.Foo b; // Noncompliant
-    db.setErrorHandler(new com.sun.org.apache.xml.internal.security.utils // Noncompliant
+    sun.Foo b; // secondary
+    db.setErrorHandler(new com.sun.org.apache.xml.internal.security.utils // secondary
         .IgnoreAllErrorHandler());
-    sun       // Noncompliant
+    sun       // secondary
         .Foo.toto
         .asd c;
 
-    new Foo<com.sun.Bar>() {}; // Noncompliant
+    new Foo<com.sun.Bar>() {}; // secondary
   }
 }
