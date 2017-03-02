f5d0e4a2e6203ef74672a9dca3dd99a3edc0f98c
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-03-02 19:13:58 +0100

    SONARJAVA-2151 Only raise issues when overriding implemented method

--- java-checks/src/test/files/checks/OverrideAnnotationCheck.java
+++ java-checks/src/test/files/checks/OverrideAnnotationCheck.java
@@ -2,21 +2,21 @@ class A {
   void foo(){} // Compliant
 }
 interface I {
-  public void finalize(); // Noncompliant [[sc=15;ec=23]] {{Add the "@Override" annotation above this method signature}}
+  public void finalize(); // Compliant - from Object
   void bar(); // Compliant
 }
 interface J extends I {
-  public void finalize(); // Noncompliant
-  void bar(); // Noncompliant
+  public void finalize(); // Compliant - from Object
+  void bar(); // Compliant - from interface
 }
 interface K extends J {
-  public boolean equals(Object obj); // Noncompliant
+  public boolean equals(Object obj); // Compliant - from Object
 }
 class B extends A implements I {
-  void foo() {} // Noncompliant
-  void bar() {} // Noncompliant
-  @Override
-  protected void finalize() { // Compliant
+  void foo() {} // Noncompliant [[sc=8;ec=11]] {{Add the "@Override" annotation above this method signature}}
+  void bar() {} // Compliant - from interface
+
+  protected void finalize() { // Compliant - from Object
     super.finalize();
   }
 }
@@ -28,3 +28,39 @@ class C extends A implements I {
   void bar() {}
 }
 
+interface InterfaceWithDefault {
+  default boolean qix() { return true; }
+  default boolean gul() { return false; }
+  boolean bar();
+}
+
+abstract class AbstractMethodFromInterface implements InterfaceWithDefault {
+  abstract boolean bar(); // Compliant
+}
+
+interface InterfaceExtendsDefault extends InterfaceWithDefault {
+  boolean qix(); // Noncompliant [[sc=11;ec=14]] {{Add the "@Override" annotation above this method signature}}
+  @Override
+  boolean gul(); // Compliant - hide default behavior
+}
+
+class ImplemInterfaceWithDefault implements InterfaceWithDefault {
+  public boolean gul() { return false; } // Noncompliant [[sc=18;ec=21]] {{Add the "@Override" annotation above this method signature}}
+}
+
+abstract class AbstractClass {
+  abstract boolean foo();
+  boolean bar() { return false; }
+}
+
+class ExtendsAbstractClass extends AbstractClass {
+  boolean foo() { return false; } // Compliant - overridee is abstract
+  boolean bar() { return super.bar(); } // Noncompliant
+}
+
+abstract class ImplementsFromJDK8 implements java.lang.reflect.AnnotatedElement {
+
+  public <A extends java.lang.annotation.Annotation> A[] getAnnotationsByType(Class<A> annotationClass) { // Noncompliant [[sc=58;ec=78]] {{Add the "@Override" annotation above this method signature}}
+    return null;
+  }
+}
