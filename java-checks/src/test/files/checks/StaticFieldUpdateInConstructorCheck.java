666751f59dced461f2e4ba79ad802a8769a09592
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-03-03 16:18:32 +0100

    SONARJAVA-2144 Static field update in constructor check

---
+++ java-checks/src/test/files/checks/StaticFieldUpdateInConstructorCheck.java
@@ -0,0 +1,33 @@
+class A {
+  String field;
+  static String staticField;
+  static String[] words = {"yolo", "fun"};
+  static int value = 14;
+
+  A() {
+    field = "world"; // Compliant
+    staticField = "hello"; // Noncompliant [[sc=5;ec=16;secondary=3]] {{Remove this assignment of "staticField".}}
+    A.staticField = "again"; // Noncompliant [[sc=7;ec=18;secondary=3]] {{Remove this assignment of "staticField".}}
+    words[0] = "noFun"; // Noncompliant [[sc=5;ec=10;secondary=4]] {{Remove this assignment of "words".}}
+    value = 42; // Noncompliant [[sc=5;ec=10;secondary=5]] {{Remove this assignment of "value".}}
+    value += 1; // Noncompliant [[sc=5;ec=10;secondary=5]] {{Remove this assignment of "value".}}
+
+    String var = "boom";
+    field = staticField = var = "why so mean, java"; // Noncompliant [[sc=13;ec=24;secondary=3]] {{Remove this assignment of "staticField".}}
+
+    value++; // Noncompliant [[sc=5;ec=10;secondary=5]] {{Remove this assignment of "value".}}
+
+    getA().field = "beer"; // Compliant
+    getA().staticField = "garden"; // Noncompliant
+
+    (A.staticField) = "hello"; // Noncompliant
+    (words)[1] = "polo"; // Noncompliant
+
+    boolean b = !true;
+  }
+
+  static A getA() {
+    return null;
+  }
+
+}
