22f9dc0f5fd968355e5fbd01a91d31448108dcf9
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-04-03 17:57:59 +0200

    SONARJAVA-2221 Do not raise issue if unknown identifier has same name

--- java-checks/src/test/files/checks/unused/UnusedPrivateFieldCheck.java
+++ java-checks/src/test/files/checks/unused/UnusedPrivateFieldCheck.java
@@ -86,3 +86,20 @@ class TestSonar {
     }
   }
 }
+
+class usageOfUnkownField {
+
+  private Object field1; // Noncompliant
+  private Object field2; // Noncompliant
+  private Object field3; // Compliant
+  private String field4; // Compliant
+  private Object[] field5; // Compliant
+
+  void foo(java.util.List<Integer> list) {
+    field5[0] = new Object();
+    field1(); // unknown method - ignored
+    list.stream().filter(stuff::field2); // unknown method reference - ignored
+    list.stream().filter(field3::equals);
+    Object value = stuff.field4; // unknown field4
+  }
+}
