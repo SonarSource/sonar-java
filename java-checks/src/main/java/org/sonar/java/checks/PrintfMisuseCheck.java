cf9761efa02417cf057afb57b3fadefe8461ad55
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-04-28 09:21:24 +0200

    SONARJAVA-2208 Split printf check in codesmell and bug

---
+++ java-checks/src/main/java/org/sonar/java/checks/PrintfMisuseCheck.java
@@ -0,0 +1,206 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import org.sonar.check.Rule;
+import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.java.matcher.TypeCriteria;
+import org.sonar.java.model.LiteralUtils;
+import org.sonar.plugins.java.api.semantic.Type;
+import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.LiteralTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.NewArrayTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Rule(key = "S3457")
+public class PrintfMisuseCheck extends AbstractPrintfChecker {
+
+  private static final MethodMatcher TO_STRING = MethodMatcher.create().typeDefinition(TypeCriteria.anyType()).name("toString").withoutParameter();
+
+
+  @Override
+  protected void onMethodInvocationFound(MethodInvocationTree mit) {
+    ExpressionTree formatStringTree;
+    List<ExpressionTree> args;
+    boolean isMessageFormat = MESSAGE_FORMAT.matches(mit);
+    if (isMessageFormat && !mit.symbol().isStatic()) {
+      // only consider the static method
+      return;
+    }
+    // Check type of first argument:
+    if (mit.arguments().get(0).symbolType().is("java.lang.String")) {
+      formatStringTree = mit.arguments().get(0);
+      args = mit.arguments().subList(1, mit.arguments().size());
+    } else {
+      // format method with "Locale" first argument, skip that one.
+      formatStringTree = mit.arguments().get(1);
+      args = mit.arguments().subList(2, mit.arguments().size());
+    }
+    if (formatStringTree.is(Tree.Kind.STRING_LITERAL)) {
+      String formatString = LiteralUtils.trimQuotes(((LiteralTree) formatStringTree).value());
+      if (isMessageFormat) {
+        handleMessageFormat(mit, formatString, args);
+      } else {
+        handlePrintfFormat(mit, formatString, args);
+      }
+    } else if (isConcatenationOnSameLine(formatStringTree)) {
+      reportIssue(mit, "Format specifiers should be used instead of string concatenation.");
+    }
+  }
+
+  @Override
+  protected void handlePrintfFormat(MethodInvocationTree mit, String formatString, List<ExpressionTree> args) {
+    List<String> params = getParameters(formatString, mit);
+    if (usesMessageFormat(formatString, params)) {
+      reportIssue(mit, "Looks like there is a confusion with the use of java.text.MessageFormat, parameters will be simply ignored here");
+      return;
+    }
+    checkLineFeed(formatString, mit);
+    if (checkEmptyParams(mit, params)) {
+      return;
+    }
+    cleanupLineSeparator(params);
+    if (!params.isEmpty() && argIndexes(params).size() <= args.size()) {
+      verifyParameters(mit, args, params);
+    }
+  }
+
+  private void verifyParameters(MethodInvocationTree mit, List<ExpressionTree> args, List<String> params) {
+    int index = 0;
+    List<ExpressionTree> unusedArgs = new ArrayList<>(args);
+    for (String rawParam : params) {
+      String param = rawParam;
+      int argIndex = index;
+      if (param.contains("$")) {
+        argIndex = getIndex(param) - 1;
+        if (argIndex == -1) {
+          return;
+        }
+        param = param.substring(param.indexOf('$') + 1);
+      } else if (param.charAt(0) == '<') {
+        //refers to previous argument
+        argIndex = Math.max(0, argIndex - 1);
+      }else {
+        index++;
+      }
+      ExpressionTree argExpressionTree = args.get(argIndex);
+      unusedArgs.remove(argExpressionTree);
+      Type argType = argExpressionTree.symbolType();
+      checkBoolean(mit, param, argType);
+    }
+    reportUnusedArgs(mit, args, unusedArgs);
+  }
+
+  protected void handleMessageFormat(MethodInvocationTree mit, String formatString, List<ExpressionTree> args) {
+    String newFormatString = cleanupDoubleQuote(formatString);
+    Set<Integer> indexes = getMessageFormatIndexes(newFormatString);
+    List<ExpressionTree> newArgs = args;
+    if (newArgs.size() == 1) {
+      ExpressionTree firstArg = newArgs.get(0);
+      if (firstArg.symbolType().isArray()) {
+        if (isNewArrayWithInitializers(firstArg)) {
+          newArgs = ((NewArrayTree) firstArg).initializers();
+        } else {
+          // size is unknown
+          return;
+        }
+      }
+    }
+    if (checkEmptyParams(mit, indexes)) {
+      return;
+    }
+    checkToStringInvocation(newArgs);
+    verifyParameters(mit, newArgs, indexes);
+  }
+
+  private boolean checkEmptyParams(MethodInvocationTree mit, Collection<?> params) {
+    if (params.isEmpty()) {
+      reportIssue(mit, "String contains no format specifiers.");
+      return true;
+    }
+    return false;
+  }
+
+  private void checkToStringInvocation(List<ExpressionTree> args) {
+    args.stream()
+      .filter(arg -> arg.is(Tree.Kind.METHOD_INVOCATION) && TO_STRING.matches((MethodInvocationTree) arg))
+      .forEach(arg -> reportIssue(arg, "No need to call toString \"method()\" as formatting and string conversion is done by the Formatter."));
+  }
+
+
+  private void verifyParameters(MethodInvocationTree mit, List<ExpressionTree> args, Set<Integer> indexes) {
+    List<ExpressionTree> unusedArgs = new ArrayList<>(args);
+    for (int index : indexes) {
+      if (index >= args.size()) {
+        return;
+      }
+      unusedArgs.remove(args.get(index));
+    }
+    reportUnusedArgs(mit, args, unusedArgs);
+  }
+
+  private void reportUnusedArgs(MethodInvocationTree mit, List<ExpressionTree> args, List<ExpressionTree> unusedArgs) {
+    for (ExpressionTree unusedArg : unusedArgs) {
+      int i = args.indexOf(unusedArg);
+      String stringArgIndex = "first";
+      if (i == 1) {
+        stringArgIndex = "2nd";
+      } else if (i == 2) {
+        stringArgIndex = "3rd";
+      } else if (i >= 3) {
+        stringArgIndex = (i + 1) + "th";
+      }
+      reportIssue(mit, stringArgIndex + " argument is not used.");
+    }
+  }
+
+
+  private void checkBoolean(MethodInvocationTree mit, String param, Type argType) {
+    if (param.charAt(0) == 'b' && !(argType.is("boolean") || argType.is("java.lang.Boolean"))) {
+      reportIssue(mit, "Directly inject the boolean value.");
+    }
+  }
+
+  private void checkLineFeed(String formatString, MethodInvocationTree mit) {
+    if (formatString.contains("\\n")) {
+      reportIssue(mit, "%n should be used in place of \\n to produce the platform-specific line separator.");
+    }
+  }
+
+  private static boolean usesMessageFormat(String formatString, List<String> params) {
+    return params.isEmpty() && (formatString.contains("{0") || formatString.contains("{1"));
+  }
+
+
+  private static boolean isConcatenationOnSameLine(ExpressionTree formatStringTree) {
+    return formatStringTree.is(Tree.Kind.PLUS) && operandsAreOnSameLine((BinaryExpressionTree) formatStringTree);
+  }
+
+  private static boolean operandsAreOnSameLine(BinaryExpressionTree formatStringTree) {
+    return formatStringTree.leftOperand().firstToken().line() == formatStringTree.rightOperand().firstToken().line();
+  }
+}
