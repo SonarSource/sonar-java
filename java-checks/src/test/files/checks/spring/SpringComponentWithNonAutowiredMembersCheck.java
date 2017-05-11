337512aa156f9cf5f4db5eac507f8b4706e90c57
Simon Schrottner <s.schrottner@netconomy.net>
2017-05-11 09:05:31 +0200

    SONARJAVA-2219: Spring Annotations
    * added missing import for test files

--- java-checks/src/test/files/checks/spring/SpringComponentWithNonAutowiredMembersCheck.java
+++ java-checks/src/test/files/checks/spring/SpringComponentWithNonAutowiredMembersCheck.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import javax.inject.Inject;
 import javax.annotation.Resource;
 
