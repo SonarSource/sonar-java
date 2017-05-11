613993be90a6a7977c1b7ad66db39c94328f8ae7
benzonico <nicolas.peru@gmail.com>
2017-05-11 16:11:16 +0200

    SONARJAVA-2233 expand cases covered by RSPEC-2681 (#1440)

--- java-checks/src/test/files/checks/MultilineBlocksCurlyBracesCheck.java
+++ java-checks/src/test/files/checks/MultilineBlocksCurlyBracesCheck.java
@@ -3,7 +3,7 @@ public class A {
   void nonCompliant() {
     if (condition)
       firstActionInBlock();
-      secondAction();  // Noncompliant {{Only the first line of this 2-line block will be executed conditionally. The rest will execute unconditionally.}}
+      secondAction();  // Noncompliant {{This line will not be executed conditionally; only the first line of this 2-line block will be. The rest will execute unconditionally.}}
     thirdAction();
 
     if (condition) {
@@ -11,29 +11,29 @@ public class A {
     } else
       firstActionInBlock();
 
-      secondAction();  // Noncompliant {{Only the first line of this 3-line block will be executed conditionally. The rest will execute unconditionally.}}
+      secondAction();  // Noncompliant {{This line will not be executed conditionally; only the first line of this 3-line block will be. The rest will execute unconditionally.}}
 
     if (condition)
       action();
     else
       firstActionInBlock();
-      secondAction();  // Noncompliant {{Only the first line of this 2-line block will be executed conditionally. The rest will execute unconditionally.}}
+      secondAction();  // Noncompliant {{This line will not be executed conditionally; only the first line of this 2-line block will be. The rest will execute unconditionally.}}
 
     String str = null;
     for (int i = 0; i < array.length; i++)
       str = array[i];
-      doTheThing(str);  // Noncompliant {{Only the first line of this 2-line block will be executed in a loop. The rest will execute only once.}}
+      doTheThing(str);  // Noncompliant {{This line will not be executed in a loop; only the first line of this 2-line block will be. The rest will execute only once.}}
 
     while (true)
       firstActionInBlock();
-      secondAction();  // Noncompliant {{Only the first line of this 2-line block will be executed in a loop. The rest will execute only once.}}
+      secondAction();  // Noncompliant {{This line will not be executed in a loop; only the first line of this 2-line block will be. The rest will execute only once.}}
 
     int[] test = new int[]{1, 2};
     for (int intValue : test)
       firstActionInBlock();
       // comment
       // bla bla bla
-      secondAction();  // Noncompliant {{Only the first line of this 4-line block will be executed in a loop. The rest will execute only once.}}
+      secondAction();  // Noncompliant {{This line will not be executed in a loop; only the first line of this 4-line block will be. The rest will execute only once.}}
   }
 
   void compliant() {
@@ -55,5 +55,32 @@ public class A {
       str = array[i];
       doTheThing(str);
     }
+  }
+
+  void expansion() {
+    if (condition); secondAction(); // Noncompliant
+    if (condition) firstActionInBlock(); secondAction();  // Noncompliant; secondAction executed unconditionally
+
+    if (condition) firstActionInBlock();
+       secondAction();  // Noncompliant {{This line will not be executed conditionally; only the first line of this 2-line block will be. The rest will execute unconditionally.}}
+
+    if (condition) firstActionInBlock();
+    secondAction();
+
+    if (condition)
+      firstActionInBlock();
+    secondAction();
+
+    for (int i = 0;i<10;i++); secondAction(); // Noncompliant
+    for (int i = 0;i<10;i++) firstActionInBlock(); secondAction();  // Noncompliant; secondAction executed unconditionally
+
+    for (int i = 0;i<10;i++) firstActionInBlock();
+      secondAction();  // Noncompliant
+
+    for (int i = 0;i<10;i++) firstActionInBlock();
+    secondAction(); // compliant : indentation is not confusing
+    for (int i = 0;i<10;i++)
+      firstActionInBlock();
+    secondAction();
   }
 }
