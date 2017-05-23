89e748ac99fd28a227234f122103085263671276
benzonico <nicolas.peru@gmail.com>
2017-05-23 10:37:46 +0200

    SONARJAVA-2310 Fix S2786 highlighting (#1478)

--- java-checks/src/test/files/checks/NestedEnumStatic.java
+++ java-checks/src/test/files/checks/NestedEnumStatic.java
@@ -2,7 +2,7 @@ enum MyEnum { // Compliant
 }
 
 // should be rejected during compilation
-static enum MyStaticEnum {// Noncompliant [[sc=8;ec=12]] {{Remove this redundant "static" qualifier.}}
+static enum MyStaticEnum {// Noncompliant [[sc=1;ec=7;secondary=5]] {{Remove this redundant "static" qualifier.}}
 }
 
 class MyClass {
