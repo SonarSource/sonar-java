a05e8915b74f91f7c1dd54403f46e7c21e98ac78
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-05-23 09:56:45 +0200

    Update travis utils to v36

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
