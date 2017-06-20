df1f85e1f7610a24f7d04912aecc598d3170c2e2
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-06-20 17:24:35 +0200

    Set up parallel job

---
+++ cix_parallelize.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+echo "Running $TEST"
+
+echo "cloning Tomcat 8 project"
+cd its/sources
+if [ ! -d tomcat80 ]; then
+  git clone git@github.com:apache/tomcat80.git
+  cd tomcat80
+  #fix version of sources tested
+  git checkout db9c56ab66fa96e136bba36e40d466b92378b5c5
+  else
+  cd tomcat80
+fi
+
+#build tomcat 8
+ant clean deploy
+
+cd ../../parallelizing
+case "$TEST" in
+  baseline)
+    mvn package -Pit-parallelizing -Dsonar.runtimeVersion=LATEST_RELEASE -Dversion.toCopy=4.11.0.10495 -Dmaven.test.redirectTestOutputToFile=false -B -e -V
+  ;;
+  parallelized)
+    mvn package -Pit-parallelizing -Dsonar.runtimeVersion=LATEST_RELEASE -Dmaven.test.redirectTestOutputToFile=false -B -e -V
+  ;;
+  *)
+    echo "Unexpected TEST mode: $TEST"
+    exit 1
+  ;;
+esac
---
+++ its/parallelizing/src/test/java/org/sonar/java/it/JavaParallelAnalysisTest.java
@@ -0,0 +1,86 @@
+package org.sonar.java.it;/*
+ * SonarQube Java
+ * Copyright (C) 2013-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import com.google.common.base.Preconditions;
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.BuildResult;
+import com.sonar.orchestrator.build.SonarScanner;
+import com.sonar.orchestrator.locator.FileLocation;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class JavaParallelAnalysisTest {
+
+  private static final String SENSOR_NAME = "JavaSquidSensor";
+
+  @ClassRule
+  public static final Orchestrator ORCHESTRATOR = Orchestrator.builderEnv()
+    .addPlugin(FileLocation.byWildcardMavenFilename(new File("../../sonar-java-plugin/target"), "sonar-java-plugin-*.jar"))
+    .restoreProfileAtStartup(FileLocation.of("src/test/profile.xml"))
+    .build();
+
+  @Test
+  public void perform() throws IOException {
+    ORCHESTRATOR.getServer().provisionProject("org.apache:tomcat8", "Apache Tomcat-8");
+    ORCHESTRATOR.getServer().associateProjectToQualityProfile("org.apache:tomcat8", "java", "rules");
+    SonarScanner build = SonarScanner.create(FileLocation.of("../sources/tomcat80").getFile())
+      .setEnvironmentVariable("SONAR_RUNNER_OPTS", "-Xmx1024m -server")
+      .setProperty("sonar.importSources", "false")
+      .setProperty("sonar.showProfiling", "true")
+      .setProperty("sonar.sources", ".")
+      .setProperty("sonar.preloadFileMetadata", "true")
+      .setProjectKey("org.apache:tomcat8")
+      .setProjectName("Apache Tomcat-8")
+      .setProjectVersion("trunk")
+      .setSourceEncoding("UTF-8")
+      .setSourceDirs(".");
+
+    BuildResult result = ORCHESTRATOR.executeBuild(build);
+
+    double time = sensorTime(build.getProjectDir(), result.getLogs(), SENSOR_NAME);
+
+    double expected = 170;
+  }
+
+  private static double sensorTime(File projectDir, String logs, String sensor) throws IOException {
+    File profilingFile = new File(projectDir, ".sonar/profiling/org.apache_tomcat8-profiler.properties");
+    Preconditions.checkArgument(profilingFile.isFile(), "Cannot find profiling file to extract time for sensor " + sensor + ": " + profilingFile.getAbsolutePath());
+    return getTimeValue(profilingFile, sensor);
+  }
+
+  private static double getTimeValue(File profilingFile, String sensor) throws IOException {
+    Properties properties = new Properties();
+    properties.load(new FileInputStream(profilingFile));
+    String time = properties.getProperty(sensor);
+    Preconditions.checkNotNull(time, "Could not find a value for property : " + sensor);
+    return toMilliseconds(time);
+  }
+
+  private static double toMilliseconds(String time) {
+    return TimeUnit.MILLISECONDS.toSeconds(Integer.parseInt(time));
+  }
+
+}
