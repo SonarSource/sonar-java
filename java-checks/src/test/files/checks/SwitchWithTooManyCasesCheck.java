26fe6e4da20d6fb1e5381298bc5c68e2c0688d9f
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-06-20 15:14:35 +0200

    SONARJAVA-2324 Update S1479 implementation to ignore enums and empty switch cases (#1523)

--- java-checks/src/test/files/checks/SwitchWithTooManyCasesCheck.java
+++ java-checks/src/test/files/checks/SwitchWithTooManyCasesCheck.java
@@ -1,6 +1,83 @@
 class A{
   void foo() {
-    switch (1) { // Noncompliant [[sc=5;ec=11]] {{Reduce the number of switch cases from 35 to at most 30.}}
+    switch (1) { // Noncompliant [[sc=5;ec=11]] {{Reduce the number of non-empty switch cases from 35 to at most 30.}}
+      case 1:
+        System.out.println("");
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      System.out.println("");
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+        break;
+      case 1:
+      case 1:
+      case 1:
+      case 1:
+        break;
+      System.out.println("");
+      default:
+        System.out.println("");
+    }
+    switch (1) { // Compliant
       case 1:
         System.out.println("");
       case 1:
