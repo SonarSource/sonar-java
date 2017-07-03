feda4518cd1abb95d652ef41d096be44269cd2d2
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-07-03 17:19:18 +0200

    Update method-resolution IT to jump from version 5.1.2 to 6.5-M1 for sonarqube server

--- its/plugin/tests/src/test/java/com/sonar/it/java/suite/SemanticTest.java
+++ its/plugin/tests/src/test/java/com/sonar/it/java/suite/SemanticTest.java
@@ -123,8 +123,8 @@ public class SemanticTest {
 
   @Test
   public void sonarqube_server() throws Exception {
-    // sonarqube/server/sonar-server (v.5.1.2)
-    test_project("org.codehaus.sonar:sonar-server", "sonarqube/server", "sonar-server");
+    // sonarqube-6.5-M1/server/sonar-server (v.6.5-M1)
+    test_project("org.sonarsource.sonarqube:sonar-server", "sonarqube-6.5-M1/server", "sonar-server");
   }
 
   private static void test_project(String projectKey, String projectName) throws IOException {
