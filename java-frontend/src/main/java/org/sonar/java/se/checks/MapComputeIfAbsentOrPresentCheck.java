5fda2697eb06431b495c643dd83cba27caf5b3fd
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-07-14 10:24:40 +0200

    SONARJAVA-2368 RSPEC-3824 SE-based implementation of S3824

---
+++ java-frontend/src/main/java/org/sonar/java/se/checks/MapComputeIfAbsentOrPresentCheck.java
@@ -0,0 +1,180 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.se.checks;
+
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+
+import org.sonar.check.Rule;
+import org.sonar.java.JavaVersionAwareVisitor;
+import org.sonar.java.cfg.CFG;
+import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.java.matcher.TypeCriteria;
+import org.sonar.java.se.CheckerContext;
+import org.sonar.java.se.ExplodedGraph;
+import org.sonar.java.se.ExplodedGraph.Node;
+import org.sonar.java.se.FlowComputation;
+import org.sonar.java.se.ProgramState;
+import org.sonar.java.se.constraint.ObjectConstraint;
+import org.sonar.java.se.symbolicvalues.SymbolicValue;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.JavaVersion;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Rule(key = "S3824")
+public class MapComputeIfAbsentOrPresentCheck extends SECheck implements JavaVersionAwareVisitor {
+
+  private static final MethodMatcher MAP_GET = mapMethod("get");
+  private static final MethodMatcher MAP_PUT = mapMethod("put");
+
+  private final Multimap<SymbolicValue, MapGetInvocation> mapGetInvocations = LinkedListMultimap.create();
+  private final List<CheckIssue> checkIssues = new ArrayList<>();
+
+  @Override
+  public boolean isCompatibleWithJavaVersion(JavaVersion version) {
+    return version.isJava8Compatible();
+  }
+
+  @Override
+  public void init(MethodTree methodTree, CFG cfg) {
+    mapGetInvocations.clear();
+    checkIssues.clear();
+  }
+
+  private static MethodMatcher mapMethod(String methodName) {
+    return MethodMatcher.create().typeDefinition(TypeCriteria.subtypeOf("java.util.Map")).name(methodName).withAnyParameters();
+  }
+
+  @Override
+  public ProgramState checkPostStatement(CheckerContext context, Tree syntaxNode) {
+    if (syntaxNode.is(Tree.Kind.METHOD_INVOCATION)) {
+      MethodInvocationTree mit = (MethodInvocationTree) syntaxNode;
+      if (MAP_GET.matches(mit)) {
+        ProgramState psBeforeInvocation = context.getNode().programState;
+        ProgramState psAfterInvocation = context.getState();
+
+        SymbolicValue keySV = psBeforeInvocation.peekValue(0);
+        SymbolicValue mapSV = psBeforeInvocation.peekValue(1);
+        SymbolicValue valueSV = psAfterInvocation.peekValue();
+
+        mapGetInvocations.put(mapSV, new MapGetInvocation(valueSV, keySV, mit));
+      }
+    }
+    return super.checkPostStatement(context, syntaxNode);
+  }
+
+  @Override
+  public ProgramState checkPreStatement(CheckerContext context, Tree syntaxNode) {
+    if (syntaxNode.is(Tree.Kind.METHOD_INVOCATION)) {
+      MethodInvocationTree mit = (MethodInvocationTree) syntaxNode;
+      if (MAP_PUT.matches(mit)) {
+        ProgramState ps = context.getState();
+
+        SymbolicValue keySV = ps.peekValue(1);
+        SymbolicValue mapSV = ps.peekValue(2);
+        mapGetInvocations.get(mapSV).stream()
+          .filter(getOnSameMap -> getOnSameMap.withSameKey(keySV))
+          .findAny()
+          .ifPresent(getOnSameMap -> {
+            ObjectConstraint constraint = ps.getConstraint(getOnSameMap.value, ObjectConstraint.class);
+            if (constraint != null) {
+              checkIssues.add(new CheckIssue(context.getNode(), getOnSameMap.mit, mit, getOnSameMap.value, constraint));
+            }
+          });
+      }
+    }
+    return super.checkPreStatement(context, syntaxNode);
+  }
+
+  @Override
+  public void checkEndOfExecution(CheckerContext context) {
+    SECheck check = this;
+    checkIssues.stream().filter(CheckIssue::isOnlyPossibleConstraintOnValueForAllPaths).forEach(issue -> issue.report(context, check));
+  }
+
+  private static class CheckIssue {
+    private final ExplodedGraph.Node node;
+
+    private final MethodInvocationTree getInvocation;
+    private final MethodInvocationTree putInvocation;
+
+    private final SymbolicValue value;
+    private final ObjectConstraint valueConstraint;
+
+    private CheckIssue(Node node, MethodInvocationTree getInvocation, MethodInvocationTree putInvocation, SymbolicValue value, ObjectConstraint valueConstraint) {
+      this.node = node;
+
+      this.getInvocation = getInvocation;
+      this.putInvocation = putInvocation;
+
+      this.value = value;
+      this.valueConstraint = valueConstraint;
+    }
+
+    private boolean isOnlyPossibleConstraintOnValueForAllPaths() {
+      return node.siblings().stream().allMatch(sibling -> valueConstraint.equals(sibling.programState.getConstraint(value, ObjectConstraint.class)));
+    }
+
+    private void report(CheckerContext context, SECheck check) {
+      context.reportIssue(getInvocation, check, issueMsg(), flows());
+    }
+
+    private String issueMsg() {
+      return String.format("Replace this \"Map.get()\" and condition with a call to \"Map.%s()\".",
+        valueConstraint == ObjectConstraint.NULL ? "computeIfAbsent" : "computeIfPresent");
+    }
+
+    private Set<List<JavaFileScannerContext.Location>> flows() {
+      // build nullness flows for value constraint
+      Set<List<JavaFileScannerContext.Location>> flows = FlowComputation.flow(node, value, Collections.singletonList(ObjectConstraint.class));
+      // enrich each flow with both map method invocations
+      return flows.stream().map(flow -> {
+        List<JavaFileScannerContext.Location> newFlow = new ArrayList<>(flow);
+        newFlow.add(new JavaFileScannerContext.Location("'Map.get()' is invoked.", getInvocation.methodSelect()));
+        newFlow.add(0, new JavaFileScannerContext.Location("'Map.put()' is invoked with same key.", putInvocation.methodSelect()));
+        return Collections.unmodifiableList(newFlow);
+      }).collect(Collectors.toSet());
+    }
+  }
+
+  private static class MapGetInvocation {
+    private final SymbolicValue value;
+    private final SymbolicValue key;
+    private final MethodInvocationTree mit;
+
+    private MapGetInvocation(SymbolicValue value, SymbolicValue key, MethodInvocationTree mit) {
+      this.value = value;
+      this.key = key;
+      this.mit = mit;
+    }
+
+    private boolean withSameKey(SymbolicValue key) {
+      return this.key.equals(key);
+    }
+  }
+}
---
+++ java-frontend/src/test/files/se/MapComputeIfAbsentOrPresentCheck.java
@@ -0,0 +1,92 @@
+import com.google.common.base.Preconditions;
+
+import java.util.Map;
+import java.util.Objects;
+
+abstract class A {
+
+  void foo(Map<String,Object> map, String key) {
+    // Noncompliant@+1 [[flows=computeIfAbsent]] {{Replace this "Map.get()" and condition with a call to "Map.computeIfAbsent()".}}
+    Object value = map.get(key);  // flow@computeIfAbsent [[order=1]] {{'Map.get()' is invoked.}}
+    if (value == null) { // flow@computeIfAbsent [[order=2]] {{Implies 'value' can be null.}}
+      map.put(key, new Object()); // flow@computeIfAbsent [[order=3]] {{'Map.put()' is invoked with same key.}}
+    }
+  }
+
+  void bar(Map<String,Object> map, String key) {
+    Object value = map.get(key); // Noncompliant {{Replace this "Map.get()" and condition with a call to "Map.computeIfPresent()".}}
+    if (value != null) {
+      map.put(key, new Object());
+    }
+  }
+
+  void del(Map<String,Object> map, String key) {
+    Object value = map.get(key); // Noncompliant - FP?
+    Preconditions.checkState(value == null, "Value should always be null!");
+    map.put(key, new Object());
+  }
+
+  void thr(Map<String,Object> map, String key) {
+    Object value = map.get(key); // Noncompliant - FP?
+    if (value == null) {
+      throw new IllegalStateException("Value should always be null!");
+    }
+    map.put(key, new Object());
+  }
+
+  void qix(Map<String, Object> map, String key) {
+    Object value = map.get(key); // Noncompliant
+    if (Objects.isNull(value)) {
+      map.put(key, new Object());
+      doSomething(value); // required to keep 'value' alive and be able to retrieve constraint...
+    }
+  }
+
+  void tmp(Map<String, Object> map1, Map<String, Object> map2, String key) {
+    Object value = map1.get(key); // Compliant - different maps
+    if (value == null) {
+      map2.put(key, new Object());
+    }
+  }
+
+  void gul(Map<String, Object> map, String key1, String key2) {
+    Object value = map.get(key1); // Compliant - different keys
+    if (value == null) {
+      map.put(key2, new Object());
+    }
+  }
+
+  void gro(Map<String, Object> map, String key) {
+    Object value = map.get(key); // Noncompliant - reassigning everything
+    Object value2 = value;
+    if (value2 == null) {
+      String key2 = key;
+      Map<String, Object> map2 = map;
+      map2.put(key2, new Object());
+    }
+  }
+
+  void til(Map<String, Object> map, String key) {
+    Object value = map.get(key); // Compliant - no constraint on value
+    map.put(key, new Object());
+  }
+
+  void lol(Map<String, Object> map, String key1, String key2) {
+    Object value = map.get(key1); // Noncompliant
+    if (value == null) {
+      map.get(key2);
+      map.put(key1, new Object());
+    }
+  }
+
+  void sal(Map<String, Object> map, String key) {
+    Object value = map.get(key); // Compliant - you can reach put with NULL and NOT_NULL constraint on 'value'
+    if (value == null) {
+      doSomething();
+    }
+    map.put(key, new Object());
+  }
+
+  abstract void doSomething(Object... objects);
+
+}
