cf87d4de61673257d467ee0ee19c7247a3039b17
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-07-19 13:50:18 +0200

    Update travis.sh to use mkdir -p : do not fail if ~/.local is already created

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 function configureTravis {
-  mkdir ~/.local
+  mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
