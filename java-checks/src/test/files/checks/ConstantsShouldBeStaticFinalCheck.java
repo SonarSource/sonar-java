eb3aba60af0b19dc6fb82f0c3c53350f365042c4
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-08-29 11:31:30 +0200

    SONARJAVA-2433 Remove FP on method references

--- java-checks/src/test/files/checks/ConstantsShouldBeStaticFinalCheck.java
+++ java-checks/src/test/files/checks/ConstantsShouldBeStaticFinalCheck.java
@@ -40,3 +40,20 @@ class A {
 interface B {
   final int f0 = 0;                                     // Compliant
 }
+
+static class C {
+  private final java.util.function.Consumer<Object> o = this::someMethod; // compliant
+  private final java.util.function.Consumer<Object> o1 = new C()::someMethod; // compliant
+  C c = new C();
+  private final java.util.function.Consumer<Object> o2 = c::someMethod; // compliant
+  private final java.util.function.Consumer<Object> o3 = C::someMethod2; // Noncompliant
+
+  void someMethod(Object o) {
+    return;
+  }
+  static void someMethod2(Object o) {
+    return;
+  }
+
+}
+
