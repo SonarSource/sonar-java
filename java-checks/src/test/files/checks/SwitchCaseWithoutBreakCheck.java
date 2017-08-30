b49052c4cf1eb4c2dde8ead12c5dd3e0708e86a8
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-08-30 10:12:23 +0200

    SONARJAVA-2418 Support fallthrough comments

--- java-checks/src/test/files/checks/SwitchCaseWithoutBreakCheck.java
+++ java-checks/src/test/files/checks/SwitchCaseWithoutBreakCheck.java
@@ -205,4 +205,31 @@ class Conditions {
         System.out.println(j);
     }
   }
+
+
+  private fallThroughComments(int i) {
+    switch (i) {
+      case 1:
+        System.out.println(i);
+        // fall-through
+      case 2:
+        // FALL-THROUGH
+        System.out.println(i);
+      case 3:
+        // fallthrough
+        System.out.println(i);
+      case 4:
+        //$FALL-THROUGH$
+        System.out.println(i);
+      case 5:
+        // fallthru
+        System.out.println(i);
+      case 6:
+        //falls-through
+        System.out.println(i);
+      case 7:
+        System.out.println("foo");
+        break;
+    }
+  }
 }
