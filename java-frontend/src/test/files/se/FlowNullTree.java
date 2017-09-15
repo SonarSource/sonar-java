6fc88e2ec34b19a98b114a0f2858ff16391b094a
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-09-15 13:05:01 +0200

    SONARJAVA-2459 fix test in flow computation (#1679)

--- java-frontend/src/test/files/se/FlowNullTree.java
+++ java-frontend/src/test/files/se/FlowNullTree.java
@@ -1,19 +1,23 @@
 abstract class A {
 
   void test(Object o, boolean b2) {
+    boolean b1 = true;
     try {
       try {
-        f(); // flow@f1,f4 {{...}}
+        f();
       } finally {
-        if (b2) { // flow@f1,f4 {{...}}
-          o = null; // flow@f1,f2,f3,f4 {{..}}
-          g(); // flow@f1,f3 {{Exception is thrown here.}}
+        if (b2) {
+          o = null;
+          g();
           o = new Object();
         }
       }
       h();
     } finally {
-      o.toString(); // Noncompliant [[flows=f1,f2,f3,f4]] flow@f1,f2,f3,f4
+      // because of incorrect stack handling, if-condition tree will be considered as throwing exception
+      // and because direct parent of b1 is empty block, it's syntax tree will be null, so no flow message should be added
+      if (b1)  // Noncompliant  (we don't care about issue details in this test, just particular shape of CFG and program state which triggers corner case)
+        o.toString(); // Noncompliant
     }
   }
 
