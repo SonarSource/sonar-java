df50b03358711e286403af46a8595324218e643e
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-09-21 09:07:43 +0200

    SONARJAVA-2464 Target usages of String.format() when using locale-dependent formatters

--- java-checks/src/main/java/org/sonar/java/checks/StringMethodsWithLocaleCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/StringMethodsWithLocaleCheck.java
@@ -20,35 +20,84 @@
 package org.sonar.java.checks;
 
 import com.google.common.collect.ImmutableList;
+
 import org.sonar.check.Rule;
 import org.sonar.java.checks.methods.AbstractMethodDetection;
 import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.LiteralTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Rule(key = "S1449")
 public class StringMethodsWithLocaleCheck extends AbstractMethodDetection {
 
-
   private static final String STRING = "java.lang.String";
+  private static final MethodMatcher STRING_FORMAT = stringMethod().name("format").withAnyParameters();
 
   @Override
   protected List<MethodMatcher> getMethodInvocationMatchers() {
     return ImmutableList.of(
-      MethodMatcher.create().typeDefinition(STRING).name("toUpperCase").withoutParameter(),
-      MethodMatcher.create().typeDefinition(STRING).name("toLowerCase").withoutParameter()
+      stringMethod().name("toUpperCase").withoutParameter(),
+      stringMethod().name("toLowerCase").withoutParameter(),
+      STRING_FORMAT
     );
+  }
+
+  private static MethodMatcher stringMethod() {
+    return MethodMatcher.create().typeDefinition(STRING);
   }
 
   @Override
   protected void onMethodInvocationFound(MethodInvocationTree mit) {
     Tree report = mit.methodSelect();
+    if (STRING_FORMAT.matches(mit) && (isLocaleVariant(mit) || !usesLocaleDependentFormatteer(mit.arguments().get(0)))) {
+      return;
+    }
     if(report.is(Tree.Kind.MEMBER_SELECT)) {
       report = ((MemberSelectExpressionTree) report).identifier();
     }
     reportIssue(report, "Define the locale to be used in this String operation.");
+  }
+
+  private static boolean isLocaleVariant(MethodInvocationTree mit) {
+    return ((Symbol.MethodSymbol) mit.symbol()).parameterTypes().get(0).is("java.util.Locale");
+  }
+
+  private static boolean usesLocaleDependentFormatteer(ExpressionTree firstArg) {
+    FormatterVisitor visitor = new FormatterVisitor();
+    firstArg.accept(visitor);
+    return visitor.hasLocaleDependantFormatter;
+  }
+
+  private static class FormatterVisitor extends BaseTreeVisitor {
+
+    // source: https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html#syntax
+    private static final String ARGUMENT_INDEX = "(\\d+\\$)?";
+    private static final String FLAGS = "([-,#+ 0(]*)?";
+    private static final String FLAGS_WITH_FORCED_COMMA = "([-#+ 0(]*,[-#+ 0(]*)";
+    private static final String WIDTH = "([0-9]*\\.?[0-9]*)?";
+    private static final String INTEGER_LOCAL_SPECIFIC_FORMAT = FLAGS_WITH_FORCED_COMMA + WIDTH + "d";
+    private static final String DATE_TIME_FLOATING_POINT_LOCAL_SPECIFIC_FORMAT = FLAGS + WIDTH + "[eEfgGaAtT]";
+
+    private static final Pattern LOCALE_DEPENDENT_FORMATTERS = Pattern
+      .compile("(.*)%" + ARGUMENT_INDEX + "(" + INTEGER_LOCAL_SPECIFIC_FORMAT + "|" + DATE_TIME_FLOATING_POINT_LOCAL_SPECIFIC_FORMAT + ")(.*)");
+
+    private boolean hasLocaleDependantFormatter = false;
+
+    @Override
+    public void visitLiteral(LiteralTree tree) {
+      String value = tree.value().replaceAll("%%", "");
+      if (tree.is(Tree.Kind.STRING_LITERAL) && LOCALE_DEPENDENT_FORMATTERS.matcher(value).matches()) {
+        hasLocaleDependantFormatter = true;
+      }
+      super.visitLiteral(tree);
+    }
   }
 }
--- java-checks/src/test/files/checks/StringMethodsWithLocaleCheck.java
+++ java-checks/src/test/files/checks/StringMethodsWithLocaleCheck.java
@@ -1,6 +1,10 @@
 import static java.lang.String.format;
+
 class A {
-  void foo() {
+
+  private static final String MY_TEMPLATE = "%t";
+
+  void foo(java.util.Date date) {
     String myString ="";
     myString.toLowerCase(); // Noncompliant [[sc=14;ec=25]]
     myString.toUpperCase(); // Noncompliant
@@ -11,5 +15,28 @@ class A {
         toLowerCase(); // Noncompliant [[sc=9;ec=20]] {{Define the locale to be used in this String operation.}}
       }
     };
+
+    myString.format("foo"); // Compliant
+    myString.format("foo", "bar", "qix"); // Compliant
+    myString.format(java.util.Locale.US, "foo", "bar", "qix"); // Compliant
+    format("foo"); // Compliant
+
+    // dates
+    myString.format("%t", date); // Noncompliant [[sc=14;ec=20]]
+    myString.format(java.util.Locale.US, "%T", date); // Compliant
+    format("%t" + 42, date); // Noncompliant [[sc=5;ec=11]]
+    format("%1$tDc", date); // Noncompliant
+
+    // integer values : only when comma is used
+    format("%d", 42); // Compliant
+    format("%0+(15d", 42); // Compliant
+    format("%%0+,(15d", 42); // compliant - double %% is simply displaying the percent char
+    format("%1$0+,(15d", 42); // Noncompliant
+
+    // any floating point format
+    format("this will display my value in dollar: %f$" + " (dollars)", 42.01234f); // Noncompliant
+    format("this will display my value in dollar: %0+,(15.25f$" + " (dollars)", 12345.01234f); // Noncompliant
+
+    format(MY_TEMPLATE, date); // Compliant - FN - non-trivial case
   }
 }
