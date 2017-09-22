c0dc99a02642189329b307d661380f0fbc87ff33
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-09-22 10:15:59 +0200

    SONARJAVA-2450 Support of octal numbers

--- java-checks/src/test/files/checks/UnderscoreOnNumberCheck.java
+++ java-checks/src/test/files/checks/UnderscoreOnNumberCheck.java
@@ -1,9 +1,10 @@
 class A {
   private static final long serialVersionUID = -8778479691651900590L;
-
   int i0 = 1000; // Compliant
-  int i1 = 10000000; // Noncompliant {{Add underscores to this numeric value for readability}}
-  int i2 = 10_000_000; // Compliant
+  int i1 = 10000; // Compliant
+  int i2 = 100000; // Noncompliant {{Add underscores to this numeric value for readability}}
+  int i3 = 10000000; // Noncompliant
+  int i4 = 10_000_000; // Compliant
 
   int b0 = 0b0000; // Compliant
   int b1 = 0b01101001010011011110010101011110; // Noncompliant
@@ -11,7 +12,11 @@ class A {
 
   long l0 = 0x0000L; // Compliant
   long l1 = 0x7fffffffffffffffL; // Noncompliant
-  long l2 = 0x7fff_ffff_ffff_ffffL; // Compliant
+  long l2 = 0X7FFFFFFFFFFFFFFFL; // Noncompliant
+  long l3 = 0x7fff_ffff_ffff_ffffL; // Compliant
+
+  long octal0 = 012354435; // Compliant
+  long octal1 = 012354435242; // Noncompliant
 
   void foo(int x) {
     if (x < -8778479691651900590L) {} // Noncompliant
