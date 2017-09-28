4e740087e2a6e34ad87666b0b29de2a2352b15d0
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-09-28 13:53:29 +0200

    SONARJAVA-2480 Detect already declared constants in S1192 (#1707)

--- java-checks/src/main/java/org/sonar/java/checks/StringLiteralDuplicatedCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/StringLiteralDuplicatedCheck.java
@@ -20,7 +20,6 @@
 package org.sonar.java.checks;
 
 import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
@@ -29,21 +28,27 @@ import org.sonar.plugins.java.api.JavaFileScanner;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.tree.AnnotationTree;
 import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.LiteralTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Modifier;
 import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Rule(key = "S1192")
 public class StringLiteralDuplicatedCheck extends BaseTreeVisitor implements JavaFileScanner {
 
   private static final int DEFAULT_THRESHOLD = 3;
 
-  private static final Integer MINIMAL_LITERAL_LENGTH = 7;
+  // String literals include quotes, so this means length 5 as defined in RSPEC
+  private static final int MINIMAL_LITERAL_LENGTH = 7;
+
   @RuleProperty(
     key = "threshold",
     description = "Number of times a literal must be duplicated to trigger an issue",
@@ -51,27 +56,34 @@ public class StringLiteralDuplicatedCheck extends BaseTreeVisitor implements Jav
   public int threshold = DEFAULT_THRESHOLD;
 
   private final Multimap<String, LiteralTree> occurrences = ArrayListMultimap.create();
+  private final Map<String, VariableTree> constants = new HashMap<>();
 
   @Override
   public void scanFile(JavaFileScannerContext context) {
     occurrences.clear();
+    constants.clear();
     scan(context.getTree());
     for (String entry : occurrences.keySet()) {
       Collection<LiteralTree> literalTrees = occurrences.get(entry);
-      int literalOccurence = literalTrees.size();
-      if (literalOccurence >= threshold) {
-        List<JavaFileScannerContext.Location> flow = new ArrayList<>();
-        for (Tree element : literalTrees) {
-          flow.add(new JavaFileScannerContext.Location("Duplication", element));
-        }
+      int literalOccurrence = literalTrees.size();
+      if (constants.containsKey(entry) && literalOccurrence > 1) {
+        VariableTree constant = constants.get(entry);
+        List<LiteralTree> duplications = literalTrees.stream().filter(literal -> literal.parent() != constant).collect(Collectors.toList());
+        context.reportIssue(this, duplications.iterator().next(),
+          "Use already defined constant '" + constant.simpleName() + "' instead of duplicating literal " + entry + ".",
+          secondaryLocations(duplications.subList(1, duplications.size())), literalOccurrence);
+      } else if (literalOccurrence >= threshold) {
         context.reportIssue(
           this,
-          Iterables.getFirst(literalTrees, null),
-          "Define a constant instead of duplicating this literal " + entry + " " + literalOccurence + " times.",
-          flow,
-          literalOccurence);
+          literalTrees.iterator().next(),
+          "Define a constant instead of duplicating this literal " + entry + " " + literalOccurrence + " times.",
+          secondaryLocations(literalTrees), literalOccurrence);
       }
     }
+  }
+
+  private static List<JavaFileScannerContext.Location> secondaryLocations(Collection<LiteralTree> literalTrees) {
+    return literalTrees.stream().map(element -> new JavaFileScannerContext.Location("Duplication", element)).collect(Collectors.toList());
   }
 
   @Override
@@ -82,6 +94,17 @@ public class StringLiteralDuplicatedCheck extends BaseTreeVisitor implements Jav
         occurrences.put(literal, tree);
       }
     }
+  }
+
+  @Override
+  public void visitVariable(VariableTree tree) {
+    ExpressionTree initializer = tree.initializer();
+    if (initializer != null && initializer.is(Tree.Kind.STRING_LITERAL)
+      && ModifiersUtils.hasModifier(tree.modifiers(), Modifier.STATIC)
+      && ModifiersUtils.hasModifier(tree.modifiers(), Modifier.FINAL)) {
+      constants.putIfAbsent(((LiteralTree) initializer).value(), tree);
+    }
+    super.visitVariable(tree);
   }
 
   @Override
--- java-checks/src/test/files/checks/StringLiteralDuplicatedCheck.java
+++ java-checks/src/test/files/checks/StringLiteralDuplicatedCheck.java
@@ -23,3 +23,31 @@ class A {
     }
   }
 }
+
+class ConstantAlreadyDefined {
+
+  private static final String A = "constant";
+  private static final String B = "constant"; // Noncompliant [[secondary=31,36]] {{Use already defined constant 'A' instead of duplicating literal "constant".}}
+  private static final String C = "constant";
+
+  private static final String REPORT_WITHOUT_THRESHOLD = "blabla";
+
+  void test() {
+    System.out.println("constant");
+    System.out.println("blabla"); // Noncompliant {{Use already defined constant 'REPORT_WITHOUT_THRESHOLD' instead of duplicating literal "blabla".}}
+  }
+
+  public IssueChangeNotification setProject(Component project) {
+    setFieldValue("projectName", project.longName());
+    setFieldValue("projectKey", project.key());
+    return this;
+  }
+
+  public IssueChangeNotification setProject(String projectKey, String projectName) {
+    setFieldValue("projectName", projectName);
+    setFieldValue("projectKey", projectKey);
+    return this;
+  }
+
+
+}
