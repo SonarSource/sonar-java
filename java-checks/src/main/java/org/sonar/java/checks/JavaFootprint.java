60fab220c850b70cb1a3a2c390ba5ca7febe8ed0
benzonico <nicolas.peru@sonarsource.com>
2017-09-29 11:30:34 +0200

    SONARJAVA-2489 Fine tune commented out lines to remove noise (#1713)

--- java-checks/src/main/java/org/sonar/java/checks/JavaFootprint.java
+++ java-checks/src/main/java/org/sonar/java/checks/JavaFootprint.java
@@ -35,11 +35,11 @@ public final class JavaFootprint implements LanguageFootprint {
 
   public JavaFootprint() {
     detectors.add(new EndWithDetector(0.95, '}', ';', '{'));
-    detectors.add(new KeywordsDetector(0.7, "||", "&&"));
+    detectors.add(new KeywordsDetector(0.7, "++", "||", "&&"));
     detectors.add(new KeywordsDetector(0.3, "public", "abstract", "class", "implements", "extends", "return", "throw",
         "private", "protected", "enum", "continue", "assert", "package", "synchronized", "boolean", "this", "double", "instanceof",
         "final", "interface", "static", "void", "long", "int", "float", "super", "true", "case:"));
-    detectors.add(new ContainsDetector(0.95, "++", "for(", "if(", "while(", "catch(", "switch(", "try{", "else{"));
+    detectors.add(new ContainsDetector(0.95, "for(", "if(", "while(", "catch(", "switch(", "try{", "else{"));
     detectors.add(new CamelCaseDetector(0.5));
   }
 
