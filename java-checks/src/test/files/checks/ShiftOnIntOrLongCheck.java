2aac920e80c1bd8689eb670cfa303c7e7a143019
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-09-29 15:43:18 +0200

    SONARJAVA-2457 better coverage

--- java-checks/src/test/files/checks/ShiftOnIntOrLongCheck.java
+++ java-checks/src/test/files/checks/ShiftOnIntOrLongCheck.java
@@ -104,4 +104,16 @@ class Shifts {
     b1 = (byte) (value >> 8);
     b2 = (byte) (value >> 0); // Compliant, because of formatting
   }
+
+  void aligned() {
+    b1 = (byte) (value >> 8);
+    b2 = (byte) (value >> 0); // Compliant
+    b2 = (byte) (value << 0); // Noncompliant - another type of shift
+    System.out.println();
+    b2 = (byte) (value >> 0); // Noncompliant - aligned but interrupted
+    System.out.println();
+    System.out.println();
+    b2 = (byte) (value >> 0); // Noncompliant  - aligned but interrupted
+    b2 = (byte) (value    >> 0); // Noncompliant  - aligned but interrupted
+  }
 }
