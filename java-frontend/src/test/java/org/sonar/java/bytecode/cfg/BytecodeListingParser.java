e601d405a9399f36594522b48ece32545ed7f465
benzonico <nicolas.peru@sonarsource.com>
2017-10-17 15:45:41 +0200

    Parse bytecode listing for generating cfg (#1740)

---
+++ java-frontend/src/test/java/org/sonar/java/bytecode/cfg/BytecodeListingParser.java
@@ -0,0 +1,95 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.bytecode.cfg;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.util.Printer;
+import org.sonar.java.resolve.JavaSymbol;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
+import static org.objectweb.asm.Opcodes.IINC;
+import static org.objectweb.asm.Opcodes.V1_8;
+import static org.sonar.java.bytecode.cfg.Instructions.FIELD_INSN;
+import static org.sonar.java.bytecode.cfg.Instructions.INT_INSN;
+import static org.sonar.java.bytecode.cfg.Instructions.JUMP_INSN;
+import static org.sonar.java.bytecode.cfg.Instructions.METHOD_INSN;
+import static org.sonar.java.bytecode.cfg.Instructions.NO_OPERAND_INSN;
+import static org.sonar.java.bytecode.cfg.Instructions.TYPE_INSN;
+import static org.sonar.java.bytecode.cfg.Instructions.VAR_INSN;
+
+public class BytecodeListingParser {
+
+
+  public static BytecodeCFGBuilder.BytecodeCFG getCFG(String bytecodeInstructions) {
+    // Define class and method stub for instructions
+    ClassWriter cw = new ClassWriter(Opcodes.ASM5);
+    cw.visit(V1_8, ACC_PUBLIC, "A", null, "java/lang/Object", null);
+    MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "test", "()V", null, null);
+    JavaSymbol.MethodJavaSymbol methodStub = new JavaSymbol.MethodJavaSymbol(0, "test", null);
+    Map<Integer, Label> labelIndexes =  new HashMap<>();
+    String[] lines = bytecodeInstructions.split("\n");
+    for (String line : lines) {
+      visitLine(line.trim().split(" "), mv, labelIndexes);
+    }
+    mv.visitEnd();
+    cw.visitEnd();
+    byte[] bytes = cw.toByteArray();
+    return BytecodeCFGBuilder.buildCFG(methodStub.completeSignature(), bytes);
+  }
+
+  private static void visitLine(String[] words, MethodVisitor mv, Map<Integer, Label> labelIndexes) {
+    // labels :
+    String initWord = words[0];
+    if(initWord.matches("L\\d+")) {
+      mv.visitLabel(labelIndexes.computeIfAbsent(Integer.parseInt(initWord.substring(1)), k -> new Label()));
+    }
+
+    int opcode = Arrays.asList(Printer.OPCODES).indexOf(initWord);
+    if(opcode == -1) {
+      return;
+    }
+    if(NO_OPERAND_INSN.contains(opcode)) {
+      mv.visitInsn(opcode);
+    } else if (INT_INSN.contains(opcode)) {
+      mv.visitIntInsn(opcode, Integer.parseInt(words[1]));
+    } else if (VAR_INSN.contains(opcode)) {
+      mv.visitVarInsn(opcode, Integer.parseInt(words[1]));
+    } else if (TYPE_INSN.contains(opcode)) {
+      mv.visitTypeInsn(opcode, words[1]);
+    } else if (FIELD_INSN.contains(opcode)) {
+      mv.visitFieldInsn(opcode, words[1], words[2], words[3]);
+    } else if (METHOD_INSN.contains(opcode)) {
+      // FIXME: interface flag is hardcoded.
+      mv.visitMethodInsn(opcode,  words[1], words[2], words[3], false);
+    } else if (JUMP_INSN.contains(opcode)) {
+      mv.visitJumpInsn(opcode, labelIndexes.computeIfAbsent(Integer.parseInt(words[1].substring(1)), k -> new Label()));
+    } else if(opcode == IINC) {
+      mv.visitIincInsn(Integer.parseInt(words[1]), Integer.parseInt(words[2]));
+    }
+
+  }
+}
---
+++ java-frontend/src/test/java/org/sonar/java/bytecode/cfg/BytecodeListingParserTest.java
@@ -0,0 +1,97 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.bytecode.cfg;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BytecodeListingParserTest {
+
+  private static void assertCFG(String listing, String expected) {
+    BytecodeCFGBuilder.BytecodeCFG cfg = BytecodeListingParser.getCFG(listing);
+    StringBuilder sb = new StringBuilder();
+    cfg.blocks.forEach(b -> sb.append(b.printBlock()));
+    assertThat(sb.toString()).isEqualTo(expected);
+  }
+
+  @Test
+  public void test_no_operand() throws Exception {
+    assertCFG("L0\n" +
+      "ICONST_0\n",
+      "B0(Exit)\n" +
+        "B1\n" +
+        "0: ICONST_0\n" +
+        "Jumps to: B0 \n");
+  }
+
+  @Test
+  public void copy_paste_from_asm_output() throws Exception {
+    String listing = "L0\n" +
+      "    LINENUMBER 465 L0\n" +
+      "    ICONST_0\n" +
+      "    ISTORE 1\n" +
+      "   L1\n" +
+      "    LINENUMBER 466 L1\n" +
+      "   FRAME APPEND [I]\n" +
+      "    ILOAD 0\n" +
+      "    GETSTATIC org/sonar/java/bytecode/cfg/BytecodeCFGBuilderTest.sizeTable : [I\n" +
+      "    ILOAD 1\n" +
+      "    IALOAD\n" +
+      "    IF_ICMPGT L2\n" +
+      "   L3\n" +
+      "    LINENUMBER 467 L3\n" +
+      "    ILOAD 1\n" +
+      "    ICONST_1\n" +
+      "    IADD\n" +
+      "    IRETURN\n" +
+      "   L2\n" +
+      "    LINENUMBER 465 L2\n" +
+      "   FRAME SAME\n" +
+      "    IINC 1 1\n" +
+      "    GOTO L1\n" +
+      "   L4\n" +
+      "    LOCALVARIABLE i I L1 L4 1\n" +
+      "    LOCALVARIABLE x I L0 L4 0\n" +
+      "    MAXSTACK = 3\n" +
+      "    MAXLOCALS = 2";
+    String expected = "B0(Exit)\n" +
+      "B1\n" +
+      "0: ICONST_0\n" +
+      "1: ISTORE\n" +
+      "Jumps to: B2 \n" +
+      "B2\n" +
+      "0: ILOAD\n" +
+      "1: GETSTATIC\n" +
+      "2: ILOAD\n" +
+      "3: IALOAD\n" +
+      "IF_ICMPGT Jumps to: B3(true) B4(false) \n" +
+      "B3\n" +
+      "0: IINC\n" +
+      "GOTO Jumps to: B2 \n" +
+      "B4\n" +
+      "0: ILOAD\n" +
+      "1: ICONST_1\n" +
+      "2: IADD\n" +
+      "3: IRETURN\n" +
+      "Jumps to: B0 \n";
+    assertCFG(listing, expected);
+  }
+}
