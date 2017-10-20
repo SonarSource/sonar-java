2317079f2180d25572305a791541b5118afc1ab0
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-10-20 18:32:00 +0200

    Implement zeroness check

--- java-frontend/src/main/java/org/sonar/java/bytecode/se/BytecodeSECheck.java
+++ java-frontend/src/main/java/org/sonar/java/bytecode/se/BytecodeSECheck.java
@@ -19,11 +19,46 @@
  */
 package org.sonar.java.bytecode.se;
 
+import java.util.List;
 import org.objectweb.asm.Opcodes;
 import org.sonar.java.bytecode.cfg.Instruction;
+import org.sonar.java.se.ExplodedGraph;
 import org.sonar.java.se.ProgramState;
+import org.sonar.java.se.checks.DivisionByZeroCheck.ZeroConstraint;
+import org.sonar.java.se.constraint.BooleanConstraint;
 import org.sonar.java.se.constraint.ObjectConstraint;
 import org.sonar.java.se.symbolicvalues.SymbolicValue;
+
+import static org.objectweb.asm.Opcodes.DADD;
+import static org.objectweb.asm.Opcodes.DDIV;
+import static org.objectweb.asm.Opcodes.DMUL;
+import static org.objectweb.asm.Opcodes.DNEG;
+import static org.objectweb.asm.Opcodes.DREM;
+import static org.objectweb.asm.Opcodes.DSUB;
+import static org.objectweb.asm.Opcodes.FADD;
+import static org.objectweb.asm.Opcodes.FDIV;
+import static org.objectweb.asm.Opcodes.FMUL;
+import static org.objectweb.asm.Opcodes.FNEG;
+import static org.objectweb.asm.Opcodes.FREM;
+import static org.objectweb.asm.Opcodes.FSUB;
+import static org.objectweb.asm.Opcodes.IADD;
+import static org.objectweb.asm.Opcodes.IDIV;
+import static org.objectweb.asm.Opcodes.IMUL;
+import static org.objectweb.asm.Opcodes.INEG;
+import static org.objectweb.asm.Opcodes.IREM;
+import static org.objectweb.asm.Opcodes.ISHL;
+import static org.objectweb.asm.Opcodes.ISHR;
+import static org.objectweb.asm.Opcodes.ISUB;
+import static org.objectweb.asm.Opcodes.IUSHR;
+import static org.objectweb.asm.Opcodes.LADD;
+import static org.objectweb.asm.Opcodes.LDIV;
+import static org.objectweb.asm.Opcodes.LMUL;
+import static org.objectweb.asm.Opcodes.LNEG;
+import static org.objectweb.asm.Opcodes.LREM;
+import static org.objectweb.asm.Opcodes.LSHL;
+import static org.objectweb.asm.Opcodes.LSHR;
+import static org.objectweb.asm.Opcodes.LSUB;
+import static org.objectweb.asm.Opcodes.LUSHR;
 
 public interface BytecodeSECheck {
 
@@ -35,7 +70,7 @@ public interface BytecodeSECheck {
     return dispatcher.getState();
   }
 
-  class NullnessCheck implements BytecodeSECheck {
+  static class NullnessCheck implements BytecodeSECheck {
 
     @Override
     public ProgramState checkPreStatement(CheckerDispatcher dispatcher, Instruction inst) {
@@ -62,7 +97,133 @@ public interface BytecodeSECheck {
     }
   }
 
-  class ZeronessCheck implements BytecodeSECheck {
+  static class ZeronessCheck implements BytecodeSECheck {
 
+    @Override
+    public ProgramState checkPostStatement(CheckerDispatcher dispatcher, Instruction inst) {
+      ProgramState currentState = dispatcher.getState();
+      ExplodedGraph.Node node = dispatcher.getNode();
+      if (node == null) {
+        return currentState;
+      }
+      ProgramState previousState = node.programState;
+      switch (inst.opcode) {
+        case DADD:
+        case FADD:
+        case IADD:
+        case LADD:
+        case DSUB:
+        case FSUB:
+        case ISUB:
+        case LSUB:
+          return handlePlusMinus(previousState, currentState);
+        case DMUL:
+        case FMUL:
+        case IMUL:
+        case LMUL:
+          return handleMultiply(previousState, currentState);
+        case DDIV:
+        case FDIV:
+        case IDIV:
+        case LDIV:
+        case DREM:
+        case FREM:
+        case IREM:
+        case LREM:
+          return handleDivisionRemainder(previousState, currentState);
+        case ISHL:
+        case LSHL:
+        case ISHR:
+        case LSHR:
+        case IUSHR:
+        case LUSHR:
+          return handleShift(previousState, currentState);
+        case INEG:
+        case LNEG:
+        case FNEG:
+        case DNEG:
+          return handleNegation(previousState, currentState);
+        default:
+          break;
+      }
+      return currentState;
+    }
+
+    private static ProgramState handlePlusMinus(ProgramState previousState, ProgramState currentState) {
+      List<SymbolicValue> operands = previousState.peekValues(2);
+      SymbolicValue result = currentState.peekValue();
+      SymbolicValue op1 = operands.get(0);
+      SymbolicValue op2 = operands.get(1);
+      boolean op1Zero = isZero(currentState, op1);
+      boolean op2Zero = isZero(currentState, op2);
+      if (op1Zero && op2Zero) {
+        // Reuse one of the zero
+        return currentState.unstackValue(1).state.stackValue(op1);
+      } else if (op1Zero) {
+        // Reuse other operand
+        return currentState.unstackValue(1).state.stackValue(op2);
+      } else if (op2Zero) {
+        // Reuse other operand
+        return currentState.unstackValue(1).state.stackValue(op1);
+      }
+      // we know nothing about zero-ness
+      return currentState.removeConstraintsOnDomain(result, BooleanConstraint.class);
+    }
+
+    private static ProgramState handleMultiply(ProgramState previousState, ProgramState currentState) {
+      List<SymbolicValue> operands = previousState.peekValues(2);
+      SymbolicValue result = currentState.peekValue();
+      SymbolicValue op1 = operands.get(0);
+      SymbolicValue op2 = operands.get(1);
+      boolean op1Zero = isZero(currentState, op1);
+      if (op1Zero || isZero(currentState, op2)) {
+        // Reuse zero
+        return currentState.unstackValue(1).state.stackValue(op1Zero ? op1 : op2);
+      }
+      return currentState.removeConstraintsOnDomain(result, BooleanConstraint.class).addConstraint(result, ZeroConstraint.NON_ZERO);
+    }
+
+    private static ProgramState handleDivisionRemainder(ProgramState previousState, ProgramState currentState) {
+      List<SymbolicValue> operands = previousState.peekValues(2);
+      SymbolicValue result = currentState.peekValue();
+      SymbolicValue op1 = operands.get(0);
+      SymbolicValue op2 = operands.get(1);
+      if (isZero(currentState, op2)) {
+        // Division by zero
+        // TODO exceptional yield ?
+        return null;
+      } else if (isZero(currentState, op1)) {
+        // Reuse zero
+        return currentState.unstackValue(1).state.stackValue(op1);
+      }
+      return currentState.removeConstraintsOnDomain(result, BooleanConstraint.class).addConstraint(result, ZeroConstraint.NON_ZERO);
+    }
+
+    private static ProgramState handleNegation(ProgramState previousState, ProgramState currentState) {
+      List<SymbolicValue> operands = previousState.peekValues(1);
+      SymbolicValue result = currentState.peekValue();
+      SymbolicValue op1 = operands.get(0);
+      if (isZero(currentState, op1)) {
+        // Reuse zero
+        return currentState.unstackValue(1).state.stackValue(op1);
+      }
+      return currentState.removeConstraintsOnDomain(result, BooleanConstraint.class).addConstraint(result, ZeroConstraint.NON_ZERO);
+    }
+
+    private static ProgramState handleShift(ProgramState previousState, ProgramState currentState) {
+      List<SymbolicValue> operands = previousState.peekValues(2);
+      SymbolicValue result = currentState.peekValue();
+      SymbolicValue op1 = operands.get(0);
+      SymbolicValue op2 = operands.get(0);
+      if (isZero(currentState, op1) || isZero(currentState, op2)) {
+        // shifting by zero or shifting zero: Reuse fist operand
+        return currentState.unstackValue(1).state.stackValue(op1);
+      }
+      return currentState.removeConstraintsOnDomain(result, BooleanConstraint.class).addConstraint(result, ZeroConstraint.NON_ZERO);
+    }
+
+    private static boolean isZero(ProgramState state, SymbolicValue sv) {
+      return state.getConstraint(sv, ZeroConstraint.class) == ZeroConstraint.ZERO;
+    }
   }
 }
