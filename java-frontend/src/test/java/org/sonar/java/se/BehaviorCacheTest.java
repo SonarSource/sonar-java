3cd04eef1891929be9504b4aca0d8456c91561c3
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-10-30 18:23:35 +0100

    Don't use behaviors for java.util.Optional

--- java-frontend/src/test/java/org/sonar/java/se/BehaviorCacheTest.java
+++ java-frontend/src/test/java/org/sonar/java/se/BehaviorCacheTest.java
@@ -167,6 +167,8 @@ public class BehaviorCacheTest {
     SymbolicExecutionVisitor sev = createSymbolicExecutionVisitor("src/test/files/se/BehaviorCacheBlacklist.java");
     assertThat(sev.behaviorCache.get("java.lang.Class#getClassLoader()Ljava/lang/ClassLoader;")).isNull();
     assertThat(sev.behaviorCache.get("java.lang.Object#wait()V;")).isNull();
+    assertThat(sev.behaviorCache.get("java.util.Optional#get()Ljava/lang/Object;")).isNull();
+    assertThat(sev.behaviorCache.get("java.util.Optional#isPresent()Z")).isNull();
     assertThat(sev.behaviorCache.behaviors.keySet()).isEmpty();
   }
 
