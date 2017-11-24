42777285671f1178f236632efb1680159f89aeb2
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-11-24 14:02:24 +0100

    Fix quality flaw : reduce timing on some test and drop redundant tests

--- java-frontend/src/test/java/org/sonar/java/se/SETestUtils.java
+++ java-frontend/src/test/java/org/sonar/java/se/SETestUtils.java
@@ -19,31 +19,29 @@
  */
 package org.sonar.java.se;
 
-import com.google.common.collect.Lists;
 import com.sonar.sslr.api.typed.ActionParser;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.apache.commons.io.FileUtils;
-import org.mockito.Mockito;
 import org.sonar.java.ast.parser.JavaParser;
 import org.sonar.java.bytecode.cfg.BytecodeCFG;
 import org.sonar.java.bytecode.cfg.BytecodeCFGMethodVisitor;
 import org.sonar.java.bytecode.loader.SquidClassLoader;
 import org.sonar.java.bytecode.se.MethodLookup;
+import org.sonar.java.model.DefaultJavaFileScannerContext;
+import org.sonar.java.model.JavaVersionImpl;
 import org.sonar.java.resolve.SemanticModel;
 import org.sonar.java.se.checks.SECheck;
 import org.sonar.java.se.xproc.BehaviorCache;
 import org.sonar.java.se.xproc.MethodBehavior;
-import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.tree.CompilationUnitTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class SETestUtils {
 
@@ -52,18 +50,19 @@ public class SETestUtils {
   static {
     CLASS_PATH.add(new File("target/test-classes"));
   }
+
+  private static final ActionParser<Tree> PARSER = JavaParser.createParser();
+  public static final SquidClassLoader CLASSLOADER = new SquidClassLoader(CLASS_PATH);
+
   public static SymbolicExecutionVisitor createSymbolicExecutionVisitor(String fileName, SECheck... checks) {
     return createSymbolicExecutionVisitorAndSemantic(fileName, checks).a;
   }
   public static Pair<SymbolicExecutionVisitor, SemanticModel> createSymbolicExecutionVisitorAndSemantic(String fileName, SECheck... checks) {
-    ActionParser<Tree> p = JavaParser.createParser();
-    CompilationUnitTree cut = (CompilationUnitTree) p.parse(new File(fileName));
-    SemanticModel semanticModel = SemanticModel.createFor(cut, new SquidClassLoader(new ArrayList<>(CLASS_PATH)));
-    SymbolicExecutionVisitor sev = new SymbolicExecutionVisitor(Lists.newArrayList(checks), new BehaviorCache(new SquidClassLoader(CLASS_PATH)));
-    JavaFileScannerContext context = mock(JavaFileScannerContext.class);
-    when(context.getTree()).thenReturn(cut);
-    when(context.getSemanticModel()).thenReturn(semanticModel);
-    sev.scanFile(context);
+    File file = new File(fileName);
+    CompilationUnitTree cut = (CompilationUnitTree) PARSER.parse(file);
+    SemanticModel semanticModel = SemanticModel.createFor(cut, CLASSLOADER);
+    SymbolicExecutionVisitor sev = new SymbolicExecutionVisitor(Arrays.asList(checks), new BehaviorCache(CLASSLOADER));
+    sev.scanFile(new DefaultJavaFileScannerContext(cut, file, semanticModel, null, new JavaVersionImpl(8), true));
     return new Pair<>(sev, semanticModel);
   }
 
@@ -77,14 +76,26 @@ public class SETestUtils {
   }
 
   public static MethodBehavior mockMethodBehavior(int arity, boolean varArgs) {
-    MethodBehavior mockMethodBehavior = Mockito.mock(MethodBehavior.class);
-    Mockito.when(mockMethodBehavior.isMethodVarArgs()).thenReturn(varArgs);
-    Mockito.when(mockMethodBehavior.methodArity()).thenReturn(arity);
-    return mockMethodBehavior;
+    return new MethodBehaviorStub(arity, varArgs);
   }
 
   public static MethodBehavior mockMethodBehavior() {
     return mockMethodBehavior(0, false);
+  }
+
+  private static class MethodBehaviorStub extends MethodBehavior {
+
+    private final int arity;
+
+    public MethodBehaviorStub(int arity, boolean varArgs) {
+      super("()", varArgs);
+      this.arity = arity;
+    }
+
+    @Override
+    public int methodArity() {
+      return arity;
+    }
   }
 
   public static BytecodeCFG bytecodeCFG(String signature, SquidClassLoader classLoader) {
