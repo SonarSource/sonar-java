c707c32ebf95fa027febba563c7f5bcba435c8e1
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-12-08 15:32:44 +0100

    SONARJAVA-2488 RSPEC-3824 Fix fp when value is invocation throwing a checked exception

--- java-frontend/src/test/files/se/MapComputeIfAbsentOrPresentCheck.java
+++ java-frontend/src/test/files/se/MapComputeIfAbsentOrPresentCheck.java
@@ -165,3 +165,35 @@ abstract class MyMap<K, V> implements Map<K, V> {
     // do nothing
   }
 }
+
+abstract class ExceptionThrown {
+  void foo(java.util.Map<String, Object> items, String key) throws MyException {
+    Object value = items.get(key);
+    if (value == null) {
+      items.put(key, bar()); // Compliant, bar() can throw a checked exception, so it can not be extracted to a lambda
+    }
+  }
+  abstract String bar() throws MyException;
+  abstract String bar2() throws MyRuntimeException;
+  abstract String bar4() throws UnknownException;
+  static class MyException extends Exception { }
+  static class MyRuntimeException extends RuntimeException { }
+  void foo2(java.util.Map<String, Object> items, String key) throws MyException {
+    Object value = items.get(key);
+    if (value == null) {
+      items.put(key, unknown_method()); // Compliant, unknown method so put is not resolved
+    }
+  }
+  void foo3(java.util.Map<String, Object> items, String key) {
+    Object value = items.get(key); // Noncompliant (bar2 is throwing a runtime exception)
+    if (value == null) {
+      items.put(key, bar2());
+    }
+  }
+  void foo4(java.util.Map<String, Object> items, String key) {
+    Object value = items.get(key);
+    if (value == null) {
+      items.put(key, bar4()); // compliant : exception thrown is unknown
+    }
+  }
+}
