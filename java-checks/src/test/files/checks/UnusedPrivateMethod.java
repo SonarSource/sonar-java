d2bd73b5b3f3e86ddf13b73cd5ad1ae958c44cd6
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-12-14 15:03:39 +0100

    SONARJAVA-2115 Adding test case to UnusedPrivateMethod (already covered)

--- java-checks/src/test/files/checks/UnusedPrivateMethod.java
+++ java-checks/src/test/files/checks/UnusedPrivateMethod.java
@@ -162,4 +162,23 @@ class KillTheNoiseUnresolvedMethodCall {
   private void unresolvedMethod(int i) {} // Compliant - method with the same name not resolved
   private void unresolvedMethodRef(int i) {} // Compliant - method ref with the same name not resolved
   private void methodRef(Object o) {} // Compliant
+
+  static class Member {
+    private Member(String firstName, String lastName, String memberID) { // Compliant
+    }
+
+    public static LastNameBuilder member(String firstName) {
+      return lastName -> memberID -> new Member(firstName, lastName, memberID); // constructor not resolved
+    }
+
+    @FunctionalInterface
+    public interface LastNameBuilder {
+      MemberIDBuilder lastName(String lastName);
+    }
+
+    @FunctionalInterface
+    public interface MemberIDBuilder {
+      Member memberID(String memberID);
+    }
+  }
 }
