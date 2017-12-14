86b4105097c00bef58f1d921343b6223cc01326c
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-12-14 14:56:41 +0100

    Fix quality flaw: remove deprecated api

--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaSonarWayProfileTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaSonarWayProfileTest.java
@@ -19,26 +19,14 @@
  */
 package org.sonar.plugins.java;
 
-import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.sonar.api.profiles.ProfileDefinition;
-import org.sonar.api.profiles.RulesProfile;
-import org.sonar.api.rules.ActiveRule;
-import org.sonar.api.rules.Rule;
-import org.sonar.api.rules.RuleFinder;
-import org.sonar.api.rules.RulePriority;
-import org.sonar.api.utils.ValidationMessages;
-import org.sonar.java.checks.CheckList;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.junit.Test;
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
+import org.sonar.api.utils.ValidationMessages;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class JavaSonarWayProfileTest {
 
@@ -46,17 +34,18 @@ public class JavaSonarWayProfileTest {
   public void should_create_sonar_way_profile() {
     ValidationMessages validation = ValidationMessages.create();
 
-    JavaSonarWayProfile profileDef = new JavaSonarWayProfile(ruleFinder(false));
-    RulesProfile profile = profileDef.createProfile(validation);
-
-    assertThat(profile.getLanguage()).isEqualTo(Java.KEY);
-    List<ActiveRule> activeRules = profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY);
-    assertThat(profile.getActiveRulesByRepository("common-java")).hasSize(1);
+    JavaSonarWayProfile profileDef = new JavaSonarWayProfile();
+    BuiltInQualityProfilesDefinition.Context context = new BuiltInQualityProfilesDefinition.Context();
+    profileDef.define(context);
+    BuiltInQualityProfilesDefinition.BuiltInQualityProfile profile = context.profile("java", "Sonar way");
+    assertThat(profile.language()).isEqualTo(Java.KEY);
+    List<BuiltInQualityProfilesDefinition.BuiltInActiveRule> activeRules = profile.rules();
+    assertThat(activeRules.stream().filter(r -> r.repoKey().equals("common-java"))).hasSize(1);
     assertThat(activeRules.size()).as("Expected number of rules in profile").isGreaterThanOrEqualTo(268);
-    assertThat(profile.getName()).isEqualTo("Sonar way");
+    assertThat(profile.name()).isEqualTo("Sonar way");
     Set<String> keys = new HashSet<>();
-    for (ActiveRule activeRule : activeRules) {
-      keys.add(activeRule.getRuleKey());
+    for (BuiltInQualityProfilesDefinition.BuiltInActiveRule activeRule : activeRules) {
+      keys.add(activeRule.ruleKey());
     }
     //Check that we store active rule with legacy keys, not RSPEC keys
     assertThat(keys.contains("S116")).isFalse();
@@ -64,37 +53,7 @@ public class JavaSonarWayProfileTest {
     assertThat(validation.hasErrors()).isFalse();
 
     // Check that we use severity from the read rule and not default one.
-    assertThat(activeRules.get(0).getSeverity()).isSameAs(RulePriority.MINOR);
+    assertThat(activeRules.get(0).overriddenSeverity()).isNull();
   }
 
-
-  @Test
-  public void should_load_profile_in_sonar_lint_context() {
-    ProfileDefinition sonarWay = new JavaSonarWayProfile(ruleFinder(true));
-    ValidationMessages validation = ValidationMessages.create();
-    RulesProfile profile = sonarWay.createProfile(validation);
-    assertThat(profile.getActiveRules()).extracting("repositoryKey").containsOnly(CheckList.REPOSITORY_KEY);
-    assertThat(profile.getActiveRulesByRepository("common-java")).isEmpty();
-    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY).size()).isGreaterThanOrEqualTo(268);
-    assertThat(profile.getName()).isEqualTo("Sonar way");
-
-    assertThat(validation.hasErrors()).isFalse();
-    // Check that we use severity from the read rule and not default one.
-    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY).get(0).getSeverity()).isSameAs(RulePriority.MINOR);
-
-  }
-
-  static RuleFinder ruleFinder(boolean forSonarLint) {
-    return when(mock(RuleFinder.class).findByKey(anyString(), anyString())).thenAnswer(new Answer<Rule>() {
-      @Override
-      public Rule answer(InvocationOnMock invocation) {
-        Object[] arguments = invocation.getArguments();
-        if(!forSonarLint || CheckList.REPOSITORY_KEY.equals(arguments[0])) {
-          Rule rule = Rule.create((String) arguments[0], (String) arguments[1], (String) arguments[1]);
-          return rule.setSeverity(RulePriority.MINOR);
-        }
-        return null;
-      }
-    }).getMock();
-  }
 }
