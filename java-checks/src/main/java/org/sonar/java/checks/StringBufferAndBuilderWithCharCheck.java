0d545786c262e5ec0aa4e14f0763ef98ec670c37
Michael Gumowski <michael.gumowski@sonarsource.com>
2018-01-08 10:18:50 +0100

    SONARJAVA-2610 Update licences

--- java-checks/src/main/java/org/sonar/java/checks/StringBufferAndBuilderWithCharCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/StringBufferAndBuilderWithCharCheck.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Java
- * Copyright (C) 2012-2017 SonarSource SA
+ * Copyright (C) 2012-2018 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
