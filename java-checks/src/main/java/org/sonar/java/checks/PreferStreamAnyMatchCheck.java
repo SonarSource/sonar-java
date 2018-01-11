1374e722760f88a1a2cbdc429b68da1dfc22d324
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-01-11 13:58:12 +0100

    SONARJAVA-2429 S4034 Add simplification for anyMatch (#1860)
    * SONARJAVA-2429 S4034 Add simplification for anyMatch
    
    * SONARJAVA-2429 review feedback

--- java-checks/src/main/java/org/sonar/java/checks/PreferStreamAnyMatchCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/PreferStreamAnyMatchCheck.java
@@ -20,22 +20,23 @@
 package org.sonar.java.checks;
 
 import com.google.common.collect.ImmutableSet;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.helpers.MethodsHelper;
 import org.sonar.java.checks.methods.AbstractMethodDetection;
 import org.sonar.java.matcher.MethodMatcher;
 import org.sonar.java.matcher.MethodMatcherCollection;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.LambdaExpressionTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodReferenceTree;
 import org.sonar.plugins.java.api.tree.Tree;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Rule(key = "S4034")
 public class PreferStreamAnyMatchCheck extends AbstractMethodDetection {
@@ -52,28 +53,77 @@ public class PreferStreamAnyMatchCheck extends AbstractMethodDetection {
     });
   }
 
+  private static final MethodMatcherCollection MAP_METHODS = MethodMatcherCollection.create();
+  static {
+    STREAM_TYPES.forEach(type ->
+      MAP_METHODS.add(MethodMatcher.create().typeDefinition(type).name("map").addParameter("java.util.function.Function"))
+    );
+  }
+
   private static final MethodMatcherCollection FILTER_METHODS = MethodMatcherCollection.create();
 
   static {
     STREAM_TYPES.forEach(type -> FILTER_METHODS.add(MethodMatcher.create().typeDefinition(type).name("filter").withAnyParameters()));
   }
 
+  private static final MethodMatcher BOOLEAN_VALUE = MethodMatcher.create().typeDefinition("java.lang.Boolean")
+    .name("booleanValue").withoutParameter();
+
   @Override
   protected List<MethodMatcher> getMethodInvocationMatchers() {
-    return Stream.of("java.util.Optional", "java.util.OptionalInt", "java.util.OptionalLong", "java.util.OptionalDouble")
+    List<MethodMatcher> matchers = new ArrayList<>();
+    Stream.of("java.util.Optional", "java.util.OptionalInt", "java.util.OptionalLong", "java.util.OptionalDouble")
       .map(type -> MethodMatcher.create().typeDefinition(type).name("isPresent").withoutParameter())
-      .collect(Collectors.toList());
+      .forEach(matchers::add);
+    STREAM_TYPES.stream()
+      .map(type -> MethodMatcher.create().typeDefinition(type).name("anyMatch").addParameter("java.util.function.Predicate"))
+      .forEach(matchers::add);
+    return matchers;
   }
 
   @Override
-  protected void onMethodInvocationFound(MethodInvocationTree isPresentMIT) {
+  protected void onMethodInvocationFound(MethodInvocationTree mit) {
+    String methodName = mit.symbol().name();
+    if (methodName.equals("isPresent")) {
+      handleIsPresent(mit);
+    } else if (methodName.equals("anyMatch")) {
+      handleAnyMatch(mit);
+    }
+  }
+
+  private void handleAnyMatch(MethodInvocationTree anyMatchMIT) {
+    ExpressionTree predicate = anyMatchMIT.arguments().get(0);
+    IdentifierTree reportTree = MethodsHelper.methodName(anyMatchMIT);
+    if (anyMatchMIT.parent().is(Tree.Kind.LOGICAL_COMPLEMENT)) {
+      if (predicate.is(Tree.Kind.LAMBDA_EXPRESSION) && ((LambdaExpressionTree) predicate).body().is(Tree.Kind.LOGICAL_COMPLEMENT)) {
+        // !stream.anyMatch(x -> !(...))
+        context.reportIssue(this, reportTree,
+          "Replace this double negation with \"allMatch()\" and positive predicate.");
+      } else {
+        context.reportIssue(this, reportTree,
+          "Replace this negation and \"anyMatch()\" with \"noneMatch()\".");
+      }
+    }
+    if (predicate.is(Tree.Kind.METHOD_REFERENCE) && isBooleanValueReference((MethodReferenceTree) predicate)) {
+      previousMITInChain(anyMatchMIT)
+        .filter(MAP_METHODS::anyMatch)
+        .ifPresent(mapMIT -> context.reportIssue(this, reportTree,
+          "Use mapper from \"map()\" directly as predicate in \"anyMatch()\"."));
+    }
+  }
+
+  private static boolean isBooleanValueReference(MethodReferenceTree predicate) {
+    return BOOLEAN_VALUE.matches(predicate.method().symbol());
+  }
+
+  private void handleIsPresent(MethodInvocationTree isPresentMIT) {
     previousMITInChain(isPresentMIT)
       .filter(FIND_METHODS::anyMatch)
       .ifPresent(findMIT ->
         previousMITInChain(findMIT).filter(FILTER_METHODS::anyMatch)
-        .ifPresent(filterMIT ->
-          context.reportIssue(this, MethodsHelper.methodName(filterMIT), MethodsHelper.methodName(isPresentMIT),
-            "Replace this \"filter()." + MethodsHelper.methodName(findMIT).name() +"().isPresent()\" chain with \"anyMatch()\"")));
+          .ifPresent(filterMIT ->
+            context.reportIssue(this, MethodsHelper.methodName(filterMIT), MethodsHelper.methodName(isPresentMIT),
+              "Replace this \"filter()." + MethodsHelper.methodName(findMIT).name() + "().isPresent()\" chain with \"anyMatch()\".")));
   }
 
   private static Optional<MethodInvocationTree> previousMITInChain(MethodInvocationTree mit) {
--- java-checks/src/test/files/checks/PreferStreamAnyMatch.java
+++ java-checks/src/test/files/checks/PreferStreamAnyMatch.java
@@ -6,8 +6,8 @@ import java.util.stream.*;
 class A {
 
   void test() {
-    IntStream.range(0, 10).filter(i -> true).findFirst().isPresent(); // Noncompliant [[sc=28;ec=67]] {{Replace this "filter().findFirst().isPresent()" chain with "anyMatch()"}}
-    IntStream.range(0, 10).filter(i -> true).findAny().isPresent(); // Noncompliant [[sc=28;ec=65]] {{Replace this "filter().findAny().isPresent()" chain with "anyMatch()"}}
+    IntStream.range(0, 10).filter(i -> true).findFirst().isPresent(); // Noncompliant [[sc=28;ec=67]] {{Replace this "filter().findFirst().isPresent()" chain with "anyMatch()".}}
+    IntStream.range(0, 10).filter(i -> true).findAny().isPresent(); // Noncompliant [[sc=28;ec=65]] {{Replace this "filter().findAny().isPresent()" chain with "anyMatch()".}}
     LongStream.range(0, 10).filter(i -> true).findFirst().isPresent(); // Noncompliant
     LongStream.range(0, 10).filter(i -> true).findAny().isPresent(); // Noncompliant
     Random.doubles(10).filter(i -> true).findFirst().isPresent(); // Noncompliant
@@ -16,6 +16,14 @@ class A {
     Stream.of("a", "b", "c").filter(s -> s.startsWith("a")).findAny().isPresent(); // Noncompliant
 
     Stream.of("a", "b", "c").findAny().isPresent(); // Compliant
+  }
+
+  void anymatch(Stream<Object> stream) {
+    boolean match = !stream.filter(o -> o instanceof A).filter(o -> o != null).anyMatch(o -> true); // Noncompliant {{Replace this negation and "anyMatch()" with "noneMatch()".}}
+    match = !stream.anyMatch(o -> !true); // Noncompliant {{Replace this double negation with "allMatch()" and positive predicate.}}
+    match = stream.map(o -> o.equals("")).anyMatch(Boolean::booleanValue); // Noncompliant [[sc=43;ec=51]] {{Use mapper from "map()" directly as predicate in "anyMatch()".}}
+
+    stream.anyMatch(o -> o != null);
   }
 
   void coverage() {
