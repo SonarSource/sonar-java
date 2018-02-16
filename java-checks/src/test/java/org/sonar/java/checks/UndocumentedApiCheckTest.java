5c47b7a962f7d80cd42027fc37b47b6f2e4ff6d8
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-02-16 09:37:52 +0100

    Fix import for unit test

--- java-checks/src/test/java/org/sonar/java/checks/UndocumentedApiCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/UndocumentedApiCheckTest.java
@@ -20,8 +20,8 @@
 package org.sonar.java.checks;
 
 import org.junit.Test;
+import org.sonar.java.AnalysisException;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
-import org.sonar.squidbridge.api.AnalysisException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
