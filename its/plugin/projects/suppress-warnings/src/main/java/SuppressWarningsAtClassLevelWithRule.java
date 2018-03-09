a7829806499b6b722965ea6e9c243a650ede4348
Michael Gumowski <michael.gumowski@sonarsource.com>
2018-03-09 18:04:06 +0100

    Remove dependency over checkstyle in SuppressWarnings IT and use rule from java-extansion test project

--- its/plugin/projects/suppress-warnings/src/main/java/SuppressWarningsAtClassLevelWithRule.java
+++ its/plugin/projects/suppress-warnings/src/main/java/SuppressWarningsAtClassLevelWithRule.java
@@ -1,6 +1,7 @@
 @SuppressWarnings("squid:S1197")
 public class SuppressWarningsAtClassLevelWithRule {
   int var[];
+  @SuppressWarnings({"java-extension:example"})
   final void method(int a) {
     // Squid:S1197
     int var[];
