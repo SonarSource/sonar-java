b4844851a19f48c030be510cccb41385d582bb11
Michael Gumowski <michael.gumowski@sonarsource.com>
2018-03-16 14:30:48 +0100

    SONARJAVA-2665 Only report from first to last wrong position

--- java-checks/src/test/java/org/sonar/java/checks/xml/maven/PomElementOrderCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/xml/maven/PomElementOrderCheckTest.java
@@ -30,6 +30,11 @@ public class PomElementOrderCheckTest {
   }
 
   @Test
+  public void should_raise_issue_with_location_only_between_first_and_last_wrong() {
+    PomCheckVerifier.verify("src/test/files/checks/xml/maven/PomElementOrderCheck2.xml", new PomElementOrderCheck());
+  }
+
+  @Test
   public void should_not_raise_issue_if_order_is_correct() {
     PomCheckVerifier.verifyNoIssue("src/test/files/checks/xml/maven/PomElementOrderCheckCorrectOrder.xml", new PomElementOrderCheck());
   }
