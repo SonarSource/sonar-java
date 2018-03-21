8d4fad922674a9b2c8e9e7710a692354fc9addb4
stylianos-agapiou-sonarsource <stylianos.agapiou@sonarsource.com>
2018-03-21 16:29:02 +0100

    More General Approach

--- java-checks/src/main/java/org/sonar/java/checks/MethodParametersOrderCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/MethodParametersOrderCheck.java
@@ -19,65 +19,103 @@
  */
 package org.sonar.java.checks;
 
-import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.sonar.check.Rule;
+import org.sonar.java.model.ExpressionUtils;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
 
 @Rule(key = "S2234")
 public class MethodParametersOrderCheck extends IssuableSubscriptionVisitor {
 
-  private Map<Symbol, LinkedHashSet<String>> methodNameKeyAndParameteresValue = new HashMap<>();
+  private Map<Symbol, List<Symbol>> parameterNamesByMethod = new HashMap<>();
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    return ImmutableList.of(Tree.Kind.METHOD, Tree.Kind.METHOD_INVOCATION);
+    return Collections.singletonList(Tree.Kind.METHOD_INVOCATION);
   }
 
   @Override
   public void scanFile(JavaFileScannerContext context) {
-    methodNameKeyAndParameteresValue.clear();
+    parameterNamesByMethod.clear();
     super.scanFile(context);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    if (tree.is(Tree.Kind.METHOD)) {
-      MethodTree methodTree = (MethodTree) tree;
-      methodNameKeyAndParameteresValue.put(methodTree.symbol(), new LinkedHashSet<>(methodTree.parameters().stream().map(param -> param.simpleName().name()
-        .toLowerCase(Locale.ENGLISH)).collect(Collectors.toList())));
-      return;
-    } else {
-      MethodInvocationTree methodInvTree = (MethodInvocationTree) tree;
-      LinkedHashSet<String> definedFunctionParameters = methodNameKeyAndParameteresValue.get(methodInvTree.symbol());
-      LinkedHashSet<String> invocationParameters = new LinkedHashSet<>(
-        methodInvTree.arguments().stream().map(param -> param.toString().toLowerCase(Locale.ENGLISH)).collect(Collectors.toList()));
-      if (definedFunctionParameters == null || invocationParameters.isEmpty()) {
-        return;
-      }
-      if (matchParameters(definedFunctionParameters, invocationParameters)) {
-        return;
+    MethodInvocationTree methodInvTree = (MethodInvocationTree) tree;
+    List<Symbol> definedFunctionParameters = parameterNamesByMethod.get(methodInvTree.symbol());
+    MethodTree methodTree = (MethodTree) methodInvTree.symbol().declaration();
+    if (definedFunctionParameters == null) {
+      if (methodTree != null) {
+        definedFunctionParameters = methodTree.parameters().stream().map(VariableTree::symbol).collect(Collectors.toCollection(ArrayList::new));
+        parameterNamesByMethod.put(methodTree.symbol(), definedFunctionParameters);
       } else {
-        reportIssue(methodInvTree, "Parameters to " + methodInvTree.symbol().name() + " have the same names but not the same order as the method arguments.");
+        return;
       }
+    }
+    List<IdentifierTree> argumentsList = methodInvTree.arguments().stream().map(arg -> {
+      if (arg.is(Tree.Kind.IDENTIFIER)) {
+        return ((IdentifierTree) arg);
+      } else if (arg.is(Tree.Kind.MEMBER_SELECT)) {
+        return (IdentifierTree) ExpressionUtils.skipParentheses(((MemberSelectExpressionTree) arg).identifier());
+      } else {
+        return null;
+      }
+    }).collect(Collectors.toList());
+    if (argumentsNamesMatchParametersNames(definedFunctionParameters, argumentsList)
+      && argumentsTypesMatchParametersTypesAndNamesNotOrdered(definedFunctionParameters, argumentsList)) {
+      List<JavaFileScannerContext.Location> flow = methodTree.parameters().stream().map(param -> new JavaFileScannerContext.Location("Formal Parameters", param))
+        .collect(Collectors.toList());
+      reportIssue(methodInvTree.arguments(), "Parameters to " + methodInvTree.symbol().name() + " have the same names but not the same order as the method arguments.",
+        flow, null);
+
     }
   }
 
-  private static boolean matchParameters(LinkedHashSet<String> functionParameters, LinkedHashSet<String> invocationParameters) {
-    if (invocationParameters.containsAll(functionParameters)) {
-      return new ArrayList<>(functionParameters).equals(new ArrayList<>(invocationParameters));
+  private static boolean argumentsNamesMatchParametersNames(List<Symbol> parametersList, List<IdentifierTree> argumentsList) {
+    List<String> argListNames = argumentsList.stream().filter(Objects::nonNull).map(parameter -> parameter.name().toLowerCase(Locale.ENGLISH)).collect(Collectors.toList());
+    return allUnique(argListNames)
+      && argListNames.stream().allMatch(arg -> parametersList.stream().map(parameter -> parameter.name().toLowerCase(Locale.ENGLISH)).collect(Collectors.toList()).contains(arg));
+
+  }
+
+  public static boolean allUnique(List<String> argListNames) {
+    return argListNames.stream().allMatch(new HashSet<>()::add);
+  }
+
+  private static boolean argumentsTypesMatchParametersTypesAndNamesNotOrdered(List<Symbol> parameterList, List<IdentifierTree> argumentList) {
+    Iterator<IdentifierTree> argumentsIterator = argumentList.stream().filter(Objects::nonNull).iterator();
+    int countArgumentsNotOrdered = 0;
+    while (argumentsIterator.hasNext()) {
+      IdentifierTree argument = argumentsIterator.next();
+      int index = (parameterList.stream().map(param -> param.name().toLowerCase(Locale.ENGLISH)).collect(Collectors.toList()))
+        .indexOf(argument.name().toLowerCase(Locale.ENGLISH));
+      if (parameterList.get(index).type().equals(argument.symbolType())) {
+        if (argumentList.indexOf(argument) != index) {
+          countArgumentsNotOrdered++;
+        }
+        continue;
+      } else {
+        return false;
+      }
     }
-    return true;
+    return countArgumentsNotOrdered >= 2;
   }
 }
