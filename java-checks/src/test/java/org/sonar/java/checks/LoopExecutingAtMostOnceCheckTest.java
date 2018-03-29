dc22da43a18245947d6fbc47378debb5baf34c8d
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2018-03-29 14:51:14 +0200

    SONARJAVA-2668 Adjust behaviour of rule S1751 "Loops with at most one iteration should be refactored"

--- java-checks/src/test/java/org/sonar/java/checks/LoopExecutingAtMostOnceCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/LoopExecutingAtMostOnceCheckTest.java
@@ -22,11 +22,11 @@ package org.sonar.java.checks;
 import org.junit.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
-public class UnconditionalJumpStatementCheckTest {
+public class LoopExecutingAtMostOnceCheckTest {
 
   @Test
   public void test() {
-    JavaCheckVerifier.verify("src/test/files/checks/UnconditionalJumpStatementCheck.java", new UnconditionalJumpStatementCheck());
+    JavaCheckVerifier.verify("src/test/files/checks/LoopExecutingAtMostOnceCheck.java", new LoopExecutingAtMostOnceCheck());
   }
 
 }
