88edabfe434f2cef611a3552cd7f240e23690e2f
Christophe Zürn <christophe.zurn@sonarsource.com>
2018-03-29 15:45:58 +0200

    SONARJAVA-2713 S2757: Fix FP and change message for ' =! ' operators when not used in the context of multiple assignment

--- java-checks/src/test/files/checks/WrongAssignmentOperatorCheck.java
+++ java-checks/src/test/files/checks/WrongAssignmentOperatorCheck.java
@@ -5,11 +5,17 @@ class A {
 
   void fun() {
     target =-num; // Noncompliant {{Was "-=" meant instead?}} [[sc=12;ec=14]]
+    target =
+            -num;
     target = -num; // Compliant intent to assign inverse value of num is clear
     target =--num;
 
     target += num;
     target =+ num; // Noncompliant {{Was "+=" meant instead?}} [[sc=12;ec=14]]
+    target =
+            + num;
+    target =
+            +num;
     target = +num;
     target =++num;
     target=+num; // Compliant - no spaces between variable, operator and expression
@@ -18,5 +24,10 @@ class A {
     a = b =! c; // Noncompliant {{Was "!=" meant instead?}} [[sc=11;ec=13]]
     a = b =!! c; // Noncompliant
     a = b = !c;
+    a =! c; // Noncompliant {{Add a space between "=" and "!" to avoid confusion.}}
+    a = ! c;
+    a = !c;
+    a =
+       !c;
   }
 }
