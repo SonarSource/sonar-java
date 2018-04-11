6cba9a669e787c0ea03a9d5f0142aef2d8af5c3a
Michael Gumowski <michael.gumowski@sonarsource.com>
2018-04-11 11:32:20 +0200

    Add new 'TypeAssertions' class to simplify testing of types in semantic

---
+++ java-frontend/src/test/java/org/sonar/java/resolve/TypeAssertions.java
@@ -0,0 +1,63 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.resolve;
+
+import org.assertj.core.api.AbstractAssert;
+import org.sonar.plugins.java.api.semantic.Type;
+
+public class TypeAssertions extends AbstractAssert<TypeAssertions, Type> {
+
+  public TypeAssertions(Type actual) {
+    super(actual, TypeAssertions.class);
+  }
+
+  public TypeAssertions isSubtypeOf(String fullyQualifiedName) {
+    if (!actual.isSubtypeOf(fullyQualifiedName)) {
+      failWithMessage("Expecting '%s' to be subtype of '%s'", actual.name(), fullyQualifiedName);
+    }
+    return this;
+  }
+
+  public TypeAssertions isNotSubtypeOf(String fullyQualifiedName) {
+    if (actual.isSubtypeOf(fullyQualifiedName)) {
+      failWithMessage("Expecting '%s' to NOT be subtype of '%s'", actual.name(), fullyQualifiedName);
+    }
+    return this;
+  }
+
+  public TypeAssertions is(String fullyQualifiedName) {
+    if (!actual.is(fullyQualifiedName)) {
+      failWithMessage("Expecting '%s' to be of type '%s'", actual.name(), fullyQualifiedName);
+    }
+    return this;
+  }
+
+  public TypeAssertions isNot(String fullyQualifiedName) {
+    if (actual.is(fullyQualifiedName)) {
+      failWithMessage("Expecting '%s' to NOT be of type '%s'", actual.name(), fullyQualifiedName);
+    }
+    return this;
+  }
+
+  public static TypeAssertions assertThat(Type actual) {
+    return new TypeAssertions(actual);
+  }
+
+}
