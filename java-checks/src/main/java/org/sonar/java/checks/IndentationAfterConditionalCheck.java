4710c44b94bda6aeb7b9689786ca06f1fc0fb71f
stylianos-agapiou-sonarsource <36889190+stylianos-agapiou-sonarsource@users.noreply.github.com>
2018-04-18 15:07:33 +0200

    SONARJAVA-2247 RSPEC-3973 Conditionally executed code should be denoted by either indentation or curly braces (#1997)

---
+++ java-checks/src/main/java/org/sonar/java/checks/IndentationAfterConditionalCheck.java
@@ -0,0 +1,86 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.Collections;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.JavaFileScanner;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.ForEachStatement;
+import org.sonar.plugins.java.api.tree.ForStatementTree;
+import org.sonar.plugins.java.api.tree.IfStatementTree;
+import org.sonar.plugins.java.api.tree.StatementTree;
+import org.sonar.plugins.java.api.tree.SyntaxToken;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.WhileStatementTree;
+
+@Rule(key = "S3973")
+public class IndentationAfterConditionalCheck extends BaseTreeVisitor implements JavaFileScanner {
+
+  private JavaFileScannerContext context;
+
+  @Override
+  public void scanFile(JavaFileScannerContext context) {
+    this.context = context;
+    scan(context.getTree());
+  }
+
+  @Override
+  public void visitIfStatement(IfStatementTree tree) {
+    Tree parentTree = tree.parent();
+    if (parentTree.is(Tree.Kind.IF_STATEMENT) && tree.equals(((IfStatementTree) parentTree).elseStatement())) {
+      checkForReport(tree.thenStatement(), ((IfStatementTree) parentTree).elseKeyword(), tree.closeParenToken(), tree.ifKeyword().firstToken().text());
+    } else {
+      checkForReport(tree.thenStatement(), tree.ifKeyword(), tree.closeParenToken(), tree.ifKeyword().text());
+    }
+    SyntaxToken elseKeyword = tree.elseKeyword();
+    if (elseKeyword != null && !tree.elseStatement().is(Tree.Kind.IF_STATEMENT)) {
+      checkForReport(tree.elseStatement(), elseKeyword, elseKeyword, elseKeyword.firstToken().text());
+    }
+    super.visitIfStatement(tree);
+  }
+
+  @Override
+  public void visitWhileStatement(WhileStatementTree tree) {
+    super.visitWhileStatement(tree);
+    checkForReport(tree.statement(), tree.whileKeyword(), tree.closeParenToken(), tree.whileKeyword().text());
+  }
+
+  @Override
+  public void visitForStatement(ForStatementTree tree) {
+    super.visitForStatement(tree);
+    checkForReport(tree.statement(), tree.forKeyword(), tree.closeParenToken(), tree.forKeyword().text());
+  }
+
+  @Override
+  public void visitForEachStatement(ForEachStatement tree) {
+    super.visitForEachStatement(tree);
+    checkForReport(tree.statement(), tree.forKeyword(), tree.closeParenToken(), tree.forKeyword().text());
+  }
+
+  private void checkForReport(StatementTree statement, Tree startTree, Tree endTree, String name) {
+    if (!(statement.is(Tree.Kind.BLOCK) || statement.firstToken().column() > startTree.firstToken().column())) {
+      context.reportIssue(this, startTree, endTree,
+        "Use curly braces or indentation to denote the code conditionally executed by this \"" + name + "\".",
+        Collections.singletonList(new JavaFileScannerContext.Location("", statement)), null);
+    }
+  }
+}
---
+++ java-checks/src/test/files/checks/IndentationAfterConditionalCheck.java
@@ -0,0 +1,180 @@
+import java.util.List;
+
+class A {
+  public int foo() {
+    int i = 0;
+    if (i <= 1) // Noncompliant [[sc=5;ec=16;secondary=7]] {{Use curly braces or indentation to denote the code conditionally executed by this "if".}}
+    i = 1;
+    i++;
+
+    if(i > 0)   // Noncompliant [[sc=5;ec=14;secondary=12]]
+    // ......
+    doTheOtherThing();
+    //.....
+    doTheThing();
+
+    if (i <= 1) {  // Compliant
+      i = 1;
+      i = 2;
+      i++;
+    }
+
+    if (i <= 1)   // Compliant
+      i = 1;
+
+    if (i == 0)
+          return 1;
+
+    if(i <= 11) // Noncompliant [[sc=5;ec=16;secondary=29]]
+    doTheThing();
+    doTheOtherThing();
+    somethingElseEntirely();
+
+    if (i != 0)
+        i++;
+
+    if(i <= 1)  // Noncompliant
+
+    i=1;
+    i++;
+
+    if(i==10)   // Noncompliant
+
+
+    i=10;
+
+    if (i == 5) // Noncompliant
+    doTheThing();
+    else if(i == 6)   // Noncompliant
+    somethingElseEntirely();
+    else         // Noncompliant
+    doTheThing();
+    doTheOtherThing();
+
+      if(i == 0)  // Noncompliant
+    doTheThing();
+      else          // Noncompliant   [sc=7;ec=11;secondary=55] {{Use curly braces or indentation to denote the code conditionally executed by this "else".}}
+    doTheOtherThing();
+
+    if(i > 10)
+      i =100;
+    i++;
+
+    for (; i <= 1;) // Noncompliant [[sc=5;ec=20;secondary=64]] {{Use curly braces or indentation to denote the code conditionally executed by this "for".}}
+    i = 1;
+    i++;
+
+    for (; i <= 1;) { // Compliant
+     i = 1;
+     i++;
+    }
+
+    for (; i <= 1;) // Compliant
+      i = 1;
+
+    while (i <= 10)  // Compliant
+      i++;
+
+    while (i <= 10) {  // Compliant
+      i++;
+      i += 10;
+    }
+
+    while(i<=11)     // Noncompliant [[sc=5;ec=17;secondary=84]] {{Use curly braces or indentation to denote the code conditionally executed by this "while".}}
+    i++;
+    i+=10;
+
+    List<Integer> l=null;
+    for(Integer ii : l) // Compliant
+      return i;
+
+    for(Integer ii : l) // Noncompliant [[sc=5;ec=24;secondary=92]] {{Use curly braces or indentation to denote the code conditionally executed by this "for".}}
+    i=1;
+    i=2;
+
+    for(Integer ii : l) // Noncompliant [[sc=5;ec=24;secondary=96]] {{Use curly braces or indentation to denote the code conditionally executed by this "for".}}
+   if(ii == 1)  // Noncompliant [[sc=4;ec=15;secondary=97]] {{Use curly braces or indentation to denote the code conditionally executed by this "if".}}
+   i++;
+   i=2;
+
+    if(i ==1)   // Noncompliant
+  i++;
+    else
+      i++;
+
+    for(i =0;i<10;i++)  // Noncompliant [[sc=5;ec=23;secondary=106]]  {{Use curly braces or indentation to denote the code conditionally executed by this "for".}}
+  if(i==1)
+    i++;
+    if(i == 1)   // Compliant
+      if(i == 2)  // Noncompliant [[sc=7;ec=17;secondary=110]] {{Use curly braces or indentation to denote the code conditionally executed by this "if".}}
+      i++;
+      i+=2;
+      
+    if(i == 0)
+      i++;
+    else if(i == 1)
+      i = 1;
+    else    if(i ==3)
+      return i;
+    else       // Noncompliant [[sc=5;ec=9;secondary=120]] {{Use curly braces or indentation to denote the code conditionally executed by this "else".}}
+    i=1;
+    
+    while(i<=0)
+      for(;i<10;)  // Noncompliant [[sc=7;ec=18;secondary=124]] {{Use curly braces or indentation to denote the code conditionally executed by this "for".}}
+     if(i<1)  // Noncompliant [[sc=6;ec=13;secondary=125]] {{Use curly braces or indentation to denote the code conditionally executed by this "if".}}
+    return 2;   
+    
+    if(i == 1)
+      return i;
+    else
+    {
+      i++;
+    }
+    
+    while(i>1)
+      if(i==1)    // Noncompliant
+    return i;
+   
+    if (i < 2) {
+      if ((i > 0) && (i > 3))  // Noncompliant [[sc=7;ec=30;secondary=140]] {{Use curly braces or indentation to denote the code conditionally executed by this "if".}}
+    return i;
+      return null;
+    }
+    
+    while (n < len) {
+      int count = in.read(b, off + n, len - n);
+      if (count < 0)        // Noncompliant
+    throw new EOFException();
+      n += count;
+  }
+    
+    if(true) {
+      if(i ==1)  // Noncompliant
+  return 1;
+    }
+    if(i==0)
+      i++;
+    else if(i==1)
+      if(i==2)
+        i++;
+      else 
+        i++;
+    else if(i ==2) // Noncompliant [[sc=5;ec=19;secondary=163]] {{Use curly braces or indentation to denote the code conditionally executed by this "if".}}
+    i+=2;  
+    else 
+      if(i==1)
+        return 1;
+    if (i > 0)
+      if (i >1) // Noncompliant
+      doTheThing();
+      else
+        doTheOtherThing();
+    else
+      somethingElseEntirely();
+    return i;  
+  }
+
+  public void doTheThing() {};
+  public void doTheOtherThing() {};
+  public void somethingElseEntirely() {};
+}
