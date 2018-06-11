1e28deddcb6a1f8754698513ed6e2c515e0e63ea
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-06-11 08:41:18 +0200

    Fix quality flaw : improve coverage

--- java-checks/src/test/files/checks/StringOffsetMethodsCheck.java
+++ java-checks/src/test/files/checks/StringOffsetMethodsCheck.java
@@ -1,7 +1,10 @@
 class A {
+  String field;
   void test(String str, char char1, int beginIndex, String str2) {
     str.substring(beginIndex).indexOf(char1); // Noncompliant [[sc=9;ec=45]] {{Replace "indexOf" with the overload that accepts an offset parameter.}}
     str.indexOf(char1, beginIndex);
+    this.field.indexOf(char1);
+    this.fun().indexOf(char1);
 
     str.substring(beginIndex).indexOf(str2); // Noncompliant [[sc=9;ec=44]]
     str.indexOf(str2, beginIndex);
@@ -15,4 +18,5 @@ class A {
     str.substring(beginIndex).startsWith(str2); // Noncompliant [[sc=9;ec=47]] {{Replace "startsWith" with the overload that accepts an offset parameter.}}
     str.startsWith(str2, beginIndex);
   }
+  String fun()  {return "";}
 }
