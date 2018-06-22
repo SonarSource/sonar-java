3fca1b61b8a47d92184cf51e3f411fe8fc99109e
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2018-06-22 08:59:17 +0200

    SONARJAVA-2797 Rule S4684: Persistent objects as request parameters (#2079)
    SONARJAVA-2797 Rule S4684: Persistent entities should not be used as arguments of "@RequestMapping" methods

---
+++ java-checks/src/main/java/org/sonar/java/checks/spring/PersistentEntityUsedAsRequestParameterCheck.java
@@ -0,0 +1,76 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.spring;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
+
+@Rule(key = "S4684")
+public class PersistentEntityUsedAsRequestParameterCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return Collections.singletonList(Tree.Kind.METHOD);
+  }
+
+  private static final List<String> REQUEST_ANNOTATIONS = Arrays.asList(
+    "org.springframework.web.bind.annotation.RequestMapping",
+    "org.springframework.web.bind.annotation.GetMapping",
+    "org.springframework.web.bind.annotation.PostMapping",
+    "org.springframework.web.bind.annotation.PutMapping",
+    "org.springframework.web.bind.annotation.DeleteMapping",
+    "org.springframework.web.bind.annotation.PatchMapping");
+
+  private static final List<String> ENTITY_ANNOTATIONS = Arrays.asList(
+    "javax.persistence.Entity",
+    "org.springframework.data.mongodb.core.mapping.Document",
+    "org.springframework.data.elasticsearch.annotations.Document");
+
+  @Override
+  public void visitNode(Tree tree) {
+    if (!hasSemantic()) {
+      return;
+    }
+
+    MethodTree methodTree = (MethodTree) tree;
+    Symbol.MethodSymbol methodSymbol = methodTree.symbol();
+
+    if (isRequestMappingAnnotated(methodSymbol)) {
+      methodTree.parameters().stream()
+        .filter(PersistentEntityUsedAsRequestParameterCheck::isPersistentEntity)
+        .forEach(p -> reportIssue(p.simpleName(), "Replace this persistent entity with a simple POJO or DTO object."));
+    }
+  }
+
+  private static boolean isRequestMappingAnnotated(Symbol.MethodSymbol methodSymbol) {
+    return REQUEST_ANNOTATIONS.stream().anyMatch(methodSymbol.metadata()::isAnnotatedWith);
+  }
+
+  private static boolean isPersistentEntity(VariableTree variableTree) {
+    return ENTITY_ANNOTATIONS.stream().anyMatch(variableTree.type().symbolType().symbol().metadata()::isAnnotatedWith);
+  }
+}
