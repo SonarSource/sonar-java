4220b6e7d933abf4c04e5d5af8647f06c4443bd0
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2018-07-16 15:25:25 +0200

    Slightly simplify Spotbugs import

--- external-reports/src/main/java/org/sonar/java/externalreport/spotbugs/SpotBugsSensor.java
+++ external-reports/src/main/java/org/sonar/java/externalreport/spotbugs/SpotBugsSensor.java
@@ -26,18 +26,14 @@ import java.io.InputStream;
 import java.util.List;
 import javax.xml.stream.XMLStreamException;
 import org.sonar.api.Plugin.Context;
-import org.sonar.api.batch.fs.FilePredicates;
-import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.sensor.Sensor;
 import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.batch.sensor.SensorDescriptor;
 import org.sonar.api.config.PropertyDefinition;
 import org.sonar.api.resources.Qualifiers;
-import org.sonar.api.rule.RuleKey;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 import org.sonar.java.externalreport.ExternalReportExtensions;
-import org.sonar.java.externalreport.commons.ExternalIssueUtils;
 import org.sonar.java.externalreport.commons.ExternalRulesDefinition;
 import org.sonarsource.analyzer.commons.ExternalReportProvider;
 import org.sonarsource.analyzer.commons.ExternalRuleLoader;
@@ -46,25 +42,16 @@ public class SpotBugsSensor implements Sensor {
 
   private static final Logger LOG = Loggers.get(SpotBugsSensor.class);
 
-  private static final String LINTER_KEY = "spotbugs";
-
+  public static final String LINTER_KEY = "spotbugs";
   private static final String LINTER_NAME = "SpotBugs";
-
   private static final String LANGUAGE_KEY = "java";
-
   private static final String REPORT_PROPERTY_KEY = "sonar.java.spotbugs.reportPaths";
 
-  private static class SingletonRuleLoader {
-    private static final ExternalRuleLoader INSTANCE = new ExternalRuleLoader(
-      SpotBugsSensor.LINTER_KEY,
-      SpotBugsSensor.LINTER_NAME,
-      "org/sonar/l10n/java/rules/spotbugs/rules.json",
-      SpotBugsSensor.LANGUAGE_KEY);
-  }
-
-  static ExternalRuleLoader ruleLoader() {
-    return SingletonRuleLoader.INSTANCE;
-  }
+  private static final ExternalRuleLoader RULE_LOADER = new ExternalRuleLoader(
+    SpotBugsSensor.LINTER_KEY,
+    SpotBugsSensor.LINTER_NAME,
+    "org/sonar/l10n/java/rules/spotbugs/rules.json",
+    SpotBugsSensor.LANGUAGE_KEY);
 
   @Override
   public void describe(SensorDescriptor descriptor) {
@@ -79,7 +66,7 @@ public class SpotBugsSensor implements Sensor {
   }
 
   public static void defineRulesAndProperties(Context context) {
-    context.addExtension(new ExternalRulesDefinition(ruleLoader()));
+    context.addExtension(new ExternalRulesDefinition(RULE_LOADER));
     context.addExtension(
       PropertyDefinition.builder(SpotBugsSensor.REPORT_PROPERTY_KEY)
         .name("SpotBugs Report Files")
@@ -100,35 +87,11 @@ public class SpotBugsSensor implements Sensor {
   private static void importReport(File reportPath, SensorContext context) {
     try (InputStream in = new FileInputStream(reportPath)) {
       LOG.info("Importing {}", reportPath);
-      SpotBugsXmlReportReader.read(context, in, SpotBugsSensor::saveIssue);
+      SpotBugsXmlReportReader.read(context, in, RULE_LOADER);
     } catch (IOException | XMLStreamException | RuntimeException e) {
       LOG.error(e.getClass().getSimpleName() + ": " + e.getMessage() +
         ", no issues information will be saved as the report file '{}' can't be read.", reportPath, e);
     }
-  }
-
-  private static void saveIssue(SensorContext context, List<String> sourceDirs,
-                                String rule, String relativeLinuxPath, String line, String message) {
-    InputFile inputFile = findInputFile(context, sourceDirs, relativeLinuxPath);
-    if (inputFile == null) {
-      LOG.warn("No input file found for '{}'. No SpotBugs issues will be imported on this file.", relativeLinuxPath);
-      return;
-    }
-    RuleKey ruleKey = RuleKey.of(SpotBugsSensor.LINTER_KEY, rule);
-    ExternalIssueUtils.saveIssue(context, ruleLoader(), inputFile, ruleKey, line, message);
-  }
-
-  private static InputFile findInputFile(SensorContext context, List<String> sourceDirs, String relativeLinuxPath) {
-    FilePredicates predicates = context.fileSystem().predicates();
-    InputFile inputFile = null;
-    for (String sourceDir : sourceDirs) {
-      File sourceFile = new File(sourceDir, relativeLinuxPath);
-      inputFile = context.fileSystem().inputFile(predicates.hasPath(sourceFile.toString()));
-      if (inputFile != null) {
-        break;
-      }
-    }
-    return inputFile;
   }
 
 }
--- external-reports/src/main/java/org/sonar/java/externalreport/spotbugs/SpotBugsXmlReportReader.java
+++ external-reports/src/main/java/org/sonar/java/externalreport/spotbugs/SpotBugsXmlReportReader.java
@@ -19,6 +19,7 @@
  */
 package org.sonar.java.externalreport.spotbugs;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -32,9 +33,14 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+import org.sonar.api.batch.fs.FilePredicates;
+import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.sensor.SensorContext;
+import org.sonar.api.rule.RuleKey;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
+import org.sonar.java.externalreport.commons.ExternalIssueUtils;
+import org.sonarsource.analyzer.commons.ExternalRuleLoader;
 
 public class SpotBugsXmlReportReader {
 
@@ -43,32 +49,24 @@ public class SpotBugsXmlReportReader {
   private static final QName TYPE_ATTRIBUTE = new QName("type");
   private static final QName SOURCE_PATH_ATTRIBUTE = new QName("sourcepath");
   private static final QName START_ATTRIBUTE = new QName("start");
-  private static final String XPATH_SEPARATOR = "/";
 
   private final SensorContext context;
-  private final IssueConsumer consumer;
+  private final ExternalRuleLoader ruleLoader;
 
   private List<String> sourceDirs = new ArrayList<>();
   private String bugInstanceType = "";
   private String bugInstanceLongMessage = "";
   private String sourceLinePath = "";
   private String sourceLineStart = "";
-  private StringBuilder characterEvents = null;
+  private StringBuilder textBuilder = null;
 
-  @FunctionalInterface
-  interface IssueConsumer {
-    void onIssue(SensorContext context, List<String> sourceDirs,
-                 String bugInstanceType, String sourceLinePath, String sourceLineStart, String bugInstanceLongMessage)
-      throws IOException;
-  }
-
-  private SpotBugsXmlReportReader(SensorContext context, IssueConsumer consumer) {
+  private SpotBugsXmlReportReader(SensorContext context, ExternalRuleLoader ruleLoader) {
     this.context = context;
-    this.consumer = consumer;
+    this.ruleLoader = ruleLoader;
   }
 
-  static void read(SensorContext context, InputStream in, IssueConsumer consumer) throws XMLStreamException, IOException {
-    new SpotBugsXmlReportReader(context, consumer).read(in);
+  static void read(SensorContext context, InputStream in, ExternalRuleLoader ruleLoader) throws XMLStreamException, IOException {
+    new SpotBugsXmlReportReader(context, ruleLoader).read(in);
   }
 
   private void read(InputStream in) throws XMLStreamException, IOException {
@@ -77,62 +75,62 @@ public class SpotBugsXmlReportReader {
     while (reader.hasNext()) {
       XMLEvent event = reader.nextEvent();
       if (event.isStartElement()) {
-        String localPart = event.asStartElement().getName().getLocalPart();
-        String xpath = elementStack.isEmpty() ? localPart : (elementStack.peek() + XPATH_SEPARATOR + localPart);
-        if (elementStack.isEmpty() && !"BugCollection".equals(xpath)) {
-          throw new IOException("Unexpected document root '" + xpath + "' instead of 'BugCollection'.");
+        String elementName = event.asStartElement().getName().getLocalPart();
+        if (elementStack.isEmpty() && !"BugCollection".equals(elementName)) {
+          throw new IOException("Unexpected document root '" + elementName + "' instead of 'BugCollection'.");
         }
-        elementStack.push(xpath);
-        onStartElement(xpath, event.asStartElement());
+        elementStack.add(elementName);
+        onStartElement(xpath(elementStack), event.asStartElement());
       } else if (event.isEndElement()) {
-        onEndElement(elementStack.peek());
-        elementStack.pop();
-      } else if (event.isCharacters() && characterEvents != null) {
-        characterEvents.append(event.asCharacters().getData());
+        onEndElement(xpath(elementStack));
+        elementStack.removeLast();
+      } else if (event.isCharacters() && textBuilder != null) {
+        textBuilder.append(event.asCharacters().getData());
       }
     }
+  }
+
+  private static String xpath(Deque<String> elementStack) {
+    return String.join("/", elementStack);
   }
 
   private void onStartElement(String xpath, StartElement element) {
     switch (xpath) {
       case "BugCollection/BugInstance":
         bugInstanceType = getAttributeValue(element, TYPE_ATTRIBUTE);
-        break;
-      case "BugCollection/BugInstance/SourceLine":
-        if (sourceLinePath.isEmpty()) {
-          sourceLinePath = getAttributeValue(element, SOURCE_PATH_ATTRIBUTE);
-          sourceLineStart = getAttributeValue(element, START_ATTRIBUTE);
-        }
-        break;
-      case "BugCollection/BugInstance/LongMessage":
-      case "BugCollection/Project/SrcDir":
-        characterEvents = new StringBuilder();
-        break;
-      default:
-        // ignore
-        break;
-    }
-  }
-
-  private void onEndElement(String xpath) throws IOException {
-    switch (xpath) {
-      case "BugCollection/BugInstance":
-        consumeBugInstance();
-        bugInstanceType = "";
         bugInstanceLongMessage = "";
         sourceLinePath = "";
         sourceLineStart = "";
         break;
+      case "BugCollection/BugInstance/SourceLine":
+        sourceLinePath = getAttributeValue(element, SOURCE_PATH_ATTRIBUTE);
+        sourceLineStart = getAttributeValue(element, START_ATTRIBUTE);
+        break;
       case "BugCollection/BugInstance/LongMessage":
-        if(characterEvents != null) {
-          bugInstanceLongMessage = characterEvents.toString();
-          characterEvents = null;
+      case "BugCollection/Project/SrcDir":
+        textBuilder = new StringBuilder();
+        break;
+      default:
+        // ignore
+        break;
+    }
+  }
+
+  private void onEndElement(String xpath) {
+    switch (xpath) {
+      case "BugCollection/BugInstance":
+        consumeBugInstance();
+        break;
+      case "BugCollection/BugInstance/LongMessage":
+        if(textBuilder != null) {
+          bugInstanceLongMessage = textBuilder.toString();
+          textBuilder = null;
         }
         break;
       case "BugCollection/Project/SrcDir":
-        if(characterEvents != null) {
-          sourceDirs.add(characterEvents.toString());
-          characterEvents = null;
+        if(textBuilder != null) {
+          sourceDirs.add(textBuilder.toString());
+          textBuilder = null;
         }
         break;
       default:
@@ -141,7 +139,7 @@ public class SpotBugsXmlReportReader {
     }
   }
 
-  private void consumeBugInstance()  throws IOException {
+  private void consumeBugInstance() {
     if (sourceDirs.isEmpty()) {
       LOG.debug("Unexpected missing 'BugCollection/Project/SrcDir/text()'.");
       return;
@@ -158,12 +156,31 @@ public class SpotBugsXmlReportReader {
       LOG.debug("Unexpected empty 'BugCollection/BugInstance/LongMessage/text()' for bug '{}'", bugInstanceType);
       return;
     }
-    consumer.onIssue(context, sourceDirs, bugInstanceType, sourceLinePath, sourceLineStart, bugInstanceLongMessage);
+    InputFile inputFile = findInputFile(context, sourceDirs, sourceLinePath);
+    if (inputFile == null) {
+      LOG.warn("No input file found for '{}'. No SpotBugs issues will be imported on this file.", sourceLinePath);
+      return;
+    }
+    RuleKey ruleKey = RuleKey.of(SpotBugsSensor.LINTER_KEY, bugInstanceType);
+    ExternalIssueUtils.saveIssue(context, ruleLoader, inputFile, ruleKey, sourceLineStart, bugInstanceLongMessage);
   }
 
   private static String getAttributeValue(StartElement element, QName attributeName) {
     Attribute attribute = element.getAttributeByName(attributeName);
     return attribute != null ? attribute.getValue() : "";
+  }
+
+  private static InputFile findInputFile(SensorContext context, List<String> sourceDirs, String relativeLinuxPath) {
+    FilePredicates predicates = context.fileSystem().predicates();
+    InputFile inputFile = null;
+    for (String sourceDir : sourceDirs) {
+      File sourceFile = new File(sourceDir, relativeLinuxPath);
+      inputFile = context.fileSystem().inputFile(predicates.hasPath(sourceFile.toString()));
+      if (inputFile != null) {
+        break;
+      }
+    }
+    return inputFile;
   }
 
 }
