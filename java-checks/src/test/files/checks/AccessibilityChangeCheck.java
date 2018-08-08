c689726c35028a0400504b41bfc423ebc4d3ea15
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-08-08 14:29:13 +0200

    SONARJAVA-2683 add testcase

--- java-checks/src/test/files/checks/AccessibilityChangeCheck.java
+++ java-checks/src/test/files/checks/AccessibilityChangeCheck.java
@@ -1,6 +1,7 @@
 class A {
-    void makeItPublic(String methodName) throws NoSuchMethodException {
+    void makeItPublic(String methodName, java.lang.reflect.AccessibleObject[] arr) throws NoSuchMethodException {
       this.getClass().getMethod(methodName).setAccessible(true); // Noncompliant {{Remove this accessibility update.}}
+      this.getClass().getMethod(methodName).setAccessible(arr, true); // Noncompliant {{Remove this accessibility update.}}
     }
     void setItAnyway(String fieldName, int value) {
       this.getClass().getDeclaredField(fieldName).setInt(this, value); // Noncompliant {{Remove this accessibility bypass.}}
