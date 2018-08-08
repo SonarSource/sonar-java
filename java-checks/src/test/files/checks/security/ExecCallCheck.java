d877850bb9b6d5088bffc0c5993dbeb38f61f936
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-08-08 11:36:00 +0200

    SONARJAVA-2854 Rule S4721: Executing OS commands is security-sensitive

---
+++ java-checks/src/test/files/checks/security/ExecCallCheck.java
@@ -0,0 +1,39 @@
+package org.sonar.java.checks.security;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+
+public class Foo {
+  public void runtime() throws IOException {
+    Runtime r = Runtime.getRuntime();
+    r.exec("mv a/ b/"); // Noncompliant {{Make sure that executing this OS command is safe here.}}
+
+    r.exec("cd ../a", new String[] {"foo=bar"}); // Noncompliant
+    r.exec("cd ../a", new String[] {"foo=bar"}, new File("../c")); // Noncompliant
+    r.exec(new String[] {"cd ../a"}); // Noncompliant
+    r.exec(new String[] {"cd ../a"}, new String[] {"foo=bar"}); // Noncompliant
+    r.exec(new String[] {"cd ../a"}, new String[] {"foo=bar"}, new File("../c")); // Noncompliant
+  }
+
+  public void commandLine() throws IOException {
+    String line = "bad.exe";
+    CommandLine cmdLine = CommandLine.parse("bad.exe");
+    DefaultExecutor executor = new DefaultExecutor();
+    executor.execute(cmdLine); // Noncompliant
+  }
+
+  public void processBuilder() {
+    List<String> commands = Arrays.asList("myCommand", "myArg1", "myArg2");
+    ProcessBuilder pb =
+      new ProcessBuilder("myCommand", "myArg1", "myArg2"); // Noncompliant
+    ProcessBuilder pb2 =
+      new ProcessBuilder(commands); // Noncompliant
+    pb.command();
+    pb.command(commands); // Noncompliant
+    pb.command("myCommand", "myArg1", "myArg2"); // Noncompliant
+  }
+}
