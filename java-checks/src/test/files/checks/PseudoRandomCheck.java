ec65e2b014d827296bcaa619b40ed6af03042089
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-08-09 15:13:47 +0200

    SONARJAVA-2842 Adjust the "message" of S2245 because RSPEC-2245 is now a Security Hotspot

--- java-checks/src/test/files/checks/PseudoRandomCheck.java
+++ java-checks/src/test/files/checks/PseudoRandomCheck.java
@@ -2,9 +2,9 @@ import java.util.Random;
 
 class A {
   void fun() {
-    Random random = new Random(); // Noncompliant [[sc=25;ec=31]] {{Use a cryptographically strong random number generator (RNG) like "java.security.SecureRandom" in place of this PRNG}}
+    Random random = new Random(); // Noncompliant [[sc=25;ec=31]] {{Make sure that using this pseudorandom number generator is safe here.}}
     byte[] bytes = new byte[20];
     random.nextBytes(bytes);
-    double j = Math.random(); // Noncompliant [[sc=21;ec=27]] {{Use a cryptographically strong random number generator (RNG) like "java.security.SecureRandom" in place of this PRNG}}
+    double j = Math.random(); // Noncompliant [[sc=21;ec=27]]
   }
 }
