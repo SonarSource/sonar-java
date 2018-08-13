c396d66ba1a7c325454de8f250442881143bb8b2
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-08-13 15:31:35 +0200

    SONARJAVA-2772 S3330 should not raise an issue when Cookie is named 'XSRF-TOKEN'

--- java-checks/src/main/java/org/sonar/java/checks/security/CookieHttpOnlyCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/security/CookieHttpOnlyCheck.java
@@ -22,7 +22,9 @@ package org.sonar.java.checks.security;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.helpers.ConstantUtils;
 import org.sonar.java.checks.helpers.IdentifierUtils;
@@ -47,9 +49,12 @@ import org.sonar.plugins.java.api.tree.VariableTree;
 @Rule(key = "S3330")
 public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
   private final List<Symbol.VariableSymbol> compliantConstructorInitializations = Lists.newArrayList();
+  private final List<Symbol.VariableSymbol> ignoredVariables = Lists.newArrayList();
   private final List<Symbol.VariableSymbol> variablesToReport = Lists.newArrayList();
   private final List<MethodInvocationTree> settersToReport = Lists.newArrayList();
   private final List<NewClassTree> newClassToReport = Lists.newArrayList();
+
+  private static final List<String> IGNORED_COOKIE_NAMES = Collections.singletonList("XSRF-TOKEN");
 
   private static final String CONSTRUCTOR = "<init>";
   private static final String JAVA_LANG_STRING = "java.lang.String";
@@ -58,6 +63,8 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
   private static final String BOOLEAN = "boolean";
 
   private static final String MESSAGE = "Add the \"HttpOnly\" cookie attribute.";
+
+  private static final int COOKIE_NAME_ARGUMENT = 0;
 
   private static final class ClassName {
     private static final String SERVLET_COOKIE = "javax.servlet.http.Cookie";
@@ -72,34 +79,38 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
   private static final List<String> SETTER_NAMES = Arrays.asList("setHttpOnly", "withHttpOnly");
 
   private static final List<String> CLASSES = Arrays.asList(
-        ClassName.SERVLET_COOKIE,
-        ClassName.NET_HTTP_COOKIE,
-        ClassName.JAX_RS_COOKIE,
-        ClassName.SHIRO_COOKIE,
-        ClassName.PLAY_COOKIE,
-        ClassName.PLAY_COOKIE_BUILDER);
+    ClassName.SERVLET_COOKIE,
+    ClassName.NET_HTTP_COOKIE,
+    ClassName.JAX_RS_COOKIE,
+    ClassName.SHIRO_COOKIE,
+    ClassName.PLAY_COOKIE,
+    ClassName.PLAY_COOKIE_BUILDER);
+
+  private static final MethodMatcher PLAY_COOKIE_BUILDER = MethodMatcher.create()
+    .typeDefinition(ClassName.PLAY_COOKIE).name("builder").withAnyParameters();
 
   private static final List<MethodMatcher> CONSTRUCTORS_WITH_HTTP_ONLY_PARAM = Arrays.asList(
-        MethodMatcher.create()
-          .typeDefinition(TypeCriteria.subtypeOf(ClassName.JAX_RS_NEW_COOKIE)).name(CONSTRUCTOR)
-          .parameters(ClassName.JAX_RS_COOKIE, JAVA_LANG_STRING, INT, JAVA_UTIL_DATE, BOOLEAN, BOOLEAN),
-        MethodMatcher.create()
-          .typeDefinition(TypeCriteria.subtypeOf(ClassName.JAX_RS_NEW_COOKIE)).name(CONSTRUCTOR)
-          .parameters(JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, INT, JAVA_LANG_STRING, INT, JAVA_UTIL_DATE, BOOLEAN, BOOLEAN),
-        MethodMatcher.create()
-          .typeDefinition(TypeCriteria.subtypeOf(ClassName.JAX_RS_NEW_COOKIE)).name(CONSTRUCTOR)
-          .parameters(JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, INT, BOOLEAN, BOOLEAN),
-        MethodMatcher.create()
-          .typeDefinition(TypeCriteria.subtypeOf(ClassName.PLAY_COOKIE)).name(CONSTRUCTOR)
-          .parameters(JAVA_LANG_STRING, JAVA_LANG_STRING, "java.lang.Integer", JAVA_LANG_STRING, JAVA_LANG_STRING, BOOLEAN, BOOLEAN));
+    MethodMatcher.create()
+      .typeDefinition(TypeCriteria.subtypeOf(ClassName.JAX_RS_NEW_COOKIE)).name(CONSTRUCTOR)
+      .parameters(ClassName.JAX_RS_COOKIE, JAVA_LANG_STRING, INT, JAVA_UTIL_DATE, BOOLEAN, BOOLEAN),
+    MethodMatcher.create()
+      .typeDefinition(TypeCriteria.subtypeOf(ClassName.JAX_RS_NEW_COOKIE)).name(CONSTRUCTOR)
+      .parameters(JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, INT, JAVA_LANG_STRING, INT, JAVA_UTIL_DATE, BOOLEAN, BOOLEAN),
+    MethodMatcher.create()
+      .typeDefinition(TypeCriteria.subtypeOf(ClassName.JAX_RS_NEW_COOKIE)).name(CONSTRUCTOR)
+      .parameters(JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, JAVA_LANG_STRING, INT, BOOLEAN, BOOLEAN),
+    MethodMatcher.create()
+      .typeDefinition(TypeCriteria.subtypeOf(ClassName.PLAY_COOKIE)).name(CONSTRUCTOR)
+      .parameters(JAVA_LANG_STRING, JAVA_LANG_STRING, "java.lang.Integer", JAVA_LANG_STRING, JAVA_LANG_STRING, BOOLEAN, BOOLEAN));
 
   private static final List<MethodMatcher> CONSTRUCTORS_WITH_GOOD_DEFAULT = Arrays.asList(
-      MethodMatcher.create().typeDefinition(TypeCriteria.subtypeOf(ClassName.SHIRO_COOKIE)).name(CONSTRUCTOR).withoutParameter(),
-      MethodMatcher.create().typeDefinition(TypeCriteria.subtypeOf(ClassName.SHIRO_COOKIE)).name(CONSTRUCTOR).parameters(JAVA_LANG_STRING));
+    MethodMatcher.create().typeDefinition(TypeCriteria.subtypeOf(ClassName.SHIRO_COOKIE)).name(CONSTRUCTOR).withoutParameter(),
+    MethodMatcher.create().typeDefinition(TypeCriteria.subtypeOf(ClassName.SHIRO_COOKIE)).name(CONSTRUCTOR).parameters(JAVA_LANG_STRING));
 
   @Override
   public void scanFile(JavaFileScannerContext context) {
     compliantConstructorInitializations.clear();
+    ignoredVariables.clear();
     variablesToReport.clear();
     settersToReport.clear();
     newClassToReport.clear();
@@ -121,19 +132,19 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
   @Override
   public List<Tree.Kind> nodesToVisit() {
     return ImmutableList.of(
-        Tree.Kind.VARIABLE,
-        Tree.Kind.ASSIGNMENT,
-        Tree.Kind.METHOD_INVOCATION,
-        Tree.Kind.RETURN_STATEMENT);
+      Tree.Kind.VARIABLE,
+      Tree.Kind.ASSIGNMENT,
+      Tree.Kind.METHOD_INVOCATION,
+      Tree.Kind.RETURN_STATEMENT);
   }
 
   @Override
   public void visitNode(Tree tree) {
     if (hasSemantic()) {
       if (tree.is(Tree.Kind.VARIABLE)) {
-        categorizeBasedOnConstructor((VariableTree) tree);
+        checkVariableDeclaration((VariableTree) tree);
       } else if (tree.is(Tree.Kind.ASSIGNMENT)) {
-        categorizeBasedOnConstructor((AssignmentExpressionTree) tree);
+        checkAssignment((AssignmentExpressionTree) tree);
       } else if (tree.is(Tree.Kind.METHOD_INVOCATION)) {
         checkSetterInvocation((MethodInvocationTree) tree);
       } else {
@@ -142,17 +153,38 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
     }
   }
 
-  private void categorizeBasedOnConstructor(VariableTree declaration) {
-    if (shouldVerify(declaration)) {
-      categorizeBasedOnConstructor((NewClassTree) declaration.initializer(),
-          (VariableSymbol) declaration.symbol());
+  private void checkAssignment(AssignmentExpressionTree assignment) {
+    checkCookieBuilder(assignment);
+    if (shouldVerify(assignment)) {
+      categorizeBasedOnConstructor((NewClassTree) assignment.expression(),
+        (VariableSymbol) ((IdentifierTree) assignment.variable()).symbol());
     }
   }
 
-  private void categorizeBasedOnConstructor(AssignmentExpressionTree assignment) {
-    if (shouldVerify(assignment)) {
-      categorizeBasedOnConstructor((NewClassTree) assignment.expression(),
-          (VariableSymbol) ((IdentifierTree) assignment.variable()).symbol());
+  private void checkVariableDeclaration(VariableTree declaration) {
+    checkCookieBuilder(declaration);
+    if (shouldVerify(declaration)) {
+      categorizeBasedOnConstructor((NewClassTree) declaration.initializer(),
+        (VariableSymbol) declaration.symbol());
+    }
+  }
+
+  private void checkCookieBuilder(AssignmentExpressionTree assignment) {
+    if (assignment.expression().is(Tree.Kind.METHOD_INVOCATION)
+      && assignment.variable().is(Tree.Kind.IDENTIFIER)) {
+      MethodInvocationTree mit = (MethodInvocationTree) assignment.expression();
+      if (PLAY_COOKIE_BUILDER.matches(mit) && isIgnoredCookieName(mit.arguments())) {
+        ignoredVariables.add((VariableSymbol) ((IdentifierTree) assignment.variable()).symbol());
+      }
+    }
+  }
+
+  private void checkCookieBuilder(VariableTree declaration) {
+    if (declaration.initializer() != null && declaration.initializer().is(Tree.Kind.METHOD_INVOCATION)) {
+      MethodInvocationTree mit = (MethodInvocationTree) declaration.initializer();
+      if (PLAY_COOKIE_BUILDER.matches(mit) && isIgnoredCookieName(mit.arguments())) {
+        ignoredVariables.add((VariableSymbol) declaration.symbol());
+      }
     }
   }
 
@@ -160,14 +192,16 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
     ExpressionTree returnedExpression = returnStatement.expression();
     if (returnedExpression != null && returnedExpression.is(Tree.Kind.NEW_CLASS)) {
       NewClassTree newClass = (NewClassTree) returnedExpression;
-      if (!isCompliantConstructorCall(newClass) && CLASSES.stream().anyMatch(newClass.symbolType()::isSubtypeOf)) {
+      if (!isIgnoredCookieName(newClass.arguments()) && !isCompliantConstructorCall(newClass) && CLASSES.stream().anyMatch(newClass.symbolType()::isSubtypeOf)) {
         newClassToReport.add(newClass);
       }
     }
   }
 
   private void categorizeBasedOnConstructor(NewClassTree newClassTree, VariableSymbol variableSymbol) {
-    if (isCompliantConstructorCall(newClassTree)) {
+    if (isIgnoredCookieName(newClassTree.arguments())) {
+      ignoredVariables.add(variableSymbol);
+    } else if (isCompliantConstructorCall(newClassTree)) {
       compliantConstructorInitializations.add(variableSymbol);
     } else {
       variablesToReport.add(variableSymbol);
@@ -178,7 +212,7 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
     ExpressionTree initializer = variableDeclaration.initializer();
     if (initializer != null && initializer.is(Tree.Kind.NEW_CLASS)) {
       boolean isSupportedClass = CLASSES.stream().anyMatch(variableDeclaration.type().symbolType()::isSubtypeOf)
-          || CLASSES.stream().anyMatch(initializer.symbolType()::isSubtypeOf);
+        || CLASSES.stream().anyMatch(initializer.symbolType()::isSubtypeOf);
       return variableDeclaration.symbol().owner().isMethodSymbol() && isSupportedClass;
     }
     return false;
@@ -188,9 +222,9 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
     if (assignment.expression().is(Tree.Kind.NEW_CLASS) && assignment.variable().is(Tree.Kind.IDENTIFIER)) {
       IdentifierTree identifier = (IdentifierTree) assignment.variable();
       boolean isMethodVariable = identifier.symbol().isVariableSymbol()
-          && identifier.symbol().owner().isMethodSymbol();
+        && identifier.symbol().owner().isMethodSymbol();
       boolean isSupportedClass = CLASSES.stream().anyMatch(identifier.symbolType()::isSubtypeOf)
-          || CLASSES.stream().anyMatch(assignment.expression().symbolType()::isSubtypeOf);
+        || CLASSES.stream().anyMatch(assignment.expression().symbolType()::isSubtypeOf);
       return isMethodVariable && isSupportedClass;
     }
     return false;
@@ -204,6 +238,15 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
     } else {
       return CONSTRUCTORS_WITH_GOOD_DEFAULT.stream().anyMatch(matcher -> matcher.matches(newClassTree));
     }
+  }
+
+  private static boolean isIgnoredCookieName(Arguments arguments) {
+    if (arguments.isEmpty()) {
+      return false;
+    }
+    ExpressionTree nameArgument = arguments.get(COOKIE_NAME_ARGUMENT);
+    String name = IdentifierUtils.getValue(nameArgument, ConstantUtils::resolveAsStringConstant);
+    return name != null && IGNORED_COOKIE_NAMES.contains(name);
   }
 
   private void checkSetterInvocation(MethodInvocationTree mit) {
@@ -225,14 +268,38 @@ public class CookieHttpOnlyCheck extends IssuableSubscriptionVisitor {
 
   private static boolean isExpectedSetter(MethodInvocationTree mit) {
     return mit.arguments().size() == 1
-        && mit.symbol().isMethodSymbol()
-        && CLASSES.stream().anyMatch(mit.symbol().owner().type()::isSubtypeOf)
-        && SETTER_NAMES.contains(getIdentifier(mit).name());
+      && mit.symbol().isMethodSymbol()
+      && CLASSES.stream().anyMatch(mit.symbol().owner().type()::isSubtypeOf)
+      && SETTER_NAMES.contains(getIdentifier(mit).name())
+      && isIgnoredBuilder(mit);
+  }
+
+  private static boolean isIgnoredBuilder(MethodInvocationTree mit) {
+    if (!mit.symbol().owner().type().isSubtypeOf(ClassName.PLAY_COOKIE_BUILDER)) {
+      return true;
+    }
+    return getMethodChain(mit)
+      .filter(method -> "builder".contains(getIdentifier(method).name()))
+      .noneMatch(method -> isIgnoredCookieName(method.arguments()));
+  }
+
+  private static Stream<MethodInvocationTree> getMethodChain(MethodInvocationTree mit) {
+    if (mit.methodSelect().is(Tree.Kind.MEMBER_SELECT)) {
+      ExpressionTree expressionTree = ((MemberSelectExpressionTree) mit.methodSelect()).expression();
+      if (expressionTree.is(Tree.Kind.METHOD_INVOCATION)) {
+        return Stream.concat(Stream.of(mit), getMethodChain((MethodInvocationTree) expressionTree));
+      }
+    }
+    return Stream.of(mit);
   }
 
   private void updateIssuesToReport(MethodInvocationTree mit) {
     MemberSelectExpressionTree mse = (MemberSelectExpressionTree) mit.methodSelect();
     VariableSymbol reference = (VariableSymbol) ((IdentifierTree) mse.expression()).symbol();
+    if (ignoredVariables.contains(reference)) {
+      // ignore XSRF-TOKEN cookies
+      return;
+    }
     if (setterArgumentHasCompliantValue(mit.arguments())) {
       variablesToReport.remove(reference);
     } else if (compliantConstructorInitializations.contains(reference)) {
--- java-checks/src/test/files/checks/security/CookieHttpOnlyCheck.java
+++ java-checks/src/test/files/checks/security/CookieHttpOnlyCheck.java
@@ -11,6 +11,7 @@ import play.mvc.Http.CookieBuilder;
 class S3330 {
 
   private static final boolean FALSE_CONSTANT = false;
+  private static final String XSRF_TOKEN = "XSRF-TOKEN";
 
   Cookie field1 = new Cookie("name", "value"); // FN
   HttpCookie field2 = new HttpCookie("name", "value"); // FN
@@ -145,10 +146,10 @@ class S3330 {
   void playFw() {
     play.mvc.Http.Cookie c1 = new play.mvc.Http.Cookie("1", "2", 3, "4", "5", true, false); // Noncompliant
     play.mvc.Http.Cookie c2 = new play.mvc.Http.Cookie("1", "2", 3, "4", "5", true, true);
-    CookieBuilder cb1 = Cookie.builder("1", "2");
+    play.mvc.Http.CookieBuilder cb1 = play.mvc.Http.Cookie.builder("1", "2");
     cb1.withHttpOnly(false); // Noncompliant
     cb1.withHttpOnly(true); // is ignored, so above is a FN
-    CookieBuilder cb2 = Cookie.builder("1", "2");
+    play.mvc.Http.CookieBuilder cb2 = play.mvc.Http.Cookie.builder("1", "2");
     cb2.withHttpOnly(true);
     play.mvc.Http.Cookie.builder("1", "2")
         .withMaxAge(1)
@@ -166,6 +167,48 @@ class S3330 {
 
   play.mvc.Http.Cookie getC6() {
     return play.mvc.Http.Cookie.builder("theme", "blue").withHttpOnly(false); // Noncompliant
+  }
+
+  // SONARJAVA-2772
+  Cookie xsfrToken() {
+    String cookieName = "XSRF-TOKEN";
+
+    Cookie xsfrToken = new Cookie("XSRF-TOKEN", "value"); // OK, used to implement XSRF
+    xsfrToken.setHttpOnly(false);
+
+    Cookie xsfrToken2 = new Cookie("XSRF-TOKEN", "value");
+    xsfrToken2.setHttpOnly(true);
+
+    Cookie xsfrToken3 = new Cookie("XSRF-TOKEN", "value");
+
+    Cookie xsfrToken4 = new Cookie(XSRF_TOKEN, "value");
+
+    HttpCookie xsfrToken5 = new HttpCookie("XSRF-TOKEN", "value");
+
+    javax.ws.rs.core.Cookie xsfrToken6 = new javax.ws.rs.core.Cookie("XSRF-TOKEN", "value");
+
+    NewCookie xsfrToken7 = new NewCookie("XSRF-TOKEN", "value", "path", "domain", "comment", 1, true);
+
+    SimpleCookie xsfrToken8 = new SimpleCookie("XSRF-TOKEN");
+
+    play.mvc.Http.Cookie xsfrToken9 = new play.mvc.Http.Cookie("XSRF-TOKEN", "2", 3, "4", "5", true, false);
+
+    play.mvc.Http.Cookie.builder("XSRF-TOKEN", "2")
+      .withMaxAge(1)
+      .withPath("x")
+      .withDomain("x")
+      .withSecure(true)
+      .withHttpOnly(false)
+      .build();
+
+    play.mvc.Http.CookieBuilder xsfrToken10;
+    xsfrToken10 = play.mvc.Http.Cookie.builder("XSRF-TOKEN", "2");
+    xsfrToken10.withHttpOnly(false);
+
+    play.mvc.Http.CookieBuilder xsfrToken11 = play.mvc.Http.Cookie.builder("XSRF-TOKEN", "2");
+    xsfrToken11.withHttpOnly(false);
+
+    return new Cookie("XSRF-TOKEN", "value");
   }
 
   void compliant(Cookie c1, HttpCookie c2, javax.ws.rs.core.Cookie c3, NewCookie c4, SimpleCookie c5) {
