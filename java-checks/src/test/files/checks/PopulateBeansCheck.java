955c9a51a727737b4487469ea8bea9caf3fc52a1
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-08-15 16:16:16 +0200

    SONARJAVA-2687 add methods on which to detect issues

--- java-checks/src/test/files/checks/PopulateBeansCheck.java
+++ java-checks/src/test/files/checks/PopulateBeansCheck.java
@@ -13,7 +13,11 @@ class Company {
       map.put(name, request.getParameterValues(name));
     }
     BeanUtils.populate(bean, map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
+    BeanUtils.setProperty(bean, "name", "value"); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     BeanUtilsBean.populate(bean, map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
+    BeanUtilsBean bub = new BeanUtilsBean();
+    bub.setProperty(bean, "name", "value"); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     pa.setPropertyValue("name", bean); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
+    pa.setPropertyValues(map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
   }
 }
