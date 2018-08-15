d7969eea10f18a796358f25dc73884a3be1d3c1b
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-08-15 14:13:46 +0200

    SONARJAVA-2858 FN on S1607 using assumeTrue(false)

--- java-checks/src/main/java/org/sonar/java/checks/IgnoredTestsCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/IgnoredTestsCheck.java
@@ -20,16 +20,33 @@
 package org.sonar.java.checks;
 
 import com.google.common.collect.ImmutableList;
+import java.util.List;
 import org.sonar.check.Rule;
+import org.sonar.java.checks.helpers.ConstantUtils;
+import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.java.matcher.MethodMatcherCollection;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.semantic.SymbolMetadata;
+import org.sonar.plugins.java.api.tree.Arguments;
+import org.sonar.plugins.java.api.tree.BlockTree;
+import org.sonar.plugins.java.api.tree.ExpressionStatementTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
-import java.util.List;
-
 @Rule(key = "S1607")
 public class IgnoredTestsCheck extends IssuableSubscriptionVisitor {
+
+  private static final String ORG_JUNIT_ASSUME = "org.junit.Assume";
+  private static final String BOOLEAN_TYPE = "boolean";
+
+  private static final MethodMatcherCollection ASSUME_METHODS = MethodMatcherCollection.create(
+    MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSUME).name("assumeTrue").parameters(BOOLEAN_TYPE),
+    MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSUME).name("assumeTrue").parameters("java.lang.String", BOOLEAN_TYPE),
+    MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSUME).name("assumeFalse").parameters(BOOLEAN_TYPE),
+    MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSUME).name("assumeFalse").parameters("java.lang.String", BOOLEAN_TYPE)
+  );
+
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
@@ -43,5 +60,22 @@ public class IgnoredTestsCheck extends IssuableSubscriptionVisitor {
     if (ignoreAnnotationValues != null && ignoreAnnotationValues.isEmpty()) {
       reportIssue(methodTree.simpleName(), "Fix or remove this skipped unit test");
     }
+    BlockTree block = methodTree.block();
+    if(block != null) {
+      block.body().stream()
+        .filter(s -> s.is(Tree.Kind.EXPRESSION_STATEMENT))
+        .map(s -> ((ExpressionStatementTree) s).expression())
+        .filter(s -> s.is(Tree.Kind.METHOD_INVOCATION))
+        .map(MethodInvocationTree.class::cast)
+        .filter(ASSUME_METHODS::anyMatch)
+        .filter(IgnoredTestsCheck::hasConstantOppositeArg)
+        .forEach(mit -> reportIssue(mit.methodSelect(), "Fix or remove this skipped unit test"));
+    }
+  }
+
+  private static boolean hasConstantOppositeArg(MethodInvocationTree mit) {
+    Arguments args = mit.arguments();
+    Boolean result = ConstantUtils.resolveAsBooleanConstant(args.get(args.size() - 1));
+    return result != null && !result.equals(mit.symbol().name().contains("True"));
   }
 }
