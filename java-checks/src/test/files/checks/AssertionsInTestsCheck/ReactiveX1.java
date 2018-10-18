899c6f75e14e233d0989040c021e1fc4f85f5b22
Christophe Zürn <christophe.zurn@sonarsource.com>
2018-10-18 15:08:28 +0200

    SONARJAVA-2801 S2699 should not raise an issue when there are ReactiveX assertions in the @Test

---
+++ java-checks/src/test/files/checks/AssertionsInTestsCheck/ReactiveX1.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import org.junit.Test;
+
+import rx.Observable;
+import rx.observers.TestObserver;
+import rx.observers.TestSubscriber;
+
+public class ReactiveX1 {
+
+  @Test
+  public void noAssert() { // Noncompliant
+    Observable<String> observable = Observable.just("string");
+    observable.test();
+  }
+
+  @Test
+  public void assertWithTestMethod() {
+    Observable<String> observable = Observable.just("string");
+    observable
+      .test()
+      .assertCompleted();
+  }
+
+  @Test
+  public void assertWithTestObserver() {
+    TestObserver<String> observer = new TestObserver<>();
+    Observable<String> observable = Observable.just("string");
+
+    observable.subscribe(observer);
+
+    observer.assertTerminalEvent();
+    observer.assertReceivedOnNext(Arrays.asList("string"));
+  }
+
+  @Test
+  public void assertWithTestSubscriber() {
+    TestSubscriber<String> subscriber = new TestSubscriber<>();
+    Observable<String> observable = Observable.just("string");
+
+    observable.subscribe(subscriber);
+
+    subscriber.assertCompleted();
+    subscriber.assertValue("string");
+  }
+
+}
---
+++ java-checks/src/test/files/checks/AssertionsInTestsCheck/ReactiveX2.java
@@ -0,0 +1,44 @@
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.subscribers.TestSubscriber;
+import org.junit.Test;
+
+public class ReactiveX2 {
+
+  @Test
+  public void noAssert() { // Noncompliant
+    Observable<String> observable = Observable.just("string");
+    observable.test();
+  }
+
+  @Test
+  public void assertWithTestMethod() {
+    Observable<String> observable = Observable.just("string");
+    observable
+      .test()
+      .assertSubscribed();
+  }
+
+  @Test
+  public void assertWithTestObserver() {
+    TestObserver<String> observer = new TestObserver<>();
+    Observable<String> observable = Observable.just("string");
+
+    observable.subscribe(observer);
+
+    observer.assertResult("string");
+  }
+
+  @Test
+  public void assertWithTestSubscriber() {
+    TestSubscriber<String> subscriber = new TestSubscriber<>();
+    Flowable<String> flowable = Flowable.just("string");
+
+    flowable.subscribe(subscriber);
+
+    subscriber.assertSubscribed();
+    subscriber.assertValue("string");
+  }
+
+}
