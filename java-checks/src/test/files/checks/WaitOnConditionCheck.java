3c02f7f177dfdbe1f18f413c403869b168abfd11
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-10-19 10:28:59 +0200

    SONARJAVA-2925 Update S1844 to work with method reference

--- java-checks/src/test/files/checks/WaitOnConditionCheck.java
+++ java-checks/src/test/files/checks/WaitOnConditionCheck.java
@@ -6,6 +6,7 @@ class A {
     new C().wait(1);
     new C().wait(1, 3);
     new B().wait();  // Noncompliant [[sc=13;ec=17]] {{The "Condition.await(...)" method should be used instead of "Object.wait(...)"}}
+    Runnable r = new B()::wait; // Noncompliant [[sc=27;ec=31]] {{The "Condition.await(...)" method should be used instead of "Object.wait(...)"}}
     new B().wait(1);  // Noncompliant
     new B().wait(1, 3);  // Noncompliant
   }
