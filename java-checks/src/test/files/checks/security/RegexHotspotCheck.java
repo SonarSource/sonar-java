a6db62dc88d595048d67979eca6b1dc5c8bb98c6
Nicolas PERU <nicolas.peru@sonarsource.com>
2018-10-22 16:16:17 +0200

    SONARJAVA-2898  RSPEC-4784 Using regex is a hotspot

---
+++ java-checks/src/test/files/checks/security/RegexHotspotCheck.java
@@ -0,0 +1,54 @@
+import java.util.regex.Pattern;
+
+class A {
+  String regex; // a regular expression
+  String input; // a user input
+
+  void fun() {
+    input.matches(regex);  // Noncompliant {{Make sure that using a regular expression is safe here.}}
+    Pattern pattern = Pattern.compile(regex);  // Noncompliant
+    pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);  // Noncompliant
+
+    String replacement = "test";
+    input.replaceAll(regex, replacement);  // Noncompliant
+    input.replaceFirst(regex, replacement);  // Noncompliant
+    input.split(regex);  // Noncompliant
+    input.split(regex, 3);  // Noncompliant
+
+    pattern.split("foo"); //compliant, excluded
+    pattern.split("foo", 12); //compliant, excluded
+    java.util.regex.Matcher matcher = pattern.matcher("someinput"); //compliant, excluded
+    matcher.find(12); //compliant, excluded
+
+    String htmlString = "some input";
+    !Pattern.matches(".*<script>.*", htmlString); // Noncompliant, even if the pattern is hard-coded
+  }
+
+  void methodRef() {
+    java.util.function.BiFunction<String, String, String> replaceAll = input::replaceAll; // Noncompliant [[sc=79;ec=89]] {{Make sure that using a regular expression is safe here.}}
+    java.util.function.BiFunction<String, String, String> replaceFirst = input::replaceFirst; // Noncompliant
+    java.util.function.Function<String, String[]> stringFunction = input::split; // Noncompliant
+  }
+}
+
+public class Test implements Serializable {
+  @javax.validation.constraints.Pattern(regexp = ".+@.+") // Noncompliant [[sc=41;ec=57]] {{Make sure that using a regular expression is safe here.}}
+  private String email;
+
+  @javax.validation.constraints.Pattern
+  @javax.validation.constraints.Pattern("arg")
+  private String emailNoPattern; // compliant no regexp param : non compilable example for coverage
+
+  @javax.validation.constraints.Email(regexp = ".+@.+") // Noncompliant
+  private String email2;
+
+  @javax.validation.constraints.Email(messge = "yabadabadoo") // compliant no regexp
+  private String email3;
+
+  @org.hibernate.validator.constraints.URL(regexp = ".*") // Noncompliant
+  private String url;
+
+  @org.hibernate.validator.constraints.URL(message = "hello") // compliant no regexp
+  private String url2;
+  // ...
+}
