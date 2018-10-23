88bac85d544161fda54798eb8456d432d1dfc926
benzonico <nicolas.peru@sonarsource.com>
2018-10-23 10:08:22 +0200

    SONARJAVA-2822 Fix resolution of inner class constructor in method reference (#2244)

--- java-frontend/src/test/files/sym/ConstructorInMethodRef.java
+++ java-frontend/src/test/files/sym/ConstructorInMethodRef.java
@@ -20,5 +20,18 @@ class ConstructorInMethodRef {
     return typeSets.stream().map(set -> set.stream().map(Type::erasure)
       .collect(Collectors.toCollection(() -> new LinkedHashSet<>()))).collect(Collectors.toList());
   }
+  public class Bar {
+    public void print() {
+      java.util.List<String> list = java.util.Arrays.asList("x", "y", "z");
+      java.util.List<Foo> foos = list.stream().map(Foo::new).collect(Collectors.toList());
+      System.out.println(foos.get(0).foo);
+    }
 
+    public class Foo {
+      private String foo;
+      private Foo(String foo) {
+        this.foo = foo;
+      }
+    }
+  }
 }
