5d7ab4ed5192ba9815f71021e145a55bb5b1a0d4
Christophe Zürn <christophe.zurn@sonarsource.com>
2018-10-25 10:53:58 +0200

    SONARJAVA-2906 Update implementation following results on peach

--- java-checks/src/test/files/checks/security/CommandLineArgumentsCheck.java
+++ java-checks/src/test/files/checks/security/CommandLineArgumentsCheck.java
@@ -1,8 +1,7 @@
+import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 class A {
-  interface Foo {} // for coverage
-
   public static void main(String[] arguments) { // Compliant - arguments not used
     String b = "arguments";
   }
@@ -19,7 +18,7 @@ class B {
 }
 
 class C {
-  public static void main(String[] arguments) { // Noncompliant [sc=27;ec=45;secondary=21,23] {{Make sure that command line arguments are used safely here.}}
+  public static void main(String[] arguments) { // Noncompliant [[sc=27;ec=45;secondary=22,24]] {{Make sure that command line arguments are used safely here.}}
     String[] b = arguments;
     // ...
     doStuff(arguments);
@@ -29,14 +28,41 @@ class C {
 }
 
 class D {
-  @Option(name="-name",usage="Sets a name") // Noncompliant {{Make sure that command line arguments are used safely here.}}
+  interface Foo {} // for coverage
+
+  @Argument(name="-name",usage="Sets a name")
+  public String name;
+
+  @Option(name="-lastName")
+  public String lastName;
+
+  private String nonAnnotatedMember;
+
+  @Option(name="-file")
+  public void setFile(File f, String other) { }
+
+  // Compliant - we don't raise issues on annotated parameters as it has no impact
+  public void setOtherArgs(@org.kohsuke.args4j.Option(name="-another1") String other1, @Option(name="-another2") String other2) { }
+
+  public void setName() { }
+
+  public void run() { } // Noncompliant [[sc=15;ec=18;secondary=33,36,41]] {{Make sure that command line arguments are used safely here.}}
+}
+
+class E {
+  // Compliant - here as there is no 'run' method inside the class
+
+  @Argument(name="-name",usage="Sets a name")
   public String name;
 
   @Option(name="-file")
-  public void setFile(File f, // Noncompliant [[sc=15;ec=22]]
-                      @Option(name="-other") String other) { } // Noncompliant [[sc=23;ec=58]]
+  public void setFile(File f, String other) { }
 
-  public void setOtherArgs(@org.kohsuke.args4j.Option(name="-another1") String other1, // Noncompliant
-                           @Option(name="-another2") String other2) // Noncompliant
-  { }
+  public void setOtherArgs(@org.kohsuke.args4j.Option(name="-another1") String other1, @Option(name="-another2") String other2) { }
+}
+
+class F {
+  public void setName() { }
+
+  public void run() { }
 }
