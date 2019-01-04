f179c6483d8d18ae36faccc47b8f9a025faa0ec8
Amelie Renard <amelie.renard@sonarsource.com>
2019-01-04 15:46:26 +0100

    Add test cases CastArithmeticOperandCheck

--- java-checks/src/test/files/checks/CastArithmeticOperandCheck.java
+++ java-checks/src/test/files/checks/CastArithmeticOperandCheck.java
@@ -1,10 +1,16 @@
 class A {
+
+  A(int a, long l) {}
+
   void foo() {
+    A a = new A(1 + 2, 1 + 2); // Noncompliant {{Cast one of the operands of this addition operation to a "long".}}
     long l1 = 1000 * 3600 * 24 * 365; // Noncompliant [[sc=32;ec=33]] {{Cast one of the operands of this multiplication operation to a "long".}}
+    l1 += 10 + 2;
     long l2 = 1000L * 3600 * 24 * 365;
     float f = 4L / 3; // Noncompliant {{Cast one of the operands of this division operation to a "float".}}
     float f1 = 2 / 3; // Noncompliant {{Cast one of the operands of this division operation to a "float".}}
     float f2 = 2f / 3;
+    float f3 = 1 / 2 / 2 * 0.5f; // Noncompliant
     l2 = 1000 * 3600 * 24 * 365; // Noncompliant {{Cast one of the operands of this multiplication operation to a "long".}}
     l2 = 1000L * 3600 * 24 * 365; // compliant
     double d = 2 / 3; // Noncompliant {{Cast one of the operands of this division operation to a "double".}}
