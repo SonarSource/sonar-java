d881dd0c37c3893f3614bcb64c11887d0722a5b1
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-01-09 09:33:25 +0100

    Update to parent pom 49

--- its/semantic/java-debugging-plugin/src/main/java/org/sonar/samples/java/UnknownConstructorCallCheck.java
+++ its/semantic/java-debugging-plugin/src/main/java/org/sonar/samples/java/UnknownConstructorCallCheck.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Java
- * Copyright (C) 2017-2018 SonarSource SA
+ * Copyright (C) 2017-2019 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
