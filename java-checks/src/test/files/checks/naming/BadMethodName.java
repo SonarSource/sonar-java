711e5f9a963dcbcc4558263d60c8c6a6ca1c1204
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-01-15 16:52:03 +0100

    SONARJAVA-3008 S100 should not raise issues on test methods

--- java-checks/src/test/files/checks/naming/BadMethodName.java
+++ java-checks/src/test/files/checks/naming/BadMethodName.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 class BadMethodName {
   public BadMethodName() {
   }
@@ -19,4 +21,20 @@ class BadMethodName {
   public String toString() { //Overrides from object
     return "...";
   }
+
+  @Test
+  public void my_test_1() { } // Compliant
+
+  @org.junit.jupiter.api.Test
+  public void my_test_2() { } // Compliant
+
+  @org.testng.annotations.Test
+  public void my_test_3() { } // Compliant
+
+  @org.foo.bar.Test
+  public void not_a_recognized_test() { } // Compliant
+
+  @org.foo.bar.Test2
+  public void not_a_recognized_test() { } // Noncompliant
+
 }
