0afba6469cff7051de52a2bc7aec984d552dd15a
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-01-18 13:54:37 +0100

    Revert "SONARJAVA-3008 S100 should not raise issues on test methods"
    This reverts commit cbda8efb9a7d2ab9d291f96bade8cd954cb6891c.

--- java-checks/src/test/files/checks/naming/BadMethodName.java
+++ java-checks/src/test/files/checks/naming/BadMethodName.java
@@ -1,5 +1,3 @@
-import org.junit.Test;
-
 class BadMethodName {
   public BadMethodName() {
   }
@@ -21,20 +19,4 @@ class BadMethodName {
   public String toString() { //Overrides from object
     return "...";
   }
-
-  @Test
-  public void my_test_1() { } // Compliant
-
-  @org.junit.jupiter.api.Test
-  public void my_test_2() { } // Compliant
-
-  @org.testng.annotations.Test
-  public void my_test_3() { } // Compliant
-
-  @org.foo.bar.Test
-  public void not_a_recognized_test() { } // Compliant
-
-  @org.foo.bar.Test2
-  public void not_a_recognized_test() { } // Noncompliant
-
 }
