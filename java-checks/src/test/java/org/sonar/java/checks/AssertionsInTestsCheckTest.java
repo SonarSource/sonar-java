eac41d13aec8b1baa63b3cb0346b2be2af0fc898
Christophe Zurn <christophe.zurn@sonarsource.com>
2019-02-13 00:08:19 +0100

    SONARJAVA-3034 Rule S2699: add support for JUnit 5 Jupiter

--- java-checks/src/test/java/org/sonar/java/checks/AssertionsInTestsCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/AssertionsInTestsCheckTest.java
@@ -43,6 +43,7 @@ public class AssertionsInTestsCheckTest {
     return Arrays.asList(new Object[][] {
       {"Junit3"},
       {"Junit4"},
+      {"Junit5"},
       {"AssertJ"},
       {"Hamcrest"},
       {"Spring"},
