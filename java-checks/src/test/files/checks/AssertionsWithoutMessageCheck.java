ec72ad08faf7709b422d20cdb91a44ef6a054dfb
Christophe Zurn <christophe.zurn@sonarsource.com>
2019-02-12 22:56:07 +0100

    SONARJAVA-3034 Rule S2698: add support for JUnit 5 Jupiter

--- java-checks/src/test/files/checks/AssertionsWithoutMessageCheck.java
+++ java-checks/src/test/files/checks/AssertionsWithoutMessageCheck.java
@@ -1,7 +1,5 @@
 import org.fest.assertions.GenericAssert;
 
-import java.lang.Integer;
-
 class A {
   void foo() {
     org.junit.Assert.assertTrue(true); // Noncompliant [[sc=5;ec=38]] {{Add a message to this assertion.}}
@@ -40,10 +38,62 @@ class A {
     junit.framework.Assert.fail("Foo");
     org.fest.assertions.Fail.fail(); // Noncompliant
     org.fest.assertions.Fail.fail("foo");
-    org.fest.assertions.Fail.fail("foo",  null);
+    org.fest.assertions.Fail.fail("foo", null);
     org.fest.assertions.Fail.failure("foo");
   }
-  
+
+  void junit5() {
+    org.junit.jupiter.api.Assertions.assertAll(null);
+    org.junit.jupiter.api.Assertions.assertLinesMatch(null, null);
+    org.junit.jupiter.api.Assertions.fail(() -> "message");
+    org.junit.jupiter.api.Assertions.fail(new java.lang.RuntimeException());
+
+    org.junit.jupiter.api.Assertions.assertFalse(false); // Noncompliant [[sc=5;ec=56]] {{Add a message to this assertion.}}
+    org.junit.jupiter.api.Assertions.assertFalse(false, "message");
+    org.junit.jupiter.api.Assertions.assertTrue(false); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertTrue(false, () -> "message");
+    org.junit.jupiter.api.Assertions.assertNull(null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertNull(null, "message");
+    org.junit.jupiter.api.Assertions.assertNotNull(null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertNotNull(null, "message");
+
+    org.junit.jupiter.api.Assertions.assertIterableEquals(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertIterableEquals(null, null, "message");
+    org.junit.jupiter.api.Assertions.assertIterableEquals(null, null, () -> "messageSupplier");
+    org.junit.jupiter.api.Assertions.assertNotEquals(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertNotEquals(null, null, "message");
+    org.junit.jupiter.api.Assertions.assertNotSame(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertNotSame(null, null, "message");
+
+    org.junit.jupiter.api.Assertions.assertSame(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertSame(null, null, "message");
+
+    org.junit.jupiter.api.Assertions.assertThrows(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertThrows(null, null, () -> "message");
+
+    org.junit.jupiter.api.Assertions.assertTimeout(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertTimeout(null, null, "message");
+
+    org.junit.jupiter.api.Assertions.assertTimeoutPreemptively(null, null); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertTimeoutPreemptively(null, null, () -> "message");
+
+
+    org.junit.jupiter.api.Assertions.assertArrayEquals(new boolean[0], new boolean[0]); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertArrayEquals(new boolean[0], new boolean[0], "message");
+    org.junit.jupiter.api.Assertions.assertArrayEquals(new float[0], new float[0]); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertArrayEquals(new float[0], new float[0], 1f); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertArrayEquals(new float[0], new float[0], 1f, "message");
+    org.junit.jupiter.api.Assertions.assertArrayEquals(new float[0], new float[0], "message");
+
+    byte b = 0;
+    org.junit.jupiter.api.Assertions.assertEquals(b, b); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertEquals(b, b, () -> "messageSupplier");
+    org.junit.jupiter.api.Assertions.assertEquals(1.0, 2.0); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertEquals(1.0, 2.0, 0.1); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertEquals(1.0, 2.0, () -> "messageSupplier");
+    org.junit.jupiter.api.Assertions.assertEquals(1.0, 2.0, 1.0, () -> "messageSupplier");
+  }
+
   class MyCustomGenericAssert extends GenericAssert<String, String> {
 
     protected MyCustomGenericAssert(Class<String> selfType, String actual) {
