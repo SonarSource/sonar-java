2101933f40d939b58c5a5f78d982ee4be364a394
Christophe Zürn <christophe.zurn@sonarsource.com>
2019-02-12 16:41:23 +0100

    SONARJAVA-3034 Rule S2701: add support for JUnit 5 Jupiter

--- java-checks/src/test/files/checks/BooleanLiteralInAssertionsCheck.java
+++ java-checks/src/test/files/checks/BooleanLiteralInAssertionsCheck.java
@@ -7,6 +7,13 @@ class A {
     org.junit.Assert.assertTrue(1 > 2);
     org.junit.Assert.assertFalse(false); // Noncompliant
     org.junit.Assert.assertFalse("message", false); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertTrue(true); // Noncompliant [[sc=49;ec=53]] {{Remove or correct this assertion.}}
+    org.junit.jupiter.api.Assertions.assertFalse(true); // Noncompliant
+    org.junit.jupiter.api.Assertions.assertTrue(getBool());
+    org.junit.jupiter.api.Assertions.assertFalse(getBool());
+    org.junit.jupiter.api.Assertions.assertEquals(true, getBool()); // Noncompliant [[sc=51;ec=55]]
+    org.junit.jupiter.api.Assertions.assertSame(getBool(), false); // Noncompliant [[sc=60;ec=65]]
+
     junit.framework.Assert.assertTrue(true); // Noncompliant
     junit.framework.Assert.assertTrue("message", true); // Noncompliant
     junit.framework.Assert.assertNull("message", true); // Noncompliant
@@ -21,6 +28,10 @@ class A {
     org.junit.Assert.assertThat(true, null); // Noncompliant
     org.junit.Assert.assertThat("", not(false)); // Compliant
     new junit.framework.TestCase().assertTrue(true); // Noncompliant
+  }
+
+  boolean getBool() {
+    return true;
   }
 
 }
