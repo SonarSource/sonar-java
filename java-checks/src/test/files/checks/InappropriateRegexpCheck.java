0b6ee3052df28a8c6f71dbcd9a496e3b1698e19a
benzonico <nicolas.peru@sonarsource.com>
2019-02-12 16:40:22 +0100

    SONARJAVA-3032 support replaceFirst in S2639 (#2429)

--- java-checks/src/test/files/checks/InappropriateRegexpCheck.java
+++ java-checks/src/test/files/checks/InappropriateRegexpCheck.java
@@ -1,10 +1,17 @@
 import java.io.File;
 class A {
-  void foo() {
+  void replaceAll() {
     "".replaceAll(".", ""); // Noncompliant [[sc=19;ec=22]] {{Correct this regular expression.}}
     "".replaceAll("|", "_");// Noncompliant [[sc=19;ec=22]] {{Correct this regular expression.}}
     "".replaceAll(File.separator, ""); // Noncompliant [[sc=19;ec=33]] {{Correct this regular expression.}}
     "".replaceAll("\\.", "");
     "".replaceAll("\\|", "");
+  }
+  void replaceFirst() {
+    "".replaceFirst(".", ""); // Noncompliant [[sc=21;ec=24]] {{Correct this regular expression.}}
+    "".replaceFirst("|", "_");// Noncompliant [[sc=21;ec=24]] {{Correct this regular expression.}}
+    "".replaceFirst(File.separator, ""); // Noncompliant [[sc=21;ec=35]] {{Correct this regular expression.}}
+    "".replaceFirst("\\.", "");
+    "".replaceFirst("\\|", "");
   }
 }
