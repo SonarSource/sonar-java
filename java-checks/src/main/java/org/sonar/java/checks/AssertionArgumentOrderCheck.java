f9a7a21f334504baef7aa3843090e67799b698fc
Christophe Zürn <christophe.zurn@sonarsource.com>
2019-02-12 11:03:03 +0100

    SONARJAVA-3034 Rule S3415: add support for JUnit 5 Jupiter

--- java-checks/src/main/java/org/sonar/java/checks/AssertionArgumentOrderCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/AssertionArgumentOrderCheck.java
@@ -22,6 +22,8 @@ package org.sonar.java.checks;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.methods.AbstractMethodDetection;
 import org.sonar.java.matcher.MethodMatcher;
+import org.sonar.java.matcher.MethodMatcherCollection;
+import org.sonar.java.matcher.NameCriteria;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.semantic.Symbol;
 import org.sonar.plugins.java.api.semantic.Type;
@@ -29,6 +31,7 @@ import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.IdentifierTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.NewArrayTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 import java.util.Arrays;
@@ -39,23 +42,62 @@ import java.util.List;
 public class AssertionArgumentOrderCheck extends AbstractMethodDetection {
 
   private static final String ORG_JUNIT_ASSERT = "org.junit.Assert";
+  private static final String ORG_JUNIT5_ASSERTIONS = "org.junit.jupiter.api.Assertions";
   private static final Tree.Kind[] LITERAL_KINDS = {Tree.Kind.STRING_LITERAL, Tree.Kind.INT_LITERAL, Tree.Kind.LONG_LITERAL, Tree.Kind.CHAR_LITERAL,
     Tree.Kind.NULL_LITERAL, Tree.Kind.BOOLEAN_LITERAL, Tree.Kind.DOUBLE_LITERAL, Tree.Kind.FLOAT_LITERAL};
+  private static final String MESSAGE = "Swap these 2 arguments so they are in the correct order: expected value, actual value.";
+
+  private static final MethodMatcherCollection COLLECTION_CREATION_CALL = MethodMatcherCollection.create(
+    MethodMatcher.create().typeDefinition("java.util.Collections").name(NameCriteria.startsWith("singleton")).withAnyParameters(),
+    MethodMatcher.create().typeDefinition("java.util.Collections").name(NameCriteria.startsWith("empty")).withAnyParameters(),
+    MethodMatcher.create().typeDefinition("java.util.Arrays").name("asList").withAnyParameters());
 
   @Override
   protected List<MethodMatcher> getMethodInvocationMatchers() {
     return Arrays.asList(MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSERT).name("assertEquals").withAnyParameters(),
       MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSERT).name("assertSame").withAnyParameters(),
-      MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSERT).name("assertNotSame").withAnyParameters());
+      MethodMatcher.create().typeDefinition(ORG_JUNIT_ASSERT).name("assertNotSame").withAnyParameters(),
+      // JUnit 5
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertArrayEquals").withAnyParameters(),
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertEquals").withAnyParameters(),
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertIterableEquals").withAnyParameters(),
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertLinesMatch").withAnyParameters(),
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertNotEquals").withAnyParameters(),
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertNotSame").withAnyParameters(),
+      MethodMatcher.create().typeDefinition(ORG_JUNIT5_ASSERTIONS).name("assertSame").withAnyParameters());
   }
 
   @Override
   protected void onMethodInvocationFound(MethodInvocationTree mit) {
-    ExpressionTree argToCheck = getActualArgument(mit);
-    if (isConstant(argToCheck)) {
-      List<JavaFileScannerContext.Location> secondaries = Collections.singletonList(new JavaFileScannerContext.Location("", previousArg(argToCheck, mit)));
-      context.reportIssue(this, argToCheck, "Swap these 2 arguments so they are in the correct order: expected value, actual value.", secondaries, null);
+    if (mit.symbol().owner().type().is(ORG_JUNIT5_ASSERTIONS)) {
+      checkArguments(mit.arguments().get(0), mit.arguments().get(1));
+    } else {
+      ExpressionTree argToCheck = getActualArgument(mit);
+      checkArguments(previousArg(argToCheck, mit), argToCheck);
     }
+  }
+
+  private void checkArguments(Tree expectedArgument, ExpressionTree actualArgument) {
+    if (isConstant(actualArgument) || isNewArrayWithConstants(actualArgument) || isCollectionCreationWithConstants(actualArgument)) {
+      List<JavaFileScannerContext.Location> secondaries = Collections.singletonList(new JavaFileScannerContext.Location("", expectedArgument));
+      context.reportIssue(this, actualArgument, MESSAGE, secondaries, null);
+    }
+  }
+
+  private static boolean isNewArrayWithConstants(ExpressionTree actualArgument) {
+    if (actualArgument.is(Tree.Kind.NEW_ARRAY)) {
+      NewArrayTree newArrayTree = (NewArrayTree) actualArgument;
+      return newArrayTree.initializers().stream().allMatch(AssertionArgumentOrderCheck::isConstant);
+    }
+    return false;
+  }
+
+  private static boolean isCollectionCreationWithConstants(ExpressionTree actualArgument) {
+    if (actualArgument.is(Tree.Kind.METHOD_INVOCATION)) {
+      MethodInvocationTree mit = (MethodInvocationTree) actualArgument;
+      return COLLECTION_CREATION_CALL.anyMatch(mit) && mit.arguments().stream().allMatch(AssertionArgumentOrderCheck::isConstant);
+    }
+    return false;
   }
 
   private static Tree previousArg(ExpressionTree argToCheck, MethodInvocationTree mit) {
