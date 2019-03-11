fd3cdf2bfbe578958727f5c2cc38b8e3c8ffca45
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-03-11 10:31:24 +0100

    update travis utils to version 55

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v53 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v55 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
