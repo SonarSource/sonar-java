e021e5d651dfdd7ccbd306b048fa39f8f8cc63e2
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-04-08 15:07:58 +0200

    Use JaCoCo XML report for coverage (travis-utils v56, parent pom v51)

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v55 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v56 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
