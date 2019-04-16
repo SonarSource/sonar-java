985ed1588b290ef67e233ba2a72b473d1152bb3c
vilchik-elena <elena.vilchik@sonarsource.com>
2019-04-16 15:17:05 +0200

    Revert "SONARJAVA-3071 Update security rule activation due to repository change (#2469)"
    This reverts commit 816002c2c98f7397c078cbf62e228753ed14b438.

--- sonar-java-plugin/src/test/java/com/sonar/plugins/security/api/JavaRules.java
+++ sonar-java-plugin/src/test/java/com/sonar/plugins/security/api/JavaRules.java
@@ -28,14 +28,8 @@ import java.util.Set;
 public class JavaRules {
 
   public static Set<String> ruleKeys = new HashSet<>();
-  private static final String REPO_KEY = "javasecurity";
 
-  public static Set<String> getSecurityRuleKeys() {
+  public static Set<String> getRuleKeys() {
     return ruleKeys;
   }
-
-  public static String getRepositoryKey() {
-    return REPO_KEY;
-  }
-
 }
