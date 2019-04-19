SonarSource/sonar-java

commit d1745712febf3bd1d8d48c02eaaa2db5fbcdb0f9

Migrate classes, enums, interfaces, methods and fields

java-squid/src/main/java/org/sonar/java/ast/parser/JavaGrammar.java
@@ -28,16 +28,10 @@ import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
 
 import java.util.Arrays;
 
-import static org.sonar.java.ast.api.JavaKeyword.CLASS;
 import static org.sonar.java.ast.api.JavaKeyword.ENUM;
-import static org.sonar.java.ast.api.JavaKeyword.EXTENDS;
-import static org.sonar.java.ast.api.JavaKeyword.IMPLEMENTS;
 import static org.sonar.java.ast.api.JavaKeyword.IMPORT;
-import static org.sonar.java.ast.api.JavaKeyword.INTERFACE;
 import static org.sonar.java.ast.api.JavaKeyword.PACKAGE;
 import static org.sonar.java.ast.api.JavaKeyword.STATIC;
-import static org.sonar.java.ast.api.JavaKeyword.THROWS;
-import static org.sonar.java.ast.api.JavaKeyword.VOID;
 import static org.sonar.java.ast.api.JavaPunctuator.AND;
 import static org.sonar.java.ast.api.JavaPunctuator.ANDAND;
 import static org.sonar.java.ast.api.JavaPunctuator.ANDEQU;
@@ -293,9 +287,6 @@ public enum JavaGrammar implements GrammarRuleKey {
     keywords(b);
 
     compilationsUnits(b);
-    classDeclaration(b);
-    interfaceDeclarations(b);
-    enums(b);
     blocksAndStatements(b);
     literals(b);
 
@@ -471,84 +462,12 @@ public enum JavaGrammar implements GrammarRuleKey {
 
     b.rule(PACKAGE_DECLARATION).is(b.zeroOrMore(ANNOTATION), PACKAGE, QUALIFIED_IDENTIFIER, SEMI);
     b.rule(IMPORT_DECLARATION).is(IMPORT, b.optional(STATIC), QUALIFIED_IDENTIFIER, b.optional(DOT, STAR), SEMI);
-    b.rule(TYPE_DECLARATION).is(b.firstOf(
-      b.sequence(MODIFIERS, b.firstOf(CLASS_DECLARATION, ENUM_DECLARATION, INTERFACE_DECLARATION, ANNOTATION_TYPE_DECLARATION)),
-      SEMI));
-  }
+    b.rule(TYPE_DECLARATION).is(
+      b.firstOf(
+        b.sequence(MODIFIERS, b.firstOf(CLASS_DECLARATION, ENUM_DECLARATION, INTERFACE_DECLARATION, ANNOTATION_TYPE_DECLARATION)),
+        SEMI));
 
-  /**
-   * 8.1. Class Declaration
-   */
-  private static void classDeclaration(LexerlessGrammarBuilder b) {
-    b.rule(CLASS_DECLARATION).is(
-      CLASS, JavaTokenType.IDENTIFIER, b.optional(TYPE_PARAMETERS),
-      b.optional(EXTENDS, QUALIFIED_IDENTIFIER),
-      b.optional(IMPLEMENTS, QUALIFIED_IDENTIFIER_LIST),
-      CLASS_BODY);
-
-    b.rule(CLASS_BODY).is(LWING, b.zeroOrMore(CLASS_BODY_DECLARATION), RWING);
-    b.rule(CLASS_BODY_DECLARATION).is(b.firstOf(
-      SEMI,
-      CLASS_INIT_DECLARATION,
-      b.sequence(MODIFIERS, MEMBER_DECL)));
-    b.rule(CLASS_INIT_DECLARATION).is(b.optional(STATIC), BLOCK);
-    b.rule(MEMBER_DECL).is(b.firstOf(
-      b.sequence(TYPE_PARAMETERS, GENERIC_METHOD_OR_CONSTRUCTOR_REST),
-      b.sequence(TYPE, JavaTokenType.IDENTIFIER, METHOD_DECLARATOR_REST),
-      FIELD_DECLARATION,
-      b.sequence(VOID, JavaTokenType.IDENTIFIER, VOID_METHOD_DECLARATOR_REST),
-      b.sequence(JavaTokenType.IDENTIFIER, CONSTRUCTOR_DECLARATOR_REST),
-      INTERFACE_DECLARATION,
-      CLASS_DECLARATION,
-      ENUM_DECLARATION,
-      ANNOTATION_TYPE_DECLARATION));
-    b.rule(FIELD_DECLARATION).is(TYPE, VARIABLE_DECLARATORS, SEMI);
-    b.rule(GENERIC_METHOD_OR_CONSTRUCTOR_REST).is(b.firstOf(
-      b.sequence(b.firstOf(TYPE, VOID), JavaTokenType.IDENTIFIER, METHOD_DECLARATOR_REST),
-      b.sequence(JavaTokenType.IDENTIFIER, CONSTRUCTOR_DECLARATOR_REST)));
-    b.rule(METHOD_DECLARATOR_REST).is(FORMAL_PARAMETERS, b.zeroOrMore(b.zeroOrMore(ANNOTATION), DIM), b.optional(THROWS, QUALIFIED_IDENTIFIER_LIST), b.firstOf(METHOD_BODY, SEMI));
-    b.rule(VOID_METHOD_DECLARATOR_REST).is(FORMAL_PARAMETERS, b.optional(THROWS, QUALIFIED_IDENTIFIER_LIST), b.firstOf(METHOD_BODY, SEMI));
-    b.rule(CONSTRUCTOR_DECLARATOR_REST).is(FORMAL_PARAMETERS, b.optional(THROWS, QUALIFIED_IDENTIFIER_LIST), METHOD_BODY);
-    b.rule(METHOD_BODY).is(BLOCK);
-  }
-
-  /**
-   * 8.9. Enums
-   */
-  private static void enums(LexerlessGrammarBuilder b) {
-    b.rule(ENUM_DECLARATION).is(ENUM, JavaTokenType.IDENTIFIER, b.optional(IMPLEMENTS, QUALIFIED_IDENTIFIER_LIST), ENUM_BODY);
-    b.rule(ENUM_BODY).is(LWING, b.optional(ENUM_CONSTANTS), b.optional(COMMA), b.optional(ENUM_BODY_DECLARATIONS), RWING);
-    b.rule(ENUM_CONSTANTS).is(ENUM_CONSTANT, b.zeroOrMore(COMMA, ENUM_CONSTANT));
-    b.rule(ENUM_CONSTANT).is(b.zeroOrMore(ANNOTATION), JavaTokenType.IDENTIFIER, b.optional(ARGUMENTS), b.optional(CLASS_BODY));
-    b.rule(ENUM_BODY_DECLARATIONS).is(SEMI, b.zeroOrMore(CLASS_BODY_DECLARATION));
-  }
-
-  /**
-   * 9.1. Interface Declarations
-   */
-  private static void interfaceDeclarations(LexerlessGrammarBuilder b) {
-    b.rule(INTERFACE_DECLARATION).is(INTERFACE, JavaTokenType.IDENTIFIER, b.optional(TYPE_PARAMETERS), b.optional(EXTENDS, QUALIFIED_IDENTIFIER_LIST), INTERFACE_BODY);
-
-    b.rule(INTERFACE_BODY).is(LWING, b.zeroOrMore(INTERFACE_BODY_DECLARATION), RWING);
-    b.rule(INTERFACE_BODY_DECLARATION).is(b.firstOf(
-      b.sequence(MODIFIERS, INTERFACE_MEMBER_DECL),
-      SEMI));
-    b.rule(INTERFACE_MEMBER_DECL).is(b.firstOf(
-      INTERFACE_METHOD_OR_FIELD_DECL,
-      INTERFACE_GENERIC_METHOD_DECL,
-      b.sequence(VOID, JavaTokenType.IDENTIFIER, VOID_INTERFACE_METHOD_DECLARATORS_REST),
-      INTERFACE_DECLARATION,
-      ANNOTATION_TYPE_DECLARATION,
-      CLASS_DECLARATION,
-      ENUM_DECLARATION));
-    b.rule(INTERFACE_METHOD_OR_FIELD_DECL).is(TYPE, JavaTokenType.IDENTIFIER, INTERFACE_METHOD_OR_FIELD_REST);
-    b.rule(INTERFACE_METHOD_OR_FIELD_REST).is(b.firstOf(
-      b.sequence(CONSTANT_DECLARATORS_REST, SEMI),
-      INTERFACE_METHOD_DECLARATOR_REST));
-    b.rule(INTERFACE_METHOD_DECLARATOR_REST).is(FORMAL_PARAMETERS, b.zeroOrMore(b.zeroOrMore(ANNOTATION), DIM),
-      b.optional(THROWS, QUALIFIED_IDENTIFIER_LIST), b.firstOf(SEMI, METHOD_BODY));
-    b.rule(INTERFACE_GENERIC_METHOD_DECL).is(TYPE_PARAMETERS, b.firstOf(TYPE, VOID), JavaTokenType.IDENTIFIER, INTERFACE_METHOD_DECLARATOR_REST);
-    b.rule(VOID_INTERFACE_METHOD_DECLARATORS_REST).is(FORMAL_PARAMETERS, b.optional(THROWS, QUALIFIED_IDENTIFIER_LIST), b.firstOf(SEMI, METHOD_BODY));
+    // TODO Remove me
     b.rule(CONSTANT_DECLARATORS_REST).is(CONSTANT_DECLARATOR_REST, b.zeroOrMore(COMMA, CONSTANT_DECLARATOR));
     b.rule(CONSTANT_DECLARATOR).is(JavaTokenType.IDENTIFIER, CONSTANT_DECLARATOR_REST);
     b.rule(CONSTANT_DECLARATOR_REST).is(b.zeroOrMore(DIM), EQU, VARIABLE_INITIALIZER);

java-squid/src/main/java/org/sonar/java/ast/parser/TreeFactory.java
@@ -40,6 +40,7 @@ import org.sonar.java.model.KindMaps;
 import org.sonar.java.model.TypeParameterTreeImpl;
 import org.sonar.java.model.declaration.AnnotationTreeImpl;
 import org.sonar.java.model.declaration.ClassTreeImpl;
+import org.sonar.java.model.declaration.EnumConstantTreeImpl;
 import org.sonar.java.model.declaration.MethodTreeImpl;
 import org.sonar.java.model.declaration.ModifierKeywordTreeImpl;
 import org.sonar.java.model.declaration.ModifiersTreeImpl;
@@ -82,10 +83,10 @@ import org.sonar.java.model.statement.TryStatementTreeImpl;
 import org.sonar.java.model.statement.WhileStatementTreeImpl;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.ModifierTree;
+import org.sonar.plugins.java.api.tree.ModifiersTree;
 import org.sonar.plugins.java.api.tree.StatementTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.Tree.Kind;
-import org.sonar.plugins.java.api.tree.TypeParameterTree;
 import org.sonar.plugins.java.api.tree.VariableTree;
 
 import java.util.Collections;
@@ -265,6 +266,303 @@ public class TreeFactory {
 
   // End of types
 
+  // Classes, enums and interfaces
+
+  public ClassTreeImpl completeClassDeclaration(
+    AstNode classTokenAstNode,
+    AstNode identifierAstNode, Optional<TypeParameterListTreeImpl> typeParameters,
+    Optional<Tuple<AstNode, ExpressionTree>> extendsClause,
+    Optional<Tuple<AstNode, QualifiedIdentifierListTreeImpl>> implementsClause,
+    ClassTreeImpl partial) {
+
+    IdentifierTreeImpl identifier = new IdentifierTreeImpl(InternalSyntaxToken.create(identifierAstNode));
+
+    List<AstNode> children = Lists.newArrayList();
+    children.add(classTokenAstNode);
+    children.add(identifier);
+    partial.completeIdentifier(identifier);
+    if (typeParameters.isPresent()) {
+      children.add(typeParameters.get());
+      partial.completeTypeParameters(typeParameters.get());
+    }
+    if (extendsClause.isPresent()) {
+      children.add(extendsClause.get().first());
+      children.add((AstNode) extendsClause.get().second());
+      partial.completeSuperclass(extendsClause.get().second());
+    }
+    if (implementsClause.isPresent()) {
+      children.add(implementsClause.get().first());
+      children.add(implementsClause.get().second());
+      partial.completeInterfaces(implementsClause.get().second());
+    }
+
+    partial.prependChildren(children);
+
+    return partial;
+  }
+
+  private ClassTreeImpl newClassBody(Kind kind, AstNode openBraceTokenAstNode, Optional<List<AstNode>> members, AstNode closeBraceTokenAstNode) {
+    List<AstNode> children = Lists.newArrayList();
+    ImmutableList.Builder<Tree> builder = ImmutableList.builder();
+
+    children.add(openBraceTokenAstNode);
+    if (members.isPresent()) {
+      for (AstNode member : members.get()) {
+        children.add(member);
+
+        if (member instanceof VariableDeclaratorListTreeImpl) {
+          for (VariableTreeImpl variable : (VariableDeclaratorListTreeImpl) member) {
+            builder.add(variable);
+          }
+        } else if (member instanceof Tree) {
+          builder.add((Tree) member);
+        }
+      }
+    }
+    children.add(closeBraceTokenAstNode);
+
+    return new ClassTreeImpl(kind, builder.build(), children);
+  }
+
+  public ClassTreeImpl newClassBody(AstNode openBraceTokenAstNode, Optional<List<AstNode>> members, AstNode closeBraceTokenAstNode) {
+    return newClassBody(Kind.CLASS, openBraceTokenAstNode, members, closeBraceTokenAstNode);
+  }
+
+  public ClassTreeImpl newEnumDeclaration(
+    AstNode enumTokenAstNode,
+    AstNode identifierAstNode,
+    Optional<Tuple<AstNode, QualifiedIdentifierListTreeImpl>> implementsClause,
+    AstNode openBraceTokenAstNode,
+    Optional<List<EnumConstantTreeImpl>> enumConstants,
+    Optional<AstNode> semicolonTokenAstNode,
+    Optional<List<AstNode>> enumDeclarations,
+    AstNode closeBraceTokenAstNode) {
+
+    ImmutableList.Builder<AstNode> members = ImmutableList.<AstNode>builder();
+    if (enumConstants.isPresent()) {
+      for (EnumConstantTreeImpl enumConstant : enumConstants.get()) {
+        members.add(enumConstant);
+      }
+    }
+    if (semicolonTokenAstNode.isPresent()) {
+      // TODO This is a hack
+      members.add(semicolonTokenAstNode.get());
+    }
+    if (enumDeclarations.isPresent()) {
+      for (AstNode enumDeclaration : enumDeclarations.get()) {
+        members.add(enumDeclaration);
+      }
+    }
+
+    ClassTreeImpl result = newClassBody(Kind.ENUM, openBraceTokenAstNode, Optional.of((List<AstNode>) members.build()), closeBraceTokenAstNode);
+
+    List<AstNode> children = Lists.newArrayList();
+    children.add(enumTokenAstNode);
+
+    IdentifierTreeImpl identifier = new IdentifierTreeImpl(InternalSyntaxToken.create(identifierAstNode));
+    result.completeIdentifier(identifier);
+    children.add(identifier);
+
+    if (implementsClause.isPresent()) {
+      children.add(implementsClause.get().first());
+      children.add(implementsClause.get().second());
+
+      result.completeInterfaces(implementsClause.get().second());
+    }
+
+    result.prependChildren(children);
+
+    return result;
+  }
+
+  public EnumConstantTreeImpl newEnumConstant(
+    Optional<List<AnnotationTreeImpl>> annotations, AstNode identifierAstNode,
+    Optional<ArgumentListTreeImpl> arguments,
+    Optional<ClassTreeImpl> classBody,
+    Optional<AstNode> semicolonTokenAstNode) {
+
+    IdentifierTreeImpl identifier = new IdentifierTreeImpl(InternalSyntaxToken.create(identifierAstNode));
+    if (annotations.isPresent()) {
+      identifier.prependChildren(annotations.get());
+    }
+
+    List<AstNode> children = Lists.newArrayList();
+    if (arguments.isPresent()) {
+      children.add(arguments.get());
+    }
+    if (classBody.isPresent()) {
+      children.add(classBody.get());
+    }
+    NewClassTreeImpl newClass = new NewClassTreeImpl(
+      arguments.isPresent() ? arguments.get() : Collections.emptyList(),
+      classBody.isPresent() ? classBody.get() : null,
+      children.toArray(new AstNode[0]));
+    newClass.completeWithIdentifier(identifier);
+
+    EnumConstantTreeImpl result = new EnumConstantTreeImpl(ModifiersTreeImpl.EMPTY, identifier, newClass);
+
+    result.addChild(identifier);
+    result.addChild(newClass);
+    if (semicolonTokenAstNode.isPresent()) {
+      result.addChild(semicolonTokenAstNode.get());
+    }
+
+    return result;
+  }
+
+  public ClassTreeImpl completeInterfaceDeclaration(
+    AstNode interfaceTokenAstNode,
+    AstNode identifierAstNode, Optional<TypeParameterListTreeImpl> typeParameters,
+    Optional<Tuple<AstNode, QualifiedIdentifierListTreeImpl>> extendsClause,
+    ClassTreeImpl partial) {
+
+    IdentifierTreeImpl identifier = new IdentifierTreeImpl(InternalSyntaxToken.create(identifierAstNode));
+
+    List<AstNode> children = Lists.newArrayList();
+    children.add(interfaceTokenAstNode);
+    children.add(identifier);
+    partial.completeIdentifier(identifier);
+    if (typeParameters.isPresent()) {
+      children.add(typeParameters.get());
+      partial.completeTypeParameters(typeParameters.get());
+    }
+    if (extendsClause.isPresent()) {
+      children.add(extendsClause.get().first());
+      children.add(extendsClause.get().second());
+      partial.completeInterfaces(extendsClause.get().second());
+    }
+
+    partial.prependChildren(children);
+
+    return partial;
+  }
+
+  public ClassTreeImpl newInterfaceBody(AstNode openBraceTokenAstNode, Optional<List<AstNode>> members, AstNode closeBraceTokenAstNode) {
+    return newClassBody(Kind.INTERFACE, openBraceTokenAstNode, members, closeBraceTokenAstNode);
+  }
+
+  // TODO Create an intermediate implementation interface for completing modifiers
+  public AstNode completeMember(ModifiersTreeImpl modifiers, JavaTree partial) {
+
+    if (partial instanceof ClassTreeImpl) {
+      ((ClassTreeImpl) partial).completeModifiers(modifiers);
+      partial.prependChildren(modifiers);
+    } else if (partial instanceof VariableDeclaratorListTreeImpl) {
+      for (VariableTreeImpl variable : (VariableDeclaratorListTreeImpl) partial) {
+        variable.completeModifiers(modifiers);
+      }
+      partial.prependChildren(modifiers);
+    } else if (partial instanceof MethodTreeImpl) {
+      ((MethodTreeImpl) partial).completeWithModifiers(modifiers);
+    } else {
+      throw new IllegalArgumentException();
+    }
+
+    return partial;
+  }
+
+  public BlockTreeImpl newInitializerMember(Optional<AstNode> staticTokenAstNode, BlockTreeImpl block) {
+    Kind kind = staticTokenAstNode.isPresent() ? Kind.STATIC_INITIALIZER : Kind.INITIALIZER;
+
+    List<AstNode> children = Lists.newArrayList();
+    if (staticTokenAstNode.isPresent()) {
+      children.add(staticTokenAstNode.get());
+    }
+    children.addAll(block.getChildren());
+
+    return new BlockTreeImpl(kind, (InternalSyntaxToken) block.openBraceToken(), block.body(), (InternalSyntaxToken) block.closeBraceToken(),
+      children.toArray(new AstNode[0]));
+  }
+
+  // TODO Need a proper strongly typed node for this
+  public AstNode newEmptyMember(AstNode semicolonTokenAstNode) {
+    return semicolonTokenAstNode;
+  }
+
+  public MethodTreeImpl completeGenericMethodOrConstructorDeclaration(TypeParameterListTreeImpl typeParameters, MethodTreeImpl partial) {
+    partial.prependChildren((AstNode) typeParameters);
+
+    return partial.completeWithTypeParameters(typeParameters);
+  }
+
+  private MethodTreeImpl newMethodOrConstructor(
+    Optional<ExpressionTree> type, AstNode identifierAstNode, FormalParametersListTreeImpl parameters,
+    Optional<List<Tuple<Optional<List<AnnotationTreeImpl>>, Tuple<AstNode, AstNode>>>> annotatedDimensions,
+    Optional<Tuple<AstNode, QualifiedIdentifierListTreeImpl>> throwsClause,
+    AstNode blockOrSemicolon) {
+
+    IdentifierTreeImpl identifier = new IdentifierTreeImpl(InternalSyntaxToken.create(identifierAstNode));
+
+    ExpressionTree actualType;
+    if (type.isPresent()) {
+      actualType = treeMaker.applyDim(type.get(), annotatedDimensions.isPresent() ? annotatedDimensions.get().size() : 0);
+    } else {
+      actualType = null;
+    }
+
+    MethodTreeImpl result = new MethodTreeImpl(
+      actualType,
+      identifier,
+      (List) parameters,
+      throwsClause.isPresent() ? (List<ExpressionTree>) throwsClause.get().second() : ImmutableList.<ExpressionTree>of(),
+      blockOrSemicolon.is(Kind.BLOCK) ? (BlockTreeImpl) blockOrSemicolon : null);
+
+    List<AstNode> children = Lists.newArrayList();
+    if (type.isPresent()) {
+      children.add((AstNode) type.get());
+    }
+    children.add(identifier);
+    children.add(parameters);
+    if (annotatedDimensions.isPresent()) {
+      for (Tuple<Optional<List<AnnotationTreeImpl>>, Tuple<AstNode, AstNode>> annotatedDimension : annotatedDimensions.get()) {
+        if (annotatedDimension.first().isPresent()) {
+          for (AnnotationTreeImpl annotation : annotatedDimension.first().get()) {
+            children.add(annotation);
+          }
+        }
+        children.add(annotatedDimension.second());
+      }
+    }
+    if (throwsClause.isPresent()) {
+      children.add(throwsClause.get().first());
+      children.add(throwsClause.get().second());
+    }
+    children.add(blockOrSemicolon);
+
+    result.prependChildren(children);
+
+    return result;
+  }
+
+  public MethodTreeImpl newMethod(
+    ExpressionTree type, AstNode identifierAstNode, FormalParametersListTreeImpl parameters,
+    Optional<List<Tuple<Optional<List<AnnotationTreeImpl>>, Tuple<AstNode, AstNode>>>> annotatedDimensions,
+    Optional<Tuple<AstNode, QualifiedIdentifierListTreeImpl>> throwsClause,
+    AstNode blockOrSemicolon) {
+
+    return newMethodOrConstructor(Optional.of(type), identifierAstNode, parameters, annotatedDimensions, throwsClause, blockOrSemicolon);
+  }
+
+  public MethodTreeImpl newConstructor(
+    AstNode identifierAstNode, FormalParametersListTreeImpl parameters,
+    Optional<List<Tuple<Optional<List<AnnotationTreeImpl>>, Tuple<AstNode, AstNode>>>> annotatedDimensions,
+    Optional<Tuple<AstNode, QualifiedIdentifierListTreeImpl>> throwsClause,
+    AstNode blockOrSemicolon) {
+
+    return newMethodOrConstructor(Optional.<ExpressionTree>absent(), identifierAstNode, parameters, annotatedDimensions, throwsClause, blockOrSemicolon);
+  }
+
+  public VariableDeclaratorListTreeImpl completeFieldDeclaration(ExpressionTree type, VariableDeclaratorListTreeImpl partial, AstNode semicolonTokenAstNode) {
+    partial.prependChildren((AstNode) type);
+    for (VariableTreeImpl variable : partial) {
+      variable.completeType(type);
+    }
+    partial.addChild(semicolonTokenAstNode);
+    return partial;
+  }
+
+  // End of classes, enums and interfaces
+
   // Annotations
 
   public ClassTreeImpl completeAnnotationType(AstNode atTokenAstNode, AstNode interfaceTokenAstNode, AstNode identifier, ClassTreeImpl partial) {
@@ -298,19 +596,19 @@ public class TreeFactory {
 
           if (declaration.is(Kind.METHOD)) {
             // method
-            members.add(((MethodTreeImpl) declaration).complete(modifiers));
+            members.add(((MethodTreeImpl) declaration).completeWithModifiers(modifiers));
             children.add(declaration);
           } else if (declaration.is(JavaGrammar.ANNOTATION_TYPE_ELEMENT_REST)) {
             // constant
-            treeMaker.appendConstantDeclarations(ModifiersTreeImpl.EMPTY, members, declaration);
+            appendConstantDeclarations(ModifiersTreeImpl.EMPTY, members, declaration);
             children.add(annotationTypeElementDeclaration);
           } else if (declaration.is(Kind.ANNOTATION_TYPE)) {
             // TODO Complete with modifiers
             members.add((Tree) declaration);
             children.add(annotationTypeElementDeclaration);
-          } else {
+          } else if (declaration.is(Kind.CLASS, Kind.INTERFACE, Kind.ENUM)) {
             // interface, class, enum
-            members.add(modifiers, treeMaker.typeDeclaration(modifiers, declaration));
+            members.add(modifiers, treeMaker.typeDeclaration(modifiers, (ClassTreeImpl) declaration));
             children.add(annotationTypeElementDeclaration);
           }
         } else {
@@ -323,6 +621,25 @@ public class TreeFactory {
     children.add(closeBraceToken);
 
     return new ClassTreeImpl(emptyModifiers, members.build(), children);
+  }
+
+  // TODO Must be deleted
+  private void appendConstantDeclarations(ModifiersTree modifiers, ImmutableList.Builder<Tree> members, AstNode astNode) {
+    AstNode typeAstNode = astNode.getFirstChild(JavaTreeMaker.TYPE_KINDS);
+    if (typeAstNode == null) {
+      typeAstNode = astNode.getFirstChild(JavaKeyword.VOID);
+    }
+    ExpressionTree type = typeAstNode instanceof Tree ? (ExpressionTree) typeAstNode : treeMaker.referenceType(typeAstNode);
+    for (AstNode constantDeclaratorRestNode : astNode.getDescendants(JavaGrammar.CONSTANT_DECLARATOR_REST)) {
+      AstNode identifierNode = constantDeclaratorRestNode.getPreviousAstNode();
+      Preconditions.checkState(identifierNode.is(JavaTokenType.IDENTIFIER));
+      members.add(new VariableTreeImpl(
+        constantDeclaratorRestNode,
+        modifiers,
+        treeMaker.applyDim(type, constantDeclaratorRestNode.getChildren(TreeFactory.WRAPPER_AST_NODE).size()),
+        treeMaker.identifier(identifierNode),
+        (ExpressionTree) constantDeclaratorRestNode.getLastChild()));
+    }
   }
 
   public AstNode completeAnnotationTypeMember(ModifiersTreeImpl modifiers, AstNode annotationTypeElementRest) {
@@ -1579,29 +1896,15 @@ public class TreeFactory {
     return result;
   }
 
-  public NewClassTreeImpl newClassCreatorRest(ArgumentListTreeImpl arguments, Optional<AstNode> classBodyAstNode) {
+  public NewClassTreeImpl newClassCreatorRest(ArgumentListTreeImpl arguments, Optional<ClassTreeImpl> classBody) {
     List<AstNode> children = Lists.newArrayList();
     children.add(arguments);
 
-    ClassTreeImpl classBody = null;
-
-    if (classBodyAstNode.isPresent()) {
-      List<Tree> body = treeMaker.classBody(classBodyAstNode.get());
-      // TODO Legacy node
-      classBody = new ClassTreeImpl(
-        classBodyAstNode.get(),
-        Tree.Kind.CLASS,
-        ModifiersTreeImpl.EMPTY,
-        null,
-        ImmutableList.<TypeParameterTree>of(),
-        null,
-        ImmutableList.<Tree>of(),
-        body);
-
-      children.add(classBodyAstNode.get());
+    if (classBody.isPresent()) {
+      children.add(classBody.get());
     }
 
-    return new NewClassTreeImpl(arguments, classBody,
+    return new NewClassTreeImpl(arguments, classBody.isPresent() ? classBody.get() : null,
       children.toArray(new AstNode[0]));
   }
 
@@ -1844,8 +2147,16 @@ public class TreeFactory {
         Optional opt = (Optional) o;
         if (opt.isPresent()) {
           Object o2 = opt.get();
-          Preconditions.checkArgument(o2 instanceof AstNode, "Unsupported optional type: " + o2.getClass().getSimpleName());
-          addChild((AstNode) o2);
+          if (o2 instanceof AstNode) {
+            addChild((AstNode) o2);
+          } else if (o2 instanceof List) {
+            for (Object o3 : (List) o2) {
+              Preconditions.checkArgument(o3 instanceof AstNode, "Unsupported type: " + o3.getClass().getSimpleName());
+              addChild((AstNode) o3);
+            }
+          } else {
+            throw new IllegalArgumentException("Unsupported type: " + o2.getClass().getSimpleName());
+          }
         }
       } else {
         throw new IllegalStateException("Unsupported argument type: " + o.getClass().getSimpleName());
@@ -1881,5 +2192,47 @@ public class TreeFactory {
   public <T, U> Tuple<T, U> newTuple6(T first, U second) {
     return newTuple(first, second);
   }
+
+  public <T, U> Tuple<T, U> newTuple7(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple8(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple9(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple10(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple11(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple12(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple13(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple14(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple15(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  public <T, U> Tuple<T, U> newTuple16(T first, U second) {
+    return newTuple(first, second);
+  }
+
+  // End
 
 }

java-checks/src/main/java/org/sonar/java/checks/AnonymousClassShouldBeLambdaCheck.java
@@ -106,4 +106,5 @@ public class AnonymousClassShouldBeLambdaCheck extends BaseTreeVisitor implement
       usesThisIdentifier |= "this".equals(tree.name());
     }
   }
+
 }

java-checks/src/main/java/org/sonar/java/checks/ArrayDesignatorAfterTypeCheck.java
@@ -23,8 +23,8 @@ import com.sonar.sslr.api.AstNode;
 import org.sonar.check.BelongsToProfile;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
-import org.sonar.java.ast.parser.JavaGrammar;
 import org.sonar.java.ast.parser.TreeFactory;
+import org.sonar.plugins.java.api.tree.Tree.Kind;
 import org.sonar.squidbridge.checks.SquidCheck;
 import org.sonar.sslr.parser.LexerlessGrammar;
 
@@ -37,8 +37,7 @@ public class ArrayDesignatorAfterTypeCheck extends SquidCheck<LexerlessGrammar> 
 
   @Override
   public void init() {
-    subscribeTo(JavaGrammar.METHOD_DECLARATOR_REST);
-    subscribeTo(JavaGrammar.INTERFACE_METHOD_DECLARATOR_REST);
+    subscribeTo(Kind.METHOD);
   }
 
   @Override

commit f8982a09b8b49049db7f3c940c541f09b869ada0

Parser should reject enum declarations without semicolon between constants and other declarations
This fixes regression introduced in
d1745712febf3bd1d8d48c02eaaa2db5fbcdb0f9

java-frontend/src/main/java/org/sonar/java/ast/parser/JavaGrammar.java
@@ -458,7 +458,7 @@ public class JavaGrammar {
           b.optional(f.newTuple12(b.token(JavaKeyword.IMPLEMENTS), QUALIFIED_IDENTIFIER_LIST())),
           b.token(JavaPunctuator.LWING),
           b.zeroOrMore(ENUM_CONSTANT()),
-          // TODO Grammar has been relaxed
+          // Grammar has been relaxed
           b.optional(b.token(JavaPunctuator.SEMI)),
           b.zeroOrMore(CLASS_MEMBER()),
           b.token(JavaPunctuator.RWING)));

java-frontend/src/main/java/org/sonar/java/ast/parser/TreeFactory.java
@@ -522,6 +522,10 @@ public class TreeFactory {
       }
     }
 
+    if (enumDeclarations.isPresent() && !semicolonToken.isPresent()) {
+      throw new IllegalStateException("missing semicolon after enum constants");
+    }
+
     ClassTreeImpl result = newClassBody(Kind.ENUM, openBraceToken, Optional.of((List<JavaTree>) ImmutableList.<JavaTree>builder().addAll(members).build()), closeBraceToken);
 
     result.completeDeclarationKeyword(enumToken);

java-checks/src/test/files/checks/EnumEqualCheck.java
@@ -4,7 +4,7 @@ class A {
   }
 
   public enum Cake {
-    LEMON_TART, CHEESE_CAKE
+    LEMON_TART, CHEESE_CAKE;
 
     boolean foo(Object o) {
       return equals(o); // compliant when called inside an enum (no member select).

java-checks/src/test/files/checks/FieldModifier.java
@@ -20,6 +20,7 @@ class D {
   static int var3; // Compliant
 }
 enum E {
+  ;
   @com.google.common.annotations.VisibleForTesting
   int varEnum; // Compliant
   @com.google.common.annotations.VisibleForTesting

java-checks/src/test/files/checks/ThreadStartedInConstructor.java
@@ -2,6 +2,7 @@ class ExtendsThread extends Thread {
 }
 
 enum Enum {
+  ;
   public static final String STRING = "string".intern(); // Compliant, should not fail with NPE
 }
 

java-frontend/src/test/java/org/sonar/java/ast/parser/JavaParserTest.java
@@ -19,6 +19,7 @@
  */
 package org.sonar.java.ast.parser;
 
+import com.sonar.sslr.api.typed.ActionParser;
 import org.junit.Test;
 import org.sonar.plugins.java.api.tree.ClassTree;
 import org.sonar.plugins.java.api.tree.CompilationUnitTree;
@@ -27,8 +28,22 @@ import org.sonar.plugins.java.api.tree.Tree;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
 
 public class JavaParserTest {
+
+  @Test
+  public void should_throw_exception_when_missing_semicolon_after_enum_constants() {
+    ActionParser<Tree> parser = JavaParser.createParser();
+    parser.parse("enum E { ; int field; }");
+
+    try {
+      parser.parse("enum E { int field; }");
+      fail("exception expected");
+    } catch (RuntimeException e) {
+      assertEquals("missing semicolon after enum constants", e.getCause().getCause().getMessage());
+    }
+  }
 
   @Test
   public void parent_link_should_be_computed() {