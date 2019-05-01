6be8e12de0c60c606000a3b2a7ec2adef9ffe56c
pivovarit <gpiwowarek@gmail.com>
2019-05-01 15:09:44 +0200

    Update test subjects

--- java-checks/src/test/files/checks/VolatileNonPrimitiveFieldCheck.java
+++ java-checks/src/test/files/checks/VolatileNonPrimitiveFieldCheck.java
@@ -3,7 +3,7 @@ class A {
   private volatile int [] vInts;  // Noncompliant [[sc=11;ec=26]] {{Use an "AtomicIntegerArray" instead.}}
   private volatile long [] vLongs;  // Noncompliant [[sc=11;ec=27]] {{Use an "AtomicLongArray" instead.}}
   private volatile Object [] vObjects;  // Noncompliant [[sc=11;ec=29]] {{Use an "AtomicReferenceArray" instead.}}
-  private volatile MyObj myObj;  // Noncompliant [[sc=11;ec=25]] {{Remove the "volatile" keyword from this field.}}
+  private volatile MyObj myObj;  // Noncompliant [[sc=11;ec=25]] {{Remember that declaring a reference "volatile" does not guarantee safe publication of the members of the referenced object.}}
   private AtomicIntegerArray vInts2;
   private MyObj myObj2;
 
@@ -13,7 +13,7 @@ enum MyEnum {
   FOO;
   private volatile int vInts0;
   private volatile int [] vInts;  // Noncompliant [[sc=11;ec=26]] {{Use an "AtomicIntegerArray" instead.}}
-  private volatile MyObj myObj;  // Noncompliant [[sc=11;ec=25]] {{Remove the "volatile" keyword from this field.}}
+  private volatile MyObj myObj;  // Noncompliant [[sc=11;ec=25]] {{Remember that declaring a reference "volatile" does not guarantee safe publication of the members of the referenced object.}}
   private AtomicIntegerArray vInts2;
   private MyObj myObj2;
 
