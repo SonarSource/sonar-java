457103d826dd44d52da5e3bc42525616a287ef54
Andrea Guarino <38876543+andrea-guarino-sonarsource@users.noreply.github.com>
2019-05-14 13:35:33 +0200

    SONARJAVA-3099 FP on Rule S2077: Stop raising on variables and array access (#2528)

--- java-checks/src/test/files/checks/SQLInjection.java
+++ java-checks/src/test/files/checks/SQLInjection.java
@@ -21,8 +21,6 @@ class A {
       Statement stmt = conn.createStatement();
       ResultSet rs = stmt.executeQuery("SELECT Lname FROM Customers WHERE Snum = 2001");
       rs = stmt.executeQuery("SELECT Lname FROM Customers WHERE Snum = "+param); // Noncompliant [[sc=30;ec=79]] {{Ensure that string concatenation is required and safe for this SQL query.}}
-      String query = "SELECT Lname FROM Customers WHERE Snum = "+param;
-      rs = stmt.executeQuery(query); // Noncompliant
 
       boolean bool = false;
       String query2 = "Select Lname ";
@@ -36,16 +34,14 @@ class A {
 
       //Prepared statement
       PreparedStatement ps = conn.prepareStatement("SELECT Lname FROM Customers"+" WHERE Snum = 2001");
-      ps.executeQuery(query); // Noncompliant
+      ps.executeQuery("SELECT Lname FROM Customers WHERE Snum = "+param); // Noncompliant
       ps  = conn.prepareStatement("SELECT Lname FROM Customers WHERE Snum = "+param); // Noncompliant
-      ps = conn.prepareStatement(query); // Noncompliant
       ps = conn.prepareStatement(query2);
 
       //Callable Statement
       CallableStatement cs = conn.prepareCall("SELECT Lname FROM Customers WHERE Snum = 2001");
-      cs.executeQuery(query); // Noncompliant
+      cs.executeQuery("SELECT Lname FROM Customers WHERE Snum = "+param); // Noncompliant
       cs  = conn.prepareCall("SELECT Lname FROM Customers WHERE Snum = "+param2); // Noncompliant
-      cs = conn.prepareCall(query); // Noncompliant
       cs = conn.prepareCall(query2);
       cs = conn.prepareCall(CONSTANT);
       cs = conn.prepareCall(foo());
@@ -64,23 +60,33 @@ class A {
       cs = conn.prepareCall(s);
       String request = foo() + " FROM table";
       cs = conn.prepareCall(request);
-      new A().prepareStatement(query);
+      new A().prepareStatement("SELECT Lname FROM Customers WHERE Snum = "+param);
       A a = new A();
-      a.prepareStatement(query);
+      a.prepareStatement("SELECT Lname FROM Customers WHERE Snum = "+param);
       ps.executeQuery();
 
 
       Session session;
       session.createQuery("From Customer where id > ?");
       session.createQuery("From Customer where id > "+param); // Noncompliant
-      session.createQuery(query); // Noncompliant
       conn.prepareStatement(param);
-      conn.prepareStatement(sqlQuery + "plop");
+      conn.prepareStatement(sqlQuery + "plop"); // Noncompliant
 
-      String sql = "SELECT lastname, firstname FROM employee where uid = '" + param + "'";
-      entityManager.createNativeQuery(sql); // Noncompliant
+      entityManager.createNativeQuery("SELECT lastname, firstname FROM employee where uid = '" + param + "'"); // Noncompliant
     } catch (Exception e) {
     }
+  }
+
+  void foo(Connection conn, String param) {
+    Statement stmt = conn.createStatement();
+    String localVar = param;
+    String aliasParam;
+    aliasParam = param;
+    stmt.execute(aliasParam); // OK
+    stmt.execute(localVar); // OK
+    stmt.execute(param); // OK
+    String[] queries = {"test"};
+    stmt.execute(queries[0]); // OK
   }
 
   String foo() {
@@ -126,12 +132,11 @@ class Spring {
   private PreparedStatementCreatorFactory preparedStatementCreatorFactory;
 
   void test(String parameter) {
-    java.lang.String sqlInjection = "select count(*) from t_actor where column =  " + parameter;
-    jdbcTemplate.queryForObject(sqlInjection, Integer.class); // Noncompliant
-    jdbcOperations.queryForObject(sqlInjection, Integer.class);  // Noncompliant
+    jdbcTemplate.queryForObject("select count(*) from t_actor where column =  " + parameter, Integer.class); // Noncompliant
+    jdbcOperations.queryForObject("select count(*) from t_actor where column =  " + parameter, Integer.class);  // Noncompliant
 
-    new PreparedStatementCreatorFactory(sqlInjection);  // Noncompliant
-    preparedStatementCreatorFactory.newPreparedStatementCreator(sqlInjection, new int[] {});  // Noncompliant
+    new PreparedStatementCreatorFactory("select count(*) from t_actor where column =  " + parameter);  // Noncompliant
+    preparedStatementCreatorFactory.newPreparedStatementCreator("select count(*) from t_actor where column =  " + parameter, new int[] {});  // Noncompliant
   }
 }
 
