c390d5b254afbc5e34191307bfc6d849b0d45de5
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-08-14 22:48:33 +0200

    Fix test file
    * same file can not contain `enum` as identifier and as keyword
    * underscore has always been forbidden as the name of a lambda parameter

--- java-checks/src/test/files/checks/naming/KeywordAsIdentifierCheck.java
+++ java-checks/src/test/files/checks/naming/KeywordAsIdentifierCheck.java
@@ -1,35 +1,18 @@
 class A {
   int foo = 0;
+  int _ = 0; // Noncompliant [[sc=7;ec=8]] {{Use a different name than "_".}}
   int enum = 0; // Noncompliant [[sc=7;ec=11]] {{Use a different name than "enum".}}
-
-  enum mynum {RED, GREEN, YELLOW};
 
   public void f(
       int a,
+      int _, // Noncompliant
       int enum) { // Noncompliant
-
   }
 
   public void g(){
     int a;
+    int _;  // Noncompliant
     int enum; // Noncompliant
+    _ = enum; // should be reported only for declarations
   }
-}
-
-enum B { // Compliant
-  ;
-}
-
-class Underscore {
-
-  void f() {
-    // underscore will become a keyword in Java 9
-    String _ = ""; // Noncompliant [[sc=12;ec=13]] {{Use a different name than "_".}}
-  }
-
-  void lambda() {
-    // usage as lambda identifier is a compilation error already in Java 8
-    IntStream.range(0, 10).forEach(_ -> System.out.println(_)); // Noncompliant [[sc=36;ec=37]] {{Use a different name than "_".}}
-  }
-
 }
