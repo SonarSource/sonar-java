e6b50984c87cb3727999e2cf7834a93f5637d2f9
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2019-09-06 14:04:06 +0200

    False positive fix on Nested JUnit 5 classes on S3577
    Rule S3577 checks a name of the test to comply with naming scheme. It
    checks for a name if a class contains a test method. JUnit 5 introduced
    nested classes that should be excluded from this rule as they do not
    need to be searched by surefire/failsafe or other tests executors.
    
    In this commit check has been changed to take acount only top level
    classes that contain tests and/or nested classes.

--- java-checks/src/main/java/org/sonar/java/checks/naming/BadTestClassNameCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/naming/BadTestClassNameCheck.java
@@ -68,8 +68,9 @@ public class BadTestClassNameCheck extends IssuableSubscriptionVisitor {
     }
     ClassTree classTree = (ClassTree) tree;
     IdentifierTree simpleName = classTree.simpleName();
-    if (hasInvalidName(simpleName) && hasTestMethod(classTree.members())) {
-      reportIssue(simpleName, "Rename class \"" + simpleName + "\" to match the regular expression: '" + format + "'");
+    if (hasInvalidName(simpleName) && isTestClass(classTree)) {
+      reportIssue(simpleName, "Rename class \"" + simpleName
+        + "\" to match the regular expression: '" + format + "'");
     }
   }
 
@@ -77,11 +78,28 @@ public class BadTestClassNameCheck extends IssuableSubscriptionVisitor {
     return className != null && !pattern.matcher(className.name()).matches();
   }
 
+  private boolean isTestClass(ClassTree classTree) {
+    return isTopLevelClass(classTree)
+      && (hasTestMethod(classTree.members()) || hasNestedClass(classTree));
+  }
+
+  private boolean isTopLevelClass(ClassTree classTree) {
+    return classTree.symbol().owner().isPackageSymbol();
+  }
+
   private static boolean hasTestMethod(List<Tree> members) {
     return members.stream()
       .filter(member -> member.is(Tree.Kind.METHOD))
       .map(MethodTree.class::cast)
       .anyMatch(UnitTestUtils::hasTestAnnotation);
+  }
+
+  private boolean hasNestedClass(ClassTree classTree) {
+    return classTree.members()
+      .stream()
+      .filter(member -> member.is(Tree.Kind.CLASS))
+      .map(ClassTree.class::cast)
+      .anyMatch(UnitTestUtils::hasNestedAnnotation);
   }
 
 }
