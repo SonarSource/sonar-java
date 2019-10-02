848751dab8405da57429769b2d2760d7d5ed05f9
Evgeny Mandrikov <mandrikov@gmail.com>
2019-10-02 15:09:33 +0200

    Fix test
    Annotation should be visible for test files.

--- java-checks/src/test/java/org/sonar/java/checks/targets/CustomAnnotation.java
+++ java-checks/src/test/java/org/sonar/java/checks/targets/CustomAnnotation.java
@@ -19,7 +19,7 @@
  */
 package org.sonar.java.checks.targets;
 
-@interface CustomAnnotation {
+public @interface CustomAnnotation {
   String field1() default "field1Default";
   String field2() default "field" + "2Default";
   String field3();
