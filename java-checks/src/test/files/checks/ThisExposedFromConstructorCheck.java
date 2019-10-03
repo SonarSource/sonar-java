deb9ff3c1269572e157752ebb16169afdc4d3587
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-10-03 14:28:46 +0200

    Fix test file
    Simplify test cases

--- java-checks/src/test/files/checks/ThisExposedFromConstructorCheck.java
+++ java-checks/src/test/files/checks/ThisExposedFromConstructorCheck.java
@@ -1,82 +1,52 @@
 import java.util.List;
 
-class A{
-  private static final List<A> list;
-  private String location;
-  private static String loc;
+class B {
+  B field;
+
+  A method(A a) {
+    return a;
+  }
+}
+
+class A extends B {
+  private static final List<A> list = new java.util.ArrayList<>();
   private static A tempVal;
   public A Instance;
   private B Instance2;
-  
-  public A(String location) {
-    list.add(this);  // Noncompliant [sc=16;ec=20] {{Make sure the use of "this" doesn't expose partially-constructed instances of this class in multi-threaded environments.}}
-    foo(this);  // Compliant
-    this.location = location; // Compliant
-    this.tempVal = this;  // Compliant
-    Instance = this;  // Noncompliant
-    this.Instance = this; // Noncompliant
-    tempVal = this;  // Compliant
-    this.loc = "loc"; // Compliant
-    foo(Instance2.foo3(this)); // Noncompliant  
-    this.Instance2.foo(this); // Noncompliant
-    this.Instance.foo3(this.Instance2.foo(this)); // Noncompliant
-    this.tempVal = Instance.foo(this);  // Compliant
-    this.Instance2 = this;  // Noncompliant
-  }
+  A[] as = new A[1];
 
   public A() {
-    this.location = ""; // Compliant
-    foo2(this);  // Noncompliant
-    foo2(); // Compliant
-    foo3(); //  Compliant
-    foo1(new A());  // Compliant
-    B.foo2();  // Compliant
-    B.foo1(this);  // Noncompliant
-    B.field = this;  // Noncompliant
-    B.field3 = this; // Noncompliant
-    this.Instance2.foo3(this); // Noncompliant
-    this.foo3(this);  // Compliant
-    this.Instance = (this); // Noncompliant
-    this.tempVal = (this); // Compliant
-  }
-  
-  public void foo(A a) {}
-  
-  private void foo2() {
-    list.add(this);
-  }
-  
-  private void foo3(A a) {
-  }
-  
-}
+    list.add(this); // Noncompliant [sc=16;ec=20] {{Make sure the use of "this" doesn't expose partially-constructed instances of this class in multi-threaded environments.}}
 
-class B {
-  public static final List<A> list1;
-  public static A field;
-  public A field3;
-  public B field2;
-  public B[] arr1;
-    
-  public B() {
-    this.field2 = this;  // Noncompliant
-    this.field3 = foo2(this);  // Compliant
+    tempVal = this; // Compliant
+    this.tempVal = this; // Compliant
+    A.tempVal = this; // Compliant
+
+    Instance = this; // Noncompliant
+    new A().Instance = this; // Noncompliant
+
+    Instance2 = this; // Noncompliant
+    super.field = this; // Noncompliant
+
+    Instance.method(this); // Compliant
+    Instance2.method(this); // Noncompliant
+    tempVal = method(this); // Compliant
+
+    as[0] = this; // Noncompliant
+
     new A() {
+      @Override
+      void notAConstructor() { }
     };
-    new Thread() {
-      public void run() {
-        A a = new A();
-        a.Instance2 = this;  // Compliant
-      }
-    }.start();
-   arr1[0] = this;  // Noncompliant
   }
-  
-  void foo(A a) {
+
+  @Override
+  A method(A a) {
+    return a;
   }
-  
-  A foo2(B b) {}
-  
-  public void foo3(A a) {
+
+  void notAConstructor() {
+    list.add(this);
+    Instance = this; // Compliant
   }
 }
