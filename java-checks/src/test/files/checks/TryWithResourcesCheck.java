f84eb7124978846df96d7f837c3709a0e4b13d43
Amélie Renard <44666826+amelie-renard-sonarsource@users.noreply.github.com>
2019-11-20 17:38:39 +0100

    SONARJAVA-2939 an issue is raised when an "AutoCloseable" resource is initialized outside of a try-catch block

--- java-checks/src/test/files/checks/TryWithResourcesCheck.java
+++ java-checks/src/test/files/checks/TryWithResourcesCheck.java
@@ -1,11 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+class Auto implements AutoCloseable {
+  public void doSomething() {}
+
+  @Override
+  public void close() {}
+}
+
 class A {
   void foo(String fileName) {
     FileReader fr = null;
     BufferedReader br = null;
-    try { // Noncompliant [[sc=5;ec=8;secondary=9,10]] {{Change this "try" to a try-with-resources.}}
+    try { // Noncompliant [[sc=5;ec=8;secondary=16,17]] {{Change this "try" to a try-with-resources.}}
       fr = new FileReader(fileName);
       br = new BufferedReader(fr);
       return br.readLine();
@@ -35,6 +42,99 @@ class A {
       return br.readLine();
     } catch (Exception e) {
     }
+  }
 
+  void newJustBeforeTryStatement() {
+    Auto a1 = new Auto();
+    Auto a2 = new Auto();
+    try { // Noncompliant [[sc=5;ec=8;secondary=48,49]] {{Change this "try" to a try-with-resources.}}
+      a1.doSomething();
+    }  finally {
+      a1.close();
+      a2.close();
+    }
+  }
+
+  void newJustBeforeAndAfterTryStatement() {
+    Auto a1 = null;
+    Auto a2 = new Auto();
+    try { // Noncompliant [[sc=5;ec=8;secondary=60,62]] {{Change this "try" to a try-with-resources.}}
+      a1 = new Auto();
+      a1.doSomething();
+    }  finally {
+      a1.close();
+      a2.close();
+    }
+  }
+
+  void methodBetweenNewAndTry() {
+    Auto a = new Auto();
+    a.doSomething();
+    try {
+      a.doSomething();
+    }  finally {
+      a.close();
+    }
+  }
+
+  class B {}
+
+  void unknownNewBetweenNewAndTry() {
+    Auto a = new Auto();
+    B b = new B();
+    try {
+      a.doSomething();
+    }  finally {
+      a.close();
+    }
+  }
+
+  Auto passThrough(Auto a) { return a; }
+
+  void newInsideMethodInvocation() {
+    Auto a = passThrough(new Auto()); // Compliant, we do not know what happens in the method
+    try {
+      a.doSomething();
+    }  finally {
+      a.close();
+    }
+  }
+
+  void newJustBeforeTryWithResource() {
+    Auto a1 = new Auto();
+    try (Auto a2 = new Auto()) {
+      a1.doSomething();
+    }  finally {
+      a1.close();
+    }
+  }
+
+  void enclosedTryWithFinallyStatements() {
+    Auto a1 = new Auto();
+    try { // Noncompliant [[sc=5;ec=8;secondary=113,116]] {{Change this "try" to a try-with-resources.}}
+      a1.doSomething();
+      Auto a2 = new Auto();
+      try {
+        a2.doSomething();
+      } finally {
+        a2.close();
+      }
+    }  finally {
+      a1.close();
+    }
+  }
+
+  void enclosedTryStatements() {
+    Auto a1 = new Auto();
+    try { // Noncompliant [[sc=5;ec=8;secondary=128,131]] {{Change this "try" to a try-with-resources.}}
+      a1.doSomething();
+      Auto a2 = new Auto();
+      try {
+        a2.doSomething();
+        a2.close();
+      } catch (Exception e) {}
+    }  finally {
+      a1.close();
+    }
   }
 }
