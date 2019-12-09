2f7d7d2dcaffe9985acf755ea10c41040f8c15de
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-12-09 18:43:00 +0100

    Drop unused test files

--- java-frontend/src/test/files/sym/MethodReferencesTypeArguments.java
+++
@@ -1,7 +0,0 @@
-abstract class A {
-  void foo(java.util.Collection<A> objects) {
-    objects.stream().map(A::<String>getValue);
-  }
-
-  abstract <T> T getValue();
-}
--- java-frontend/src/test/files/sym/TypeInferenceOnSupertypes.java
+++
@@ -1,8 +0,0 @@
-class A<T> {}
-class B<U> extends A<U> {}
-class Test {
-  B<String> b;
-  <S> S foo(A<S> a) {
-    foo(b); // should be resolved with infered substitution S -> String
-  }
-}
