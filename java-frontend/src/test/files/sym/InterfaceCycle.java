8ca458d8184f2a849a52bba57fcbad1ff0ca4e72
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-12-09 18:43:00 +0100

    Drop unused test files

--- java-frontend/src/test/files/sym/InterfaceCycle.java
+++
@@ -1,6 +0,0 @@
-class InterfaceCycle {
-  // This code does not compile as it contains a cycle
-  public interface A extends B {}
-  public interface B extends C {}
-  public interface C extends A {}
-}
