6111b76be2c4c64eeae5cb7a7da5332e255c258b
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-02-11 15:12:04 +0100

    SONARJAVA-3292 Deprecate ConstantUtils and migrate its implementation

--- java-checks/src/main/java/org/sonar/java/checks/helpers/ConstantUtils.java
+++ java-checks/src/main/java/org/sonar/java/checks/helpers/ConstantUtils.java
@@ -20,17 +20,14 @@
 package org.sonar.java.checks.helpers;
 
 import javax.annotation.CheckForNull;
-import org.sonar.java.model.JUtils;
-import org.sonar.java.model.LiteralUtils;
-import org.sonar.plugins.java.api.semantic.Symbol;
-import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
-import org.sonar.plugins.java.api.tree.IdentifierTree;
-import org.sonar.plugins.java.api.tree.LiteralTree;
-import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
-import org.sonar.plugins.java.api.tree.ParenthesizedTree;
-import org.sonar.plugins.java.api.tree.Tree;
 
+/**
+ * This is deprecated, rely on {@link ExpressionTree#asConstant()} methods instead
+ *
+ * @deprecated since SonarJava 6.1 - now available from {@link ExpressionTree} public API.
+ */
+@Deprecated
 public class ConstantUtils {
 
   private ConstantUtils() {
@@ -38,19 +35,17 @@ public class ConstantUtils {
 
   @CheckForNull
   public static String resolveAsStringConstant(ExpressionTree tree) {
-    Object constant = resolveAsConstant(tree);
-    return constant instanceof String ? (String) constant : null;
+    return tree.asConstant(String.class).orElse(null);
   }
 
   @CheckForNull
   public static Integer resolveAsIntConstant(ExpressionTree tree) {
-    Object constant = resolveAsConstant(tree);
-    return constant instanceof Integer ? (Integer) constant : null;
+    return tree.asConstant(Integer.class).orElse(null);
   }
 
   @CheckForNull
   public static Long resolveAsLongConstant(ExpressionTree tree) {
-    Object constant = resolveAsConstant(tree);
+    Object constant = tree.asConstant().orElse(null);
     if (constant instanceof Long) {
       return (Long) constant;
     }
@@ -62,77 +57,11 @@ public class ConstantUtils {
 
   @CheckForNull
   public static Boolean resolveAsBooleanConstant(ExpressionTree tree) {
-    Object constant = resolveAsConstant(tree);
-    return constant instanceof Boolean ? (Boolean) constant : null;
+    return tree.asConstant(Boolean.class).orElse(null);
   }
 
   @CheckForNull
   public static Object resolveAsConstant(ExpressionTree tree) {
-    ExpressionTree expression = tree;
-    while (expression.is(Tree.Kind.PARENTHESIZED_EXPRESSION)) {
-      expression = ((ParenthesizedTree) expression).expression();
-    }
-    if (expression.is(Tree.Kind.MEMBER_SELECT)) {
-      expression = ((MemberSelectExpressionTree) expression).identifier();
-    }
-    if (expression.is(Tree.Kind.IDENTIFIER)) {
-      return resolveIdentifier((IdentifierTree) expression);
-    }
-    if (expression.is(Tree.Kind.BOOLEAN_LITERAL)) {
-      return Boolean.parseBoolean(((LiteralTree) expression).value());
-    }
-    if (expression.is(Tree.Kind.STRING_LITERAL)) {
-      return LiteralUtils.trimQuotes(((LiteralTree) expression).value());
-    }
-    if (tree.is(Tree.Kind.INT_LITERAL, Tree.Kind.UNARY_MINUS, Tree.Kind.UNARY_PLUS)) {
-      return LiteralUtils.intLiteralValue(tree);
-    }
-    if (tree.is(Tree.Kind.LONG_LITERAL)) {
-      return LiteralUtils.longLiteralValue(tree);
-    }
-    if (expression.is(Tree.Kind.PLUS)) {
-      return resolvePlus((BinaryExpressionTree) expression);
-    }
-    return null;
+    return tree.asConstant().orElse(null);
   }
-
-  @CheckForNull
-  private static Object resolveIdentifier(IdentifierTree tree) {
-    Symbol symbol = tree.symbol();
-    if (!symbol.isVariableSymbol()) {
-      return null;
-    }
-    Symbol owner = symbol.owner();
-    if (owner.isTypeSymbol() && owner.type().is("java.lang.Boolean")) {
-      if ("TRUE".equals(symbol.name())) {
-        return Boolean.TRUE;
-      } else if ("FALSE".equals(symbol.name())) {
-        return Boolean.FALSE;
-      }
-    }
-    return JUtils.constantValue((Symbol.VariableSymbol) symbol).orElse(null);
-  }
-
-  @CheckForNull
-  private static Object resolvePlus(BinaryExpressionTree binaryExpression) {
-    Object left = resolveAsConstant(binaryExpression.leftOperand());
-    Object right = resolveAsConstant(binaryExpression.rightOperand());
-    if (left == null || right == null) {
-      return null;
-    } else if (left instanceof String) {
-      return ((String) left) + right;
-    } else if (right instanceof String) {
-      return left + ((String) right);
-    } else if (left instanceof Long && right instanceof Long) {
-      return ((Long) left) + ((Long) right);
-    } else if (left instanceof Long && right instanceof Integer) {
-      return ((Long) left) + ((Integer) right);
-    } else if (left instanceof Integer && right instanceof Long) {
-      return ((Integer) left) + ((Long) right);
-    } else if (left instanceof Integer && right instanceof Integer) {
-      return ((Integer) left) + ((Integer) right);
-    }
-    return null;
-  }
-
 }
