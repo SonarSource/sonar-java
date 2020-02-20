520e1b4fe24c7468f48476f44e0ac8c0bb8fc203
David Pursehouse <dpursehouse@collab.net>
2020-02-21 08:33:24 +0900

    Also use correct package in 'compliant' code of NioFileDeleteCheck
    Change-Id: Idd943a4150745f80f8ec1e76af04420f68663230
    Signed-off-by: David Pursehouse <dpursehouse@collab.net>

--- java-checks/src/test/files/checks/NioFileDeleteCheck.java
+++ java-checks/src/test/files/checks/NioFileDeleteCheck.java
@@ -5,6 +5,6 @@ class A {
     File file = new File(path);
     if (!file.delete()) {  // Noncompliant {{Use "java.nio.file.Files#delete" here for better messages on error conditions.}}
     }
-    java.nio.Files.delete(path); // compliant
+    java.nio.file.Files.delete(path); // compliant
   }
 }
