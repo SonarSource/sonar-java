8d21cb69ca2834dd722011f072db4fc9f7533175
Alban Auzeill <alban.auzeill@sonarsource.com>
2020-03-02 11:48:05 +0100

    Move SerializableObjectInSessionCheck unit test in java-checks-test-sources

--- java-checks-test-sources/src/main/java/checks/serialization/SerializableObjectInSessionCheck.java
+++ java-checks-test-sources/src/main/java/checks/serialization/SerializableObjectInSessionCheck.java
@@ -1,7 +1,12 @@
-class A {
+package checks.serialization;
 
-  void foo() {
-    javax.servlet.http.HttpSession session = request.getSession();
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+class SerializableObjectInSessionCheck {
+
+  void foo(HttpServletRequest request) {
+    HttpSession session = request.getSession();
     session.setAttribute("address", new Address()); // Noncompliant [[sc=37;ec=50]] {{Make "Address" serializable or don't store it in the session.}}
     session.setAttribute("person", new Person()); // Noncompliant {{Make "Person" serializable or don't store it in the session.}}
     session.setAttribute("person", 1);
