6962871b54b2e57acc81d5b95a6f9f446de07614
Alban Auzeill <alban.auzeill@sonarsource.com>
2020-03-03 08:02:17 +0100

    Add more java collection false-positives

--- java-checks-test-sources/src/main/java/checks/NonSerializableWriteCheck.java
+++ java-checks-test-sources/src/main/java/checks/NonSerializableWriteCheck.java
@@ -4,9 +4,25 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 class NonSerializableWriteCheck {
   void myMethod(ObjectOutputStream out, Object x, byte[] array) throws IOException {
@@ -29,8 +45,42 @@ class NonSerializableWriteCheck {
     out.writeObject(array);
 
     out.writeObject(java.util.Arrays.asList("one", "two")); // Noncompliant - false positive
+
+    Iterable<String> iterable = new ArrayList<>();
+    out.writeObject(iterable); // Noncompliant - false positive
+
+    Collection<String> collection = new ArrayList<>();
+    out.writeObject(collection); // Noncompliant - false positive
+
     List<String> list = new ArrayList<>();
     out.writeObject(list); // Noncompliant - false positive
+
+    Set<String> set = new HashSet<>();
+    out.writeObject(set); // Noncompliant - false positive
+
+    SortedSet<String> sortedSet = new TreeSet<>();
+    out.writeObject(sortedSet); // Noncompliant - false positive
+
+    NavigableSet<String> navigableSet = new TreeSet<>();
+    out.writeObject(navigableSet); // Noncompliant - false positive
+
+    Queue<String> queue = new LinkedList<>();
+    out.writeObject(queue); // Noncompliant - false positive
+
+    Deque<String> deque = new ArrayDeque<>();
+    out.writeObject(deque); // Noncompliant - false positive
+
+    Map<String, String> map = new HashMap<>();
+    out.writeObject(map); // Noncompliant - false positive
+
+    SortedMap<String, String> sortedMap = new TreeMap<>();
+    out.writeObject(sortedMap); // Noncompliant - false positive
+
+    NavigableMap<String, String> navigableMap = new TreeMap<>();
+    out.writeObject(navigableMap); // Noncompliant - false positive
+
+    Enumeration<String> enumeration = Collections.emptyEnumeration();
+    out.writeObject(enumeration); // Noncompliant - false positive
   }
 }
 
--- java-checks-test-sources/src/main/java/checks/serialization/SerializableObjectInSessionCheck.java
+++ java-checks-test-sources/src/main/java/checks/serialization/SerializableObjectInSessionCheck.java
@@ -1,7 +1,24 @@
 package checks.serialization;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -26,8 +43,42 @@ class SerializableObjectInSessionCheck {
 
     session.setAttribute("nonSerializableParameterized", new CustomStack<String>()); // Noncompliant {{Make "CustomStack" and its parameters serializable or don't store it in the session.}}
 
+    Iterable<String> iterable = new ArrayList<>();
+    session.setAttribute("name", iterable); // Noncompliant - false positive
+
+    Collection<String> collection = new ArrayList<>();
+    session.setAttribute("name", collection); // Compliant
+
+    List<String> list = new ArrayList<>();
+    session.setAttribute("name", list); // Compliant
+
+    Set<String> set = new HashSet<>();
+    session.setAttribute("name", set); // Compliant
+
+    SortedSet<String> sortedSet = new TreeSet<>();
+    session.setAttribute("name", sortedSet); // Compliant
+
+    NavigableSet<String> navigableSet = new TreeSet<>();
+    session.setAttribute("name", navigableSet); // Compliant
+
+    Queue<String> queue = new LinkedList<>();
+    session.setAttribute("name", queue); // Compliant
+
+    Deque<String> deque = new ArrayDeque<>();
+    session.setAttribute("name", deque); // Compliant
+
     Map<String, String> map = new HashMap<>();
-    session.setAttribute("test with map", map); // Noncompliant - false positive
+    session.setAttribute("name", map); // Noncompliant - false positive
+
+    SortedMap<String, String> sortedMap = new TreeMap<>();
+    session.setAttribute("name", sortedMap); // Noncompliant - false positive
+
+    NavigableMap<String, String> navigableMap = new TreeMap<>();
+    session.setAttribute("name", navigableMap); // Noncompliant - false positive
+
+    Enumeration<String> enumeration = Collections.emptyEnumeration();
+    session.setAttribute("name", enumeration); // Noncompliant - false positive
+
   }
 
   public class Address {
