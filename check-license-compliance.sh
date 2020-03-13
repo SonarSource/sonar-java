e43cad3f078d16cbc6bdc73634346f860de1f98e
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2020-03-13 11:33:24 +0100

    SONARJAVA-3309 fix licensing for tomcat-servlet-api

--- check-license-compliance.sh
+++ check-license-compliance.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 
 # See https://xtranet.sonarsource.com/display/DEV/Open+Source+Licenses
 
-mvn org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party -Dlicense.aggregateMissingLicensesFile=$(pwd)/missing-dep-licenses.properties -DuseMissingFile
+mvn org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party \
+  -Dlicense.missingFile=$(pwd)/missing-dep-licenses.properties \
+  -DuseMissingFile \
+  -Dlicense.overrideUrl=file://$(pwd)/override-dep-licenses.properties
