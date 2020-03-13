cba90b84aead7264a9a75b68c4011aa66fa25087
Evgeny Mandrikov <mandrikov@gmail.com>
2020-03-13 11:04:09 +0100

    (WIP) debug output

--- java-frontend/src/main/java/org/sonar/java/model/JParser.java
+++ java-frontend/src/main/java/org/sonar/java/model/JParser.java
@@ -349,6 +349,7 @@ public class JParser {
       new FileASTRequestor() {
         @Override
         public void acceptAST(String sourceFilePath, CompilationUnit ast) {
+          System.err.println("Processing " + sourceFilePath);
           InputFile inputFile = inputs.get(new File(sourceFilePath));
           Result result;
           try {
