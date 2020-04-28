7f0f208ede9055611d4e19867217af6e079a9d0a
Alban Auzeill <alban.auzeill@sonarsource.com>
2020-04-28 13:54:55 +0200

    SONARJAVA-3353 Rule S5776: Exception testing via JUnit ExpectedException rule should not be mixed with other assertions

---
+++ java-checks/src/main/java/org/sonar/java/checks/ExpectedExceptionCheck.java
@@ -0,0 +1,98 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.java.model.ExpressionUtils;
+import org.sonar.plugins.java.api.JavaFileScannerContext.Location;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.BlockTree;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.LambdaExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+
+@Rule(key = "S5776")
+public class ExpectedExceptionCheck extends AbstractMethodDetection {
+
+  private static final String MESSAGE = "Do not test exceptions via ExpectedException before other assertions.";
+
+  @Override
+  protected MethodMatchers getMethodInvocationMatchers() {
+    return MethodMatchers.create().ofTypes("org.junit.rules.ExpectedException").names("expect").withAnyParameters().build();
+  }
+
+  @Override
+  protected void onMethodInvocationFound(MethodInvocationTree mit) {
+    MethodTree enclosingMethod = ExpressionUtils.getEnclosingMethod(mit);
+    BlockTree methodBody = enclosingMethod != null ? enclosingMethod.block() : null;
+    if (methodBody == null) {
+      return;
+    }
+    IdentifierTree methodIdentifier = ExpressionUtils.methodName(mit);
+    int collectAfterLine = methodIdentifier.identifierToken().line();
+    AssertionCollector assertionCollector = new AssertionCollector(collectAfterLine);
+    methodBody.accept(assertionCollector);
+    if (!assertionCollector.assertions.isEmpty()) {
+      reportIssue(methodIdentifier, MESSAGE, assertionCollector.assertions, null);
+    }
+  }
+
+  private static class AssertionCollector extends BaseTreeVisitor {
+
+    private static final MethodMatchers JUNIT_ASSERT_METHOD_MATCHER = MethodMatchers.create()
+      .ofTypes("org.junit.Assert", "org.junit.jupiter.api.Assertions")
+      .name(name -> name.startsWith("assert"))
+      .withAnyParameters()
+      .build();
+
+    private int collectAfterLine;
+    private List<Location> assertions = new ArrayList<>();
+
+    public AssertionCollector(int collectAfterLine) {
+      this.collectAfterLine = collectAfterLine;
+    }
+
+    @Override
+    public void visitMethodInvocation(MethodInvocationTree methodInvocation) {
+      if (methodInvocation.firstToken().line() > collectAfterLine &&
+          JUNIT_ASSERT_METHOD_MATCHER.matches(methodInvocation)) {
+        assertions.add(new Location("Other assertion", ExpressionUtils.methodName(methodInvocation)));
+      }
+    }
+
+    @Override
+    public void visitClass(ClassTree tree) {
+      // Skip class
+    }
+
+    @Override
+    public void visitLambdaExpression(LambdaExpressionTree lambdaExpressionTree) {
+      // Skip lambdas
+    }
+
+  }
+
+}
