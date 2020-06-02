d00245b5330106a4adb554f390f1d340bd0be5cd
Sebastian Hungerecker <63844301+sebastian-hungerecker-sonarsource@users.noreply.github.com>
2020-06-02 09:47:29 +0200

    [REGEX] Improve error messages for unexpected quantifiers and add more tests (#2975)
    This also slightly changes the API by making it possible for a
    SyntaxError to refer to an arbitrary SyntaxElement instead of just
    allowing tokens.

--- java-frontend/src/test/java/org/sonar/java/regex/parsertests/CombinedTests.java
+++ java-frontend/src/test/java/org/sonar/java/regex/parsertests/CombinedTests.java
@@ -30,6 +30,8 @@ import org.sonar.java.regex.ast.SequenceTree;
 import org.sonar.java.regex.ast.SimpleQuantifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertKind;
 import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertLocation;
 import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertPlainCharacter;
 import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertPlainString;
@@ -42,10 +44,13 @@ class CombinedTests {
   void testNonTrivialRegex() {
     RegexTree regex = assertSuccessfulParse("(ab|b)*(||)");
     assertLocation(0, 11, regex);
+    assertKind(RegexTree.Kind.SEQUENCE, regex);
+    assertFalse(regex.is(RegexTree.Kind.DISJUNCTION), "`is` should return false when kinds don't match");
     SequenceTree sequence = assertType(SequenceTree.class, regex);
     List<RegexTree> items = sequence.getItems();
     assertEquals(2, items.size(), "The sequence should have two elements.");
 
+    assertKind(RegexTree.Kind.REPETITION, items.get(0));
     RepetitionTree firstPart = assertType(RepetitionTree.class, items.get(0));
     assertLocation(0, 7, firstPart);
     assertLocation(0, 6, firstPart.getElement());
@@ -53,19 +58,24 @@ class CombinedTests {
     SimpleQuantifier quantifier = assertType(SimpleQuantifier.class, firstPart.getQuantifier());
     assertEquals(SimpleQuantifier.Kind.STAR, quantifier.getKind(), "Quantifier should be a star.");
     assertEquals(Quantifier.Modifier.GREEDY, quantifier.getModifier(), "Quantifier should be greedy.");
+    assertKind(RegexTree.Kind.GROUP, firstPart.getElement());
     GroupTree repeatedGroup = assertType(GroupTree.class, firstPart.getElement());
+    assertKind(RegexTree.Kind.DISJUNCTION, repeatedGroup.getElement());
     DisjunctionTree repeatedDisjunction = assertType(DisjunctionTree.class, repeatedGroup.getElement());
     List<RegexTree> repeatedAlternatives = repeatedDisjunction.getAlternatives();
     assertEquals(2, repeatedAlternatives.size(), "First disjunction should have two alternatives.");
     assertPlainString("ab", repeatedAlternatives.get(0));
     assertPlainCharacter('b', repeatedAlternatives.get(1));
 
+    assertKind(RegexTree.Kind.GROUP, items.get(1));
     GroupTree secondPart = assertType(GroupTree.class, items.get(1));
     assertLocation(7, 11, secondPart);
+    assertKind(RegexTree.Kind.DISJUNCTION, secondPart.getElement());
     DisjunctionTree disjunction = assertType(DisjunctionTree.class, secondPart.getElement());
     List<RegexTree> alternatives = disjunction.getAlternatives();
     assertEquals(3, alternatives.size(), "Second disjunction should have three alternatives");
     for (RegexTree alternative : alternatives) {
+      assertKind(RegexTree.Kind.SEQUENCE, alternative);
       SequenceTree empty = assertType(SequenceTree.class, alternative);
       assertEquals(0, empty.getItems().size(), "Second disjunction should contain only empty sequences.");
     }
---
+++ java-frontend/src/test/java/org/sonar/java/regex/parsertests/SimpleQuantifierTests.java
@@ -0,0 +1,117 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.regex.parsertests;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.sonar.java.regex.RegexParseResult;
+import org.sonar.java.regex.RegexParser;
+import org.sonar.java.regex.SyntaxError;
+import org.sonar.java.regex.ast.IndexRange;
+import org.sonar.java.regex.ast.Location;
+import org.sonar.java.regex.ast.Quantifier;
+import org.sonar.java.regex.ast.RegexTree;
+import org.sonar.java.regex.ast.RepetitionTree;
+import org.sonar.java.regex.ast.SimpleQuantifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertPlainCharacter;
+import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertSuccessfulParse;
+import static org.sonar.java.regex.parsertests.RegexParserTestUtils.assertType;
+import static org.sonar.java.regex.parsertests.RegexParserTestUtils.makeSource;
+
+class SimpleQuantifierTests {
+
+  @Test
+  void testGreedyStar() {
+    RegexTree regex = assertSuccessfulParse("x*");
+    RepetitionTree repetition = assertType(RepetitionTree.class, regex);
+    assertPlainCharacter('x', repetition.getElement());
+    SimpleQuantifier quantifier = assertType(SimpleQuantifier.class, repetition.getQuantifier());
+    assertEquals(SimpleQuantifier.Kind.STAR, quantifier.getKind(), "Quantifier should be a Kleene star.");
+    assertEquals(0, quantifier.getMinimumRepetitions(), "Lower bound should be 0.");
+    assertNull(quantifier.getMaximumRepetitions(), "Kleene star should have no upper bound.");
+    assertTrue(quantifier.isOpenEnded(), "Kleene star should be open ended.");
+    assertEquals(Quantifier.Modifier.GREEDY, quantifier.getModifier(), "Quantifier should be greedy.");
+  }
+
+  @Test
+  void testGreedyPlus() {
+    RegexTree regex = assertSuccessfulParse("x+");
+    RepetitionTree repetition = assertType(RepetitionTree.class, regex);
+    assertPlainCharacter('x', repetition.getElement());
+    SimpleQuantifier quantifier = assertType(SimpleQuantifier.class, repetition.getQuantifier());
+    assertEquals(SimpleQuantifier.Kind.PLUS, quantifier.getKind(), "Quantifier should be a plus.");
+    assertEquals(1, quantifier.getMinimumRepetitions(), "Lower bound should be 1.");
+    assertNull(quantifier.getMaximumRepetitions(), "Plus should have no upper bound.");
+    assertTrue(quantifier.isOpenEnded(), "Plus should be open ended.");
+    assertEquals(Quantifier.Modifier.GREEDY, quantifier.getModifier(), "Quantifier should be greedy.");
+  }
+
+  @Test
+  void testGreedyQuestionMark() {
+    RegexTree regex = assertSuccessfulParse("x?");
+    RepetitionTree repetition = assertType(RepetitionTree.class, regex);
+    assertPlainCharacter('x', repetition.getElement());
+    SimpleQuantifier quantifier = assertType(SimpleQuantifier.class, repetition.getQuantifier());
+    assertEquals(SimpleQuantifier.Kind.QUESTION_MARK, quantifier.getKind(), "Quantifier should be a question mark.");
+    assertEquals(0, quantifier.getMinimumRepetitions(), "Lower bound should be 0.");
+    assertEquals(1, quantifier.getMaximumRepetitions(), "The upper bound of a question mark quantifier should be 1.");
+    assertFalse(quantifier.isOpenEnded(), "Question mark should not be open ended.");
+    assertEquals(Quantifier.Modifier.GREEDY, quantifier.getModifier(), "Quantifier should be greedy.");
+  }
+
+  @Test
+  void testLazyStar() {
+    RegexTree regex = assertSuccessfulParse("x*?");
+    RepetitionTree repetition = assertType(RepetitionTree.class, regex);
+    assertPlainCharacter('x', repetition.getElement());
+    SimpleQuantifier quantifier = assertType(SimpleQuantifier.class, repetition.getQuantifier());
+    assertEquals(SimpleQuantifier.Kind.STAR, quantifier.getKind(), "Quantifier should be a Kleene star.");
+    assertEquals(Quantifier.Modifier.LAZY, quantifier.getModifier(), "Quantifier should be lazy.");
+  }
+
+  @Test
+  void testPossessiveStar() {
+    RegexTree regex = assertSuccessfulParse("x*+");
+    RepetitionTree repetition = assertType(RepetitionTree.class, regex);
+    assertPlainCharacter('x', repetition.getElement());
+    SimpleQuantifier quantifier = assertType(SimpleQuantifier.class, repetition.getQuantifier());
+    assertEquals(SimpleQuantifier.Kind.STAR, quantifier.getKind(), "Quantifier should be a Kleene star.");
+    assertEquals(Quantifier.Modifier.POSSESSIVE, quantifier.getModifier(), "Quantifier should be possessive.");
+  }
+
+  @Test
+  void testDoubleQuantifier() {
+    RegexParseResult result = new RegexParser(makeSource("x**")).parse();
+    assertEquals(1, result.getSyntaxErrors().size(), "Expected exactly one error.");
+    SyntaxError error = result.getSyntaxErrors().get(0);
+    assertEquals("Unexpected quantifier '*'", error.getMessage(), "Error should have the right message.");
+    assertEquals("*", error.getOffendingSyntaxElement().getText(), "Error should complain about the correct part of the regex.");
+    List<Location> locations = error.getLocations();
+    assertEquals(1, locations.size(), "Error should only have one location.");
+    assertEquals(new IndexRange(2,3), locations.get(0).getIndexRange(), "Error should have the right location.");
+    assertFalse(locations.get(0).getIndexRange().isEmpty(), "Error location should not be empty range.");
+  }
+
+}
