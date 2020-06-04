ef337a3a30b417270852127056e5a6864fa0f090
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-06-04 15:43:40 +0200

    SONARJAVA-3429: FP in S2384: support "copyOf"

--- java-checks-test-sources/src/main/files/non-compiling/checks/MutableMembersUsageCheck.java
+++ java-checks-test-sources/src/main/files/non-compiling/checks/MutableMembersUsageCheck.java
@@ -1,20 +1,31 @@
 package checks;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 class Fields {
   private static final List<String> MODIFIABLE = new ArrayList<>();
   private static final List<Integer> IMMUTABLE_LIST = List.of(1, 2, 3);
+  private static final List<Integer> IMMUTABLE_COPY_LIST = List.copyOf(new ArrayList<>());
   private static final Set<String> IMMUTABLE_SET = Set.of("a");
+  private static final Set<String> IMMUTABLE_COPY_SET = Set.copyOf(new HashSet<>());
 
   public List<Integer> immutableList() {
-    return IMMUTABLE_LIST;
+    return IMMUTABLE_LIST; // Compliant
+  }
+
+  public List<Integer> immutableListCopy() {
+    return IMMUTABLE_COPY_LIST; // Compliant
   }
 
   public Set<String> immutableSet() {
-    return IMMUTABLE_SET;
+    return IMMUTABLE_SET; // Compliant
+  }
+
+  public Set<String> immutableSetCopy() {
+    return IMMUTABLE_COPY_SET; // Compliant
   }
 
   public List<String> modifiable() {
