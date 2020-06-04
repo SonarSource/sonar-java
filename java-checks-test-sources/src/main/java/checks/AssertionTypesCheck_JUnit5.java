287c36f930b4338f6a0dc38f6b4d31241a8b492b
Alban Auzeill <alban.auzeill@sonarsource.com>
2020-06-04 15:26:39 +0200

    SONARJAVA-3399 Rule S5845: Assertions of dissimilar types should not be made

---
+++ java-checks-test-sources/src/main/java/checks/AssertionTypesCheck_JUnit5.java
@@ -0,0 +1,184 @@
+package checks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class AssertionTypesCheck_JUnit5 {
+
+  @Test
+  void test_junit() {
+    assertNotNull(bytePrimitive()); // Noncompliant [[sc=19;ec=34]] {{Change the assertion arguments to not compare dissimilar types.}}
+    assertNotNull(shortPrimitive(), "msg"); // Noncompliant
+    assertNotNull(intPrimitive(), () -> "msg"); // Noncompliant
+    assertNotNull(charPrimitive()); // Noncompliant
+    assertNotNull(getString(), "msg"); // Compliant
+
+    assertNotNull(null);
+
+    assertNull(longPrimitive()); // Noncompliant
+    assertNull(floatPrimitive(), "msg"); // Noncompliant
+    assertNull(doublePrimitive(), () -> "msg"); // Noncompliant
+
+    assertEquals(
+      null, // Noncompliant [[sc=7;ec=11;secondary=28]] {{Change the assertion arguments to not compare dissimilar types.}}
+      doublePrimitive());
+    assertEquals(null, booleanPrimitive(), "msg"); // Noncompliant
+
+    assertNotEquals(null, doublePrimitive()); // Noncompliant
+    assertNotEquals(null, booleanPrimitive(), "msg"); // Noncompliant
+
+    assertNotEquals(42, intArray()); // Noncompliant
+    assertNotEquals(new int[] {42}, intPrimitive()); // Noncompliant
+
+    Object o = new A();
+    A a = new A();
+    A a2 = new A();
+    B b = new B();
+    X x = new X();
+    Y y = new Y();
+    I1 i1 = new B();
+    I2 i2 = new Y();
+
+    assertNotEquals(new int[] {42}, o); // Compliant
+    assertNotEquals(new int[] {42}, a); // Noncompliant
+
+    assertNotEquals(new A[] {}, new A[] {});     // Compliant
+    assertNotEquals(new A[] {}, new B[] {});     // Compliant
+    assertNotEquals(new A[] {}, new X[] {});     // Noncompliant
+    assertNotEquals(new A[][] {}, new A[] {});   // Noncompliant
+    assertNotEquals(new A[][] {}, new B[][] {}); // Compliant
+    assertNotEquals(new A[][] {}, new X[][] {}); // Noncompliant
+
+    assertNotEquals(new int[] {}, new int[] {}); // Compliant
+    assertNotEquals(new A[] {}, new int[] {});   // Noncompliant
+    assertNotEquals(new long[] {}, new int[] {});// Noncompliant
+
+    assertNotEquals(a, a2);         // Compliant
+    assertNotEquals(b, new B(){});  // Compliant
+    assertNotEquals(b, i1);         // Compliant
+    assertNotEquals(i1, b);         // Compliant
+    assertNotEquals(b, new I1(){}); // Noncompliant
+    assertNotEquals(b, i2);         // Noncompliant
+    assertNotEquals(a, b);          // Compliant
+    assertNotEquals(b, a);          // Compliant
+    assertNotEquals(b, o);          // Compliant
+
+    assertNotEquals(a, x);          // Noncompliant
+    assertEquals(a, x);             // Noncompliant
+
+    // Here we are not sure, but it seems valuable to raise an issue that
+    // could be a false-positive because the negative assertion is useless by
+    // always passing if types are dissimilar
+    assertNotEquals(a, i1);         // Noncompliant
+
+    // Here we are not sure, but it seems NOT valuable to raise an issue that
+    // could be a false-positive because the positive assertion is helpful and
+    // always fails if types are dissimilar
+    assertEquals(a, i1);            // Compliant
+
+    assertNotEquals(a, i2);         // Noncompliant
+    assertEquals(a, i2);            // Compliant
+    assertEquals(a, x);             // Noncompliant
+
+    assertNotEquals(i1, i1);         // Compliant
+    assertEquals(i1, i1);            // Compliant
+
+    assertNotEquals(i1, i2);         // Noncompliant
+    assertEquals(i1, i2);            // Compliant
+
+    assertNotEquals(y, i1);         // Noncompliant
+    // Y is a final class
+    assertEquals(y, i1);            // Noncompliant
+
+    assertNotEquals(42, Integer.valueOf(42)); // Compliant
+    assertNotEquals(42L, Long.valueOf(42L)); // Compliant
+
+    assertEquals(true, booleanPrimitive());       // Compliant
+    assertEquals(Boolean.TRUE, booleanPrimitive());        // Compliant
+    assertNotEquals(Boolean.FALSE, booleanPrimitive());    // Compliant
+    assertNotEquals(false, booleanPrimitive()); // Compliant
+    assertNotEquals(5, booleanPrimitive());     // Noncompliant
+    assertNotEquals(5.0, intPrimitive());       // Compliant
+    assertEquals(5, floatPrimitive());            // Compliant
+    assertNotEquals(5L, intPrimitive());        // Compliant
+    assertEquals(5, longPrimitive());             // Compliant
+    assertNotEquals(5.0d, floatPrimitive());    // Compliant
+    assertEquals(5.0f, doublePrimitive());        // Compliant
+    assertEquals('a', charPrimitive());           // Compliant
+    assertNotEquals(97, charPrimitive());       // Compliant
+    assertEquals('a', intPrimitive());            // Compliant
+    assertNotEquals("a", charPrimitive());      // Noncompliant
+    assertNotEquals('a', getString());          // Noncompliant
+
+    assertEquals(Long.valueOf(5), Long.valueOf(5));       // Compliant
+    assertNotEquals(Integer.valueOf(5), Long.valueOf(5)); // Noncompliant
+    assertNotEquals(Long.valueOf(5), Integer.valueOf(5)); // Noncompliant
+  }
+
+  interface I1 {
+  }
+
+  interface I2 {
+  }
+
+  static class A {
+  }
+
+  static class B extends A implements I1 {
+  }
+
+  static class X {
+  }
+
+  static final class Y extends X implements I2 {
+  }
+
+  byte bytePrimitive() {
+    return 1;
+  }
+
+  short shortPrimitive() {
+    return 1;
+  }
+
+  int intPrimitive() {
+    return 1;
+  }
+
+  long longPrimitive() {
+    return 1;
+  }
+
+  float floatPrimitive() {
+    return 1;
+  }
+
+  double doublePrimitive() {
+    return 1;
+  }
+
+  boolean booleanPrimitive() {
+    return true;
+  }
+
+  char charPrimitive() {
+    return 'a';
+  }
+
+  String getString() {
+    return "a";
+  }
+
+  Character getCharacter() {
+    return 'a';
+  }
+
+  int[] intArray() {
+    return new int[0];
+  }
+
+}
---
+++ java-checks/src/test/java/org/sonar/java/checks/AssertionTypesCheckTest.java
@@ -0,0 +1,133 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.java.checks.AssertionTypesCheck.Argument;
+import org.sonar.java.checks.helpers.JParserTestUtils;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
+import org.sonar.plugins.java.api.semantic.Type;
+import org.sonar.plugins.java.api.tree.ExpressionStatementTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.sonar.java.CheckTestUtils.nonCompilingTestSourcesPath;
+import static org.sonar.java.CheckTestUtils.testSourcesPath;
+
+class AssertionTypesCheckTest {
+
+  @Test
+  void test_junit4_with_semantic() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(testSourcesPath("checks/AssertionTypesCheck_JUnit4.java"))
+      .withCheck(new AssertionTypesCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void test_junit4_unknown_symbol_coverage() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/AssertionTypesCheck_JUnit4.java"))
+      .withCheck(new AssertionTypesCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void test_junit5_with_semantic() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(testSourcesPath("checks/AssertionTypesCheck_JUnit5.java"))
+      .withCheck(new AssertionTypesCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void test_assertj_with_semantic() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(testSourcesPath("checks/AssertionTypesCheck_AssertJ.java"))
+      .withCheck(new AssertionTypesCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void test_without_semantic() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(testSourcesPath("checks/AssertionTypesCheck_JUnit5.java"))
+      .withCheck(new AssertionTypesCheck())
+      .withoutSemantic()
+      .verifyNoIssues();
+  }
+
+  @Test
+  void expected_argument_types() {
+    MethodTree method = JParserTestUtils.methodTree(JParserTestUtils.newCode(
+      "void foo() {",
+      "  bar('a', 'b', null);",
+      "}",
+      "void bar(char a, Object... b) {}"
+    ));
+    ExpressionStatementTree statement = (ExpressionStatementTree) method.block().body().get(0);
+    MethodInvocationTree asList = (MethodInvocationTree) statement.expression();
+
+    Argument firstArgument = new Argument(asList, 0);
+    assertThat(firstArgument.isPrimitive()).isTrue();
+    assertThat(firstArgument.type.isPrimitive()).isTrue();
+    assertThat(firstArgument.expressionType.fullyQualifiedName()).isEqualTo("char");
+    assertThat(firstArgument.type.fullyQualifiedName()).isEqualTo("char");
+
+    Argument secondArgument = new Argument(asList, 1);
+    assertThat(secondArgument.isPrimitive()).isTrue();
+    assertThat(secondArgument.type.isPrimitive()).isFalse();
+    assertThat(secondArgument.expressionType.fullyQualifiedName()).isEqualTo("char");
+    assertThat(secondArgument.type.fullyQualifiedName()).isEqualTo("java.lang.Character");
+
+    Argument thirdArgument = new Argument(asList, 2);
+    assertThat(thirdArgument.isPrimitive()).isFalse();
+    assertThat(thirdArgument.isNullLiteral()).isTrue();
+  }
+
+  @Test
+  void arguments_of_unknown_method() {
+    MethodTree method = JParserTestUtils.methodTree(JParserTestUtils.newCode(
+      "void foo() {",
+      "  foo('a');",
+      "}"
+    ));
+    ExpressionStatementTree statement = (ExpressionStatementTree) method.block().body().get(0);
+    MethodInvocationTree asList = (MethodInvocationTree) statement.expression();
+    Argument firstArgument = new Argument(asList, 0);
+    assertThat(firstArgument.isPrimitive()).isTrue();
+    assertThat(firstArgument.type.isPrimitive()).isTrue();
+    assertThat(firstArgument.expressionType.fullyQualifiedName()).isEqualTo("char");
+    assertThat(firstArgument.type.fullyQualifiedName()).isEqualTo("char");
+  }
+
+  @Test
+  void wrapper_type_with_invalid_primitive() {
+    Type invalid_primitive = mock(Type.class);
+    when(invalid_primitive.isPrimitive()).thenReturn(true);
+    when(invalid_primitive.fullyQualifiedName()).thenReturn("invalid_name");
+    Type type = AssertionTypesCheck.wrapperType(invalid_primitive);
+    assertThat(type).isSameAs(invalid_primitive);
+  }
+
+}
