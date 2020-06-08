2a90f87b76648b4da8483afb6f7a9f7a8a385ac3
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-06-08 18:03:21 +0200

    [REGEX] Add support of back references and boundaries

--- java-frontend/src/main/java/org/sonar/java/regex/ast/RegexTree.java
+++ java-frontend/src/main/java/org/sonar/java/regex/ast/RegexTree.java
@@ -21,6 +21,8 @@ package org.sonar.java.regex.ast;
 
 public abstract class RegexTree extends RegexSyntaxElement {
   public enum Kind {
+    BACK_REFERENCE,
+    BOUNDARY,
     CHARACTER_CLASS,
     CHARACTER_CLASS_INTERSECTION,
     CHARACTER_CLASS_UNION,
