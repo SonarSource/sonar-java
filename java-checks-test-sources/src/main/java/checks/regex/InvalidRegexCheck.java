ee41b3ebd6076ddcb9ceab60e4c95451f018e3e4
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-06-15 19:58:48 +0200

    SONARJAVA-3420 Handle invalid groups in S5856

--- java-checks-test-sources/src/main/java/checks/regex/InvalidRegexCheck.java
+++ java-checks-test-sources/src/main/java/checks/regex/InvalidRegexCheck.java
@@ -2,9 +2,9 @@ package checks.regex;
 
 import java.util.regex.Pattern;
 
-public class RegexParseErrorCheck {
+public class InvalidRegexCheck {
 
-  void noncompliant(String str) {
+  void noncompliant_syntax(String str) {
     Pattern.compile("("); // Noncompliant [[sc=23;ec=24;secondary=8]] {{Fix the syntax error inside this regex.}}
 
     str.matches("("); // Noncompliant
@@ -13,10 +13,17 @@ public class RegexParseErrorCheck {
     str.replaceAll("x{1,2,3}|(", "x"); // Noncompliant [[sc=26;ec=27;secondary=13,13]] {{Fix the syntax errors inside this regex.}}
 
     str.matches("(\\w+-(\\d+)"); // Noncompliant [[sc=30;ec=31;secondary=15]] {{Fix the syntax error inside this regex.}}
+  }
 
-    str.matches("(\\w+)-\\2"); // False Negative - group numbers not handled in rule
+  void noncompliant_backreference(String str) {
+    str.matches("(?<name>\\w+)-\\k<nae>"); // Noncompliant {{Fix the back reference error inside this regex.}}
 
-    str.matches("(?<name>\\w+)-\\k<nae>"); // False Negative - group names not handled in rule
+    str.matches(
+      "(?<g1>ab)"
+        + "\\k<g2>" // Noncompliant [[sc=12;ec=19;secondary=23,24]] {{Fix the back reference errors inside this regex.}}
+        + "\\k<g3>"
+        + "(?<g2>cd)"
+        + "(?<g3>ed)");
   }
 
   void compliant(String str) {
@@ -34,7 +41,7 @@ public class RegexParseErrorCheck {
     str.replaceAll("x{42}", "x");
 
     str.matches("(\\w+)-(\\d+)");
-    str.matches("(\\w+)-\\1");
+    str.matches("(\\w+)-\\2"); // does not make compilation of the regex fail, even if there is no 2nd group at this point
     str.matches("(?<name>\\w+)-\\k<name>");
   }
 }
