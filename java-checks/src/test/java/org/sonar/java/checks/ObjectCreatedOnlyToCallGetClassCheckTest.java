c34abc49853e33757f49626028c16c59ef275467
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-06-17 14:52:30 +0200

    Migrate S2133 test file to new test module

--- java-checks/src/test/java/org/sonar/java/checks/ObjectCreatedOnlyToCallGetClassCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/ObjectCreatedOnlyToCallGetClassCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.CheckTestUtils.testSourcesPath;
+
 class ObjectCreatedOnlyToCallGetClassCheckTest {
 
   @Test
   void test() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/ObjectCreatedOnlyToCallGetClassCheck.java")
+      .onFile(testSourcesPath("checks/ObjectCreatedOnlyToCallGetClassCheck.java"))
       .withCheck(new ObjectCreatedOnlyToCallGetClassCheck())
       .verifyIssues();
   }
