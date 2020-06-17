3f94b3cbafb2a191ca00a9865ae1745d061aec90
Sebastian Hungerecker <sebastian.hungerecker@sonarsource.com>
2020-06-17 16:43:55 +0200

    SONARJAVA-3423 Rule S5866: Case insensitive Unicode regular ...
    ... expressions should enable the “UNICODE_CASE” flag

---
+++ java-checks-test-sources/src/main/java/checks/regex/UnicodeCaseCheck.java
@@ -0,0 +1,26 @@
+package checks.regex;
+
+import java.util.regex.Pattern;
+
+public class UnicodeCaseCheck {
+
+  void noncompliant(String str) {
+    Pattern.compile("söme pättern", Pattern.CASE_INSENSITIVE); // Noncompliant [sc=37;ec=61] {{Also use "Pattern.UNICODE_CASE" to correctly handle non-ASCII letters.}}
+    str.matches("(?i)söme pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    str.matches("(?i:söme) pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    // In these cases the location of the issue is a bit confusing, but code like this will probably not occur in the wild
+    str.matches("(?iu)söme (?-u)pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    str.matches("(?iu)söme (?-U)pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+  }
+
+  void complaint(String str) {
+    Pattern.compile("söme pättern", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    str.matches("(?iu)söme pättern");
+    str.matches("(?iu:söme) pättern");
+
+    // UNICODE_CHARACTER_CLASS implies UNICODE_CASE
+    Pattern.compile("söme pättern", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CHARACTER_CLASS);
+    str.matches("(?iU)söme pättern");
+    str.matches("(?iU:söme) pättern");
+  }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/regex/UnicodeCaseCheck.java
@@ -0,0 +1,81 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.regex;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import org.sonar.check.Rule;
+import org.sonar.java.regex.RegexParseResult;
+import org.sonar.java.regex.ast.JavaCharacter;
+import org.sonar.java.regex.ast.PlainCharacterTree;
+import org.sonar.java.regex.ast.RegexBaseVisitor;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+
+@Rule(key = "S5866")
+public class UnicodeCaseCheck extends AbstractRegexCheck {
+
+  private static final String MESSAGE = "Also use %s to correctly handle non-ASCII letters.";
+
+  @Override
+  public void checkRegex(RegexParseResult regexForLiterals, MethodInvocationTree mit) {
+    if (!regexForLiterals.hasSyntaxErrors()) {
+      Visitor visitor = new Visitor();
+      visitor.setActiveFlags(getFlags(mit));
+      visitor.visit(regexForLiterals.getResult());
+      if (visitor.problematicFlagSetOutsideOfRegex) {
+        getFlagsTree(mit).ifPresent( flagsTree ->
+          reportIssue(flagsTree, String.format(MESSAGE, "\"Pattern.UNICODE_CASE\""))
+        );
+      }
+      for (JavaCharacter flag : visitor.problematicFlags) {
+        reportIssue(flag, String.format(MESSAGE, "the \"u\" flag"), null, Collections.emptyList());
+      }
+    }
+  }
+
+  private static class Visitor extends RegexBaseVisitor {
+    final Set<JavaCharacter> problematicFlags = new HashSet<>();
+
+    boolean problematicFlagSetOutsideOfRegex = false;
+
+    @Override
+    public void visitPlainCharacter(PlainCharacterTree tree) {
+      if (isProblematic(tree.getCharacter())) {
+        JavaCharacter character = getJavaCharacterForFlag(Pattern.CASE_INSENSITIVE);
+        if (character == null) {
+          problematicFlagSetOutsideOfRegex = true;
+        } else {
+          problematicFlags.add(character);
+        }
+      }
+    }
+
+    static boolean isNonAsciiLetter(char ch) {
+      return ch > 127 && Character.isLetter(ch);
+    }
+
+    boolean isProblematic(char ch) {
+      return flagActive(Pattern.CASE_INSENSITIVE) && !flagActive(Pattern.UNICODE_CASE) && isNonAsciiLetter(ch);
+    }
+  }
+
+}
