4b3ebf7f670003c3650e1b8cbb947d44477750ac
Sebastian Hungerecker <sebastian.hungerecker@sonarsource.com>
2020-06-18 11:14:36 +0200

    Fixes from review

--- java-checks-test-sources/src/main/java/checks/regex/UnicodeCaseCheck.java
+++ java-checks-test-sources/src/main/java/checks/regex/UnicodeCaseCheck.java
@@ -5,12 +5,17 @@ import java.util.regex.Pattern;
 public class UnicodeCaseCheck {
 
   void noncompliant(String str) {
-    Pattern.compile("söme pättern", Pattern.CASE_INSENSITIVE); // Noncompliant [sc=37;ec=61] {{Also use "Pattern.UNICODE_CASE" to correctly handle non-ASCII letters.}}
-    str.matches("(?i)söme pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
-    str.matches("(?i:söme) pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    Pattern.compile("söme pättern", Pattern.CASE_INSENSITIVE); // Noncompliant [[sc=37;ec=61]] {{Also use "Pattern.UNICODE_CASE" to correctly handle non-ASCII letters.}}
+    str.matches("(?i)söme pättern"); // Noncompliant [[sc=20;ec=21]] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    str.matches("(?i:söme) pättern"); // Noncompliant [[sc=20;ec=21]] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+
+    String regexPart1 = "(?i:söme)"; // Noncompliant [[sc=28;ec=29]] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    String regexPart2 = "(?i:pättern)"; // Noncompliant [[sc=28;ec=29]] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    str.matches(regexPart1 + regexPart2);
+
     // In these cases the location of the issue is a bit confusing, but code like this will probably not occur in the wild
-    str.matches("(?iu)söme (?-u)pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
-    str.matches("(?iu)söme (?-U)pättern"); // Noncompliant [sc=20;ec=21] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    str.matches("(?iu)söme (?-u)pättern"); // Noncompliant [[sc=20;ec=21]] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
+    str.matches("(?iu)söme (?-U)pättern"); // Noncompliant [[sc=20;ec=21]] {{Also use the "u" flag to correctly handle non-ASCII letters.}}
   }
 
   void complaint(String str) {
