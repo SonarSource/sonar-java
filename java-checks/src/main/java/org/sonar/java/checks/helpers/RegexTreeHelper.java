b121b174320601b8a76de5b844cc9cf1414e1636
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-06-30 17:00:56 +0200

    Extract logic to detect grapheme cluster

---
+++ java-checks/src/main/java/org/sonar/java/checks/helpers/RegexTreeHelper.java
@@ -0,0 +1,78 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import javax.annotation.Nullable;
+import org.sonar.java.regex.RegexCheck;
+import org.sonar.java.regex.ast.JavaCharacter;
+import org.sonar.java.regex.ast.PlainCharacterTree;
+import org.sonar.java.regex.ast.RegexSyntaxElement;
+import org.sonar.java.regex.ast.RegexTree;
+
+public class RegexTreeHelper {
+
+  // M (Mark) is "a character intended to be combined with another character (e.g. accents, umlauts, enclosing boxes, etc.)."
+  // See https://www.regular-expressions.info/unicode.html
+  private static final Pattern MARK_PATTERN = Pattern.compile("\\p{M}");
+
+  private RegexTreeHelper() {
+    // Utils class
+  }
+
+  public static List<RegexCheck.RegexIssueLocation> getGraphemeInList(List<RegexTree> trees) {
+    List<RegexCheck.RegexIssueLocation> result = new ArrayList<>();
+    RegexSyntaxElement startGrapheme = null;
+    RegexSyntaxElement endGrapheme = null;
+    for (RegexTree child : trees) {
+      if (child.is(RegexTree.Kind.PLAIN_CHARACTER)) {
+        JavaCharacter currentCharacter = ((PlainCharacterTree) child).getContents();
+        if (!currentCharacter.isEscapedUnicode()) {
+          if (!isMark(currentCharacter)) {
+            addCurrentGrapheme(result, startGrapheme, endGrapheme);
+            startGrapheme = child;
+            endGrapheme = null;
+          } else if (startGrapheme != null) {
+            endGrapheme = child;
+          }
+          continue;
+        }
+      }
+      addCurrentGrapheme(result, startGrapheme, endGrapheme);
+      startGrapheme = null;
+      endGrapheme = null;
+    }
+    addCurrentGrapheme(result, startGrapheme, endGrapheme);
+    return result;
+  }
+
+  private static boolean isMark(JavaCharacter currentChar) {
+    return MARK_PATTERN.matcher(String.valueOf(currentChar.getCharacter())).matches();
+  }
+
+  private static void addCurrentGrapheme(List<RegexCheck.RegexIssueLocation> result, @Nullable RegexSyntaxElement start, @Nullable RegexSyntaxElement end) {
+    if (start != null && end != null) {
+      result.add(new RegexCheck.RegexIssueLocation(start, end, ""));
+    }
+  }
+
+}
