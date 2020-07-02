c26a2de444534168dfbc365b1bbb19cf19ed8b7a
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-07-02 16:28:34 +0200

    SONARJAVA-3413 RSPEC-5842 Regex repetition pattern's body should not match the empty String (#3074)
    Co-authored-by: Quentin Jaquier <quentin.jaquier@sonarsource.com>

---
+++ java-checks-test-sources/src/main/java/checks/regex/EmptyStringRepetitionCheck.java
@@ -0,0 +1,51 @@
+package checks.regex;
+
+class EmptyStringRepetitionCheck {
+
+  private static final String REPLACEMENT = "empty";
+
+  void noncompliant(String input) {
+    input.replaceFirst("(?:)*", REPLACEMENT); // Noncompliant [[sc=25;ec=29]] {{Rework this part of the regex to not match the empty string.}}
+    input.replaceFirst("(?:)?", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:)+", REPLACEMENT); // Noncompliant
+    input.replaceFirst("()*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("()?", REPLACEMENT); // Noncompliant
+    input.replaceFirst("()+", REPLACEMENT); // Noncompliant
+    input.replaceFirst("xyz|(?:)*", REPLACEMENT); // Noncompliant [[sc=29;ec=33]]
+    input.replaceFirst("(?:|x)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x|)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x|y*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x*|y*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x?|y*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x?)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x*)?", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x?)?", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x*)+", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x?)+", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(x*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("((x*))*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:x*y*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:())*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(?:(?:))*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("((?i))*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(())*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(()x*)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(()|x)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("($)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("(\\b)*", REPLACEMENT); // Noncompliant
+    input.replaceFirst("((?!x))*", REPLACEMENT); // Noncompliant
+  }
+
+  void compliant(String input) {
+    input.replaceFirst("x*|", REPLACEMENT);
+    input.replaceFirst("x*|", REPLACEMENT);
+    input.replaceFirst("x*", REPLACEMENT);
+    input.replaceFirst("x?", REPLACEMENT);
+    input.replaceFirst("(?:x|y)*", REPLACEMENT);
+    input.replaceFirst("(?:x+)+", REPLACEMENT);
+    input.replaceFirst("(?:x+)*", REPLACEMENT);
+    input.replaceFirst("(?:x+)?", REPLACEMENT);
+    input.replaceFirst("((x+))*", REPLACEMENT);
+  }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/regex/EmptyStringRepetitionCheck.java
@@ -0,0 +1,75 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.regex;
+
+import java.util.Collections;
+import org.sonar.check.Rule;
+import org.sonar.java.regex.RegexParseResult;
+import org.sonar.java.regex.ast.DisjunctionTree;
+import org.sonar.java.regex.ast.GroupTree;
+import org.sonar.java.regex.ast.RegexBaseVisitor;
+import org.sonar.java.regex.ast.RegexTree;
+import org.sonar.java.regex.ast.RepetitionTree;
+import org.sonar.java.regex.ast.SequenceTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+
+@Rule(key = "S5842")
+public class EmptyStringRepetitionCheck extends AbstractRegexCheck {
+
+  private static final String MESSAGE = "Rework this part of the regex to not match the empty string.";
+
+  @Override
+  public void checkRegex(RegexParseResult regex, MethodInvocationTree mit) {
+    new Visitor().visit(regex);
+  }
+
+  private class Visitor extends RegexBaseVisitor {
+
+    @Override
+    public void visitRepetition(RepetitionTree tree) {
+      RegexTree element = tree.getElement();
+      if (matchEmptyString(element)) {
+        reportIssue(element, MESSAGE, null, Collections.emptyList());
+      }
+    }
+
+    private boolean matchEmptyString(RegexTree element) {
+      switch (element.kind()) {
+        case SEQUENCE:
+          return ((SequenceTree) element).getItems().stream().allMatch(this::matchEmptyString);
+        case DISJUNCTION:
+          return ((DisjunctionTree) element).getAlternatives().stream().anyMatch(this::matchEmptyString);
+        case REPETITION:
+          return ((RepetitionTree) element).getQuantifier().getMinimumRepetitions() == 0;
+        case LOOK_AROUND:
+        case BOUNDARY:
+          return true;
+        default:
+          if (element instanceof GroupTree) {
+            RegexTree nestedElement = ((GroupTree) element).getElement();
+            return nestedElement == null || matchEmptyString(nestedElement);
+          }
+          return false;
+      }
+    }
+
+  }
+
+}
