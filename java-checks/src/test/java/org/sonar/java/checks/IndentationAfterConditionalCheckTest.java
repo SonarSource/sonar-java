9fdef9fcd48ae2485a88d003cf538ba7a4abe360
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-07-06 17:42:36 +0200

    S3973 Move test file to new module

--- java-checks/src/test/java/org/sonar/java/checks/IndentationAfterConditionalCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/IndentationAfterConditionalCheckTest.java
@@ -22,11 +22,13 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.CheckTestUtils.testSourcesPath;
+
 class IndentationAfterConditionalCheckTest {
   @Test
   void test() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/IndentationAfterConditionalCheck.java")
+      .onFile(testSourcesPath("checks/IndentationAfterConditionalCheck.java"))
       .withCheck(new IndentationAfterConditionalCheck())
       .verifyIssues();
   }
