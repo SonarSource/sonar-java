924c63db3bc52e7a9840da36f53a61ae16e12ed7
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-07-06 11:26:12 +0200

    [REGEX] Report only one issue per element and rule

--- java-checks-test-sources/src/main/java/checks/regex/EmptyStringRepetitionCheck.java
+++ java-checks-test-sources/src/main/java/checks/regex/EmptyStringRepetitionCheck.java
@@ -48,4 +48,22 @@ class EmptyStringRepetitionCheck {
     input.replaceFirst("(?:x+)?", REPLACEMENT);
     input.replaceFirst("((x+))*", REPLACEMENT);
   }
+
+  void no_duplications(String input) {
+    String regex = "(?:)*"; // Noncompliant
+
+    input.replaceFirst(regex, REPLACEMENT);
+    input.replaceFirst(regex, REPLACEMENT);
+
+    String regex2_1 = "(?:"; // Noncompliant
+    String regex2_2 = ")*";
+
+    input.replaceFirst(regex2_1 + regex2_2, REPLACEMENT);
+    input.replaceFirst(regex2_1 + regex2_2, REPLACEMENT);
+
+    String regex3_1 = "(?:"; // Compliant
+    String regex3_2 = ")*";
+
+    input.replaceFirst(regex3_1 + "x|y" +  regex3_2, REPLACEMENT);
+  }
 }
