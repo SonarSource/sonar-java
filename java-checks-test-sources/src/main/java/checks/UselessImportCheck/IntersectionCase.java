aa3a347b073dbf8684a858b069868850e8640aa9
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-07-09 16:35:23 +0200

    Relocate test files

--- java-checks-test-sources/src/main/java/checks/UselessImportCheck/IntersectionCase.java
+++ java-checks-test-sources/src/main/java/checks/UselessImportCheck/IntersectionCase.java
@@ -4,7 +4,7 @@ import java.io.Serializable; // Compliant
 import java.util.Optional; // Noncompliant
 import java.util.function.Function;
 
-public class TestingSerializable {
+class TestingSerializable {
   Function<String, Integer> getScalarReferenceResolver() {
     return (Function<String, Integer> & Serializable) absoluteReference -> {
       return null;
