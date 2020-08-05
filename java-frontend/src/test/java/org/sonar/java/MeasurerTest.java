b03fa9b2de918b623a85409c91a8b229d554c84a
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-08-05 10:31:39 +0200

    Add unit test for TestFileMeasurer

--- java-frontend/src/test/java/org/sonar/java/MeasurerTest.java
+++ java-frontend/src/test/java/org/sonar/java/MeasurerTest.java
@@ -23,8 +23,11 @@ import java.io.File;
 import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.sonar.api.SonarEdition;
+import org.sonar.api.SonarQubeSide;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
+import org.sonar.api.internal.SonarRuntimeImpl;
 import org.sonar.api.issue.NoSonarFilter;
 import org.sonar.api.utils.PathUtils;
 import org.sonar.java.model.JavaVersionImpl;
@@ -86,21 +89,71 @@ class MeasurerTest {
     checkMetric("EmptyFile.java", "ncloc", 0);
   }
 
+  @Test
+  void verify_ncloc_for_test_are_sent_to_sq_8_5() {
+    InputFile inputFile = createFile("LinesOfCode.java");
+    context.setRuntime(
+      SonarRuntimeImpl.forSonarQube(org.sonar.api.utils.Version.create(8,5),
+        SonarQubeSide.SCANNER,
+        SonarEdition.COMMUNITY));
+
+    scanOnTestSquid(inputFile, true);
+
+    assertThat(context.measures(inputFile.key())).hasSize(1);
+    assertThat(context.measure(inputFile.key(), "ncloc").value()).isEqualTo(2);
+  }
+
+  @Test
+  void verify_ncloc_for_test_are_not_sent_to_sq_7_9() {
+    InputFile inputFile = createFile("LinesOfCode.java");
+    context.setRuntime(
+      SonarRuntimeImpl.forSonarQube(org.sonar.api.utils.Version.create(7,9),
+        SonarQubeSide.SCANNER,
+        SonarEdition.COMMUNITY));
+
+    scanOnTestSquid(inputFile, true);
+
+    assertThat(context.measures(inputFile.key())).isEmpty();
+  }
+
+  @Test
+  void verify_ncloc_for_test_are_not_sent_to_sonarlint() {
+    InputFile inputFile = createFile("LinesOfCode.java");
+    context.setRuntime(SonarRuntimeImpl.forSonarLint(org.sonar.api.utils.Version.create(7,9)));
+
+    scanOnTestSquid(inputFile, true);
+
+    assertThat(context.measures(inputFile.key())).isEmpty();
+  }
+
   /**
    * Utility method to quickly get metric out of a file.
    */
   private void checkMetric(String filename, String metric, Number expectedValue) {
-    String relativePath = PathUtils.sanitize(new File(BASE_DIR, filename).getPath());
-    InputFile inputFile = TestUtils.inputFile(relativePath);
+    InputFile inputFile = createFile(filename);
+
+    scanOnTestSquid(inputFile, false);
+
+    assertThat(context.measures(inputFile.key())).hasSize(NB_OF_METRICS);
+    assertThat(context.measure(inputFile.key(), metric).value()).isEqualTo(expectedValue);
+  }
+
+  private void scanOnTestSquid(InputFile inputFile, boolean asTest) {
     context.fileSystem().add(inputFile);
 
     Measurer measurer = new Measurer(context, mock(NoSonarFilter.class));
     JavaSquid squid = new JavaSquid(new JavaVersionImpl(), null, measurer, null, null, new JavaCheck[0]);
 
-    squid.scan(Collections.singletonList(inputFile), Collections.emptyList(), Collections.emptyList());
+    if (asTest) {
+      squid.scan(Collections.emptyList(), Collections.singletonList(inputFile), Collections.emptyList());
+    } else {
+      squid.scan(Collections.singletonList(inputFile), Collections.emptyList(), Collections.emptyList());
+    }
+  }
 
-    assertThat(context.measures(inputFile.key())).hasSize(NB_OF_METRICS);
-    assertThat(context.measure(inputFile.key(), metric).value()).isEqualTo(expectedValue);
+  private InputFile createFile(String filename) {
+    String relativePath = PathUtils.sanitize(new File(BASE_DIR, filename).getPath());
+    return TestUtils.inputFile(relativePath);
   }
 
 }
