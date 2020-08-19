eb766f47f46ebf4c61bb6e87221c6677c29532d3
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-08-19 10:42:18 +0200

    Drop semantic IT
    Less is More: we do not have anymore a semantic engine, we rely
    fully on Eclipse compiler and semantic. No reason to test it
    anymore, especially on correctly built projects.

--- its/semantic/java-debugging-plugin/src/test/files/UnknownConstructorCallCheck.java
+++
@@ -1,6 +0,0 @@
-class A {
-  void foo() {
-    new A(); // Compliant
-    new B(); // Noncompliant
-  }
-}
--- its/semantic/java-debugging-plugin/src/test/files/UnknownMethodInvocationsCheck.java
+++
@@ -1,9 +0,0 @@
-class A {
-  void foo() {
-    foo(); // Compliant
-    bar(); // Noncompliant
-
-    this.foo(); // Compliant
-    this.bar(); // Noncompliant
-  }
-}
