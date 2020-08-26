3d59e973da58a08458702de7c84581c4a4a2fcd5
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-08-26 17:38:37 +0200

    Move S3052 test file in compiled project

--- java-checks/src/test/java/org/sonar/java/checks/DefaultInitializedFieldCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/DefaultInitializedFieldCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.CheckTestUtils.testSourcesPath;
+
 class DefaultInitializedFieldCheckTest {
 
   @Test
   void test() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/DefaultInitializedFieldCheck.java")
+      .onFile(testSourcesPath("checks/DefaultInitializedFieldCheck.java"))
       .withCheck(new DefaultInitializedFieldCheck())
       .verifyIssues();
   }
