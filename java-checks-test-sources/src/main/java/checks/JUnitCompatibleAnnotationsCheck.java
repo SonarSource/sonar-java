f44a2ff26e5b2677d41a6874879c577aea90f5a2
Margarita <margarita.nedzelska@sonarsource.com>
2020-09-03 18:12:36 +0200

    First implementation

---
+++ java-checks-test-sources/src/main/java/checks/JUnitCompatibleAnnotationsCheck.java
@@ -0,0 +1,22 @@
+package checks;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class JUnitCompatibleAnnotationsCheck {
+
+  @Test
+  @RepeatedTest(2)
+  void test() { } // Noncompliant {{Remove one of these conflicting annotations.}}
+
+  @RepeatedTest(4)
+  void test2() { } // Compliant
+
+  @ParameterizedTest
+  @Test
+  @MethodSource("methodSource")
+  void test3(int argument) { } // Noncompliant
+
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/tests/JUnitCompatibleAnnotationsCheck.java
@@ -0,0 +1,59 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.tests;
+
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.semantic.SymbolMetadata;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Rule(key = "S5967")
+public class JUnitCompatibleAnnotationsCheck extends IssuableSubscriptionVisitor {
+
+  private static final Set<String> annotations = new HashSet<>(Arrays.asList(
+    "org.junit.jupiter.api.Test",
+    "org.junit.jupiter.api.RepeatedTest",
+    "org.junit.jupiter.api.TestFactory",
+    "org.junit.jupiter.api.TestTemplate",
+    "org.junit.jupiter.params.ParameterizedTest"
+  ));
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return Collections.singletonList(Tree.Kind.METHOD);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    MethodTree method = (MethodTree) tree;
+    Symbol.MethodSymbol symbol = method.symbol();
+    List<SymbolMetadata.AnnotationInstance> filteredAnnotations = symbol.metadata().annotations().stream()
+      .filter(annotation -> annotations.contains(annotation.symbol().type().fullyQualifiedName()))
+      .collect(Collectors.toList());
+    if (filteredAnnotations.size() > 1) {
+      reportIssue(method.simpleName(), "Remove one of these conflicting annotations."); //TODO Add secondary location
+    }
+  }
+}
