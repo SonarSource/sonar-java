8367c4568a755ec954714894aa291645c66aee8d
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-09-09 10:51:38 +0200

    SONARJAVA-3509 RSPEC-5958 AssertJ "assertThatThrownBy" should not be used alone

---
+++ java-checks/src/main/java/org/sonar/java/checks/tests/AssertThatThrownByAloneCheck.java
@@ -0,0 +1,48 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.tests;
+
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.java.model.ExpressionUtils;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S5958")
+public class AssertThatThrownByAloneCheck extends AbstractMethodDetection {
+
+  @Override
+  protected MethodMatchers getMethodInvocationMatchers() {
+    return MethodMatchers.create()
+      .ofTypes("org.assertj.core.api.Assertions")
+      .names("assertThatThrownBy")
+      .withAnyParameters()
+      .build();
+  }
+
+  @Override
+  protected void onMethodInvocationFound(MethodInvocationTree mit) {
+    if (mit.parent().is(Tree.Kind.EXPRESSION_STATEMENT)) {
+      reportIssue(ExpressionUtils.methodName(mit), "Test further the exception raised by this assertThatThrownBy call.");
+    }
+  }
+
+}
