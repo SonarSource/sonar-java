98280caae3f77d0473813cf523d2098d05bbcca9
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-09-15 10:49:16 +0200

    SONARJAVA-3530 Fix FPs on classes ending with *Tests

--- java-checks/src/test/files/checks/naming/BadTestClassNameCheck.java
+++ java-checks/src/test/files/checks/naming/BadTestClassNameCheck.java
@@ -1,12 +1,17 @@
 import org.junit.Test;
 
-class A { // Noncompliant {{Rename class "A" to match the regular expression: '^((Test|IT)[a-zA-Z0-9]+|[A-Z][a-zA-Z0-9]*(Test|IT|TestCase|ITCase))$'}}
+class A { // Noncompliant {{Rename class "A" to match the regular expression: '^((Test|IT)[a-zA-Z0-9]+|[A-Z][a-zA-Z0-9]*(Test|Tests|TestCase|IT|ITCase))$'}}
   @Test
   void foo() {}
 }
 
 class ATest { // Compliant
   @org.testng.annotations.Test
+  void foo() {}
+}
+
+class SomeTests { // Compliant
+  @Test
   void foo() {}
 }
 
