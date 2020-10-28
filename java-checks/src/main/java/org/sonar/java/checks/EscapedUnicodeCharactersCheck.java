c76700278998e1ec77b1bf939d8a27a1de00c840
David Corbett <dcorbett@basistech.com>
2020-10-28 14:13:36 -0400

    Fix FP on S2973 for lowercase escape sequences

--- java-checks/src/main/java/org/sonar/java/checks/EscapedUnicodeCharactersCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/EscapedUnicodeCharactersCheck.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableSet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -73,7 +74,7 @@ public class EscapedUnicodeCharactersCheck extends IssuableSubscriptionVisitor {
   }
 
   private static boolean isPrintableEscapedUnicode(String input) {
-    String hexValue = input.substring(input.length() - 4);
+    String hexValue = input.substring(input.length() - 4).toUpperCase(Locale.ROOT);
     if (UNICODE_WHITESPACES.contains(hexValue)) {
       return false;
     }
