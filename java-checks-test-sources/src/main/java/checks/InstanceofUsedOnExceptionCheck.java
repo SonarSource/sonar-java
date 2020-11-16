289c4e175257ec365168a426893731f25102b7fa
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-11-16 14:46:09 +0100

    SONARJAVA-3613 FP in S1193 when the catch block contains more code

--- java-checks-test-sources/src/main/java/checks/InstanceofUsedOnExceptionCheck.java
+++ java-checks-test-sources/src/main/java/checks/InstanceofUsedOnExceptionCheck.java
@@ -3,20 +3,176 @@ package checks;
 import java.io.IOException;
 
 class InstanceofUsedOnExceptionCheck {
+  private void withEalseIf() {
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+
+      } else if (e instanceof IllegalArgumentException) { // Noncompliant
+
+      }
+    }
+
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+
+      } else if (e instanceof IllegalArgumentException && anotherCondition()) { // Compliant, not possible to refactor in a nicer way
+
+      }
+    }
+  }
+
   private void f(MyException foo) {
     try {
     } catch (Exception e) {
       if (e instanceof IOException) {} // Noncompliant [[sc=13;ec=23]] {{Replace the usage of the "instanceof" operator by a catch block.}}
-      if (e instanceof MyInterface) {}
-      if (foo instanceof IOException) {}
-      if (foo instanceof e) {}
-      if (foo.foo instanceof IOException) {}
-      if (e
-          instanceof IOException) {} // Noncompliant
     }
-
+    try {
+    } catch (Exception e) {
+      if (e instanceof MyInterface) {}
+    }
+    try {
+    } catch (Exception e) {
+      if (foo instanceof IOException) {}
+    }
+    try {
+    } catch (Exception e) {
+      if (foo instanceof e) {}
+    }
+    try {
+    } catch (Exception e) {
+      if (foo.foo instanceof IOException) {}
+    }
+    try {
+    } catch (Exception e) {
+      if (e
+        instanceof IOException) {} // Noncompliant
+    }
     Object e = 0;
     if (e instanceof Integer) {}
+  }
+
+  private void withMoreCode() {
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { } // Compliant, refactoring results in duplication
+      doSomething();
+    }
+
+    try {
+    } catch (Exception e) {
+      doSomething();
+      if (e instanceof IOException) { } // Compliant
+    }
+
+    try {
+    } catch (Exception e) {
+      doSomething();
+      if (e instanceof IOException) { } // Compliant
+      doSomethingElse();
+    }
+  }
+
+  private void withoutTrivialInstanceOf() {
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+        doSomething();
+      }
+      if (e instanceof IllegalArgumentException && anotherCondition()) { // Compliant, not possible to refactor in a nicer way
+        doSomethingElse();
+      }
+    }
+
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+        doSomething();
+      } else {
+        doSomething();
+      }
+    }
+  }
+
+  private void withElseIf() {
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+        doSomething();
+      } else if (e instanceof IllegalArgumentException) { // Noncompliant
+        doSomethingElse();
+      }
+    }
+
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+
+        doSomething();
+      } else if (e instanceof IllegalArgumentException && anotherCondition()) { // Compliant, not possible to refactor in a nicer way
+
+      } else if (e instanceof MyException) { // Noncompliant
+
+        doSomethingElse();
+        doSomething();
+      }
+    }
+  }
+
+  private void withThrow() throws IllegalAccessException {
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) {// Noncompliant
+        throw new IllegalAccessException("");
+      }
+      throw e;
+    }
+
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) { // Noncompliant
+        doSomething();
+      }
+      throw e;
+    }
+
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException && anotherCondition()) { // Compliant, not possible to refactor in a nicer way
+        throw new IllegalAccessException("");
+      }
+      throw e;
+    }
+  }
+
+  private int withReturn() {
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException) {// Noncompliant
+        return 1;
+      }
+      return 2;
+    }
+
+    try {
+    } catch (Exception e) {
+      if (e instanceof IOException && anotherCondition()) { // Compliant, not possible to refactor in a nicer way
+        return 1;
+      }
+      return 2;
+    }
+    return 0;
+  }
+
+  private boolean anotherCondition() {
+    return true;
+  }
+
+  private void doSomething() {
+  }
+
+  private void doSomethingElse() {
   }
 
   private static class MyException extends IOException implements MyInterface {
