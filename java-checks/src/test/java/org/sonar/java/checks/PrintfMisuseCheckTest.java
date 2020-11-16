948f27165056759fb84d387afdb7adada24304d7
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-11-16 11:47:19 +0100

    Move test files into compiled project

--- java-checks/src/test/java/org/sonar/java/checks/PrintfMisuseCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/PrintfMisuseCheckTest.java
@@ -22,6 +22,7 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.CheckTestUtils.nonCompilingTestSourcesPath;
 import static org.sonar.java.CheckTestUtils.testSourcesPath;
 
 class PrintfMisuseCheckTest {
@@ -37,7 +38,7 @@ class PrintfMisuseCheckTest {
   @Test
   void test_lambda_in_unknown_method_invocation() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/PrintfMisuseCheckLambda.java")
+      .onFile(nonCompilingTestSourcesPath("checks/PrintfMisuseCheckLambda.java"))
       .withCheck(new PrintfMisuseCheck())
       .verifyNoIssues();
   }
