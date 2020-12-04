cb91eaaae1b93c10ca9651c54e0f533f3017f6e5
Alban Auzeill <alban.auzeill@sonarsource.com>
2020-12-04 16:19:06 +0100

    SONARJAVA-3550 S5994 fix false positive for fixed repetition (#3323)

--- java-checks-test-sources/src/main/java/checks/regex/PossessiveQuantifierContinuationCheck.java
+++ java-checks-test-sources/src/main/java/checks/regex/PossessiveQuantifierContinuationCheck.java
@@ -21,6 +21,7 @@ public class PossessiveQuantifierContinuationCheck {
     f(compile("(bx++)+x")); // false-negative, limitation of the algorithm when there's infinite loop
     f(compile("(?:xx++)+x")); // Noncompliant [[sc=25;ec=26]]
     f(compile("(xx++)x")); // Noncompliant [[sc=22;ec=23]]
+    f(compile("(:[0-9])?+(:[0-9])?+"));
   }
 
 }
