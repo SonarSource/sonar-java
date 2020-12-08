0a34a1be5199ec9be0976e091a17e52c6edc8921
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2020-12-08 11:20:15 +0100

    SONARJAVA-3583 S3516: Add a message on the secondary location

--- java-frontend/src/test/files/se/InvariantReturnCheck.java
+++ java-frontend/src/test/files/se/InvariantReturnCheck.java
@@ -2,11 +2,11 @@ class A {
   private int foo(boolean a) { // Noncompliant [[flows=issue1]] {{Refactor this method to not always return the same value.}}
     int b = 12;
     if (a) {
-      return b; // flow@issue1 [[order=1]]
+      return b; // flow@issue1 [[order=1]] {{Returned value.}}
     } else if (polop()) {
-      return b;  // flow@issue1 [[order=2]]
+      return b;  // flow@issue1 [[order=2]] {{Returned value.}}
     }
-    return b; // flow@issue1 [[order=3]]
+    return b; // flow@issue1 [[order=3]] {{Returned value.}}
   }
 
   private int foo2(boolean a) {
