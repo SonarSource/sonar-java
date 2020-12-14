01a902afb8ba57861b1042f1c36515c5af6dbb50
Dorian Burihabwa <75226315+dorian-burihabwa-sonarsource@users.noreply.github.com>
2020-12-14 17:36:26 +0100

    SONARJAVA-3632 Rule S6104: Map "computeIfAbsent()" should not be used to add "null" values. (#3346)

---
+++ java-checks-test-sources/src/main/java/checks/NullReturnedOnComputeIfPresentOrAbsent.java
@@ -0,0 +1,53 @@
+package checks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NullReturnedOnComputeIfPresentOrAbsent {
+
+  public void badComputeIfPresent() {
+    Map<String, String> map = new HashMap<>();
+    map.computeIfPresent("myKey", (key, value) -> // Noncompliant [[sc=9;ec=25;secondary=+1]] {{Use "Map.containsKey(key)" followed by "Map.put(key, null)" to add null values.}}
+      null);
+
+    map.computeIfPresent("myKey", NullReturnedOnComputeIfPresentOrAbsent::presentLambda); // Compliant uninteresting corner case
+
+    String nullValue = null;
+    map.computeIfPresent("myKey", (key, value) -> nullValue); // Compliant uninteresting corner case
+
+    map.computeIfPresent("myKey", (key, value) -> value + " modified"); // Compliant
+
+    TreeMap<String, String> second = new TreeMap<>();
+    second.computeIfPresent("myKey", (key, value) -> null); // Compliant corner case where subtype of java.util.Map may break specification
+
+    Map<String, String> third = new TreeMap<>();
+    third.computeIfPresent("myKey", (key, value) -> null); // Noncompliant
+  }
+
+  public void badComputeIfAbsent() {
+    Map<String, String> map = new HashMap<>();
+    map.computeIfAbsent("myKey", key -> // Noncompliant [[sc=9;ec=24;secondary=+1]] {{Use "Map.containsKey(key)" followed by "Map.put(key, null)" to add null values.}}
+      null);
+    map.computeIfAbsent("myKey", NullReturnedOnComputeIfPresentOrAbsent::absentLambda); // Compliant uninteresting corner case
+
+    String nullValue = null;
+    map.computeIfAbsent("myKey", key -> nullValue); // Compliant uninteresting corner case
+
+    map.computeIfAbsent("myKey", key -> "brand new"); // Compliant
+
+    TreeMap<String, String> second = new TreeMap<>();
+    second.computeIfAbsent("myKey", key -> null); // Compliant corner case where subtype of java.util.Map may break specification
+
+    Map<String, String> third = new TreeMap<>();
+    third.computeIfAbsent("myKey", key -> null); // Noncompliant
+  }
+
+  public static String presentLambda(String k, String v) {
+    return null;
+  }
+
+  public static String absentLambda(String k) {
+    return null;
+  }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/NullReturnedOnComputeIfPresentOrAbsentCheck.java
@@ -0,0 +1,77 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.java.model.ExpressionUtils;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.Arguments;
+import org.sonar.plugins.java.api.tree.LambdaExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S6104")
+public class NullReturnedOnComputeIfPresentOrAbsentCheck extends AbstractMethodDetection {
+  private static final String PRIMARY_MESSAGE = "Use \"Map.containsKey(key)\" followed by \"Map.put(key, null)\" to add null values.";
+  private static final String SECONDARY_MESSAGE = "null literal in the arguments";
+  private static final MethodMatchers COMPUTE_IF = MethodMatchers.create()
+    .ofTypes("java.util.Map")
+    .names("computeIfPresent", "computeIfAbsent")
+    .addParametersMatcher(MethodMatchers.ANY, MethodMatchers.ANY)
+    .build();
+
+  /**
+   * The method is overridden for optimisation by restricting node visits to method invocations.
+   */
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return Collections.singletonList(Tree.Kind.METHOD_INVOCATION);
+  }
+
+  @Override
+  protected MethodMatchers getMethodInvocationMatchers() {
+    return COMPUTE_IF;
+  }
+
+  @Override
+  public void onMethodInvocationFound(MethodInvocationTree invocation) {
+    Arguments arguments = invocation.arguments();
+    getNullReturnInLambda(arguments.get(1)).ifPresent(nullReturn ->
+      reportIssue(ExpressionUtils.methodName(invocation),
+        PRIMARY_MESSAGE,
+        Collections.singletonList(new JavaFileScannerContext.Location(SECONDARY_MESSAGE, nullReturn)),
+        null));
+  }
+
+  private static Optional<Tree> getNullReturnInLambda(Tree tree) {
+    if (tree.is(Tree.Kind.LAMBDA_EXPRESSION)) {
+      Tree body = ((LambdaExpressionTree) tree).body();
+      if (body.is(Tree.Kind.NULL_LITERAL)) {
+        return Optional.of(body);
+      }
+    }
+    return Optional.empty();
+  }
+}
