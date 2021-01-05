facd6fe57b08565d1857528eab2b429910b06fb1
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-01-05 16:23:24 +0100

    Remove duplicated test case

--- java-checks-test-sources/src/main/java/checks/JdbcDriverExplicitLoadingCheck_no_version.java
+++ java-checks-test-sources/src/main/java/checks/JdbcDriverExplicitLoadingCheck_no_version.java
@@ -4,6 +4,5 @@ class JdbcDriverExplicitLoadingCheck_no_version {
   void doStuff() throws ClassNotFoundException {
     Class.forName("org.h2.Driver"); // Noncompliant [[sc=11;ec=18]] {{Remove this "Class.forName()", it is useless. (sonar.java.source not set. Assuming 6 or greater.)}}
     Class.forName("java.lang.String");
-    Class.forName(javax.print.ServiceUIFactory.DIALOG_UI);
   }
 }
