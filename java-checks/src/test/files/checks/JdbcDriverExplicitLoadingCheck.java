00517fd394f52c76b3ba191929429b448f5f7360
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2021-01-05 16:59:41 +0100

    SONARJAVA-3651 S4925: add support for DB2 JDBC Type 4 driver (#3370)

--- java-checks/src/test/files/checks/JdbcDriverExplicitLoadingCheck.java
+++
@@ -1,8 +0,0 @@
-
-class A {
-  void doStuff() {
-    Class.forName("org.h2.Driver"); // Noncompliant [[sc=11;ec=18]] {{Remove this "Class.forName()", it is useless.}}
-    Class.forName("java.lang.String");
-    Class.forName(javax.print.ServiceUIFactory.DIALOG_UI);
-  }
-}
