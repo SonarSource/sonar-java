585f55f7c2f829be3b5dcd96f848d010fca03f42
Margarita <margarita.nedzelska@sonarsource.com>
2021-01-14 18:08:18 +0100

    SONARJAVA-3658 Add support of TEXT BLOCKs in S1858 (StringToStringCheck)

---
+++ java-checks-test-sources/src/main/files/non-compiling/checks/TextBlockToString.java
@@ -0,0 +1,11 @@
+package checks;
+
+public class TextBlockToString {
+  
+  String string = """
+              
+  hello
+  
+  """.toString(); // Noncompliant@-4 [[sc=19;ec=5]] {{there's no need to call "toString()" on a text block.}}
+
+}
