baa1f341c62e04fced61cb1edf7678e9681bed6f
Margarita <margarita.nedzelska@sonarsource.com>
2021-01-15 14:16:48 +0100

    SONARJAVA-3255 Rule S5663: Simple string literal should be used for single line strings

---
+++ java-checks/src/main/java/org/sonar/java/checks/SimpleStringLiteralForSingleLineStringsCheck.java
@@ -0,0 +1,47 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.Collections;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.LiteralTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S5663")
+public class SimpleStringLiteralForSingleLineStringsCheck extends IssuableSubscriptionVisitor {
+  private static final String MESSAGE = "Use simple literal for a single-line string.";
+  
+  @Override
+  public List<Tree.Kind> nodesToVisit() { 
+    return Collections.singletonList(Tree.Kind.TEXT_BLOCK);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    LiteralTree textBlock = (LiteralTree) tree;
+    String value = textBlock.value();
+
+    if (value.split("(\r?\n)|\r").length <= 2) {
+      reportIssue(tree, MESSAGE);
+    }
+  }
+}
