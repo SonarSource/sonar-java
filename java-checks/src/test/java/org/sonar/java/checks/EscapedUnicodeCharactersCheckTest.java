9d023a808fe9e6773a0d98f9f5c0c8512b10d3b6
Margarita <margarita.nedzelska@sonarsource.com>
2021-01-19 17:35:16 +0100

    SONARJAVA-3665 Add support of Text Blocks in S2973 (Escaped unicode characters)

--- java-checks/src/test/java/org/sonar/java/checks/EscapedUnicodeCharactersCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/EscapedUnicodeCharactersCheckTest.java
@@ -22,6 +22,7 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.CheckTestUtils.nonCompilingTestSourcesPath;
 import static org.sonar.java.CheckTestUtils.testSourcesPath;
 
 class EscapedUnicodeCharactersCheckTest {
@@ -30,6 +31,14 @@ class EscapedUnicodeCharactersCheckTest {
   void test() {
     JavaCheckVerifier.newVerifier()
       .onFile(testSourcesPath("checks/EscapedUnicodeCharactersCheck.java"))
+      .withCheck(new EscapedUnicodeCharactersCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void testTextBlocks() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/EscapedUnicodeCharactersTextBlocksCheck.java"))
       .withCheck(new EscapedUnicodeCharactersCheck())
       .verifyIssues();
   }
