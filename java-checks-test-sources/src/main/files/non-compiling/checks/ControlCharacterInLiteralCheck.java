d78e694e4e4a9a76a9ce98ad39f76dc9bb0ee992
margarita-nedzelska-sonarsource <70522623+margarita-nedzelska-sonarsource@users.noreply.github.com>
2021-01-25 17:43:46 +0100

    SONARJAVA-3667 Improve S2479 to support non-indent tabs in Text blocks (#3406)

--- java-checks-test-sources/src/main/files/non-compiling/checks/ControlCharacterInLiteralCheck.java
+++ java-checks-test-sources/src/main/files/non-compiling/checks/ControlCharacterInLiteralCheck.java
@@ -13,6 +13,20 @@ public class ControlCharacterInLiteralCheck {
     """
       U+200B '​' zero width space
     """,
+
+    // Compliant
+    """
+      U+0009 \u0009 tab here
+    """,
+    // Noncompliant@+1 {{Remove the non-escaped \u0009 character from this literal.}}
+    """
+      U+0009 '	' tab here
+    """,
+    // Compliant, leading tabs
+    """
+			U+0009 Compliant code
+			""",
+    
   };
 
 }
