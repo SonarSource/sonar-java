272260e7e9f25210beb1306ed3be67ca43e83a46
Dorian Burihabwa <75226315+dorian-burihabwa-sonarsource@users.noreply.github.com>
2021-01-27 16:36:36 +0100

    SONARJAVA-3480 S5917: DateTimeFormatters should not use mismatched year and week numbers (#3400)

---
+++ java-checks-test-sources/src/main/java/checks/DateTimeFormatterMismatch.java
@@ -0,0 +1,211 @@
+package checks;
+
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
+import java.time.format.SignStyle;
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.ValueRange;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+import java.util.Map;
+
+public class DateTimeFormatterMismatch {
+
+  public void createUsingBuilder() {
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.YEAR, 4) // Noncompliant [[sc=20;ec=36;secondary=+2]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.MONTH_OF_YEAR, 2)
+      .appendLiteral("-")
+      .appendValue(ChronoField.YEAR, 4) // Noncompliant [[sc=20;ec=36;secondary=+2]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendLiteral('[')
+      .appendValue(ChronoField.YEAR_OF_ERA, 4) // Noncompliant [[sc=20;ec=43;secondary=+3]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral(']')
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(WeekFields.ISO.weekBasedYear(), 4) // Noncompliant [[sc=20;ec=50;secondary=+2]] {{Change this year format to use ChronoField.YEAR instead (or the week format to WeekFields.ISO.weekOfWeekBasedYear()).}}
+      .appendLiteral('-')
+      .appendValue(ChronoField.ALIGNED_WEEK_OF_YEAR, 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder() // Compliant
+      .appendValue(WeekFields.ISO.weekBasedYear(), 4)
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder() // Compliant
+      .appendValue(ChronoField.YEAR, 4)
+      .appendLiteral('-')
+      .appendValue(ChronoField.ALIGNED_WEEK_OF_YEAR, 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder() // Compliant
+      .appendValue(ChronoField.YEAR_OF_ERA, 4)
+      .appendLiteral('-')
+      .appendValue(ChronoField.ALIGNED_WEEK_OF_YEAR, 2)
+      .toFormatter();
+
+    DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.YEAR, 4) // Noncompliant [[sc=20;ec=36;secondary=+1]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2)
+      .appendLiteral('-');
+
+    builder.toFormatter();
+
+    new DateTimeFormatterBuilder() // Compliant
+      .appendValue(WeekFields.ISO.weekBasedYear(), 4)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder() // Compliant
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2)
+      .toFormatter();
+
+    DateTimeFormatterBuilder builder2 = new DateTimeFormatterBuilder();
+    builder2.appendValue(ChronoField.YEAR, 4); // Compliant FN - consecutive calls on builders are not considered
+    builder2.appendLiteral('-');
+    builder2.appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 2);
+    builder2.toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(FakeChronoField.YEAR, 4) // Compliant FN - Ignore cases where a weird custom temporal field is used
+      .appendLiteral('-')
+      .appendValue(FakeChronoField.ALIGNED_WEEK_OF_YEAR, 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(FakeChronoField.YEAR, 4) // Compliant FN - Ignore cases where a weird custom temporal field is used
+      .appendLiteral('-')
+      .appendValue(FakeChronoField.NULL, 2)
+      .toFormatter();
+
+    TemporalField customField = new EmptyTemporalFieldImpl();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.YEAR, 4) // Compliant FN - Ignore cases where a weird custom temporal field is used
+      .appendLiteral('-')
+      .appendValue(customField, 2)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(WeekFields.ISO.weekBasedYear(), 4)  // Noncompliant [[sc=20;ec=50;secondary=+3]] {{Change this year format to use ChronoField.YEAR instead (or the week format to WeekFields.ISO.weekOfWeekBasedYear()).}}
+      .appendValue(ChronoField.YEAR_OF_ERA, 4)
+      .appendLiteral('-')
+      .appendValue(ChronoField.ALIGNED_WEEK_OF_YEAR, 2)
+      .toFormatter();
+
+    // Matches on appendValue methods with different parameter list
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.YEAR) // Noncompliant [[sc=20;ec=36;secondary=+2]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear())
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.MONTH_OF_YEAR)
+      .appendLiteral("-")
+      .appendValue(ChronoField.YEAR) // Noncompliant [[sc=20;ec=36;secondary=+2]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 1, 2, SignStyle.NORMAL)
+      .toFormatter();
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.YEAR, 2, 4, SignStyle.NORMAL) // Noncompliant [[sc=20;ec=36;secondary=+2]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 1, 2, SignStyle.NORMAL)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.MONTH_OF_YEAR, 2, 4, SignStyle.NORMAL)
+      .appendLiteral("-")
+      .appendValue(ChronoField.YEAR, 4) // Noncompliant [[sc=20;ec=36;secondary=+2]] {{Change this year format to use the week-based year instead (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).}}
+      .appendLiteral('-')
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 1, 2, SignStyle.NORMAL)
+      .toFormatter();
+
+    new DateTimeFormatterBuilder()
+      .appendValue(WeekFields.ISO.weekBasedYear(), 4)
+      .appendInstant()
+      .appendValue(WeekFields.ISO.weekOfWeekBasedYear(), 1, 2, SignStyle.NORMAL)
+      .toFormatter();
+  }
+
+  private static class FakeChronoField {
+    private static final TemporalField YEAR = ChronoField.YEAR_OF_ERA;
+    private static final TemporalField ALIGNED_WEEK_OF_YEAR = ChronoField.DAY_OF_YEAR;
+    private static final TemporalField NULL = null;
+  }
+
+  private class EmptyTemporalFieldImpl implements TemporalField {
+
+    @Override
+    public String getDisplayName(Locale locale) {
+      return null;
+    }
+
+    @Override
+    public TemporalUnit getBaseUnit() {
+      return null;
+    }
+
+    @Override
+    public TemporalUnit getRangeUnit() {
+      return null;
+    }
+
+    @Override
+    public ValueRange range() {
+      return null;
+    }
+
+    @Override
+    public boolean isDateBased() {
+      return false;
+    }
+
+    @Override
+    public boolean isTimeBased() {
+      return false;
+    }
+
+    @Override
+    public boolean isSupportedBy(TemporalAccessor temporalAccessor) {
+      return false;
+    }
+
+    @Override
+    public ValueRange rangeRefinedBy(TemporalAccessor temporalAccessor) {
+      return null;
+    }
+
+    @Override
+    public long getFrom(TemporalAccessor temporalAccessor) {
+      return 0;
+    }
+
+    @Override
+    public <R extends Temporal> R adjustInto(R r, long l) {
+      return null;
+    }
+
+    @Override
+    public TemporalAccessor resolve(Map<TemporalField, Long> fieldValues, TemporalAccessor partialTemporal, ResolverStyle resolverStyle) {
+      return null;
+    }
+  }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/DateTimeFormatterMismatchCheck.java
@@ -0,0 +1,189 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S5917")
+public class DateTimeFormatterMismatchCheck extends IssuableSubscriptionVisitor {
+  private static final String TEMPORAL_FIELD_TYPE = "java.time.temporal.TemporalField";
+  private static final String INT_TYPE = "int";
+
+  private static final MethodMatchers APPEND_VALUE_MATCHER = MethodMatchers.create()
+    .ofTypes("java.time.format.DateTimeFormatterBuilder")
+    .names("appendValue")
+    .addParametersMatcher(TEMPORAL_FIELD_TYPE)
+    .addParametersMatcher(TEMPORAL_FIELD_TYPE, INT_TYPE)
+    .addParametersMatcher(TEMPORAL_FIELD_TYPE, INT_TYPE, INT_TYPE, "java.time.format.SignStyle")
+    .build();
+
+  private static final String CHANGE_YEAR_FORMAT_WEEK_BASED_MESSAGE = "Change this year format to use the week-based year instead" +
+    " (or the week format to Chronofield.ALIGNED_WEEK_OF_YEAR).";
+  private static final String CHANGE_YEAR_FORMAT_TO_CHRONOFIELD_MESSAGE = "Change this year format to use ChronoField.YEAR instead" +
+    " (or the week format to WeekFields.ISO.weekOfWeekBasedYear()).";
+  private static final String SECONDARY_LOCATION_MESSAGE = "Week format is inconsistent with year.";
+
+  private int depth = 0;
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return Collections.singletonList(Tree.Kind.METHOD_INVOCATION);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    MethodInvocationTree invocation = (MethodInvocationTree) tree;
+    if (APPEND_VALUE_MATCHER.matches(invocation)) {
+      if (depth == 0) {
+        ChainedInvocationVisitor visitor = new ChainedInvocationVisitor();
+        invocation.accept(visitor);
+        if (visitor.usesWeek && visitor.usesYear) {
+          if (visitor.usesWeekBasedYear && !visitor.usesWeekOfWeekBasedYear) {
+            reportIssue(visitor.primary, CHANGE_YEAR_FORMAT_TO_CHRONOFIELD_MESSAGE, visitor.secondaries, null);
+          } else if (!visitor.usesWeekBasedYear && visitor.usesWeekOfWeekBasedYear) {
+            reportIssue(visitor.primary, CHANGE_YEAR_FORMAT_WEEK_BASED_MESSAGE, visitor.secondaries, null);
+          }
+        }
+      }
+      depth++;
+    }
+  }
+
+  @Override
+  public void leaveNode(Tree tree) {
+    if (APPEND_VALUE_MATCHER.matches((MethodInvocationTree) tree)) {
+      depth--;
+    }
+  }
+
+  @Override
+  public void leaveFile(JavaFileScannerContext context) {
+    depth = 0;
+  }
+
+
+  private static class ChainedInvocationVisitor extends BaseTreeVisitor {
+    private static final MethodMatchers WEEK_BASED_YEAR_MATCHER = MethodMatchers.create()
+      .ofTypes("java.time.temporal.WeekFields")
+      .names("weekBasedYear")
+      .addWithoutParametersMatcher()
+      .build();
+
+    private static final MethodMatchers WEEK_OF_WEEK_BASED_YEAR_MATCHER = MethodMatchers.create()
+      .ofTypes("java.time.temporal.WeekFields")
+      .names("weekOfWeekBasedYear")
+      .addWithoutParametersMatcher()
+      .build();
+
+    private boolean usesWeek = false;
+    private boolean usesWeekOfWeekBasedYear = false;
+    private boolean usesYear = false;
+    private boolean usesWeekBasedYear = false;
+    private final List<JavaFileScannerContext.Location> secondaries = new ArrayList<>();
+    private ExpressionTree primary = null;
+
+    @Override
+    public void visitMethodInvocation(MethodInvocationTree tree) {
+      inspectCall(tree);
+      ExpressionTree expressionTree = tree.methodSelect();
+      expressionTree.accept(this);
+    }
+
+    private void inspectCall(MethodInvocationTree invocation) {
+      if (!APPEND_VALUE_MATCHER.matches(invocation)) {
+        return;
+      }
+      ExpressionTree argument = invocation.arguments().get(0);
+      if (refersToYear(argument)) {
+        usesYear = true;
+        if (primary == null) {
+          primary = argument;
+        }
+        boolean isWeekBasedYearArgument = isWeekBasedYearUsed(argument);
+        usesWeekBasedYear |= isWeekBasedYearArgument;
+        if (isWeekBasedYearArgument) {
+          primary = argument;
+        }
+      } else if (refersToWeek(argument)) {
+        usesWeek = true;
+        secondaries.add(new JavaFileScannerContext.Location(SECONDARY_LOCATION_MESSAGE, argument));
+        usesWeekOfWeekBasedYear |= isWeekOfWeekBasedYearUsed(argument);
+      }
+    }
+
+    private static boolean refersToWeek(ExpressionTree argument) {
+      return isChronoFieldWeek(argument) || isWeekOfWeekBasedYearUsed(argument);
+    }
+
+    private static boolean isWeekOfWeekBasedYearUsed(ExpressionTree argument) {
+      if (argument.is(Tree.Kind.METHOD_INVOCATION)) {
+        MethodInvocationTree call = (MethodInvocationTree) argument;
+        return WEEK_OF_WEEK_BASED_YEAR_MATCHER.matches(call);
+      }
+      return false;
+    }
+
+    private static boolean isChronoFieldWeek(ExpressionTree argument) {
+      if (!argument.is(Tree.Kind.MEMBER_SELECT)) {
+        return false;
+      }
+      MemberSelectExpressionTree select = (MemberSelectExpressionTree) argument;
+      if (!select.symbolType().is("java.time.temporal.ChronoField")) {
+        return false;
+      }
+      IdentifierTree identifier = select.identifier();
+      return identifier.name().equals("ALIGNED_WEEK_OF_YEAR");
+    }
+
+    private static boolean refersToYear(ExpressionTree argument) {
+      return isChronoFieldYear(argument) || isWeekBasedYearUsed(argument);
+    }
+
+    private static boolean isWeekBasedYearUsed(ExpressionTree argument) {
+      if (argument.is(Tree.Kind.METHOD_INVOCATION)) {
+        MethodInvocationTree call = (MethodInvocationTree) argument;
+        return WEEK_BASED_YEAR_MATCHER.matches(call);
+      }
+      return false;
+    }
+
+    private static boolean isChronoFieldYear(ExpressionTree argument) {
+      if (argument.is(Tree.Kind.MEMBER_SELECT)) {
+        MemberSelectExpressionTree select = (MemberSelectExpressionTree) argument;
+        IdentifierTree identifier = select.identifier();
+        return select.symbolType().is("java.time.temporal.ChronoField") &&
+          (identifier.name().equals("YEAR") || identifier.name().equals("YEAR_OF_ERA"));
+      }
+      return false;
+    }
+  }
+}
