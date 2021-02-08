858b56d155d17d38c4a3eff7ae2f4ae9fb79bda2
Dorian Burihabwa <75226315+dorian-burihabwa-sonarsource@users.noreply.github.com>
2021-02-08 16:00:08 +0100

    SONARJAVA-3683 S4502: raise an issue when CSRF protection is disabled on a path (#3419)

--- java-checks/src/test/java/org/sonar/java/checks/spring/SpringSecurityDisableCSRFCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/spring/SpringSecurityDisableCSRFCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks.spring;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.CheckTestUtils.testSourcesPath;
+
 class SpringSecurityDisableCSRFCheckTest {
 
   @Test
   void test() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/spring/SpringSecurityDisableCSRFCheck.java")
+      .onFile(testSourcesPath("checks/spring/SpringSecurityDisableCSRFCheck.java"))
       .withCheck(new SpringSecurityDisableCSRFCheck())
       .verifyIssues();
   }
