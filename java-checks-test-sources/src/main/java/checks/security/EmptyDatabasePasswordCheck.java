9ec55858f0111f39d439e511212d006003c7e03b
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-02-10 10:25:13 +0100

    SONARJAVA-3278 S2115: Ignore passwordless connection urls
    Distinguish between urls where password are not required and those
    where the password field is given an empty value.

--- java-checks-test-sources/src/main/java/checks/security/EmptyDatabasePasswordCheck.java
+++ java-checks-test-sources/src/main/java/checks/security/EmptyDatabasePasswordCheck.java
@@ -1,18 +1,20 @@
 // place itself in the same package as the test to access the constants
-package org.sonar.java.checks.security;
+package checks.security;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import static org.sonar.java.checks.security.EmptyDatabasePasswordCheckTest.EMPTY_PASSWORD;
-import static org.sonar.java.checks.security.EmptyDatabasePasswordCheckTest.NON_EMPTY_PASSWORD;
+import java.util.Properties;
 
-class S2115 {
+import static checks.security.EmptyDatabasePasswordCheckVariables.EMPTY_PASSWORD;
+import static checks.security.EmptyDatabasePasswordCheckVariables.NON_EMPTY_PASSWORD;
+
+class EmptyDatabasePasswordCheck {
   void foo(Properties connectionProps, String unknown) throws SQLException {
     DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", ""); // Noncompliant [[sc=5;ec=86]] {{Add password protection to this database.}}
     DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", "Foo");
 
     String pwd = "";
-    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", pwd); // Noncompliant [[secondary=14]]
+    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", pwd); // Noncompliant [[secondary=-1]]
 
     String pwd2 = "foo";
     DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", pwd2);
@@ -20,7 +22,7 @@ class S2115 {
     String pRef = "";
     String pwd3 = pRef;
 
-    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", pwd3); // Noncompliant [[secondary=20,21]]
+    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", pwd3); // Noncompliant [[secondary=-3,-2]]
 
     DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", getPassword());
 
@@ -42,15 +44,10 @@ class S2115 {
     String string = "jdbc:db2://myhost:5021/mydb:user=dbadm;password=";
     DriverManager.getConnection(string); // Noncompliant
 
-    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", ConnectionProps);
-
     String url = null;
     DriverManager.getConnection(url);
     DriverManager.getConnection(null);
 
-    String url2 = javax.sql.DataSource.getConnection().getMetadata().getURL();
-    url2 += (";shutdown=true");
-    DriverManager.getConnection(url2);
 
     String pwd4 = getPassword();
     pwd4 += "";
@@ -64,25 +61,18 @@ class S2115 {
 
     DriverManager.getConnection("jdbc:oracle:oci8:scott/tiger@myhost");
 
-    DriverManager.getConnection("jdbc:mysql://sandy:@[myhost1:1111,address=(host=myhost2)(port=2222)(key2=value2)]/db"); // Noncompliant
+    DriverManager.getConnection("jdbc:oracle:oci8:   /@myhost"); // Compliant even though the URL is not accepted by the driver
 
-    DriverManager.getConnection("jdbc:oracle:oci8:scott/@myhost"); // Noncompliant
+    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "AppLogin", unknown);
 
-    DriverManager.getConnection("jdbc:oracle:oci8:   /@myhost"); // Noncompliant
+    DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", connectionProps);
   }
 
   String getPassword() {
     return "foo";
   }
 
-  void missingPassword() {
-    // ://user@
-    DriverManager.getConnection("jdbc:mysql://sandy@localhost:1111/db"); // Noncompliant
-    // empty ://user:@
-    DriverManager.getConnection("jdbc:mysql://sandy:@localhost:1111/db"); // Noncompliant
-
-    // no ,password= nor (password=
-    DriverManager.getConnection("jdbc:mysql://[(host=myhost1,port=1111,user=sandy),(host=myhost2,port=2222,user=finn)]/db"); // Noncompliant
+  void missingPassword() throws SQLException {
     // empty (password=)
     DriverManager.getConnection("jdbc:mysql://address=(host=myhost1)(port=1111)(user=sandy)(password=),address=(host=myhost2)(port=2222)(user=finn)(password=)/db"); // Noncompliant
     // empty ,password=)
@@ -92,19 +82,28 @@ class S2115 {
     DriverManager.getConnection("jdbc:mysql://localhost:1111/db?user=user&password="); // Noncompliant
     // empty ?password=&
     DriverManager.getConnection("jdbc:mysql://localhost:1111/db?password=&user=user"); // Noncompliant
-    // no &password= nor ?password=
-    DriverManager.getConnection("jdbc:mysql://localhost:1111/db"); // Noncompliant
 
-    // empty :user/@//
-    DriverManager.getConnection("jdbc:oracle:thin:scott/@//myhost:1521/myservicename"); // Noncompliant
-    // empty :user//@
-    DriverManager.getConnection("jdbc:oracle:oci:scott/tiger/@"); // Noncompliant
 
-    // no ;password=
-    DriverManager.getConnection("jdbc:derby:sample"); // Noncompliant
+
     // empty ;password=;
     DriverManager.getConnection("jdbc:derby:sample;password=;user=jill"); // Noncompliant
     // empty ;password= at the end
     DriverManager.getConnection("jdbc:derby:sample;user=jill;password="); // Noncompliant
+  }
+
+  void compliantNoPassword() throws SQLException {
+    DriverManager.getConnection("jdbc:oracle:oci8:scott/@myhost");
+    DriverManager.getConnection("jdbc:oracle:thin:scott/@//myhost:1521/myservicename");
+    DriverManager.getConnection("jdbc:oracle:oci:scott/tiger/@");
+
+    DriverManager.getConnection("jdbc:mysql://sandy@localhost:1111/db");
+    DriverManager.getConnection("jdbc:mysql://sandy:@localhost:1111/db");
+    DriverManager.getConnection("jdbc:mysql://sandy:@[myhost1:1111,address=(host=myhost2)(port=2222)(key2=value2)]/db");
+    DriverManager.getConnection("jdbc:mysql://[(host=myhost1,port=1111,user=sandy),(host=myhost2,port=2222,user=finn)]/db");
+    DriverManager.getConnection("jdbc:mysql://localhost:1111/db");
+
+    DriverManager.getConnection("jdbc:derby:sample");
+
+    DriverManager.getConnection("jdbc:db2://myhost:5021/mydb");
   }
 }
