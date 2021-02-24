7e1840190e518bc8c919e22420147aa518cf989c
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-02-24 17:06:11 +0100

    SONARJAVA-3558 Issue filter should extends its filter to IDE-specific suppressed warnings

--- java-checks/src/test/java/org/sonar/java/filters/SuppressWarningFilterTest.java
+++ java-checks/src/test/java/org/sonar/java/filters/SuppressWarningFilterTest.java
@@ -20,24 +20,41 @@
 package org.sonar.java.filters;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.java.checks.BoxedBooleanExpressionsCheck;
 import org.sonar.java.checks.CallToDeprecatedCodeMarkedForRemovalCheck;
 import org.sonar.java.checks.CallToDeprecatedMethodCheck;
+import org.sonar.java.checks.DeadStoreCheck;
 import org.sonar.java.checks.EmptyBlockCheck;
 import org.sonar.java.checks.EmptyStatementUsageCheck;
 import org.sonar.java.checks.EqualsOverridenWithHashCodeCheck;
+import org.sonar.java.checks.ImmediateReverseBoxingCheck;
 import org.sonar.java.checks.MissingDeprecatedCheck;
 import org.sonar.java.checks.ObjectFinalizeCheck;
 import org.sonar.java.checks.RawTypeCheck;
 import org.sonar.java.checks.RedundantTypeCastCheck;
 import org.sonar.java.checks.ReturnInFinallyCheck;
+import org.sonar.java.checks.StaticFieldUpateCheck;
 import org.sonar.java.checks.StaticMembersAccessCheck;
+import org.sonar.java.checks.StaticMethodCheck;
 import org.sonar.java.checks.SuppressWarningsCheck;
 import org.sonar.java.checks.SwitchCaseWithoutBreakCheck;
+import org.sonar.java.checks.SynchronizedOverrideCheck;
 import org.sonar.java.checks.TodoTagPresenceCheck;
+import org.sonar.java.checks.TryWithResourcesCheck;
+import org.sonar.java.checks.TypeParametersShadowingCheck;
+import org.sonar.java.checks.UndocumentedApiCheck;
 import org.sonar.java.checks.naming.BadConstantNameCheck;
 import org.sonar.java.checks.serialization.SerialVersionUidCheck;
+import org.sonar.java.checks.unused.UnusedLabelCheck;
+import org.sonar.java.checks.unused.UnusedLocalVariableCheck;
+import org.sonar.java.checks.unused.UnusedPrivateClassCheck;
 import org.sonar.java.checks.unused.UnusedPrivateFieldCheck;
+import org.sonar.java.checks.unused.UnusedPrivateMethodCheck;
+import org.sonar.java.checks.unused.UnusedTypeParameterCheck;
+import org.sonar.java.se.checks.ConditionalUnreachableCodeCheck;
 import org.sonar.java.se.checks.DivisionByZeroCheck;
+import org.sonar.java.se.checks.NullDereferenceCheck;
+import org.sonar.java.se.checks.UnclosedResourcesCheck;
 
 class SuppressWarningFilterTest {
   /**
@@ -66,6 +83,46 @@ class SuppressWarningFilterTest {
       new StaticMembersAccessCheck(),
       new SerialVersionUidCheck(),
       new RawTypeCheck()
+    );
+  }
+
+  @Test
+  void verify_2() {
+    FilterVerifier.verify("src/test/files/filters/SuppressWarningFilter_2.java", new SuppressWarningFilter(),
+      // activated rules
+      new BoxedBooleanExpressionsCheck(),
+      new ImmediateReverseBoxingCheck(),
+      new TypeParametersShadowingCheck(),
+      new NullDereferenceCheck(),
+      new TryWithResourcesCheck(),
+      new UnclosedResourcesCheck(),
+      new SerialVersionUidCheck(),
+      new StaticFieldUpateCheck(),
+      new StaticMethodCheck(),
+      new SynchronizedOverrideCheck()
+    );
+  }
+
+  @Test
+  void verify_unused() {
+    FilterVerifier.verify("src/test/files/filters/SuppressWarningFilter_unused.java", new SuppressWarningFilter(),
+      // activated rules
+      new UnusedLocalVariableCheck(),
+      new UnusedLabelCheck(),
+      new UnusedPrivateFieldCheck(),
+      new UnusedPrivateClassCheck(),
+      new UnusedTypeParameterCheck(),
+      new UnusedPrivateMethodCheck(),
+      new DeadStoreCheck(),
+      new ConditionalUnreachableCodeCheck()
+    );
+  }
+
+  @Test
+  void verify_javadoc() {
+    FilterVerifier.verify("src/test/files/filters/SuppressWarningFilter_javadoc.java", new SuppressWarningFilter(),
+      // activated rules
+      new UndocumentedApiCheck()
     );
   }
 
