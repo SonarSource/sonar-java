168d643ea5f19bf07e90ada6023fdc13d3283cd3
Sebastian Hungerecker <sebastian.hungerecker@sonarsource.com>
2021-03-12 14:29:23 +0100

    SONARJAVA-3640 FN in S6002 for full matches and anchored patterns

--- java-checks/src/main/java/org/sonar/java/checks/helpers/IntersectAutomataChecker.java
+++ java-checks/src/main/java/org/sonar/java/checks/helpers/IntersectAutomataChecker.java
@@ -25,6 +25,11 @@ public class IntersectAutomataChecker extends AbstractAutomataChecker {
   }
 
   @Override
+  protected boolean neutralAnswer() {
+    return false;
+  }
+
+  @Override
   protected boolean checkAuto1AndAuto2Successors(SubAutomaton auto1, SubAutomaton auto2, boolean defaultAnswer, boolean hasConsumedInput) {
     SimplifiedRegexCharacterClass characterClass1 = SimplifiedRegexCharacterClass.of(auto1.start);
     SimplifiedRegexCharacterClass characterClass2 = SimplifiedRegexCharacterClass.of(auto2.start);
