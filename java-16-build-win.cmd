505cd0b2c7840a532252c590beceda565bed11ef
Margarita <margarita.nedzelska@sonarsource.com>
2021-04-01 16:19:57 +0200

    Use Jdk 15 images

--- java-16-build-win.cmd
+++
@@ -1,4 +0,0 @@
-echo %JAVA_HOME%
-choco install AdoptOpenJDK15 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome"
-echo %JAVA_HOME%
-mvn.cmd clean verify
--- java-16-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir .jdk
-curl -L "https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz" -o .jdk/jdk.tar.gz
-tar -xzf .jdk/jdk.tar.gz -C .jdk --strip-components 1
-.jdk/bin/java -version
-export JAVA_HOME=$PWD/.jdk
-
-regular_mvn_build_deploy_analyze
