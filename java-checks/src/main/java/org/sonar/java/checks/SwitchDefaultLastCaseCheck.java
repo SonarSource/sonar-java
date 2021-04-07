1b023b63662ac4edb3ff311a08a9ee2225fac559
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-04-07 10:48:55 +0200

    SONARJAVA-3744 Support Switch Expression in S4524

--- java-checks/src/main/java/org/sonar/java/checks/SwitchDefaultLastCaseCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/SwitchDefaultLastCaseCheck.java
@@ -19,14 +19,15 @@
  */
 package org.sonar.java.checks;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.sonar.check.Rule;
 import org.sonar.java.ast.api.JavaKeyword;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.CaseGroupTree;
 import org.sonar.plugins.java.api.tree.CaseLabelTree;
-import org.sonar.plugins.java.api.tree.SwitchStatementTree;
+import org.sonar.plugins.java.api.tree.SwitchTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 @Rule(key = "S4524")
@@ -34,22 +35,23 @@ public class SwitchDefaultLastCaseCheck extends IssuableSubscriptionVisitor {
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    return Collections.singletonList(Tree.Kind.SWITCH_STATEMENT);
+    return Arrays.asList(Tree.Kind.SWITCH_STATEMENT, Tree.Kind.SWITCH_EXPRESSION);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    SwitchStatementTree switchStatementTree = (SwitchStatementTree) tree;
-    getDefaultLabelAtWrongPosition(switchStatementTree).ifPresent(defaultLabel -> reportIssue(defaultLabel, "Move this default to the end of the switch."));
+    SwitchTree switchTree = (SwitchTree) tree;
+    getDefaultLabelAtWrongPosition(switchTree).ifPresent(defaultLabel -> reportIssue(defaultLabel, "Move this default to the end of the switch."));
   }
 
-  private static Optional<CaseLabelTree> getDefaultLabelAtWrongPosition(SwitchStatementTree switchStatementTree) {
-    for (int i = 0; i < switchStatementTree.cases().size(); i++) {
-      List<CaseLabelTree> labels = switchStatementTree.cases().get(i).labels();
+  private static Optional<CaseLabelTree> getDefaultLabelAtWrongPosition(SwitchTree switchTree) {
+    List<CaseGroupTree> cases = switchTree.cases();
+    for (int i = 0; i < cases.size(); i++) {
+      List<CaseLabelTree> labels = cases.get(i).labels();
       for (int j = 0; j < labels.size(); j++) {
         CaseLabelTree label = labels.get(j);
         boolean defaultExists = isDefault(label);
-        if (defaultExists && ((j != labels.size() - 1) || (i == switchStatementTree.cases().size() - 1))) {
+        if (defaultExists && ((j != labels.size() - 1) || (i == cases.size() - 1))) {
           /*
            * we return Optional.empty() because either we have default at the end which is a best practise
            * or it is in a place in a case group where it can not affect the result of the execution
