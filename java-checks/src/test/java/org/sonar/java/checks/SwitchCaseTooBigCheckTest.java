5616458e034a70c044b8f4884da5beb712116db3
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-04-07 10:43:31 +0200

    SONARJAVA-3744 Support Switch Expression in S1151

--- java-checks/src/test/java/org/sonar/java/checks/SwitchCaseTooBigCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/SwitchCaseTooBigCheckTest.java
@@ -22,6 +22,7 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.nonCompilingTestSourcesPath;
 import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
 
 class SwitchCaseTooBigCheckTest {
@@ -51,6 +52,15 @@ class SwitchCaseTooBigCheckTest {
     JavaCheckVerifier.newVerifier()
       .onFile(testSourcesPath("checks/SwitchCaseTooBigCheck.java"))
       .withCheck(new SwitchCaseTooBigCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void test_switch_expressions() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/SwitchCaseTooBigCheck.java"))
+      .withCheck(new SwitchCaseTooBigCheck())
+      .withJavaVersion(14)
       .verifyIssues();
   }
 
