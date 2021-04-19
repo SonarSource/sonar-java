53683639418365ca0f685d4af4eb644cb9022144
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-04-19 17:49:40 +0200

    SONARJAVA-3805 Rule S5332 should support okhttp library

--- java-checks/src/main/java/org/sonar/java/checks/security/ClearTextProtocolCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/security/ClearTextProtocolCheck.java
@@ -19,19 +19,29 @@
  */
 package org.sonar.java.checks.security;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.sonar.check.Rule;
-import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.Arguments;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.NewClassTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import static org.sonar.plugins.java.api.semantic.MethodMatchers.ANY;
 
 @Rule(key = "S5332")
-public class ClearTextProtocolCheck extends AbstractMethodDetection {
+public class ClearTextProtocolCheck extends IssuableSubscriptionVisitor {
 
   private static final Map<String, Protocol> PROTOCOLS = new HashMap<>();
 
   private static final String MESSAGE = "Using %s protocol is insecure. Use %s instead.";
+  private static final String MESSAGE_HTTP = "Using HTTP protocol is insecure. Use HTTPS instead.";
 
   static {
     PROTOCOLS.put("org.apache.commons.net.ftp.FTPClient", new Protocol("FTP", "SFTP, SCP or FTPS"));
@@ -39,20 +49,59 @@ public class ClearTextProtocolCheck extends AbstractMethodDetection {
     PROTOCOLS.put("org.apache.commons.net.telnet.TelnetClient", new Protocol("Telnet", "SSH"));
   }
 
+  private static final MethodMatchers UNSECURE_CLIENTS = MethodMatchers.create()
+    .ofTypes(PROTOCOLS.keySet().toArray(new String[0]))
+    .constructor()
+    .withAnyParameters()
+    .build();
+
+  private static final MethodMatchers OK_HTTP_CONNECTION_SPEC_BUILDERS = MethodMatchers.create()
+    .ofTypes("okhttp3.ConnectionSpec$Builder")
+    .constructor()
+    .addParametersMatcher("okhttp3.ConnectionSpec")
+    .build();
+
+  private static final MethodMatchers OK_HTTP_BUILDERS = MethodMatchers.create()
+    .ofTypes("okhttp3.OkHttpClient$Builder")
+    .names("connectionSpecs")
+    .addParametersMatcher(ANY)
+    .build();
+
   @Override
-  protected MethodMatchers getMethodInvocationMatchers() {
-    return MethodMatchers.create()
-      .ofTypes(PROTOCOLS.keySet().toArray(new String[0]))
-      .constructor()
-      .withAnyParameters()
-      .build();
+  public List<Tree.Kind> nodesToVisit() {
+    return Arrays.asList(Tree.Kind.METHOD_INVOCATION, Tree.Kind.NEW_CLASS);
   }
 
   @Override
-  protected void onConstructorFound(NewClassTree newClassTree) {
-    Protocol protocol = PROTOCOLS.get(newClassTree.symbolType().fullyQualifiedName());
-    String message = String.format(MESSAGE, protocol.protocolName, protocol.alternatives);
-    reportIssue(newClassTree.identifier(), message);
+  public void visitNode(Tree tree) {
+    if (tree.is(Tree.Kind.NEW_CLASS)) {
+      NewClassTree newClassTree = (NewClassTree) tree;
+      if (UNSECURE_CLIENTS.matches(newClassTree)) {
+        Protocol protocol = PROTOCOLS.get(newClassTree.symbolType().fullyQualifiedName());
+        String message = String.format(MESSAGE, protocol.protocolName, protocol.alternatives);
+        reportIssue(newClassTree.identifier(), message);
+      } else if (OK_HTTP_CONNECTION_SPEC_BUILDERS.matches(newClassTree)) {
+        reportIfUsesClearText(newClassTree.arguments());
+      }
+    } else {
+      MethodInvocationTree mit = (MethodInvocationTree) tree;
+      if (OK_HTTP_BUILDERS.matches(mit)) {
+        reportIfUsesClearText(mit.arguments());
+      }
+    }
+  }
+
+  private void reportIfUsesClearText(Arguments arguments) {
+    arguments.accept(new ClearTextVisitor());
+  }
+
+  class ClearTextVisitor extends BaseTreeVisitor {
+    @Override
+    public void visitIdentifier(IdentifierTree tree) {
+      if ("CLEARTEXT".equals(tree.name())) {
+        reportIssue(tree, MESSAGE_HTTP);
+      }
+    }
   }
 
   private static class Protocol {
