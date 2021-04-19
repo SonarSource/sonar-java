80eb1d8a47029060a9878212388d4e456d0ef1b0
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-04-19 16:00:13 +0200

    Move test files to compiled module for S3077

--- java-checks/src/test/java/org/sonar/java/checks/VolatileNonPrimitiveFieldCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/VolatileNonPrimitiveFieldCheckTest.java
@@ -21,13 +21,14 @@ package org.sonar.java.checks;
 
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
+import org.sonar.java.checks.verifier.TestUtils;
 
 class VolatileNonPrimitiveFieldCheckTest {
 
   @Test
   void test() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/VolatileNonPrimitiveFieldCheck.java")
+      .onFile(TestUtils.testSourcesPath("checks/VolatileNonPrimitiveFieldCheck.java"))
       .withCheck(new VolatileNonPrimitiveFieldCheck())
       .verifyIssues();
   }
