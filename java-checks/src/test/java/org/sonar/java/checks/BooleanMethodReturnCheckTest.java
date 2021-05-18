ad735d0d34734a74ed82fd589a98ced5e48ba55d
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-05-18 17:29:08 +0200

    Move test files to compiled module for S2447

--- java-checks/src/test/java/org/sonar/java/checks/BooleanMethodReturnCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/BooleanMethodReturnCheckTest.java
@@ -21,13 +21,14 @@ package org.sonar.java.checks;
 
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
+import org.sonar.java.checks.verifier.TestUtils;
 
 class BooleanMethodReturnCheckTest {
 
   @Test
   void test() {
     JavaCheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/BooleanMethodReturnCheck.java")
+      .onFile(TestUtils.testSourcesPath("checks/BooleanMethodReturnCheck.java"))
       .withCheck(new BooleanMethodReturnCheck())
       .verifyIssues();
   }
