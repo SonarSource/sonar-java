2f71df48f8f1126a2c0f9f5fe714932766ab1c0e
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-05-19 15:21:10 +0200

    SONARJAVA-3847 S107 should work with incomplete semantic

---
+++ java-checks-test-sources/src/main/files/non-compiling/checks/TooManyParametersCheck.java
@@ -0,0 +1,17 @@
+package checks;
+
+class TooManyParametersExtended extends Unknown {
+
+
+  TooManyParametersExtended(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8) { // Noncompliant - FP, ideally, the method isOverriding should return null in this case
+
+    super(p1, p2, p3, p4, p5, p6, p7, p8);
+  }
+  // FP, ideally, the method isOverriding should return null in this case, because we have no idea if this method is an override or not.
+  void foo(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8) {} // Noncompliant
+}
+
+class MethodsUsingSpringGetMapping {
+  @Unknown
+  void foo(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8) {} // Compliant, Unknown annotation
+}
