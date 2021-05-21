9dc7cdf1279849ff0b4837994899af2a7e090798
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-05-21 14:55:49 +0200

    SONARJAVA-3828 Drop deprecated java:S1609

--- java-checks/src/test/files/checks/SAMAnnotatedCheck_java9.java
+++
@@ -1,8 +0,0 @@
-interface Nok { // Noncompliant {{Annotate the "Nok" interface with the @FunctionalInterface annotation}}
-  void m();
-  private void privateMethod() {}
-}
-
-interface Ok {
-  private void privateMethod() {}
-}
