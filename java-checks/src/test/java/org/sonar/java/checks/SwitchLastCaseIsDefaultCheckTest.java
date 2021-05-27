e2c2c2fe4c5d4a00cf082a92f58d9305c1a71611
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-05-27 11:24:51 +0200

    SONARJAVA-3857: FP on S131 when enum type is unknown
    Fixed a FP in S131 when the enum type cannot be resolved.

--- java-checks/src/test/java/org/sonar/java/checks/SwitchLastCaseIsDefaultCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/SwitchLastCaseIsDefaultCheckTest.java
@@ -19,9 +19,11 @@
  */
 package org.sonar.java.checks;
 
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.nonCompilingTestSourcesPath;
 import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
 
 class SwitchLastCaseIsDefaultCheckTest {
@@ -30,6 +32,24 @@ class SwitchLastCaseIsDefaultCheckTest {
     JavaCheckVerifier.newVerifier()
       .onFile(testSourcesPath("checks/SwitchLastCaseIsDefaultCheck.java"))
       .withCheck(new SwitchLastCaseIsDefaultCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void test_non_compiling() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/SwitchLastCaseIsDefaultCheck.java"))
+      .withCheck(new SwitchLastCaseIsDefaultCheck())
+      .withClassPath(Collections.emptyList())
+      .verifyNoIssues();
+  }
+
+  @Test
+  void test_without_semantic() {
+    JavaCheckVerifier.newVerifier()
+      .onFile(testSourcesPath("checks/SwitchLastCaseIsDefaultCheck.java"))
+      .withCheck(new SwitchLastCaseIsDefaultCheck())
+      .withClassPath(Collections.emptyList())
       .verifyIssues();
   }
 }
