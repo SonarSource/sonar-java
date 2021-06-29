1e30adb9a782c322607fc7c443d7e26f70233975
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-06-29 16:32:15 +0200

    SONARJAVA-3769: Add non compliant FPs in non compiling source code

---
+++ java-checks-test-sources/src/main/files/non-compiling/checks/AccessibilityChangeCheck.java
@@ -0,0 +1,36 @@
+package checks;
+
+import java.lang.reflect.Field;
+
+class AccessibilityChangeCheck {
+  record Person(String name, int age) {
+  }
+
+  void fieldModificationIsAlwaysCompliantForRecords() throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException {
+    Person person = new Person("A", 26);
+
+    Field uninitializedField;
+    uninitializedField.setAccessible(true); // Noncompliant FP Not exploring fields from unkwnon methods
+    uninitializedField.set(person, "B"); // Noncompliant FP Not exploring fields from unkwnon methods
+
+    Field fieldOfUnknownOrigin = unknown();
+    fieldOfUnknownOrigin.setAccessible(true); // Noncompliant FP Not exploring fields from unkwnon methods
+    fieldOfUnknownOrigin.set(person, "B"); // Noncompliant FP Not exploring fields from unkwnon methods
+
+    Field fieldFromConstructor = new Field(){};
+    fieldFromConstructor.setAccessible(true); // Noncompliant FP Not exploring fields from unkwnon methods
+    fieldFromConstructor.set(person, "B"); // Noncompliant FP Not exploring fields from unkwnon methods
+
+    Field fieldFromUnkwownClass = Unknown.class.getField("myField");
+    fieldFromUnkwownClass.setAccessible(true); // Noncompliant FP Not exploring fields from unkwnon methods
+    fieldFromUnkwownClass.set(person, "B"); // Noncompliant FP Not exploring fields from unkwnon methods
+
+    Field fieldFromUnkwownClass = getUnknownClass().getField("myField");
+    fieldFromUnkwownClass.setAccessible(true); // Noncompliant FP Not exploring fields from unkwnon methods
+    fieldFromUnkwownClass.set(person, "B"); // Noncompliant FP Not exploring fields from unkwnon methods
+  }
+
+  Field getAField() {
+    return Person.class.getDeclaredFields()[0];
+  }
+}
