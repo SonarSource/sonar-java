969c2384d8d1a6b44742275b1fd6eae9e27ce3ad
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-07-01 10:28:41 +0200

    SONARJAVA-3768: Extract behavior of S3011 to abstract class

--- java-checks/src/main/java/org/sonar/java/checks/AccessibilityChangeCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/AccessibilityChangeCheck.java
@@ -19,140 +19,22 @@
  */
 package org.sonar.java.checks;
 
-import java.util.Optional;
 import org.sonar.check.Rule;
-import org.sonar.java.checks.helpers.ExpressionsHelper;
-import org.sonar.java.checks.methods.AbstractMethodDetection;
-import org.sonar.plugins.java.api.semantic.MethodMatchers;
-import org.sonar.plugins.java.api.tree.Arguments;
-import org.sonar.plugins.java.api.tree.ArrayAccessExpressionTree;
-import org.sonar.plugins.java.api.tree.ExpressionTree;
-import org.sonar.plugins.java.api.tree.IdentifierTree;
-import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
-import org.sonar.plugins.java.api.tree.Tree;
-import org.sonar.plugins.java.api.tree.VariableTree;
 
 @Rule(key = "S3011")
-public class AccessibilityChangeCheck extends AbstractMethodDetection {
-
-  private static final String JAVA_LANG_REFLECT_FIELD = "java.lang.reflect.Field";
-  private static final MethodMatchers METHOD_MATCHERS = MethodMatchers.or(
-    MethodMatchers.create().ofSubTypes("java.lang.reflect.AccessibleObject").names("setAccessible").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("set").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setBoolean").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setByte").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setChar").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setDouble").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setFloat").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setInt").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setLong").withAnyParameters().build(),
-    MethodMatchers.create().ofTypes(JAVA_LANG_REFLECT_FIELD).names("setShort").withAnyParameters().build()
-  );
-
-  private static final MethodMatchers FIELD_FETCHING_METHODS = MethodMatchers.or(
-    MethodMatchers.create().ofTypes("java.lang.Class")
-      .names("getField", "getDeclaredField")
-      .addParametersMatcher("java.lang.String")
-      .build(),
-    MethodMatchers.create().ofTypes("java.lang.Class")
-      .names("getFields", "getDeclaredFields")
-      .addWithoutParametersMatcher()
-      .build()
-  );
-
-  @Override
-  protected MethodMatchers getMethodInvocationMatchers() {
-    return METHOD_MATCHERS;
-  }
-
+public class AccessibilityChangeCheck extends AbstractAccessibilityChangeChecker {
   @Override
   protected void onMethodInvocationFound(MethodInvocationTree mit) {
     if (isModifyingFieldFromRecord(mit)) {
       return;
     }
-    if (mit.symbol().name().equals("setAccessible")) {
-      checkAccessibilityUpdate(mit);
+    if (SET_ACCESSIBLE_MATCHER.matches(mit)) {
+      if (setsToPubliclyAccessible(mit)) {
+        reportIssue(mit, "This accessibility update should be removed.");
+      }
     } else {
       reportIssue(mit, "This accessibility bypass should be removed.");
-    }
-  }
-
-  private static boolean isModifyingFieldFromRecord(MethodInvocationTree mit) {
-    ExpressionTree fieldModificationExpression = mit.methodSelect();
-    if (!fieldModificationExpression.is(Tree.Kind.MEMBER_SELECT)) {
-      return false;
-    }
-    MemberSelectExpressionTree memberSelect = (MemberSelectExpressionTree) fieldModificationExpression;
-    ExpressionTree expression = memberSelect.expression();
-    if (expression.is(Tree.Kind.ARRAY_ACCESS_EXPRESSION)) {
-      ArrayAccessExpressionTree arrayAccess = (ArrayAccessExpressionTree) expression;
-      expression = arrayAccess.expression();
-    }
-    MethodInvocationTree fieldInitializer = null;
-    if (expression.is(Tree.Kind.METHOD_INVOCATION)) {
-      fieldInitializer = (MethodInvocationTree) expression;
-    } else if (expression.is(Tree.Kind.IDENTIFIER)) {
-      IdentifierTree identifier = (IdentifierTree) expression;
-      Optional<MethodInvocationTree> fieldGettingInvocation = getInitializingMethodInvocation(identifier);
-      if (!fieldGettingInvocation.isPresent()) {
-        return false;
-      }
-      fieldInitializer = fieldGettingInvocation.get();
-    } else {
-      return false;
-    }
-    if (!FIELD_FETCHING_METHODS.matches(fieldInitializer)) {
-      return false;
-    }
-    ExpressionTree initializerExpression = fieldInitializer.methodSelect();
-    Optional<IdentifierTree> classIdentifier = getClassIdentifier((MemberSelectExpressionTree) initializerExpression);
-    if (!classIdentifier.isPresent()) {
-      return false;
-    }
-    return classIdentifier.get().symbol().type().isSubtypeOf("java.lang.Record");
-  }
-
-  private static Optional<MethodInvocationTree> getInitializingMethodInvocation(IdentifierTree identifier) {
-    Tree declaration = identifier.symbol().declaration();
-    if (declaration == null || !declaration.is(Tree.Kind.VARIABLE)) {
-      return Optional.empty();
-    }
-    VariableTree variable = (VariableTree) declaration;
-    ExpressionTree initializer = variable.initializer();
-    if (initializer == null || !initializer.is(Tree.Kind.METHOD_INVOCATION)) {
-      return Optional.empty();
-    }
-    return Optional.of((MethodInvocationTree) initializer);
-  }
-
-  private static Optional<IdentifierTree> getClassIdentifier(MemberSelectExpressionTree memberSelect) {
-    ExpressionTree expression = memberSelect.expression();
-    while (true) {
-      if (expression.is(Tree.Kind.MEMBER_SELECT)) {
-        MemberSelectExpressionTree currentMemberSelect = (MemberSelectExpressionTree) expression;
-        expression = currentMemberSelect.expression();
-      } else if (expression.is(Tree.Kind.METHOD_INVOCATION)) {
-        MethodInvocationTree currentMethodInvocation = (MethodInvocationTree) expression;
-        expression = currentMethodInvocation.methodSelect();
-      } else {
-        break;
-      }
-    }
-    if (expression.symbolType().isUnknown() || !expression.is(Tree.Kind.IDENTIFIER)) {
-      return Optional.empty();
-    }
-    return Optional.of((IdentifierTree) expression);
-  }
-
-  private void checkAccessibilityUpdate(MethodInvocationTree mit) {
-    Arguments arguments = mit.arguments();
-    ExpressionTree arg = arguments.get(0);
-    if (arguments.size() > 1) {
-      arg = arguments.get(1);
-    }
-    if (Boolean.TRUE.equals(ExpressionsHelper.getConstantValueAsBoolean(arg).value())) {
-      reportIssue(mit, "This accessibility update should be removed.");
     }
   }
 }
