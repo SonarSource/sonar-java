8dbee0140f3c2b35ccd62243a98a877f97a3de0d
Sebastian Hungerecker <63844301+sebastian-hungerecker-sonarsource@users.noreply.github.com>
2021-07-05 10:08:05 +0200

    SONARJAVA-3760 Improve rule S2094: 'Classes should not be empty' to support Records (#3676)

--- java-checks-test-sources/src/main/java/checks/emptyclass/EmptyClass.java
+++ java-checks-test-sources/src/main/java/checks/emptyclass/EmptyClass.java
@@ -1,3 +1,5 @@
+package checks.emptyclass;
+
 class A {
   int field;
 }
@@ -18,7 +20,21 @@ class L { // Noncompliant {{Remove this empty class, write its code or make it a
   ;
 }
 
-@SpringAnnotation
+@annotation
 class markerType { // compliant because of annotation
 
+}
+
+record R() { // Noncompliant
+
+}
+
+record R2(int x) { // Compliant because there's a component
+
+}
+
+record R3() { // Compliant because there's a method
+  int f() {
+    return 42;
+  }
 }
