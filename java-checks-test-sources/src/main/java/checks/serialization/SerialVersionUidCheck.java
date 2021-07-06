8c3ba4ba05e582f28c5ffa90bfdd3d8e7af88615
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-07-06 11:49:36 +0200

    SONARJAVA-3755 Add an annotated record field example to S2057 test source

--- java-checks-test-sources/src/main/java/checks/serialization/SerialVersionUidCheck.java
+++ java-checks-test-sources/src/main/java/checks/serialization/SerialVersionUidCheck.java
@@ -1,5 +1,6 @@
 package checks.serialization;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 class SerialVersionUidCheckA implements Cloneable {}
@@ -47,5 +48,10 @@ record RecordWithoutSerializationField(Object unused) implements Serializable { 
 }
 
 record RecordWithSerializationField(Object unused) implements Serializable { // Compliant
+  static final long serialVersionUID = 2L;
+}
+
+record RecordWithSerializationFieldAndAnnotation(Object unused) implements Serializable { // Compliant
+  @Serial
   static final long serialVersionUID = 2L;
 }
