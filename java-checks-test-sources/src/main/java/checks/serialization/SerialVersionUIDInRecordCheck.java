ef01a067fbabe32608658887e2833f4321978620
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-07-06 12:09:57 +0200

    SONARJAVA-3773 Add base rule files

---
+++ java-checks-test-sources/src/main/java/checks/serialization/SerialVersionUIDInRecordCheck.java
@@ -0,0 +1,19 @@
+package checks.serialization;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class SerialVersionUIDInRecordCheck {
+  record Person(String name, int age) implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 0L; // Noncompliant
+  }
+
+  record Individual(String name, int age) implements Serializable {
+  } // Compliant
+
+  record Human(String name, int age) implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 42L; // Compliant
+  }
+}
