b5ef0e1bd3ad651e46631597d030bc6c57265a96
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-07-08 16:29:02 +0200

    SONARJAVA-3768 Change comments in test source files for easier verification

--- java-checks-test-sources/src/main/java/checks/AccessibilityChangeOnRecordsCheck.java
+++ java-checks-test-sources/src/main/java/checks/AccessibilityChangeOnRecordsCheck.java
@@ -11,27 +11,27 @@ public class AccessibilityChangeOnRecordsCheck {
     Person person = new Person("A", 26);
     Field field = Person.class.getDeclaredField("name");
     field.setAccessible(true);
-    field.set(person, "B"); // Noncompliant {Remove this private field update which will never succeed}[[secondary=-1]]
+    field.set(person, "B"); // Noncompliant {{Remove this private field update which will never succeed}}[[secondary=-1]]
 
 
     Field nameField = Person.class.getDeclaredField("name");
     nameField.setAccessible(false);
-    nameField.set(person, "B"); // Noncompliant {Remove this private field update which will never succeed}
+    nameField.set(person, "B"); // Noncompliant {{Remove this private field update which will never succeed}}
 
     Field ageField = Person.class.getDeclaredField("age");
-    ageField.setInt(person, 42); // Noncompliant {Remove this private field update which will never succeed}
+    ageField.setInt(person, 42); // Noncompliant {{Remove this private field update which will never succeed}}
 
 
-    Person.class.getField("age").setInt(person, 42);// Noncompliant {Remove this private field update which will never succeed}
-    Person.class.getDeclaredField("age").setInt(person, 42);// Noncompliant {Remove this private field update which will never succeed}
+    Person.class.getField("age").setInt(person, 42); // Noncompliant {{Remove this private field update which will never succeed}}
+    Person.class.getDeclaredField("age").setInt(person, 42); // Noncompliant {{Remove this private field update which will never succeed}}
 
-    Person.class.getFields()[0].set(person, null);// Noncompliant {Remove this private field update which will never succeed}
-    Person.class.getDeclaredFields()[0].set(person, null);// Noncompliant {Remove this private field update which will never succeed}
+    Person.class.getFields()[0].set(person, null); // Noncompliant {{Remove this private field update which will never succeed}}
+    Person.class.getDeclaredFields()[0].set(person, null); // Noncompliant {{Remove this private field update which will never succeed}}
   }
 
   class ChildOfAccessibleObject extends AccessibleObject {
     private void selfModify() {
-      setAccessible(true); // Compliant are out of scope
+      setAccessible(true); // Compliant as S6216 only checks Records
     }
   }
 
