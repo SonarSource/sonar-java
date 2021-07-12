ef34d7e9edef7c58e00c88707f0a1bc4e0344e5d
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-07-12 17:42:31 +0200

    SONARJAVA-3763 Move S2693 test files in compiled sources

---
+++ java-checks-test-sources/src/main/java/checks/ThreadStartedInConstructor.java
@@ -0,0 +1,49 @@
+package checks;
+
+class ThreadStartedInConstructor {
+  public static class ExtendsThread extends Thread {
+  }
+
+  enum Enum {
+    ;
+    public static final String STRING = "string".intern();
+  }
+
+  interface Interface {
+    String STRING = "string".intern();
+  }
+
+  abstract static class TestClass1 {
+
+    public static final String STRING = "string".intern(); // Compliant
+
+    public abstract void abstractMethod();
+
+    static {
+      new Thread((Runnable) null).start(); // Compliant
+      new ExtendsThread().start(); // Compliant
+    }
+
+    TestClass1() {
+      toString(); // Compliant
+      new Thread((Runnable) null).start(); // Noncompliant [[sc=35;ec=40]] {{Move this "start" call to another method.}}
+      new ExtendsThread().start(); // Noncompliant {{Move this "start" call to another method.}}
+    }
+
+    public void method() {
+      new Thread((Runnable) null).start(); // Compliant
+    }
+  }
+
+  final static class TestClass2 {
+    TestClass2() {
+      new Thread((Runnable) null).start(); // Compliant
+      new ExtendsThread().start(); // Compliant
+    }
+
+    public void method() {
+      new Thread((Runnable) null).start(); // Compliant
+    }
+  }
+}
+
