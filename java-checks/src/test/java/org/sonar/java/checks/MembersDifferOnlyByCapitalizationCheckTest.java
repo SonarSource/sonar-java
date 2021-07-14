8d8a90fe4d9e5caf8f25b824f8c244e60297e1a4
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-07-14 17:43:48 +0200

    SONARJAVA-3763 Move S1845 test files in compiled sources

--- java-checks/src/test/java/org/sonar/java/checks/MembersDifferOnlyByCapitalizationCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/MembersDifferOnlyByCapitalizationCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
+
 class MembersDifferOnlyByCapitalizationCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/MembersDifferOnlyByCapitalizationCheck.java")
+      .onFile(testSourcesPath("checks/MembersDifferOnlyByCapitalizationCheck.java"))
       .withCheck(new MembersDifferOnlyByCapitalizationCheck())
       .verifyIssues();
   }
