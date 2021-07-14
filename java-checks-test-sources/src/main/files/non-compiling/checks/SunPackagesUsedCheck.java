e408601f4e08e3fca0af4536722170fe4e2b56bd
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-07-14 14:56:09 +0200

    SONARJAVA-3892 Exclude "com.sun.jersey" and "com.sun.faces" from S1191 by default

--- java-checks-test-sources/src/main/files/non-compiling/checks/SunPackagesUsedCheck.java
+++ java-checks-test-sources/src/main/files/non-compiling/checks/SunPackagesUsedCheck.java
@@ -1,5 +1,7 @@
-import com.sun.imageio.plugins; // Noncompliant [[sc=8;ec=31;secondary=2,7,8,10,11,13,17]]  {{Use classes from the Java API instead of Sun classes.}}
-import com.sun.jersey.api.client.ClientHandlerException; // secondary
+import com.sun.imageio.plugins; // Noncompliant [[sc=8;ec=31;secondary=2,9,10,12,13,15,19]]  {{Use classes from the Java API instead of Sun classes.}}
+import com.sun.security.ntlm.Client; // secondary
+import com.sun.jersey.api.client.ClientHandlerException; // com.sun.jersey is excluded by default since it has nothing to do with Java interal
+import com.sun.faces.application.ApplicationAssociate; // Excluded by default
 import java.util.ArrayList;
 
 class SunPackagesUsedCheck {
