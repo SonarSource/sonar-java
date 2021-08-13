f7ee96a5260d08410c734f3b7139c78c1166f995
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-08-13 09:53:20 +0200

    SONARJAVA-3911 Add quick fixes for S1155 (CollectionIsEmptyCheck)

--- java-checks/src/main/java/org/sonar/java/checks/CollectionIsEmptyCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/CollectionIsEmptyCheck.java
@@ -19,8 +19,14 @@
  */
 package org.sonar.java.checks;
 
+import java.util.Optional;
 import org.sonar.check.Rule;
+import org.sonar.java.model.DefaultJavaFileScannerContext;
+import org.sonar.java.model.ExpressionUtils;
 import org.sonar.java.model.LiteralUtils;
+import org.sonar.java.reporting.InternalJavaIssueBuilder;
+import org.sonar.java.reporting.JavaQuickFix;
+import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.JavaFileScanner;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.semantic.MethodMatchers;
@@ -28,12 +34,19 @@ import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
 import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
 import org.sonar.plugins.java.api.tree.ClassTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.Tree.Kind;
 
+import static org.sonar.java.reporting.AnalyzerMessage.textSpanBetween;
+
 @Rule(key = "S1155")
 public class CollectionIsEmptyCheck extends BaseTreeVisitor implements JavaFileScanner {
+
+  private enum EmptyComparisonType {
+    EMPTY, NOT_EMPTY
+  }
 
   private static final String JAVA_UTIL_COLLECTION = "java.util.Collection";
   private static final MethodMatchers SIZE_METHOD = MethodMatchers.create()
@@ -64,42 +77,86 @@ public class CollectionIsEmptyCheck extends BaseTreeVisitor implements JavaFileS
   public void visitBinaryExpression(BinaryExpressionTree tree) {
     super.visitBinaryExpression(tree);
 
-    if (hasCallToSizeMethod(tree) && isEmptyComparison(tree)) {
-      context.reportIssue(this, tree, "Use isEmpty() to check whether the collection is empty or not.");
+    getCallToSizeInvocation(tree).ifPresent(callToSizeInvocation ->
+      getEmptyComparisonType(tree).ifPresent(comparisonType ->
+        ((InternalJavaIssueBuilder) ((DefaultJavaFileScannerContext) context).newIssue())
+          .forRule(this)
+          .onTree(tree)
+          .withMessage("Use isEmpty() to check whether the collection is empty or not.")
+          .withQuickFix(() -> getQuickFix(tree, callToSizeInvocation, comparisonType))
+          .report()
+      ));
+  }
+
+  private static JavaQuickFix getQuickFix(BinaryExpressionTree tree, MethodInvocationTree callToSizeInvocation, EmptyComparisonType emptyComparisonType) {
+    IdentifierTree sizeCallIdentifier = ExpressionUtils.methodName(callToSizeInvocation);
+    // We want to keep the object on which "size" is called, we therefore replace everything before with ! (if needed) and after with "isEmpty()".
+    return JavaQuickFix.newQuickFix("Use \"isEmpty()\"")
+      .addTextEdit(
+        JavaTextEdit.replaceTextSpan(textSpanBetween(tree.firstToken(), true, callToSizeInvocation, false),
+          emptyComparisonType == EmptyComparisonType.EMPTY ? "" : "!"),
+        JavaTextEdit.replaceTextSpan(textSpanBetween(sizeCallIdentifier, true, tree.lastToken(), true),
+          "isEmpty()"))
+      .build();
+  }
+
+  private static Optional<MethodInvocationTree> getCallToSizeInvocation(BinaryExpressionTree tree) {
+    Optional<MethodInvocationTree> leftCallToSize = getCallToSizeInvocation(tree.leftOperand());
+    if (leftCallToSize.isPresent()) {
+      return leftCallToSize;
     }
+    return getCallToSizeInvocation(tree.rightOperand());
   }
 
-  private static boolean hasCallToSizeMethod(BinaryExpressionTree tree) {
-    return isCallToSizeMethod(tree.leftOperand()) ||
-      isCallToSizeMethod(tree.rightOperand());
-  }
-
-  private static boolean isCallToSizeMethod(ExpressionTree tree) {
-    if (!tree.is(Kind.METHOD_INVOCATION)) {
-      return false;
+  private static Optional<MethodInvocationTree> getCallToSizeInvocation(ExpressionTree tree) {
+    if (tree.is(Kind.METHOD_INVOCATION)) {
+      MethodInvocationTree invocationTree = (MethodInvocationTree) tree;
+      if (SIZE_METHOD.matches(invocationTree)) {
+        return Optional.of(invocationTree);
+      }
     }
-    return SIZE_METHOD.matches((MethodInvocationTree) tree);
+    return Optional.empty();
   }
 
-  private static boolean isEmptyComparison(BinaryExpressionTree tree) {
-    boolean result;
+  private static Optional<EmptyComparisonType> getEmptyComparisonType(BinaryExpressionTree tree) {
+    boolean leftIsZero = LiteralUtils.isZero(tree.leftOperand());
+    boolean leftIsOne = LiteralUtils.isOne(tree.leftOperand());
+    boolean rightIsZero = LiteralUtils.isZero(tree.rightOperand());
+    boolean rightIsOne = LiteralUtils.isOne(tree.rightOperand());
+    boolean anyZero = leftIsZero || rightIsZero;
 
-    if (isEqualityExpression(tree)) {
-      result = LiteralUtils.isZero(tree.leftOperand()) || LiteralUtils.isZero(tree.rightOperand());
-    } else if (tree.is(Kind.GREATER_THAN_OR_EQUAL_TO) || tree.is(Kind.LESS_THAN)) {
-      result = LiteralUtils.isZero(tree.leftOperand()) || LiteralUtils.isOne(tree.rightOperand());
-    } else if (tree.is(Kind.GREATER_THAN) || tree.is(Kind.LESS_THAN_OR_EQUAL_TO)) {
-      result = LiteralUtils.isOne(tree.leftOperand()) || LiteralUtils.isZero(tree.rightOperand());
-    } else {
-      result = false;
+    if (isEmptyComparison(tree, leftIsZero, leftIsOne, rightIsZero, rightIsOne, anyZero)) {
+      return Optional.of(EmptyComparisonType.EMPTY);
+    } else if (isNotEmptyComparison(tree, leftIsZero, leftIsOne, rightIsZero, rightIsOne, anyZero)) {
+      return Optional.of(EmptyComparisonType.NOT_EMPTY);
     }
-
-    return result;
+    return Optional.empty();
   }
 
-  private static boolean isEqualityExpression(BinaryExpressionTree tree) {
-    return tree.is(Kind.EQUAL_TO) ||
-      tree.is(Kind.NOT_EQUAL_TO);
+  private static boolean isEmptyComparison(BinaryExpressionTree tree, boolean leftIsZero, boolean leftIsOne, boolean rightIsZero, boolean rightIsOne, boolean anyZero) {
+    // size == 0, 0 == size
+    return (tree.is(Kind.EQUAL_TO) && anyZero)
+      // size > 1
+      || (tree.is(Kind.LESS_THAN) && rightIsOne)
+      // size <= 0
+      || (tree.is(Kind.LESS_THAN_OR_EQUAL_TO) && rightIsZero)
+      // 1 > size
+      || (tree.is(Kind.GREATER_THAN) && leftIsOne)
+      // 0 >= size
+      || (tree.is(Kind.GREATER_THAN_OR_EQUAL_TO) && leftIsZero);
+  }
+
+  private static boolean isNotEmptyComparison(BinaryExpressionTree tree, boolean leftIsZero, boolean leftIsOne, boolean rightIsZero, boolean rightIsOne, boolean anyZero) {
+    // size != 0, 0 != size
+    return (tree.is(Kind.NOT_EQUAL_TO) && anyZero)
+      // size > 0
+      || (tree.is(Kind.GREATER_THAN) && rightIsZero)
+      // size >= 1
+      || (tree.is(Kind.GREATER_THAN_OR_EQUAL_TO) && rightIsOne)
+      // 0 < size
+      || (tree.is(Kind.LESS_THAN) && leftIsZero)
+      // 1 <= size
+      || (tree.is(Kind.LESS_THAN_OR_EQUAL_TO) && leftIsOne);
   }
 
 }
