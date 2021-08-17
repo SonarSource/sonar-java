43cfdbc12bc4313fa4f77a5529bbb0675a2330e0
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-08-17 11:35:35 +0200

    SONARJAVA-3952 Add quick fixes for S2111 (BigDecimalDoubleConstructorCheck)

--- java-checks-test-sources/src/main/java/checks/BigDecimalDoubleConstructorCheck.java
+++ java-checks-test-sources/src/main/java/checks/BigDecimalDoubleConstructorCheck.java
@@ -10,8 +10,10 @@ class BigDecimalDoubleConstructorCheck {
   BigDecimal bd1_1 = new BigDecimal("2", mc);
   BigDecimal bd1_2 = new BigDecimal(new BigInteger("10"), mc);
   BigDecimal bd1_3 = new BigDecimal(1);
-  BigDecimal bd2 = new BigDecimal(2.0); // Noncompliant [[sc=20;ec=39]] {{Use "BigDecimal.valueOf" instead.}}
-  BigDecimal bd4 = new BigDecimal(2.0, mc); // Noncompliant {{Use "BigDecimal.valueOf" instead.}}
+  BigDecimal bd2 = new BigDecimal(2.0); // Noncompliant [[sc=20;ec=39;quickfixes=qf1]] {{Use "BigDecimal.valueOf" instead.}}
+  // fix@qf1 {{Replace with BigDecimal.valueOf}}
+  // edit@qf1 [[sc=20;ec=34]] {{BigDecimal.valueOf}}
+  BigDecimal bd4 = new BigDecimal(2.0, mc); // Noncompliant [[sc=20;ec=43;quickfixes=!]] {{Use "BigDecimal.valueOf" instead.}}
   BigDecimal bd5 = new BigDecimal(2.0f); // Noncompliant {{Use "BigDecimal.valueOf" instead.}}
   BigDecimal bd6 = new BigDecimal(2.0f, mc); // Noncompliant {{Use "BigDecimal.valueOf" instead.}}
   BigDecimal bd3 = BigDecimal.valueOf(2.0);
@@ -30,7 +32,9 @@ class BigDecimalDoubleConstructorCheck {
 
   float f2 = 0.1f;
   BigDecimal bd9_2 = new BigDecimal(f2); // Noncompliant
-  BigDecimal bd10_2 = new BigDecimal((float) 0.1); // Noncompliant
+  BigDecimal bd10_2 = new BigDecimal((float) 0.1); // Noncompliant [[sc=23;ec=50;quickfixes=qf2]]
+  // fix@qf2 {{Replace with BigDecimal.valueOf}}
+  // edit@qf2 [[sc=23;ec=37]] {{BigDecimal.valueOf}}
 
   Object myDoubleObject = 23.5d;
   BigDecimal bd11 = new BigDecimal((Double) myDoubleObject); // Noncompliant
