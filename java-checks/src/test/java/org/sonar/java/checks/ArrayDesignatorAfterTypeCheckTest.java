5a46d97931731eab9acae43793091ad85aa2b4a5
Alban Auzeill <alban.auzeill@sonarsource.com>
2021-08-23 16:58:51 +0200

    SONARJAVA-3998 Add quick fixes for S1195 (ArrayDesignatorAfterTypeCheck)

--- java-checks/src/test/java/org/sonar/java/checks/ArrayDesignatorAfterTypeCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/ArrayDesignatorAfterTypeCheckTest.java
@@ -20,15 +20,18 @@
 package org.sonar.java.checks;
 
 import org.junit.jupiter.api.Test;
-import org.sonar.java.checks.verifier.CheckVerifier;
+import org.sonar.java.checks.verifier.internal.InternalCheckVerifier;
+
+import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
 
 class ArrayDesignatorAfterTypeCheckTest {
 
   @Test
   void test() {
-    CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/ArrayDesignatorAfterTypeCheck.java")
+    InternalCheckVerifier.newInstance()
+      .onFile(testSourcesPath("checks/ArrayDesignatorAfterTypeCheck.java"))
       .withCheck(new ArrayDesignatorAfterTypeCheck())
+      .withQuickFixes()
       .verifyIssues();
   }
 
