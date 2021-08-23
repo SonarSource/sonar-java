cf1dcb70ee842bfc0fe8d7c6fdc0a24a97ba033a
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-08-23 14:33:20 +0200

    SONARJAVA-3945 Add quick fixes for S1107 (RightCurlyBraceSameLineAsNextBlockCheck)

--- java-checks/src/test/java/org/sonar/java/checks/RightCurlyBraceSameLineAsNextBlockCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/RightCurlyBraceSameLineAsNextBlockCheckTest.java
@@ -20,7 +20,7 @@
 package org.sonar.java.checks;
 
 import org.junit.jupiter.api.Test;
-import org.sonar.java.checks.verifier.CheckVerifier;
+import org.sonar.java.checks.verifier.internal.InternalCheckVerifier;
 
 import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
 
@@ -28,9 +28,10 @@ class RightCurlyBraceSameLineAsNextBlockCheckTest {
 
   @Test
   void detected() {
-    CheckVerifier.newVerifier()
+    InternalCheckVerifier.newInstance()
       .onFile(testSourcesPath("checks/RightCurlyBraceSameLineAsNextBlockCheck.java"))
       .withCheck(new RightCurlyBraceSameLineAsNextBlockCheck())
+      .withQuickFixes()
       .verifyIssues();
   }
 
