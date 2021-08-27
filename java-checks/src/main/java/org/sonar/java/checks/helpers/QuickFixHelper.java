01f89cf4b633a0e0cd2a3dd1db5339ac15278e65
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-08-27 18:50:33 +0200

    Provide JavaTextEdit helper method when requiring a new import

--- java-checks/src/main/java/org/sonar/java/checks/helpers/QuickFixHelper.java
+++ java-checks/src/main/java/org/sonar/java/checks/helpers/QuickFixHelper.java
@@ -19,13 +19,27 @@
  */
 package org.sonar.java.checks.helpers;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import javax.annotation.Nullable;
 import org.sonar.java.annotations.Beta;
+import org.sonar.java.annotations.VisibleForTesting;
 import org.sonar.java.collections.ListUtils;
 import org.sonar.java.model.DefaultJavaFileScannerContext;
 import org.sonar.java.model.JavaTree;
 import org.sonar.java.reporting.InternalJavaIssueBuilder;
+import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.tree.CompilationUnitTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.ImportTree;
+import org.sonar.plugins.java.api.tree.PackageDeclarationTree;
 import org.sonar.plugins.java.api.tree.SyntaxToken;
 import org.sonar.plugins.java.api.tree.Tree;
 
@@ -118,5 +132,157 @@ public class QuickFixHelper {
     sb.append(ListUtils.getLast(lines).substring(0, endIndex));
 
     return sb.toString();
+  }
+
+  /**
+   * Check if a given type "requiredType" is available in the current "context". Imports are cached to not have to recompute order all the time.
+   * A new import is required if the given type:
+   * <ul>
+   *   <li>is not in the same package of the current compilation unit</li>
+   *   <li>is not yet imported</li>
+   * </ul>
+   * If the type is not yet known in the context, the supplier will provide an edit to be inserted with a quick-fix,
+   * which will add the type as import in the existing list of imports, at the best place it can.
+   *
+   * If the type is already available in the current context, then no changes are required. There is no need of an extra import.
+   *
+   * @param context The context of analysis
+   */
+  public static ImportSupplier newImportSupplier(JavaFileScannerContext context) {
+    return new ImportSupplier(context.getTree());
+  }
+
+  public static class ImportSupplier {
+
+    @Nullable private final PackageDeclarationTree packageDeclaration;
+    @Nullable private final String packageName;
+    @Nullable private final Tree firstType;
+    private final List<ImportWithName> sortedNonStaticImports;
+    private final Set<String> importedTypes;
+    private final Set<String> starImportPackages;
+
+    private final Map<String, Optional<JavaTextEdit>> cachedResults = new HashMap<>();
+
+    private ImportSupplier(CompilationUnitTree cut) {
+      this.packageDeclaration = cut.packageDeclaration();
+      this.packageName = packageDeclaration == null ? null : ExpressionsHelper.concatenate(packageDeclaration.packageName());
+
+      List<Tree> types = cut.types();
+      this.firstType = types.isEmpty() ? null :  types.get(0);
+
+      this.sortedNonStaticImports = new ArrayList<>();
+      this.importedTypes = new HashSet<>();
+      this.starImportPackages = new HashSet<>();
+
+      cut.imports()
+        .stream()
+        .filter(importClauseTree -> importClauseTree.is(Tree.Kind.IMPORT))
+        .map(ImportTree.class::cast)
+        .filter(importTree -> !importTree.isStatic())
+        .map(ImportWithName::new)
+        .sorted(ImportWithName.COMPARATOR)
+        .forEach(importWithName -> {
+          sortedNonStaticImports.add(importWithName);
+          String importName = importWithName.importName();
+          if (importName.endsWith(".*")) {
+            starImportPackages.add(typeToPackageName(importName));
+          } else {
+            importedTypes.add(importName);
+          }
+        });
+    }
+
+    /**
+     * Only entry point to the supplier. Check if importing the required type is needed.
+     *
+     * @param requiredType The fully qualified name of a the type required to compile the associated quick-fix
+     * @return An empty Optional if there is no need of an extra import.
+     *         Otherwise, the edit inserting a new import at the best possible place corresponding to the required type.
+     */
+    public Optional<JavaTextEdit> newImportEdit(String requiredType) {
+      return cachedResults.computeIfAbsent(requiredType, this::locateNewImportEdit);
+    }
+
+    // We use "\n" systematically, the IDE will decide which one to use,
+    // therefore suppressing java:S3457 (Printf-style format strings should be used correctly)
+    @SuppressWarnings("java:S3457")
+    private Optional<JavaTextEdit> locateNewImportEdit(String requiredType) {
+      if (!requiresImportOf(requiredType)) {
+        return Optional.empty();
+      }
+
+      if (sortedNonStaticImports.isEmpty()) {
+        if (packageDeclaration != null) {
+          // could be a normal compilation unit or a package-info file: 2 lines after the package declaration
+          return Optional.of(JavaTextEdit.insertAfterTree(packageDeclaration, String.format("\n\nimport %s;", requiredType)));
+        }
+        // default package
+        if (firstType != null) {
+          // two lines before the first type
+          return Optional.of(JavaTextEdit.insertBeforeTree(firstType, String.format("import %s;\n\n", requiredType)));
+        }
+        // no package declaration and no type? Should be impossible or an empty file
+        return Optional.empty();
+      }
+
+      ImportTree lastCheckedImport = null;
+      for (ImportWithName importWithName : sortedNonStaticImports) {
+        String importedType = importWithName.importName();
+        if (requiredType.compareTo(importedType) <= 0) {
+          break;
+        }
+        lastCheckedImport = importWithName.tree();
+      }
+
+      if (lastCheckedImport != null) {
+        // in between the similar ones, in a logical order, alphabetically
+        return Optional.of(JavaTextEdit.insertAfterTree(lastCheckedImport, String.format("\nimport %s;", requiredType)));
+      }
+      // before the first one in alphabetical order
+      return Optional.of(JavaTextEdit.insertBeforeTree(sortedNonStaticImports.get(0).tree(), String.format("import %s;\n", requiredType)));
+    }
+
+    @VisibleForTesting
+    boolean requiresImportOf(String requiredType) {
+      if (importedTypes.contains(requiredType)) {
+        // already explicitly imported
+        return false;
+      }
+
+      String requiredPackage = typeToPackageName(requiredType);
+      if (starImportPackages.contains(requiredPackage)) {
+        // included in a star-import
+        return false;
+      }
+
+      // not part of the same package
+      return !requiredPackage.equals(packageName);
+    }
+
+    private static String typeToPackageName(String requiredType) {
+      return requiredType.substring(0, requiredType.lastIndexOf("."));
+    }
+
+    private static class ImportWithName {
+
+      private static final Comparator<ImportWithName> COMPARATOR = (i1, i2) -> i1.importName.compareTo(i2.importName);
+
+      private final String importName;
+      private final ImportTree tree;
+
+      private ImportWithName(ImportTree tree) {
+        this.tree = tree;
+        this.importName = ExpressionsHelper.concatenate((ExpressionTree) tree.qualifiedIdentifier());
+      }
+
+      public ImportTree tree() {
+        return tree;
+      }
+
+      public String importName() {
+        return importName;
+      }
+    }
+
   }
 }
--- java-checks/src/test/java/org/sonar/java/checks/helpers/QuickFixHelperTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/helpers/QuickFixHelperTest.java
@@ -19,11 +19,18 @@
  */
 package org.sonar.java.checks.helpers;
 
+import java.util.Optional;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.sonar.java.checks.helpers.QuickFixHelper.ImportSupplier;
 import org.sonar.java.model.InternalSyntaxToken;
+import org.sonar.java.reporting.JavaTextEdit;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.tree.ClassTree;
 import org.sonar.plugins.java.api.tree.CompilationUnitTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class QuickFixHelperTest {
@@ -69,4 +76,221 @@ class QuickFixHelperTest {
     assertThat(QuickFixHelper.previousToken(a.declarationKeyword())).isEqualTo(a.declarationKeyword());
   }
 
+  @Nested
+  class Imports {
+
+    /**
+     * Can only happen in a package-info file
+     */
+    @Test
+    void no_imports() {
+      String source = "package org.foo;";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.requiresImportOf("org.foo.A")).isFalse();
+      assertThat(supplier.requiresImportOf("org.bar.A")).isTrue();
+    }
+
+    @Test
+    void imported_via_star_import() {
+      String source = "package org.foo;\n"
+        + "import java.util.*;\n"
+        + "import org.bar.B;\n"
+        + "import static java.util.function.Function.identity;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.requiresImportOf("org.foo.B")).isFalse();
+      assertThat(supplier.requiresImportOf("java.util.List")).isFalse();
+      assertThat(supplier.requiresImportOf("java.util.Collections")).isFalse();
+
+      // requires import
+      assertThat(supplier.requiresImportOf("org.bar.A")).isTrue();
+      assertThat(supplier.requiresImportOf("java.util.function.Function")).isTrue();
+    }
+
+    @Test
+    void imported_via_explicit_import() {
+      String source = "package org.foo;\n"
+        + "import java.util.List;\n"
+        + "import org.bar.B;\n"
+        + "import static java.util.function.Function.identity;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.requiresImportOf("org.foo.B")).isFalse();
+      assertThat(supplier.requiresImportOf("java.util.List")).isFalse();
+
+      // requires import
+      assertThat(supplier.requiresImportOf("org.bar.A")).isTrue();
+      assertThat(supplier.requiresImportOf("java.util.Collections")).isTrue();
+    }
+
+    @Test
+    void default_package() {
+      String source = "import java.util.List;\n"
+        + "import org.bar.B;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.requiresImportOf("java.util.List")).isFalse();
+      assertThat(supplier.requiresImportOf("org.bar.B")).isFalse();
+
+      // requires import
+      assertThat(supplier.requiresImportOf("org.foo.B")).isTrue();
+      assertThat(supplier.requiresImportOf("org.bar.A")).isTrue();
+      assertThat(supplier.requiresImportOf("java.util.Collections")).isTrue();
+    }
+
+    @Test
+    void import_insertion_are_computed_only_once() {
+      String source = "package org.foo;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.newImportEdit("java.util.List"))
+        .isSameAs(supplier.newImportEdit("java.util.List"))
+        .isPresent();
+
+      assertThat(supplier.newImportEdit("org.foo.B"))
+        .isSameAs(supplier.newImportEdit("org.foo.B"))
+        .isNotPresent();
+    }
+
+    @Test
+    void type_already_imported_does_not_require_edits() {
+      String source = "package org.foo;\n"
+        + "import java.util.List;\n"
+        + "import java.util.function.*;\n"
+        + "import static java.util.function.Function.identity;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.newImportEdit("java.util.List")).isNotPresent();
+      assertThat(supplier.newImportEdit("java.util.function.Function")).isNotPresent();
+      assertThat(supplier.newImportEdit("org.foo.B")).isNotPresent();
+    }
+
+    @Test
+    void import_inserted_middle_other_imports() {
+      String source = "package org.foo;\n"
+        + "import java.util.List;\n"
+        + "import org.bar.B;\n"
+        + "import static java.util.function.Function.identity;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      Optional<JavaTextEdit> returnedValueSet = supplier.newImportEdit("java.util.Set");
+      assertThat(returnedValueSet).isPresent();
+      JavaTextEdit javaUtilSetEdit = returnedValueSet.get();
+      assertThat(javaUtilSetEdit.getReplacement()).isEqualTo("\nimport java.util.Set;");
+      assertThat(javaUtilSetEdit.getTextSpan().startLine).isEqualTo(2);
+      assertThat(javaUtilSetEdit.getTextSpan().startCharacter).isEqualTo(22);
+
+      Optional<JavaTextEdit> returnedValueAnimal = supplier.newImportEdit("org.bar.Animal");
+      assertThat(returnedValueAnimal).isPresent();
+      JavaTextEdit orgBarAnimal = returnedValueAnimal.get();
+      assertThat(orgBarAnimal.getReplacement()).isEqualTo("\nimport org.bar.Animal;");
+      assertThat(orgBarAnimal.getTextSpan().startLine).isEqualTo(2);
+      assertThat(orgBarAnimal.getTextSpan().startCharacter).isEqualTo(22);
+    }
+
+    @Test
+    void import_inserted_on_top_of_other_imports() {
+      String source = "package org.foo;\n"
+        + "import java.util.List;\n"
+        + "import org.bar.B;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      Optional<JavaTextEdit> returnedValue = supplier.newImportEdit("a.b.C");
+      assertThat(returnedValue).isPresent();
+      JavaTextEdit abcEdit = returnedValue.get();
+      assertThat(abcEdit.getReplacement()).isEqualTo("import a.b.C;\n");
+      assertThat(abcEdit.getTextSpan().startLine).isEqualTo(2);
+      assertThat(abcEdit.getTextSpan().startCharacter).isZero();
+    }
+
+    @Test
+    void import_inserted_after_other_imports() {
+      String source = "package org.foo;\n"
+        + "import java.util.List;\n"
+        + "import org.bar.B;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      Optional<JavaTextEdit> returnedValue = supplier.newImportEdit("z.z.Z");
+      assertThat(returnedValue).isPresent();
+      JavaTextEdit zzzEdit = returnedValue.get();
+      assertThat(zzzEdit.getReplacement()).isEqualTo("\nimport z.z.Z;");
+      assertThat(zzzEdit.getTextSpan().startLine).isEqualTo(3);
+      assertThat(zzzEdit.getTextSpan().startCharacter).isEqualTo(17);
+    }
+
+    @Test
+    void import_inserted_after_package_when_no_imports() {
+      String source = "package org.foo;\n"
+        + "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      Optional<JavaTextEdit> returnedValue = supplier.newImportEdit("org.bar.B");
+      assertThat(returnedValue).isPresent();
+      JavaTextEdit orgBarBEdit = returnedValue.get();
+      assertThat(orgBarBEdit.getReplacement()).isEqualTo("\n\nimport org.bar.B;");
+      assertThat(orgBarBEdit.getTextSpan().startLine).isEqualTo(1);
+      assertThat(orgBarBEdit.getTextSpan().startCharacter).isEqualTo(16);
+    }
+
+    @Test
+    void import_inserted_before_first_type_when_no_imports() {
+      String source = "class A { }";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      Optional<JavaTextEdit> returnedValue = supplier.newImportEdit("org.bar.B");
+      assertThat(returnedValue).isPresent();
+      JavaTextEdit orgBarBEdit = returnedValue.get();
+      assertThat(orgBarBEdit.getReplacement()).isEqualTo("import org.bar.B;\n\n");
+      assertThat(orgBarBEdit.getTextSpan().startLine).isEqualTo(1);
+      assertThat(orgBarBEdit.getTextSpan().startCharacter).isZero();
+    }
+
+    @Test
+    void import_not_inserted_on_empty_file() {
+      String source = "";
+
+      JavaFileScannerContext context = mockContext(source);
+      ImportSupplier supplier = QuickFixHelper.newImportSupplier(context);
+
+      assertThat(supplier.newImportEdit("org.bar.B")).isEmpty();
+    }
+
+    private JavaFileScannerContext mockContext(String source) {
+      JavaFileScannerContext context = Mockito.mock(JavaFileScannerContext.class);
+      Mockito.when(context.getTree())
+        .thenReturn(JParserTestUtils.parse(source));
+      return context;
+    }
+  }
 }
