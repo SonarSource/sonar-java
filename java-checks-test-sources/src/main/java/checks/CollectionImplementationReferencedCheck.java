0e3626c79a3364d141626fda5fccc20f82dadc4e
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-08-30 11:00:23 +0200

    SONARJAVA-3928 Move S1319 test files to compiled project

---
+++ java-checks-test-sources/src/main/java/checks/CollectionImplementationReferencedCheck.java
@@ -0,0 +1,64 @@
+package checks;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+class EmployeesTopLevel {
+
+  public EmployeesTopLevel(String s) { }
+
+  public HashSet<Employee> employees = new HashSet<Employee>();  // Noncompliant {{The type of the "employees" object should be an interface such as "Set" rather than the implementation "HashSet".}} [[sc=10;ec=27]]
+  private HashSet<Employee> employees2 = new HashSet<Employee>();
+
+  public HashSet<Employee> getEmployees() { return employees; }  // Noncompliant {{The return type of this method should be an interface such as "Set" rather than the implementation "HashSet".}}
+
+  public LinkedList<Employee> foo1() { return null; }            // Noncompliant {{The return type of this method should be an interface such as "List" rather than the implementation "LinkedList".}} [[sc=10;ec=30]]
+
+  private LinkedList<Employee> foo2() { return null; }
+
+  public java.util.HashSet<Employee> foo3() { return null; }      // Compliant - limitation
+
+  public HashMap foo4() { return null; }                          // Noncompliant {{The return type of this method should be an interface such as "Map" rather than the implementation "HashMap".}} [[sc=10;ec=17]]
+
+  private Stack stack;
+  private Vector vector;
+  public LinkedList<Employee> publicList; // Noncompliant {{The type of the "publicList" object should be an interface such as "List" rather than the implementation "LinkedList".}}
+  private LinkedList<Employee> privateList;
+  public ConcurrentHashMap concurrentHashMap() { // Noncompliant {{The return type of this method should be an interface such as "ConcurrentMap" rather than the implementation "ConcurrentHashMap".}}
+    return null;
+  }
+  public ConcurrentSkipListMap concurrentSkipListMap() { // Noncompliant {{The return type of this method should be an interface such as "ConcurrentMap" rather than the implementation "ConcurrentSkipListMap".}}
+    return null;
+  }
+  private void method1(LinkedList<Employee> employees) {}
+  public void method2(LinkedList<Employee> employees) {} // Noncompliant {{The type of the "employees" object should be an interface such as "List" rather than the implementation "LinkedList".}}
+
+  class A {
+    public void foo(HashMap<String, String> map) { // Noncompliant
+    }
+  }
+
+  class B extends A {
+    @Override
+    public void foo(HashMap<String, String> map) { // Compliant method is inherited
+    }
+  }
+}
+
+class Employee {
+
+  public Employee() {
+  }
+
+  private Set<Employee> employees = new HashSet<Employee>();      // Compliant
+
+  public Set<Employee> getEmployees(){                            // Compliant
+    return employees;
+  }
+}
