4c3d576bc2912ac9c14bc2ebb61d287f0e38dc25
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-08-30 16:47:49 +0200

    SONARJAVA-3941 Correctly handle binary expression

--- java-checks-test-sources/src/main/java/checks/BoxedBooleanExpressionsCheckWithQuickFixes.java
+++ java-checks-test-sources/src/main/java/checks/BoxedBooleanExpressionsCheckWithQuickFixes.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public class BoxedBooleanExpressionsCheckWithQuickFixes {
 
-  void boxedFor1(Boolean B) {
+  void boxedFor1(Boolean B, boolean b) {
     if (B) { // Noncompliant [[sc=9;ec=10;quickfixes=qf1]]
       // fix@qf1 {{Use the primitive boolean expression}}
       // edit@qf1 [[sc=9;ec=9]] {{Boolean.TRUE.equals(}}
@@ -30,6 +30,20 @@ public class BoxedBooleanExpressionsCheckWithQuickFixes {
       // fix@qf4 {{Use the primitive boolean expression}}
       // edit@qf4 [[sc=9;ec=9]] {{Boolean.TRUE.equals(}}
       // edit@qf4 [[sc=15;ec=15]] {{)}}
+      foo();
+    }
+
+    if (B == b) { // Noncompliant [[sc=9;ec=10;quickfixes=qf5]]
+      // fix@qf5 {{Use the primitive boolean expression}}
+      // edit@qf5 [[sc=9;ec=9]] {{Boolean.TRUE.equals(}}
+      // edit@qf5 [[sc=10;ec=10]] {{)}}
+      foo();
+    }
+
+    if (b == B) { // Noncompliant [[sc=14;ec=15;quickfixes=qf6]]
+      // fix@qf6 {{Use the primitive boolean expression}}
+      // edit@qf6 [[sc=14;ec=14]] {{Boolean.TRUE.equals(}}
+      // edit@qf6 [[sc=15;ec=15]] {{)}}
       foo();
     }
 
