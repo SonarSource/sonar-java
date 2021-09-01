a4e11fc6ce5f228ef7a80b3c7bdae011fad750c9
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-09-01 10:59:39 +0200

    SONARJAVA-3864 Missing arguments in Deprecated annotation should be reported in its own rule

---
+++ java-checks/src/main/java/org/sonar/java/checks/AbstractMissingDeprecatedChecker.java
@@ -0,0 +1,75 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import javax.annotation.CheckForNull;
+import org.sonar.java.ast.visitors.PublicApiChecker;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.AnnotationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.Tree.Kind;
+import org.sonar.plugins.java.api.tree.VariableTree;
+
+import static org.sonar.java.checks.helpers.DeprecatedCheckerHelper.deprecatedAnnotation;
+import static org.sonar.java.checks.helpers.DeprecatedCheckerHelper.hasJavadocDeprecatedTag;
+import static org.sonar.java.model.JUtils.isLocalVariable;
+
+public abstract class AbstractMissingDeprecatedChecker extends IssuableSubscriptionVisitor {
+
+  private static final Kind[] CLASS_KINDS = PublicApiChecker.classKinds();
+
+  private final Deque<Boolean> classOrInterfaceIsDeprecated = new ArrayDeque<>();
+
+  @Override
+  public List<Kind> nodesToVisit() {
+    return Arrays.asList(PublicApiChecker.apiKinds());
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    boolean isLocalVar = tree.is(Tree.Kind.VARIABLE) && isLocalVariable(((VariableTree) tree).symbol());
+    AnnotationTree deprecatedAnnotation = deprecatedAnnotation(tree);
+    boolean hasDeprecatedAnnotation = deprecatedAnnotation != null;
+    boolean hasJavadocDeprecatedTag = hasJavadocDeprecatedTag(tree);
+    if (currentClassNotDeprecated() && !isLocalVar) {
+      handleDeprecatedElement(tree, deprecatedAnnotation, hasJavadocDeprecatedTag);
+    }
+    if (tree.is(CLASS_KINDS)) {
+      classOrInterfaceIsDeprecated.push(hasDeprecatedAnnotation || hasJavadocDeprecatedTag);
+    }
+  }
+
+  abstract void handleDeprecatedElement(Tree tree, @CheckForNull AnnotationTree deprecatedAnnotation, boolean hasJavadocDeprecatedTag);
+
+  private boolean currentClassNotDeprecated() {
+    return classOrInterfaceIsDeprecated.isEmpty() || !classOrInterfaceIsDeprecated.peek();
+  }
+
+  @Override
+  public void leaveNode(Tree tree) {
+    if (tree.is(CLASS_KINDS)) {
+      classOrInterfaceIsDeprecated.pop();
+    }
+  }
+}
