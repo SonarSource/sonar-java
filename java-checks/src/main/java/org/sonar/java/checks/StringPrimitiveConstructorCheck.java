4fccca9b6da2767c9a547f38cdd69ecb8a8604ed
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2021-09-01 12:20:50 +0200

    Revert SONARJAVA-3924 Add quick fixes for S2129 (StringPrimitiveConstructorCheck) (#3785)

--- java-checks/src/main/java/org/sonar/java/checks/StringPrimitiveConstructorCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/StringPrimitiveConstructorCheck.java
@@ -22,14 +22,8 @@ package org.sonar.java.checks;
 import java.math.BigInteger;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.methods.AbstractMethodDetection;
-import org.sonar.java.model.DefaultJavaFileScannerContext;
 import org.sonar.java.model.LiteralUtils;
-import org.sonar.java.reporting.AnalyzerMessage;
-import org.sonar.java.reporting.InternalJavaIssueBuilder;
-import org.sonar.java.reporting.JavaQuickFix;
-import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.semantic.MethodMatchers;
-import org.sonar.plugins.java.api.tree.Arguments;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.LiteralTree;
 import org.sonar.plugins.java.api.tree.NewClassTree;
@@ -39,8 +33,6 @@ import org.sonar.plugins.java.api.tree.Tree;
 public class StringPrimitiveConstructorCheck extends AbstractMethodDetection {
 
   private static final String STRING = "java.lang.String";
-  private static final String BIG_INTEGER = "java.math.BigInteger";
-
   private static final BigInteger MIN_BIG_INTEGER_VALUE = BigInteger.valueOf(Long.MIN_VALUE);
   private static final BigInteger MAX_BIG_INTEGER_VALUE = BigInteger.valueOf(Long.MAX_VALUE);
 
@@ -61,7 +53,7 @@ public class StringPrimitiveConstructorCheck extends AbstractMethodDetection {
       MethodMatchers.create().ofTypes("java.lang.Float").constructor().addParametersMatcher("float").build(),
       MethodMatchers.create().ofTypes("java.lang.Double").constructor().addParametersMatcher("double").build(),
       MethodMatchers.create().ofTypes("java.lang.Boolean").constructor().addParametersMatcher("boolean").build(),
-      MethodMatchers.create().ofTypes(BIG_INTEGER).constructor().addParametersMatcher(STRING).build());
+      MethodMatchers.create().ofTypes("java.math.BigInteger").constructor().addParametersMatcher(STRING).build());
   }
 
   @Override
@@ -72,72 +64,11 @@ public class StringPrimitiveConstructorCheck extends AbstractMethodDetection {
     if(isBigIntegerPotentiallyBiggerThanLong(newClassTree)) {
       return;
     }
-    ((InternalJavaIssueBuilder) ((DefaultJavaFileScannerContext) context).newIssue())
-      .forRule(this)
-      .onTree(newClassTree.identifier())
-      .withMessage("Remove this \"%s\" constructor", newClassTree.symbolType().name())
-      .withQuickFix(() -> createQuickFix(newClassTree))
-      .report();
-  }
-
-  private static JavaQuickFix createQuickFix(NewClassTree newClassTree) {
-    if (newClassTree.symbolType().is(STRING)) {
-      return createStringQuickFix(newClassTree);
-    }
-    if (newClassTree.symbolType().is(BIG_INTEGER)) {
-      return createBigIntegerQuickFix(newClassTree);
-    }
-    return createDefaultQuickFix(newClassTree);
-  }
-
-  private static JavaQuickFix createDefaultQuickFix(NewClassTree newClassTree) {
-    String typeName = newClassTree.symbolType().name();
-    return JavaQuickFix.newQuickFix("Replace with " + typeName + ".valueOf")
-      .addTextEdit(JavaTextEdit.replaceBetweenTree(
-        newClassTree.firstToken(),
-        newClassTree.arguments().openParenToken(),
-        typeName + ".valueOf("))
-      .build();
-  }
-
-  private static JavaQuickFix createBigIntegerQuickFix(NewClassTree newClassTree) {
-    AnalyzerMessage.TextSpan constructor = AnalyzerMessage.textSpanFor(newClassTree);
-    AnalyzerMessage.TextSpan argument = AnalyzerMessage.textSpanFor(newClassTree.arguments().get(0));
-    return JavaQuickFix.newQuickFix("Replace with BigInteger.valueOf")
-        .addTextEdit(JavaTextEdit.replaceTextSpan(new AnalyzerMessage.TextSpan(
-          constructor.startLine, constructor.startCharacter,
-          argument.startLine, argument.startCharacter + 1 /* remove the first " */),
-          "BigInteger.valueOf("))
-        .addTextEdit(JavaTextEdit.replaceTextSpan(new AnalyzerMessage.TextSpan(
-          argument.endLine, argument.endCharacter - 1 /* remove the last " */,
-          constructor.endLine, constructor.endCharacter),
-          "L)"))
-        .build();
-  }
-
-  private static JavaQuickFix createStringQuickFix(NewClassTree newClassTree) {
-    Arguments arguments = newClassTree.arguments();
-    if (arguments.isEmpty()) {
-      return JavaQuickFix.newQuickFix("Replace with \"\"")
-        .addTextEdit(JavaTextEdit.replaceTree(newClassTree, "\"\""))
-        .build();
-    }
-    if (arguments.get(0).is(Tree.Kind.STRING_LITERAL)) {
-      return JavaQuickFix.newQuickFix("Remove \"new String\"")
-        .addTextEdit(JavaTextEdit.removeBetweenTree(newClassTree.firstToken(), arguments.openParenToken()))
-        .addTextEdit(JavaTextEdit.removeTree(arguments.closeParenToken()))
-        .build();
-    }
-    return JavaQuickFix.newQuickFix("Remove \"new String\"")
-      .addTextEdit(JavaTextEdit.removeTextSpan(AnalyzerMessage.textSpanBetween(
-        newClassTree.firstToken(), true,
-        arguments.openParenToken(), false)))
-      .build();
-
+    reportIssue(newClassTree.identifier(), "Remove this \"" + newClassTree.symbolType().name() + "\" constructor");
   }
 
   private static boolean isBigIntegerPotentiallyBiggerThanLong(NewClassTree newClassTree) {
-    if (!newClassTree.symbolType().is(BIG_INTEGER)) {
+    if (!newClassTree.symbolType().is("java.math.BigInteger")) {
       return false;
     }
     ExpressionTree argument = newClassTree.arguments().get(0);
--- java-checks/src/test/java/org/sonar/java/checks/StringPrimitiveConstructorCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/StringPrimitiveConstructorCheckTest.java
@@ -20,7 +20,7 @@
 package org.sonar.java.checks;
 
 import org.junit.jupiter.api.Test;
-import org.sonar.java.checks.verifier.internal.InternalCheckVerifier;
+import org.sonar.java.checks.verifier.CheckVerifier;
 
 import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
 
@@ -28,10 +28,9 @@ class StringPrimitiveConstructorCheckTest {
 
   @Test
   void test() {
-    InternalCheckVerifier.newInstance()
+    CheckVerifier.newVerifier()
       .onFile(testSourcesPath("checks/StringPrimitiveConstructorCheck.java"))
       .withCheck(new StringPrimitiveConstructorCheck())
-      .withQuickFixes()
       .verifyIssues();
   }
 }
