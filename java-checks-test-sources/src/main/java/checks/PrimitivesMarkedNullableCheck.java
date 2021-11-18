f2cab3deeea91b167a1b19ddd6c6d7f249d5ecfe
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2021-11-18 14:29:48 +0100

    SONARJAVA-4079 Improve Nullability annotations support in S4682 (PrimitivesMarkedNullableCheck) (#3847)

--- java-checks-test-sources/src/main/java/checks/PrimitivesMarkedNullableCheck.java
+++ java-checks-test-sources/src/main/java/checks/PrimitivesMarkedNullableCheck.java
@@ -1,15 +1,20 @@
 package checks;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 @interface AnotherAnnotation {
 }
 
+@CheckForNull
+@interface MyCheckForNull {
+}
+
 abstract class PrimitivesMarkedNullableCheck {
 
   @CheckForNull
-  abstract int getInt0(); // Noncompliant [[sc=12;ec=15]] {{"@CheckForNull" annotation should not be used on primitive types}}
+  abstract int getInt0(); // Noncompliant [[sc=12;ec=15;secondary=-1]] {{"@CheckForNull" annotation should not be used on primitive types}}
 
   abstract int getInt1();
 
@@ -17,15 +22,25 @@ abstract class PrimitivesMarkedNullableCheck {
   abstract int getInt2();
 
   @Nullable
-  protected abstract boolean isBool(); // Noncompliant [[sc=22;ec=29]] {{"@Nullable" annotation should not be used on primitive types}}
+  protected abstract boolean isBool(); // Noncompliant [[sc=22;ec=29;quickfixes=qf1]] {{"@Nullable" annotation should not be used on primitive types}}
+  // fix@qf1 {{Remove "@Nullable"}}
+  // edit@qf1 [[sl=-1;el=+0;sc=3;ec=3]] {{}}
 
   @javax.annotation.CheckForNull
-  public double getDouble0() { return 0.0; } // Noncompliant {{"@CheckForNull" annotation should not be used on primitive types}}
+  public double getDouble0() { return 0.0; } // Noncompliant [[sc=10;ec=16;quickfixes=qf2]] {{"@CheckForNull" annotation should not be used on primitive types}}
+  // fix@qf2 {{Remove "@CheckForNull"}}
+  // edit@qf2 [[sl=-1;el=+0;sc=3;ec=3]] {{}}
 
   @javax.annotation.Nullable
   public double getDouble1() { return 0.0; } // Noncompliant {{"@Nullable" annotation should not be used on primitive types}}
 
   public double getDouble2() { return 0.0; }
+
+  @MyCheckForNull
+  public double getDouble2_1() { return 0.0; } // Compliant, Nullable meta annotation are not taken into account
+
+  @Nonnull
+  public double getDouble2_2() { return 0.0; } // Compliant, Nonnull is useless, but is accepted as it can be added for consistency
 
   @javax.annotation.Nullable
   public Double getDouble3() { return 0.0; }
@@ -40,5 +55,15 @@ abstract class PrimitivesMarkedNullableCheck {
   public Object getObj1() { return null; }
 
   public Object getObj2() { return null; }
+
+  protected abstract @Nullable boolean isBool2(); // Noncompliant [[sc=32;ec=39;quickfixes=qf3]] {{"@Nullable" annotation should not be used on primitive types}}
+  // fix@qf3 {{Remove "@Nullable"}}
+  // edit@qf3 [[sc=22;ec=32]] {{}}
+
+}
+
+abstract class PrimitivesMarkedNullableCheckChild extends PrimitivesMarkedNullableCheckParent {
+
+  abstract int getInt0(); // Compliant, not directly marked as CheckForNull, an issue will be on the parent if needed.
 
 }
--- java-checks/src/main/java/org/sonar/java/checks/PrimitivesMarkedNullableCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/PrimitivesMarkedNullableCheck.java
@@ -22,11 +22,18 @@ package org.sonar.java.checks;
 import java.util.Collections;
 import java.util.List;
 import org.sonar.check.Rule;
-import org.sonar.java.se.NullableAnnotationUtils;
+import org.sonar.java.checks.helpers.QuickFixHelper;
+import org.sonar.java.reporting.JavaQuickFix;
+import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.semantic.SymbolMetadata;
 import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.TypeTree;
+
+import static org.sonar.java.reporting.AnalyzerMessage.textSpanBetween;
+import static org.sonar.plugins.java.api.semantic.SymbolMetadata.NullabilityLevel.METHOD;
 
 @Rule(key = "S4682")
 public final class PrimitivesMarkedNullableCheck extends IssuableSubscriptionVisitor {
@@ -41,9 +48,26 @@ public final class PrimitivesMarkedNullableCheck extends IssuableSubscriptionVis
     MethodTree methodTree = (MethodTree) tree;
     TypeTree returnType = methodTree.returnType();
     if (returnType.symbolType().isPrimitive()) {
-      NullableAnnotationUtils.nullableAnnotation(methodTree.modifiers())
-        .map(annotation -> annotation.annotationType().symbolType().name())
-        .ifPresent(annotation -> reportIssue(returnType, String.format("\"@%s\" annotation should not be used on primitive types", annotation)));
+      SymbolMetadata.NullabilityData nullabilityData = methodTree.symbol().metadata().nullabilityData();
+      if (nullabilityData.isNullable(METHOD, true, false)) {
+        SymbolMetadata.AnnotationInstance annotation = nullabilityData.annotation();
+        Tree annotationTree = nullabilityData.declaration();
+        // Both "annotation" and "declaration" should never be null, as we only target directly annotated methods. We keep the check for defensive programming.
+        if (annotation != null && annotationTree != null) {
+          String annotationName = annotation.symbol().name();
+          QuickFixHelper.newIssue(context)
+            .forRule(this)
+            .onTree(returnType)
+            .withMessage("\"@%s\" annotation should not be used on primitive types", annotationName)
+            .withSecondaries(new JavaFileScannerContext.Location("Child annotation", annotationTree))
+            .withQuickFix(() ->
+              JavaQuickFix.newQuickFix("Remove \"@%s\"", annotationName)
+                .addTextEdit(JavaTextEdit.removeTextSpan(textSpanBetween(annotationTree, true,
+                  QuickFixHelper.nextToken(annotationTree), false)))
+                .build())
+            .report();
+        }
+      }
     }
   }
 
