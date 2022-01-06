00dcc7ef0e80fed98dd23547893991744abd901f
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2022-01-06 10:42:53 +0100

    SONARJAVA-3770 Implement rule S6217: Omit permitted types when subclasses are in the same file as their superclass

---
+++ java-checks-test-sources/src/main/java/checks/OmitPermittedTypesCheck/OmitPermittedTypesCheckOtherFile.java
@@ -0,0 +1,9 @@
+package checks.OmitPermittedTypesCheck;
+
+public class OmitPermittedTypesCheckOtherFile {}
+
+final class InOtherFile2B extends InOtherFile2A {}
+
+final class InOtherFile2C extends InOtherFile2A {}
+
+final class PartiallyInOtherFile2C extends PartiallyInOtherFile2A {}
