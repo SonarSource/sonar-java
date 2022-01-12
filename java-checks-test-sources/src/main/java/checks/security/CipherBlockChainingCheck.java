eeba80d1e4fe17855a6bf15c8aaf6f24c7a1eca8
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-01-12 18:03:56 +0100

    SONARJAVA-4122 Added reproducer in test file

--- java-checks-test-sources/src/main/java/checks/security/CipherBlockChainingCheck.java
+++ java-checks-test-sources/src/main/java/checks/security/CipherBlockChainingCheck.java
@@ -2,7 +2,9 @@ package checks.security;
 
 import java.io.UnsupportedEncodingException;
 import java.security.SecureRandom;
+import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 class CipherBlockChainingCheck {
 
@@ -125,6 +127,35 @@ class CipherBlockChainingCheck {
     byte[] iv1 = "111".getBytes("UTF-8");
     byte[] iv2 = random.generateSeed(16);
     IvParameterSpec ivParameterSpec = new IvParameterSpec(iv1); // Noncompliant
+  }
+
+  private static final String OPERATION_MODE = "Threefish-1024/CBC/ISO10126Padding";
+  private static final int IV_SIZE = 2048;
+
+  static void encryptImpl(SecretKeySpec ks) throws Exception {
+    byte[] biv = new byte[IV_SIZE];
+
+    SecureRandom.getInstanceStrong().nextBytes(biv);
+
+    IvParameterSpec iv = new IvParameterSpec(biv); // Compliant
+
+    Cipher
+      .getInstance(OPERATION_MODE, "BC")
+      .init(Cipher.ENCRYPT_MODE, ks, iv);
+  }
+
+  static void decryptImpl1(byte[] biv, SecretKeySpec ks) throws Exception {
+    IvParameterSpec iv = new IvParameterSpec(biv); // Compliant - iv is going to be use for decryption only,
+                                                   // and needs to be in pair with the one used for encryption
+    Cipher
+      .getInstance(OPERATION_MODE, "BC")
+      .init(Cipher.DECRYPT_MODE, ks, iv);
+  }
+
+  static void decryptImpl2(byte[] biv, SecretKeySpec ks) throws Exception {
+    Cipher
+      .getInstance(OPERATION_MODE, "BC")
+      .init(Cipher.DECRYPT_MODE, ks, new IvParameterSpec(biv)); // compliant
   }
 }
 
