88b5007637da853d9b39e8244246a9843a34e4bf
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-14 17:02:38 +0100

    Move test files of rules S1111, S1114, S1150 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/ImplementsEnumerationCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/ImplementsEnumerationCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class ImplementsEnumerationCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/ImplementsEnumerationCheck.java")
+      .onFile(mainCodeSourcesPath("checks/ImplementsEnumerationCheck.java"))
       .withCheck(new ImplementsEnumerationCheck())
       .verifyIssues();
   }
