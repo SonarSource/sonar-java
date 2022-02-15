dc768c058aee2ac6657ebfd99df17e57f326b70d
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-02-15 16:11:23 +0100

    Move S2122 test source to compiling package

--- java-checks-test-sources/src/main/java/checks/ScheduledThreadPoolExecutorZeroCheck.java
+++ java-checks-test-sources/src/main/java/checks/ScheduledThreadPoolExecutorZeroCheck.java
@@ -1,6 +1,9 @@
+package checks;
+
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-class A {
+class ScheduledThreadPoolExecutorZeroCheck {
+  private static final int POOL_SIZE = 42;
   public void foo() {
     ScheduledThreadPoolExecutor stpe1 = new ScheduledThreadPoolExecutor(0); // Noncompliant [[sc=73;ec=74]] {{Increase the "corePoolSize".}}
     ScheduledThreadPoolExecutor stpe2 = new ScheduledThreadPoolExecutor(POOL_SIZE);
