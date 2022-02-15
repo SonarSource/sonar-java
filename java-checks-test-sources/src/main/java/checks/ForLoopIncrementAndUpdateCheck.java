a59907588f85fec58c47b09a938a35ebb9237bdb
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-15 16:51:03 +0100

    Move test file of rule S1994 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/ForLoopIncrementAndUpdateCheck.java
+++ java-checks-test-sources/src/main/java/checks/ForLoopIncrementAndUpdateCheck.java
@@ -1,8 +1,15 @@
-class A {
+package checks;
 
-  void foo() {
-    int i,j,k,l = 0; A a = new A(); int[] m;
-    for (i = 0; i< 10; j++, m[0]++) { // Noncompliant [[sc=5;ec=8;secondary=6]] {{Move the update of "i" into this loop's update clause.}}
+import java.util.Enumeration;
+
+class ForLoopIncrementAndUpdateCheck {
+
+  Object foo() {
+    int i = 0, j = 0, k = 0, l = 0;
+    A a = new A(), p, h = new A();
+    int[] m = new int[1000];
+
+    for (i = 0; i< 10; j++, m[0]++) { // Noncompliant [[sc=5;ec=8;secondary=13]] {{Move the update of "i" into this loop's update clause.}}
       i++;
     }
     for (i = 0; i< 10; j++) { // Compliant - i is updated multiple times
@@ -18,11 +25,11 @@ class A {
     for (i = 0; k< 10 && l<10 ; l++) {} // Compliant condition is using one of the incrementer
     for (i = 0; k< 10 && l<10 ; i++, j++, l++) {} // Compliant
     for (i = 0; k< 10 ; i++, j++, l++) {} // Compliant
-    for (i = 0; k< 10 && l<10; i++, j++) { // Noncompliant [[secondary=23]] {{Move the update of "k" into this loop's update clause.}}
+    for (i = 0; k< 10 && l<10; i++, j++) { // Noncompliant [[secondary=30]] {{Move the update of "k" into this loop's update clause.}}
       l -= 42;
       k++;
     }
-    for (i = 0; k< 10 && l<10; i++, j++) { // Noncompliant [[secondary=26,27]] {{Move the update of "k","l" into this loop's update clause.}}
+    for (i = 0; k< 10 && l<10; i++, j++) { // Noncompliant [[secondary=33,34]] {{Move the update of "k","l" into this loop's update clause.}}
       l--;
       k++;
     }
@@ -30,42 +37,48 @@ class A {
     for (i = 0; i< 10; a.myField+= 2) {} // Compliant
     for (i = 0; i< 10; a.myField++) {} // Compliant
     for (i = 0; a.myField < 10; a.myField++) {} // Compliant
-    for (i = 0; a.myField < 10; j++) { // Noncompliant  {{Move the update of "myField" into this loop's update clause.}}
+    for (i = 0; a.myField < 10; j++) { // Noncompliant {{Move the update of "myField" into this loop's update clause.}}
       (a.myField)++;
     }
 
     for (i = 0; j < 10 && l < 10 && i < 50; k++) { // Compliant
       m[0]++;
       m[l++] = 0;
-      if (fooBar()) {
+      if (blah()) {
         i++;
       }
-      unknown++;
     }
 
     for(i = 0; i<10; foo()) {}
-
     for (; !a.foo(); l++) {} // Compliant - no update in body
-    for (; foo(); l++) {} // Compliant - no update in body
-    // bar is unknown
-    for (; bar(); l++) {} // Compliant
-    for (; (p=h.next) !=null; h = p) {} // Compliant 
+    for (; blah(); l++) {} // Compliant - no update in body
+    for (; (p = h.next()) != null; h = p) {} // Compliant
     for (; new A().foo(); l++) {} // Compliant
 
-    for (Enumeration serverIds = db.serverTable.keys(); serverIds.hasMoreElements();){} // Compliant
+    for (Enumeration serverIds = a.next().keys(); serverIds.hasMoreElements();) {} // Compliant
     for (ExecutionState current = state; !current.equals(this); current = current.parentState) {} // Compliant
     for (Integer integer = 0; integer.longValue() < 100L ; integer += 1) {} // Compliant
-    for (int i = 2; ; i++) { // Compliant
-      if (someCondition(i)) {
+    for (int inc = 2; ; inc++) { // Compliant
+      if (someCondition(inc)) {
         return something;
       }
     }
   }
-  
-  int myField;
-  
+
+  private boolean blah() { return true; }
+  private boolean someCondition(int i) { return true; }
+
+  Object something;
+  ExecutionState state;
+
   class ExecutionState {
     ExecutionState parentState;
   }
-}
 
+  class A {
+    int myField;
+    boolean foo() { return true; }
+    A next() { return null; }
+    Enumeration keys() { return null; }
+  }
+}
