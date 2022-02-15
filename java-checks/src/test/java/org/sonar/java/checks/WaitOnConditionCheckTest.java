c3b1fc500a1f114853d7d42a805fc0047fbeb0c5
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-15 12:10:05 +0100

    Move test file of rule S1844 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/WaitOnConditionCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/WaitOnConditionCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class WaitOnConditionCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/WaitOnConditionCheck.java")
+      .onFile(mainCodeSourcesPath("checks/WaitOnConditionCheck.java"))
       .withCheck(new WaitOnConditionCheck())
       .verifyIssues();
   }
