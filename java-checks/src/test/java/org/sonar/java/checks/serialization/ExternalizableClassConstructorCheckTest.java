c271f48e8f0a7d2caa3dd2dae235d4c9bf49cfdf
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-15 18:05:28 +0100

    Move test file of rule S2060 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/serialization/ExternalizableClassConstructorCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/serialization/ExternalizableClassConstructorCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks.serialization;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class ExternalizableClassConstructorCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/serialization/ExternalizableClassConstructorCheck.java")
+      .onFile(mainCodeSourcesPath("checks/serialization/ExternalizableClassConstructorCheck.java"))
       .withCheck(new ExternalizableClassConstructorCheck())
       .verifyIssues();
   }
