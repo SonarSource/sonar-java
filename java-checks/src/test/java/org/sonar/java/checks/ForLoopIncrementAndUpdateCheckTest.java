09849ec2ecbf9b896e35a245224de19ccf200c24
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-15 16:51:03 +0100

    Move test file of rule S1994 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/ForLoopIncrementAndUpdateCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/ForLoopIncrementAndUpdateCheckTest.java
@@ -22,12 +22,23 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+import static org.sonar.java.checks.verifier.TestUtils.nonCompilingTestSourcesPath;;
+
 class ForLoopIncrementAndUpdateCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/ForLoopIncrementAndUpdateCheck.java")
+      .onFile(mainCodeSourcesPath("checks/ForLoopIncrementAndUpdateCheck.java"))
+      .withCheck(new ForLoopIncrementAndUpdateCheck())
+      .verifyIssues();
+  }
+
+  @Test
+  void non_compiling() {
+    CheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/ForLoopIncrementAndUpdateCheck.java"))
       .withCheck(new ForLoopIncrementAndUpdateCheck())
       .verifyIssues();
   }
