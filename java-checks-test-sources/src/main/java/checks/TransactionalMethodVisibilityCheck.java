ee41f8ea1e75923867a5f9238980c78961706ef9
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-02-16 15:24:09 +0100

    Move S2230 test source into compiling and non-compiling package

--- java-checks-test-sources/src/main/java/checks/TransactionalMethodVisibilityCheck.java
+++ java-checks-test-sources/src/main/java/checks/TransactionalMethodVisibilityCheck.java
@@ -1,6 +1,8 @@
+package checks;
+
 import org.springframework.transaction.annotation.Transactional;
 
-class A {
+class TransactionalMethodVisibilityCheck {
   
   public interface C {
     @Transactional
@@ -16,18 +18,8 @@ class A {
   public void publicTransactionalMethod() {} // Compliant
 
   @org.springframework.transaction.annotation.Transactional
-  private void privateTransactionalMethod() {} // Noncompliant [[sc=16;ec=42]] {{Make this method "public" or remove the "@Transactional" annotation}}
-
-  @org.springframework.transaction.annotation.Transactional
   protected void protectedTransactionalMethod() {} // Noncompliant {{Make this method "public" or remove the "@Transactional" annotation}}
   
   @org.springframework.transaction.annotation.Transactional
   void defaultVisibilityTransactionalMethod() {} // Noncompliant {{Make this method "public" or remove the "@Transactional" annotation}}
-
-  @org.xxx.Transactional
-  private void privateMethodWithNonSpringAnnotation() {} // Compliant
-
-  @Transactional
-  private void privateTransactionalMethodWithImportBasedAnnotation() {} // Noncompliant {{Make this method "public" or remove the "@Transactional" annotation}}
-  
 }
