6884bad900b32ddfbfaa7298d06d6d1926a61013
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-16 17:47:20 +0100

    Move test file of rule S4512 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/PopulateBeansCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/PopulateBeansCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class PopulateBeansCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/PopulateBeansCheck.java")
+      .onFile(mainCodeSourcesPath("checks/PopulateBeansCheck.java"))
       .withCheck(new PopulateBeansCheck())
       .verifyIssues();
   }
