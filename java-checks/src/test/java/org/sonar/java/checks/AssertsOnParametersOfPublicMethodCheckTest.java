97480159bcb1a8aba6eb5db6fbad61d2828af74d
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-16 16:47:58 +0100

    Move test file of rule S4274 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/AssertsOnParametersOfPublicMethodCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/AssertsOnParametersOfPublicMethodCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class AssertsOnParametersOfPublicMethodCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/AssertsOnParametersOfPublicMethodCheck.java")
+      .onFile(mainCodeSourcesPath("checks/AssertsOnParametersOfPublicMethodCheck.java"))
       .withCheck(new AssertsOnParametersOfPublicMethodCheck())
       .verifyIssues();
   }
