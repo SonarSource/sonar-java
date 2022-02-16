9f78293a4a488fce5733f0ad1f5be5823c0daa90
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-02-16 15:24:09 +0100

    Move S2230 test source into compiling and non-compiling package

---
+++ java-checks-test-sources/src/main/files/non-compiling/checks/TransactionalMethodVisibilityCheck.java
@@ -0,0 +1,17 @@
+package checks;
+
+import org.springframework.transaction.annotation.Transactional;
+
+class TransactionalMethodVisibilityCheck {
+  // Cannot compile because a Transactional method should be overridable
+  @org.springframework.transaction.annotation.Transactional
+  private void privateTransactionalMethod() {} // Noncompliant [[sc=16;ec=42]] {{Make this method "public" or remove the "@Transactional" annotation}}
+
+  // Cannot compile because a Transactional method should be overridable
+  @Transactional
+  private void privateTransactionalMethodWithImportBasedAnnotation() {} // Noncompliant {{Make this method "public" or remove the "@Transactional" annotation}}
+
+  @org.xxx.Transactional
+  private void privateMethodWithNonSpringAnnotation() {} // Compliant
+  
+}
