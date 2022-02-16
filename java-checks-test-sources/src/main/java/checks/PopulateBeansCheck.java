584d84577da0ca12a387ca1be5e9b685e4b32815
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-16 17:47:20 +0100

    Move test file of rule S4512 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/PopulateBeansCheck.java
+++ java-checks-test-sources/src/main/java/checks/PopulateBeansCheck.java
@@ -1,21 +1,18 @@
+package checks;
+
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.springframework.beans.PropertyAccessor;
 
-class Company {
-  String name;
-  void fun(PropertyAccessor pa){
-    Company bean = new Company();
+class S4512_Company {
+
+  void fun(PropertyAccessor pa) throws Exception {
+    S4512_Company bean = new S4512_Company();
     java.util.Map<String, Object> map = new java.util.HashMap();
-    Enumeration names = request.getParameterNames();
-    while (names.hasMoreElements()) {
-      String name = (String) names.nextElement();
-      map.put(name, request.getParameterValues(name));
-    }
     BeanUtils.populate(bean, map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     BeanUtils.setProperty(bean, "name", "value"); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
-    BeanUtilsBean.populate(bean, map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     BeanUtilsBean bub = new BeanUtilsBean();
+    bub.populate(bean, map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     bub.setProperty(bean, "name", "value"); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     pa.setPropertyValue("name", bean); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
     pa.setPropertyValues(map); // Noncompliant {{Make sure that setting JavaBean properties is safe here.}}
