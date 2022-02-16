fb221281f789abea4ea4d13859f77b70385aa4ac
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-16 11:55:15 +0100

    Move test file of rule S3046 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/TwoLocksWaitCheck.java
+++ java-checks-test-sources/src/main/java/checks/TwoLocksWaitCheck.java
@@ -1,10 +1,10 @@
+package checks;
 
-class Wait {
-
+class TwoLocksWaitCheck {
   Object mon1;
   Object mon2;
 
-  public Wait() {
+  public TwoLocksWaitCheck() throws Exception {
     synchronized (this.mon1) {
       System.out.println("Hello");
       synchronized (this.mon2) {
@@ -13,13 +13,13 @@ class Wait {
     }
   }
 
-  public synchronized void method() {
+  public synchronized void method() throws Exception {
     synchronized (this.mon2) {
       this.mon2.wait(); // Noncompliant [[sc=7;ec=23;secondary=16,17]] {{Don't use "wait()" here; multiple locks are held.}}
     }
   }
 
-  public synchronized void waitsWithTimeoutAreOK() {
+  public synchronized void waitsWithTimeoutAreOK() throws Exception {
     synchronized (this) {
       wait(10); // Compliant - wait with timeout is fishy, but perhaps OK
       wait(10, 1000); // Compliant - another kind of wait with timeout
@@ -29,8 +29,26 @@ class Wait {
   void lambdas() {
     synchronized (mon1) {
       synchronized (mon2) {
-        new Thread(() -> wait()).run(); // Compliant
+        new MyThread(() -> wait()).run(); // Compliant
       }
     }
+  }
+
+  static class MyThread {
+    MyThread(MyRunnable r) {}
+    void run() {}
+
+    void foo() {
+      MyThread m = new MyThread(null) {
+        @Override
+        void run() {
+          super.run();
+        }
+      };
+    }
+  }
+
+  interface MyRunnable {
+    void run() throws Exception;
   }
 }
