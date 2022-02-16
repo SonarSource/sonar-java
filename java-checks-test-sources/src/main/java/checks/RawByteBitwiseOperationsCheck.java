3ad7c2a51ce73cf5a8660852e4c6e9aa697fc3b9
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-16 11:16:25 +0100

    Move test file of rule S3034 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/RawByteBitwiseOperationsCheck.java
+++ java-checks-test-sources/src/main/java/checks/RawByteBitwiseOperationsCheck.java
@@ -1,34 +1,36 @@
-class A {
+package checks;
+
+class RawByteBitwiseOperationsCheck {
   byte b = 100;
 
   void foo() {
-    A a = new A();
+    RawByteBitwiseOperationsCheck a = new RawByteBitwiseOperationsCheck();
     int value = 1;
-    int longValue = 1L;
+    long longValue = 1L;
     byte b = 100;
 
     value = (value << 8) + b;            // Noncompliant [[sc=28;ec=29]] {{Prevent "int" promotion by adding "& 0xff" to this expression.}}
     value = readByte() + (value << 8);   // Noncompliant [[sc=13;ec=23]] {{Prevent "int" promotion by adding "& 0xff" to this expression.}}
     value = b | (value << 8);            // Noncompliant [[sc=13;ec=14]] {{Prevent "int" promotion by adding "& 0xff" to this expression.}}
     value = (value << 8) | readByte();   // Noncompliant [[sc=28;ec=38]] {{Prevent "int" promotion by adding "& 0xff" to this expression.}}
-    
+
     value = (value << 8) + (b & 0xff);          // Compliant
     value = (0xff & readByte()) + (value << 8); // Compliant
     value = (0xff & b) | (value << 8);          // Compliant
     value = (value << 8) | (readByte() & 0xff); // Compliant
     value = (value << 8) | (readByte() | 0xaa); // Compliant
-    
+
     longValue = (longValue << 8) + a.b;          // Noncompliant [[sc=36;ec=39]] {{Prevent "int" promotion by adding "& 0xff" to this expression.}}
     longValue = a.readByte() | (longValue << 8); // Noncompliant [[sc=17;ec=29]] {{Prevent "int" promotion by adding "& 0xff" to this expression.}}
-    
+
     value += b; // Compliant
     value += readByte(); // Compliant
     value = value & b; // Compliant
-    
+
     boolean bar = bar() & bar();
   }
-  
-  byte readByte() {}
-  
-  boolean bar();
+
+  byte readByte() { return 0;}
+
+  boolean bar() { return true; }
 }
--- java-checks/src/test/java/org/sonar/java/checks/RawByteBitwiseOperationsCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/RawByteBitwiseOperationsCheckTest.java
@@ -22,11 +22,13 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class RawByteBitwiseOperationsCheckTest {
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/RawByteBitwiseOperationsCheck.java")
+      .onFile(mainCodeSourcesPath("checks/RawByteBitwiseOperationsCheck.java"))
       .withCheck(new RawByteBitwiseOperationsCheck())
       .verifyIssues();
   }
