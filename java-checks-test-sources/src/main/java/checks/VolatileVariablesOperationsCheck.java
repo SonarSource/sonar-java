57aa99208ba9b432ed013c4d64baff886f7424d8
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-16 15:58:31 +0100

    Move test file of rule S3078 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/VolatileVariablesOperationsCheck.java
+++ java-checks-test-sources/src/main/java/checks/VolatileVariablesOperationsCheck.java
@@ -1,6 +1,8 @@
+package checks;
+
 import java.util.function.ObjIntConsumer;
 
-class A {
+class VolatileVariablesOperationsCheck {
   private volatile int count1 = 0;
   private volatile long count2 = 0L;
   private volatile Integer count3 = 0;
@@ -16,7 +18,7 @@ class A {
     count1++; // Noncompliant {{Use an "AtomicInteger" for this field; its operations are atomic.}}
     ++this.count1; // Noncompliant {{Use an "AtomicInteger" for this field; its operations are atomic.}}
     (count2)++; // Noncompliant {{Use an "AtomicLong" for this field; its operations are atomic.}}
-    count1 = (++count2); // Noncompliant {{Use an "AtomicLong" for this field; its operations are atomic.}}
+    count2 = (++count2); // Noncompliant {{Use an "AtomicLong" for this field; its operations are atomic.}}
     count3++; // Noncompliant {{Use an "AtomicInteger" for this field; its operations are atomic.}}
     ++count3; // Noncompliant
     count4++; // Noncompliant {{Use an "AtomicLong" for this field; its operations are atomic.}}
@@ -33,7 +35,7 @@ class A {
     count1--; // Noncompliant {{Use an "AtomicInteger" for this field; its operations are atomic.}}
     --count1; // Noncompliant
     (count2)--; // Noncompliant {{Use an "AtomicLong" for this field; its operations are atomic.}}
-    count1 = (--count2); // Noncompliant
+    count2 = (--count2); // Noncompliant
     count3--; // Noncompliant
     --count3; // Noncompliant
     count4--; // Noncompliant
@@ -56,7 +58,7 @@ class A {
     this.boo2 = (!this.boo1);
     boo1 = !boo2;
     boo1 = !true;
-    bool1 = boo2 = !false;
+    boo1 = boo2 = !false;
     return !boo1;
   }
 
