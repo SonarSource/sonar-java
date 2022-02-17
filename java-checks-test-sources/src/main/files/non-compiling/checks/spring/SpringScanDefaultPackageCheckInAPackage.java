e614ef86c1ed532a0f142df1dd5183c49b2c85a9
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-02-17 11:01:16 +0100

    Split S4602 test sources between compiling and non-compiling packages

--- java-checks-test-sources/src/main/files/non-compiling/checks/spring/SpringScanDefaultPackageCheckInAPackage.java
+++ java-checks-test-sources/src/main/files/non-compiling/checks/spring/SpringScanDefaultPackageCheckInAPackage.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "") // Noncompliant
 @ComponentScan(basePackageClasses = App.class)
-
 @SpringBootApplication(scanBasePackages = "") // Noncompliant
 @SpringBootApplication(scanBasePackageClasses = App.class)
 
