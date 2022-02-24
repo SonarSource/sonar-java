e8f74c1002b2171026d9cf46797ad7606f34e503
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-24 16:59:05 +0100

    Move test file of rule S1153 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/ConcatenationWithStringValueOfCheck.java
+++ java-checks-test-sources/src/main/java/checks/ConcatenationWithStringValueOfCheck.java
@@ -1,7 +1,11 @@
-class A {
-  Object o;
+package checks;
 
-  private void f() {
+class ConcatenationWithStringValueOfCheck {
+  Object o;
+  Object[] arr;
+  ConcatenationWithStringValueOfCheck valueOf;
+
+  private void f(int buf, ConcatenationWithStringValueOfCheck a) {
     System.out.println("" + String.valueOf(0)); // Noncompliant [[sc=29;ec=46]] {{Directly append the argument of String.valueOf().}}
     System.out.println("" + String.valueOf(null, 0, 0)); // Compliant
     System.out.println(String.valueOf(0)); // Compliant
@@ -11,12 +15,12 @@ class A {
       String.valueOf('a') + // Noncompliant
       String.valueOf('b') + // Noncompliant
       "");
-    System.out.println("" + String.valueOf()); // Compliant
-    System.out.println("" + String.foo(0)); // Compliant
-    System.out.println("" + foo.valueOf(0)); // Compliant
-    System.out.println("" + String.valueOf); // Compliant
-    System.out.println("" + String.valueOf[0]); // Compliant
-    System.out.println("" + String.valueOf.bar(0)); // Compliant
+    System.out.println("" + "".length()); // Compliant
+    System.out.println("" + a.bar(0)); // Compliant
+    System.out.println("" + Integer.valueOf(0)); // Compliant
+    System.out.println("" + a.o); // Compliant
+    System.out.println("" + a.arr[0]); // Compliant
+    System.out.println("" + a.valueOf.bar(0)); // Compliant
 
     o = ("" + String.valueOf('a')) + ""; // Noncompliant
     o = "" + ("" + String.valueOf('a')); // Noncompliant
@@ -30,4 +34,6 @@ class A {
     // coverage on targeted binary expressions
     o = 42 - 2;
   }
+
+  int bar(int i) { return 0; }
 }
