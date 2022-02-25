c8370b30e93fe066442c3a37047ae1b6b0974e7e
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-25 09:44:12 +0100

    Move test file of rule S1872 into 'java-checks-test-sources'

--- its/ruling/src/test/java/org/sonar/java/it/AutoScanTest.java
+++ its/ruling/src/test/java/org/sonar/java/it/AutoScanTest.java
@@ -171,7 +171,7 @@ public class AutoScanTest {
 
     assertThat(knownDiffByRules).isEqualTo(newDiffByRules);
     assertThat(rulesCausingFPs).hasSize(8);
-    assertThat(rulesNotReporting).hasSize(24);
+    assertThat(rulesNotReporting).hasSize(23);
     assertThat(rulesSilenced).hasSize(58);
 
     /**
---
+++ java-checks-test-sources/src/main/java/checks/ClassComparedByNameCheck.java
@@ -0,0 +1,58 @@
+package checks;
+
+import java.util.List;
+
+class ClassComparedByNameCheck {
+  Class valueClass;
+
+  String foo(String s) {
+    A.class.getSimpleName().equals("A"); // Noncompliant [[sc=5;ec=40]] {{Use an "instanceof" comparison instead.}}
+    new A().getClass().getSimpleName().equals("A"); // Noncompliant {{Use an "instanceof" comparison instead.}}
+    new A().getClass().getName().equals("A"); // Noncompliant {{Use an "instanceof" comparison instead.}}
+    String name = new A().getClass().getName();
+    name.equals("A"); // False negative ?
+    A.class.getSimpleName().substring(0).equals(name); // Compliant - ref: SONARJAVA-2603
+    A a = new A();
+    "A".equals(a.getClass().getSimpleName()); // Noncompliant
+    foo(A.class.getSimpleName()).equals("A");
+
+    StackTraceElement element = getElement();
+    A.class.getSimpleName().equals(element.getClassName()); // Compliant
+    boolean b = (List.class.getName().equals(valueClass.getName())); // Noncompliant {{Use "isAssignableFrom" instead.}}
+    A.class.getSimpleName().equals(foo("A")); // Compliant
+
+    return "";
+  }
+
+  StackTraceElement getElement() {
+    return null;
+  }
+
+    public static class A {
+    Object foo(String realType) {
+      if (Integer.class.getName().equals(realType)) { // compliant
+        return null;
+      }
+      return null;
+    }
+  }
+
+  public abstract class B {
+    boolean foo(Iterable<String> argTypes) {
+      for (String argType : argTypes) {
+        if (Object.class.getName().equals(argType)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    boolean bar() {
+      String argType = getParameterTypeFromName("MY_PARAM");
+      return Object.class.getName().equals(argType);
+
+    }
+
+    abstract String getParameterTypeFromName(String s);
+  }
+}
