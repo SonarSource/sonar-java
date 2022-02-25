e7eb831c940eb2a0ecc587c644b551af97ff466f
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-25 17:32:06 +0100

    Move test file of rule S2388 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/CallSuperMethodFromInnerClassCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/CallSuperMethodFromInnerClassCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class CallSuperMethodFromInnerClassCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/CallSuperMethodFromInnerClassCheck.java")
+      .onFile(mainCodeSourcesPath("checks/CallSuperMethodFromInnerClassCheck.java"))
       .withCheck(new CallSuperMethodFromInnerClassCheck())
       .verifyIssues();
   }
