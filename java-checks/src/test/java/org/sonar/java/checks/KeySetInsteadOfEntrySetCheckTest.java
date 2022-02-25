210c58dd86eeb747fe004f075b3269c079e7c3d0
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-25 22:28:30 +0100

    Move test file of rule S2864 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/KeySetInsteadOfEntrySetCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/KeySetInsteadOfEntrySetCheckTest.java
@@ -22,12 +22,14 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class KeySetInsteadOfEntrySetCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/KeySetInsteadOfEntrySet.java")
+      .onFile(mainCodeSourcesPath("checks/KeySetInsteadOfEntrySet.java"))
       .withCheck(new KeySetInsteadOfEntrySetCheck())
       .verifyIssues();
   }
