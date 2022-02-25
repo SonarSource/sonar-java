7fcb943efedb9079175d1592e1b8c8dfabf87222
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-25 17:15:59 +0100

    Move test file of rule S2254 into 'java-checks-test-sources'

--- java-checks-test-sources/src/main/java/checks/GetRequestedSessionIdCheck.java
+++ java-checks-test-sources/src/main/java/checks/GetRequestedSessionIdCheck.java
@@ -1,8 +1,12 @@
+package checks;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyServlet extends HttpServlet {
+public class GetRequestedSessionIdCheck extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String sessionId = request.getRequestedSessionId(); // Noncompliant [[sc=32;ec=53]] {{Remove use of this unsecured "getRequestedSessionId()" method}}
   }
