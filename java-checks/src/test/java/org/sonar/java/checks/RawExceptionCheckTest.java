13a8f6a5742159eba9df5b7b7a3fd8117b243b06
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-28 17:52:13 +0100

    Move test file of rule S112 into 'java-checks-test-sources'

--- java-checks/src/test/java/org/sonar/java/checks/RawExceptionCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/RawExceptionCheckTest.java
@@ -22,11 +22,13 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
 class RawExceptionCheckTest {
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile("src/test/files/checks/RawException.java")
+      .onFile(mainCodeSourcesPath("checks/RawExceptionCheck.java"))
       .withCheck(new RawExceptionCheck())
       .verifyIssues();
   }
