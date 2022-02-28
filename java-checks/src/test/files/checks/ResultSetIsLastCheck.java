15d2eeaa51e31f25d8f881e3a327b660927c21c7
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-02-28 12:00:45 +0100

    Move test file of rule S2232 into 'java-checks-test-sources'

--- java-checks/src/test/files/checks/ResultSetIsLastCheck.java
+++
@@ -1,9 +0,0 @@
-import java.sql.ResultSet;
-
-class A{
-  void foo() {
-    ResultSet rs;
-    rs.isLast(); // Noncompliant [[sc=8;ec=14]] {{Remove this call to "isLast()".}}
-    rs.afterLast();
-  }
-}
