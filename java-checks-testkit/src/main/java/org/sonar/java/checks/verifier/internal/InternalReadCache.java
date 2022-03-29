535d869a2f850a6d9c642a6e7cb957b8def8b723
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-03-29 22:50:44 +0200

    Add an internal read cache implementation

---
+++ java-checks-testkit/src/main/java/org/sonar/java/checks/verifier/internal/InternalReadCache.java
@@ -0,0 +1,49 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.verifier.internal;
+
+import org.sonar.api.batch.sensor.cache.ReadCache;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InternalReadCache implements ReadCache {
+  private Map<String, byte[]> cache = new HashMap<>();
+
+  @Override
+  public InputStream read(String key) {
+    if (!cache.containsKey(key)) {
+      throw new IllegalArgumentException(String.format("cache does not contain key \"%s\"", key));
+    }
+    return new ByteArrayInputStream(cache.get(key));
+  }
+
+  @Override
+  public boolean contains(String key) {
+    return cache.containsKey(key);
+  }
+
+  public ReadCache put(String key, byte[] data) {
+    cache.put(key, data);
+    return this;
+  }
+}
