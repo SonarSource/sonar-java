514da8144ec40cdf67208e2aeaa12efb5fa7fe7e
Alban Auzeill <alban.auzeill@sonarsource.com>
2022-03-29 13:35:31 +0200

    Temporary test when= When.MAYBE

--- java-checks-test-sources/src/main/java/checks/NullShouldNotBeUsedWithOptionalCheck_jdk.java
+++ java-checks-test-sources/src/main/java/checks/NullShouldNotBeUsedWithOptionalCheck_jdk.java
@@ -94,7 +94,7 @@ class NullShouldNotBeUsedWithOptionalCheck_jdkClassA {
   }
 
   public void NonnullWithArgument1() {
-    @javax.annotation.Nonnull(when= When.MAYBE) // Noncompliant [[sc=5;ec=48]] {{"Optional" variables should not be "@Nonnull(when=MAYBE)".}}
+    @javax.annotation.Nonnull(when= When.NEVER) // Noncompliant [[sc=5;ec=48]] {{"Optional" variables should not be "@Nonnull(when=NEVER)".}}
     Optional<String> var;
   }
 
