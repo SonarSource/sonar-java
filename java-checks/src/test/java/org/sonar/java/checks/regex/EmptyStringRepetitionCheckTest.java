cc33bb6fad760dd0a43423ed060202c0ce94fd74
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-04-26 14:54:31 +0200

    SONARJAVA-4153 Refactor S5842 using sonar-analyzer-commons

--- java-checks/src/test/java/org/sonar/java/checks/regex/EmptyStringRepetitionCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/regex/EmptyStringRepetitionCheckTest.java
@@ -22,14 +22,14 @@ package org.sonar.java.checks.regex;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
 
 class EmptyStringRepetitionCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile(testSourcesPath("checks/regex/EmptyStringRepetitionCheck.java"))
+      .onFile(mainCodeSourcesPath("checks/regex/EmptyStringRepetitionCheck.java"))
       .withCheck(new EmptyStringRepetitionCheck())
       .verifyIssues();
   }
