028e45f606eaecc4ac6cdaf039fb70a967671d05
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-04-26 10:15:28 +0200

    SONARJAVA-4223 Update autoscan IT expectations with S5693 test sources

--- java-checks-test-sources/src/main/java/checks/security/ExcessiveContentRequestCheck/caching/Sanitizer.java
+++ java-checks-test-sources/src/main/java/checks/security/ExcessiveContentRequestCheck/caching/Sanitizer.java
@@ -4,12 +4,13 @@ import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 public class Sanitizer {
-  public static CommonsMultipartResolver sanitize(CommonsMultipartResolver resolver) {
+
+  public CommonsMultipartResolver sanitize(CommonsMultipartResolver resolver) {
     resolver.setMaxUploadSize(1L);
     return resolver;
   }
 
-  public static MultipartConfigFactory sanitize(MultipartConfigFactory factory) {
+  public MultipartConfigFactory sanitize(MultipartConfigFactory factory) {
     factory.setMaxFileSize(1L);
     factory.setMaxRequestSize(1L);
     return factory;
