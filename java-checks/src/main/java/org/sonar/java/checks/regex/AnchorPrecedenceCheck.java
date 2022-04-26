b2466b67d603631598e9afac844087d0b26faadd
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-04-26 14:24:09 +0200

    SONARJAVA-4156 Refactor S5850 using sonar-analyzer-commons

--- java-checks/src/main/java/org/sonar/java/checks/regex/AnchorPrecedenceCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/regex/AnchorPrecedenceCheck.java
@@ -19,99 +19,17 @@
  */
 package org.sonar.java.checks.regex;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.sonar.check.Rule;
-import org.sonarsource.analyzer.commons.regex.RegexParseResult;
-import org.sonarsource.analyzer.commons.regex.ast.BoundaryTree;
-import org.sonarsource.analyzer.commons.regex.ast.DisjunctionTree;
-import org.sonarsource.analyzer.commons.regex.ast.NonCapturingGroupTree;
-import org.sonarsource.analyzer.commons.regex.ast.RegexBaseVisitor;
-import org.sonarsource.analyzer.commons.regex.ast.RegexTree;
-import org.sonarsource.analyzer.commons.regex.ast.SequenceTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonarsource.analyzer.commons.regex.RegexParseResult;
+import org.sonarsource.analyzer.commons.regex.finders.AnchorPrecedenceFinder;
 
 @Rule(key = "S5850")
 public class AnchorPrecedenceCheck extends AbstractRegexCheck {
 
   @Override
   public void checkRegex(RegexParseResult regexForLiterals, ExpressionTree methodInvocationOrAnnotation) {
-    new Visitor().visit(regexForLiterals);
-  }
-
-  private enum Position {
-    BEGINNING, END
-  }
-
-  private class Visitor extends RegexBaseVisitor {
-
-    @Override
-    public void visitDisjunction(DisjunctionTree tree) {
-      List<RegexTree> alternatives = tree.getAlternatives();
-      if ((anchoredAt(alternatives, Position.BEGINNING) || anchoredAt(alternatives, Position.END))
-        && notAnchoredElseWhere(alternatives)) {
-        reportIssue(tree, "Group parts of the regex together to make the intended operator precedence explicit.", null, Collections.emptyList());
-      }
-      super.visitDisjunction(tree);
-    }
-
-    private boolean anchoredAt(List<RegexTree> alternatives, Position position) {
-      int itemIndex = position == Position.BEGINNING ? 0 : (alternatives.size() - 1);
-      RegexTree firstOrLast = alternatives.get(itemIndex);
-      return isAnchored(firstOrLast, position);
-    }
-
-    private boolean notAnchoredElseWhere(List<RegexTree> alternatives) {
-      if (isAnchored(alternatives.get(0), Position.END)
-        || isAnchored(alternatives.get(alternatives.size() - 1), Position.BEGINNING)) {
-        return false;
-      }
-      for (RegexTree alternative : alternatives.subList(1, alternatives.size() - 1)) {
-        if (isAnchored(alternative, Position.BEGINNING) || isAnchored(alternative, Position.END)) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    private boolean isAnchored(RegexTree tree, Position position) {
-      if (!tree.is(RegexTree.Kind.SEQUENCE)) {
-        return false;
-      }
-      SequenceTree sequence = (SequenceTree) tree;
-      List<RegexTree> items = sequence.getItems().stream()
-        .filter(item -> !isFlagSetter(item))
-        .collect(Collectors.toList());
-      if (items.isEmpty()) {
-        return false;
-      }
-      int index = position == Position.BEGINNING ? 0 : (items.size() - 1);
-      RegexTree firstOrLast = items.get(index);
-      return firstOrLast.is(RegexTree.Kind.BOUNDARY) && isAnchor((BoundaryTree) firstOrLast);
-    }
-
-    private boolean isAnchor(BoundaryTree tree) {
-      switch (tree.type()) {
-        case INPUT_START:
-        case LINE_START:
-        case INPUT_END:
-        case INPUT_END_FINAL_TERMINATOR:
-        case LINE_END:
-          return true;
-        default:
-          return false;
-      }
-    }
-
-    /**
-     * Return whether the given regex is a non-capturing group without contents, i.e. one that only sets flags for the
-     * rest of the expression
-     */
-    private boolean isFlagSetter(RegexTree tree) {
-      return tree.is(RegexTree.Kind.NON_CAPTURING_GROUP) && ((NonCapturingGroupTree) tree).getElement() == null;
-    }
-
+    new AnchorPrecedenceFinder(this::reportIssueFromCommons).visit(regexForLiterals);
   }
 
 }
