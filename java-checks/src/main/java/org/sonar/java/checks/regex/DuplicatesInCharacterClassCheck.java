cb9b6e429226123862d2a668d5eac7d913299800
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-04-27 16:17:57 +0200

    SONARJAVA-4161 Refactor S5869 using sonar-analyzer-commons

--- java-checks/src/main/java/org/sonar/java/checks/regex/DuplicatesInCharacterClassCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/regex/DuplicatesInCharacterClassCheck.java
@@ -19,59 +19,17 @@
  */
 package org.sonar.java.checks.regex;
 
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.sonar.check.Rule;
-import org.sonar.java.checks.helpers.SimplifiedRegexCharacterClass;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonarsource.analyzer.commons.regex.RegexParseResult;
-import org.sonarsource.analyzer.commons.regex.ast.CharacterClassElementTree;
-import org.sonarsource.analyzer.commons.regex.ast.CharacterClassUnionTree;
-import org.sonarsource.analyzer.commons.regex.ast.RegexBaseVisitor;
-import org.sonarsource.analyzer.commons.regex.ast.RegexSyntaxElement;
+import org.sonarsource.analyzer.commons.regex.finders.DuplicatesInCharacterClassFinder;
 
 @Rule(key = "S5869")
 public class DuplicatesInCharacterClassCheck extends AbstractRegexCheck {
 
-  private static final String MESSAGE = "Remove duplicates in this character class.";
-
   @Override
   public void checkRegex(RegexParseResult regexForLiterals, ExpressionTree methodInvocationOrAnnotation) {
-    new DuplicateFinder().visit(regexForLiterals);
-  }
-
-  private class DuplicateFinder extends RegexBaseVisitor {
-
-    @Override
-    public void visitCharacterClassUnion(CharacterClassUnionTree tree) {
-      Set<RegexSyntaxElement> duplicates = new LinkedHashSet<>();
-      SimplifiedRegexCharacterClass characterClass = new SimplifiedRegexCharacterClass();
-      for (CharacterClassElementTree element : tree.getCharacterClasses()) {
-        List<RegexSyntaxElement> intersections = new SimplifiedRegexCharacterClass(element).findIntersections(characterClass);
-
-        if (!intersections.isEmpty()) {
-          for (RegexSyntaxElement intersection : intersections) {
-            if (!duplicates.contains(intersection)) {
-              // The element the current element is intersecting with should be included as well.
-              duplicates.add(intersection);
-            }
-          }
-          duplicates.add(element);
-        }
-        characterClass.add(element);
-      }
-      if (!duplicates.isEmpty()) {
-        List<RegexIssueLocation> secondaries = duplicates.stream()
-          .skip(1)
-          .map(duplicate -> new RegexIssueLocation(duplicate, "Additional duplicate"))
-          .collect(Collectors.toList());
-        reportIssue(duplicates.iterator().next(), MESSAGE, null, secondaries);
-      }
-      super.visitCharacterClassUnion(tree);
-    }
-
+    new DuplicatesInCharacterClassFinder(this::reportIssueFromCommons).visit(regexForLiterals);
   }
 
 }
