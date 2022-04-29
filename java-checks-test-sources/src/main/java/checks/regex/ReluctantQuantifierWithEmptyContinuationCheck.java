dd13ea9a2a4700c6f7d39303e794825ef9e7f904
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-04-29 16:08:37 +0200

    SONARJAVA-4166 Refactor S6019 using sonar-analyzer-commons

--- java-checks-test-sources/src/main/java/checks/regex/ReluctantQuantifierWithEmptyContinuationCheck.java
+++ java-checks-test-sources/src/main/java/checks/regex/ReluctantQuantifierWithEmptyContinuationCheck.java
@@ -7,25 +7,48 @@ import org.apache.commons.lang3.RegExUtils;
 
 public class ReluctantQuantifierWithEmptyContinuationCheck {
 
-  @Email(regexp = ".*?") // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
   void noncompliant(String str) {
-    Pattern.compile(".*?x?").matcher(str).find(); // Noncompliant [[sc=22;ec=25]] {{Fix this reluctant quantifier that will only ever match the empty string.}}
-    Pattern.compile(".*?$").matcher(str).find(); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
-    Pattern.compile(".*?x?^").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match the empty string.}}
-    str.split(".*?x?^"); // Noncompliant {{Fix this reluctant quantifier that will only ever match the empty string.}}
+    // ====== Reluctant quantifier that will only ever match the empty string ======
+    // Partial match
+    Pattern.compile(".*?").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    Pattern.compile(".+?").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match 1 repetition.}}
+    Pattern.compile(".{4}?").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match 4 repetitions.}}
+    Pattern.compile(".{2,4}?").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match 2 repetitions.}}
+    Pattern.compile(".*?x?").matcher(str).find(); // Noncompliant [[sc=22;ec=25]] {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    Pattern.compile(".*?()").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    Pattern.compile(".*?x?^").matcher(str).find(); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    str.split(".*?x?^"); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    Matcher mPartial = Pattern.compile(".*?").matcher(str); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    mPartial.find();
+    RegExUtils.removeAll(str, ".*?"); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    Pattern p = Pattern.compile(".*?"); // Noncompliant {{Fix this reluctant quantifier that will only ever match 0 repetitions.}}
+    RegExUtils.removeAll(str, p);
+
+    // ====== Unnecessarily reluctant quantifier ======
+    // Full match (implicitly end with end anchor "$")
     str.matches(".*?"); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    Matcher mFull = Pattern.compile(".*?").matcher(str); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    mFull.matches();
     str.matches(".*?()"); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
     str.matches(".*?()*"); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
     str.matches(".*?((?=))*"); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
     str.matches(".*?(?!x)"); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
-
-    Matcher m = Pattern.compile(".*?").matcher(str); // Noncompliant {{Fix this reluctant quantifier that will only ever match the empty string.}}
-    m.find();
-
-    RegExUtils.removeAll(str, ".*?"); // Noncompliant {{Fix this reluctant quantifier that will only ever match the empty string.}}
-    Pattern p = Pattern.compile(".*?"); // Noncompliant {{Fix this reluctant quantifier that will only ever match the empty string.}}
-    RegExUtils.removeAll(str, p);
+    // Followed explicitly by end anchor ("$")
+    Pattern.compile(".*?$").matcher(str).matches(); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    Pattern.compile(".*?()$").matcher(str).matches(); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    // The match type does not change anything
+    Pattern.compile(".*?$").matcher(str).find(); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    Pattern.compile(".*?()$").matcher(str).find(); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    // Even when the match type is both or unknown, the reluctant quantifier is still useless if we have an explicit "$"
+    Matcher mBoth = Pattern.compile(".*?$").matcher(str); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    mBoth.find();
+    mBoth.matches();
+    Pattern.compile(".*?$").matcher(str); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+    Pattern.compile(".*?()$").matcher(str); // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
   }
+  // Full match
+  @Email(regexp = ".*?") // Noncompliant {{Remove the '?' from this unnecessarily reluctant quantifier.}}
+  void fullMatch() { }
 
   Matcher compliant(String str) {
     str.matches(".*?x");
@@ -41,7 +64,6 @@ public class ReluctantQuantifierWithEmptyContinuationCheck {
     Pattern.compile(".*?"); // Compliant because unused
 
     Matcher m2 = Pattern.compile(".*?").matcher(str); // Compliant because the matcher is returned so we may be using it for a full match later
-    m.find();
     return m2;
   }
 
