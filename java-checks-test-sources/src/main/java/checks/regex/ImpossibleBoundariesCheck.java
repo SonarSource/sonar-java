c01814e26ba06edfb80b9b75af9f2e4660a8050b
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-05-05 15:55:11 +0200

    SONARJAVA-4163 Refactor S5996 using sonar-analyzer-commons (#4091)

--- java-checks-test-sources/src/main/java/checks/regex/ImpossibleBoundariesCheck.java
+++ java-checks-test-sources/src/main/java/checks/regex/ImpossibleBoundariesCheck.java
@@ -28,8 +28,6 @@ public class ImpossibleBoundariesCheck {
     str.matches("abc(?<=abc$)def"); // Noncompliant [[sc=28;ec=29]]
     str.matches("abc(?<!abc$)def"); // Noncompliant [[sc=28;ec=29]]
     str.matches("(?:abc(X|^))*Y?"); // Noncompliant [[sc=27;ec=28]]
-    // FP we raise an issue below because the usage of $ in the middle of a regex is suspicious and redundant
-    str.matches("(?m)^1$\n2"); // Noncompliant
   }
   
   void probablyNonCompliant(String str) {
@@ -85,6 +83,18 @@ public class ImpossibleBoundariesCheck {
     Pattern.compile("^(0\\d{2}-\\d{8}(-\\d{1,4})?)|(0\\d{3}-\\d{7,8}(-\\d{1,4})?)$");
     Pattern.compile("^(((13[0-9])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8})|(0\\d{2}-\\d{8})|(0\\d{3}-\\d{7})$");
     Pattern.compile("^((?<major>[0-9]{1,9})?(\\.(?<minor>[0-9]{1,9})(\\.$|[.-](?<micro>[0-9]{1,9}))?)?)([.-]?(?<qualifier>.+?))??([.-]redhat-(?<suffixversion>[0-9]{1,9}))?$");
+  }
+
+  void withMultilineFlag() {
+    Pattern.compile("a\\n^b$", Pattern.MULTILINE); // Compliant, matches "a\nb"
+    Pattern.compile("(?m)a\\n^b$"); // Compliant
+    Pattern.compile("a\\n^b$"); // Noncompliant
+
+    // Even if the usage of "$" is suspicious and redundant, we do not raise an issue
+    Pattern.compile("a$\nb", Pattern.MULTILINE); // Compliant, matches "a\nb"
+    Pattern.compile("(?m)a$\nb"); // Compliant
+    Pattern.compile("(?m)^1$\n2"); // Compliant
+    Pattern.compile("a$\nb"); // Noncompliant
   }
 
 }
--- java-checks/src/main/java/org/sonar/java/checks/regex/ImpossibleBoundariesCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/regex/ImpossibleBoundariesCheck.java
@@ -19,137 +19,17 @@
  */
 package org.sonar.java.checks.regex;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.sonar.check.Rule;
-import org.sonar.java.checks.helpers.RegexReachabilityChecker;
-import org.sonar.java.checks.helpers.RegexTreeHelper;
-import org.sonarsource.analyzer.commons.regex.RegexParseResult;
-import org.sonarsource.analyzer.commons.regex.ast.AutomatonState;
-import org.sonarsource.analyzer.commons.regex.ast.BoundaryTree;
-import org.sonarsource.analyzer.commons.regex.ast.DisjunctionTree;
-import org.sonarsource.analyzer.commons.regex.ast.LookAroundTree;
-import org.sonarsource.analyzer.commons.regex.ast.RegexBaseVisitor;
-import org.sonarsource.analyzer.commons.regex.ast.RegexTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
-
-import static org.sonarsource.analyzer.commons.regex.ast.AutomatonState.TransitionType.CHARACTER;
+import org.sonarsource.analyzer.commons.regex.RegexParseResult;
+import org.sonarsource.analyzer.commons.regex.finders.ImpossibleBoundaryFinder;
 
 @Rule(key = "S5996")
 public class ImpossibleBoundariesCheck extends AbstractRegexCheck {
 
-  private static final String MESSAGE = "Remove or replace this boundary that will never match because it appears %s mandatory input.";
-  private static final String SOFT_MESSAGE =
-    "Remove or replace this boundary that can only match if the previous part matched the empty string because it appears %s mandatory input.";
-  private final Set<RegexTree> excluded = new HashSet<>();
-  private final RegexReachabilityChecker regexReachabilityChecker = new RegexReachabilityChecker(false);
-
   @Override
   public void checkRegex(RegexParseResult regexForLiterals, ExpressionTree methodInvocationOrAnnotation) {
-    new ImpossibleBoundaryFinder().visit(regexForLiterals);
+    new ImpossibleBoundaryFinder(this::reportIssueFromCommons).visit(regexForLiterals);
   }
 
-  private class ImpossibleBoundaryFinder extends RegexBaseVisitor {
-
-    private AutomatonState start;
-    private AutomatonState end;
-
-    @Override
-    public void visit(RegexParseResult regexParseResult) {
-      regexReachabilityChecker.clearCache();
-      start = regexParseResult.getStartState();
-      end = regexParseResult.getFinalState();
-      super.visit(regexParseResult);
-    }
-
-    @Override
-    public void visitLookAround(LookAroundTree tree) {
-      // Inside a lookaround we consider the end/start of the lookahead/behind respectively as if it were the end/start
-      // of the regex. This avoids false positives for cases like `(?=.*$)foo` or `foo(?<=^...)`.
-      if (tree.getDirection() == LookAroundTree.Direction.BEHIND) {
-        AutomatonState oldStart = start;
-        start = tree.getElement();
-        super.visitLookAround(tree);
-        start = oldStart;
-      } else {
-        AutomatonState oldEnd = end;
-        // Set end to the lookaround's end-of-lookaround state
-        end = tree.getElement().continuation();
-        super.visitLookAround(tree);
-        end = oldEnd;
-      }
-    }
-
-    @Override
-    public void visitDisjunction(DisjunctionTree tree) {
-      BoundaryInDisjunctionFinder boundaryInDisjunctionFinder = new BoundaryInDisjunctionFinder();
-      boundaryInDisjunctionFinder.visit(tree);
-      excluded.addAll(boundaryInDisjunctionFinder.foundBoundaries());
-      super.visitDisjunction(tree);
-    }
-
-    @Override
-    public void visitBoundary(BoundaryTree boundaryTree) {
-      switch (boundaryTree.type()) {
-        case LINE_START:
-        case INPUT_START:
-          if (!RegexTreeHelper.canReachWithoutConsumingInput(start, boundaryTree)) {
-            reportIssue(boundaryTree, String.format(MESSAGE, "after"), null, Collections.emptyList());
-          } else if (!excluded.contains(boundaryTree) && probablyShouldConsumeInput(start, boundaryTree)) {
-            reportIssue(boundaryTree, String.format(SOFT_MESSAGE, "after"), null, Collections.emptyList());
-          }
-          break;
-        case LINE_END:
-        case INPUT_END:
-        case INPUT_END_FINAL_TERMINATOR:
-          if (!RegexTreeHelper.canReachWithoutConsumingInput(boundaryTree, end)) {
-            reportIssue(boundaryTree, String.format(MESSAGE, "before"), null, Collections.emptyList());
-          } else if (!excluded.contains(boundaryTree) && probablyShouldConsumeInput(boundaryTree, end)) {
-            reportIssue(boundaryTree, String.format(SOFT_MESSAGE, "before"), null, Collections.emptyList());
-          }
-          break;
-        default:
-          // Do nothing
-      }
-    }
-
-    private boolean probablyShouldConsumeInput(AutomatonState start, AutomatonState goal) {
-      return  canReachWithConsumingInput(start, goal, new HashSet<>());
-    }
-  }
-
-  private static class BoundaryInDisjunctionFinder extends RegexBaseVisitor {
-    private final Set<BoundaryTree> foundBoundaries = new HashSet<>();
-
-    @Override
-    public void visitBoundary(BoundaryTree boundaryTree) {
-      foundBoundaries.add(boundaryTree);
-    }
-
-    public Set<BoundaryTree> foundBoundaries() {
-      return new HashSet<>(foundBoundaries);
-    }
-  }
-
-  private boolean canReachWithConsumingInput(AutomatonState start, AutomatonState goal, Set<AutomatonState> visited) {
-    if (start == goal || visited.contains(start)) {
-      return false;
-    }
-    visited.add(start);
-
-    if (start instanceof LookAroundTree) {
-      return canReachWithConsumingInput(start.continuation(), goal, visited);
-    }
-
-    for (AutomatonState successor : start.successors()) {
-      AutomatonState.TransitionType transition = successor.incomingTransitionType();
-      if (((transition == CHARACTER) && regexReachabilityChecker.canReach(successor, goal))
-        || ((transition != CHARACTER) && canReachWithConsumingInput(successor, goal, visited))) {
-        return true;
-      }
-    }
-    return false;
-  }
 }
