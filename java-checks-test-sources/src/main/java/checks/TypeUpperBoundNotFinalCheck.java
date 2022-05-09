f41faebf2fb5aed991e994d3e6794968d34dafd2
chrislain-razafimahefa-sonarsource <102787590+chrislain-razafimahefa-sonarsource@users.noreply.github.com>
2022-05-09 18:24:17 +0200

    Remove unused import

--- java-checks-test-sources/src/main/java/checks/TypeUpperBoundNotFinalCheck.java
+++ java-checks-test-sources/src/main/java/checks/TypeUpperBoundNotFinalCheck.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
-import org.rapidoid.collection.Coll;
 
 public class TypeUpperBoundNotFinalCheck {
   public static class NonExtendableTypeParam<T extends FinalClass> { } // Noncompliant {{Replace this type parametrization by the 'final' type `FinalClass`}}
