cbe774f6c2e5cd6731950337e4352973d31d9733
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-05-10 17:26:37 +0200

    SONARJAVA-4165 Refactor S6002 using sonar-analyzer-commons (#4096)

--- java-checks/src/test/java/org/sonar/java/checks/regex/RegexLookaheadCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/regex/RegexLookaheadCheckTest.java
@@ -22,14 +22,14 @@ package org.sonar.java.checks.regex;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-import static org.sonar.java.checks.verifier.TestUtils.testSourcesPath;
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
 
 class RegexLookaheadCheckTest {
 
   @Test
   void test() {
     CheckVerifier.newVerifier()
-      .onFile(testSourcesPath("checks/regex/RegexLookaheadCheck.java"))
+      .onFile(mainCodeSourcesPath("checks/regex/RegexLookaheadCheck.java"))
       .withCheck(new RegexLookaheadCheck())
       .verifyIssues();
   }
