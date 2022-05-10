adb813625e00f68fef959e6883e3cfe1e574ff26
chrislain-razafimahefa-sonarsource <102787590+chrislain-razafimahefa-sonarsource@users.noreply.github.com>
2022-05-10 15:16:05 +0200

    SONARJAVA-2940 S4968: Upper bound of type variable should not be final (#4088)

---
+++ java-checks-test-sources/src/main/java/checks/TypeUpperBoundNotFinalCheck.java
@@ -0,0 +1,98 @@
+package checks;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.annotation.Nullable;
+
+public class TypeUpperBoundNotFinalCheck {
+  public static class NonExtendableTypeParam<T extends FinalClass> { } // Noncompliant {{Replace this type parametrization by the 'final' type `FinalClass`}}
+
+  public static <T extends FinalClass> void methodTypeParameter() { } // Noncompliant
+
+  public static void nonExtendableWildcard(Collection<? extends FinalClass> c){ } // Noncompliant
+
+  public static <T extends NonFinalClass<? extends FinalClass>> void complexTypeParameter() { } // Noncompliant
+
+  public static class Variables {
+    private TwoParams<? extends FinalClass, String> complexVarParams = null; // Noncompliant
+    private FinalClass<? extends NonFinalClass> finalVar = null; // Compliant
+
+    public static void methodTypeParameter() {
+      Collection<? extends FinalClass> variableDeclInMethod = null; // Noncompliant
+      TwoParams<String, ? extends FinalClass> complexVarParams = null; // Noncompliant
+    }
+  }
+
+  static abstract class AbstractClass {
+    public abstract <T extends FinalClass> Set<T> overriddenWithTypeParam(); // Noncompliant
+    public abstract Set<String> overriddenWithArgument(Collection<? extends FinalClass> v); // Noncompliant
+  }
+
+  public static void boundedWildcard(Collection<? extends FinalClass> c) { } // Noncompliant
+
+  public static class FinalAndNonFinalBounds<T extends FinalClass & Comparable> { } // Noncompliant [[sc=46;ec=79]]
+
+  @Nullable
+  public static <T extends FinalClass & Comparable & Serializable> T multipleBounds() { // Noncompliant
+    return null;
+  }
+
+  public static <T extends FinalClass<T>> void finalParameterizedBound() { } // Noncompliant [[sc=18;ec=41]]
+
+  public static <T extends NonFinalClass<? extends FinalClass>> void finalInnerBound() { } // Noncompliant [[sc=42;ec=62]]
+
+  public static <T extends TwoParams<? extends NonFinalClass, ? extends FinalClass>> void complexFinalInnerBound() { } // Noncompliant [[sc=63;ec=83]]
+
+  public static <T extends NonFinalClass, B extends FinalClass> void multipleTypeParams() { } // Noncompliant [[sc=43;ec=63]]
+
+  public static NonFinalClass<? extends FinalClass<T>> methodReturn() { return null; } // Noncompliant [[sc=31;ec=54]]
+
+  public static final class ImmutableClass<B> {
+    public <T extends B> NonFinalClass<B> extendsClass(Map<? extends Class<? extends T>, ? extends T> map) { return null; } // Noncompliant [[sc=60;ec=88]]
+  }
+
+
+
+  public static class Extendable<T extends Object> { // Compliant
+    private FinalClass fc = null;
+    private Extendable() { fc = new FinalClass(); }
+  }
+
+  public static class OverridingClass extends AbstractClass {
+    @Override
+    public <T extends FinalClass> Set<T> overriddenWithTypeParam() {return new HashSet<>();} // Compliant because overridden methods signature might not be modifiable by user
+    @Override
+    public Set<String> overriddenWithArgument(Collection<? extends FinalClass> v) { // Compliant
+      TwoParams<? extends FinalClass, String> complexVarParams = null; // Noncompliant
+      return new HashSet<>();
+    };
+  }
+
+  public static class MultipleBounds<T extends Object & Comparable> { }
+
+  public static void extendableWildcard(Collection<? extends Object> c) { }
+
+  public static void superTypes(List<Comparable<? super T>> list) { }
+
+  public static <T extends Comparable<T>> void complexBound() { }
+
+  public static <T extends Comparable<? super T>> void extendsAndSuper(List<T> list) { }
+
+  public static void unboundedWildcard(Collection<?> c) { }
+
+  public static FinalClass returnFinal() { return null; }
+
+  public static FinalClass<? extends NonFinalClass<T>> finalMethodReturn() { return null; }
+
+  public static NonFinalClass<? extends FinalClass[]> arrayBound() { return null; }
+
+
+
+  public static class TwoParams<T, B> { }
+  static class NonFinalClass<T> { }
+  final static class FinalClass<T> { }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/TypeUpperBoundNotFinalCheck.java
@@ -0,0 +1,102 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.ParameterizedTypeTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.TypeParameterTree;
+import org.sonar.plugins.java.api.tree.TypeTree;
+import org.sonar.plugins.java.api.tree.WildcardTree;
+
+@Rule(key = "S4968")
+public class TypeUpperBoundNotFinalCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return Arrays.asList(Tree.Kind.TYPE_PARAMETER, Tree.Kind.EXTENDS_WILDCARD);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    if (tree.is(Tree.Kind.TYPE_PARAMETER)) {
+      handleBounds(((TypeParameterTree) tree).bounds(), tree);
+    } else if (tree.is(Tree.Kind.EXTENDS_WILDCARD)) {
+      handleBounds(Collections.singletonList(((WildcardTree) tree).bound()), tree);
+    }
+  }
+
+  private void handleBounds(List<TypeTree> bounds, Tree treeToReport) {
+    bounds.stream()
+      .map(TypeUpperBoundNotFinalCheck::getIdentifier)
+      .flatMap(Optional::stream)
+      .filter(bound -> isFinal(bound) && !inOverridingMethodDeclaration(treeToReport))
+      .findFirst()
+      .ifPresent(identifierTree -> reportIssue(treeToReport, "Replace this type parametrization by the 'final' type `" + identifierTree.name() + "`"));
+  }
+
+  private static Optional<IdentifierTree> getIdentifier(TypeTree t) {
+    if (t.is(Tree.Kind.IDENTIFIER)) {
+      return Optional.of((IdentifierTree) t);
+    } else if (t.is(Tree.Kind.PARAMETERIZED_TYPE)) {
+      ParameterizedTypeTree type = (ParameterizedTypeTree) t;
+      return getIdentifier(type.type());
+    }
+
+    return Optional.empty();
+  }
+
+  private static boolean isFinal(IdentifierTree bound) {
+    return bound.symbol().isFinal();
+  }
+
+  /**
+   * Returns true if 'type' is part of the signature of an overriding method.
+   */
+  private static boolean inOverridingMethodDeclaration(Tree type) {
+    return getMethodDeclaration(type).filter(TypeUpperBoundNotFinalCheck::isOverriding).isPresent();
+  }
+
+  /**
+   * Returns the method where 'type' appears in its signature, if any.
+   */
+  private static Optional<MethodTree> getMethodDeclaration(Tree type) {
+    Tree parent = type.parent();
+    while (parent != null && !parent.is(Tree.Kind.BLOCK)) {
+      if (parent.is(Tree.Kind.METHOD)) {
+        return Optional.of((MethodTree) parent);
+      }
+      parent = parent.parent();
+    }
+    return Optional.empty();
+  }
+
+  private static boolean isOverriding(MethodTree method) {
+    return !Boolean.FALSE.equals(method.isOverriding());
+  }
+}
