db6a5a98430d1db3a7f38201b0decdd934151857
chrislain-razafimahefa-sonarsource <102787590+chrislain-razafimahefa-sonarsource@users.noreply.github.com>
2022-06-22 17:37:31 +0200

    Rename variable

--- java-checks/src/test/files/checks/aws/AwsRegionSetterCheck.java
+++ java-checks/src/test/files/checks/aws/AwsRegionSetterCheck.java
@@ -3,7 +3,7 @@ import com.amazonaws.regions.Regions;
 
 public class T {
   private static final String EU_WEST_1 = "EU_WEST_1";
-  private static final Regions REGION_EU_WEST_1 = Regions.EU_WEST_1;
+  private static final Regions ENUM_EU_WEST_1 = Regions.EU_WEST_1;
 
   void nonCompliant() {
     AmazonS3ClientBuilder.standard().withRegion("eu_west_1").build(); // Noncompliant [[sc=49;ec=60]]
@@ -12,6 +12,6 @@ public class T {
 
   void compliant() {
     AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build(); // Compliant
-    AmazonS3ClientBuilder.standard().withRegion(REGION_EU_WEST_1).build(); // Compliant
+    AmazonS3ClientBuilder.standard().withRegion(ENUM_EU_WEST_1).build(); // Compliant
   }
 }
