b02d62247dff0b9a659f88f159c97c5d867dbcfe
chrislain-razafimahefa-sonarsource <102787590+chrislain-razafimahefa-sonarsource@users.noreply.github.com>
2022-06-22 11:16:18 +0200

    Add tests.

--- java-checks/src/test/files/checks/aws/AWSRegionSetterCheck.java
+++ java-checks/src/test/files/checks/aws/AWSRegionSetterCheck.java
@@ -1,9 +1,12 @@
 public class T {
-    void nonCompliant() {
-        AmazonS3ClientBuilder.standard().withRegion("eu_west_1").build(); // Noncompliant
-    }
+  private static final String EU_WEST_1 = "EU_WEST_1";
 
-    void compliant() {
-        AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build(); // Compliant
-    }
+  void nonCompliant() {
+    AmazonS3ClientBuilder.standard().withRegion("eu_west_1").build(); // Noncompliant
+    AmazonS3ClientBuilder.standard().withRegion(EU_WEST_1).build(); // Noncompliant
+  }
+
+  void compliant() {
+    AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build(); // Compliant
+  }
 }
