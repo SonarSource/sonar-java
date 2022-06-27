fee1c7ab28eab16dab542429f982003d7d6bb6bf
chrislain-razafimahefa-sonarsource <102787590+chrislain-razafimahefa-sonarsource@users.noreply.github.com>
2022-06-27 10:54:28 +0200

    Remove unused file

--- java-checks-test-sources/src/main/java/checks/aws/externalpackage/ExternalClass.java
+++
@@ -1,9 +0,0 @@
-package checks.aws.externalpackage;
-
-public class ExternalClass {
-  public static final String US_EAST_2 = "US_EAST_2";
-
-  public String getRegion() {
-    return "EU_WEST_1";
-  }
-}
