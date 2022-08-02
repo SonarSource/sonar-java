6e14ac63e7c99e12591885a9af371dbf327072d6
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-08-02 17:11:48 +0200

    SONARJAVA-4275 Add failing compliant code examples

--- java-checks-test-sources/src/main/java/checks/security/CredentialsShouldNotBeHardcodedCheck.java
+++ java-checks-test-sources/src/main/java/checks/security/CredentialsShouldNotBeHardcodedCheck.java
@@ -1,11 +1,37 @@
 package checks.security;
 
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 import org.h2.security.SHA256;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 public class CredentialsShouldNotBeHardcodedCheck {
-  void nonCompliant(byte[] message) {
+  public static void nonCompliant(byte[] message) {
     String inputString = "s3cr37";
     byte[] key = inputString.getBytes();
     SHA256.getHMAC(key, message);  // Noncompliant
+  }
+
+  public static void compliantAzure(SecretClient secretClient, String secretName, byte[] message) {
+    KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
+    String secret = retrievedSecret.getValue();
+
+    byte[] key = secret.getBytes();
+    SHA256.getHMAC(key, message);
+  }
+
+  public static void compliantAws(SecretsManagerClient secretsClient, String secretName, byte[] message) {
+    GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
+      .secretId(secretName)
+      .build();
+
+    GetSecretValueResponse valueResponse = secretsClient.getSecretValue(valueRequest);
+    String secret                        = valueResponse.secretString();
+
+    byte[] key = secret.getBytes();
+    SHA256.getHMAC(key, message);
   }
 }
