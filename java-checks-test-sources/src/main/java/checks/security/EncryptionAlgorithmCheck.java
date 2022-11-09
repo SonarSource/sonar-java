a01900fcb5cca13061855151f4f944480f630e5c
Irina Batinic <117161143+irina-batinic-sonarsource@users.noreply.github.com>
2022-11-09 17:45:36 +0100

    SONARJAVA-4268 Rule S5542: Detect CBC mode when used with padding (#4213)

--- java-checks-test-sources/src/main/java/checks/security/EncryptionAlgorithmCheck.java
+++ java-checks-test-sources/src/main/java/checks/security/EncryptionAlgorithmCheck.java
@@ -13,39 +13,39 @@ abstract class EncryptionAlgorithmCheck {
   public void foo(java.util.Properties props) {
     /*
     should complain:
-    - everytime ECB mode is used whatever the encryption algorithm
+    - everytime ECB mode is used
        - By default without specifying operation mode ECB is chosen
-    - when CBC mode is used with PKCS5Padding or PKCS7Padding
+    - when CBC mode is used with padding different from "NoPadding"
     - when RSA is used without OAEPWithSHA-1AndMGF1Padding or OAEPWITHSHA-256ANDMGF1PADDING padding scheme
     */
 
     try {
       // First case
-      Cipher.getInstance("AES"); // Noncompliant [[sc=26;ec=31]] {{Use secure mode and padding scheme.}}
+      Cipher.getInstance("AES"); // Noncompliant [[sc=26;ec=31]] {{Use a secure padding scheme.}}
 
-      Cipher.getInstance("AES/ECB/NoPadding"); // Noncompliant
+      Cipher.getInstance("AES/ECB/NoPadding"); // Noncompliant {{Use a secure cipher mode.}}
       Cipher.getInstance("AES" + "/ECB/NoPadding"); // Noncompliant
       Cipher.getInstance("AES/ECB/NoPadding", getProvider()); // Noncompliant
       Cipher.getInstance("AES/ECB/NoPadding", "someProvider"); // Noncompliant
 
-      Cipher.getInstance("Blowfish/ECB/PKCS5Padding"); // Noncompliant
+      Cipher.getInstance("Blowfish/ECB/PKCS5Padding"); // Noncompliant {{Use a secure cipher mode.}}
       Cipher.getInstance("DES/ECB/PKCS5Padding"); // Noncompliant
 
       Cipher.getInstance("AES/GCM/NoPadding"); // Compliant
 
       // Second case
-      Cipher.getInstance("AES/CBC/PKCS5Padding"); // Compliant - CBC considered as safe
-      Cipher.getInstance("Blowfish/CBC/PKCS5Padding"); // Compliant - CBC considered as safe
-      Cipher.getInstance("DES/CBC/PKCS5Padding"); // Compliant - CBC considered as safe
-      Cipher.getInstance("AES/CBC/PKCS7Padding"); // Compliant - CBC considered as safe
-      Cipher.getInstance("Blowfish/CBC/PKCS7Padding"); // Compliant - CBC considered as safe
-      Cipher.getInstance("DES/CBC/PKCS7Padding"); // Compliant - CBC considered as safe
-      Cipher.getInstance("DES/CBC/NoPadding"); // Compliant - CBC considered as safe
+      Cipher.getInstance("AES/CBC/PKCS5Padding"); // Noncompliant {{Use another cipher mode or disable padding.}}
+      Cipher.getInstance("Blowfish/CBC/PKCS5Padding"); // Noncompliant
+      Cipher.getInstance("DES/CBC/PKCS5Padding"); // Noncompliant
+      Cipher.getInstance("AES/CBC/PKCS7Padding"); // Noncompliant
+      Cipher.getInstance("Blowfish/CBC/PKCS7Padding"); // Noncompliant
+      Cipher.getInstance("DES/CBC/PKCS7Padding"); // Noncompliant
+      Cipher.getInstance("DES/CBC/NoPadding"); // Compliant
       Cipher.getInstance("AES/GCM/NoPadding"); // Compliant
       Cipher.getInstance("Blowfish/GCM/NoPadding"); // Compliant
 
       // Third case
-      Cipher.getInstance("RSA/NONE/NoPadding"); // Noncompliant
+      Cipher.getInstance("RSA/NONE/NoPadding"); // Noncompliant {{Use a secure padding scheme.}}
       Cipher.getInstance("RSA/GCM/NoPadding"); // Noncompliant
       Cipher.getInstance("RSA/ECB/NoPadding"); // Noncompliant
 
@@ -78,6 +78,7 @@ abstract class EncryptionAlgorithmCheck {
       Cipher.getInstance("DES/CBC/NOPADDING"); // Compliant
       Cipher.getInstance("RSA/NONE/OAEPWITHSHA-1AndMGF1Padding"); // Compliant
       String algoUpperCase = props.getProperty("myAlgo", "AES/ECB/PKCS5PADDING");
+      Cipher.getInstance(algoUpperCase); // Noncompliant [[sc=26;ec=39;secondary=80]]
 
     } catch (Exception  e) {
     }
