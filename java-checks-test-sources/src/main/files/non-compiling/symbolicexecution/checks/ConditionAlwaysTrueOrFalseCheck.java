a411b54ce941fd365735864ce5073918fd656670
Michael Gumowski <michael.gumowski@sonarsource.com>
2022-12-06 11:38:27 +0100

    SONARJAVA-4368 Workaround CFG and SE to support patterns

--- java-checks-test-sources/src/main/files/non-compiling/symbolicexecution/checks/ConditionAlwaysTrueOrFalseCheck.java
+++ java-checks-test-sources/src/main/files/non-compiling/symbolicexecution/checks/ConditionAlwaysTrueOrFalseCheck.java
@@ -250,21 +250,6 @@ class Class extends SuperClass {
     }
   }
 
-  public void instanceOfPatternMathing() {
-    Object object = new Object();
-    // Java 16 pattern matching instance of
-    if (object instanceof String s) { // Compliant
-    } else if (object instanceof Integer i) { // Compliant
-    }
-
-    if (object instanceof String str) {
-      // str inherits the contraints from objects
-      if (str == null) { // Noncompliant {{Change this condition so that it does not always evaluate to "false"}}
-
-      }
-    }
-  }
-
   public void literals() {
     // literals
     if (!true) { // Noncompliant {{Change this condition so that it does not always evaluate to "false"}}
