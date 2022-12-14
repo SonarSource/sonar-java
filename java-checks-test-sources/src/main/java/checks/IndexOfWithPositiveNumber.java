580298bb5caf867d279243d83b895051b04b7dfd
Irina Batinic <117161143+irina-batinic-sonarsource@users.noreply.github.com>
2022-12-14 17:50:36 +0100

    SONARJAVA-4250 Fix FN in S2692 when the number is coming from a constant (#4242)

--- java-checks-test-sources/src/main/java/checks/IndexOfWithPositiveNumber.java
+++ java-checks-test-sources/src/main/java/checks/IndexOfWithPositiveNumber.java
@@ -3,8 +3,38 @@ package checks;
 import java.util.List;
 
 class IndexOfWithPositiveNumber {
-  void method(int length, List<String> strings) {
 
+  public static final int literal1 = 0;
+
+  void method(int length, List<String> strings, int literal3) {
+    if ("".indexOf(" ") > literal3) { // Compliant
+    }
+    if (literal3 > "".indexOf(" ")) { // Compliant
+    }
+    int literal4 = 2;
+    literal4 = 0;
+    if ("".indexOf(" ") > literal4) { // Noncompliant
+    }
+    if (literal4 < "".indexOf(" ")) { // Noncompliant
+    }
+
+    if ("".indexOf(' ') > literal1) { // Noncompliant
+    }
+    if (literal1 < "".indexOf(' ')) { // Noncompliant
+    }
+    final int literal2 = 0;
+    if ("".indexOf(" ") > literal2) { // Noncompliant
+    }
+    if (literal2 < "".indexOf(" ")) { // Noncompliant
+    }
+    if ("".indexOf(" ") > getLiteral0()) { // Compliant - do not check
+    }
+    if (getLiteral0() < "".indexOf(" ")) { // Compliant - do not check
+    }
+    if ("".indexOf(" ") > getLiteral()) { // Compliant - do not check
+    }
+    if (getLiteral() < "".indexOf(" ")) { // Compliant - do not check
+    }
     if (length > 0) { // Compliant
     }
     if (length < length) { // Compliant
@@ -43,4 +73,13 @@ class IndexOfWithPositiveNumber {
     if (2 <= "".indexOf("")) { // Compliant
     }
   }
+
+  private int getLiteral() {
+    return 2;
+  }
+
+  private int getLiteral0() {
+    return 0;
+  }
+
 }
