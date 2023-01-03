e4d783d59f83c5fb60b88d18bd41d49f2d603597
Michael Gumowski <michael.gumowski@sonarsource.com>
2023-01-03 12:43:31 +0100

    SONARJAVA-4331 Add test case to make sure S1213 is not impacted anymore

--- java-checks-test-sources/src/main/java/checks/IncorrectOrderOfMembersCheck.java
+++ java-checks-test-sources/src/main/java/checks/IncorrectOrderOfMembersCheck.java
@@ -40,6 +40,24 @@ class IncorrectOrderOfMembersCheck {
     boolean locked;
     public int current = counter;
   }
+
+  class OneLiner { void foo() {System.out.println("");} }
+
+  record Output(String title, String summary, String text) {
+    public static final String TRUNCATE_MESSAGE = "abc"; // compliant
+    static final int GREAT_VALUE = 42; // compliant
+    private static final String DEFAULT = "xyz"; // compliant
+
+    public boolean isTooLong() {
+      return title.length() + summary.length() + text.length() > 1000;
+    }
+
+    public static final int DEFAULT_NUMBER = 0; // Noncompliant
+
+    Output(String s) { // Noncompliant
+      this(s, s, s);
+    }
+  }
 }
 
 
