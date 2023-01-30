62eba28dd539044bf9c321ee70562a457ca35780
Dorian Burihabwa <75226315+dorian-burihabwa-sonarsource@users.noreply.github.com>
2023-01-30 15:35:21 +0100

    SONARJAVA-4315 Add missing closing parenthesis to quick fix (#4289)

--- java-checks-test-sources/src/main/java/checks/CollapsibleIfCandidateCheck.java
+++ java-checks-test-sources/src/main/java/checks/CollapsibleIfCandidateCheck.java
@@ -1,13 +1,18 @@
 package checks;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class CollapsibleIfCandidateCheck {
+  private static final Logger LOGGER = Logger.getLogger(CollapsibleIfCandidateCheck.class.getCanonicalName());
   void testMyFile(File file) {
     if (file != null) {
       if (file.isFile() || file.isDirectory()) { // Noncompliant [[sc=7;ec=9;quickfixes=qf1]]
-        /* ... */
+        LOGGER.log(Level.INFO, file.getAbsolutePath());
       }
       // fix@qf1 {{Merge this if statement with the enclosing one}}
       // edit@qf1 [[sl=+2;el=+2;sc=7;ec=8]] {{}}
+      // edit@qf1 [[sc=47;ec=47]] {{)}}
       // edit@qf1 [[sl=-1;el=+0;sc=21;ec=9]] {{ && }}
     }
   }
@@ -15,17 +20,19 @@ public class CollapsibleIfCandidateCheck {
   void noBraceOnOuter(File file) {
     if (file != null)
       if (file.isFile() || file.isDirectory()) { // Noncompliant [[sc=7;ec=9;quickfixes=qf2]]
-        /* ... */
+        LOGGER.log(Level.INFO, file.getAbsolutePath());
       }
       // fix@qf2 {{Merge this if statement with the enclosing one}}
+      // edit@qf2 [[sc=47;ec=47]] {{)}}
       // edit@qf2 [[sl=-1;el=+0;sc=21;ec=9]] {{ && }}
   }
 
   void noBraceOnInner(File file) {
     if (file != null) {
-      if (file.isFile() || file.isDirectory()) System.out.println("Good enough"); // Noncompliant [[sc=7;ec=9;quickfixes=qf3]]
+      if (file.isFile() || file.isDirectory()) LOGGER.log(Level.INFO, file.getAbsolutePath()); // Noncompliant [[sc=7;ec=9;quickfixes=qf3]]
       // fix@qf3 {{Merge this if statement with the enclosing one}}
       // edit@qf3 [[sc=48;ec=48]] {{{}}
+      // edit@qf3 [[sc=47;ec=47]] {{)}}
       // edit@qf3 [[sl=-1;el=+0;sc=21;ec=9]] {{ && }}
     }
   }
