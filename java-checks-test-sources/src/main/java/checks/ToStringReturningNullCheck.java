9e32d88279fc7469a5a73dfd3cedcde8935b1f39
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2023-02-01 15:31:17 +0100

    Add test cases

--- java-checks-test-sources/src/main/java/checks/ToStringReturningNullCheck.java
+++ java-checks-test-sources/src/main/java/checks/ToStringReturningNullCheck.java
@@ -26,21 +26,32 @@ class ToStringReturningNullCheckC {
 
 class ToStringReturningNullCheckD {
   protected Object clone() {
-    return null; // Noncompliant [[sc=12;ec=16]] {{Return a non null object.}}
+    return null; // Noncompliant [[sc=12;ec=16;quickfixes=!]] {{Return a non null object.}}
   }
 }
 
-class QuickFixes{
+class QuickFixesA{
   public String toString() {
     return (null); // Noncompliant [[sc=13;ec=17;quickfixes=qf1]]
     // fix@qf1 {{Replace null with an empty string}}
     // edit@qf1 [[sc=12;ec=18]] {{""}}
   }
 }
-class QuickFixes2{
+class QuickFixesB{
   public String toString() {
     return null; // Noncompliant [[sc=12;ec=16;quickfixes=qf2]]
     // fix@qf2 {{Replace null with an empty string}}
     // edit@qf2 [[sc=12;ec=16]] {{""}}
   }
+}
+class QuickFixesC{
+  public String toString() {
+    if(someCondition()) {
+      return null; // Noncompliant [[sc=14;ec=18;quickfixes=qf3]]
+      // fix@qf3 {{Replace null with an empty string}}
+      // edit@qf3 [[sc=14;ec=18]] {{""}}
+    }
+    return "";
+  }
+  boolean someCondition() {return false;}
 }
