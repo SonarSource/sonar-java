f4d303dfa2b10b5dfb08bad25417b44683ad4358
Alban Auzeill <alban.auzeill@sonarsource.com>
2023-02-02 06:21:50 +0100

    SONARJAVA-4185 Test S3740 with unknown class hierarchy and unknown parameter type

--- java-checks-test-sources/src/main/files/non-compiling/checks/RawTypeCheck.java
+++ java-checks-test-sources/src/main/files/non-compiling/checks/RawTypeCheck.java
@@ -1,9 +1,13 @@
 package checks;
 
 class RawTypeCheck {
-  class Child extends UnkownParent {
+  class KnownParent {
+    public void method(Comparable arg1, String arg2) {} // Noncompliant
+  }
+
+  class ChildWithKnownHierarchy extends KnownParent {
     @Override
-    public int compare(Comparable first, Comparable second) { // Compliant because overriding
+    public int compare(Comparable first, Comparable second) { // Compliant because of the "@Override" annotation
       checkNotNull(first);
       if (first == second) {
         return 0;
@@ -12,7 +16,19 @@ class RawTypeCheck {
       return second.compareTo(first);
     }
 
-    private void checkNotNull(Comparable left) { // Noncompliant [[sc=31;ec=41]]
+    public void checkNotNull(Comparable left) { // Noncompliant [[sc=30;ec=40]]
+    }
+
+    public void method(Comparable arg1, Unknown arg2) { // Compliant, unknown arg2 type, so may override
+    }
+  }
+
+  class ChildWithUnknownHierarchy extends UnkownParent {
+    public void checkNotNull(Comparable left) { // Compliant, !private and !static, so may override
+    }
+    private void privateCheckNotNull(Comparable left) { // Noncompliant
+    }
+    public static void staticCheckNotNull(Comparable left) { // Noncompliant
     }
   }
 }
