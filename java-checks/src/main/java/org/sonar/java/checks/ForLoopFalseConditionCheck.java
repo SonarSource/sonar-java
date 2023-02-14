77cdf191c283123e14f727ed2902552817da11f2
Alban Auzeill <alban.auzeill@sonarsource.com>
2023-02-14 10:37:30 +0100

    Update license headers

--- java-checks/src/main/java/org/sonar/java/checks/ForLoopFalseConditionCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/ForLoopFalseConditionCheck.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Java
- * Copyright (C) 2012-2022 SonarSource SA
+ * Copyright (C) 2012-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
