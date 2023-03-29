fdb0edb010927341a1ef06fd59bb5a7cb746d602
Johann Beleites <johann.beleites@sonarsource.com>
2023-03-29 09:23:10 +0200

    Initial implementation

---
+++ java-checks-test-sources/src/main/java/checks/SingletonUsageCheckSample.java
@@ -0,0 +1,113 @@
+package checks;
+
+import java.io.Serial;
+
+// TODO: check code snippet license
+public class SingletonUsageCheckSample {
+
+  public static class EagerInitializedSingleton { // Noncompliant [[sc=23;ec=48;secondary=+2]]
+
+    private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+    // private constructor to avoid client applications using the constructor
+    private EagerInitializedSingleton(){}
+
+    public static EagerInitializedSingleton getInstance() {
+      return instance;
+    }
+  }
+
+  public static class StaticBlockSingleton { // Noncompliant
+
+    private static StaticBlockSingleton instance;
+
+    private StaticBlockSingleton(){}
+
+    // static block initialization for exception handling
+    static {
+      try {
+        instance = new StaticBlockSingleton();
+      } catch (Exception e) {
+        throw new RuntimeException("Exception occurred in creating singleton instance");
+      }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+      return instance;
+    }
+  }
+
+  public static class LazyInitializedSingleton { // Noncompliant
+
+    private static LazyInitializedSingleton instance;
+
+    private LazyInitializedSingleton(){}
+
+    public static LazyInitializedSingleton getInstance() {
+      if (instance == null) {
+        instance = new LazyInitializedSingleton();
+      }
+      return instance;
+    }
+  }
+
+
+  public static class ThreadSafeSingleton { // Noncompliant
+
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton(){}
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+      if (instance == null) {
+        instance = new ThreadSafeSingleton();
+      }
+      return instance;
+    }
+
+  }
+
+  public static class BillPughSingleton { // Noncompliant
+
+    private BillPughSingleton(){}
+
+    private static class SingletonHelper {
+      private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+
+    public static BillPughSingleton getInstance() {
+      return SingletonHelper.INSTANCE;
+    }
+  }
+
+  public enum EnumSingleton { // Noncompliant
+
+    INSTANCE;
+
+    public static void doSomething() {
+      // do something
+    }
+  }
+
+  public static class SerializedSingleton implements java.io.Serializable { // Noncompliant
+
+    private static final long serialVersionUID = -7604766932017737115L;
+
+    private SerializedSingleton(){}
+
+    private static class SingletonHelper {
+      private static final SerializedSingleton instance = new SerializedSingleton();
+    }
+
+    public static SerializedSingleton getInstance() {
+      return SingletonHelper.instance;
+    }
+
+    @Serial
+    protected Object readResolve() {
+      return getInstance();
+    }
+
+  }
+
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/SingletonUsageCheck.java
@@ -0,0 +1,120 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.helpers.ExpressionsHelper;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
+
+@Rule(key = "S6548")
+public class SingletonUsageCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return Arrays.asList(Tree.Kind.CLASS, Tree.Kind.ENUM);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    final var classTree = (ClassTree) tree;
+    if (tree.is(Tree.Kind.CLASS)) {
+      visitClass(classTree);
+    } else {
+      visitEnum(classTree);
+    }
+  }
+
+  private void visitEnum(ClassTree classTree) {
+    var enumConstants = classTree.members().stream().filter(member -> member.is(Tree.Kind.ENUM_CONSTANT)).collect(Collectors.toList());
+    if (enumConstants.size() == 1) {
+      reportIssue(classTree.simpleName(), "Enum singleton pattern detected",
+        Collections.singletonList(new JavaFileScannerContext.Location("Single enum", enumConstants.get(0))), null);
+    }
+  }
+
+  private void visitClass(ClassTree classTree) {
+    ClassTree wrappingClass = null;
+    final var parent = classTree.parent();
+    if (parent != null && parent.is(Tree.Kind.CLASS)) {
+      wrappingClass = (ClassTree) parent;
+    }
+
+    VariableTree singletonField = null;
+    ClassTree singletonClass = null;
+    for (var member: classTree.members()) {
+      if (!(member.is(Tree.Kind.VARIABLE))) continue;
+
+      final var varTree = (VariableTree) member;
+      final var fieldSymbol = varTree.symbol();
+
+      if (!fieldSymbol.isStatic()) continue;
+
+      if (fieldSymbol.type().equals(classTree.symbol().type())) {
+        singletonClass = classTree;
+      } else if (wrappingClass != null && fieldSymbol.type().equals(wrappingClass.symbol().type())) {
+        singletonClass = wrappingClass;
+      } else {
+        continue;
+      }
+
+      if(isEffectivelyFinal(fieldSymbol)) {
+        if (singletonField != null) {
+          return;
+        } else {
+          singletonField = varTree;
+        }
+      }
+    }
+
+    if (singletonField == null) return;
+
+    var allConstructors = classTree.members().stream()
+      .filter(member -> member.is(Tree.Kind.CONSTRUCTOR))
+      .map(MethodTree.class::cast)
+      .collect(Collectors.toList());
+
+    if (allConstructors.size() <= 1 && allConstructors.stream().allMatch(constructor -> constructor.symbol().isPrivate())) {
+      var writeUsage = ExpressionsHelper.getSingleWriteUsage(singletonField.symbol());
+      var flows = new ArrayList<JavaFileScannerContext.Location>();
+      flows.add(new JavaFileScannerContext.Location("Singleton field", writeUsage));
+      if (singletonClass != classTree) {
+        flows.add(new JavaFileScannerContext.Location("Singleton helper", classTree.simpleName()));
+      }
+
+      reportIssue(singletonClass.simpleName(), "This looks like a singleton class", flows, null);
+    }
+  }
+
+  private static boolean isEffectivelyFinal(Symbol symbol) {
+    return symbol.isFinal() ||
+      (symbol.isPrivate() && ExpressionsHelper.getSingleWriteUsage(symbol) != null);
+  }
+}
