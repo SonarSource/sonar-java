24f4e5749738edae027de0f278ac4168dd28004d
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2023-04-05 15:00:41 +0200

    SONARJAVA-4440 Changes from feedback
    * Update issue message to match RSPEC
    * Ignore enums with a single constant built with one ore more parameters
    * Add private constructor as secondary location
    * Add assignments as secondaries
    * Refactoring
    * Raise when class contains static fields of non-relevant types
    * Improve coverage
    * Cover cases where instance members are all private
    * Cover cases where an enum has more than one constant
    * Cover cases where the single constructor is public and takes parameters

--- java-checks-test-sources/src/main/java/checks/SingletonUsageCheckSample.java
+++ java-checks-test-sources/src/main/java/checks/SingletonUsageCheckSample.java
@@ -5,7 +5,7 @@ import java.io.Serial;
 // TODO: check code snippet license
 public class SingletonUsageCheckSample {
 
-  public static class EagerInitializedSingleton { // Noncompliant [[sc=23;ec=48;secondary=+2]]
+  public static class EagerInitializedSingleton { // Noncompliant [[sc=23;ec=48;secondary=+2,+5]] {{A Singleton implementation was detected. Make sure the use of the Singleton pattern is required and the implementation is the right one for the context.}}
 
     private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
 
@@ -21,7 +21,7 @@ public class SingletonUsageCheckSample {
     }
   }
 
-  public static class StaticBlockSingleton { // Noncompliant
+  public static class StaticBlockSingleton { // Noncompliant [[sc=23;ec=43;secondary=+2,+4,+9]]
 
     private static StaticBlockSingleton instance;
 
@@ -99,7 +99,7 @@ public class SingletonUsageCheckSample {
     }
   }
 
-  public enum EnumSingleton { // Noncompliant
+  public enum EnumSingleton { // Noncompliant [[sc=15;ec=28]] {{An Enum-based Singleton implementation was detected. Make sure the use of the Singleton pattern is required and an Enum-based implementation is the right one for the context.}}
 
     INSTANCE;
 
@@ -133,4 +133,158 @@ public class SingletonUsageCheckSample {
 
   }
 
+  public static class MultipleStaticFieldsOfDifferentTypesDoNotPreventDetection { // Noncompliant
+    public static final MultipleStaticFieldsOfDifferentTypesDoNotPreventDetection ONE = new MultipleStaticFieldsOfDifferentTypesDoNotPreventDetection();
+    public static final String MESSAGE = "Hello, World!";
+    private int value;
+
+    private MultipleStaticFieldsOfDifferentTypesDoNotPreventDetection() {
+      this.value = 1;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
+
+  public static class PublicConstructorWithParameters { // Compliant the constructor is public and takes parameters
+    public static final PublicConstructorWithParameters INSTANCE = new PublicConstructorWithParameters(42);
+    private int value;
+
+    public PublicConstructorWithParameters(int value) {
+      this.value = value;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
+
+  enum MoreThanOneConstant {
+    ONE(),
+    THE_SAME();
+
+    private int value;
+
+    private MoreThanOneConstant() {
+      value = 42;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
+
+  interface WithSides {
+    int sides();
+  }
+
+  enum Shape implements WithSides { // Compliant because single enum constants are not singletons
+    TRIANGLE(3),;
+    private int sides;
+
+    Shape(int sides) {
+      this.sides = sides;
+    }
+
+    @Override
+    public int sides() {
+      return sides;
+    }
+  }
+
+  enum EnumWithPrivateInstanceMethodAndFields { // Compliant there are non-private fields or methods
+    INSTANCE();
+    private int value;
+    private EnumWithPrivateInstanceMethodAndFields() {
+      value = 42;
+    }
+
+    private int increment() {
+      return value++;
+    }
+  }
+
+  static class ClassWithPrivateInstanceMethodAndFields { // Compliant there are non-private fields or methods
+    public static final ClassWithPrivateInstanceMethodAndFields INSTANCE = new ClassWithPrivateInstanceMethodAndFields();
+    private int value;
+    private ClassWithPrivateInstanceMethodAndFields() {
+      value = 42;
+    }
+
+    private int increment() {
+      return value++;
+    }
+  }
+
+  public static class TooManyConstructors { // Compliant
+    public static final TooManyConstructors INSTANCE = new TooManyConstructors();
+    private int field;
+
+    private TooManyConstructors() {
+      field = 42;
+    }
+
+    private TooManyConstructors(int value) {
+      field = value;
+    }
+  }
+
+  public static class SinglePrivateConstructorWithParameter { // Compliant
+    public static final SinglePrivateConstructorWithParameter INSTANCE = new SinglePrivateConstructorWithParameter(42);
+    private int field;
+
+    private SinglePrivateConstructorWithParameter(int value) {
+      field = value;
+    }
+
+    public int value() {
+      return field;
+    }
+  }
+
+  public static class LackNonPublicFieldOrInstanceMethod { // Compliant
+    public static final LackNonPublicFieldOrInstanceMethod INSTANCE = new LackNonPublicFieldOrInstanceMethod();
+    private int field;
+
+    private LackNonPublicFieldOrInstanceMethod() {
+      field = 42;
+    }
+  }
+
+  public static class Numbers { // Compliant because there are multiple constant instances of the same type and not a singleton
+    public static final Numbers ONE = new Numbers();
+    public static final Numbers POSITIVE_ONE = new Numbers();
+    private int value;
+
+    private Numbers() {
+      this.value = 1;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
+
+  public static class MultipleReassignmentsPossible { // Compliant
+    private static MultipleReassignmentsPossible INSTANCE = new MultipleReassignmentsPossible();
+    private int value;
+
+    private MultipleReassignmentsPossible() {
+      value = 0;
+    }
+
+    public int increment() {
+      return value++;
+    }
+
+    public int getValue() {
+      return value;
+    }
+
+    public MultipleReassignmentsPossible reset() {
+      INSTANCE = new MultipleReassignmentsPossible();
+      return INSTANCE;
+    }
+  }
 }
