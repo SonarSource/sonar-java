f937998729832515381fbda72c2652d1099e2127
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2023-04-27 14:13:56 +0200

    Add autoscan support for windows

--- its/ruling/src/test/java/org/sonar/java/it/ProjectBuilder.java
+++ its/ruling/src/test/java/org/sonar/java/it/ProjectBuilder.java
@@ -34,21 +34,32 @@ import java.nio.file.Path;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 
 public class ProjectBuilder {
   private static final Pattern SOURCE_PATTERN = Pattern.compile("\\<source\\>(?<version>[^\\<]++)\\</source\\>");
   private static final URI JAVA_17_LINUX = URI.create("https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz");
+  private static final URI JAVA_17_WINDOWS = URI.create("https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.7_7.zip");
   private static final URI JAVA_19_LINUX = URI.create("https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2%2B7/OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz");
-  private static final Map<Integer, Map<String, URI>> JAVA_VERSIONS = Map.of(
-    17, Map.of("linux", JAVA_17_LINUX),
-    19, Map.of("linux", JAVA_19_LINUX)
+  private static final URI JAVA_19_WINDOWS = URI.create("https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2%2B7/OpenJDK19U-jdk_x64_windows_hotspot_19.0.2_7.zip");
+  private static final Map<Integer, Map<String, Map<String, URI>>> JAVA_VERSIONS = Map.of(
+    17, Map.of(
+      "linux", Map.of("amd64", JAVA_17_LINUX),
+      "windows", Map.of("amd64", JAVA_17_WINDOWS)
+    ),
+    19, Map.of(
+      "linux", Map.of("amd64", JAVA_19_LINUX),
+      "windows", Map.of("amd64", JAVA_19_WINDOWS)
+    )
   );
 
   private static final Path CACHE_FOLDER = Path.of(".autoscan_cache");
@@ -57,7 +68,9 @@ public class ProjectBuilder {
 
   private static final Map<URI, String> SHA256_FINGERPRINTS = Map.of(
     JAVA_17_LINUX, "e9458b38e97358850902c2936a1bb5f35f6cffc59da9fcd28c63eab8dbbfbc3b",
-    JAVA_19_LINUX, "3a3ba7a3f8c3a5999e2c91ea1dca843435a0d1c43737bd2f6822b2f02fc52165"
+    JAVA_17_WINDOWS, "daab0bac6681e8dbf7bce071c2d6b1b6feaf7479897871a705d10f5f0873d299",
+    JAVA_19_LINUX, "3a3ba7a3f8c3a5999e2c91ea1dca843435a0d1c43737bd2f6822b2f02fc52165",
+    JAVA_19_WINDOWS, "78406ce8ca86909634b5d07b511f6e4b5c3f91fa1b841411ae1b64f0f7761839"
   );
 
   private Path project;
@@ -70,8 +83,9 @@ public class ProjectBuilder {
     Path projectPath = project.toAbsolutePath();
     int javaVersion = extractSourceVersion(projectPath);
     System.out.println(projectPath + " uses Java" + javaVersion);
-    Path jdk = getJDK(javaVersion, "linux");
-    System.out.println("Will try to build with JDK" + javaVersion + " (" + jdk + ")" );
+    System.getProperty("os.name");
+    Path jdk = getJDK(javaVersion);
+    System.out.println("Will try to build with JDK" + javaVersion + " (" + jdk + ")");
     if (!build(projectPath, jdk)) {
       throw new IllegalStateException("Could not build " + projectPath + " using " + jdk);
     }
@@ -91,9 +105,17 @@ public class ProjectBuilder {
     throw new IllegalStateException("Could not find source version in " + pomXml);
   }
 
-  private static Path getJDK(Integer javaVersion, String platform) throws IOException, InterruptedException, NoSuchAlgorithmException {
-    // Get the right JDK archive
-    URI link = JAVA_VERSIONS.get(javaVersion).get(platform);
+  private static URI selectJDK(int javaVersion) {
+    String os = System.getProperty("os.name").split("\\s++")[0].toLowerCase(Locale.ROOT);
+    String architecture = System.getProperty("os.arch");
+    return JAVA_VERSIONS.get(javaVersion)
+      .get(os)
+      .get(architecture);
+  }
+
+  private static Path getJDK(Integer javaVersion) throws IOException, InterruptedException, NoSuchAlgorithmException {
+    // Get the right JDK link
+    URI link = selectJDK(javaVersion);
 
     // Set up directory to download
     Files.createDirectories(DOWNLOADS_FOLDER);
@@ -155,55 +177,73 @@ public class ProjectBuilder {
     String mimeType = Files.probeContentType(archive);
     switch (mimeType) {
       case "application/gzip":
-        Path topFolder = null;
-        try (var in = new FileInputStream(archive.toFile());
-             var gzFile = new GzipCompressorInputStream(in);
-             var tarFile = new TarArchiveInputStream(gzFile)) {
-          ArchiveEntry entry = null;
-          while ((entry = tarFile.getNextEntry()) != null) {
-            if (!tarFile.canReadEntryData(entry)) {
-              // log something?
-              continue;
-            }
-            String name = outputDirectory.resolve(entry.getName()).toAbsolutePath().toString();
-            File f = new File(name);
-            if (entry.isDirectory()) {
-              if (topFolder == null) {
-                topFolder = Path.of(name);
-              }
-              if (!f.isDirectory() && !f.mkdirs()) {
-                throw new IOException("failed to create directory " + f);
-              }
-            } else {
-              File parent = f.getParentFile();
-              if (!parent.isDirectory() && !parent.mkdirs()) {
-                throw new IOException("failed to create directory " + parent);
-              }
-              try (OutputStream o = Files.newOutputStream(f.toPath())) {
-                IOUtils.copy(tarFile, o);
-                if (parent.getName().equals("bin") ||
-                  (parent.getName().equals("lib") && f.getName().equals("jexec") || f.getName().equals("jspawnhelper") )) {
-                  f.setExecutable(true);
-                }
-              }
-            }
-          }
-          return topFolder;
-        }
+        return extractTarGz(archive, outputDirectory);
+      case "application/zip":
+        return extractZip(archive, outputDirectory);
       default:
         throw new UnsupportedOperationException("Cannot extract archive of type " + mimeType);
     }
   }
 
-  public boolean build(Path project, Path jdk) throws IOException, InterruptedException {
+  private static Path extractTarGz(Path archive, Path outputDirectory) throws IOException {
+    try (var in = new FileInputStream(archive.toFile());
+         var gzFile = new GzipCompressorInputStream(in);
+         var tarFile = new TarArchiveInputStream(gzFile)) {
+      return extractCommon(outputDirectory, tarFile);
+    }
+  }
+
+  private static Path extractZip(Path archive, Path outputDirectory) throws IOException {
+    try (var in = new FileInputStream(archive.toFile());
+         var zin = new ZipArchiveInputStream(in)) {
+      return extractCommon(outputDirectory, zin);
+    }
+  }
+
+  private static Path extractCommon(Path outputDirectory, ArchiveInputStream in) throws IOException {
+    Path topFolder = null;
+    ArchiveEntry entry = null;
+    while ((entry = in.getNextEntry()) != null) {
+      if (!in.canReadEntryData(entry)) {
+        // log something?
+        continue;
+      }
+      String name = outputDirectory.resolve(entry.getName()).toAbsolutePath().toString();
+      File f = new File(name);
+      if (entry.isDirectory()) {
+        if (topFolder == null) {
+          topFolder = Path.of(name);
+        }
+        if (!f.isDirectory() && !f.mkdirs()) {
+          throw new IOException("failed to create directory " + f);
+        }
+      } else {
+        File parent = f.getParentFile();
+        if (!parent.isDirectory() && !parent.mkdirs()) {
+          throw new IOException("failed to create directory " + parent);
+        }
+        try (OutputStream o = Files.newOutputStream(f.toPath())) {
+          IOUtils.copy(in, o);
+          if (parent.getName().equals("bin") ||
+            (parent.getName().equals("lib") && f.getName().equals("jexec") || f.getName().equals("jspawnhelper"))) {
+            f.setExecutable(true);
+          }
+        }
+      }
+    }
+    return topFolder;
+  }
+
+  private boolean build(Path project, Path jdk) throws IOException, InterruptedException {
+    String mavenBinary = System.getProperty("os.name").toLowerCase().startsWith("windows") ? "mvn.cmd" : "mvn";
     String[] command = new String[]{
-      "mvn", "clean", "verify",
+      mavenBinary, "clean", "verify",
     };
     ProcessBuilder processBuilder = new ProcessBuilder(command)
       .inheritIO()
       .directory(project.toFile());
     processBuilder.environment().put("JAVA_HOME", jdk.toString());
-    System.out.println(processBuilder.command() + " (" + processBuilder.environment() +")");
+    System.out.println(processBuilder.command() + " (" + processBuilder.environment() + ")");
     Process process = processBuilder.start();
     int returnCode = process.waitFor();
     return returnCode == 0;
