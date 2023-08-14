9b1e4bdcff4c49b5ec2d6f5622557e4dbde33a7c
erwan-serandour-sonarsource <erwan.serandour@sonarsource.com>
2023-08-14 14:01:43 +0200

    SONARJAVA-4574 move testfile from rule S106 to java-checks-test-sources

--- java-checks-test-sources/src/main/java/checks/SystemOutOrErrUsageCheck.java
+++ java-checks-test-sources/src/main/java/checks/SystemOutOrErrUsageCheck.java
@@ -1,15 +1,24 @@
-class A {
+package checks;
+
+import java.io.PrintStream;
+
+class SystemOutOrErrUsageCheck {
 
   void f() {
     System.out.println("");                  // Noncompliant {{Replace this use of System.out by a logger.}}
     System.err.println("");                  // Noncompliant [[sc=5;ec=15]] {{Replace this use of System.err by a logger.}}
 
-    f(System.out);                           // Noncompliant
+
+    g(System.out);                           // Noncompliant
 
     System.arraycopy(null, 0, null, 0, 0);   // Compliant
     java.lang.System.out.println("");        // Noncompliant [[sc=5;ec=25]]
     java.lang.                               // Noncompliant [[sc=5;el=+1;ec=17]]
       System.out.println("");
+  }
+
+  void g(PrintStream stream){
+
   }
 
 }
