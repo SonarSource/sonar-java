4924e89abcc77397c3ae6953ae44dafa4bc9cf94
Dorian Burihabwa <75226315+dorian-burihabwa-sonarsource@users.noreply.github.com>
2023-10-18 23:00:21 +0200

    SONARJAVA-4653 S6814 Optional REST parameters should have an object type

---
+++ java-checks-test-sources/src/main/java/checks/spring/OptionalRestParametersShouldBeObjects.java
@@ -0,0 +1,66 @@
+package checks.spring;
+
+import com.mongodb.lang.Nullable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+class OptionalRestParametersShouldBeObjects {
+
+  @GetMapping(value = {"/article", "/article/{id}"})
+  public Article getArticle(@PathVariable(required = false) int articleId, int unused) { // Noncompliant [[sc=29;ec=75]] {{Convert this optional parameter to an Object type.}}
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article", "/article/{id}"})
+  public Article getArticleWithRequestParam(@RequestParam(required = false) int articleId, int unused) { // Noncompliant [[sc=45;ec=91]] {{Convert this optional parameter to an Object type.}}
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article", "/article/{id}"})
+  public Article getArticle(
+    @PathVariable(required = false) float articleId, // Noncompliant [[sc=5;ec=53]]
+    @RequestParam(required = false) int someIssue, // Noncompliant [[sc=5;ec=51]]
+    @PathVariable(required = false) boolean anotherIssue // Noncompliant [[sc=5;ec=57]]
+  ) {
+    return new Article((int) (Math.floor(articleId)));
+  }
+
+  @GetMapping(value = {"/article", "/article/{id}"})
+  public Article getArticle(@PathVariable(required = false) Integer articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article/{id}"})
+  public Article getArticleButImplictlyRequired(@PathVariable int articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article/{id}"})
+  public Article getArticleRequestParamButImplictlyRequired(@RequestParam int articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article/{id}"})
+  public Article getArticleButExplicitlyRequired(@PathVariable(required = true) int articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article/{id}"})
+  public Article getArticleRequestParamButExplicitlyRequired(@RequestParam(required = true) int articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article/{id}"})
+  public Article getArticleButSettingADifferentValue(@PathVariable(name = "articleId") int articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  @GetMapping(value = {"/article/{id}"})
+  public Article getArticleButDifferentlyAnnotated(@Nullable int articleId) { // Compliant
+    return new Article(articleId);
+  }
+
+  record Article(int id) {
+  }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/spring/OptionalRestParametersShouldBeObjectsCheck.java
@@ -0,0 +1,69 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.spring;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.AnnotationTree;
+import org.sonar.plugins.java.api.tree.AssignmentExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
+
+@Rule(key = "S6814")
+public class OptionalRestParametersShouldBeObjectsCheck extends IssuableSubscriptionVisitor {
+
+  private static final List<String> PARAMETER_ANNOTATIONS = List.of(
+    "org.springframework.web.bind.annotation.PathVariable",
+    "org.springframework.web.bind.annotation.RequestParam"
+  );
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.METHOD);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    MethodTree method = (MethodTree) tree;
+    method.parameters().stream()
+      .filter(OptionalRestParametersShouldBeObjectsCheck::isOptionalPrimitive)
+      .forEach(parameter -> reportIssue(parameter, "Convert this optional parameter to an Object type."));
+  }
+
+  private static boolean isOptionalPrimitive(VariableTree parameter) {
+    return parameter.type().symbolType().isPrimitive() &&
+      parameter.modifiers().annotations().stream()
+        .anyMatch(OptionalRestParametersShouldBeObjectsCheck::isMarkingAsOptional);
+  }
+
+  private static boolean isMarkingAsOptional(AnnotationTree annotation) {
+    return PARAMETER_ANNOTATIONS.stream().anyMatch(candidate -> annotation.annotationType().symbolType().is(candidate)) &&
+      annotation.arguments().stream().anyMatch(expressionTree -> {
+        // Because all parameters of the supported annotations are assignments, we can cast safely here.
+        AssignmentExpressionTree assignment = (AssignmentExpressionTree) expressionTree;
+        IdentifierTree variable = (IdentifierTree) assignment.variable();
+        Boolean constant = assignment.expression().asConstant(Boolean.class).orElse(Boolean.TRUE);
+        return "required".equals(variable.name()) && Boolean.FALSE.equals(constant);
+      });
+  }
+}
