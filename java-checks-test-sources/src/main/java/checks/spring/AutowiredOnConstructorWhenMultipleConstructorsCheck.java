9137f5867370e9531bd5491d4c546c842bed1770
Irina Batinic <irina.batinic@sonarsource.com>
2023-10-27 09:17:59 +0200

    SONARJAVA-4676 S6829: @Autowired should be used when multiple constructors are provided #3350

---
+++ java-checks-test-sources/src/main/java/checks/spring/AutowiredOnConstructorWhenMultipleConstructorsCheck.java
@@ -0,0 +1,93 @@
+package checks.spring;
+
+import javax.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public class AutowiredOnConstructorWhenMultipleConstructorsCheck { // Compliant
+
+  public AutowiredOnConstructorWhenMultipleConstructorsCheck() {
+  };
+
+  public AutowiredOnConstructorWhenMultipleConstructorsCheck(int i) {
+  };
+
+  public AutowiredOnConstructorWhenMultipleConstructorsCheck(String s) {
+  };
+}
+
+@Component
+class SpringComponent { // Noncompliant [[sc=7;ec=22]] {{Add @Autowired to one of the constructors.}}
+
+  public SpringComponent() {
+  };
+
+  public SpringComponent(int i) {
+  };
+
+  public SpringComponent(String s) {
+  };
+}
+
+@Component
+class CompliantSpringComponent { // Compliant
+
+  @Autowired
+  public CompliantSpringComponent() {
+  };
+
+  public CompliantSpringComponent(int i) {
+  };
+
+  public CompliantSpringComponent(String s) {
+  };
+}
+
+@Component
+class ComponentWithOtherAnnotations { // Noncompliant
+
+  public ComponentWithOtherAnnotations() {
+  };
+
+  public ComponentWithOtherAnnotations(int i) {
+  };
+
+  @Deprecated
+  public ComponentWithOtherAnnotations(String s) {
+  };
+
+}
+
+@Component
+class ComponentWithOneConstructorWithAutowired { // Compliant
+
+  public ComponentWithOneConstructorWithAutowired() {
+  };
+}
+
+@Nullable
+class ClassWithSomeOtherAnnotation { // Compliant
+
+  public ClassWithSomeOtherAnnotation() {
+  };
+
+  public ClassWithSomeOtherAnnotation(int i) {
+  };
+
+  public ClassWithSomeOtherAnnotation(String s) {
+  };
+}
+
+@Nullable
+@Component
+class ClassWithTwoAnnotations { // Noncompliant
+
+  public ClassWithTwoAnnotations() {
+  };
+
+  public ClassWithTwoAnnotations(int i) {
+  };
+
+  public ClassWithTwoAnnotations(String s) {
+  };
+}
