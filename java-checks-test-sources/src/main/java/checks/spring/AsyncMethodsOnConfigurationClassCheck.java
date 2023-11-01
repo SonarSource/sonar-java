c98387c51888f9aa80582b39317efd0c1673936a
Irina Batinic <117161143+irina-batinic-sonarsource@users.noreply.github.com>
2023-11-01 11:21:54 +0100

    SONARJAVA-4650 S6817: Use of the @Async annotation on methods declare… (#4519)

---
+++ java-checks-test-sources/src/main/java/checks/spring/AsyncMethodsOnConfigurationClassCheck.java
@@ -0,0 +1,32 @@
+package checks.spring;
+
+import javax.annotation.Nullable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+
+@Configuration
+public class AsyncMethodsOnConfigurationClassCheck {
+
+  @Async // Noncompliant [[sc=3;ec=9;quickfixes=qf1]] {{Remove this "@Async" annotation from this method.}}
+  // fix@qf1 {{Remove "@Async"}}
+  // edit@qf1 [[sc=3;ec=9]] {{}}
+  public void asyncMethod() {
+  }
+
+  public void method() { // Compliant
+  }
+
+  @Nullable
+  @Async // Noncompliant
+  public void someMethod() {
+  }
+
+}
+
+class NotAConfigurationClass {
+
+  @Async // Compliant
+  public void asyncMethod() {
+  }
+
+}
