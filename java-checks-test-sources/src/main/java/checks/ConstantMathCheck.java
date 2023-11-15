653465ac0d3fee802b6fa42689c375e1cab2612e
Marco Kaufmann <marco.kaufmann@sonarsource.com>
2023-11-15 15:25:00 +0100

    moved test samples to module java-checks-test-sources

--- java-checks-test-sources/src/main/java/checks/ConstantMathCheck.java
+++ java-checks-test-sources/src/main/java/checks/ConstantMathCheck.java
@@ -1,14 +1,16 @@
-public class TestClass {
+package checks;
+
+public class ConstantMathCheck {
 
   public void method(int a, Integer integer, Double doubleParam) {
-    byte b = a % 1; // Noncompliant [[sc=16;ec=17]] {{Remove this computation of % 1, which always evaluates to zero.}}
+    byte b = (byte) (a % 1); // Noncompliant [[sc=24;ec=25]] {{Remove this computation of % 1, which always evaluates to zero.}}
     double v = 7.4d;
     double remainder = v % 1; // compliant, remainder is ~0.4
-    b = integer % 1; // Noncompliant
-    b = doubleParam % 1; // Compliant, double
+    b = (byte) (integer % 1); // Noncompliant
+    b = (byte) (doubleParam % 1); // Compliant, double
     int c = a % 2; // Compliant
     int d = a % a; // Compliant, currently not covered by this rule
-    short s;
+    short s = 0x7fff;
 
     Math.abs((double)' '); // Noncompliant [[sc=5;ec=13]] {{Remove this unnecessary call to "Math.abs"}}
     Math.abs((float) 0); // Noncompliant {{Remove this unnecessary call to "Math.abs"}}
@@ -16,9 +18,6 @@ public class TestClass {
     Math.abs(0L); // Noncompliant {{Remove this unnecessary call to "Math.abs"}}
     Math.abs(a); // Compliant
     Math.abs((float) a); // Compliant
-
-    // the following does not compile, but is checked anyway from a type perspective.
-    Math.ceil((double) true); // Noncompliant {{Remove this unnecessary call to "Math.ceil"}}
     Math.ceil((double) new Object()); // Compliant
 
     Math.ceil(a); // Noncompliant {{Remove this unnecessary call to "Math.ceil"}}
@@ -36,7 +35,7 @@ public class TestClass {
     Math.round((float) 0); // Noncompliant {{Remove this unnecessary call to "Math.round"}}
     Math.ceil(((double) ((double) (a)))); // Noncompliant {{Remove this unnecessary call to "Math.ceil"}}
 
-    float value;
+    float value = 3.14f;
     Math.acos(value); // Compliant
     Math.cos(value); // Compliant
     Math.acos(2.0); // Compliant
@@ -73,5 +72,4 @@ public class TestClass {
     Math.toDegrees(1.0d); // Noncompliant {{Remove this unnecessary call to "Math.toDegrees"}}
     Math.toRadians(0.0d); // Noncompliant {{Remove this unnecessary call to "Math.toRadians"}}
   }
-
 }
