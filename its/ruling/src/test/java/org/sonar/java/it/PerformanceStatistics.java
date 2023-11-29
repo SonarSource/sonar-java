5ee16ac1c33ad554c1a9a4a649ae0b42d3e8d073
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2023-11-29 16:49:21 +0100

    [NO-JIRA] Add logs to ruling test to display most time consuming rules

--- its/ruling/src/test/java/org/sonar/java/it/PerformanceStatistics.java
+++ its/ruling/src/test/java/org/sonar/java/it/PerformanceStatistics.java
@@ -22,13 +22,19 @@ package org.sonar.java.it;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonarsource.performance.measure.DurationMeasure;
 import org.sonarsource.performance.measure.DurationMeasureFiles;
 
 public class PerformanceStatistics {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PerformanceStatistics.class);
 
   public static void main(String[] args) throws IOException {
     PerformanceStatistics.generate(Paths.get("target","performance"));
@@ -49,6 +55,26 @@ public class PerformanceStatistics {
     measure.recursiveMergeOnUpperLevel("JavaWriteCache.write");
     Path performanceStatFile = performanceDirectory.resolve("sonar.java.performance.statistics.txt");
     DurationMeasureFiles.writeStatistics(performanceStatFile, measure, categoryNames, groupedMeasurePredicate);
+    logMostTimeConsumingRules(measure);
+  }
+
+  private static void logMostTimeConsumingRules(DurationMeasure measure) {
+    List<DurationMeasure> checksMeasures = new ArrayList<>();
+    extractChecksMeasures(measure, checksMeasures);
+    LOG.info("Most time consuming rules:");
+    checksMeasures.stream()
+      .sorted((check1, check2) -> Long.compare(check2.durationNanos(), check1.durationNanos()))
+      .limit(10)
+      .forEach(check -> LOG.info("{}: {} ns", check.name(), check.durationNanos()));
+  }
+
+  private static void extractChecksMeasures(DurationMeasure root, List<DurationMeasure> result) {
+    if (root.name().endsWith("Check")) {
+      result.add(root);
+    }
+    if (!root.children().isEmpty()) {
+      root.children().forEach(child -> extractChecksMeasures(child, result));
+    }
   }
 
 }
