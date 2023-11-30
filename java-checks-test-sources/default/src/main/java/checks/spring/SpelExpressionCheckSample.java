a5f59d6aa36846b0dac8aa916eedb1d171fa7469
Marco Kaufmann <marco.kaufmann@sonarsource.com>
2023-11-30 14:28:15 +0100

    SONARJAVA-4722 Implement S6857: SpEL expression should have valid syntax

---
+++ java-checks-test-sources/default/src/main/java/checks/spring/SpelExpressionCheckSample.java
@@ -0,0 +1,295 @@
+package checks.spring;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SpelExpressionCheckSample {
+
+  @Value("#{systemProperties['user.region'}") // Noncompliant
+  private String regionNc;
+
+  @Value("#{systemProperties['user.region']}") // Compliant
+  private String regionC;
+
+  @Value("#{'${listOfValues}' split(',')}") // Noncompliant, missing operator
+  private List<String> valuesListNc;
+
+  @Value("#{'${listOfValues}'.split(',')}") // Compliant
+  private List<String> valuesListC;
+
+  @Value("#{T(java.lang.Math).random() * 64h}") // Noncompliant, invalid number
+  private Double randPercentNc;
+
+  @Value("#{T(java.lang.Math).random() * 100.0}") // Compliant
+  private Double randPercentC;
+
+  static class User {
+    String status;
+  }
+
+  static class Customer {
+    String firstname;
+    String name;
+  }
+
+  interface Repo {
+
+    @Query("select u from User u where u.age = ?#{[0]}") // Compliant
+    List<User> findUsersByAge1(int age);
+
+    @Query("select u from User u where u.age = ?#{[0}") // Noncompliant
+    List<User> findUsersByAge2(int age);
+
+    @Query("select u from User u where u.age = ?#{[0]") // Noncompliant
+    List<User> findUsersByAge3(int age);
+
+    @Query("select u from User u where u.age = ?#{[0*]}") // Noncompliant
+    List<User> findUsersByAge4(int age);
+
+    @Query("select u from User u where u.firstname = :#{#customer.firstname}") // Compliant
+    List<User> findUsersByCustomersFirstname1(@Param("customer") Customer customer);
+
+    @Query("select u from User u where u.firstname = :#{#customer firstname}") // Noncompliant
+    List<User> findUsersByCustomersFirstname2(@Param("customer") Customer customer);
+
+    @Query("select u from User u where u.name = :#{#customer.name} and u.firstname = :#{#customer.firstname}")  // Compliant
+    List<User> findUsersByCustomersFullName1(@Param("customer") Customer customer);
+
+    @Query("select u from User u where u.name = :#{#customer.name} and u.firstname = :#{#customer.firstname")  // Noncompliant
+    List<User> findUsersByCustomersFullName2(@Param("customer") Customer customer);
+
+    @Query("select u from User u where u.name = :#{#customer.name} and u.firstname = :#{#*customer.firstname}")  // Noncompliant
+    List<User> findUsersByCustomersFullName3(@Param("customer") Customer customer);
+
+    @Query("select u from User u where u.firstname = :#{#customer.firstname} and u.role=${admin}")  // Compliant
+    List<User> findAdminUsersByFirstname1(@Param("customer") Customer customer);
+
+    @Query("select u from User u where u.firstname = :#{#customer.firstname} and u.role=${admin")  // Noncompliant
+    List<User> findAdminUsersByFirstname2(@Param("customer") Customer customer);
+  }
+
+  @Controller
+  @RequestMapping("#{1+2+3}") // Compliant
+  public static class RequestController1 { }
+
+  @Controller
+  @RequestMapping("#{1+2+}") // Noncompliant
+  public static class RequestController2 { }
+
+  @Value("foo") // Compliant
+  String noTemplate0;
+
+  @Value("foo.bar") // Compliant
+  String noTemplate1;
+
+  @Value("foo[10]") // Compliant
+  String noTemplate2;
+
+  @Value("foo[10][20]") // Compliant
+  String noTemplate3;
+
+  @Value("foo[10].bar") // Compliant
+  String noTemplate4;
+
+  @Value("foo.bar[10][20].baz") // Compliant
+  String noTemplate5;
+
+  @Value("{}") // Compliant
+  String delimiters0;
+
+  @Value("{123") // Compliant
+  String delimiters1;
+
+  @Value("123}") // Compliant
+  String delimiters2;
+
+  @Value("$") // Compliant
+  String delimiters3;
+
+  @Value("$ ") // Compliant
+  String delimiters4;
+
+  @Value("#") // Compliant
+  String delimiters5;
+
+  @Value("# ") // Compliant
+  String delimiters6;
+
+  @Value("$123") // Compliant
+  String delimiters7;
+
+  @Value("$foo") // Compliant
+  String delimiters8;
+
+  @Value("${}") // Noncompliant
+  String delimiters9;
+
+  @Value("${123") // Noncompliant
+  String delimiters10;
+
+  @Value("$123}") // Compliant
+  String delimiters11;
+
+  @Value("#{}") // Noncompliant
+  String delimiters12;
+
+  @Value("#{123") // Noncompliant
+  String delimiters13;
+
+  @Value("#123}") // Compliant
+  String delimiters14;
+
+  @Value("#{123}}") // Compliant
+  String delimiters15;
+
+  @Value("#{{123}") // Noncompliant
+  String delimiters16;
+
+  @Value("#{12{}3}") // Noncompliant, open
+  String delimiters17;
+
+  @Value("#{{12}3{}") // Noncompliant, open
+  String delimiters18;
+
+  @Value("#{{12}3{4{5}6}") // Noncompliant
+  String delimiters19;
+
+  @Value("{ }") // Compliant
+  String delimiters20;
+
+  @Value("${ }") // Noncompliant
+  String delimiters21;
+
+  @Value("#{ }") // Noncompliant
+  String delimiters22;
+
+  @Value("${") // Noncompliant
+  String delimiters23;
+
+  @Value("${ ") // Noncompliant
+  String delimiters24;
+
+  @Value("#{ ") // Noncompliant
+  String delimiters25;
+
+  @Value("$ {") // Compliant
+  String delimiters26;
+
+  @Value("# {") // Compliant
+  String delimiters27;
+
+  @Value("$}") // Compliant
+  String delimiters28;
+
+  @Value("$ }") // Compliant
+  String delimiters29;
+
+  @Value("# }") // Compliant
+  String delimiters30;
+
+  @Value("${3foo}") // Noncompliant
+  String ncPlaceholder0;
+
+  @Value("${foo bar}") // Noncompliant
+  String ncPlaceholder1;
+
+  @Value("${foo .bar}") // Noncompliant
+  String ncPlaceholder2;
+
+  @Value("${foo,bar}") // Noncompliant
+  String ncPlaceholder3;
+
+  @Value("${foo..bar}") // Noncompliant
+  String ncPlaceholder4;
+
+  @Value("${foo.}") // Noncompliant
+  String ncPlaceholder5;
+
+  @Value("${.bar}") // Noncompliant
+  String ncPlaceholder6;
+
+  @Value("${foo[10}") // Noncompliant
+  String ncPlaceholder7;
+
+  @Value("${foo 10]}") // Noncompliant
+  String ncPlaceholder8;
+
+  @Value("${foo[10 20]}") // Noncompliant
+  String ncPlaceholder9;
+
+  @Value("${foo[10.bar]}") // Noncompliant
+  String ncPlaceholder10;
+
+  @Value("${foo.bar[][20].baz}") // Noncompliant
+  String ncPlaceholder11;
+
+  @Value("${foo[]}") // Noncompliant
+  String ncPlaceholder12;
+
+  @Value("${[10]}") // Noncompliant
+  String ncPlaceholder13;
+
+  @Value("${[[10]}") // Noncompliant
+  String ncPlaceholder14;
+
+  @Value("${foo + bar}") // Noncompliant
+  String ncPlaceholder15;
+
+  @Value("${foo}") // Compliant
+  String cPlaceholder0;
+
+  @Value("${foo.bar}") // Compliant
+  String cPlaceholder1;
+
+  @Value("${foo[10]}") // Compliant
+  String cPlaceholder2;
+
+  @Value("${foo[10][20]}") // Compliant
+  String cPlaceholder3;
+
+  @Value("${foo[10].bar}") // Compliant
+  String cPlaceholder4;
+
+  @Value("${foo.bar[10][20].baz}") // Compliant
+  String cPlaceholder5;
+
+  @Value("#{foo bar}") // Noncompliant
+  String spel0;
+
+  @Value("#{foo .bar}") // Compliant
+  String spel1;
+
+  @Value("#{foo,bar}") // Noncompliant
+  String spel2;
+
+  @Value("#{foo}") // Compliant
+  String spel3;
+
+  @Value("#{foo.bar}") // Compliant
+  String spel4;
+
+  @Value("#{10 * (20 + foo)}") // Compliant
+  String spel5;
+
+  @Value("#{foo[10].bar}") // Compliant
+  String spel6;
+
+  @Value("#{foo.bar[10][20].baz}") // Compliant
+  String spel7;
+
+  @Value("#(123))") // Compliant
+  String spel8;
+
+  @Value("#())") // Compliant
+  String spel9;
+
+  @Value("#{()})") // Noncompliant
+  String spel10;
+
+  @Value("#{(42)})") // Compliant
+  String spel11;
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/spring/SpelExpressionCheck.java
@@ -0,0 +1,192 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks.spring;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.ObjIntConsumer;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.AnnotationTree;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.LiteralTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
+import org.springframework.expression.ParseException;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+@Rule(key = "S6857")
+public class SpelExpressionCheck extends IssuableSubscriptionVisitor {
+
+  private static final String SPRING_PREFIX = "org.springframework";
+
+  private static final Pattern PROPERTY_PLACEHOLDER_PATTERN = Pattern.compile("[a-zA-Z_]\\w*+(\\[\\d++])*+(\\.[a-zA-Z_]\\w*+(\\[\\d++])" +
+    "*+)*+");
+
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.CLASS, Tree.Kind.INTERFACE, Tree.Kind.ANNOTATION_TYPE);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    getClassAndMemberAnnotations((ClassTree) tree)
+      .filter(SpelExpressionCheck::isSpringAnnotation)
+      .forEach(this::checkSpringAnnotationArguments);
+  }
+
+  private static Stream<AnnotationTree> getClassAndMemberAnnotations(ClassTree cls) {
+    return Stream.concat(
+      Stream.of(cls.modifiers().annotations()),
+      cls.members().stream().map(SpelExpressionCheck::getMemberAnnotations)
+    ).flatMap(Collection::stream);
+  }
+
+  private static List<AnnotationTree> getMemberAnnotations(Tree member) {
+    if (member.is(Tree.Kind.METHOD)) {
+      return ((MethodTree) member).modifiers().annotations();
+    } else if (member.is(Tree.Kind.VARIABLE)) {
+      return ((VariableTree) member).modifiers().annotations();
+    } else {
+      return Collections.emptyList();
+    }
+  }
+
+  private static boolean isSpringAnnotation(AnnotationTree annotation) {
+    return annotation.symbolType().fullyQualifiedName().startsWith(SPRING_PREFIX);
+  }
+
+  private void checkSpringAnnotationArguments(AnnotationTree annotation) {
+    annotation.arguments().stream()
+      .filter(argument -> argument.is(Tree.Kind.STRING_LITERAL))
+      .forEach(argument -> checkSpringAnnotationStringArgument((LiteralTree) argument));
+  }
+
+  private void checkSpringAnnotationStringArgument(LiteralTree argument) {
+    try {
+      parseStringArgument(argument.value());
+    } catch (SyntaxError e) {
+      reportIssue(argument, e.getMessage());
+    }
+  }
+
+  private void parseStringArgument(String value) {
+    value = value.substring(1, value.length() - 1);
+    var i = 0;
+    while (i < value.length()) {
+      switch (value.charAt(i++)) {
+        case '$':
+          i = parsePropertyPlaceholder(value, i);
+          break;
+        case '#':
+          i = parseSpelExpression(value, i);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private static int parsePropertyPlaceholder(String value, int startIndex) {
+    return parseDelimitersAndContents(value, startIndex, (contents, endIndex) -> {
+      if (!isValidPropertyPlaceholder(contents)) {
+        throw new SyntaxError("Correct this malformed property placeholder.", startIndex + 1, endIndex - 1);
+      }
+    });
+  }
+
+  private static int parseSpelExpression(String value, int startIndex) {
+    return parseDelimitersAndContents(value, startIndex, (contents, endIndex) -> {
+      if (!isValidSpelExpression(contents)) {
+        throw new SyntaxError("Correct this malformed SpEL expression.", startIndex + 1, endIndex - 1);
+      }
+    });
+  }
+
+  private static int parseDelimitersAndContents(String value, int startIndex, ObjIntConsumer<String> parseContents) {
+    if (startIndex == value.length()) {
+      return startIndex;
+    }
+    var endIndex = parseDelimiterBraces(value, startIndex);
+    if (endIndex == startIndex) {
+      return endIndex;
+    }
+    var contents = value.substring(startIndex + 1, endIndex - 1).strip();
+    parseContents.accept(contents, endIndex);
+    return endIndex;
+  }
+
+  private static int parseDelimiterBraces(String value, int startIndex) throws SyntaxError {
+    if (value.charAt(startIndex) != '{') {
+      return startIndex;
+    }
+    var i = startIndex + 1;
+    var openCount = 1;
+
+    while (i < value.length()) {
+      switch (value.charAt(i++)) {
+        case '{':
+          openCount++;
+          break;
+        case '}':
+          openCount--;
+          if (openCount == 0) {
+            return i;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    throw new SyntaxError("Add missing '}' for this property placeholder or SpEL expression.", startIndex, i);
+  }
+
+  private static boolean isValidPropertyPlaceholder(String placeholder) {
+    return PROPERTY_PLACEHOLDER_PATTERN.matcher(placeholder).matches();
+  }
+
+  private static boolean isValidSpelExpression(String expressionString) {
+    if (expressionString.isEmpty()) {
+      return false;
+    }
+    try {
+      new SpelExpressionParser().parseExpression(expressionString);
+    } catch (ParseException | IllegalStateException e) {
+      return false;
+    }
+    return true;
+  }
+
+  private static class SyntaxError extends RuntimeException {
+
+    SyntaxError(String message, int startIndex, int endIndex) {
+      super(message);
+      this.startIndex = startIndex;
+      this.endIndex = endIndex;
+    }
+
+    public final int startIndex;
+
+    public final int endIndex;
+  }
+}
