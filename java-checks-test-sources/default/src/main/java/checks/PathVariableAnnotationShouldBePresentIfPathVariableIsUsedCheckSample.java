06c58fb200d7b6f50357f6d671d1363c37f382f2
erwan-serandour-sonarsource <erwan.serandour@sonarsource.com>
2023-12-01 12:24:56 +0100

    implement core logic of PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheck

--- java-checks-test-sources/default/src/main/java/checks/PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheckSample.java
@@ -9,49 +9,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 public class PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheckSample {
-
-  @GetMapping("/{id}")
-  public String get(String id) { // Noncompliant [[sc=27;ec=37]] {{Add the "@PathVariable" annotation to "id".}}
+  @GetMapping("/{id}") // Noncompliant {{Bind path variable "id" to a method parameter.}}
+  public String get(String id) {
     return "Hello World";
-  } // Noncompliant [[sc=3;ec=4]] {{Add the "@PathVariable" annotation to "id".}}
+  }
 
-  @PostMapping("/{id}")
-  public String post(String id) { // Noncompliant [[sc=27;ec=38]] {{Add the "@PathVariable" annotation to "id".}}
+  @PostMapping(value = "/{name}") // Noncompliant {{Bind path variable "name" to a method parameter.}}
+  public String post(String id) {
     return "Hello World";
-  } // Noncompliant
+  }
 
-  @PutMapping("/{id}")
-  public String put(String id) { // Noncompliant
+  @PutMapping(path = "/{id}") // Noncompliant
+  public String put(String id) {
     return "Hello World";
-  } // Noncompliant
+  }
 
-  @DeleteMapping("/{id}")
-  public String delete(String id) { // Noncompliant
+  @DeleteMapping("/{id}") // Noncompliant
+  public String delete(String id) {
     return "Hello World";
-  } // Noncompliant
+  }
+
+  @PutMapping("/id")
+  @DeleteMapping("/{id}") // Noncompliant
+  public String deletePut(String id) {
+    return "Hello World";
+  }
+
 
   @GetMapping("/{id}")
   public String getCompliant(@PathVariable  String id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @PostMapping("/{id}")
-  public String postCpmpliant(@PathVariable  String id) { // Noncompliant [[sc=27;ec=38]] {{Add the "@PathVariable" annotation to "id".}}
+  public String postCompliant(@PathVariable  String id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @PutMapping("/{id}")
   public String putCompliant(@PathVariable  String id) { // compliant
     return "Hello World";
-  } // compliant
+  }
+
 
   @DeleteMapping("/{id}")
   public String deleteCompliant(@PathVariable String id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @GetMapping("/{id}")
-  public String getOtherThanString(@PathVariable Integer id) { // compliant, is it good
+  @DeleteMapping({"/{id}"})
+  public String deleteGetCompliant(@PathVariable String id) { // compliant
+    return "Hello World";
+  }
+
+  @GetMapping("/{id}")
+  public String getOtherThanString(@PathVariable Integer id) { // compliant
+    return "Hello World";
+  }
+
+  @GetMapping()
+  public String getEmpty() { // compliant
     return "Hello World";
   }
 
@@ -63,27 +81,32 @@ public class PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheckSampl
   @GetMapping("/{id}/{name}")
   public String get2PathVariables(@PathVariable String id, @PathVariable String name) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
-  @GetMapping("/{id}")
-  public String getBadName(@PathVariable String a) { // compliant, does it even compile
+  @GetMapping("/{id}") // Noncompliant
+  public String getBadName(@PathVariable String a) {
     return "Hello World";
-  } // Noncompliant
+  }
+
+  @GetMapping("/{id}/{name}/{age}") // Noncompliant
+  public String get2SameName(@PathVariable("name") String a, @PathVariable(name = "name") String b, @PathVariable(value = "id", required=false) String c) {
+    return "Hello World";
+  }
 
   @GetMapping("/{id}/{name}/{age}")
-  public String getNotSameName(@PathVariable("name") String a, @PathVariable(name = "name") String b, @PathVariable(value = "id", required=false) String c) { // compliant
+  public String get3Name(@PathVariable("name") String a, @PathVariable(name = "age") String b, @PathVariable(value = "id", required=false) String c) { // compliant
     return "Hello World";
   }
 
   @GetMapping("/{id}")
   public String getMap(@PathVariable Map<String, String> map) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @GetMapping("/{id}/{name}")
   public String getMap2(@PathVariable Map<String, String> map) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @GetMapping("/{id}/{name}/{age}")
   public String getMapMixed(@PathVariable Map<String, String> map, @PathVariable String age) { // compliant
@@ -93,12 +116,12 @@ public class PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheckSampl
   @GetMapping(value = {"/a/{id}", "/b/{id}", "/c"})
   public String getSeveralPaths(@PathVariable Optional<String> id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @GetMapping({"/a/{id}", "/b/{id}", "/c"})
   public String getSeveralPathsDefault(@PathVariable Optional<String> id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
   @GetMapping("/a/{id:.+}")
   public String getRegex(@PathVariable String id) { // compliant
@@ -112,19 +135,20 @@ public class PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheckSampl
 
   public String withoutAnnotation(String id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
-  public String withoutRequestMappingAnnotation(@PathVariable  String id) { // compliant, does it compile ?
+  public String withoutRequestMappingAnnotation(@PathVariable  String id) { // compliant
     return "Hello World";
-  } // compliant
+  }
 
 
-  @GetMapping(name="aName",
+  @GetMapping(
     path={"/{id}", "/{name}"},
     produces={"application/json", "application/xml"},
     consumes={"application/json", "application/xml"},
     headers={"aHeader=aValue", "anotherHeader=anotherValue"},
-    params={"aPara", "anotherParam=anotherValue"}
+    params={"aPara", "anotherParam=anotherValue"},
+    name="aName"
   )
   public String getFullExample(@PathVariable Map<String,String> x) { // compliant
     return "Hello World";
--- java-checks/src/main/java/org/sonar/java/checks/PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheck.java
@@ -20,23 +20,153 @@
 package org.sonar.java.checks;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.sonar.check.Rule;
+import org.sonar.java.model.LiteralUtils;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.Type;
+import org.sonar.plugins.java.api.tree.AnnotationTree;
+import org.sonar.plugins.java.api.tree.AssignmentExpressionTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.LiteralTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.ModifiersTree;
+import org.sonar.plugins.java.api.tree.NewArrayTree;
 import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.VariableTree;
 
 
 @Rule(key = "S6856")
 public class PathVariableAnnotationShouldBePresentIfPathVariableIsUsedCheck extends IssuableSubscriptionVisitor {
+  private static final List<String> MAPPING_ANNOTATION_PATH_ARGUMENTS = List.of("path", "value");
+  private static final String PATH_VARIABLE_ANNOTATION = "org.springframework.web.bind.annotation.PathVariable";
+  private static final List<String> MAPPING_ANNOTATIONS = List.of(
+    "org.springframework.web.bind.annotation.GetMapping",
+    "org.springframework.web.bind.annotation.PostMapping",
+    "org.springframework.web.bind.annotation.PutMapping",
+    "org.springframework.web.bind.annotation.DeleteMapping");
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    // TODO: Specify the kind of nodes you want to be called to visit here.
-    return List.of();
+    return List.of(Tree.Kind.METHOD);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    MethodTree method = (MethodTree) tree;
+
+    method.modifiers()
+      .annotations()
+      .stream()
+      .filter(annotation -> MAPPING_ANNOTATIONS.stream().anyMatch(name -> annotation.symbolType().is(name)))
+      .forEach(annotation -> reportIssueOnParameters(annotation, method));
   }
-  
+
+  private void reportIssueOnParameters(AnnotationTree annotation, MethodTree method) {
+    boolean containsMap = method.parameters().stream()
+      .anyMatch(parameter -> {
+        Type type = parameter.type().symbolType();
+        boolean stringToString = type.typeArguments().stream().allMatch(typeArgument -> typeArgument.is("java.lang.String"));
+        return type.isSubtypeOf("java.util.Map") && stringToString;
+      });
+
+    if (containsMap) {
+      return;
+    }
+
+    Set<String> pathVariablesNames = method.parameters().stream()
+      .map(variable -> pathVariableName(variable))
+      .flatMap(Optional::stream)
+      .collect(Collectors.toSet());
+
+    extractPathArgumentFromMappingAnnotations(annotation)
+      .map(path -> extractPathVariables(path))
+      .map(pathVariables -> {
+        pathVariables.removeAll(pathVariablesNames);
+        return pathVariables;
+      })
+      .filter(pathVariables -> !pathVariables.isEmpty())
+      .forEach(pathVariables -> reportIssue(
+        annotation.arguments(),
+        "Bind path variable \"" + String.join("\", \"", pathVariables) + "\" to a method parameter."));
+  }
+
+  private static Set<String> extractPathVariables(String path) {
+    return List.of(path.split("/"))
+      .stream()
+      .filter(part -> part.startsWith("{") && part.endsWith("}"))
+      .map(part -> cropFirstAndLast(part))
+      .map(part -> part.split(":")[0])
+      .collect(Collectors.toSet());
+  }
+
+  private static String cropFirstAndLast(String str) {
+    if (str.length() >= 2) {
+      return str.substring(1, str.length() - 1);
+    } else {
+      return str;
+    }
+  }
+
+  private static Optional<String> pathVariableName(VariableTree parameter) {
+    return getAnnotation(parameter.modifiers(), PATH_VARIABLE_ANNOTATION)
+      .flatMap(annotation -> getArgumentNamed(annotation, "name", expression -> extractLiteral(expression))
+        .or(() -> getArgumentNamed(annotation, "value", expression -> extractLiteral(expression)))
+        .or(() -> getDefaultArgument(annotation, expression -> extractLiteral(expression)))
+        .or(() -> Optional.of(parameter.simpleName().name())));
+  }
+
+  private static Optional<AnnotationTree> getAnnotation(ModifiersTree modifiers, String annotationName) {
+    return modifiers.annotations()
+      .stream()
+      .filter(annotation -> annotation.symbolType().is(annotationName))
+      .findAny();
+  }
+
+  private static Stream<String> extractPathArgumentFromMappingAnnotations(AnnotationTree annotation){
+    Optional<Stream<String>> defaultPath = getDefaultArgument(annotation, expression -> extractLiteralOrArray(expression));
+
+    Stream<String> assignedPaths = MAPPING_ANNOTATION_PATH_ARGUMENTS.stream()
+      .map(name -> getArgumentNamed(annotation, name, expression -> extractLiteralOrArray(expression)))
+      .flatMap(Optional::stream)
+      .flatMap(Function.identity());
+
+    return Stream.concat(defaultPath.orElse(Stream.of()), assignedPaths);
+  }
+
+  private static <A> Optional<A> getDefaultArgument(AnnotationTree annotation, Function<ExpressionTree, A> readValue) {
+    return annotation.arguments().stream()
+      .filter(argument -> argument.is(Tree.Kind.STRING_LITERAL) || argument.is(Tree.Kind.ARRAY_TYPE))
+      .findFirst()
+      .map(readValue);
+  }
+
+  private static <A> Optional<A> getArgumentNamed(AnnotationTree annotation, String name, Function<ExpressionTree, A> readValue) {
+    return annotation.arguments().stream()
+      .filter(argument -> argument.is(Tree.Kind.ASSIGNMENT))
+      .map(AssignmentExpressionTree.class::cast)
+      .filter(assignment -> ((IdentifierTree) assignment.variable()).name().equals(name))
+      .findFirst()
+      .map(assignment -> readValue.apply(assignment.expression()));
+  }
+
+  private static String extractLiteral(ExpressionTree expression) {
+    LiteralTree literal = (LiteralTree) expression;
+    return LiteralUtils.trimQuotes((literal).value());
+  }
+
+  private static Stream<String> extractLiteralOrArray(ExpressionTree expression) {
+    if (expression.is(Tree.Kind.STRING_LITERAL)) {
+      return Stream.of(extractLiteral(expression));
+    }
+
+    NewArrayTree array = (NewArrayTree) expression;
+    return array.initializers().stream().map(lit -> extractLiteral(lit));
+  }
+
 }
