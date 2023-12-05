d44fbed72ef001739e82a456d50f81ded2cff43f
Irina Batinic <117161143+irina-batinic-sonarsource@users.noreply.github.com>
2023-12-05 14:37:30 +0100

    SONARJAVA-4727 S6862: Beans in "@Configuration" class should have different names (#4599)

---
+++ java-checks-test-sources/default/src/main/java/checks/ConfigurationBeanNamesCheckSample.java
@@ -0,0 +1,110 @@
+package checks;
+
+import javax.annotation.Nullable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ConfigurationBeanNamesCheckSample {
+
+  class User {
+  }
+
+  @Configuration
+  class Config1 {
+    @Bean
+    public User user() {
+      return new User();
+    }
+
+    @Bean
+    public User user(String name) { // Noncompliant [[sc=17;ec=21]] {{Rename this bean method to prevent any conflict with other beans.}}
+      return new User();
+    }
+  }
+
+  @Configuration
+  class Config2 {
+    @Bean
+    public User user() {
+      return new User();
+    }
+
+    @Bean
+    public User userWithName(String name) { // Compliant
+      return new User();
+    }
+  }
+
+  @Configuration
+  class Config3 {
+    @Bean
+    public User user() { // Compliant
+      return new User();
+    }
+  }
+
+  @Configuration
+  class Config4 {
+    @Bean
+    public User user() { // Compliant
+      return new User();
+    }
+
+    @Nullable
+    public User user(String name) {
+      return new User();
+    }
+  }
+
+  @Configuration
+  class Config5 {
+    @Bean
+    public User user() {
+      return new User();
+    }
+
+    @Bean
+    public User userWithName(String name) { // Compliant
+      return new User();
+    }
+
+    @Bean
+    public User user(String name, String password) { // Noncompliant
+      return new User();
+    }
+
+    @Bean
+    public User user(String name, String password, boolean enabled) { // Noncompliant
+      return new User();
+    }
+  }
+
+  @Configuration
+  class Config6 {
+  }
+
+  @Configuration
+  class Config7 {
+    @Bean
+    public User user1() {
+      return new User();
+    }
+
+    @Bean
+    public User user1(String name) { // Noncompliant
+      return new User();
+    }
+
+    @Bean
+    public User user2() {
+      return new User();
+    }
+
+    @Bean
+    public User user2(String name) { // Noncompliant
+      return new User();
+    }
+
+  }
+
+}
