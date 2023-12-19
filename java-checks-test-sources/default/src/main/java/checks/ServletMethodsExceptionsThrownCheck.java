2983751b7e553560555a38755b6b1f45760b8e6a
Angelo Buono <angelo.buono@sonarsource.com>
2023-12-19 11:57:07 +0100

    Update ServletMethodsExceptionsThrownCheck

--- java-checks-test-sources/default/src/main/java/checks/ServletMethodsExceptionsThrownCheck.java
+++ java-checks-test-sources/default/src/main/java/checks/ServletMethodsExceptionsThrownCheck.java
@@ -14,7 +14,7 @@ class ServletMethodsExceptionsThrownCheck extends HttpServlet {
 
   private static boolean staticMethod() { return true; }
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String ip = request.getRemoteAddr();
     InetAddress addr = InetAddress.getByName(ip); // Noncompliant [[sc=36;ec=45]] {{Handle the following exception that could be thrown by "getByName": UnknownHostException.}}
     try {
@@ -32,7 +32,7 @@ class ServletMethodsExceptionsThrownCheck extends HttpServlet {
     InetAddress addr = InetAddress.getByName(ip);
   }
 
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
     throw new IllegalStateException("bla"); // Noncompliant {{Handle the "IllegalStateException" thrown here in a "try/catch" block.}}
   }
   public void bar(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -44,6 +44,47 @@ class ServletMethodsExceptionsThrownCheck extends HttpServlet {
       foo(request, response); // Noncompliant [[sc=7;ec=10]] {{Handle the following exceptions that could be thrown by "foo": IOException, ServletException.}}
     } catch (NamingException ne) {
       throw new ServletException(ne); // Noncompliant {{Handle the "ServletException" thrown here in a "try/catch" block.}}
+    }
+  }
+}
+
+
+class JakartaServletMethodsExceptionsThrownCheck extends jakarta.servlet.http.HttpServlet {
+
+  private static boolean var = staticMethod();
+
+  private static boolean staticMethod() { return true; }
+
+  public void doGet(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws IOException {
+    String ip = request.getRemoteAddr();
+    InetAddress addr = InetAddress.getByName(ip); // Noncompliant [[sc=36;ec=45]] {{Handle the following exception that could be thrown by "getByName": UnknownHostException.}}
+    try {
+      addr = InetAddress.getByName(ip);
+    } catch (IllegalArgumentException e) {
+      throw e; // Noncompliant [[sc=7;ec=15]] {{Handle the "IllegalArgumentException" thrown here in a "try/catch" block.}}
+    } catch (Exception e) {
+      throw e; // Noncompliant {{Handle the "Exception" thrown here in a "try/catch" block.}}
+    }
+    staticMethod();
+  }
+
+  public void foo(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws IOException, jakarta.servlet.ServletException, NamingException {
+    String ip = request.getRemoteAddr();
+    InetAddress addr = InetAddress.getByName(ip);
+  }
+
+  public void doPost(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) {
+    throw new IllegalStateException("bla"); // Noncompliant {{Handle the "IllegalStateException" thrown here in a "try/catch" block.}}
+  }
+  public void bar(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws IOException, jakarta.servlet.ServletException {
+    throw new IllegalStateException("bla");
+  }
+
+  protected void doPut(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws jakarta.servlet.ServletException, IOException {
+    try {
+      foo(request, response); // Noncompliant [[sc=7;ec=10]] {{Handle the following exceptions that could be thrown by "foo": IOException, ServletException.}}
+    } catch (NamingException ne) {
+      throw new jakarta.servlet.ServletException(ne); // Noncompliant {{Handle the "ServletException" thrown here in a "try/catch" block.}}
     }
   }
 }
