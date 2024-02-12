8befbbc5acfc7b597bf6f7678da2f1bb7591c576
Johann Beleites <sdefend_9jftz8pq@esnail.de>
2024-02-12 12:01:50 +0100

    SONARJAVA-4839 Add S6904

---
+++ java-checks-test-sources/default/src/main/java/checks/JpaEagerFetchTypeCheckSample.java
@@ -0,0 +1,5 @@
+package checks;
+
+public class JpaEagerFetchTypeCheckSample {
+  // TODO: Implement the sample class
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/JpaEagerFetchTypeCheck.java
@@ -0,0 +1,42 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.Tree;
+
+
+@Rule(key = "S6904")
+public class JpaEagerFetchTypeCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    // TODO: Specify the kind of nodes you want to be called to visit here.
+    return List.of();
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+  
+}
