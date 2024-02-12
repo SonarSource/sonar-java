979ab2907571a16d00582238c0fef728f59c20ed
Johann Beleites <sdefend_9jftz8pq@esnail.de>
2024-02-12 14:58:38 +0100

    Rule implementation

--- java-checks-test-sources/default/src/main/java/checks/JpaEagerFetchTypeCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/JpaEagerFetchTypeCheckSample.java
@@ -1,5 +1,64 @@
 package checks;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.util.List;
+import static jakarta.persistence.FetchType.EAGER;
+
 public class JpaEagerFetchTypeCheckSample {
-  // TODO: Implement the sample class
+  private enum CustomFetchType {
+    EAGER, LAZY
+  }
+
+  @Target(ElementType.FIELD)
+  private @interface CustomAnnotation {
+    FetchType value() default FetchType.LAZY;
+    CustomFetchType fetch() default CustomFetchType.LAZY;
+  }
+
+  @Basic(fetch = FetchType.EAGER) // Noncompliant [[sc=28;ec=33]] {{Use lazy fetching instead.}}
+  private String foo;
+
+  @javax.persistence.Basic(fetch = javax.persistence.FetchType.EAGER) // Noncompliant
+  private String foo2;
+
+  @Basic(optional = true, fetch = FetchType.EAGER) // Noncompliant
+  private String baz;
+
+  @Basic(fetch = EAGER) // Noncompliant
+  private String baz4;
+
+  @OneToMany(fetch = FetchType.EAGER) // Noncompliant
+  private List<String> foos;
+
+  @javax.persistence.OneToMany(fetch = javax.persistence.FetchType.EAGER) // Noncompliant
+  private List<String> foos2;
+
+  @Basic(fetch = FetchType.LAZY) // Compliant
+  private String bar;
+
+  @javax.persistence.Basic(fetch = javax.persistence.FetchType.LAZY) // Compliant
+  private String bar2;
+
+  @OneToMany(fetch = FetchType.LAZY) // Compliant
+  private List<String> bars;
+
+  @javax.persistence.OneToMany(fetch = javax.persistence.FetchType.LAZY) // Compliant
+  private List<String> bars2;
+
+  @Basic // Compliant
+  private String baz2;
+
+  @OneToOne // Compliant
+  private String baz3;
+
+  @CustomAnnotation(fetch = CustomFetchType.EAGER) // Compliant
+  private String custom;
+
+  @CustomAnnotation(EAGER) // Compliant
+  private String customNoAssignment;
 }
--- java-checks/src/main/java/org/sonar/java/checks/JpaEagerFetchTypeCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/JpaEagerFetchTypeCheck.java
@@ -20,23 +20,61 @@
 package org.sonar.java.checks;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.AnnotationTree;
+import org.sonar.plugins.java.api.tree.AssignmentExpressionTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.Tree;
-
 
 @Rule(key = "S6904")
 public class JpaEagerFetchTypeCheck extends IssuableSubscriptionVisitor {
 
+  private static final Set<String> FETCH_TYPE_ENUMS = Set.of(
+    "jakarta.persistence.FetchType",
+    "javax.persistence.FetchType");
+
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    // TODO: Specify the kind of nodes you want to be called to visit here.
-    return List.of();
+    return List.of(Tree.Kind.ANNOTATION);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    var at = (AnnotationTree) tree;
+
+    // Report an issue if any argument in the annotation is FetchType.EAGER
+    at.arguments().stream()
+      .map(JpaEagerFetchTypeCheck::getEagerArgument)
+      .filter(Objects::nonNull)
+      .findAny()
+      .ifPresent(arg -> reportIssue(arg, "Use lazy fetching instead."));
   }
-  
+
+  @Nullable
+  private static IdentifierTree getEagerArgument(ExpressionTree tree) {
+    if (tree.is(Tree.Kind.ASSIGNMENT)) {
+      var assignedExpr = ((AssignmentExpressionTree) tree).expression();
+      IdentifierTree fetchType;
+      if (assignedExpr.is(Tree.Kind.MEMBER_SELECT)) {
+        fetchType = ((MemberSelectExpressionTree) assignedExpr).identifier();
+      } else if (assignedExpr.is(Tree.Kind.IDENTIFIER)) {
+        fetchType = (IdentifierTree) assignedExpr;
+      } else {
+        return null;
+      }
+
+      if ("EAGER".equals(fetchType.name()) &&
+        FETCH_TYPE_ENUMS.contains(assignedExpr.symbolType().fullyQualifiedName())) {
+        return fetchType;
+      }
+    }
+
+    return null;
+  }
 }
