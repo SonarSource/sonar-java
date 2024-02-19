f3c0551f3ad385962aa72cdb280d95c25902758d
Johann Beleites <sdefend_9jftz8pq@esnail.de>
2024-02-19 10:55:46 +0100

    Small improvements

--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/MetadataTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/MetadataTest.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.plugins.java;
 
 import com.google.common.base.Charsets;
@@ -18,7 +37,7 @@ import org.junit.jupiter.api.Test;
 public class MetadataTest {
   @Test
   void ensure_sane_Sonar_way_profile() throws FileNotFoundException {
-    var profilePath = Path.of("src/main/resources/org/sonar/l10n/java/rules/java/Sonar_way_profile.json");
+    var profilePath = Path.of("src/main/resources/" + JavaSonarWayProfile.SONAR_WAY_PATH);
     var reader = Files.newReader(profilePath.toFile(), Charsets.UTF_8);
 
     Gson gson = new Gson();
@@ -40,7 +59,9 @@ public class MetadataTest {
       .toList();
 
     softly.assertThat(ruleKeys)
-      .hasSameSizeAs(uniqueRuleKeys);
+      .hasSameSizeAs(uniqueRuleKeys)
+      // Sanity check for a reasonable number of rules in the quality profile:
+      .hasSizeGreaterThan(400);
 
     var stringKeys = StreamSupport.stream(ruleKeys.spliterator(), false)
       .map(JsonElement::getAsString).toList();
