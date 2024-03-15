cad7e54b65217e0bce3a65f75c9d3580f3b0252a
leonardo-pilastri-sonarsource <115481625+leonardo-pilastri-sonarsource@users.noreply.github.com>
2024-03-15 15:02:48 +0100

    SONARJAVA-4837 Implement rule S6878 Use record pattern instead of explicit field access (#4706)

---
+++ java-checks-test-sources/default/src/main/java/checks/RecordPatternInsteadOfFieldAccessCheckSample.java
@@ -0,0 +1,138 @@
+package checks;
+
+import java.util.Objects;
+
+public class RecordPatternInsteadOfFieldAccessCheckSample {
+
+  int sameComponentAccessTwice(Object obj){
+    if (obj instanceof Point p) { // Compliant; not all record components are used
+      return p.x() + p.x();
+    }
+    return 0;
+  }
+
+  int notAComponentAccessTwice(Object obj){
+    if (obj instanceof Point p) { // Compliant; not all record components are used
+      return p.x() + p.notAComponent();
+    }
+    return 0;
+  }
+
+  int allComponentsPlusAMethod(Object obj){
+    if (obj instanceof Point p) { // Noncompliant
+      return p.x() + p.y() + p.notAComponent();
+    }
+    return 0;
+  }
+
+  int nonCompliant(Object obj) {
+    if (obj instanceof Point p) { // Noncompliant [[sc=24;ec=31;secondary=+1,+2]] {{Use the record pattern instead of this pattern match variable.}}
+      int x = p.x();
+      int y = p.y();
+      return x + y;
+    } else if (obj instanceof String s) {
+      return s.length();
+    }
+    return 0;
+  }
+
+  int compliantSinceOnly1MemberUsed(Object obj) {
+    if (obj instanceof Point p) {
+      return p.x();
+    } else if (obj instanceof String s) {
+      return s.length();
+    }
+    return 0;
+  }
+
+  int nonCompliantSwitch(Object o) {
+    return switch (o) {
+      case Point p -> p.x() + p.y(); // Noncompliant [[secondary=+0,+0]]
+      default -> 0;
+    };
+  }
+
+  int nonCompliantSwitchOnLine(Object obj) {
+    if (obj instanceof Line line) { // Noncompliant [[secondary=+1,+2]]
+      int x = line.start().x();
+      int y = line.end().y();
+      return x + y;
+    }
+    return 0;
+  }
+
+  int compliantSwitch(Object o) {
+    return switch (o) {
+      case Point p -> {
+        if (Objects.equals(p.toString(), "")) {
+          yield -1;
+        }
+        int x = p.x();
+        int y = p.y();
+        yield x + y;
+      }
+      default -> 0;
+    };
+  }
+
+  int compliant(Object obj) {
+    if (obj instanceof Point(int x, int y)) {
+      return x + y;
+    }
+    return 0;
+  }
+
+  int compliantSwitch2(Object o) {
+    return switch (o) {
+      case Point(int x, int y) -> x + y;
+      default -> 0;
+    };
+  }
+
+  int compliantBecausePIsUsed(Object obj, Point a) {
+    if (obj instanceof Point p) {
+      if (p == a) {
+        return -1;
+      }
+      int x = p.x(); // Compliant
+      int y = p.y(); // Compliant
+      return x + y;
+    }
+    return 0;
+  }
+
+  int compliantBecauseHashCodeIsUsed(Object obj) {
+    if (obj instanceof Line line) {
+      if (line.hashCode() < 1) {
+        return -1;
+      }
+      int x = line.start().x();
+      int y = line.end().y();
+      return x + y;
+    }
+    return 0;
+  }
+
+  int compliantSwitchOnOtherTypePattern(Object o) {
+    return switch (o) {
+      case String s -> s.length();
+      default -> 0;
+    };
+  }
+
+  record Point(int x, int y) {
+    public int notAComponent() {
+      return 0;
+    }
+  }
+
+  record Line(Point start, Point end) {
+    @Override
+    public int hashCode() {
+      return 0;
+    }
+  }
+
+  record Shape(int myInt){}
+
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/RecordPatternInsteadOfFieldAccessCheck.java
@@ -0,0 +1,124 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.JavaVersion;
+import org.sonar.plugins.java.api.JavaVersionAwareVisitor;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.CaseLabelTree;
+import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
+import org.sonar.plugins.java.api.tree.PatternInstanceOfTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.TypePatternTree;
+import org.sonar.plugins.java.api.tree.VariableTree;
+
+
+@Rule(key = "S6878")
+public class RecordPatternInsteadOfFieldAccessCheck extends IssuableSubscriptionVisitor implements JavaVersionAwareVisitor {
+
+  private static final List<String> ALLOWED_METHODS = List.of("toString", "hashCode", "equals");
+
+  @Override
+  public boolean isCompatibleWithJavaVersion(JavaVersion version) {
+    return version.isJava21Compatible();
+  }
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.PATTERN_INSTANCE_OF, Tree.Kind.CASE_LABEL);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    if (tree instanceof PatternInstanceOfTree instanceOf) {
+      var pattern = instanceOf.pattern();
+      if (pattern instanceof TypePatternTree typePattern && isRecordPattern(typePattern)) {
+        checkTypePatternVariableUsage(typePattern.patternVariable());
+      }
+    } else {
+      checkCaseLabel((CaseLabelTree) tree);
+    }
+  }
+
+  private void checkCaseLabel(CaseLabelTree caseLabel) {
+    var typePattern = getRecordTypePatternFromCaseGroup(caseLabel);
+    typePattern.ifPresent(typePatternTree -> checkTypePatternVariableUsage(typePatternTree.patternVariable()));
+  }
+
+  private static Optional<TypePatternTree> getRecordTypePatternFromCaseGroup(CaseLabelTree caseLabel) {
+    if (caseLabel.expressions().size() == 1
+      && caseLabel.expressions().get(0) instanceof TypePatternTree typePattern
+      && isRecordPattern(typePattern)) {
+      return Optional.of(typePattern);
+    }
+    return Optional.empty();
+  }
+
+  private void checkTypePatternVariableUsage(VariableTree patternVariable) {
+    var secondaryLocationsTrees = new HashSet<MemberSelectExpressionTree>();
+    var type = patternVariable.symbol().type().symbol();
+    var comps = recordComponentNames(type);
+    for (Tree usage : patternVariable.symbol().usages()) {
+      if (usage.parent() instanceof MemberSelectExpressionTree mse && isNotRecordGetter(mse)) {
+        secondaryLocationsTrees.add(mse);
+      } else {
+        return;
+      }
+    }
+    // only if all the records components are used we report an issue
+    if (secondaryLocationsTrees.stream().map(mse -> mse.identifier().name()).toList().containsAll(comps)) {
+      reportIssue(patternVariable, "Use the record pattern instead of this pattern match variable.",
+        getSecondaryLocations(secondaryLocationsTrees), null);
+    }
+  }
+
+  private static boolean isNotRecordGetter(MemberSelectExpressionTree mse) {
+    return !ALLOWED_METHODS.contains(mse.identifier().name());
+  }
+
+  private static List<JavaFileScannerContext.Location> getSecondaryLocations(Set<MemberSelectExpressionTree> secondaryLocationsTrees) {
+    return secondaryLocationsTrees.stream()
+      .map(tree ->
+        new JavaFileScannerContext.Location("Replace this getter with the respective record pattern component", tree))
+      .toList();
+  }
+
+  private static boolean isRecordPattern(TypePatternTree typePattern) {
+    return typePattern.patternVariable().type().symbolType().isSubtypeOf("java.lang.Record");
+  }
+
+  private static List<String> recordComponentNames(Symbol.TypeSymbol recordSymbol) {
+    return recordSymbol
+      .memberSymbols()
+      .stream()
+      .filter(Symbol::isVariableSymbol)
+      .map(Symbol.VariableSymbol.class::cast)
+      .map(Symbol.VariableSymbol::name)
+      .toList();
+  }
+
+}
