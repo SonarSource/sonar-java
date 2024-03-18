2f272fea0a47fc0c087d57c35a0de06ca930580d
valentin.aebi <valentin.aebi@sonarsource.com>
2024-03-18 15:33:59 +0100

    More rigorous implementation without quickfix

--- java-checks/src/main/java/org/sonar/java/checks/PatternMatchUsingIfCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/PatternMatchUsingIfCheck.java
@@ -25,9 +25,6 @@ import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.helpers.QuickFixHelper;
-import org.sonar.java.model.expression.InstanceOfTreeImpl;
-import org.sonar.java.reporting.JavaQuickFix;
-import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.JavaVersion;
 import org.sonar.plugins.java.api.JavaVersionAwareVisitor;
@@ -35,17 +32,15 @@ import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.IdentifierTree;
 import org.sonar.plugins.java.api.tree.IfStatementTree;
+import org.sonar.plugins.java.api.tree.PatternInstanceOfTree;
+import org.sonar.plugins.java.api.tree.PatternTree;
 import org.sonar.plugins.java.api.tree.Tree;
-
-
-// FIXME use InstanceOfTreeImpl?
 
 
 @Rule(key = "S6880")
 public class PatternMatchUsingIfCheck extends IssuableSubscriptionVisitor implements JavaVersionAwareVisitor {
 
   private static final String ISSUE_MESSAGE = "Replace the chain of if/else with a switch expression.";
-  private static final int INDENT = 2;
 
   @Override
   public boolean isCompatibleWithJavaVersion(JavaVersion version) {
@@ -59,113 +54,112 @@ public class PatternMatchUsingIfCheck extends IssuableSubscriptionVisitor implem
 
   @Override
   public void visitNode(Tree tree) {
-    var ifStat = (IfStatementTree) tree;
+    var topLevelIfStat = (IfStatementTree) tree;
 
     // Do not report errors more than once for the same chain of ifs
-    if (isElseIf(ifStat)) {
+    if (isElseIf(topLevelIfStat) || !(topLevelIfStat.elseStatement() instanceof IfStatementTree)) {
       return;
     }
 
-    if (isFaultyIfSequence(ifStat)){
-      QuickFixHelper.newIssue(context).forRule(this)
-        .onTree(ifStat.ifKeyword())
-        .withMessage(ISSUE_MESSAGE)
-        .withQuickFix(() -> computeQuickFix(ifStat))
-        .report();
+    // Optimization
+    var conditionKind = topLevelIfStat.condition().kind();
+    if (conditionKind != Tree.Kind.PATTERN_INSTANCE_OF && conditionKind != Tree.Kind.EQUAL_TO
+      && conditionKind != Tree.Kind.CONDITIONAL_AND && conditionKind != Tree.Kind.CONDITIONAL_OR) {
+      return;
     }
+
+    var cases = extractCasesFromIf(topLevelIfStat);
+    if (cases == null || !casesHaveCommonScrutinee(cases)) {
+      return;
+    }
+
+    QuickFixHelper.newIssue(context).forRule(this)
+      .onTree(topLevelIfStat.ifKeyword())
+      .withMessage(ISSUE_MESSAGE)
+      .withQuickFix(() -> null) // FIXME
+      .report();
   }
 
-  private static boolean isFaultyIfSequence(IfStatementTree topLevelIfStat){
-    var elseStat = topLevelIfStat.elseStatement();
-    if (!(elseStat instanceof IfStatementTree)){
-      return false;
-    }
-    var targetVarName = findScrutineeInCondition(topLevelIfStat.condition());
-    if (targetVarName == null){
-      return false;
-    }
-    for (Tree tree = elseStat; tree instanceof IfStatementTree elseIfStat; tree = elseIfStat.elseStatement()){
-      if (!(elseIfStat.condition() instanceof InstanceOfTreeImpl instOf && instOf.expression() instanceof IdentifierTree idTree
-        && idTree.name().equals(targetVarName))){
+  /**
+   * Precondition: cases is not empty
+   */
+  private static boolean casesHaveCommonScrutinee(List<Case> cases) {
+    var iter = cases.iterator();
+    var scrutinee = iter.next().scrutinee();
+    while (iter.hasNext()) {
+      if (!iter.next().scrutinee().equals(scrutinee)) {
         return false;
       }
     }
     return true;
   }
 
-  private static @Nullable String findScrutineeInCondition(ExpressionTree condition){
+  private static @Nullable List<Case> extractCasesFromIf(IfStatementTree topLevelIfStat) {
+    var cases = new LinkedList<Case>();
+    for (Tree tree = topLevelIfStat; tree instanceof IfStatementTree ifStat; tree = ifStat.elseStatement()) {
+      var caze = convertToCase(ifStat.condition());
+      if (caze == null) {
+        return null;
+      }
+      cases.add(caze);
+    }
+    return cases;
+  }
+
+  private static @Nullable Case convertToCase(ExpressionTree condition) {
     var leftmost = findLeftmostInConjunction(condition);
-    if (leftmost instanceof InstanceOfTreeImpl instOf && instOf.expression() instanceof IdentifierTree idTree){
-      return idTree.name();
+    var guards = new LinkedList<ExpressionTree>();
+    populateGuardsList(condition, guards);
+    if (leftmost instanceof PatternInstanceOfTree patInstOf && patInstOf.pattern() != null && patInstOf.expression() instanceof IdentifierTree idTree) {
+      return new PatternMatchCase(idTree.name(), patInstOf.pattern(), guards);
+    } else if (leftmost.kind() == Tree.Kind.CONDITIONAL_OR || leftmost.kind() == Tree.Kind.EQUAL_TO) {
+      return extractVarAndConstantsFromEqualityChecksDisjunction(leftmost, guards);
     } else {
       return null;
     }
   }
 
-  private static ExpressionTree findLeftmostInConjunction(ExpressionTree expr) {
-    if (expr instanceof BinaryExpressionTree binary) {
-      return findLeftmostInConjunction(binary.leftOperand());
+  private static @Nullable EqualityCase extractVarAndConstantsFromEqualityChecksDisjunction(ExpressionTree expr,
+                                                                                            List<ExpressionTree> guards) {
+    var constantsList = new LinkedList<ExpressionTree>();
+    String scrutinee = null;
+    while (expr.kind() == Tree.Kind.CONDITIONAL_OR) {
+      var binary = (BinaryExpressionTree) expr;
+      var varAndCst = extractVarAndConstFromEqualityCheck(binary.rightOperand());
+      if (varAndCst == null) {
+        return null;
+      } else if (scrutinee == null) {
+        scrutinee = varAndCst.a;
+      } else if (!varAndCst.a.equals(scrutinee)) {
+        return null;
+      }
+      constantsList.add(varAndCst.b);
+      expr = binary.leftOperand();
     }
-    return expr;
+    var varAndCst = extractVarAndConstFromEqualityCheck(expr);
+    if (varAndCst == null || (scrutinee != null && !varAndCst.a.equals(scrutinee))) {
+      return null;
+    }
+    constantsList.add(varAndCst.b);
+    return new EqualityCase(scrutinee == null ? varAndCst.a : scrutinee, constantsList, guards);
   }
 
-  private static boolean isElseIf(IfStatementTree ifStat) {
-    return ifStat.parent() instanceof IfStatementTree parentIf && parentIf.elseStatement() == ifStat;
-  }
-
-  private JavaQuickFix computeQuickFix(IfStatementTree topLevelIfStat){
-    var baseIndent = topLevelIfStat.firstToken().range().start().column() - 1;
-    var targetName = scrutineeVarName(findLeftmostInConjunction(topLevelIfStat.condition()));
-    var sb = new StringBuilder();
-    sb.append("switch (").append(targetName).append(") {\n");
-    Tree tree;
-    for (tree = topLevelIfStat; tree instanceof IfStatementTree ifStat; tree = ifStat.elseStatement()){
-      sb.append(makeCase(ifStat, baseIndent).indent(INDENT));
-    }
-    if (tree != null){
-      sb.append(makeDefault(tree, baseIndent).indent(INDENT));
-    }
-    sb.append("}");
-    var quickFixBuilder = JavaQuickFix.newQuickFix(ISSUE_MESSAGE);
-    quickFixBuilder.addTextEdit(JavaTextEdit.replaceTree(topLevelIfStat, sb.toString()));
-    return quickFixBuilder.build();
-  }
-
-  private String makeCase(IfStatementTree ifStat, int baseIndent){
-    var instOf = (InstanceOfTreeImpl) findLeftmostInConjunction(ifStat.condition());
-    var sb = new StringBuilder();
-    sb.append(" ".repeat(baseIndent)).append("case ");
-    var pattern = instOf.pattern();
-    if (pattern == null){
-      sb.append(QuickFixHelper.contentForTree(instOf.type(), context)).append(" ignored");
-    } else {
-      sb.append(QuickFixHelper.contentForTree(pattern, context));
-    }
-    var guardsList = new LinkedList<ExpressionTree>();
-    populateGuardsList(ifStat.condition(), guardsList);
-    if (!guardsList.isEmpty()){
-      var guardsIter = guardsList.iterator();
-      while (guardsIter.hasNext()){
-        sb.append(QuickFixHelper.contentForTree(guardsIter.next(), context));
-        if (guardsIter.hasNext()){
-          sb.append(" && ");
-        }
+  private static @Nullable Pair<String, ExpressionTree> extractVarAndConstFromEqualityCheck(ExpressionTree expr) {
+    if (expr.kind() == Tree.Kind.EQUAL_TO) {
+      var binary = (BinaryExpressionTree) expr;
+      if (binary.leftOperand() instanceof IdentifierTree idTree
+        && (binary.rightOperand().asConstant().isPresent() || binary.rightOperand().symbolType().symbol().isEnum())) {
+        return new Pair<>(idTree.name(), binary.rightOperand());
       }
     }
-    sb.append(" -> ").append(QuickFixHelper.contentForTree(ifStat.thenStatement(), context));
-    return sb.toString();
+    return null;
   }
 
-  private String makeDefault(Tree defaultCaseBody, int baseIndent){
-    var sb = new StringBuilder();
-    sb.append(" ".repeat(baseIndent)).append("default -> ").append(QuickFixHelper.contentForTree(defaultCaseBody, context));
-    return sb.toString();
-  }
-
-  private static String scrutineeVarName(ExpressionTree instanceOfExpr){
-    var instOf = (InstanceOfTreeImpl) instanceOfExpr;
-    var idTree = ((IdentifierTree) instOf.expression());
-    return idTree.name();
+  private static ExpressionTree findLeftmostInConjunction(ExpressionTree expr) {
+    while (expr.kind() == Tree.Kind.CONDITIONAL_AND) {
+      expr = ((BinaryExpressionTree) expr).leftOperand();
+    }
+    return expr;
   }
 
   private static void populateGuardsList(ExpressionTree expr, Deque<ExpressionTree> guards) {
@@ -173,6 +167,25 @@ public class PatternMatchUsingIfCheck extends IssuableSubscriptionVisitor implem
       guards.addFirst(binary.rightOperand());
       populateGuardsList(binary.leftOperand(), guards);
     }
+  }
+
+  private static boolean isElseIf(IfStatementTree ifStat) {
+    return ifStat.parent() instanceof IfStatementTree parentIf && parentIf.elseStatement() == ifStat;
+  }
+
+  private sealed interface Case permits PatternMatchCase, EqualityCase {
+    String scrutinee();
+
+    List<ExpressionTree> guards();
+  }
+
+  private record PatternMatchCase(String scrutinee, PatternTree pattern, List<ExpressionTree> guards) implements Case {
+  }
+
+  private record EqualityCase(String scrutinee, List<ExpressionTree> constants, List<ExpressionTree> guards) implements Case {
+  }
+
+  private record Pair<A, B>(A a, B b) {
   }
 
 }
