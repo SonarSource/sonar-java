353d220fa39b67abbe35cea693ad763a4d597d81
Marco Kaufmann <marco.kaufmann@sonarsource.com>
2024-03-20 16:03:52 +0100

    Removed unused import

--- java-checks/src/main/java/org/sonar/java/checks/ThreadAsRunnableArgumentCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/ThreadAsRunnableArgumentCheck.java
@@ -21,7 +21,6 @@ package org.sonar.java.checks;
 
 import java.text.MessageFormat;
 import java.util.List;
-import java.util.Objects;
 import org.sonar.check.Rule;
 import org.sonar.java.model.ExpressionUtils;
 import org.sonar.plugins.java.api.JavaFileScanner;
