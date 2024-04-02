086bfadf5c3e7a7d05077e60f967a36e46425888
Irina Batinic <irina.batinic@sonarsource.com>
2024-04-02 16:49:10 +0200

    Add more test cases and update autoscan

--- java-checks-test-sources/spring-3.2/src/main/java/checks/SQLInjectionCheckSample.java
+++ java-checks-test-sources/spring-3.2/src/main/java/checks/SQLInjectionCheckSample.java
@@ -1,42 +1,23 @@
 package checks;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.RowMapperResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class SQLInjectionCheckSample {
 
-  private final JdbcTemplate jdbcTemplate;
-  private final JdbcOperations jdbcOperations;
-  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-  private final JdbcClient jdbcClient;
-
-  @Autowired
-  public SQLInjectionCheckSample(JdbcTemplate jdbcTemplate,
-    JdbcOperations jdbcOperations,
-    NamedParameterJdbcTemplate namedParameterJdbcTemplate,
-    JdbcClient jdbcClient) {
-    this.jdbcTemplate = jdbcTemplate;
-    this.jdbcOperations = jdbcOperations;
-    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    this.jdbcClient = jdbcClient;
-  }
-
-
-  @GetMapping("/S2077")
-  public String s2077(@RequestParam String input) {
-    RowMapper<String> rowMapper = (rs, rowNum) -> null;
-    PreparedStatementCallback<String> preparedStatementCallback = ps -> null;
+  public void s2077(String input, JdbcTemplate jdbcTemplate, JdbcOperations jdbcOperations,
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate, JdbcClient jdbcClient, RowMapper<String> rowMapper,
+    PreparedStatementCallback<String> preparedStatementCallback) {
 
     jdbcTemplate.execute("SELECT " + input); // Noncompliant
     jdbcTemplate.queryForStream("SELECT " + input, rowMapper); // Noncompliant
@@ -44,17 +25,45 @@ public class SQLInjectionCheckSample {
     jdbcOperations.execute("SELECT " + input); // Noncompliant
     jdbcOperations.queryForStream("SELECT " + input, rowMapper); // Noncompliant
 
+    namedParameterJdbcTemplate.batchUpdate("SELECT " + input, new SqlParameterSource[1]); // Noncompliant
     namedParameterJdbcTemplate.execute("SELECT " + input, preparedStatementCallback); // Noncompliant
+    namedParameterJdbcTemplate.query("SELECT " + input, new BeanPropertySqlParameterSource(new Object()), new RowMapperResultSetExtractor(rowMapper)); // Noncompliant
+    namedParameterJdbcTemplate.queryForList("SELECT " + input, EmptySqlParameterSource.INSTANCE); // Noncompliant
+    namedParameterJdbcTemplate.queryForMap("SELECT " + input, EmptySqlParameterSource.INSTANCE); // Noncompliant
+    namedParameterJdbcTemplate.queryForObject("SELECT " + input, EmptySqlParameterSource.INSTANCE, rowMapper); // Noncompliant
+    namedParameterJdbcTemplate.queryForRowSet("SELECT " + input, EmptySqlParameterSource.INSTANCE); // Noncompliant
+    namedParameterJdbcTemplate.queryForStream("SELECT " + input, EmptySqlParameterSource.INSTANCE, rowMapper); // Noncompliant
+    namedParameterJdbcTemplate.update("SELECT " + input, EmptySqlParameterSource.INSTANCE); // Noncompliant
+
 
     JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
     jdbcDao.setAuthoritiesByUsernameQuery("SELECT " + input); // Noncompliant
+    jdbcDao.setGroupAuthoritiesByUsernameQuery("SELECT " + input); // Noncompliant
+    jdbcDao.setUsersByUsernameQuery("SELECT " + input); // Noncompliant
 
     JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+    jdbcUserDetailsManager.setChangePasswordSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setCreateAuthoritySql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setCreateUserSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteGroupAuthoritiesSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteGroupAuthoritySql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteGroupMemberSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteGroupMembersSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteGroupSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteUserAuthoritiesSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setDeleteUserSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setFindAllGroupsSql("SELECT " + input); // Noncompliant
     jdbcUserDetailsManager.setFindGroupIdSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setFindUsersInGroupSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setGroupAuthoritiesSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setInsertGroupAuthoritySql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setInsertGroupMemberSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setInsertGroupSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setRenameGroupSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setUpdateUserSql("SELECT " + input); // Noncompliant
+    jdbcUserDetailsManager.setUserExistsSql("SELECT " + input); // Noncompliant
 
     jdbcClient.sql("SELECT " + input).query(); // Noncompliant
-
-    return "Done";
   }
 
 }
