46ae6aa468c7789495ee5835a90201809b3a92f5
Johann Beleites <sdefend_9jftz8pq@esnail.de>
2024-04-10 12:57:02 +0000

    Workaround: remove MockingAllMethodsCheck from renaming batch

--- java-checks-test-sources/default/src/test/java/checks/tests/MockingAllMethodsCheckSample_Helper.java
+++
@@ -1,7 +0,0 @@
-package checks.tests;
-
-import static org.mockito.Mockito.*;
-
-public class MockingAllMethodsCheckSample_Helper {
-  public static MockingAllMethodsCheckSample.MyClass4 staticMockedObject = mock(MockingAllMethodsCheckSample.MyClass4.class);
-}
---
+++ java-checks-test-sources/default/src/test/java/checks/tests/MockingAllMethodsCheck_Helper.java
@@ -0,0 +1,7 @@
+package checks.tests;
+
+import static org.mockito.Mockito.*;
+
+public class MockingAllMethodsCheck_Helper {
+  public static MockingAllMethodsCheck.MyClass4 staticMockedObject = mock(MockingAllMethodsCheck.MyClass4.class);
+}
