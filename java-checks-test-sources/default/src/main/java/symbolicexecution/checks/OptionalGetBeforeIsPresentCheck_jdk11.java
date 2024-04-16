cd50cb6e0d07bef685b6a6b96be000c1e6bfb8eb
valentin.aebi <valentin.aebi@sonarsource.com>
2024-04-16 10:03:45 +0200

    SONARJAVA-4520 Rule S3655: False Positive with JUnit assertions

--- java-checks-test-sources/default/src/main/java/symbolicexecution/checks/OptionalGetBeforeIsPresentCheck_jdk11.java
+++ java-checks-test-sources/default/src/main/java/symbolicexecution/checks/OptionalGetBeforeIsPresentCheck_jdk11.java
@@ -1,6 +1,8 @@
 package symbolicexecution.checks;
 
 import java.util.Optional;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 abstract class OptionalGetBeforeIsPresentCheck_jdk11 {
 
@@ -49,4 +51,105 @@ abstract class OptionalGetBeforeIsPresentCheck_jdk11 {
     }
     op.get(); // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
   }
+
+  void test_junit5_1(Optional<String> op){
+    Assertions.assertTrue(op.isPresent(), "Hello");
+    op.get();  // Compliant
+  }
+
+  void test_junit5_2(Optional<String> op){
+    Assertions.assertTrue(op.isEmpty(), "Hello");
+    op.get();  // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit5_3(Optional<String> op){
+    Assertions.assertFalse(op.isPresent());
+    op.get();   // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit5_4(Optional<String> op){
+    Assertions.assertFalse(op.isEmpty());
+    op.get(); // Compliant
+  }
+
+  void test_junit5_5(Optional<String> op, int i){
+    Assertions.assertFalse(i < 0);
+    op.get();   // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit5_6(Optional<String> op){
+    Assertions.assertTrue(op.isPresent(), () -> "Hello");
+    op.get();  // Compliant
+  }
+
+  void test_junit5_7(Optional<String> op){
+    Assertions.assertFalse(op.isEmpty(), () -> "Hello");
+    op.get(); // Compliant
+  }
+
+  void test_junit5_8(Optional<String> op, int i){
+    Assertions.assertFalse(i < 0, () -> "Hello");
+    op.get();   // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit5_9(Optional<String> op){
+    Assertions.fail();
+    op.get(); // Compliant
+  }
+
+  void test_junit5_10(Optional<String> op){
+    Assertions.fail("Hello");
+    op.get(); // Compliant
+  }
+
+  void test_junit5_11(Optional<String> op){
+    Assertions.fail("Hello", new Exception());
+    op.get(); // Compliant
+  }
+
+  void test_junit5_12(Optional<String> op){
+    Assertions.fail(new Exception());
+    op.get(); // Compliant
+  }
+
+  void test_junit5_13(Optional<String> op){
+    Assertions.fail(() -> "Hello");
+    op.get(); // Compliant
+  }
+
+  void test_junit4_1(Optional<String> op){
+    Assert.assertTrue("Hello", op.isPresent());
+    op.get();  // Compliant
+  }
+
+  void test_junit4_2(Optional<String> op){
+    Assert.assertTrue("Hello", op.isEmpty());
+    op.get();  // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit4_3(Optional<String> op){
+    Assert.assertFalse(op.isPresent());
+    op.get();   // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit4_4(Optional<String> op){
+    Assert.assertFalse(op.isEmpty());
+    op.get(); // Compliant
+  }
+
+  void test_junit4_5(Optional<String> op, int i){
+    Assert.assertFalse(i < 0);
+    op.get();   // Noncompliant {{Call "op.isPresent()" or "!op.isEmpty()" before accessing the value.}}
+  }
+
+  void test_junit4_6(Optional<String> op){
+    Assert.fail();
+    op.get(); // Compliant
+  }
+
+  void test_junit4_7(Optional<String> op){
+    Assert.fail("Hello");
+    op.get(); // Compliant
+  }
+
 }
