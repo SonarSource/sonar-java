7ceef99728f174a2c57d88cfc5bc31b59927a820
Irina Batinic <irina.batinic@sonarsource.com>
2024-04-30 09:28:39 +0200

    Fix StaticImportCountCheckTest

--- java-checks/src/test/files/checks/StaticImportCountCheck/MixedStandardAndStaticImportsCustomThreshold.java
+++ java-checks/src/test/files/checks/StaticImportCountCheck/MixedStandardAndStaticImportsCustomThreshold.java
@@ -2,14 +2,14 @@ import java.util.Date;
 import java.util.Collections;
 import java.util.List;
 
-import static java.lang.Math.*; // Noncompliant {{Reduce the number of "static" imports in this class from 5 to the maximum allowed 3.}}
-//^[sc=1;ec=32]
-//  ^^^<
-import static java.util.Collections.*;
-//  ^^^<
-import static com.myco.corporate.Constants.*;
-//  ^^^<
-import static com.myco.division.Constants.*;
-//  ^^^<
-import static com.myco.department.Constants.*;
-//  ^^^<
+  import static java.lang.Math.*; // Noncompliant {{Reduce the number of "static" imports in this class from 5 to the maximum allowed 3.}}
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^@-1<
+  import static java.util.Collections.*;
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^<
+  import static com.myco.corporate.Constants.*;
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^<
+  import static com.myco.division.Constants.*;
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^<
+  import static com.myco.department.Constants.*;
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^<
--- java-checks/src/test/files/checks/StaticImportCountCheck/StaticImportCountCheck.java
+++ java-checks/src/test/files/checks/StaticImportCountCheck/StaticImportCountCheck.java
@@ -1,15 +1,15 @@
 import static java.lang.Math.*; // Noncompliant {{Reduce the number of "static" imports in this class from 7 to the maximum allowed 4.}}
-//^[sc=1;ec=32]
-//  ^^^<
+//^[sc=1;ec=31]
+//^[sc=1;ec=31]@-1<
 import static java.util.Collections.*;
-//  ^^^<
+//^[sc=1;ec=38]<
 import static com.myco.corporate.Constants.*;
-//  ^^^<
+//^[sc=1;ec=45]<
 import static com.myco.division.Constants.*;
-//  ^^^<
+//^[sc=1;ec=44]<
 import static com.myco.department.Constants.*;
-//  ^^^<
+//^[sc=1;ec=46]<
 import static com.myco.department.Constants.*;
-//  ^^^<
+//^[sc=1;ec=46]<
 import static com.myco.department.Constants.*;
-//  ^^^<
+//^[sc=1;ec=46]<
