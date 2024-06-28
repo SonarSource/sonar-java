b586d4002e5dde155e84ee9ccc0c672f26eeec91
Alban Auzeill <alban.auzeill@sonarsource.com>
2024-06-28 14:17:42 +0200

    SONARJAVA-5052 [Support Java 22] Update Eclipse Compiler for Java 3.38.0

--- java-frontend/src/main/java/org/sonar/plugins/java/api/JavaVersion.java
+++ java-frontend/src/main/java/org/sonar/plugins/java/api/JavaVersion.java
@@ -145,6 +145,14 @@ public interface JavaVersion {
   boolean isJava21Compatible();
 
   /**
+   * Test if java version of the project is greater than or equal to 22.
+   * Remark - Contrary to other isJava*Compatible methods, this one will NOT return true if version is not set
+   * @return true if java version used is >= 22
+   * @since SonarJava 8.0: Support of Java 22
+   */
+  boolean isJava22Compatible();
+
+  /**
    * get java version as integer
    * @return an int representing the java version
    */
