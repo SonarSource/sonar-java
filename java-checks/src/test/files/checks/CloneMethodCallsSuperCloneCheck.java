f7f028d693ee53b8de2a73a1fae93c7a22e1075c
Gabriel Fleischer <gabriel.fleischer@sonarsource.com>
2024-09-11 14:51:06 +0200

    SONARJAVA-5120 S1182 Super call that are not directly in the scope of the method are wrongly taken into account

--- java-checks/src/test/files/checks/CloneMethodCallsSuperCloneCheck.java
+++ java-checks/src/test/files/checks/CloneMethodCallsSuperCloneCheck.java
@@ -63,12 +63,67 @@ class G1 extends G0 {
 
 class H {
   @Override
-  protected Object clone() throws CloneNotSupportedException { // Compliant - limitation
+  protected Object clone() throws CloneNotSupportedException { // Noncompliant {{Use super.clone() to create and seed the cloned instance to be returned.}}
+//                 ^^^^^
     class H2 {
       @Override
       protected Object clone() throws CloneNotSupportedException { // Compliant
         return super.clone();
       }
     }
+  }
+}
+
+abstract class I {
+  public abstract Object clone(); // Compliant
+}
+
+// Coverage
+
+class Cov1 {
+  @Override
+  protected Object clone() throws CloneNotSupportedException { // Noncompliant {{Use super.clone() to create and seed the cloned instance to be returned.}}
+//                 ^^^^^
+    return super.toString();
+  }
+}
+
+class Cov2 {
+  @Override
+  protected Object clone() throws CloneNotSupportedException { // Noncompliant {{Use super.clone() to create and seed the cloned instance to be returned.}}
+//                 ^^^^^
+    return this.clone();
+  }
+}
+
+class Cov3 {
+  @Override
+  protected Object clone() throws CloneNotSupportedException { // Noncompliant {{Use super.clone() to create and seed the cloned instance to be returned.}}
+//                 ^^^^^
+    return new I4().clone();
+  }
+}
+
+class Cov4 {
+  @Override
+  protected Object clone() throws CloneNotSupportedException { // Noncompliant {{Use super.clone() to create and seed the cloned instance to be returned.}}
+//                 ^^^^^
+    return toString();
+  }
+}
+
+// Limitation
+class J {
+  @Override
+  protected Object clone() throws CloneNotSupportedException { // Noncompliant
+    Supplier<Object> supplier = () -> {
+      try {
+        return J.super.clone();
+      } catch (CloneNotSupportedException e) {
+        throw Throwables.propagate(e);
+      }
+    };
+
+    return supplier.get();
   }
 }
