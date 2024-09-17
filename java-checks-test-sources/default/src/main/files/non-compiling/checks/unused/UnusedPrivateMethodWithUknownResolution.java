6da4f40abe86292b041321190f8f8bcbc4490a3a
Marco Kaufmann <marco.kaufmann@sonarsource.com>
2024-09-17 16:27:24 +0200

    SONARJAVA-5058 S1144: FP when encountering nested class's private method without semantics

--- java-checks-test-sources/default/src/main/files/non-compiling/checks/unused/UnusedPrivateMethodWithUknownResolution.java
+++ java-checks-test-sources/default/src/main/files/non-compiling/checks/unused/UnusedPrivateMethodWithUknownResolution.java
@@ -1,8 +1,41 @@
 package checks.unused;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
 public class UnusedPrivateMethodWithUknownResolution {
 
   private void init(@Observes Object object) {} // Compliant
   private void init2(@UnknownAnnotation Object object) {} // Noncompliant
-  
+
+  void sonarJava5012Minimal() {
+    Z b = () -> bar();
+  }
+
+  class Bar {
+    private static void bar() {
+    }
+  }
+
+  List<ClientHttpRequestInterceptor> sonarJava5012AsReported() {
+    List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+
+    User userToAuthenticate = new User();
+
+    interceptors.add((request, body, execution) -> {
+      request.getHeaders().set(HttpHeaders.AUTHORIZATION, "Bearer " + userToAuthenticate.token());
+      return execution.execute(request, body);
+    });
+
+    return interceptors;
+  }
+
+  class User {
+    public User() {}
+    private String token() { // FP (used in line 14 as `userToAuthenticate.token()`)
+      return "123456";
+    }
+  }
 }
