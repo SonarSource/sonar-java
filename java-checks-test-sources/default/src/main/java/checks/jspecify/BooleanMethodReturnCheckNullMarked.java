98685eb095358e22a5a738e852e353a765b35f4a
Paul O'Reilly <paul.oreilly@sonarsource.com>
2024-10-01 10:04:43 +0100

    SONARJAVA-4546 Support @org.jspecify.annotations.NullMarked in existing nullability checks

---
+++ java-checks-test-sources/default/src/main/java/checks/jspecify/BooleanMethodReturnCheckNullMarked.java
@@ -0,0 +1,83 @@
+package checks.jspecify;
+
+import java.util.stream.Stream;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.NullUnmarked;
+
+@NullMarked
+class BooleanMethodReturnCheckJSpecifySampleA {
+  public Boolean myMethod() {
+    return null; // Noncompliant {{Null is returned but a "Boolean" is expected.}}
+  }
+
+  public Boolean myOtherMethod() {
+    return Boolean.TRUE; // Compliant
+  }
+
+  BooleanMethodReturnCheckJSpecifySampleA() {
+    // constructor (with null return type) are not covered by the rule
+    return;
+  }
+
+  @Nullable
+  public Boolean foo() {
+    return null; // Compliant
+  }
+
+  @NullUnmarked
+  public Boolean bar() {
+    return null; // Compliant
+  }
+}
+
+@NullMarked
+class BooleanMethodReturnCheckJSpecifySampleB {
+  private class Boolean {
+  }
+
+  public Boolean myMethodFailing() {
+    return null; // Compliant
+  }
+
+  public java.lang.Boolean myOtherMethod() {
+    class BooleanMethodReturnCheckSampleC {
+      private java.lang.Boolean myInnerMethod() {
+        return null; // Noncompliant {{Null is returned but a "Boolean" is expected.}}
+      }
+      private BooleanMethodReturnCheckSampleC foo() {
+        return null; // Compliant
+      }
+    }
+    return null; // Noncompliant {{Null is returned but a "Boolean" is expected.}}
+//         ^^^^
+  }
+
+  @CheckForNull
+  public java.lang.Boolean myMethod2() {
+    return null; // compliant method is annotated with @CheckForNull
+  }
+}
+
+@NullMarked
+class BooleanMethodReturnCheckJSpecifySampleD {
+  public Boolean foo() {
+    class BooleanMethodReturnCheckSampleE {
+      void bar() {
+        return;
+      }
+    }
+    Stream.of("A").forEach(a -> {
+      return; // Compliant
+    });
+    return true;
+  }
+}
+
+class BooleanMethodReturnCheckJSpecifySampleE {
+  @NullMarked
+  public Boolean myMethod() {
+    return null; // Noncompliant {{Null is returned but a "Boolean" is expected.}}
+  }
+}
