90b5544c9a9d4c7d5e9e655a221eb67b3ae93001
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2025-01-10 13:31:32 +0100

    imports

--- java-checks/src/main/java/org/sonar/java/checks/tests/NoTestInTestClassCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/tests/NoTestInTestClassCheck.java
@@ -20,7 +20,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
