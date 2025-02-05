710cd3e6b7f1d23c108454484174be830ef84154
erwan.serandour <erwan.serandour@sonarsource.com>
2025-02-05 12:11:00 +0100

    add check sample

--- java-checks-test-sources/default/src/main/java/checks/spring/DirtyContextShouldUseCorrectControlModeCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/spring/DirtyContextShouldUseCorrectControlModeCheckSample.java
@@ -1,5 +1,65 @@
 package checks.spring;
 
+import org.springframework.test.annotation.DirtiesContext;
+
 public class DirtyContextShouldUseCorrectControlModeCheckSample {
-  // TODO: Implement the sample class
+
+  @interface MyAnnotation{}
+
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD) // Noncompliant
+  public static class WrongContextMode {
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) // Noncompliant
+    public void wrongMode() {}
+
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void correctMode() {}
+
+
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) // Noncompliant
+    @MyAnnotation
+    public void additionalAnnotation(){}
+
+    @DirtiesContext(hierarchyMode = DirtiesContext.HierarchyMode.EXHAUSTIVE)
+    public void hierarchyMode(){}
+
+  }
+
+  @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+  public static class CorrectContextMode {
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) // Noncompliant
+    public void wrongMode() {}
+
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void correctMode() {}
+
+
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) // Noncompliant
+    @MyAnnotation
+    public void additionalAnnotation(){}
+  }
+
+  @DirtiesContext(hierarchyMode = DirtiesContext.HierarchyMode.EXHAUSTIVE)
+  public static class HierarchyMode {}
+
+  @MyAnnotation
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD) // Noncompliant
+  public class AdditonalAnnotation {}
+
+
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+  @interface MyDirtyContext {}
+
+  @MyDirtyContext  // we don't raise an issue, as I don't know if it is valid
+  // and the computing time would be too costly compared to the benefit
+  public static class MyWrongDirtyContext {}
+
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD) // Noncompliant
+  record WrongRecordContext() {
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS) // Noncompliant
+    public void wrongMode() {}
+
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void correctMode() {}
+  }
+
 }
