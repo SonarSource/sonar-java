b86370fe353de264acc415259d412be50faf501a
Alban Auzeill <alban.auzeill@sonarsource.com>
2025-02-05 16:41:22 +0100

    SONARJAVA-5273 FP for S125 for Apache 2.0 license header (#4994)

--- java-checks-test-sources/default/src/main/java/checks/CommentedCode.java
+++ java-checks-test-sources/default/src/main/java/checks/CommentedCode.java
@@ -1,4 +1,8 @@
-/* No detection of commented-out code in header
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * No detection of commented-out code in header
  * for (Visitor visitor : visitors) {
  *   continue;
  * }
