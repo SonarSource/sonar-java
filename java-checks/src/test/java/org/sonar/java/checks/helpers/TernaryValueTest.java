0fa9a95ecc8e4d255cba62d99875c38737df8c2a
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-02-26 12:08:48 +0100

    SONARJAVA-5341 Add TernaryValue enum
    To handle better the cases where something is unknown.

---
+++ java-checks/src/test/java/org/sonar/java/checks/helpers/TernaryValueTest.java
@@ -0,0 +1,32 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks.helpers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TernaryValueTest {
+
+  @Test
+  void maybeTrue() {
+    assertTrue(TernaryValue.of(true).maybeTrue());
+    assertTrue(TernaryValue.of(null).maybeTrue());
+    assertFalse(TernaryValue.of(false).maybeTrue());
+    assertTrue(TernaryValue.of(Boolean.TRUE).maybeTrue());
+  }
+}
