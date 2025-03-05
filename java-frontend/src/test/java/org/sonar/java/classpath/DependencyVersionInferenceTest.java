992a55377e1e51d8b01db7f2507be4a5b57ca42c
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-03-05 10:43:50 +0100

    Support Spring boot with manifest inference

--- java-frontend/src/test/java/org/sonar/java/classpath/DependencyVersionInferenceTest.java
+++ java-frontend/src/test/java/org/sonar/java/classpath/DependencyVersionInferenceTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
 
 class DependencyVersionInferenceTest {
-  String path = "/home/romain.brenguier/git/examples/target/classes,/home/romain.brenguier/.sdkman/candidates/java/current/lib/jrt-fs.jar,/home/romain.brenguier/.m2/repository/org/projectlombok/lombok/1.18.30/lombok-1.18.30.jar,/home/romain.brenguier/.m2/repository/org/jetbrains/annotations/25.0.0/annotations-25.0.0.jar";
+  String path = "/home/romain.brenguier/.m2/repository/org/springframework/boot/spring-boot/3.4.1/spring-boot-3.4.1.jar,/home/romain.brenguier/git/examples/target/classes,/home/romain.brenguier/.sdkman/candidates/java/current/lib/jrt-fs.jar,/home/romain.brenguier/.m2/repository/org/projectlombok/lombok/1.18.30/lombok-1.18.30.jar,/home/romain.brenguier/.m2/repository/org/jetbrains/annotations/25.0.0/annotations-25.0.0.jar";
   List<File> classpath = Arrays.stream(path.split(",")).map(File::new).toList();
 
   @Test
@@ -56,6 +56,24 @@ class DependencyVersionInferenceTest {
     assertEquals(1, version.get().major());
     assertEquals(18, version.get().minor());
     assertEquals(30, version.get().patch());
+    assertEquals(null, version.get().qualifier());
+  }
+
+  @Test
+  void inferenceImplementations() {
+    // Act
+    Optional<Version> version =
+      DependencyVersionInference.inferenceImplementations.stream()
+        .filter(i -> i.handles("org.springframework.boot", "spring-boot"))
+        .filter(i -> i instanceof DependencyVersionInference.ManifestInference)
+        .map(i -> i.infer(classpath))
+        .flatMap(Optional::stream)
+        .findFirst();
+
+    // Assert
+    assertEquals(3, version.get().major());
+    assertEquals(4, version.get().minor());
+    assertEquals(1, version.get().patch());
     assertEquals(null, version.get().qualifier());
   }
 }
