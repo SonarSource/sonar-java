37c77dad766b1f314ffad926e8b267f15998f137
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-03-06 09:28:52 +0100

    Add missing licence headers

--- java-frontend/src/test/java/org/sonar/java/classpath/DependencyVersionInferenceServiceTest.java
+++ java-frontend/src/test/java/org/sonar/java/classpath/DependencyVersionInferenceServiceTest.java
@@ -1,3 +1,19 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
 package org.sonar.java.classpath;
 
 import java.util.List;
