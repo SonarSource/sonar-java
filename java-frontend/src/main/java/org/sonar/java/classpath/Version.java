8d439084d3dbd7e01dee43fc5a73004c71f75f58
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-03-11 11:06:36 +0100

    Add Version and version inference classes
    For inferring version of libraries present in the classpatth

---
+++ java-frontend/src/main/java/org/sonar/java/classpath/Version.java
@@ -0,0 +1,125 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.classpath;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.annotation.Nullable;
+
+/**
+ * Class to parse and compare versions of library jars.
+ */
+public record Version(Integer major, Integer minor, @Nullable Integer patch, @Nullable String qualifier) implements Comparable<Version> {
+
+  public static String VERSION_REGEX = "([0-9]+).([0-9]+)(.[0-9]+)?([^0-9].*)?";
+
+  private static final Pattern VERSION_PATTERN = Pattern.compile(VERSION_REGEX);
+
+  /**
+   * matcher must come from a match against a pattern that contains {@link #VERSION_REGEX} and no other groups.
+   */
+  public static Version matcherToVersion(Matcher matcher) {
+    return new Version(
+      Integer.parseInt(matcher.group(1)),
+      Integer.parseInt(matcher.group(2)),
+      matcher.group(3) != null ? Integer.parseInt(matcher.group(3).substring(1)) : null,
+      matcher.group(4));
+  }
+
+  public static Version parse(String versionString) {
+    Matcher matcher = VERSION_PATTERN.matcher(versionString);
+    if (matcher.matches()) {
+      return matcherToVersion(matcher);
+    }
+    throw new IllegalArgumentException("Not a valid version string: " + versionString);
+  }
+
+  /**
+   * Warning: this is a partial order: 2.5 and 2.5.1 are uncomparable.
+   * Qualifiers are ignored.
+   */
+  @Override
+  public int compareTo(Version o) {
+    if (!Objects.equals(major, o.major)) {
+      return major - o.major;
+    }
+    if (!Objects.equals(minor, o.minor)) {
+      return minor - o.minor;
+    }
+    if (!Objects.equals(patch, o.patch)) {
+      if (patch == null || o.patch == null) return 0;
+      return patch - o.patch;
+    }
+    return 0;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Version version = (Version) o;
+    return Objects.equals(major, version.major)
+      && Objects.equals(minor, version.minor)
+      && Objects.equals(patch, version.patch)
+      && Objects.equals(qualifier, version.qualifier);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(major, minor, patch, qualifier);
+  }
+
+  @Override
+  public String toString() {
+    return major + "." + minor +
+      (patch == null ? "" : ("." + patch)) +
+      (qualifier == null ? "" : qualifier);
+  }
+
+  public boolean isGreaterThanOrEqualTo(Version version) {
+    return compareTo(version) >= 0;
+  }
+
+  public boolean isGreaterThan(Version version) {
+    return compareTo(version) > 0;
+  }
+
+  public boolean isLowerThanOrEqualTo(Version version) {
+    return compareTo(version) <= 0;
+  }
+
+  public boolean isLowerThan(Version version) {
+    return compareTo(version) < 0;
+  }
+
+  public boolean isGreaterThanOrEqualTo(String version) {
+    return isGreaterThanOrEqualTo(Version.parse(version));
+  }
+
+  public boolean isGreaterThan(String version) {
+    return isGreaterThan(Version.parse(version));
+  }
+
+  public boolean isLowerThanOrEqualTo(String version) {
+    return isLowerThanOrEqualTo(Version.parse(version));
+  }
+
+  public boolean isLowerThan(String version) {
+    return isLowerThan(Version.parse(version));
+  }
+
+}
