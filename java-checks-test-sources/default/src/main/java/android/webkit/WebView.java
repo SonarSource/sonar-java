90e8de05142882f92146801d68078ccbb69760f2
Antonio Aversa <antonio.aversa@sonarsource.com>
2025-04-01 12:12:51 +0200

    SONARJAVA-5412 Rule S7409: Exposing Java objects through JavaScript interfaces is security-sensitive (#5068)

--- java-checks-test-sources/default/src/main/java/android/webkit/WebView.java
+++ java-checks-test-sources/default/src/main/java/android/webkit/WebView.java
@@ -3,4 +3,7 @@ package android.webkit;
 public class WebView {
   public static void setWebContentsDebuggingEnabled(boolean enabled) {
   }
+
+  public void addJavascriptInterface(Object object, String name) {
+  }
 }
