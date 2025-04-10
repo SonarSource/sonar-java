9a244287e2907850079debdcc3eec7a2be83e6b3
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-04-10 11:42:23 +0200

    Document DependencyVersionAware API

--- java-frontend/src/main/java/org/sonar/plugins/java/api/DependencyVersionAware.java
+++ java-frontend/src/main/java/org/sonar/plugins/java/api/DependencyVersionAware.java
@@ -19,8 +19,33 @@ package org.sonar.plugins.java.api;
 import java.util.Optional;
 import java.util.function.Function;
 
+/**
+ * Implementing this interface allows a check to be executed - or not - during analysis, depending on the
+ * version of desired libraries among dependencies of the current module.
+ *
+ * <p> For example, if a rule is to be used only on modules of a project that use spring-web version at least 4.3,
+ * it should implement {@link DependencyVersionAware} with the following method:
+ * <pre>
+ *  {@literal @}Override
+ *   public boolean isCompatibleWithDependencies(Function<String, Optional<Version>> dependencyFinder) {
+ *     return dependencyFinder.apply("spring-web")
+ *       .map(v -> v.isGreaterThanOrEqualTo("4.3"))
+ *       .orElse(false);
+ *   }
+ * </pre>
+ */
 public interface DependencyVersionAware {
 
+  /**
+   * Control whether the check is compatible with the dependencies of the project being analysed.
+   *
+   * @param dependencyFinder is a function that takes in the name of an artifact that may be found in the classpath
+   *                         of the project. It returns the version of that artifact that was detected, or an empty
+   *                         optional if it is not detected in the classpath.
+   *                         Note that we cannot guarantee that a dependency will always be detected in the case were
+   *                         the classpath doesn't come from a standard build system like maven or gradle.
+   * @return true if the check is compatible with the detected dependencies and should be executed on sources, false otherwise.
+   */
   boolean isCompatibleWithDependencies(Function<String, Optional<Version>> dependencyFinder);
 
 }
