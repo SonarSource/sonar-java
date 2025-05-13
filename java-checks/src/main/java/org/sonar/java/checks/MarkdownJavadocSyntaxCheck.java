9ad9669c9646ef0cbc054779dd60da33e8dd9aa5
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-05-13 14:54:24 +0200

    SONARJAVA-5501 Create new check for rule S7474

---
+++ java-checks/src/main/java/org/sonar/java/checks/MarkdownJavadocSyntaxCheck.java
@@ -0,0 +1,39 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.Tree;
+
+
+@Rule(key = "S7474")
+public class MarkdownJavadocSyntaxCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    // TODO: Specify the kind of nodes you want to be called to visit here.
+    return List.of();
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+  
+}
