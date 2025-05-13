86b9a995b4f4ee5ec248ea17ac7d8bf8d9246489
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2025-05-13 17:33:16 +0200

    revert reordering imports

--- java-checks/src/test/java/org/sonar/java/checks/MissingDeprecatedCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/MissingDeprecatedCheckTest.java
@@ -19,8 +19,8 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
 import static org.sonar.java.checks.verifier.TestUtils.nonCompilingTestSourcesPath;
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
 
 class MissingDeprecatedCheckTest {
 
