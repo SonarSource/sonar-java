e54ee21183e4c193b2af0d9324ad16fcc89b22ff
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-05-14 11:45:20 +0200

    SONARJAVA-5501 Implement check for HTML and javadoc tags in markdown comments

--- java-checks-test-sources/default/src/main/java/checks/MarkdownJavadocSyntaxCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/MarkdownJavadocSyntaxCheckSample.java
@@ -1,5 +1,101 @@
 package checks;
 
-public class MarkdownJavadocSyntaxCheckSample {
-  // TODO: Implement the sample class
+/// The javadoc in this class uses markdown comment (3 slashes) but uses some HTML tags.
+/// It makes some text appear in <b>bold</b>.
+public class MarkdownJavadocSyntaxCheckSample { // Noncompliant {{replace HTML syntax with Markdown syntax in javadoc}}
+
+  /// Some text appears in <i>italic</i>.
+  public void withItalic() { // Noncompliant
+  }
+
+  /// Some text appears in _italic_.
+  public void withItalicMarkdown() { // Compliant
+  }
+
+  /// Separate <p> paragraphs are created by simply using the {@code <p>} tag.
+  public void withParagraph() { // Noncompliant
+  }
+
+  /// Separate
+  ///
+  /// paragraphs are created by simply using the `<p>` tag.
+  public void withParagraphMarkdown() { // Compliant
+  }
+
+  /// For inline code snippets, it uses the {@code <code>} tag.
+  public void withCode() { // Noncompliant
+  }
+
+  /// For larger blocks of code, the `<pre>` tag is used:
+  /// <pre>
+  /// public class Example {
+  ///   public static void main(String[] args) {
+  ///     System.out.println("Hello, Javadoc!");
+  ///   }
+  /// }
+  /// </pre>
+  public void withPreBlock() { // Noncompliant
+  }
+
+  /// For larger blocks of code, triple quotes should be used:
+  /// ```
+  /// public class Example {
+  ///   public static void main(String[] args) {
+  ///     System.out.println("Hello, <b>Javadoc</b>!");
+  ///   }
+  /// }
+  /// ```
+  public void withMarkdownBlock() { // Compliant
+  }
+
+  /// {@link String#length()} links to the {@link java.lang.String#length()} method.
+  public void withLink() { // Noncompliant
+  }
+
+  /// [String#length()] links to the [java.lang.String#length()] method.
+  public void withLinkMarkdown() { // Compliant
+  }
+
+  /// Here is a list:
+  /// <ul>
+  ///   <li> Red </li>
+  ///   <li> Blue </li>
+  ///   <li> Green </li>
+  /// </ul>
+  public void withList() { // Noncompliant
+  }
+
+  /// Here is a list:
+  /// * Red
+  /// * Blue
+  /// * Green
+  public void withListMarkdown() { // Compliant
+  }
+
+  /// Here is a table:
+  /// <table>
+  /// <tr>
+  /// <th>Header 1</th>
+  /// <th>Header 2</th>
+  /// </tr>
+  /// <tr>
+  ///  <td>Row 1, Col 1</td>
+  /// <td>Row 1, Col 2</td>
+  /// </tr>
+  /// <tr>
+  /// <td>Row 2, Col 1</td>
+  /// <td>Row 2, Col 2</td>
+  /// </tr>
+  /// </table>
+  public void withTable() { // Noncompliant
+  }
+
+  /// Here is a table:
+  ///
+  /// | Header 1     | Header 2     |
+  /// |--------------|--------------|
+  /// | Row 1, Col 1 | Row 1, Col 2 |
+  /// | Row 2, Col 1 | Row 2, Col 2 |
+  public void withTableMarkdown() { // Compliant
+  }
 }
--- java-checks/src/main/java/org/sonar/java/checks/MarkdownJavadocSyntaxCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/MarkdownJavadocSyntaxCheck.java
@@ -16,24 +16,83 @@
  */
 package org.sonar.java.checks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.sonar.check.Rule;
+import org.sonar.java.annotations.VisibleForTesting;
+import org.sonar.java.ast.visitors.PublicApiChecker;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.SyntaxToken;
+import org.sonar.plugins.java.api.tree.SyntaxTrivia;
 import org.sonar.plugins.java.api.tree.Tree;
-
 
 @Rule(key = "S7474")
 public class MarkdownJavadocSyntaxCheck extends IssuableSubscriptionVisitor {
 
+  /**
+   * Pattern to find Javadoc or HTML tags that can be replaced with Markdown.
+   */
+  private static final Pattern NON_MARKDOWN_JAVADOC_PATTERN = Pattern.compile("<b>|<i>|<p>|<pre>|<ul>|<table>|\\{@code |\\{@link ");
+
+  private static final Pattern TRIPLE_QUOTE = Pattern.compile("```");
+
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    // TODO: Specify the kind of nodes you want to be called to visit here.
-    return List.of();
+    return Arrays.asList(PublicApiChecker.apiKinds());
   }
 
   @Override
   public void visitNode(Tree tree) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    SyntaxToken firstToken = tree.firstToken();
+    if (firstToken == null) {
+      return;
+    }
+    boolean containsNonMarkdownSyntax = firstToken
+      .trivias()
+      .stream()
+      .filter(trivia -> trivia.isComment(SyntaxTrivia.CommentKind.MARKDOWN))
+      .map(SyntaxTrivia::comment)
+      .flatMap(comment -> removeQuotedCode(comment).stream())
+      .anyMatch(comment -> NON_MARKDOWN_JAVADOC_PATTERN.matcher(comment).find());
+
+    if (containsNonMarkdownSyntax) {
+      context.reportIssue(this, tree, "replace HTML syntax with Markdown syntax in javadoc");
+    }
   }
-  
+
+  /**
+   * Remove from the text, parts that are between backquotes ({@code `}) and triple backquote ({@code ```}) for Markdown code.
+   */
+  @VisibleForTesting
+  static List<String> removeQuotedCode(String javadoc) {
+    List<String> nonEscaped = new ArrayList<>();
+    int currentPosition = 0;
+    while (currentPosition != -1) {
+      int nextQuote = javadoc.indexOf("`", currentPosition);
+      if (nextQuote != -1) {
+        nonEscaped.add(javadoc.substring(currentPosition, nextQuote));
+        currentPosition = findEndOfMarkdownQuote(javadoc, nextQuote);
+      } else {
+        nonEscaped.add(javadoc.substring(currentPosition));
+        currentPosition = -1;
+      }
+    }
+    return nonEscaped;
+  }
+
+  @VisibleForTesting
+  static int findEndOfMarkdownQuote(String javadoc, int startPosition) {
+    Matcher matcher = TRIPLE_QUOTE.matcher(javadoc);
+    matcher.region(startPosition, javadoc.length());
+    if (matcher.lookingAt()) {
+      boolean closingQuotesFound = matcher.find(startPosition + 3);
+      return !closingQuotesFound ? -1 : matcher.end();
+    } else {
+      int closingQuotePosition = javadoc.indexOf("`", startPosition + 1);
+      return closingQuotePosition == -1 ? -1 : (closingQuotePosition + 1);
+    }
+  }
 }
