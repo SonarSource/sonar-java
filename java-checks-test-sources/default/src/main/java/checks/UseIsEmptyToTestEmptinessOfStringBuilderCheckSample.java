bb1e88f222b906686fb9eadb14e0a1d1390ecf9c
erwan.serandour <erwan.serandour@sonarsource.com>
2025-06-13 15:00:41 +0200

    add quick fix

--- java-checks-test-sources/default/src/main/java/checks/UseIsEmptyToTestEmptinessOfStringBuilderCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/UseIsEmptyToTestEmptinessOfStringBuilderCheckSample.java
@@ -6,17 +6,18 @@ public class UseIsEmptyToTestEmptinessOfStringBuilderCheckSample {
     StringBuilder sb = new StringBuilder();
 
     if ("".equals(sb.toString())) { // Noncompliant {{Replace "equals()" with "isEmpty()".}}
-      // ^^^^^^^^^^^^^^^^^^^^^^^^
+     // ^^^^^^^^^^^^^^^^^^^^^^^^
       System.out.println("Empty StringBuilder");
     }
 
-    if (sb.toString().isEmpty()) { // Noncompliant {{Replace "toString().isEmpty()" with "isEmpty()".}}
-      // ^^^^^^^^^^^^^^^^^^^^^^^
+    if (sb.toString().isEmpty()) { // Noncompliant {{Replace "toString().isEmpty()" with "isEmpty()".}} [[quickfixes=qf1]]
+      // fix@qf1 {{Replace with "isEmpty()"}}
+      // edit@qf1 [[sc=11;ec=32]] {{.isEmpty()}}
       System.out.println("Empty StringBuilder");
     }
 
     if (sb.toString().length() == 0) { // Noncompliant {{Replace "toString().length()" with "isEmpty()".}}
-      // ^^^^^^^^^^^^^^^^^^^^^^
+     // ^^^^^^^^^^^^^^^^^^^^^^
       System.out.println("Empty StringBuilder");
     }
 
@@ -34,13 +35,21 @@ public class UseIsEmptyToTestEmptinessOfStringBuilderCheckSample {
     boolean inExpression = "".equals(sb.toString()); // Noncompliant
 
     if (sb.toString().equals("")) { // Noncompliant {{Replace "equals()" with "isEmpty()".}}
-      // ^^^^^^^^^^^^^^^^^^^^^^^^
+     // ^^^^^^^^^^^^^^^^^^^^^^^^
       System.out.println("Empty StringBuilder using equals reversed");
     }
 
     if (!sb.toString().equals("")) { // Noncompliant
       System.out.println("Non-empty StringBuilder using equals reversed");
     }
+
+  }
+
+  void quickFix(){
+    StringBuilder sb = new StringBuilder();
+    var x = sb.toString().isEmpty() && false; // Noncompliant [[quickfixes=qf2]]
+    // fix@qf2 {{Replace with "isEmpty()"}}
+    // edit@qf2 [[sc=15;ec=36]] {{.isEmpty()}}
 
   }
 
