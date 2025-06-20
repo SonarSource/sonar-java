f4f7017370690d44246834ad1441f67d4bcde1af
tomasz-tylenda-sonarsource <tomasz.tylenda@sonarsource.com>
2025-06-20 17:37:26 +0200

    SONARJAVA-5599 S3024: Report issues only inside loops (#5204)

--- java-checks-test-sources/default/src/main/java/checks/StringBufferAndBuilderConcatenationCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/StringBufferAndBuilderConcatenationCheckSample.java
@@ -1,99 +1,235 @@
 package checks;
 
 import java.util.Iterator;
+import java.util.List;
 
 public class StringBufferAndBuilderConcatenationCheckSample {
-  public String appendSimple() {
+  public String appendCorrect() {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append("text1")
+        .append("text2");
+    }
+    return sb.toString();
+  }
+
+  public String noLoop() {
+    StringBuffer sb = new StringBuffer();
+    sb.append("text1" + "text2");
+    return sb.toString();
+  }
+
+  public String nestedLoops() {
+    StringBuffer sbOutside = new StringBuffer();
+    StringBuffer sbInside1 = new StringBuffer();
+    StringBuffer sbInside2 = new StringBuffer();
+    StringBuffer sbInside21 = new StringBuffer();
+
+    sbInside1.append("text1" + "text2");
+    for (int i = 0; i < 5; i++) {
+      sbInside1.append("i = " + i); // Noncompliant
+      for (int j = 0; j < 5; j++) {
+        sbInside2.append("j = " + j); // Noncompliant
+        if (i == j) {
+          sbInside21.append("i eq j = " + i); // Noncompliant
+        }
+      }
+    }
+
     return new StringBuilder()
-      .append("text1")
-      .append("text2")
+      .append(sbOutside)
+      .append(sbInside1)
+      .append(sbInside2)
+      .append(sbInside21)
       .toString();
+  }
+
+  public String loopWhile() {
+    StringBuffer sb = new StringBuffer();
+    int i = 0;
+    while (i < 10) {
+      sb.append("text1" + "text2"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfWhile]]
+//              ^^^^^^^^^^^^^^^^^
+//              fix@qfWhile {{Call "append" multiple times.}}
+//              edit@qfWhile [[sc=16;ec=35]] {{("text1").append("text2")}}
+      i++;
+    }
+    return sb.toString();
+  }
+
+  public String loopDoWhile() {
+    StringBuffer sb = new StringBuffer();
+    int i = 0;
+    do {
+      sb.append("text1" + "text2"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfDoWhile]]
+//              ^^^^^^^^^^^^^^^^^
+//              fix@qfDoWhile {{Call "append" multiple times.}}
+//              edit@qfDoWhile [[sc=16;ec=35]] {{("text1").append("text2")}}
+      i++;
+    } while (i < 10);
+    return sb.toString();
+  }
+
+  public String loopForeach(List<String> items) {
+    StringBuffer sb = new StringBuffer();
+    for (String item : items) {
+      sb.append("<" + item + ">\n"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfForeach]]
+//              ^^^^^^^^^^^^^^^^^^
+//              fix@qfForeach {{Call "append" multiple times.}}
+//              edit@qfForeach [[sc=16;ec=36]] {{("<").append(item).append(">\n")}}
+    }
+    return sb.toString();
+  }
+
+  public String functionalForeach(List<String> items) {
+    // FN: not implemented
+    StringBuffer sb = new StringBuffer();
+    items.forEach(item -> sb.append("<" + item + ">\n"));
+    return sb.toString();
+  }
+
+  public String functionalStreamForeach(List<String> items) {
+    // FN: not implemented
+    StringBuffer sb = new StringBuffer();
+    items
+      .stream()
+      .forEach(item -> sb.append("<" + item + ">\n"));
+    return sb.toString();
+  }
+
+  public String loopForIf() {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
+        sb.append("number: " + i); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfForIf]]
+//                ^^^^^^^^^^^^^^
+//              fix@qfForIf {{Call "append" multiple times.}}
+//              edit@qfForIf [[sc=18;ec=34]] {{("number: ").append(i)}}
+      }
+    }
+    return sb.toString();
   }
 
   public String concatThree() {
-    return new StringBuilder()
-      .append("text1" + "text2" + "text3") // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf2]]
-//            ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-//            fix@qf2 {{Call "append" multiple times.}}
-//            edit@qf2 [[sc=14;ec=43]] {{("text1").append("text2").append("text3")}}
-      .toString();
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append("text1" + "text2" + "text3"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfThree]]
+//              ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//              fix@qfThree {{Call "append" multiple times.}}
+//              edit@qfThree [[sc=16;ec=45]] {{("text1").append("text2").append("text3")}}
+    }
+    return sb.toString();
   }
 
-  public String concatIntegerNoncompliant() {
-    return new StringBuilder()
-      .append("text" + 1) // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf3]]
-//            ^^^^^^^^^^
-//            fix@qf3 {{Call "append" multiple times.}}
-//            edit@qf3 [[sc=14;ec=26]] {{("text").append(1)}}
-      .toString();
+  public String concatIntNoncompliant() {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append("text" + 1); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfInt]]
+//              ^^^^^^^^^^
+//              fix@qfInt {{Call "append" multiple times.}}
+//              edit@qfInt [[sc=16;ec=28]] {{("text").append(1)}}
+    }
+    return sb.toString();
   }
 
-  public String concatIntegerCompliant() {
-    return new StringBuilder()
-      .append(1 + 1)
-      .toString();
+  public String concatIntCompliant() {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append(1 + 1);
+    }
+    return sb.toString();
   }
 
   public String complexArgumentList() {
-    return new StringBuilder()
-      .append("two " + (1 + 1)) // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf4]]
-//            ^^^^^^^^^^^^^^^^
-//            fix@qf4 {{Call "append" multiple times.}}
-//            edit@qf4 [[sc=14;ec=32]] {{("two ").append(1 + 1)}}
-      .toString();
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append("two " + (1 + 1)); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfComplex]]
+//              ^^^^^^^^^^^^^^^^
+//              fix@qfComplex {{Call "append" multiple times.}}
+//              edit@qfComplex [[sc=16;ec=34]] {{("two ").append(1 + 1)}}
+    }
+    return sb.toString();
   }
 
-  public StringBuilder notChained() {
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append("text1" + "text2"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf5]]
-//                       ^^^^^^^^^^^^^^^^^
-//                       fix@qf5 {{Call "append" multiple times.}}
-//                       edit@qf5 [[sc=25;ec=44]] {{("text1").append("text2")}}
-    return stringBuilder;
+  public void chained() {
+    for (int i = 0; i < 10; i++) {
+      String s = new StringBuilder()
+        .append("text1" + "text2") // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfChained]]
+//              ^^^^^^^^^^^^^^^^^
+//              fix@qfChained {{Call "append" multiple times.}}
+//              edit@qfChained [[sc=16;ec=35]] {{("text1").append("text2")}}
+        .toString();
+      System.out.println(s);
+    }
   }
 
   public void parameter(StringBuilder passed) {
-    passed.append("hello " + "hello");  // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf6]]
-//                ^^^^^^^^^^^^^^^^^^
-//                fix@qf6 {{Call "append" multiple times.}}
-//                edit@qf6 [[sc=18;ec=38]] {{("hello ").append("hello")}}
+    for (int i = 0; i < 10; i++) {
+      passed.append("hello " + "hello");  // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfPassed]]
+//                  ^^^^^^^^^^^^^^^^^^
+//                  fix@qfPassed {{Call "append" multiple times.}}
+//                  edit@qfPassed [[sc=20;ec=40]] {{("hello ").append("hello")}}
+    }
   }
 
   public String call(Iterator<String> iter) {
-    return new StringBuilder()
-      .append("<" + iter.next() + "/>") // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf7]]
-//            ^^^^^^^^^^^^^^^^^^^^^^^^
-//            fix@qf7 {{Call "append" multiple times.}}
-//            edit@qf7 [[sc=14;ec=40]] {{("<").append(iter.next()).append("/>")}}
-      .toString();
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append("<" + iter.next() + "/>"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfCall]]
+//              ^^^^^^^^^^^^^^^^^^^^^^^^
+//              fix@qfCall {{Call "append" multiple times.}}
+//              edit@qfCall [[sc=16;ec=42]] {{("<").append(iter.next()).append("/>")}}
+    }
+    return sb.toString();
   }
 
   public String nestedCall(String s) {
-    return new StringBuilder()
-      .append(s.toLowerCase())
-      .toString();
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append(s.toLowerCase());
+    }
+    return sb.toString();
   }
 
   public String stringBufferAppendSimple() {
-    return new StringBuffer()
-      .append("text1")
-      .append("text2")
-      .toString();
+    StringBuffer stringBuffer = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      stringBuffer.append("text1")
+        .append("text2");
+    }
+    return stringBuffer.toString();
+  }
+
+  public String stringBufferConcatSimple() {
+    StringBuffer stringBuffer = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      stringBuffer.append("text1" + "text2"); // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qfBuffer]]
+//                        ^^^^^^^^^^^^^^^^^
+//                        fix@qfBuffer {{Call "append" multiple times.}}
+//                        edit@qfBuffer [[sc=26;ec=45]] {{("text1").append("text2")}}
+    }
+    return stringBuffer.toString();
   }
 
   public String appendWithStartEnd() {
     // Breaking this up would require changing the second and third argument.
-    return new StringBuilder()
-      .append("text1" + "text2" , 2, 3)
-      .toString();
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      sb.append("text1" + "text2", 2, 3);
+    }
+    return sb.toString();
   }
 
-  public String stringBufferConcatSimple() {
-    return new StringBuffer()
-      .append("text1" + "text2") // Noncompliant {{Use multiple calls to "append" instead of string concatenation.}} [[quickfixes=qf101]]
-//            ^^^^^^^^^^^^^^^^^
-//            fix@qf101 {{Call "append" multiple times.}}
-//            edit@qf101 [[sc=14;ec=33]] {{("text1").append("text2")}}
-      .toString();
+  public String crossesMethodBoundary() {
+    // FN: we do not analyze across method calls
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 10; i++) {
+      anotherMethod(sb);
+    }
+    return sb.toString();
+  }
+
+  private void anotherMethod(StringBuffer sb) {
+    sb.append("text1" + "text2");
   }
 }
--- java-checks/src/main/java/org/sonar/java/checks/StringBufferAndBuilderConcatenationCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/StringBufferAndBuilderConcatenationCheck.java
@@ -20,49 +20,84 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.helpers.QuickFixHelper;
-import org.sonar.java.checks.methods.AbstractMethodDetection;
 import org.sonar.java.model.ExpressionUtils;
 import org.sonar.java.reporting.JavaQuickFix;
 import org.sonar.java.reporting.JavaTextEdit;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.semantic.MethodMatchers;
 import org.sonar.plugins.java.api.tree.Arguments;
 import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
+import org.sonar.plugins.java.api.tree.DoWhileStatementTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.ForEachStatement;
+import org.sonar.plugins.java.api.tree.ForStatementTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.WhileStatementTree;
 
 /**
  * Suggest that <code>stringBuilder.append("text1" + "text2")</code> is replaced with
  * <code>stringBuilder.append("text1").append("text2")</code>.
  */
 @Rule(key = "S3024")
-public class StringBufferAndBuilderConcatenationCheck extends AbstractMethodDetection {
+public class StringBufferAndBuilderConcatenationCheck extends IssuableSubscriptionVisitor {
 
   private static final String JAVA_LANG_STRING = "java.lang.String";
 
+  private static final MethodMatchers APPEND_MATCHER = MethodMatchers.create()
+    .ofTypes("java.lang.StringBuffer", "java.lang.StringBuilder")
+    .names("append")
+    .addParametersMatcher(JAVA_LANG_STRING)
+    .build();
+
+  private int loopNesting = 0;
+
   @Override
-  protected MethodMatchers getMethodInvocationMatchers() {
-    return MethodMatchers.create()
-      .ofTypes("java.lang.StringBuffer", "java.lang.StringBuilder")
-      .names("append")
-      .addParametersMatcher(JAVA_LANG_STRING)
-      .build();
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(
+      Tree.Kind.METHOD_INVOCATION,
+      Tree.Kind.FOR_STATEMENT,
+      Tree.Kind.FOR_EACH_STATEMENT,
+      Tree.Kind.WHILE_STATEMENT,
+      Tree.Kind.DO_STATEMENT);
   }
 
   @Override
-  protected void onMethodInvocationFound(MethodInvocationTree mit) {
-    ExpressionTree arg = mit.arguments().get(0);
+  public void visitNode(Tree tree) {
+    if (isLoopNode(tree)) {
+      loopNesting++;
+    } else if (loopNesting > 0 && tree instanceof MethodInvocationTree mit && APPEND_MATCHER.matches(mit)) {
+      onAppendInvocationFound(mit);
+    }
+  }
 
-    getConcatenationTree(arg).ifPresent(binaryExpressionTree ->
+  @Override
+  public void leaveNode(Tree tree) {
+    if (isLoopNode(tree)) {
+      loopNesting--;
+    }
+  }
+
+  private void onAppendInvocationFound(MethodInvocationTree mit) {
+    ExpressionTree arg = mit.arguments().get(0);
+    BinaryExpressionTree binaryExpressionTree = getConcatenationTree(arg);
+
+    if (binaryExpressionTree != null) {
       QuickFixHelper.newIssue(context)
-          .forRule(this)
-          .onTree(arg)
-          .withMessage("Use multiple calls to \"append\" instead of string concatenation.")
-          .withQuickFix(() -> getQuickFix(mit.arguments()))
-          .report()
-    );
+        .forRule(this)
+        .onTree(arg)
+        .withMessage("Use multiple calls to \"append\" instead of string concatenation.")
+        .withQuickFix(() -> getQuickFix(mit.arguments()))
+        .report();
+    }
+  }
+
+  private static boolean isLoopNode(Tree tree) {
+    return tree instanceof ForStatementTree || tree instanceof ForEachStatement
+      || tree instanceof WhileStatementTree || tree instanceof DoWhileStatementTree;
   }
 
   /**
@@ -70,11 +105,12 @@ public class StringBufferAndBuilderConcatenationCheck extends AbstractMethodDete
    * otherwise return <code>Optional.empty()</code>.
    * Using optional, rather than if-then, helps us avoid some problems in coverage by unit tests.
    */
-  private static Optional<BinaryExpressionTree> getConcatenationTree(ExpressionTree tree) {
+  private static @Nullable BinaryExpressionTree getConcatenationTree(ExpressionTree tree) {
     return Optional.of(tree)
       .filter(BinaryExpressionTree.class::isInstance)
       .map(BinaryExpressionTree.class::cast)
-      .filter(bet -> bet.is(Tree.Kind.PLUS));
+      .filter(bet -> bet.is(Tree.Kind.PLUS))
+      .orElse(null);
   }
 
   private JavaQuickFix getQuickFix(Arguments arguments) {
@@ -95,14 +131,14 @@ public class StringBufferAndBuilderConcatenationCheck extends AbstractMethodDete
   }
 
   private static void splitExpressionOnPlus(List<ExpressionTree> accumulator, ExpressionTree tree) {
-    getConcatenationTree(tree).ifPresentOrElse(
-      bet -> {
-        ExpressionTree left = bet.leftOperand();
-        ExpressionTree right = bet.rightOperand();
-        splitExpressionOnPlus(accumulator, left);
-        accumulator.add(right);
-      },
-      () -> accumulator.add(tree)
-    );
+    BinaryExpressionTree bet = getConcatenationTree(tree);
+    if (bet != null) {
+      ExpressionTree left = bet.leftOperand();
+      ExpressionTree right = bet.rightOperand();
+      splitExpressionOnPlus(accumulator, left);
+      accumulator.add(right);
+    } else {
+      accumulator.add(tree);
+    }
   }
 }
