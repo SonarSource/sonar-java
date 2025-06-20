fd996bc2c4cd1c9163d850d59a2d3785f67d3baf
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2025-06-20 10:37:25 +0200

    S3024: Report issues only inside loops

--- java-checks/src/main/java/org/sonar/java/checks/StringBufferAndBuilderConcatenationCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/StringBufferAndBuilderConcatenationCheck.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.helpers.QuickFixHelper;
 import org.sonar.java.checks.methods.AbstractMethodDetection;
@@ -29,9 +30,14 @@ import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.semantic.MethodMatchers;
 import org.sonar.plugins.java.api.tree.Arguments;
 import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
+import org.sonar.plugins.java.api.tree.DoWhileStatementTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.ForEachStatement;
+import org.sonar.plugins.java.api.tree.ForStatementTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.WhileStatementTree;
 
 /**
  * Suggest that <code>stringBuilder.append("text1" + "text2")</code> is replaced with
@@ -54,15 +60,30 @@ public class StringBufferAndBuilderConcatenationCheck extends AbstractMethodDete
   @Override
   protected void onMethodInvocationFound(MethodInvocationTree mit) {
     ExpressionTree arg = mit.arguments().get(0);
+    BinaryExpressionTree binaryExpressionTree = getConcatenationTree(arg);
 
-    getConcatenationTree(arg).ifPresent(binaryExpressionTree ->
+    if (binaryExpressionTree != null && isInsideLoop(mit)) {
       QuickFixHelper.newIssue(context)
-          .forRule(this)
-          .onTree(arg)
-          .withMessage("Use multiple calls to \"append\" instead of string concatenation.")
-          .withQuickFix(() -> getQuickFix(mit.arguments()))
-          .report()
-    );
+        .forRule(this)
+        .onTree(arg)
+        .withMessage("Use multiple calls to \"append\" instead of string concatenation.")
+        .withQuickFix(() -> getQuickFix(mit.arguments()))
+        .report();
+    }
+  }
+
+  private static boolean isInsideLoop(Tree tree) {
+    while (tree != null) {
+      if (tree instanceof ForStatementTree || tree instanceof ForEachStatement
+        || tree instanceof WhileStatementTree || tree instanceof DoWhileStatementTree) {
+        return true;
+      }
+      if (tree instanceof MethodTree) {
+        break;
+      }
+      tree = tree.parent();
+    }
+    return false;
   }
 
   /**
@@ -70,11 +91,12 @@ public class StringBufferAndBuilderConcatenationCheck extends AbstractMethodDete
    * otherwise return <code>Optional.empty()</code>.
    * Using optional, rather than if-then, helps us avoid some problems in coverage by unit tests.
    */
-  private static Optional<BinaryExpressionTree> getConcatenationTree(ExpressionTree tree) {
+  private static @Nullable BinaryExpressionTree getConcatenationTree(ExpressionTree tree) {
     return Optional.of(tree)
       .filter(BinaryExpressionTree.class::isInstance)
       .map(BinaryExpressionTree.class::cast)
-      .filter(bet -> bet.is(Tree.Kind.PLUS));
+      .filter(bet -> bet.is(Tree.Kind.PLUS))
+      .orElse(null);
   }
 
   private JavaQuickFix getQuickFix(Arguments arguments) {
@@ -95,14 +117,14 @@ public class StringBufferAndBuilderConcatenationCheck extends AbstractMethodDete
   }
 
   private static void splitExpressionOnPlus(List<ExpressionTree> accumulator, ExpressionTree tree) {
-    getConcatenationTree(tree).ifPresentOrElse(
-      bet -> {
-        ExpressionTree left = bet.leftOperand();
-        ExpressionTree right = bet.rightOperand();
-        splitExpressionOnPlus(accumulator, left);
-        accumulator.add(right);
-      },
-      () -> accumulator.add(tree)
-    );
+    BinaryExpressionTree bet = getConcatenationTree(tree);
+    if (bet != null) {
+      ExpressionTree left = bet.leftOperand();
+      ExpressionTree right = bet.rightOperand();
+      splitExpressionOnPlus(accumulator, left);
+      accumulator.add(right);
+    } else {
+      accumulator.add(tree);
+    }
   }
 }
