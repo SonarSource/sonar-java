f90f3728c2321c2ce671d07b71a1158943771fb2
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-06-27 17:52:17 +0200

    fixup! SONARJAVA-5494 Add check for unnecessary usage of classname in transformClass

--- java-checks/src/main/java/org/sonar/java/checks/ClassNameInClassTransformCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/ClassNameInClassTransformCheck.java
@@ -19,21 +19,83 @@ package org.sonar.java.checks;
 import java.util.List;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
-import org.sonar.plugins.java.api.tree.Tree;
-
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.*;
 
 @Rule(key = "S7477")
 public class ClassNameInClassTransformCheck extends IssuableSubscriptionVisitor {
+  private static final String CLASS_ENTRY_CLASSNAME = "java.lang.classfile.constantpool.ClassEntry";
+
+  MethodMatchers classTransformMatcher = MethodMatchers.create()
+    .ofTypes("java.lang.classfile.ClassFile")
+    .names("transformClass")
+    .addParametersMatcher(
+      "java.lang.classfile.ClassModel",
+      "java.lang.constant.ClassDesc",
+      "java.lang.classfile.ClassTransform")
+    .build();
+
+  MethodMatchers thisClassMatcher = MethodMatchers.create()
+    .ofTypes("java.lang.classfile.ClassModel")
+    .names("thisClass")
+    .addWithoutParametersMatcher()
+    .build();
+
+  MethodMatchers internalNameMatcher = MethodMatchers.create()
+    .ofTypes(CLASS_ENTRY_CLASSNAME)
+    .names("asInternalName")
+    .addWithoutParametersMatcher()
+    .build();
+
+  MethodMatchers asSymbolMatcher = MethodMatchers.create()
+    .ofTypes(CLASS_ENTRY_CLASSNAME)
+    .names("asSymbol")
+    .addWithoutParametersMatcher()
+    .build();
+
+  MethodMatchers nameMatcher = MethodMatchers.create()
+    .ofTypes(CLASS_ENTRY_CLASSNAME)
+    .names("name")
+    .addWithoutParametersMatcher()
+    .build();
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    // TODO: Specify the kind of nodes you want to be called to visit here.
-    return List.of();
+    return List.of(Tree.Kind.METHOD_INVOCATION);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    MethodInvocationTree mit = (MethodInvocationTree) tree;
+    if (classTransformMatcher.matches(mit)) {
+      ExpressionTree classModel = mit.arguments().get(0);
+      ExpressionTree classDesc = mit.arguments().get(1);
+      // TODO we only handle case where classModel is a variable for now
+      if (classModel instanceof IdentifierTree classModelId && isClassNameOf(classModelId, classDesc)) {
+        context.reportIssue(this, classDesc, "Unchanged class name in transformClass call.");
+      }
+    }
   }
-  
+
+  private boolean isClassNameOf(IdentifierTree classModel, ExpressionTree classTree) {
+    if (classTree instanceof MethodInvocationTree mit) {
+      if (asSymbolMatcher.matches(mit) || internalNameMatcher.matches(mit)) {
+        // TODO complete
+        if (mit.methodSelect() instanceof MemberSelectExpressionTree mset) {
+          return isThisClassOf(classModel, mset.expression());
+        }
+      }
+    }
+    return false;
+  }
+
+  private boolean isThisClassOf(IdentifierTree classModel, ExpressionTree expression) {
+    if (expression instanceof MethodInvocationTree mit && thisClassMatcher.matches(mit)) {
+      if (mit.methodSelect() instanceof MemberSelectExpressionTree mset
+        && mset.expression() instanceof IdentifierTree identifier) {
+        return identifier.symbol().equals(classModel.symbol());
+      }
+    }
+    return false;
+  }
 }
