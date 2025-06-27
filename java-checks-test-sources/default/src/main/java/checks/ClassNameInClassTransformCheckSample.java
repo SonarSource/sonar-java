f89ea722ce4742378ca9e717821658c7d154134b
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-06-27 15:17:46 +0200

    SONARJAVA-5494 Add sample for transformClass check

--- java-checks-test-sources/default/src/main/java/checks/ClassNameInClassTransformCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/ClassNameInClassTransformCheckSample.java
@@ -1,5 +1,33 @@
 package checks;
 
+import java.io.IOException;
+import java.lang.classfile.ClassFile;
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.ClassTransform;
+import java.lang.classfile.MethodModel;
+import java.nio.file.Path;
+
 public class ClassNameInClassTransformCheckSample {
-  // TODO: Implement the sample class
+  ClassTransform classTransform = (classBuilder, classElement) -> {
+    if (!(classElement instanceof MethodModel methodModel &&
+      methodModel.methodName().stringValue().startsWith("debug"))) {
+      classBuilder.with(classElement);
+    }
+  };
+
+  public void transformClassFileNonCompliant(Path path) throws IOException {
+    ClassFile classFile = ClassFile.of();
+    ClassModel classModel = classFile.parse(path);
+    byte[] newBytes = classFile.transformClass(classModel,
+      classModel.thisClass().asSymbol(), // Noncompliant
+//    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+      classTransform);
+  }
+
+  public void transformClassFile(Path path) throws IOException {
+    ClassFile classFile = ClassFile.of();
+    ClassModel classModel = classFile.parse(path);
+    byte[] newBytes = classFile.transformClass(classModel,
+      classTransform);
+  }
 }
