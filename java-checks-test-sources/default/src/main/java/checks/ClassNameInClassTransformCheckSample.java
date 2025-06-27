90b0664418d2be400a763bd4f4131bfef8734a23
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-06-27 17:52:37 +0200

    fixup! SONARJAVA-5494 Add sample for transformClass check

--- java-checks-test-sources/default/src/main/java/checks/ClassNameInClassTransformCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/ClassNameInClassTransformCheckSample.java
@@ -5,6 +5,7 @@ import java.lang.classfile.ClassFile;
 import java.lang.classfile.ClassModel;
 import java.lang.classfile.ClassTransform;
 import java.lang.classfile.MethodModel;
+import java.lang.constant.ClassDesc;
 import java.nio.file.Path;
 
 public class ClassNameInClassTransformCheckSample {
@@ -24,10 +25,31 @@ public class ClassNameInClassTransformCheckSample {
       classTransform);
   }
 
+  public void transformClassFileNonCompliantName(Path path) throws IOException {
+    ClassFile classFile = ClassFile.of();
+    ClassModel classModel = classFile.parse(path);
+    byte[] newBytes = classFile.transformClass(classModel,
+      ClassDesc.ofInternalName(classModel.thisClass().asInternalName()), // Noncompliant
+//    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+      classTransform);
+  }
+
   public void transformClassFile(Path path) throws IOException {
     ClassFile classFile = ClassFile.of();
     ClassModel classModel = classFile.parse(path);
     byte[] newBytes = classFile.transformClass(classModel,
       classTransform);
   }
+
+  // TODO: add checks with:
+  // classModel.thisClass().name().stringValue());
+  // classModel.thisClass().asInternalName();
+  // classModel.thisClass().asSymbol();
+  // ClassDesc.ofInternalName(classModel.thisClass().asInternalName()));
+  // ClassDesc.ofInternalName(classModel.thisClass().name().stringValue()));
+  //   * classModel.thisClass().asSymbol()
+  //   * classModel.thisClass().stringValue()
+  //   * classModel.thisClass().asClassDesc()
+  //   * classModel.thisClass().asSymbolycRef()
+
 }
