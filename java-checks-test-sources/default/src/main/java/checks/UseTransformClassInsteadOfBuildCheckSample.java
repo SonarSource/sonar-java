f49c24c0b81b190c174c71e786a45d2629c4c2cc
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-06-30 15:24:17 +0200

    SONARJAVA-5493 Implement check for uses of build instead of transform

--- java-checks-test-sources/default/src/main/java/checks/UseTransformClassInsteadOfBuildCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/UseTransformClassInsteadOfBuildCheckSample.java
@@ -1,5 +1,41 @@
 package checks;
 
+import java.io.IOException;
+import java.lang.classfile.ClassElement;
+import java.lang.classfile.ClassFile;
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.MethodModel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class UseTransformClassInsteadOfBuildCheckSample {
-  // TODO: Implement the sample class
+  public static void transformClassFile(Path path) throws IOException {
+    ClassFile classFile = ClassFile.of();
+    ClassModel classModel = classFile.parse(path);
+    byte[] newBytes = classFile.build( // Noncompliant
+      //              ^^^^^^^^^^^^^^^
+      classModel.thisClass().asSymbol(), classBuilder -> {
+        for (ClassElement classElement : classModel) {
+          if (!(classElement instanceof MethodModel methodModel &&
+            methodModel.methodName().stringValue().startsWith("debug"))) {
+            classBuilder.with(classElement);
+
+          }
+        }
+      });
+    Files.write(path, newBytes);
+  }
+
+  public static void transformClassFileCompliant(Path path) throws IOException {
+    ClassFile classFile = ClassFile.of();
+    ClassModel classModel = classFile.parse(path);
+    byte[] newBytes = classFile.transformClass(
+      classModel, (classBuilder, classElement) -> {
+        if (!(classElement instanceof MethodModel methodModel &&
+          methodModel.methodName().stringValue().startsWith("debug"))) {
+          classBuilder.with(classElement);
+        }
+      });
+    Files.write(path, newBytes);
+  }
 }
--- java-checks/src/main/java/org/sonar/java/checks/UseTransformClassInsteadOfBuildCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/UseTransformClassInsteadOfBuildCheck.java
@@ -18,22 +18,59 @@ package org.sonar.java.checks;
 
 import java.util.List;
 import org.sonar.check.Rule;
+import org.sonar.java.matcher.TreeMatcher;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
+import static org.sonar.java.matcher.TreeMatcher.forEachStatement;
+import static org.sonar.java.matcher.TreeMatcher.hasSize;
+import static org.sonar.java.matcher.TreeMatcher.matching;
+import static org.sonar.java.matcher.TreeMatcher.statementAt;
+import static org.sonar.java.matcher.TreeMatcher.withBody;
+import static org.sonar.java.matcher.TreeMatcher.withExpression;
 
 @Rule(key = "S7478")
 public class UseTransformClassInsteadOfBuildCheck extends IssuableSubscriptionVisitor {
 
+  private static final String CLASS_MODEL_CLASSNAME = "java.lang.classfile.ClassModel";
+  private static final String CLASS_DESC_CLASSNAME = "java.lang.constant.ClassDesc";
+
+  private final MethodMatchers classFileBuildMatcher = MethodMatchers.create()
+    .ofTypes("java.lang.classfile.ClassFile")
+    .names("build")
+    .addParametersMatcher(
+      CLASS_DESC_CLASSNAME,
+      "java.util.function.Consumer")
+    .build();
+
+  /**
+   * Matcher for expressions of the form `x -> { for (ClassModel cm : m) { ... } }`.
+   *  Note that we handle only lambda expressions here, not method references. 
+   */
+  private final TreeMatcher<ExpressionTree> treeMatcher = TreeMatcher
+    .isLambdaExpression(
+      withBody(
+        hasSize(1).and(
+          statementAt(0,
+            forEachStatement(
+              withExpression(
+                matching(e -> e.symbolType().fullyQualifiedName().equals(CLASS_MODEL_CLASSNAME))))))));
+
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    // TODO: Specify the kind of nodes you want to be called to visit here.
-    return List.of();
+    return List.of(Tree.Kind.METHOD_INVOCATION);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    MethodInvocationTree methodInvocation = (MethodInvocationTree) tree;
+    if (classFileBuildMatcher.matches(methodInvocation) &&
+      treeMatcher.check(methodInvocation.arguments().get(1))) {
+      reportIssue(methodInvocation.methodSelect(), "Replace this 'build()' call with 'transformClass()'.");
+    }
   }
-  
+
 }
