b3f411b6d84e26ffb82b9dffa0a5ce19d4e9d1bd
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-07-03 13:45:08 +0200

    SONARJAVA-5668 Add sample compliant and non-compliant examples

--- java-checks-test-sources/default/src/main/java/checks/DefaultFinisherInGathererFactoryCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/DefaultFinisherInGathererFactoryCheckSample.java
@@ -1,5 +1,38 @@
 package checks;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Gatherer;
+
 public class DefaultFinisherInGathererFactoryCheckSample {
-  // TODO: Implement the sample class
+  Gatherer<Integer, AtomicInteger, Integer> noncompliantGatherer = Gatherer.ofSequential(
+    () -> new AtomicInteger(-1),
+    (state, number, downstream) -> {
+      if (state.get() < 0) {
+        state.set(number);
+        return true;
+      }
+      return downstream.push(number - state.get());
+    },
+    Gatherer.defaultFinisher()); // noncompliant {{Remove the default finisher from this Gatherer factory.}}
+
+  Gatherer<Integer, AtomicInteger, Integer> noncompliantGatherWithLambdaFinisher = Gatherer.ofSequential(
+    () -> new AtomicInteger(-1),
+    (state, number, downstream) -> {
+      if (state.get() < 0) {
+        state.set(number);
+        return true;
+      }
+      return downstream.push(number - state.get());
+    },
+    (state, number) -> { }); // noncompliant {{Remove the default finisher from this Gatherer factory.}}
+
+  Gatherer<Integer, AtomicInteger, Integer> compliantGatherer = Gatherer.ofSequential(
+    () -> new AtomicInteger(-1),
+    (state, number, downstream) -> {
+      if (state.get() < 0) {
+        state.set(number);
+        return true;
+      }
+      return downstream.push(number - state.get());
+    });
 }
