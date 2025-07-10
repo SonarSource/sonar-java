0ce64cf885ad7cefcf8abecb36720ee33ee65081
Alban Auzeill <alban.auzeill@sonarsource.com>
2025-07-10 14:09:46 +0200

    Fix from review 2

--- java-frontend/src/test/java/org/sonar/java/telemetry/AlphaNumericComparatorTest.java
+++ java-frontend/src/test/java/org/sonar/java/telemetry/AlphaNumericComparatorTest.java
@@ -25,25 +25,75 @@ class AlphaNumericComparatorTest {
 
   private static final AlphaNumericComparator COMP = new AlphaNumericComparator();
 
+  private static AbstractStringAssert<?> assertComp(String str) {
+    return assertThat(str).usingComparator(COMP);
+  }
+
   @Test
   void test_alpha_numeric_comparator() {
     assertThat(COMP.compare(null, null)).isZero();
     assertThat(COMP.compare(null, "")).isLessThan(0);
 
-    assertComp("").isGreaterThan(null).isEqualTo("").isLessThan("a");
-    assertComp("a").isGreaterThan("").isEqualTo("a").isLessThan("b");
-    assertComp("22").isGreaterThan("3").isEqualTo("22").isLessThan("100");
-    assertComp("2").isLessThan("a").isEqualTo("2").isLessThan("10");
-    assertComp("22abc22abc").isGreaterThan("3abc22abc").isEqualTo("22abc22abc").isLessThan("abc22abc");
-    assertComp("aaa").isGreaterThan("A").isEqualTo("aaa").isLessThan("bb");
-    assertComp("b").isGreaterThan("a").isEqualTo("b").isLessThan("c");
-    assertComp("cc").isGreaterThan("bbb").isEqualTo("cc").isLessThan("d22");
-    assertComp("abc123def").isGreaterThan("abc22def").isEqualTo("abc123def").isLessThan("abc99999999def");
-    assertComp("abc22abc").isGreaterThan("22abc22abc");
-    assertComp("abc22def").isLessThan("abc123def");
-    assertComp("abc22v1").isLessThan("abc22v1.12");
-    assertComp("abc22v1.6").isLessThan("abc22v1.12");
-    assertComp("abc22v2").isGreaterThan("abc22v1.12");
+    assertComp("")
+      .isGreaterThan(null)
+      .isEqualTo("")
+      .isLessThan("a");
+
+    assertComp("a")
+      .isGreaterThan("")
+      .isEqualTo("a")
+      .isLessThan("b");
+
+    assertComp("22")
+      .isGreaterThan("3")
+      .isEqualTo("22")
+      .isLessThan("100");
+
+    assertComp("2")
+      .isLessThan("a")
+      .isEqualTo("2")
+      .isLessThan("10");
+
+    assertComp("22abc22abc")
+      .isGreaterThan("3abc22abc")
+      .isEqualTo("22abc22abc")
+      .isLessThan("abc22abc");
+
+    assertComp("aaa")
+      .isGreaterThan("A")
+      .isEqualTo("aaa")
+      .isLessThan("bb");
+
+    assertComp("b")
+      .isGreaterThan("a")
+      .isEqualTo("b")
+      .isLessThan("c");
+
+    assertComp("cc")
+      .isGreaterThan("bbb")
+      .isEqualTo("cc")
+      .isLessThan("d22");
+
+    assertComp("abc123def")
+      .isGreaterThan("abc22def")
+      .isEqualTo("abc123def")
+      .isLessThan("abc99999999def");
+
+    assertComp("abc22abc")
+      .isGreaterThan("22abc22abc");
+
+    assertComp("abc22def")
+      .isLessThan("abc123def");
+
+    assertComp("abc22v1")
+      .isLessThan("abc22v1.12");
+
+    assertComp("abc22v1.6")
+      .isLessThan("abc22v1.12");
+
+    assertComp("abc22v2")
+      .isGreaterThan("abc22v1.12");
+
     assertComp("abc22v1.12")
       .isGreaterThan("abc22v1")
       .isGreaterThan("abc22v1.6")
@@ -51,10 +101,6 @@ class AlphaNumericComparatorTest {
       .isLessThan("abc22v1.20")
       .isLessThan("abc22v1.12beta")
       .isLessThan("abc22v2");
-  }
-
-  private AbstractStringAssert<?> assertComp(String str) {
-    return assertThat(str).usingComparator(COMP);
   }
 
 }
