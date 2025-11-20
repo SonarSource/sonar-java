29104ea1343214b1098c71756f40bab4845a7341
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2025-11-20 12:01:15 +0100

    Delete Cirrus CI config

--- check-license-compliance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# See https://xtranet.sonarsource.com/display/DEV/Open+Source+Licenses
-
-mvn org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party \
-  -Dlicense.missingFile=$(pwd)/missing-dep-licenses.properties \
-  -DuseMissingFile \
-  -Dlicense.overrideUrl=file://$(pwd)/override-dep-licenses.properties
