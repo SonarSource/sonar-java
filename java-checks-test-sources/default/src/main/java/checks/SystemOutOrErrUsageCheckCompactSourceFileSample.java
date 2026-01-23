090dccef582c44016621fb8a6316db9407b0a760
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2026-01-23 11:49:26 +0100

    WIP Unnamed Class

--- java-checks-test-sources/default/src/main/java/checks/SystemOutOrErrUsageCheckCompactSourceFileSample.java
+++ java-checks-test-sources/default/src/main/java/checks/SystemOutOrErrUsageCheckCompactSourceFileSample.java
@@ -1,7 +1,7 @@
 void main() {
   String name = IO.readln("Enter your name: ");
 
-  System.out.println("Goodbye " + name + "!");
+  System.out.println("Goodbye " + name + "!"); // Noncompliant
 
-  IO.println("Hello " + name + "!"); // Noncompliant
+  IO.println("Hello " + name + "!");
 }
