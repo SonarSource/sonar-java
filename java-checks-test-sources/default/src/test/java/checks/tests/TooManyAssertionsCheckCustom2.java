610dd3f7499ac4d85ba2a6588c303be69c18dda8
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-01-29 08:37:35 +0100

    SONARJAVA-5873 Fix the assertion counts in S5961 (#5409)

--- java-checks-test-sources/default/src/test/java/checks/tests/TooManyAssertionsCheckCustom2.java
+++ java-checks-test-sources/default/src/test/java/checks/tests/TooManyAssertionsCheckCustom2.java
@@ -1,8 +1,10 @@
 package checks.tests;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import rx.Observable;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class TooManyAssertionsCheckCustom2 {
@@ -35,6 +37,35 @@ public class TooManyAssertionsCheckCustom2 {
 //  ^^^^^^^^^^^^^^^^^^^^^<
     customAssert();
 //  ^^^^^^^^^^^^^^<
+  }
+
+  @Test
+  void test4() { // Compliant, chained assertions only count as a single instance
+    var trueCondition = true;
+    var falseCondition = false;
+
+    assertThat(trueCondition).isTrue().as("true").isTrue().describedAs("isTrue").isTrue();
+    assertThat(falseCondition).isFalse().as("false").isFalse().describedAs("isFalse").isFalse();
+  }
+
+  @Test
+  void test5() { // Compliant, chained assertions only count as a single instance
+    var myObject = new Object();
+
+    Assertions.assertThat(myObject).as("someObject").isNotNull().withFailMessage("fail").isNotNull().overridingErrorMessage("error").isInstanceOf(Object.class);
+  }
+
+  @Test
+  void test6() { // Noncompliant {{Refactor this method to reduce the number of assertions from 3 to less than 2.}}
+//     ^^^^^
+    var myObject = new Object();
+
+    assertThat(myObject).as("description").isNotNull().describedAs("description").isNotNull();
+//  ^^^^^^^^^^^^^^^^^^^^<
+    assertThat(myObject).describedAs("description").isNotNull();
+//  ^^^^^^^^^^^^^^^^^^^^<
+    assertThat(myObject).withFailMessage("failure").describedAs("someObject").isNotNull();
+//  ^^^^^^^^^^^^^^^^^^^^<
   }
 
   void customAssert() {
