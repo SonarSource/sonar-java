d5b0c88c1346df1a8f83c805502af35d5ce9deaa
romain.birling <romain.birling@sonarsource.com>
2026-02-04 11:40:23 +0100

    Skip new block instantation for implicit classes

--- java-checks-test-sources/default/src/main/java/checks/IndentationCheck_compactSource.java
+++ java-checks-test-sources/default/src/main/java/checks/IndentationCheck_compactSource.java
@@ -1,11 +1,11 @@
 // SONARJAVA-6028: FPs ahead. Only the line with "Too much." should be noncompliant.
 
-void main() { // Noncompliant
-  System.out.println("Just right."); // Noncompliant
+void main() {
+  System.out.println("Just right.");
   if (true) {
         System.out.println("Too much."); // Noncompliant
   }
 }
 
-class MyClass { // Noncompliant
+class MyClass {
 }
