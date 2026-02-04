04f6f9287ae6f8afc187471f7128351de634f3d1
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2026-02-04 13:16:32 +0100

    Fix unused import

--- java-checks/src/main/java/org/sonar/java/checks/SystemOutOrErrUsageCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/SystemOutOrErrUsageCheck.java
@@ -23,7 +23,6 @@ import org.sonar.plugins.java.api.tree.IdentifierTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
-import java.util.Collections;
 import java.util.List;
 
 @Rule(key = "S106")
