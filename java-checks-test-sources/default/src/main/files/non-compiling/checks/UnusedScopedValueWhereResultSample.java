5e071cbd55baf32ec192ac20008086d276000b84
romain.birling <romain.birling@sonarsource.com>
2026-02-05 15:28:44 +0100

    Add edge cases "multi-level carrier used" to test sample

--- java-checks-test-sources/default/src/main/files/non-compiling/checks/UnusedScopedValueWhereResultSample.java
+++ java-checks-test-sources/default/src/main/files/non-compiling/checks/UnusedScopedValueWhereResultSample.java
@@ -1,14 +1,19 @@
 static final ScopedValue myScopedValue = ScopedValue.newInstance();
 
 void main() {
-  var unusedCarrierGet = ScopedValue.where(myScopedValue, "hello").get(myScopedValue); // Noncompliant
+  ScopedValue.where(myScopedValue, "hello").get(myScopedValue); // Noncompliant
   ScopedValue.where(myScopedValue, "hello").run(() -> {
+  }); // Compliant, the result is used immediately
+  ScopedValue.where(myScopedValue, "hello").where(ScopedValue.newInstance(), "hello").run(() -> {
   }); // Compliant, the result is used immediately
   ScopedValue.where(myScopedValue, "hello"); // Noncompliant
   var myUnusedCarrier = ScopedValue.where(myScopedValue, "hello"); // Noncompliant
   var myUnused2LevelCarrier = ScopedValue.where(myScopedValue, "hello").where(ScopedValue.newInstance(), "hello"); // Noncompliant
   var myUsedCarrier = ScopedValue.where(myScopedValue, "hello"); // Compliant, the result is assigned to a variable and used
+  var myUsed2LevelCarrier = ScopedValue.where(myScopedValue, "hello").where(ScopedValue.newInstance(), "hello"); // Compliant, the result is assigned to a variable and used
   myUsedCarrier.run(() -> {
+  });
+  myUsed2LevelCarrier.run(() -> {
   });
 }
 
