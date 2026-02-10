912ec74157be8c4d9305f33085959518d09d92d7
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2026-02-10 17:11:01 +0100

    add compliant main()

--- java-checks-test-sources/default/src/main/java/checks/unused/UnusedMethodParameterCheckMainSample.java
+++ java-checks-test-sources/default/src/main/java/checks/unused/UnusedMethodParameterCheckMainSample.java
@@ -4,4 +4,8 @@ public class UnusedMethodParameterCheckMainSample {
   public static void main(String[] args) { // Noncompliant {{Remove this unused method parameter "args".}}
     System.out.println("Hello World!");
   }
+
+  void main() { // Compliant
+    System.out.println("Hello World!");
+  }
 }
