78a6b1fb21d0447629c398ed4dde4a09215e5b16
Asya Vorobeva <asya.vorobeva@sonarsource.com>
2026-02-11 11:44:15 +0100

    SONARJAVA-6117 S8433 should not raise issue for classes without superclass declared (#5453)

--- java-checks-test-sources/default/src/main/files/non-compiling/checks/FlexibleConstructorBodyValidationCheckSample.java
+++ java-checks-test-sources/default/src/main/files/non-compiling/checks/FlexibleConstructorBodyValidationCheckSample.java
@@ -94,7 +94,7 @@ class FlexibleConstructorBodyValidationCheckSample {
     public LargeCoffee(String name) {
       super(100, 100);
       this.name = name;
-      if (!isValidName(name)) { // NonCompliant
+      if (!isValidName(name)) { // Noncompliant
         throw new IllegalArgumentException();
       }
     }
@@ -163,6 +163,26 @@ class FlexibleConstructorBodyValidationCheckSample {
             throw new IllegalArgumentException();
           }
         }
+      }
+    }
+  }
+
+  // Test when there is no superclass
+  static class NoSuperclassCoffee {
+    private int water;
+    private int milk;
+    private static final int MAX_SIZE = 500;
+
+    public NoSuperclassCoffee(int water, int milk) {
+      if (water + milk > MAX_SIZE) { // Compliant: no superclass and no this(...) call
+        throw new IllegalArgumentException();
+      }
+    }
+
+    public NoSuperclassCoffee(int water) {
+      this(water, 0);
+      if (water <= 0) { // Noncompliant {{Move this validation logic before the super() or this() call.}}
+        throw new IllegalArgumentException();
       }
     }
   }
