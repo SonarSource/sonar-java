ad56ce11ed17a7473f894da370ae25616fd4b3bc
rombirli <56340680+rombirli@users.noreply.github.com>
2026-02-13 08:29:19 +0100

    SONARJAVA-6113 : implement rule S8444 : Excessive logic before super() should not bloat constructor (#5452)

---
+++ java-checks/src/main/java/org/sonar/java/checks/PresuperLogicBloatsConstructorCheck.java
@@ -0,0 +1,53 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
+import org.sonar.java.ast.visitors.StatementVisitor;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.StatementTree;
+
+@Rule(key = "S8444")
+public class PresuperLogicBloatsConstructorCheck extends FlexibleConstructorVisitor {
+
+  private static final int DEFAULT_STATEMENTS_THRESHOLD = 5;
+  @RuleProperty(
+    key = "statementsThreshold",
+    description = "Maximum number of statements allowed before the constructor call.",
+    defaultValue = "" + DEFAULT_STATEMENTS_THRESHOLD)
+  public int statementsThreshold = DEFAULT_STATEMENTS_THRESHOLD;
+
+  @Override
+  void validateConstructor(MethodTree constructor, List<StatementTree> body, int constructorCallIndex) {
+    if (constructorCallIndex < 0) {
+      // No constructor call, nothing to check
+      return;
+    }
+    StatementVisitor statementVisitor = new StatementVisitor();
+    int statementsBeforeConstructorCall = body.stream().limit(constructorCallIndex).map(statementVisitor::numberOfStatements).reduce(0, Integer::sum);
+    if (statementsBeforeConstructorCall > statementsThreshold) {
+      reportIssue(
+        body.get(0),
+        body.get(constructorCallIndex - 1),
+        "Excessive logic in this \"pre-construction\" phase makes the code harder to read and maintain."
+      );
+    }
+  }
+}
+
---
+++ java-checks/src/test/java/org/sonar/java/checks/PresuperLogicBloatsConstructorCheckTest.java
@@ -0,0 +1,51 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.sonar.java.checks.verifier.CheckVerifier;
+
+import static org.sonar.java.checks.verifier.TestUtils.nonCompilingTestSourcesPath;
+
+class PresuperLogicBloatsConstructorCheckTest {
+  private static final PresuperLogicBloatsConstructorCheck check = new PresuperLogicBloatsConstructorCheck();
+
+  @BeforeAll
+  static void setUp() {
+    check.statementsThreshold = 3;
+  }
+
+  @Test
+  void test() {
+
+    CheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/PresuperLogicShoudntBloatConstructorSample.java"))
+      .withCheck(check)
+      .withJavaVersion(25)
+      .verifyIssues();
+  }
+
+  @Test
+  void test_java_24() {
+    CheckVerifier.newVerifier()
+      .onFile(nonCompilingTestSourcesPath("checks/PresuperLogicShoudntBloatConstructorSample.java"))
+      .withCheck(check)
+      .withJavaVersion(24)
+      .verifyNoIssues();
+  }
+}
