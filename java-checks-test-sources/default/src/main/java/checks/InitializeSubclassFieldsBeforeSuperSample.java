c3849e1039cdac4e2008b09c9e7314f1954deeda
romain.birling <romain.birling@sonarsource.com>
2026-02-13 11:45:42 +0100

    Improve rule : resolve overrides (limited), not fully working

--- java-checks-test-sources/default/src/main/java/checks/InitializeSubclassFieldsBeforeSuperSample.java
+++ java-checks-test-sources/default/src/main/java/checks/InitializeSubclassFieldsBeforeSuperSample.java
@@ -10,6 +10,7 @@ public class InitializeSubclassFieldsBeforeSuperSample {
     protected ParentAbstract() {
       IO.println("name is " + getName());
     }
+
     abstract String getName();
   }
 
@@ -23,6 +24,7 @@ public class InitializeSubclassFieldsBeforeSuperSample {
     protected ParentConcreteHelper() {
       doInit();
     }
+
     void doInit() {
       IO.println("default init");
     }
@@ -32,15 +34,26 @@ public class InitializeSubclassFieldsBeforeSuperSample {
     protected ParentTransitive() {
       helper();
     }
+
     private void helper() {
       IO.println(getValue());
     }
+
     abstract String getValue();
+  }
+
+  abstract static class ParentAbstractInit {
+    protected ParentAbstractInit() {
+      init();
+    }
+
+    abstract void init();
   }
 
   static class ParentDirectFieldAccess {
     protected String usedField;
     protected String unusedField;
+
     protected ParentDirectFieldAccess() {
       IO.println("Value: " + usedField);
     }
@@ -51,17 +64,22 @@ public class InitializeSubclassFieldsBeforeSuperSample {
   // Field used via abstract method in super constructor
   public static class NonCompliant extends ParentAbstract {
     private final String name;
+
     NonCompliant(String name) {
       super();
       this.name = name; // Noncompliant
     }
+
     @Override
-    String getName() { return name; }
+    String getName() {
+      return name;
+    }
   }
 
   // Assignment in nested if/else block after super
   public static class NonCompliantNestedBlock extends ParentAbstract {
     private final String name;
+
     NonCompliantNestedBlock(String name, boolean condition) {
       super();
       if (condition) {
@@ -70,19 +88,26 @@ public class InitializeSubclassFieldsBeforeSuperSample {
         this.name = "default"; // Noncompliant
       }
     }
+
     @Override
-    String getName() { return name; }
+    String getName() {
+      return name;
+    }
   }
 
   // Field used transitively: super() → helper() → abstract getValue()
   public static class NonCompliantTransitive extends ParentTransitive {
     private final String data;
+
     NonCompliantTransitive(String data) {
       super();
       this.data = data; // Noncompliant
     }
+
     @Override
-    String getValue() { return data; }
+    String getValue() {
+      return data;
+    }
   }
 
   // Parent directly accesses inherited field in its constructor
@@ -93,17 +118,21 @@ public class InitializeSubclassFieldsBeforeSuperSample {
     }
   }
 
-  // Multiple fields, all conservatively flagged because super calls abstract method
+  // Multiple fields, all used in the child's getName() override
   public static class NonCompliantMultipleFieldsAbstract extends ParentAbstract {
     private String first;
     private String last;
+
     NonCompliantMultipleFieldsAbstract(String first, String last) {
       super();
       this.first = first; // Noncompliant
       this.last = last; // Noncompliant
     }
+
     @Override
-    String getName() { return first + " " + last; }
+    String getName() {
+      return first + " " + last;
+    }
   }
 
   // Mixed: only the inherited field actually used in parent constructor is flagged
@@ -115,22 +144,59 @@ public class InitializeSubclassFieldsBeforeSuperSample {
     }
   }
 
+  // Abstract method override uses the field
+  public static class NonCompliantAbstractOverrideUsesField extends ParentAbstractInit {
+    private String value;
+
+    NonCompliantAbstractOverrideUsesField(String value) {
+      super();
+      this.value = value; // Noncompliant
+    }
+
+    @Override
+    void init() {
+      IO.println(value);
+    }
+  }
+
+  // Abstract method override: multiple fields, only one used in override
+  public static class NonCompliantMixedAbstractOverride extends ParentAbstract {
+    private String name;
+    private int age;
+
+    NonCompliantMixedAbstractOverride(String name, int age) {
+      super();
+      this.name = name; // Noncompliant
+      this.age = age; // Compliant - getName() override doesn't use age
+    }
+
+    @Override
+    String getName() {
+      return name;
+    }
+  }
+
   // =================== Compliant cases ===================
 
   // Field assigned before super()
   public static class Compliant extends ParentAbstract {
     private final String name;
+
     Compliant(String name) {
       this.name = name;
       super();
     }
+
     @Override
-    String getName() { return name; }
+    String getName() {
+      return name;
+    }
   }
 
   // Super constructor doesn't use child's fields or overridable methods
   public static class CompliantFieldNotUsed extends ParentSimple {
     private String data;
+
     CompliantFieldNotUsed(String data) {
       super();
       this.data = data; // Compliant - ParentSimple doesn't use this field
@@ -140,6 +206,7 @@ public class InitializeSubclassFieldsBeforeSuperSample {
   // No explicit super() call (implicit super)
   public static class CompliantImplicitSuper extends ParentSimple {
     private String value;
+
     CompliantImplicitSuper(String value) {
       this.value = value; // Compliant - no explicit super()
     }
@@ -148,6 +215,7 @@ public class InitializeSubclassFieldsBeforeSuperSample {
   // Super calls concrete method that doesn't use child's field
   public static class CompliantConcreteMethod extends ParentConcreteHelper {
     private String extra;
+
     CompliantConcreteMethod(String extra) {
       super();
       this.extra = extra; // Compliant - doInit() doesn't use this field
@@ -157,13 +225,32 @@ public class InitializeSubclassFieldsBeforeSuperSample {
   // Local variable assignment after super (not a field)
   public static class CompliantLocalVar extends ParentAbstract {
     private final String name;
+
     CompliantLocalVar(String name) {
       this.name = name;
       super();
       String localVar = "test"; // Compliant - not a field assignment
     }
+
     @Override
-    String getName() { return name; }
+    String getName() {
+      return name;
+    }
+  }
+
+  // Abstract method override does NOT use the field
+  public static class CompliantAbstractOverrideNotUsingField extends ParentAbstractInit {
+    private String unused;
+
+    CompliantAbstractOverrideNotUsingField(String unused) {
+      super();
+      this.unused = unused; // Compliant - init() override doesn't use this field
+    }
+
+    @Override
+    void init() {
+      IO.println("init without field");
+    }
   }
 
   // Only the unused inherited field is assigned - not the one read in parent constructor
@@ -171,6 +258,67 @@ public class InitializeSubclassFieldsBeforeSuperSample {
     CompliantUnusedInheritedField(String value) {
       super();
       this.unusedField = value; // Compliant - only usedField is read in parent constructor
+    }
+  }
+
+  static class TrickyOverridesChain {
+    abstract class A {
+      A() {
+        hello();
+      }
+
+      abstract void hello();
+
+    }
+
+    abstract class B extends A {
+      protected final String name;
+
+      B() {
+        super();
+        this.name = "name"; // Noncompliant
+      }
+
+      abstract void hello();
+    }
+
+    abstract class C extends B {
+      C() {
+        super();
+      }
+
+      @Override
+      void hello() {
+        IO.println(name);
+      }
+    }
+  }
+
+
+  static class NonAbstractOverridesChain {
+    class A {
+      A() {
+        hello();
+      }
+
+      void hello() {
+        IO.println("hello");
+      }
+
+    }
+
+    class B extends A {
+      protected final String name;
+
+      B() {
+        super();
+        this.name = "name"; // TODO : FN here
+      }
+
+      @Override
+      void hello() {
+        IO.println(name);
+      }
     }
   }
 }
