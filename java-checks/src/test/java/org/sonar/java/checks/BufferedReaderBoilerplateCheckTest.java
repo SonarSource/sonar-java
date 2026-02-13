780247b09869e0eb041b0c49ae4f068056c313f9
romain.birling <romain.birling@sonarsource.com>
2026-02-13 16:01:26 +0100

    Try to fix QG | 1 - remove unnecessary imports

--- java-checks/src/test/java/org/sonar/java/checks/BufferedReaderBoilerplateCheckTest.java
+++ java-checks/src/test/java/org/sonar/java/checks/BufferedReaderBoilerplateCheckTest.java
@@ -19,7 +19,6 @@ package org.sonar.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
 import static org.sonar.java.checks.verifier.TestUtils.nonCompilingTestSourcesPath;
 
 class BufferedReaderBoilerplateCheckTest {
