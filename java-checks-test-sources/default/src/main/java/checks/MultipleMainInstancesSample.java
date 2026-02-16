a6613feb175e31ec989e7e8ac7400abb2a7dad2e
romain.birling <romain.birling@sonarsource.com>
2026-02-16 11:08:06 +0100

    Extend rule implementation - detect multiple main methods due to overrides

--- java-checks-test-sources/default/src/main/java/checks/MultipleMainInstancesSample.java
+++ java-checks-test-sources/default/src/main/java/checks/MultipleMainInstancesSample.java
@@ -1,6 +1,6 @@
 public class MultipleMainInstancesSample {
   public static class NonCompliant {
-    public static void main(String[] args) { // Noncompliant
+    public static void main(String[] args) { // Noncompliant {{At most one main method should be defined in a class.}}
 //                     ^^^^
       System.out.println("Static main detected; shadowing instance main.");
     }
@@ -56,8 +56,103 @@ public class MultipleMainInstancesSample {
     }
 
     void main() {
-
       System.out.println("Unreachable entry point in record due to static precedence.");
+    }
+  }
+
+  public static class NonCompliantWithOverloads {
+    class Parent {
+      void main() {
+        System.out.println("Parent instance main method.");
+      }
+    }
+
+    class Child extends Parent {
+      public static void main(String[] args) { // Noncompliant {{Main method should not be defined in a class if a main method is already defined in a superclass.}}
+        System.out.println("Static main in child class detected; shadowing instance main.");
+      }
+    }
+  }
+
+  public static class CompliantWithOverloads {
+    class Parent {
+      void main() {
+        System.out.println("Parent instance main method.");
+      }
+    }
+
+    class Child extends Parent {
+      @Override
+      void main() { // Compliant: This is an instance method that overrides the parent main
+        System.out.println("Child instance main method overriding parent main.");
+      }
+    }
+  }
+
+  public static class TwoMainsInParentOneOverriddenInChild {
+    class Parent {
+      void main() { // Noncompliant {{At most one main method should be defined in a class.}}
+        System.out.println("Parent void main method.");
+      }
+
+      void main(String[] args) {
+        System.out.println("Parent String args main method.");
+      }
+    }
+
+    class Child extends Parent {
+      @Override
+      void main() { // Compliant: This is an instance method that overrides the parent main
+        System.out.println("Child instance main method overriding parent main.");
+      }
+    }
+  }
+
+  public static class TwoMainsInParentTwoOverriddenInChild {
+    class Parent {
+      void main() { // Noncompliant {{At most one main method should be defined in a class.}}
+        System.out.println("Parent void main method.");
+      }
+
+      void main(String[] args) {
+        System.out.println("Parent String args main method.");
+      }
+    }
+
+    class Child extends Parent {
+      @Override
+      void main() { // Noncompliant {{At most one main method should be defined in a class.}}
+        System.out.println("Child instance main method overriding parent main.");
+      }
+
+      @Override
+      void main(String[] args) {
+        System.out.println("Child String args main method overriding parent main.");
+      }
+    }
+  }
+
+  public static class ChainedOverrides {
+    class GrandParent {
+      void main() {
+        System.out.println("Parent void main method.");
+      }
+    }
+
+    class Parent extends GrandParent {
+    }
+
+    class NonCompliantChild extends Parent {
+      void main(String[] args) { // Noncompliant {{Main method should not be defined in a class if a main method is already defined in a superclass.}}
+        System.out.println("Child main method detected; shadowing grandparent main.");
+      }
+    }
+
+    class CompliantChild extends Parent {
+      @Override
+      void main() { // Compliant: This is an instance method that overrides the parent main
+        System.out.println("Child instance main method overriding grandparent main.");
+      }
     }
   }
 }
