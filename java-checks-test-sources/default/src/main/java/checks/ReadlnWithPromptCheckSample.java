f6477c0f029af1d9f984b768c6aedd08758cd683
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-02-27 13:29:01 +0100

    Fix the error messages in the tests

--- java-checks-test-sources/default/src/main/java/checks/ReadlnWithPromptCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/ReadlnWithPromptCheckSample.java
@@ -6,17 +6,17 @@ public class ReadlnWithPromptCheckSample {
 
   void nonCompliant() {
     IO.print("Enter your name: ");
-    String name = IO.readln(); // Noncompliant {{Use "IO.readln(prompt)" instead of separate "IO.print(prompt)" and "IO.readln()" calls.}}
+    String name = IO.readln(); // Noncompliant {{Use "IO.readln(String prompt)" instead of separate "IO.print(Object obj)" and "IO.readln()" calls.}}
 //                ^^^^^^^^^^^
     IO.print(name);
 
     IO.println("Enter your age: ");
-    String age = IO.readln(); // Noncompliant {{Use "IO.readln(prompt)" instead of separate "IO.println(prompt)" and "IO.readln()" calls.}}
+    String age = IO.readln(); // Noncompliant {{Use "IO.readln(String prompt)" instead of separate "IO.println(Object obj)" and "IO.readln()" calls.}}
 //               ^^^^^^^^^^^
     IO.print(age);
 
     IO.print("Enter city: ");
-    IO.readln(); // Noncompliant {{Use "IO.readln(prompt)" instead of separate "IO.print(prompt)" and "IO.readln()" calls.}}
+    IO.readln(); // Noncompliant {{Use "IO.readln(String prompt)" instead of separate "IO.print(Object obj)" and "IO.readln()" calls.}}
 //  ^^^^^^^^^^^
   }
 
