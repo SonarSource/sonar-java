49377ab11bd8118be10f8a3f9aaf977b897a8140
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-03-02 09:55:03 +0100

    SONARJAVA-6106 ScopedValue instances should be assigned to a stable reference (#5480)

---
+++ java-checks-test-sources/default/src/main/java/checks/ScopedValueStableReferenceCheckSample.java
@@ -0,0 +1,52 @@
+package checks;
+
+import java.util.Objects;
+import org.apache.commons.lang3.tuple.Pair;
+
+class ScopedValueStableReferenceCheckSample {
+
+  private static final ScopedValue<String> VALUE = ScopedValue.newInstance();
+
+  public void where() {
+    ScopedValue.where(ScopedValue.newInstance(), "inaccessible").run(() -> { // Noncompliant {{Consider using a stable reference for ScopedValue instances.}}
+//                    ^^^^^^^^^^^^^^^^^^^^^^^^^
+      // Cannot reference the scoped value here, as it has no name.
+    });
+  }
+
+  public void chainedWhere() {
+    ScopedValue<String> scopedValue = ScopedValue.newInstance();
+    ScopedValue.where(scopedValue, "accessible").where(ScopedValue.newInstance(), "inaccessible").run(() -> { // Noncompliant {{Consider using a stable reference for ScopedValue instances.}}
+//                                                     ^^^^^^^^^^^^^^^^^^^^^^^^^
+    });
+  }
+
+  public void nestedArgument() {
+    ScopedValue.where(Objects.requireNonNull(ScopedValue.newInstance()), "scopedValue").run(() -> {}); // Noncompliant {{Consider using a stable reference for ScopedValue instances.}}
+//                                           ^^^^^^^^^^^^^^^^^^^^^^^^^
+    ScopedValue.where((ScopedValue.newInstance()), "scopedValue").run(() -> {}); // Noncompliant {{Consider using a stable reference for ScopedValue instances.}}
+//                     ^^^^^^^^^^^^^^^^^^^^^^^^^
+    ScopedValue.where(Pair.of(ScopedValue.newInstance(), 0).getLeft(), "scopedValue").run(() -> {}); // Noncompliant {{Consider using a stable reference for ScopedValue instances.}}
+//                            ^^^^^^^^^^^^^^^^^^^^^^^^^
+    ScopedValue<String> scopedValue;
+    ScopedValue.where(Pair.of((scopedValue = ScopedValue.newInstance()), ScopedValue.newInstance()).getLeft(), "scopedValue").run(scopedValue::get); // Noncompliant {{Consider using a stable reference for ScopedValue instances.}}
+//                                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^
+  }
+
+  public String readFieldInWhere() {
+    return ScopedValue.where(VALUE, "field value").call(VALUE::get); // Compliant
+  }
+
+  public String readLocalVarInWhere() {
+    ScopedValue<String> value = ScopedValue.newInstance();
+    return ScopedValue.where(value, "local value").call(value::get); // Compliant
+  }
+
+  public String assignLocalVarInWhere() {
+    ScopedValue<String> value;
+    String result = ScopedValue.where((value = ScopedValue.newInstance()), "local value").call(value::get); // Compliant
+    result = ScopedValue.where((value = ((ScopedValue.newInstance()))), "local value").call(value::get); // Compliant
+    return result;
+  }
+
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/ScopedValueStableReferenceCheck.java
@@ -0,0 +1,91 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import org.sonar.check.Rule;
+import org.sonar.java.checks.methods.AbstractMethodDetection;
+import org.sonar.plugins.java.api.JavaVersion;
+import org.sonar.plugins.java.api.JavaVersionAwareVisitor;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S8465")
+public class ScopedValueStableReferenceCheck extends AbstractMethodDetection implements JavaVersionAwareVisitor {
+
+  private static final String MESSAGE = "Consider using a stable reference for ScopedValue instances.";
+
+  private static final MethodMatchers WHERE_MATCHER = MethodMatchers.create()
+    .ofTypes("java.lang.ScopedValue", "java.lang.ScopedValue$Carrier")
+    .names("where")
+    .withAnyParameters()
+    .build();
+
+  @Override
+  public boolean isCompatibleWithJavaVersion(JavaVersion version) {
+    return version.isJava25Compatible();
+  }
+
+  @Override
+  protected MethodMatchers getMethodInvocationMatchers() {
+    return WHERE_MATCHER;
+  }
+
+  @Override
+  protected void onMethodInvocationFound(MethodInvocationTree methodInvocation) {
+    var finder = new NewInstanceInvocationFinder();
+    methodInvocation.arguments()
+      .get(0)
+      .accept(finder);
+    MethodInvocationTree newInstanceInvocation = finder.invocation;
+    if (newInstanceInvocation != null) {
+      reportIssue(newInstanceInvocation, MESSAGE);
+    }
+  }
+
+  private static class NewInstanceInvocationFinder extends BaseTreeVisitor {
+
+    private static final MethodMatchers NEW_INSTANCE_MATCHER = MethodMatchers.create()
+      .ofTypes("java.lang.ScopedValue")
+      .names("newInstance")
+      .addWithoutParametersMatcher()
+      .build();
+
+    private MethodInvocationTree invocation = null;
+
+    @Override
+    public void visitMethodInvocation(MethodInvocationTree methodInvocation) {
+      Tree parent = nearestNonParenthesizedParent(methodInvocation);
+      if (NEW_INSTANCE_MATCHER.matches(methodInvocation.methodSymbol()) && parent != null && !parent.is(Tree.Kind.ASSIGNMENT)) {
+        invocation = methodInvocation;
+        return;
+      }
+      super.visitMethodInvocation(methodInvocation);
+    }
+
+    private static Tree nearestNonParenthesizedParent(Tree tree) {
+      Tree parent = tree.parent();
+      while (parent != null && parent.is(Tree.Kind.PARENTHESIZED_EXPRESSION)) {
+        parent = parent.parent();
+      }
+      return parent;
+    }
+
+  }
+
+}
