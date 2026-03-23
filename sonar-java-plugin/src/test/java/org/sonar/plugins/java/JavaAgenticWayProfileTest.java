6bc7087116fed7c40f124ee0edebe884033790ae
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2026-03-23 17:56:41 +0100

    SONARJAVA-6205 Add test script to generate profile from CSV file

--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaAgenticWayProfileTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/JavaAgenticWayProfileTest.java
@@ -17,14 +17,27 @@
 package org.sonar.plugins.java;
 
 
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.commons.csv.CSVFormat;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JavaAgenticWayProfileTest {
+
+  private static final Path RULE_DESCRIPTION_DIRECTORY = Path.of("sonar-java-plugin", "src", "main", "resources", "org", "sonar", "l10n", "java", "rules", "java");
 
   @Test
   void profile_is_registered_as_expected() {
@@ -103,5 +116,83 @@ class JavaAgenticWayProfileTest {
         "S6912",
         "S8491"
       ));
+  }
+
+  @Disabled("This method should be used as a utility method to generate a quality profile from a CSV file")
+  void generate_ai_quality_profile() throws IOException {
+    generate(
+      Path.of("Path", "to", "your", "input.csv"),
+      RULE_DESCRIPTION_DIRECTORY.resolve("Agentic_way_profile.json"),
+      "AI Quality Profile"
+    );
+  }
+
+  public static Path generate(Path input, Path output, String profileName) throws IOException {
+    CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
+      .setHeader()
+      .setSkipHeaderRecord(true)
+      .get();
+
+    Set<String> keysOfImplementedRules = getImplementedRuleKeys();
+
+    String ruleKeys;
+    try (FileReader in = new FileReader(input.toFile(), StandardCharsets.UTF_8)) {
+      ruleKeys = csvFormat.parse(in).stream()
+        // Filter rules that have not been classified as AI and Sonar Way
+        .filter(ruleRecord -> "AI and Sonar way".equalsIgnoreCase(ruleRecord.get("classification_status")))
+        // Recover keys
+        .map(ruleRecord -> ruleRecord.get("ruleid"))
+        // Filter out keys that do not have a sonar-java implementation
+        .filter(keysOfImplementedRules::contains)
+        // Sort keys
+        .sorted(JavaAgenticWayProfileTest::compareRuleKeys)
+        // Surround with double quotes for output in JSON document
+        .map("    \"%s\""::formatted)
+        // Comma and line separate rule keys
+        .collect(Collectors.joining(",%s".formatted(System.lineSeparator())));
+    }
+    try (
+      FileWriter out = new FileWriter(output.toFile(), StandardCharsets.UTF_8);
+      BufferedWriter writer = new BufferedWriter(out)
+    ) {
+      writer.write("{");
+      writer.newLine();
+      writer.write("  \"name\": \"%s\",".formatted(profileName));
+      writer.newLine();
+      writer.write("  \"ruleKeys\": [");
+      writer.newLine();
+      writer.write(ruleKeys);
+      writer.newLine();
+      writer.write("  ]");
+      writer.newLine();
+      writer.write("}");
+    }
+    return output;
+
+  }
+
+  private static Set<String> getImplementedRuleKeys() throws IOException {
+    if (!Files.isDirectory(RULE_DESCRIPTION_DIRECTORY)) {
+      throw new IllegalStateException("This should not happen!");
+    }
+    return Files.list(RULE_DESCRIPTION_DIRECTORY)
+      .filter(path -> !path.endsWith("_profile.json"))
+      .map(path -> {
+        String fileName = path.getFileName().toString();
+        return fileName.substring(0, fileName.lastIndexOf('.'));
+      })
+      .collect(Collectors.toSet());
+  }
+
+  private static Integer getSortingKey(String ruleKey) {
+    try {
+      return Integer.parseInt(ruleKey.substring(1));
+    } catch (NumberFormatException ignored) {
+      return Integer.MIN_VALUE;
+    }
+  }
+
+  private static int compareRuleKeys(String first, String second) {
+    return getSortingKey(first) - getSortingKey(second);
   }
 }
