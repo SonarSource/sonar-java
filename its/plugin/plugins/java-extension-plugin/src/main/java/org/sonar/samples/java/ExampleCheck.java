643e39b208e5cc0f34fb6e5730843d2e0509c99f
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2026-04-01 11:44:07 +0200

    SONARJAVA-6229 Upgrade parent pom to version 87.0.0.3057

--- its/plugin/plugins/java-extension-plugin/src/main/java/org/sonar/samples/java/ExampleCheck.java
+++ its/plugin/plugins/java-extension-plugin/src/main/java/org/sonar/samples/java/ExampleCheck.java
@@ -1,10 +1,10 @@
 /*
  * SonarQube Java
- * Copyright (C) 2013-2025 SonarSource Sàrl
+ * Copyright (C) SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
