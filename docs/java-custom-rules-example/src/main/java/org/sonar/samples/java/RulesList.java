343350f95466fcaae917d748faeff9a2ea725b88
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2026-04-01 21:38:05 +0200

    SONARJAVA-6229 Update license headers for custom plugin template

--- docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/RulesList.java
+++ docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/RulesList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) ${license.years} SonarSource Sàrl - mailto:info AT sonarsource DOT com
+ * Copyright (C) SonarSource Sàrl - mailto:info AT sonarsource DOT com
  * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
  */
 package org.sonar.samples.java;
