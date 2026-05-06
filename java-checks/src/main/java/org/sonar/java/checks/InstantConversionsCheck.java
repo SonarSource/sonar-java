e623394fa9e777f7395f088fa0a6e744772fb5f2
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-05-06 11:21:24 +0200

    Remove unused import

--- java-checks/src/main/java/org/sonar/java/checks/InstantConversionsCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/InstantConversionsCheck.java
@@ -19,7 +19,6 @@ package org.sonar.java.checks;
 import java.util.Arrays;
 import org.sonar.check.Rule;
 import org.sonar.java.checks.methods.AbstractMethodDetection;
-import org.sonar.java.model.ExpressionUtils;
 import org.sonar.plugins.java.api.JavaVersion;
 import org.sonar.plugins.java.api.JavaVersionAwareVisitor;
 import org.sonar.plugins.java.api.semantic.MethodMatchers;
