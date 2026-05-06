8e6a95494cbcd826c28403d4cbe28d9b6e692c53
Noémie Bénard <noemie.benard@sonarsource.com>
2026-05-06 14:20:02 +0200

    add more tests

--- java-checks-test-sources/default/src/main/java/checks/NowWithoutParametersCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/NowWithoutParametersCheckSample.java
@@ -1,5 +1,7 @@
 package checks;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -8,7 +10,9 @@ import java.time.OffsetDateTime;
 import java.time.OffsetTime;
 import java.time.Year;
 import java.time.YearMonth;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class NowWithoutParametersCheckSample {
 
@@ -31,7 +35,27 @@ public class NowWithoutParametersCheckSample {
     //                              ^^^^^
     ZonedDateTime zonedDateTime = ZonedDateTime.now(); // Noncompliant {{Explicitly specify the time zone by passing a ZoneId or a Clock to the .now() method.}}
     //                                          ^^^^^
+  }
 
+  void nowWithParameters(Clock clock, ZoneId zoneId) {
+    LocalDateTime dateTime = LocalDateTime.now(ZoneId.systemDefault());
+    LocalDate date = LocalDate.now(clock);
+    LocalTime time = LocalTime.now(ZoneId.of("Europe/Zurich"));
+    MonthDay monthDay = MonthDay.now(ZoneId.of("UTC"));
+    OffsetDateTime offsetDateTime = OffsetDateTime.now(zoneId);
+  }
+
+  void nowOnInstant() {
+    Instant instant = Instant.now(); // Compliant, instant doesn't need a timezone
+  }
+
+  Year nowOnOtherPatterns() {
+    System.out.println(OffsetTime.now()); // Noncompliant {{Explicitly specify the time zone by passing a ZoneId or a Clock to the .now() method.}}
+    //                            ^^^^^
+    String formatted = ZonedDateTime.now().format(DateTimeFormatter.ISO_DATE); // Noncompliant {{Explicitly specify the time zone by passing a ZoneId or a Clock to the .now() method.}}
+    //                               ^^^^^
+    return Year.now(); // Noncompliant {{Explicitly specify the time zone by passing a ZoneId or a Clock to the .now() method.}}
+    //          ^^^^^
   }
 
 }
