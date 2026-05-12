b6bd7ad4a07484bbc27d33af81393603f22283de
Noémie Bénard <noemie.benard@sonarsource.com>
2026-05-12 08:59:19 +0200

    add example with calendar subclass

--- java-checks/src/test/files/checks/DateAndTimesCheckWildcardImport.java
+++ java-checks/src/test/files/checks/DateAndTimesCheckWildcardImport.java
@@ -7,5 +7,7 @@ class A {
     Calendar christmas  = Calendar.getInstance(); // Noncompliant
     //                    ^^^^^^^^^^^^^^^^^^^^^^
     Timestamp timestamp = new Timestamp(1735689600000L); // Compliant, the rule ignores this case
+    GregorianCalendar calendar = GregorianCalendar.getInstance(); // Noncompliant {{Use the Java 8 Date and Time API instead.}}
+    //                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   }
 }
