d0f79942d09616e41f7ef12353221ad7f4783aed
lpilastri <leonardo.pilastri@sonarsource.com>
2026-05-28 09:51:08 +0200

    Fix incompatible mvn toolchain

--- its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
+++ its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
@@ -194,7 +194,9 @@ public class JavaRulingTest {
     MavenBuild build = test_project("com.macro.mall:mall", projectName);
     build
       .setProperty("docker.skip", "true")
-      .setProperty("java.version", "17");
+      .setProperty("java.version", "17")
+      .setProperty("maven-bundle-plugin.version", "5.1.4")
+      .setProperty("maven.javadoc.skip", "true");
     executeBuildWithCommonProperties(build, projectName);
   }
 
