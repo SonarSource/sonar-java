ae7950073d145e0b039ca34a21ee58b1bdc4c70c
rombirli <romain.birling@sonarsource.com>
2026-05-29 13:09:49 +0200

    Improve sample (more reproducers)

--- java-checks-test-sources/default/src/main/java/checks/AssertThrowsInsteadOfTryCatchFailCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/AssertThrowsInsteadOfTryCatchFailCheckSample.java
@@ -22,15 +22,63 @@ public class AssertThrowsInsteadOfTryCatchFailCheckSample {
       fail();
     }
 
-    assertThrows(IllegalStateException.class, AssertThrowsInsteadOfTryCatchFailCheckSample::raise);
-    assertDoesNotThrow(AssertThrowsInsteadOfTryCatchFailCheckSample::dontRaise);
+    try { // Noncompliant {{Use assertDoesNotThrow() instead of try/catch and fail() in the try block.}}
+//      ^[el=+4;ec=5]
+      raise();
+      org.junit.Assert.fail("expected exception");
+    } catch (Exception _) {
+      // test passed
+    }
+
+    try { // Noncompliant {{Use assertDoesNotThrow() instead of try/catch and fail() in the try block.}}
+//      ^[el=+4;ec=5]
+      raise();
+      junit.framework.Assert.fail("expected exception");
+    } catch (Exception _) {
+      // test passed
+    }
+
+    try { // Noncompliant {{Use assertDoesNotThrow() instead of try/catch and fail() in the try block.}}
+//      ^[el=+4;ec=5]
+      raise();
+      org.fest.assertions.Fail.fail("expected exception");
+    } catch (Exception _) {
+      // test passed
+    }
+
+    try { // Noncompliant {{Use assertDoesNotThrow() instead of try/catch and fail() in the try block.}}
+//      ^[el=+4;ec=5]
+      raise();
+      org.assertj.core.api.Fail.fail("expected exception");
+    } catch (Exception _) {
+      // test passed
+    }
+
+    try { // Noncompliant {{Use assertDoesNotThrow() instead of try/catch and fail() in the try block.}}
+//      ^[el=+4;ec=5]
+      raise();
+      org.assertj.core.api.Assertions.fail("expected exception");
+    } catch (Exception _) {
+      // test passed
+    }
+
+    try { // Noncompliant {{Use assertDoesNotThrow() instead of try/catch and fail() in the try block.}}
+//      ^[el=+4;ec=5]
+      raise();
+      org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown(IllegalStateException.class);
+    } catch (Exception _) {
+      // test passed
+    }
+
+    assertThrows(IllegalStateException.class, AssertThrowsInsteadOfTryCatchFailCheckSample::raise); // compliant
+    assertDoesNotThrow(AssertThrowsInsteadOfTryCatchFailCheckSample::dontRaise); // non-compliant
   }
 
   private static void raise() {
     throw new IllegalStateException();
   }
 
-  private static void dontRaise(){
+  private static void dontRaise() {
     // do nothing
   }
 }
