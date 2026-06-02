211440a254592df8c659dbc200477e25f5bbe1e7
rombirli <romain.birling@sonarsource.com>
2026-06-02 16:44:49 +0200

    quickfix skeleton

--- java-checks/src/main/java/org/sonar/java/checks/AssertThrowsInsteadOfTryCatchFailCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/AssertThrowsInsteadOfTryCatchFailCheck.java
@@ -17,7 +17,10 @@
 package org.sonar.java.checks;
 
 import org.sonar.check.Rule;
+import org.sonar.java.checks.helpers.QuickFixHelper;
 import org.sonar.java.checks.helpers.UnitTestUtils;
+import org.sonar.java.reporting.JavaQuickFix;
+import org.sonar.java.reporting.JavaTextEdit;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
 import org.sonar.plugins.java.api.tree.BlockTree;
@@ -25,7 +28,9 @@ import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.TryStatementTree;
 
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Optional;
 
 @Rule(key = "S8714")
 public class AssertThrowsInsteadOfTryCatchFailCheck extends IssuableSubscriptionVisitor {
@@ -44,24 +49,53 @@ public class AssertThrowsInsteadOfTryCatchFailCheck extends IssuableSubscription
   }
 
   private final class TryStatementsVisitor extends BaseTreeVisitor {
-    private final boolean isJunitJupiter;
+    private final boolean isJunit56;
 
-    public TryStatementsVisitor(boolean isJunitJupiter) {
-      this.isJunitJupiter = isJunitJupiter;
+    public TryStatementsVisitor(boolean isJunit56) {
+      this.isJunit56 = isJunit56;
     }
 
     @Override
     public void visitTryStatement(TryStatementTree tree) {
-      checkBlock(tree.block(), "Use assertThrows() instead of try/catch and fail() in the try block.");
-      tree.catches().forEach(c -> checkBlock(c.block(), "Use assertDoesNotThrow() instead of try/catch and fail() in the catch block."));
+      checkBlock(tree.block(), tree, true, "Use assertThrows() instead of try/catch and fail() in the try block.");
+      tree.catches().forEach(c ->
+        checkBlock(c.block(), tree, false, "Use assertDoesNotThrow() instead of try/catch and fail() in the catch block.")
+      );
       super.visitTryStatement(tree);
     }
 
-    private void checkBlock(BlockTree block, String message) {
+    private void checkBlock(BlockTree block, TryStatementTree tryStatement, boolean isTryBlock, String message) {
       UnitTestUtils.findFail(block).ifPresent(failMethodInvocation -> {
-          if (isJunitJupiter || failMethodInvocation.methodSymbol().signature().contains("org.assertj")) {
-            reportIssue(failMethodInvocation, message);
+
+          @Nullable String replacement = "";
+          Optional<String> failArgument = failMethodInvocation.arguments().;
+
+          if (isJunit56) {
+            if (isTryBlock) {
+
+            } else {
+
+            }
+          } else if (failMethodInvocation.methodSymbol().signature().contains("org.assertj")) {
+            if (isTryBlock) {
+
+            } else {
+
+            }
           }
+
+          if (replacement == null) {
+            return;
+          }
+
+          var quickfix = JavaQuickFix.newQuickFix(message).addTextEdit(JavaTextEdit.replaceTree(tryStatement, replacement)).build();
+
+          QuickFixHelper
+            .newIssue(AssertThrowsInsteadOfTryCatchFailCheck.this.context)
+            .forRule(AssertThrowsInsteadOfTryCatchFailCheck.this)
+            .withMessage(message)
+            .withQuickFix(() -> quickfix)
+            .onTree(failMethodInvocation);
         }
       );
     }
