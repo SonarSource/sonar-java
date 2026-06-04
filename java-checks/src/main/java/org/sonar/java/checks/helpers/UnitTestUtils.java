15732a4941c427863d1f52f0c2a499dac46ba8ad
rombirli <romain.birling@sonarsource.com>
2026-06-04 16:48:08 +0200

    Fix QG, remove import java.util.*

--- java-checks/src/main/java/org/sonar/java/checks/helpers/UnitTestUtils.java
+++ java-checks/src/main/java/org/sonar/java/checks/helpers/UnitTestUtils.java
@@ -16,7 +16,11 @@
  */
 package org.sonar.java.checks.helpers;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
