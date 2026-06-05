6f2b7c8edfec9f8564c61c9488ad892ac9dbea8d
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2026-06-05 16:49:05 +0200

    Fix issue after rebase

--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/SpringContextModelSensorTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/SpringContextModelSensorTest.java
@@ -1,3 +1,19 @@
+/*
+ * SonarQube Java
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
 package org.sonar.plugins.java;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +29,7 @@ class SpringContextModelSensorTest {
     DefaultSensorDescriptor descriptor = new DefaultSensorDescriptor();
     SpringContextModelSensor sensor = new SpringContextModelSensor(new SpringContextModel());
     sensor.describe(descriptor);
-    assertThat(descriptor.name()).isEqualTo("Java SpringContextModelSensor.");
+    assertThat(descriptor.name()).isEqualTo("Java SpringContextModelSensor");
     assertThat(descriptor.languages()).containsExactly("java", "jsp");
   }
 
