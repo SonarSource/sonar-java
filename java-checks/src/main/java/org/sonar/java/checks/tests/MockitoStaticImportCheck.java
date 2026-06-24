2210c361c2380222323f0e119e8ec81d815ceb6e
Noémie Bénard <noemie.benard@sonarsource.com>
2026-06-24 14:47:13 +0200

    remove redundant check

--- java-checks/src/main/java/org/sonar/java/checks/tests/MockitoStaticImportCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/tests/MockitoStaticImportCheck.java
@@ -51,7 +51,7 @@ public class MockitoStaticImportCheck extends IssuableSubscriptionVisitor {
     }
     MemberSelectExpressionTree mset = (MemberSelectExpressionTree) methodSelect;
     String methodName = mset.identifier().name();
-    if (MOCKITO_METHODS.matches(mit.methodSymbol()) && mset.expression().symbolType().is(MOCKITO_CLASS)) {
+    if (MOCKITO_METHODS.matches(mit.methodSymbol())) {
       reportIssue(methodSelect, "Use a static import for \"%s\".".formatted(methodName));
     }
   }
