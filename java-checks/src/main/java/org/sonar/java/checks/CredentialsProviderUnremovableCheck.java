de75e91a2b6e388a2903440a27ae72a0af4789c5
Romain Brenguier <romain.brenguier@sonarsource.com>
2026-06-26 14:23:37 +0200

    SONARJAVA-6492: Implement rule S8912

---
+++ java-checks/src/main/java/org/sonar/java/checks/CredentialsProviderUnremovableCheck.java
@@ -0,0 +1,76 @@
+/*
+ * SonarQube Java
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.SymbolMetadata;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S8912")
+public class CredentialsProviderUnremovableCheck extends IssuableSubscriptionVisitor {
+  private static final String CREDENTIALS_PROVIDER_FQN = "io.quarkus.credentials.CredentialsProvider";
+  private static final String UNREMOVABLE_FQN = "io.quarkus.arc.Unremovable";
+
+  private static final List<String> CDI_SCOPE_ANNOTATIONS = List.of(
+    "jakarta.enterprise.context.ApplicationScoped",
+    "jakarta.enterprise.context.RequestScoped",
+    "jakarta.enterprise.context.SessionScoped",
+    "jakarta.enterprise.context.Dependent",
+    "jakarta.inject.Singleton"
+  );
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.CLASS);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    ClassTree classTree = (ClassTree) tree;
+
+    if (!implementsCredentialsProvider(classTree)) {
+      return;
+    }
+
+    SymbolMetadata metadata = classTree.symbol().metadata();
+
+    if (!hasCdiScopeAnnotation(metadata)) {
+      return;
+    }
+
+    if (metadata.isAnnotatedWith(UNREMOVABLE_FQN)) {
+      return;
+    }
+
+    reportIssue(
+      classTree.simpleName(),
+      "Add the @Unremovable annotation to this CredentialsProvider implementation."
+    );
+  }
+
+  private static boolean implementsCredentialsProvider(ClassTree classTree) {
+    return classTree.symbol().type().isSubtypeOf(CREDENTIALS_PROVIDER_FQN);
+  }
+
+  private static boolean hasCdiScopeAnnotation(SymbolMetadata metadata) {
+    return CDI_SCOPE_ANNOTATIONS.stream()
+      .anyMatch(metadata::isAnnotatedWith);
+  }
+}
---
+++ java-checks/src/test/java/org/sonar/java/checks/CredentialsProviderUnremovableCheckTest.java
@@ -0,0 +1,53 @@
+/*
+ * SonarQube Java
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.java.checks.verifier.CheckVerifier;
+
+import static org.sonar.java.checks.verifier.TestUtils.mainCodeSourcesPath;
+
+class CredentialsProviderUnremovableCheckTest {
+  private static final String SAMPLE_FILE = mainCodeSourcesPath("checks/CredentialsProviderUnremovableCheckSample.java");
+  private static final String MINIMAL_FILE = mainCodeSourcesPath("checks/CredentialsProviderUnremovableCheckSampleMinimal.java");
+  private static final CredentialsProviderUnremovableCheck CHECK = new CredentialsProviderUnremovableCheck();
+
+  @Test
+  void testMinimal() {
+    CheckVerifier.newVerifier()
+      .onFile(MINIMAL_FILE)
+      .withCheck(CHECK)
+      .verifyIssues();
+  }
+
+  @Test
+  void test() {
+    CheckVerifier.newVerifier()
+      .onFile(SAMPLE_FILE)
+      .withCheck(CHECK)
+      .verifyIssues();
+  }
+
+  @Test
+  void testWithoutSemantic() {
+    CheckVerifier.newVerifier()
+      .onFile(SAMPLE_FILE)
+      .withCheck(CHECK)
+      .withoutSemantic()
+      .verifyNoIssues();
+  }
+}
