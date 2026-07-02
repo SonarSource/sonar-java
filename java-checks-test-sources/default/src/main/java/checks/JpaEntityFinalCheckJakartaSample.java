dbdfcdd511c60f592e19910840de16b774ff1020
Noémie Bénard <163140136+NoemieBenard@users.noreply.github.com>
2026-07-02 15:15:05 +0200

    SONARJAVA-6539 Create rule S8947: JPA entity classes should not be final (#5719)

---
+++ java-checks-test-sources/default/src/main/java/checks/JpaEntityFinalCheckJakartaSample.java
@@ -0,0 +1,99 @@
+package checks;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@Entity
+final class JpaEntityFinalCheckJakartaFinalEntity { // Noncompliant {{Remove this "final" modifier from this JPA entity class.}}
+//^[sc=1;ec=5]
+  @Id
+  private Long id;
+
+  public Long getId() {
+    return id;
+  }
+}
+
+@MappedSuperclass
+final class JpaEntityFinalCheckJakartaFinalMappedSuperclass { // Noncompliant {{Remove this "final" modifier from this JPA entity class.}}
+//^[sc=1;ec=5]
+  @Id
+  private Long id;
+}
+
+@Entity
+class JpaEntityFinalCheckJakartaEntityWithFinalMethod { // Compliant - class itself is not final
+  @Id
+  private Long id;
+
+  public final Long getId() { // Noncompliant {{Remove this "final" modifier from this JPA entity method.}}
+  //     ^^^^^
+    return id;
+  }
+
+  public Long getIdCompliant() { // Compliant
+    return id;
+  }
+
+  private final Long getIdPrivate() { // Compliant - private methods cannot be overridden by proxies
+    return id;
+  }
+
+  public static final Long getIdStatic() { // Compliant - static methods cannot be overridden by proxies
+    return 0L;
+  }
+}
+
+@MappedSuperclass
+class JpaEntityFinalCheckJakartaMappedSuperclassWithFinalMethod { // Compliant - class itself is not final
+  @Id
+  private Long id;
+
+  public final Long getId() { // Noncompliant {{Remove this "final" modifier from this JPA entity method.}}
+  //     ^^^^^
+    return id;
+  }
+}
+
+interface JpaEntityFinalCheckJakartaEntityInterface {
+  Long getId();
+}
+
+@Entity
+final class JpaEntityFinalCheckJakartaFinalEntityWithInterface implements JpaEntityFinalCheckJakartaEntityInterface { // Compliant - implements interface, proxy can be interface-based
+  @Id
+  private Long id;
+
+  @Override
+  public Long getId() {
+    return id;
+  }
+}
+
+@Entity
+class JpaEntityFinalCheckJakartaCompliantEntity { // Compliant
+  @Id
+  private Long id;
+
+  public Long getId() {
+    return id;
+  }
+}
+
+@MappedSuperclass
+class JpaEntityFinalCheckJakartaCompliantMappedSuperclass { // Compliant
+  @Id
+  private Long id;
+}
+
+class JpaEntityFinalCheckJakartaNotAnEntity { // Compliant - not a JPA entity
+}
+
+final class JpaEntityFinalCheckJakartaFinalNotAnEntity { // Compliant - not a JPA entity
+}
+
+class JpaEntityFinalCheckJakartaNotAnEntityWithFinalMethod { // Compliant - not a JPA entity
+  public final void doSomething() { // Compliant - not a JPA entity
+  }
+}
