27a9c3ebb76487cb3d515c19cadf73ea48460302
Noémie Bénard <163140136+NoemieBenard@users.noreply.github.com>
2026-07-03 16:06:42 +0200

    SONARJAVA-6540 Implement S8954: Bean Validation constraints should not be placed on static fields (#5724)

---
+++ java-checks-test-sources/default/src/main/java/checks/BeanValidationConstraintOnStaticFieldCheckSample.java
@@ -0,0 +1,69 @@
+package checks;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+class BeanValidationConstraintOnStaticFieldCheckSample {
+
+  @NotNull // Noncompliant {{Move this constraint to an instance field.}} [[sc=3;ec=11]]
+  private static String staticField;
+
+  @Size(min = 1, max = 100) // Noncompliant
+  static String anotherStaticField;
+
+  @Min(1) // Noncompliant
+  private static int minStaticField;
+
+  @Pattern(regexp = ".*") // Noncompliant
+  private static String patternStaticField;
+
+  @NotBlank // Noncompliant
+  private static String notBlankStaticField;
+
+  @NotNull // Compliant - instance field
+  private String instanceField;
+
+  @Size(min = 1, max = 100) // Compliant - instance field
+  String anotherInstanceField;
+
+  private static String unannotatedStaticField; // Compliant - no constraint annotation
+
+  @SuppressWarnings("unused") // Compliant - not a Bean Validation constraint
+  private static String suppressedStaticField;
+
+  @CustomConstraint // Noncompliant
+  private static String customConstraintStaticField;
+
+  @CustomConstraint // Compliant - instance field
+  private String customConstraintInstanceField;
+
+  @Pattern.List({@Pattern(regexp = "[a-z]+"), @Pattern(regexp = ".{1,10}")}) // Compliant - container annotations are not detected (limitation)
+  private static String patternListStaticField;
+}
+
+@Constraint(validatedBy = CustomConstraintValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@interface CustomConstraint {
+  String message() default "custom constraint violated";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+}
+
+class CustomConstraintValidator implements ConstraintValidator<CustomConstraint, String> {
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    return value != null && !value.isEmpty();
+  }
+}
---
+++ java-checks-test-sources/default/src/main/java/checks/BeanValidationConstraintOnStaticFieldCheckSampleJavax.java
@@ -0,0 +1,69 @@
+package checks;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+class BeanValidationConstraintOnStaticFieldCheckSampleJavax {
+
+  @NotNull // Noncompliant {{Move this constraint to an instance field.}} [[sc=3;ec=11]]
+  private static String staticField;
+
+  @Size(min = 1, max = 100) // Noncompliant
+  static String anotherStaticField;
+
+  @Min(1) // Noncompliant
+  private static int minStaticField;
+
+  @Pattern(regexp = ".*") // Noncompliant
+  private static String patternStaticField;
+
+  @NotNull // Compliant - instance field
+  private String instanceField;
+
+  @Size(min = 1, max = 100) // Compliant - instance field
+  String anotherInstanceField;
+
+  private static String unannotatedStaticField; // Compliant - no constraint annotation
+
+  @SuppressWarnings("unused") // Compliant - not a Bean Validation constraint
+  private static String suppressedStaticField;
+
+  @JavaxCustomConstraint // Noncompliant
+  private static String customConstraintStaticField;
+
+  @JavaxCustomConstraint // Compliant - instance field
+  private String customConstraintInstanceField;
+
+  @Pattern.List({@Pattern(regexp = "[a-z]+"), @Pattern(regexp = ".{1,10}")}) // Compliant - container annotations are not detected (limitation)
+  private static String patternListStaticField;
+}
+
+@Constraint(validatedBy = JavaxCustomConstraintValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@interface JavaxCustomConstraint {
+  String message() default "custom constraint violated";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+}
+
+class JavaxCustomConstraintValidator implements ConstraintValidator<JavaxCustomConstraint, String> {
+  @Override
+  public void initialize(JavaxCustomConstraint constraintAnnotation) {
+  }
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    return value != null && !value.isEmpty();
+  }
+}
