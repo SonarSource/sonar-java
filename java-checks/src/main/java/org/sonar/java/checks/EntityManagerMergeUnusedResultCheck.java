0c79e7f1b10f39f7f1a1cdfd8ba5ac35dc61abbc
Noémie Bénard <163140136+NoemieBenard@users.noreply.github.com>
2026-07-07 17:27:13 +0200

    SONARJAVA-6579 Implement S8975: The return value of "EntityManager.merge()" should be used (#5756)

---
+++ java-checks/src/main/java/org/sonar/java/checks/EntityManagerMergeUnusedResultCheck.java
@@ -0,0 +1,62 @@
+/*
+ * SonarQube Java
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.java.model.ExpressionUtils;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.ExpressionStatementTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S8975")
+public class EntityManagerMergeUnusedResultCheck extends IssuableSubscriptionVisitor {
+
+  private static final String MESSAGE = "Use the return value of \"merge()\".";
+
+  private static final MethodMatchers MERGE_MATCHER = MethodMatchers.or(
+    MethodMatchers.create()
+      .ofTypes("javax.persistence.EntityManager")
+      .names("merge")
+      .withAnyParameters()
+      .build(),
+    MethodMatchers.create()
+      .ofTypes("jakarta.persistence.EntityManager")
+      .names("merge")
+      .withAnyParameters()
+      .build());
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.EXPRESSION_STATEMENT);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    ExpressionTree expression = ((ExpressionStatementTree) tree).expression();
+    ExpressionTree unwrapped = ExpressionUtils.skipParentheses(expression);
+    if (unwrapped.is(Tree.Kind.METHOD_INVOCATION)) {
+      MethodInvocationTree mit = (MethodInvocationTree) unwrapped;
+      if (MERGE_MATCHER.matches(mit)) {
+        reportIssue(ExpressionUtils.methodName(mit), MESSAGE);
+      }
+    }
+  }
+}
