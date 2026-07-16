5311db8198e5bac131fe4c522a6b46b44df8859b
Victor Diez <victor.diez@sonarsource.com>
2026-07-16 09:21:34 +0200

    Use JDK Files API in InputFileUtils

--- java-frontend/src/test/java/org/sonar/java/InputFileUtils.java
+++ java-frontend/src/test/java/org/sonar/java/InputFileUtils.java
@@ -16,10 +16,10 @@
  */
 package org.sonar.java;
 
-import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -33,17 +33,17 @@ class InputFileUtils {
     if (matcher.find()) {
       String className = matcher.group(1);
       InputFile.Type type = className.endsWith("Test") ? InputFile.Type.TEST : InputFile.Type.MAIN;
-      File file = java.nio.file.Files.createFile(temp.resolve(className + ".java")).toFile().getAbsoluteFile();
+      File file = Files.createFile(temp.resolve(className + ".java")).toFile().getAbsoluteFile();
       return generateInputFile(code, context, file, type);
     } else {
-      File file = java.nio.file.Files.createFile(temp.resolve("Unnamed.java")).toFile().getAbsoluteFile();
+      File file = Files.createFile(temp.resolve("Unnamed.java")).toFile().getAbsoluteFile();
       return generateInputFile(code, context, file, InputFile.Type.MAIN);
     }
   }
 
   @NotNull
   private static InputFile generateInputFile(String code, SensorContextTester context, File file, InputFile.Type type) throws IOException {
-    Files.asCharSink(file, StandardCharsets.UTF_8).write(code);
+    Files.writeString(file.toPath(), code, StandardCharsets.UTF_8);
     InputFile defaultFile = TestUtils.inputFile(context.fileSystem().baseDir().getAbsolutePath(), file, type);
     context.fileSystem().add(defaultFile);
     return defaultFile;
