26207b8fa62c7a190d14bd8b7666f8e4e8bb57cd
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-08-04 10:13:31 +0200

    Raise content hash cache misses log message to DEBUG level

--- java-frontend/src/main/java/org/sonar/java/caching/ContentHashCache.java
+++ java-frontend/src/main/java/org/sonar/java/caching/ContentHashCache.java
@@ -69,7 +69,7 @@ public class ContentHashCache {
       }
       return isHashEqual;
     } catch (IllegalArgumentException e) {
-      LOG.trace(String.format("Could not find key %s in the cache", cacheKey));
+      LOG.debug("Could not find key {} in the cache", cacheKey);
       writeToCache(inputFile);
     } catch (IOException | NoSuchAlgorithmException e) {
       LOG.warn(String.format(HASH_COMPUTE_FAIL_MSG, inputFile.key()));
