f555ade8387508882b29dd03a8fc31b741acbccb
Noémie Bénard <noemie.benard@sonarsource.com>
2026-08-05 16:31:00 +0200

    Fix review comments

--- java-checks/src/main/java/org/sonar/java/checks/SynchronizedOnConcurrentObjectCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/SynchronizedOnConcurrentObjectCheck.java
@@ -18,6 +18,7 @@ package org.sonar.java.checks;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.semantic.Type;
@@ -28,7 +29,17 @@ import org.sonar.plugins.java.api.tree.Tree;
 @Rule(key = "S9141")
 public class SynchronizedOnConcurrentObjectCheck extends IssuableSubscriptionVisitor {
 
-  private static final String CONCURRENT_PACKAGE_PREFIX = "java.util.concurrent.";
+  private static final String CONCURRENT_LOCKS_PREFIX = "java.util.concurrent.locks.";
+  private static final String CONCURRENT_ATOMIC_PREFIX = "java.util.concurrent.atomic.";
+  private static final Set<String> CONCURRENT_SYNC_TYPES = Set.of(
+    "java.util.concurrent.Semaphore",
+    "java.util.concurrent.CountDownLatch",
+    "java.util.concurrent.CyclicBarrier",
+    "java.util.concurrent.Exchanger",
+    "java.util.concurrent.Phaser",
+    "java.util.concurrent.BlockingQueue",
+    "java.util.concurrent.BlockingDeque",
+    "java.util.concurrent.TransferQueue");
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
@@ -39,18 +50,21 @@ public class SynchronizedOnConcurrentObjectCheck extends IssuableSubscriptionVis
   public void visitNode(Tree tree) {
     ExpressionTree expression = ((SynchronizedStatementTree) tree).expression();
     Type expressionType = expression.symbolType();
-    if (isFromConcurrentPackage(expressionType)) {
+    if (isConcurrentSyncPrimitive(expressionType)) {
       reportIssue(expression, String.format(
         "Use the \"%s\" API for synchronization instead of a \"synchronized\" block.", expressionType.name()));
     }
   }
 
-  private static boolean isFromConcurrentPackage(Type type) {
-    if (type.fullyQualifiedName().startsWith(CONCURRENT_PACKAGE_PREFIX)) {
-      return true;
-    }
-    return type.symbol().superTypes().stream()
-      .anyMatch(superType -> superType.fullyQualifiedName().startsWith(CONCURRENT_PACKAGE_PREFIX));
+  private static boolean isConcurrentSyncPrimitive(Type type) {
+    return isKnownSyncPrimitive(type) || type.symbol().superTypes().stream().anyMatch(SynchronizedOnConcurrentObjectCheck::isKnownSyncPrimitive);
+  }
+
+  private static boolean isKnownSyncPrimitive(Type type) {
+    String fqn = type.fullyQualifiedName();
+    return fqn.startsWith(CONCURRENT_LOCKS_PREFIX)
+      || fqn.startsWith(CONCURRENT_ATOMIC_PREFIX)
+      || CONCURRENT_SYNC_TYPES.contains(fqn);
   }
 
 }
