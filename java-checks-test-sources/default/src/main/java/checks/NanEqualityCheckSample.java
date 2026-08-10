9b81b2f407066fb24d449759b30f97d022ebd2ac
Romain Brenguier <romain.brenguier@sonarsource.com>
2026-08-10 11:05:42 +0200

    Implement new rule S9147
    Detect equality and inequality comparisons (== and !=) with Double.NaN
    and Float.NaN constants, which always produce incorrect results due to
    IEEE 754 semantics. Suggests using Double.isNaN() or Float.isNaN()
    instead.

---
+++ java-checks-test-sources/default/src/main/java/checks/NanEqualityCheckSample.java
@@ -0,0 +1,81 @@
+package checks;
+
+class NanEqualityCheckSample {
+
+  void noncompliantDoubleNaN() {
+    double result = Math.sqrt(-1);
+    if (result == Double.NaN) { } // Noncompliant {{Use "Double.isNaN()" instead of comparison with "Double.NaN".}}
+//             ^^
+    if (result != Double.NaN) { } // Noncompliant {{Use "Double.isNaN()" instead of comparison with "Double.NaN".}}
+//             ^^
+  }
+
+  void noncompliantFloatNaN() {
+    float value = 0.0f / 0.0f;
+    if (value == Float.NaN) { } // Noncompliant {{Use "Float.isNaN()" instead of comparison with "Float.NaN".}}
+//            ^^
+    if (value != Float.NaN) { } // Noncompliant {{Use "Float.isNaN()" instead of comparison with "Float.NaN".}}
+//            ^^
+  }
+
+  void noncompliantReversedOperandOrder() {
+    double result = 0.0;
+    if (Double.NaN == result) { } // Noncompliant {{Use "Double.isNaN()" instead of comparison with "Double.NaN".}}
+//                 ^^
+    if (Double.NaN != result) { } // Noncompliant {{Use "Double.isNaN()" instead of comparison with "Double.NaN".}}
+//                 ^^
+  }
+
+  void noncompliantNaNComparedToItself() {
+    if (Double.NaN == Double.NaN) { } // Noncompliant
+//                 ^^
+  }
+
+  void noncompliantInExpressions() {
+    double x = 1.0;
+    boolean isNan = x == Double.NaN; // Noncompliant
+//                    ^^
+    boolean notNan = x != Double.NaN; // Noncompliant
+//                     ^^
+    String s = x != Double.NaN ? "valid" : "invalid"; // Noncompliant
+//               ^^
+  }
+
+  boolean noncompliantInReturn(double x) {
+    return x == Double.NaN; // Noncompliant
+//           ^^
+  }
+
+  void noncompliantWithParentheses(double x) {
+    if (x == (Double.NaN)) { } // Noncompliant
+//        ^^
+    if ((x) == Double.NaN) { } // Noncompliant
+//          ^^
+  }
+
+  void compliantIsNaN() {
+    double result = Math.sqrt(-1);
+    if (Double.isNaN(result)) { } // Compliant
+    if (!Double.isNaN(result)) { } // Compliant
+  }
+
+  void compliantFloatIsNaN() {
+    float value = 0.0f / 0.0f;
+    if (Float.isNaN(value)) { } // Compliant
+  }
+
+  void compliantRegularComparisons() {
+    double a = 1.0;
+    double b = 2.0;
+    if (a == b) { } // Compliant - regular double comparison
+    if (a == 0.0) { } // Compliant - comparison with regular constant
+    if (a == Double.POSITIVE_INFINITY) { } // Compliant - comparison with other Double constant
+    if (a == Double.MAX_VALUE) { } // Compliant
+  }
+
+  void compliantInstanceIsNaN() {
+    Double d = Double.valueOf(1.0);
+    if (d.isNaN()) { } // Compliant - instance method usage
+  }
+
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/NanEqualityCheck.java
@@ -0,0 +1,68 @@
+/*
+ * SonarQube Java
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import java.util.List;
+import javax.annotation.Nullable;
+import org.sonar.check.Rule;
+import org.sonar.java.model.ExpressionUtils;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S9147")
+public class NanEqualityCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.EQUAL_TO, Tree.Kind.NOT_EQUAL_TO);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    BinaryExpressionTree binaryExpression = (BinaryExpressionTree) tree;
+    String typeName = getNanTypeName(binaryExpression.leftOperand());
+    if (typeName == null) {
+      typeName = getNanTypeName(binaryExpression.rightOperand());
+    }
+    if (typeName != null) {
+      reportIssue(binaryExpression.operatorToken(),
+        String.format("Use \"%s.isNaN()\" instead of comparison with \"%s.NaN\".", typeName, typeName));
+    }
+  }
+
+  @Nullable
+  private static String getNanTypeName(ExpressionTree expression) {
+    ExpressionTree expr = ExpressionUtils.skipParentheses(expression);
+    if (expr.is(Tree.Kind.MEMBER_SELECT)) {
+      MemberSelectExpressionTree memberSelect = (MemberSelectExpressionTree) expr;
+      if ("NaN".equals(memberSelect.identifier().name())) {
+        String ownerType = memberSelect.identifier().symbol().owner().type().fullyQualifiedName();
+        if ("java.lang.Double".equals(ownerType)) {
+          return "Double";
+        }
+        if ("java.lang.Float".equals(ownerType)) {
+          return "Float";
+        }
+      }
+    }
+    return null;
+  }
+
+}
