5782ea60121588efb7df34d9784779edb059b10e
Romain Brenguier <romain.brenguier@sonarsource.com>
2026-08-10 10:22:59 +0200

    Implement new rule S9146
    Detect calls to setEnabledForExtensions() on Apache XML-RPC
    configuration objects where the argument is not provably false,
    as enabling extensions activates Java object deserialization
    which can lead to remote code execution.

---
+++ java-checks-test-sources/default/src/main/files/non-compiling/checks/security/XmlRpcExtensionsCheckSample.java
@@ -0,0 +1,79 @@
+package checks.security;
+
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.common.XmlRpcHttpRequestConfigImpl;
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
+
+class XmlRpcExtensionsCheckSample {
+
+  private static final boolean EXTENSIONS_ENABLED = true;
+  private static final boolean EXTENSIONS_DISABLED = false;
+
+  void serverConfigNoncompliant() {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setEnabledForExtensions(true); // Noncompliant {{Disable extensions on this Apache XML RPC configuration.}}
+  }
+
+  void clientConfigNoncompliant() {
+    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+    config.setEnabledForExtensions(true); // Noncompliant
+  }
+
+  void serverConfigWithOtherSettings() {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setBasicEncoding("UTF-8");
+    config.setEnabledForExtensions(true); // Noncompliant
+  }
+
+  void constantTrue() {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setEnabledForExtensions(EXTENSIONS_ENABLED); // Noncompliant
+  }
+
+  void unknownVariable(boolean enable) {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setEnabledForExtensions(enable); // Noncompliant
+  }
+
+  void parentType() {
+    XmlRpcHttpRequestConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setEnabledForExtensions(true); // Noncompliant
+  }
+
+  // Compliant examples
+
+  void serverConfigDefault() {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    // Extensions are disabled by default
+  }
+
+  void clientConfigDefault() {
+    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+    // Extensions are disabled by default
+  }
+
+  void explicitlyDisabled() {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setEnabledForExtensions(false);
+  }
+
+  void clientExplicitlyDisabled() {
+    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+    config.setEnabledForExtensions(false);
+  }
+
+  void constantFalse() {
+    XmlRpcServerConfigImpl config = new XmlRpcServerConfigImpl();
+    config.setEnabledForExtensions(EXTENSIONS_DISABLED);
+  }
+
+  void unrelatedType() {
+    UnrelatedConfig config = new UnrelatedConfig();
+    config.setEnabledForExtensions(true);
+  }
+}
+
+class UnrelatedConfig {
+  void setEnabledForExtensions(boolean enabled) {
+  }
+}
