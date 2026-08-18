4759d18153e17415291849d788649bf822f2d303
nathsou <nathan.soufflet@sonarsource.com>
2026-08-18 11:46:42 +0200

    Implement S9350

---
+++ java-checks-test-sources/default/src/main/java/checks/EqualsMismatchedMembersCheckSample.java
@@ -0,0 +1,336 @@
+package checks;
+
+import java.util.Arrays;
+
+class EqualsMismatchedMembersCheckSample {
+
+  static class User {
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof User other) {
+        return java.util.Objects.equals(this.firstName, other.firstName)
+          && java.util.Objects.equals(this.lastName, other.firstName); // Noncompliant {{This equals() implementation compares mismatched members; pairing "lastName" with "firstName" breaks the equality contract.}}
+      }
+      return false;
+    }
+  }
+
+  static class CompliantUser {
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof CompliantUser other) {
+        return java.util.Objects.equals(this.firstName, other.firstName)
+          && java.util.Objects.equals(this.lastName, other.lastName);
+      }
+      return false;
+    }
+  }
+
+  static class Point {
+    private int x;
+    private int y;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Point that)) {
+        return false;
+      }
+      return x == that.x && y == that.x; // Noncompliant {{This equals() implementation compares mismatched members; pairing "y" with "x" breaks the equality contract.}}
+    }
+  }
+
+  static class CompliantPoint {
+    private int x;
+    private int y;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof CompliantPoint that)) {
+        return false;
+      }
+      return x == that.x && y == that.y;
+    }
+  }
+
+  static class NotEqual {
+    private int x;
+    private int y;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof NotEqual that)) {
+        return false;
+      }
+      return x != that.y; // Noncompliant {{This equals() implementation compares mismatched members; pairing "x" with "y" breaks the equality contract.}}
+    }
+  }
+
+  static class Box {
+    private Object key;
+    private Object value;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Box that)) {
+        return false;
+      }
+      return key.equals(that.key) && value.equals(that.key); // Noncompliant {{This equals() implementation compares mismatched members; pairing "value" with "key" breaks the equality contract.}}
+    }
+  }
+
+  static class CompliantBox {
+    private Object key;
+    private Object value;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof CompliantBox that)) {
+        return false;
+      }
+      return key.equals(that.key) && value.equals(that.value);
+    }
+  }
+
+  static class Person {
+    private String firstName;
+    private String lastName;
+
+    String getFirstName() {
+      return firstName;
+    }
+
+    String getLastName() {
+      return lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Person that)) {
+        return false;
+      }
+      return getFirstName().equals(that.getFirstName())
+        && getLastName().equals(that.getFirstName()); // Noncompliant {{This equals() implementation compares mismatched members; pairing "getLastName()" with "getFirstName()" breaks the equality contract.}}
+    }
+  }
+
+  static class CompliantPerson {
+    private String firstName;
+    private String lastName;
+
+    String getFirstName() {
+      return firstName;
+    }
+
+    String getLastName() {
+      return lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof CompliantPerson that)) {
+        return false;
+      }
+      return getFirstName().equals(that.getFirstName())
+        && getLastName().equals(that.getLastName());
+    }
+  }
+
+  static class PrimitiveGetters {
+    private int a;
+    private int b;
+
+    int getA() {
+      return a;
+    }
+
+    int getB() {
+      return b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof PrimitiveGetters that)) {
+        return false;
+      }
+      return getA() == that.getB() && getB() == that.getB(); // Noncompliant {{This equals() implementation compares mismatched members; pairing "getA()" with "getB()" breaks the equality contract.}}
+    }
+  }
+
+  record NamedPoint(int x, int y) {
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof NamedPoint that)) {
+        return false;
+      }
+      return this.x() == that.y(); // Noncompliant {{This equals() implementation compares mismatched members; pairing "x()" with "y()" breaks the equality contract.}}
+    }
+  }
+
+  static class ArraysHolder {
+    private int[] left;
+    private int[] right;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof ArraysHolder that)) {
+        return false;
+      }
+      return Arrays.equals(this.left, that.right); // Noncompliant {{This equals() implementation compares mismatched members; pairing "left" with "right" breaks the equality contract.}}
+    }
+  }
+
+  static class ArraysRangeHolder {
+    private int[] left;
+    private int[] right;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof ArraysRangeHolder that)) {
+        return false;
+      }
+      return Arrays.equals(this.left, 0, 1, that.right, 0, 1); // Noncompliant {{This equals() implementation compares mismatched members; pairing "left" with "right" breaks the equality contract.}}
+    }
+  }
+
+  static class GuavaHolder {
+    private Object a;
+    private Object b;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof GuavaHolder that)) {
+        return false;
+      }
+      return com.google.common.base.Objects.equal(a, that.b) && com.google.common.base.Objects.equal(b, that.b); // Noncompliant {{This equals() implementation compares mismatched members; pairing "a" with "b" breaks the equality contract.}}
+    }
+  }
+
+  static class MultipleMismatches {
+    private int a;
+    private int b;
+    private int c;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof MultipleMismatches that)) {
+        return false;
+      }
+      return a == that.b // Noncompliant {{This equals() implementation compares mismatched members; pairing "a" with "b" breaks the equality contract.}}
+        && b == that.c; // Noncompliant {{This equals() implementation compares mismatched members; pairing "b" with "c" breaks the equality contract.}}
+    }
+  }
+
+  static class UnorderedPair {
+    private int a;
+    private int b;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof UnorderedPair that)) {
+        return false;
+      }
+      return (a == that.a && b == that.b) || (a == that.b && b == that.a);
+    }
+  }
+
+  static class MixedFieldAndGetter {
+    private String name;
+
+    String getName() {
+      return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof MixedFieldAndGetter that)) {
+        return false;
+      }
+      return this.name.equals(that.getName());
+    }
+  }
+
+  static class Parent {
+    int inherited;
+  }
+
+  static class Child extends Parent {
+    int own;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Child that)) {
+        return false;
+      }
+      return this.inherited == that.own;
+    }
+  }
+
+  static class Statics {
+    static int DEFAULT;
+    int value;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Statics that)) {
+        return false;
+      }
+      return this.value == DEFAULT && this.value == that.value;
+    }
+  }
+
+  static class SuperCall {
+    int a;
+    int b;
+
+    @Override
+    public boolean equals(Object obj) {
+      return super.equals(obj);
+    }
+  }
+
+  static class NotEqualsMethod {
+    int a;
+    int b;
+
+    public boolean equals(NotEqualsMethod other) {
+      return a == other.b;
+    }
+
+    public int compareTo(Object other) {
+      return a == ((NotEqualsMethod) other).b ? 0 : 1;
+    }
+  }
+
+  static class Locals {
+    int a;
+    int b;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Locals that)) {
+        return false;
+      }
+      int temp = that.a;
+      return this.a == temp && this.b == that.b;
+    }
+  }
+
+  static class Parentheses {
+    int a;
+    int b;
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Parentheses that)) {
+        return false;
+      }
+      return (a) == (that.b); // Noncompliant {{This equals() implementation compares mismatched members; pairing "a" with "b" breaks the equality contract.}}
+    }
+  }
+}
---
+++ java-checks/src/main/java/org/sonar/java/checks/EqualsMismatchedMembersCheck.java
@@ -0,0 +1,236 @@
+/*
+ * SonarQube Java
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.java.checks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.helpers.MethodTreeUtils;
+import org.sonar.java.model.ExpressionUtils;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.BinaryExpressionTree;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+@Rule(key = "S9350")
+public class EqualsMismatchedMembersCheck extends IssuableSubscriptionVisitor {
+
+  private static final String ISSUE_MESSAGE =
+    "This equals() implementation compares mismatched members; pairing \"%s\" with \"%s\" breaks the equality contract.";
+  private static final String SECONDARY_THIS = "Compared member on this";
+  private static final String SECONDARY_OTHER = "Compared member on the other instance";
+  private static final String JAVA_LANG_OBJECT = "java.lang.Object";
+
+  private static final MethodMatchers OBJECTS_EQUALS = MethodMatchers.create()
+    .ofTypes("java.util.Objects")
+    .names("equals")
+    .addParametersMatcher(JAVA_LANG_OBJECT, JAVA_LANG_OBJECT)
+    .build();
+
+  private static final MethodMatchers GUAVA_OBJECTS_EQUAL = MethodMatchers.create()
+    .ofTypes("com.google.common.base.Objects")
+    .names("equal")
+    .withAnyParameters()
+    .build();
+
+  private static final MethodMatchers ARRAYS_EQUALS = MethodMatchers.create()
+    .ofTypes("java.util.Arrays")
+    .names("equals")
+    .withAnyParameters()
+    .build();
+
+  private static final MethodMatchers INSTANCE_EQUALS = MethodMatchers.create()
+    .ofAnyType()
+    .names("equals")
+    .addParametersMatcher(JAVA_LANG_OBJECT)
+    .build();
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return List.of(Tree.Kind.METHOD);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    MethodTree methodTree = (MethodTree) tree;
+    if (!MethodTreeUtils.isEqualsMethod(methodTree) || methodTree.block() == null) {
+      return;
+    }
+    Symbol owner = methodTree.symbol().owner();
+    if (owner == null || !owner.isTypeSymbol() || owner.isUnknown() || owner.type().isUnknown()) {
+      return;
+    }
+    ComparisonCollector collector = new ComparisonCollector(owner);
+    methodTree.block().accept(collector);
+    Set<MemberPair> suspiciousPairs = collector.comparisons.stream()
+      .map(ComparisonSite::pair)
+      .collect(Collectors.toSet());
+    for (ComparisonSite comparison : collector.comparisons) {
+      if (suspiciousPairs.contains(comparison.pair().reversed())) {
+        continue;
+      }
+      reportIssue(
+        comparison.tree,
+        String.format(ISSUE_MESSAGE, comparison.lhs.displayName, comparison.rhs.displayName),
+        List.of(
+          new JavaFileScannerContext.Location(SECONDARY_THIS, comparison.lhs.tree),
+          new JavaFileScannerContext.Location(SECONDARY_OTHER, comparison.rhs.tree)),
+        null);
+    }
+  }
+
+  private static final class ComparisonCollector extends BaseTreeVisitor {
+    private final Symbol enclosingClass;
+    private final List<ComparisonSite> comparisons = new ArrayList<>();
+
+    private ComparisonCollector(Symbol enclosingClass) {
+      this.enclosingClass = enclosingClass;
+    }
+
+    @Override
+    public void visitClass(ClassTree tree) {
+      // Nested types are visited independently through METHOD subscription.
+    }
+
+    @Override
+    public void visitBinaryExpression(BinaryExpressionTree tree) {
+      if (tree.is(Tree.Kind.EQUAL_TO, Tree.Kind.NOT_EQUAL_TO)) {
+        addIfDubious(tree, tree.leftOperand(), tree.rightOperand());
+      }
+      super.visitBinaryExpression(tree);
+    }
+
+    @Override
+    public void visitMethodInvocation(MethodInvocationTree tree) {
+      List<ExpressionTree> arguments = tree.arguments();
+      if (isTwoArgEqualityHelper(tree)) {
+        addIfDubious(tree, arguments.get(0), arguments.get(1));
+      } else if (ARRAYS_EQUALS.matches(tree) && arguments.size() >= 2) {
+        addIfDubious(tree, arguments.get(0), arguments.get(arguments.size() / 2));
+      } else if (INSTANCE_EQUALS.matches(tree) && arguments.size() == 1) {
+        ExpressionTree receiver = receiver(tree);
+        if (receiver != null && !isSuper(receiver)) {
+          addIfDubious(tree, receiver, arguments.get(0));
+        }
+      }
+      super.visitMethodInvocation(tree);
+    }
+
+    private void addIfDubious(Tree comparisonTree, ExpressionTree lhs, ExpressionTree rhs) {
+      Optional<MemberRef> left = member(lhs);
+      Optional<MemberRef> right = member(rhs);
+      if (left.isEmpty() || right.isEmpty()) {
+        return;
+      }
+      MemberRef leftMember = left.get();
+      MemberRef rightMember = right.get();
+      if (leftMember.kind != rightMember.kind || leftMember.symbol.equals(rightMember.symbol)) {
+        return;
+      }
+      comparisons.add(new ComparisonSite(comparisonTree, leftMember, rightMember));
+    }
+
+    private Optional<MemberRef> member(ExpressionTree expression) {
+      ExpressionTree expr = ExpressionUtils.skipParentheses(expression);
+      if (expr instanceof IdentifierTree identifierTree) {
+        return field(identifierTree.symbol(), expr);
+      }
+      if (expr instanceof MemberSelectExpressionTree memberSelect) {
+        return field(memberSelect.identifier().symbol(), expr);
+      }
+      if (expr instanceof MethodInvocationTree invocation && invocation.arguments().isEmpty()) {
+        return getter(invocation);
+      }
+      return Optional.empty();
+    }
+
+    private Optional<MemberRef> field(Symbol symbol, ExpressionTree tree) {
+      if (symbol.isUnknown() || !symbol.isVariableSymbol() || symbol.isStatic() || !enclosingClass.equals(symbol.owner())) {
+        return Optional.empty();
+      }
+      return Optional.of(new MemberRef(symbol, MemberKind.FIELD, symbol.name(), tree));
+    }
+
+    private Optional<MemberRef> getter(MethodInvocationTree invocation) {
+      Symbol.MethodSymbol method = invocation.methodSymbol();
+      if (method.isUnknown() || method.isStatic() || !enclosingClass.equals(method.owner())) {
+        return Optional.empty();
+      }
+      Symbol.TypeSymbol returnType = method.returnType();
+      if (returnType == null || returnType.isUnknown() || returnType.type().isVoid() || !method.parameterTypes().isEmpty()) {
+        return Optional.empty();
+      }
+      return Optional.of(new MemberRef(method, MemberKind.METHOD, method.name() + "()", invocation));
+    }
+
+    private static boolean isTwoArgEqualityHelper(MethodInvocationTree tree) {
+      if (tree.arguments().size() != 2) {
+        return false;
+      }
+      if (OBJECTS_EQUALS.matches(tree) || GUAVA_OBJECTS_EQUAL.matches(tree)) {
+        return true;
+      }
+      Symbol.MethodSymbol method = tree.methodSymbol();
+      return (method.isUnknown() || method.isStatic())
+        && "equal".equals(ExpressionUtils.methodName(tree).name());
+    }
+
+    private static ExpressionTree receiver(MethodInvocationTree invocation) {
+      if (invocation.methodSelect() instanceof MemberSelectExpressionTree memberSelect) {
+        return memberSelect.expression();
+      }
+      return null;
+    }
+
+    private static boolean isSuper(ExpressionTree expression) {
+      ExpressionTree expr = ExpressionUtils.skipParentheses(expression);
+      return expr instanceof IdentifierTree identifierTree && "super".equals(identifierTree.name());
+    }
+  }
+
+  private enum MemberKind {
+    FIELD,
+    METHOD
+  }
+
+  private record MemberRef(Symbol symbol, MemberKind kind, String displayName, ExpressionTree tree) {
+  }
+
+  private record ComparisonSite(Tree tree, MemberRef lhs, MemberRef rhs) {
+    private MemberPair pair() {
+      return new MemberPair(lhs.symbol, rhs.symbol);
+    }
+  }
+
+  private record MemberPair(Symbol lhs, Symbol rhs) {
+    private MemberPair reversed() {
+      return new MemberPair(rhs, lhs);
+    }
+  }
+}
