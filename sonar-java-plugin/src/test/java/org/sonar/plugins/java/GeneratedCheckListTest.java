2712bee52a1ca628ea69609400e504b74df13358
nathsou <nathan.soufflet@sonarsource.com>
2026-08-19 14:27:39 +0200

    fix-ci: exclude AbstractComparisonMethodCheck from the generated check list count
    GeneratedCheckListTest.count failed because every *Check.java is expected to
    be a registered rule. The new shared comparison-method base is abstract and
    has no @Rule, matching the existing AbstractRegexCheck blacklist.

--- sonar-java-plugin/src/test/java/org/sonar/plugins/java/GeneratedCheckListTest.java
+++ sonar-java-plugin/src/test/java/org/sonar/plugins/java/GeneratedCheckListTest.java
@@ -50,7 +50,8 @@ class GeneratedCheckListTest {
     "AbstractXPathBasedCheck.java",
     "AbstractWebXmlXPathBasedCheck.java",
     "AbstractRedosCheck.java",
-    "AbstractRegexCheck.java");
+    "AbstractRegexCheck.java",
+    "AbstractComparisonMethodCheck.java");
 
   /**
    * Enforces that each check declared in list.
