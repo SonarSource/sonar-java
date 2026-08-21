f58b175d9a0937bf3c43ba279c17a07d923925eb
nathsou <nathan.soufflet@sonarsource.com>
2026-08-21 11:04:56 +0200

    SONARJAVA-6820 Fix S9355 false positives on Eclipse (non-Javadoc) comments

--- java-checks-test-sources/default/src/main/java/checks/AlmostJavadocCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/AlmostJavadocCheckSample.java
@@ -121,6 +121,35 @@ class AlmostJavadocCheckSample {
     return name;
   }
 
+  /*
+   * (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    return 0;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object other) {
+    return this == other;
+  }
+
+  /* (non-Javadoc)
+   * @param ignored this Eclipse stub still contains a Javadoc tag
+   */
+  public void eclipseMarkerWithoutSee(int ignored) {
+  }
+
+  // Noncompliant@+1
+  /* @see java.lang.Object#clone() */
+  public Object seeWithoutEclipseMarker() {
+    return this;
+  }
+
   record Point(int x, int y) {
     // Noncompliant@+1
     /* @param x the x coordinate */
