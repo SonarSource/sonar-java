7b32d96edd265e44d7c9417efb928cd768fd230a
Nathan Soufflet <nathsou@users.noreply.github.com>
2026-09-18 10:20:04 +0200

    SONARJAVA-7002: Fix FN in S2123: detect discarded postfix increments in expression-bodied lambdas (#6175)

--- java-checks/src/main/java/org/sonar/java/checks/UselessIncrementCheck.java
+++ java-checks/src/main/java/org/sonar/java/checks/UselessIncrementCheck.java
@@ -16,26 +16,28 @@
  */
 package org.sonar.java.checks;
 
+import java.util.Arrays;
+import java.util.List;
 import org.sonar.check.Rule;
 import org.sonar.java.model.ExpressionUtils;
 import org.sonar.java.model.SyntacticEquivalence;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.Symbol;
 import org.sonar.plugins.java.api.tree.AssignmentExpressionTree;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.LambdaExpressionTree;
 import org.sonar.plugins.java.api.tree.ReturnStatementTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.UnaryExpressionTree;
-
-import java.util.Arrays;
-import java.util.List;
+import org.sonar.plugins.java.api.tree.VariableTree;
 
 @Rule(key = "S2123")
 public class UselessIncrementCheck extends IssuableSubscriptionVisitor {
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    return Arrays.asList(Tree.Kind.RETURN_STATEMENT, Tree.Kind.ASSIGNMENT);
+    return Arrays.asList(Tree.Kind.RETURN_STATEMENT, Tree.Kind.ASSIGNMENT, Tree.Kind.LAMBDA_EXPRESSION);
   }
 
   @Override
@@ -49,7 +51,7 @@ public class UselessIncrementCheck extends IssuableSubscriptionVisitor {
           reportIssue(unaryExpression);
         }
       }
-    } else {
+    } else if (tree.is(Tree.Kind.ASSIGNMENT)) {
       AssignmentExpressionTree aet = (AssignmentExpressionTree) tree;
       if (isPostfix(aet.expression())) {
         UnaryExpressionTree postfix = (UnaryExpressionTree) aet.expression();
@@ -57,6 +59,33 @@ public class UselessIncrementCheck extends IssuableSubscriptionVisitor {
           reportIssue(postfix);
         }
       }
+    } else {
+      checkLambda((LambdaExpressionTree) tree);
+    }
+  }
+
+  private void checkLambda(LambdaExpressionTree lambda) {
+    Tree body = lambda.body();
+    if (body instanceof ExpressionTree expressionTree) {
+      body = ExpressionUtils.skipParentheses(expressionTree);
+    }
+    if (!isPostfix(body)) {
+      return;
+    }
+    UnaryExpressionTree unary = (UnaryExpressionTree) body;
+    if (lambda.symbol().returnType().type().isVoid()) {
+      return;
+    }
+    ExpressionTree operand = ExpressionUtils.skipParentheses(unary.expression());
+    if (!operand.is(Tree.Kind.IDENTIFIER)) {
+      return;
+    }
+    Symbol operandSymbol = ((IdentifierTree) operand).symbol();
+    boolean isParameter = lambda.parameters().stream()
+      .map(VariableTree::symbol)
+      .anyMatch(operandSymbol::equals);
+    if (isParameter) {
+      reportIssue(unary);
     }
   }
 
@@ -64,7 +93,7 @@ public class UselessIncrementCheck extends IssuableSubscriptionVisitor {
     reportIssue(expression.operatorToken(), "Remove this increment or correct the code not to waste it.");
   }
 
-  private static boolean isPostfix(ExpressionTree tree) {
+  private static boolean isPostfix(Tree tree) {
     return tree.is(Tree.Kind.POSTFIX_INCREMENT, Tree.Kind.POSTFIX_DECREMENT);
   }
 
